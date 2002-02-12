3021016e68b3369a7914166a7fb4729daf9cf7a7
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-02-12 02:17:56 +0000

    Turning off verbose rule loading.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@46 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WModel.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WModel.java
@@ -349,7 +349,8 @@ public class ERD2WModel extends D2WModel {
     protected void mergeFile(File modelFile) {
         if(cat.isDebugEnabled()) cat.debug("model file being merged = "+modelFile);
         setCurrentFile(modelFile);
-        /* Uncomment this code if rules are not unarchiving correctly */
+        // Uncomment this code if rules are not unarchiving correctly
+        /*
         try {
             NSDictionary dic = Services.dictionaryFromFile(modelFile);
             cat.info("\n\n Got dictionary for file: " + modelFile);
@@ -365,8 +366,8 @@ public class ERD2WModel extends D2WModel {
             }
         } catch (IOException except) {
             cat.error("Bad, bad" + except.getMessage());
-        } 
-        //super.mergeFile(modelFile);
+        } */
+        super.mergeFile(modelFile);
         //uniqueRuleAssignments();
         setCurrentFile(null);
         ERXExtensions.forceGC(1);
