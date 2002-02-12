13781ae4b53f4586344f4f024cadd38a353363f8
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-02-12 23:30:05 +0000

    Moving number formatter subclass down to extensions, also subclassing NSNumberFormatter instead of Format.

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXNumberFormatter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) NetStruxr, Inc. All rights reserved.
+ *
+ * This software is published under the terms of the NetStruxr 
+ * Public Software License version 0.5, a copy of which has been
+ * included with this distribution in the LICENSE.NPL file.  */
+package er.extensions;
+
+import com.webobjects.foundation.*;
+import org.apache.log4j.Category;
+import java.text.*;
+
+// An extension to the simple number formatter that will ignore characters ($%)
+public class ERXNumberFormatter extends NSNumberFormatter {
+
+    //////////////////////////////////////////////  log4j category  //////////////////////////////////////////
+    public final static Category cat = Category.getInstance(ERXNumberFormatter.class);
+
+    protected static ERXNumberFormatter _sharedInstance;
+    public static ERXNumberFormatter sharedInstance() {
+        if (_sharedInstance == null)
+            _sharedInstance = new ERXNumberFormatter();
+        return _sharedInstance;
+    }
+
+    // FIXME: SHould provide api to allow for additions to the list of ignored characters
+    public Object parseObject(String aString) throws java.text.ParseException {
+        char[] chars = aString.toCharArray();
+        char[] filteredChars = new char[chars.length];
+        String ignoredChars = "$%,";
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (ignoredChars.indexOf((int)chars[i]) < 0) {
+                filteredChars[count++] = chars[i];
+            }
+        }
+        String filteredString = new String(filteredChars, 0, count);
+        return parseObject(filteredString);
+    }
+}
