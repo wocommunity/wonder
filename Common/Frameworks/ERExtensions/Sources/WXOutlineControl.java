57b486d7bf7d545c1317ca143887b62ff6a869b3
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-02-19 02:35:24 +0000

    Adding category-like patch to correct an issue with event logging.

---
+++ Common/Frameworks/ERExtensions/Sources/WXOutlineControl.java
@@ -0,0 +1,92 @@
+/*
+ * WXOutlineControl.java
+ * [JavaWOExtensions Project]
+ *
+ * © Copyright 2001 Apple Computer, Inc. All rights reserved.
+ *
+ * IMPORTANT:  This Apple software is supplied to you by Apple Computer,
+ * Inc. (“Apple”) in consideration of your agreement to the following
+ * terms, and your use, installation, modification or redistribution of
+ * this Apple software constitutes acceptance of these terms.  If you do
+ * not agree with these terms, please do not use, install, modify or
+ * redistribute this Apple software.
+ *
+ * In consideration of your agreement to abide by the following terms,
+ * and subject to these terms, Apple grants you a personal, non-
+ * exclusive license, under Apple’s copyrights in this original Apple
+ * software (the “Apple Software”), to use, reproduce, modify and
+ * redistribute the Apple Software, with or without modifications, in
+ * source and/or binary forms; provided that if you redistribute the
+ * Apple Software in its entirety and without modifications, you must
+ * retain this notice and the following text and disclaimers in all such
+ * redistributions of the Apple Software.  Neither the name, trademarks,
+ * service marks or logos of Apple Computer, Inc. may be used to endorse
+ * or promote products derived from the Apple Software without specific
+ * prior written permission from Apple.  Except as expressly stated in
+ * this notice, no other rights or licenses, express or implied, are
+ * granted by Apple herein, including but not limited to any patent
+ * rights that may be infringed by your derivative works or by other
+ * works in which the Apple Software may be incorporated.
+ *
+ * The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ * MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ * THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS
+ * USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ *
+ * IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT,
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+                                        * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+                                        * PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE,
+ * REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE,
+ * HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING
+                                                              * NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.woextensions.*;
+
+// This works around a bug in WXOutlineControl from JavaWOExtensions.  By being in the default package it will be
+// loaded instead of com.webobjects.woextensions.WXOutlineControl
+public class WXOutlineControl extends WOComponent {
+    private int _anchor;
+
+    private static int _counter = 0;
+
+    public WXOutlineControl(WOContext aContext)  {
+        super(aContext);
+        // just a hack to get a unique anchor in a thread safe manner.
+        synchronized(WOApplication.application()) {
+            _counter++;
+            _anchor = _counter;
+        }
+    }
+
+    /////////////
+    // No-Sync
+    ////////////
+    public boolean synchronizesVariablesWithBindings() {
+        return false;
+    }
+
+    // This method causes WOGenericElement/Container to throw a ClassCastException
+    // when bound to the 'name' binding.  Suck.
+    //public int fragmentIdentifier() { return _anchor; }
+    public String fragmentIdentifier() { return new String("" + _anchor); }
+    
+    public WXOutlineEntry currentEntry() {
+        return (WXOutlineEntry)session().objectForKey("_outlineEntry");
+    }
+
+    public int indentation() {
+        return currentEntry().nestingLevel() * 20;
+    }
+
+    public String currentToggleImageName() {
+        return (currentEntry().isExpanded()) ? "DownTriangle.gif": "RightTriangle.gif";
+    }
+}
