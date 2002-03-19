0b4ecbbe1c9164847513a7a660f1cb31543c0f83
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-03-19 14:36:39 +0000

    added ERXOncePerRequestConditional and ERXDHTML component
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@115 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXOncePerRequestConditional.java
@@ -0,0 +1,98 @@
+//
+// ERXOncePerRequestConditional.java: Class file for WO Component 'ERXOncePerRequestConditional'
+// Project simple
+//
+// Created by ak on Tue Mar 19 2002
+//
+package er.extensions;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import er.extensions.*;
+import org.apache.log4j.Category;
+
+/** ERXOncePerRequestConditional is a component that will render it's embedded content only once during the RR loop.
+ *  Useful for JavaScript code that should be included only once.
+ */
+public class ERXOncePerRequestConditional extends ERXStatelessComponent {
+    static final Category cat = Category.getInstance(ERXOncePerRequestConditional.class.getName());
+    
+    String keyName = null;
+    NSMutableDictionary displayCountDict;
+    int stage = 0;
+    String lastID;
+	
+    public ERXOncePerRequestConditional(WOContext context) {
+        super(context);
+    }
+
+    public int displayCountForKey(String key) {
+	return ((Integer)displayCountDict.objectForKey(key)).intValue();
+    }
+    
+    String keyName() {
+	if(!hasBinding("keyName")) {
+	    throw new IllegalStateException("'keyName' is a required binding");
+	}
+	if(keyName == null) {
+	    keyName = (String)valueForBinding("keyName");
+	}
+	return keyName;
+    }
+
+    public void reset() {
+	super.reset();
+	keyName = null;
+    }
+
+    public void awake() {
+	super.awake();
+    }
+
+    public void resetDict(int currentStage, String contextID) {
+	int count = 0;
+	String key = keyName();
+
+	cat.debug(currentStage + "," + contextID);
+
+	
+	if(stage != currentStage || lastID != contextID) {
+	    if(displayCountDict != null)
+		displayCountDict.removeAllObjects();
+	    stage = currentStage;
+	    lastID = contextID;
+	}
+	
+	if(displayCountDict == null) {
+	    displayCountDict = new NSMutableDictionary(new Integer(0), key);
+	} else if(displayCountDict.objectForKey(key) == null) {
+	    displayCountDict.setObjectForKey(new Integer(0),key);
+	} else {
+	    count = ((Integer)displayCountDict.objectForKey(key)).intValue() + 1;
+	    displayCountDict.setObjectForKey(new Integer(count), key);
+	}
+    }
+    
+    public WOActionResults invokeAction(WORequest aRequest, WOContext aContext) {
+	resetDict(0, context().session().sessionID());
+	return super.invokeAction(aRequest,aContext);
+    }
+
+    public void takeValuesFromRequest(WORequest aRequest, WOContext aContext) {
+	resetDict(1, context().session().sessionID());
+	super.takeValuesFromRequest(aRequest,aContext);
+    }
+
+    public void appendToResponse(WOResponse aResponse, WOContext aContext) {
+	resetDict(2, context().session().sessionID());
+	super.appendToResponse(aResponse,aContext);
+    }
+
+    public boolean displayContent() {
+	int showCount = displayCountForKey(keyName());
+	cat.debug("displayContent - showCount: " + showCount + " stage:" + stage);
+	return showCount == 0;
+    }
+}
