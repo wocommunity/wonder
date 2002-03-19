cfcf0d50f3d0f7cae9b17e2f184abac85440eb97
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-03-19 14:36:39 +0000

    added ERXOncePerRequestConditional and ERXDHTML component
    git-svn-id: file:///Users/q/working/wondersvn/trunk@115 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXDHTMLComponent.java
@@ -0,0 +1,29 @@
+//
+// ERXDHTMLComponent.java: Class file for WO Component 'ERXDHTMLComponent'
+// Project simple
+//
+// Created by ak on Tue Mar 19 2002
+//
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import er.extensions.*;
+import org.apache.log4j.Category;
+
+/** ERXDHTMLComponent is covers a textarea with a DHTMLEdit control (IE,PC only)
+  * It is pretty cool as it can be used as a replacement for WOText, since it works no matter is JS is enabled or not.
+  */
+public class ERXDHTMLComponent extends ERXStatelessComponent {
+    static final Category cat = Category.getInstance(ERXDHTMLComponent.class);
+
+    public ERXDHTMLComponent(WOContext context) {
+        super(context);
+    }
+
+    public String varName()  {
+	String varName = (String)valueForBinding("varName");
+	return varName;
+    }
+}
