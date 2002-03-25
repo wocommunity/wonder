8d9ea8e9bf4581d3385183f44e76eb280d85631f
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-03-25 12:58:22 +0000

    initial import
    git-svn-id: file:///Users/q/working/wondersvn/trunk@140 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Utilities/PBXAppleScriptMenuPlugin/PBXTextViewPoser.h
@@ -0,0 +1,18 @@
+//
+//  PBXTextViewPoser.h
+//  CMMJavaMenu
+//
+//  Created by Anjo Krank on Mon Mar 25 2002.
+//  Copyright (c) 2001 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+#define PATCH_SELECTOR @selector(AS_handleMenu:)
+
+@interface PBXTextViewPoser : NSTextView {
+
+}
+
+@end
