665564e0d1b2246ba60442098a0f7523af7966dd
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-03-27 22:36:43 +0000

    removed comments, added compare of NSArray
    git-svn-id: file:///Users/q/working/wondersvn/trunk@166 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Utilities/D2WSorter/main.m
+++ Wonder/Utilities/D2WSorter/main.m
@@ -19,27 +19,25 @@ static int compareDictEntries(id o1, id o2, void *dummy) {
     NSComparisonResult result;
 
     result = [[o1 valueForKeyPath:key1] compare:[o2 valueForKeyPath:key1]];
-    // NSLog(@"%d:%@-%@",result,[o1 valueForKeyPath:key1],[o2 valueForKeyPath:key1]);
     if(result == NSOrderedSame && key2 != nil) {
 	result = [[o1 valueForKeyPath:key2] compare:[o2 valueForKeyPath:key2]];
-	// NSLog(@"  %d:%@-%@",result,[o1 valueForKeyPath:key2],[o2 valueForKeyPath:key2]);
 	if(result == NSOrderedSame && key3 != nil) {
 	    result = [[o1 valueForKeyPath:key3] compare:[o2 valueForKeyPath:key3]];
-	    // NSLog(@"    %d:%@-%@",result,[o1 valueForKeyPath:key3],[o2 valueForKeyPath:key3]);
 	}
     }
     return result;
 }
 
+@implementation NSMutableArray(additions)
+- (NSComparisonResult)compare:(id)other {
+    return [[self description] compare:[other description]];
+}
+@end
+
 @implementation NSMutableDictionary(additions)
 - (NSComparisonResult)compare:(id)other {
     return [[self description] compare:[other description]];
 }
-
-/*
-- (id) author {
-    return [NSDecimalNumber decimalNumberWithString:[self objectForKey:@"author"]];
-}*/
 @end
 
 int main (int argc, const char * argv[]) {
@@ -49,13 +47,11 @@ int main (int argc, const char * argv[]) {
     id plist;
     NSString *appName;
     
-    // insert code here...
     if ([arguments count] > 0) {
         appName=[arguments objectAtIndex:0];
     }
     if ([arguments count] < 2) {
-
-        NSLog(@"Usage: D2WSorter <d2wmodel> key1 [key2 [key3]]\n keyN = author|rhs|lhs");
+        NSLog(@"Usage: %@ <d2wmodel> key1 [key2 [key3]]\n keyN = author|rhs|lhs", appName);
         return 1;
     }
     key1 = [arguments objectAtIndex:2];
@@ -63,16 +59,14 @@ int main (int argc, const char * argv[]) {
 	key2 = [arguments objectAtIndex:3];
     if([arguments count] > 4)
 	key3 = [arguments objectAtIndex:4];
-    // NSLog(@"%@,%@,%@", key1, key2, key3);
    
     plist = [NSDictionary dictionaryWithContentsOfFile:[arguments objectAtIndex:1]];
-    // NSLog([plist description]);
 
     [plist setObject:[[plist objectForKey:@"rules"] sortedArrayUsingFunction:compareDictEntries context:nil] forKey:@"rules"];
 
     [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithInt:1] forKey:@"NSWriteOldStylePropertyLists"];
     [plist writeToFile:[arguments objectAtIndex:1] atomically:NO];
-    // NSLog([[[[plist objectForKey:@"rules"] objectAtIndex:0] valueForKeyPath:key2] description]);
+
     [pool release];
     return 0;
 }
