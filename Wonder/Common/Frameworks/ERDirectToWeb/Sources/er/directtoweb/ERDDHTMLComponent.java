0e940776cfbe3601da6f3c8db31e9f3e861c8262
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-03-28 14:32:49 +0000

    added ERDDHTMLComponent
    git-svn-id: file:///Users/q/working/wondersvn/trunk@168 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDDHTMLComponent.java
@@ -0,0 +1,41 @@
+//
+// ERDDHTMLComponent.java: Class file for WO Component 'ERDDHTMLComponent'
+// Project simple
+//
+// Created by ak on Wed Mar 20 2002
+//
+package er.directtoweb;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import er.extensions.*;
+import er.directtoweb.*;
+import org.apache.log4j.Category;
+
+public class ERDDHTMLComponent extends ERDCustomEditComponent {
+    static final Category cat = Category.getInstance(ERDDHTMLComponent.class);
+
+    String varName = null;
+    
+    public ERDDHTMLComponent(WOContext context) {
+        super(context);
+    }
+
+    public boolean isStateless() {
+	return true;
+    }
+
+    public void reset() {
+        varName = null;
+    }
+    
+    public String varName()  {
+	if(varName == null) {
+	    varName = ERXExtensions.replaceStringByStringInString("-", "_", "dhtml-" + context().elementID().hashCode() + "-" + key());
+	    cat.info(varName);
+	}
+	return varName;
+    }
+}
