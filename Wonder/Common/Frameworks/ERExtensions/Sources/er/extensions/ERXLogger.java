4b30bacaf64784e61303138d4240552d653093e9
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-04-10 12:19:56 +0000

    Logging revisited. I hope it works correctly *this* time.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@213 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXLogger.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXLogger.java
@@ -13,6 +13,24 @@ import org.apache.log4j.*;
 import org.apache.log4j.spi.*;
 
 public class ERXLogger extends org.apache.log4j.Category {
+    public static Category cat;
+
+    static {
+        // (ak): Why, just why do we need this
+        Category.defaultHierarchy.setCategoryFactory(new ERXLogger.Factory());
+        ERXLog4j.configureLogging();
+        cat = Category.getInstance("er.utilities.log4j.ERXLogger");
+    }
+
+
+    public static class Factory implements org.apache.log4j.spi.CategoryFactory {
+        public Category makeNewCategoryInstance(String name) {
+            if(cat != null && cat.isDebugEnabled())
+                cat.debug("makeNewCategoryInstance: " + name);
+            return new ERXLogger(name);
+        }
+    }
+
     public ERXLogger(String name) {
         super(name);
     }
@@ -20,7 +38,7 @@ public class ERXLogger extends org.apache.log4j.Category {
     public static ERXLogger getLogger(String name) {
         org.apache.log4j.Category logger = org.apache.log4j.Category.getInstance(name);
         if(logger != null && !(logger instanceof ERXLogger))
-            throw new RuntimeException("Can't load Logger for \""+name+"\" because it is not of class ERXLogger but \""+logger.getClass().getName()+"\". Let your Application class inherit from ERXApplication or call ERXLog4j.configureLogging() statically the first thing in your app.");
+            throw new RuntimeException("Can't load Logger for \""+name+"\" because it is not of class ERXLogger but \""+logger.getClass().getName()+"\". Let your Application class inherit from ERXApplication or call ERXLog4j.configureLogging() statically the first thing in your app. \nAlso check if there is a \"log4j.categoryFactory=er.extensions.ERXLogger$Factory\" line in your properties.");
         return (ERXLogger)logger;
     }
 
