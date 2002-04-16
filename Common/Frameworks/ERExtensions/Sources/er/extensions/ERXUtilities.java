35ff9623eab88418611c48f94404041b9cf17661
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-04-16 11:39:06 +0000

    fixed frameworks to be ordered. now ERExtensions comes before ERDirectoweb

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXUtilities.java
@@ -279,7 +279,6 @@ public class ERXUtilities {
             else
                 cat.warn("Null framework name for bundle: " + bundle);
         }
-        cat.info(frameworkNames);
         return frameworkNames;
     }
 
