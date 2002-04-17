b07ef88c2c12872a173d8e78172fd381ad019036
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-04-17 01:08:04 +0000

    ERDLocalizedAssignment added

---
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDLocalizedAssignment.java
@@ -0,0 +1,48 @@
+//
+// ERDLocalizabledAssignment.java
+// Project ERDirectToWeb
+//
+// Created by ak on Wed Apr 17 2002
+//
+package er.directtoweb;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.directtoweb.*;
+import er.extensions.*;
+
+public class ERDLocalizedAssignment extends ERDAssignment implements ERDLocalizableAssignmentInterface, ERDComputingAssignmentInterface {
+    static final ERXLogger log = ERXLogger.getLogger(ERDLocalizedAssignment.class);
+
+    public ERDLocalizedAssignment (EOKeyValueUnarchiver u) { super(u); }
+    public ERDLocalizedAssignment (String key, Object value) { super(key,value); }
+
+    public static final NSArray _DEPENDENT_KEYS=new NSArray(new String[] { "propertyKey"});
+    public NSArray dependentKeys(String keyPath) {
+        return _DEPENDENT_KEYS;
+    }
+    public Object localizedValueForKey(D2WContext c) {
+        return localizedValueForKeyInContext((String)value(), c);
+    }
+    public Object localizedValueForKeyWithDefault(D2WContext c) {
+        return localizedValueForKeyWithDefaultInContext((String)value(), c);
+    }
+    public Object localizedValueForKeyPath(D2WContext c) {
+        return localizerForContext(c).valueForKeyPath((String)value());
+    }
+    public Object displayNameForProperty(D2WContext c) {
+        log.info("fire.displayNameForProperty:" + "--" + "- " + value() + " - " + c);
+        String value = ERD2WUtilities.displayNameForKey((String)c.valueForKey("propertyKey"));
+        return localizedValueForKeyWithDefaultInContext(value, c);
+    }
+
+    public ERXLocalizer localizerForContext(D2WContext c) {
+        return (ERXLocalizer) c.valueForKeyPath("session.localizer");
+    }
+    
+    public Object fire(D2WContext c) {
+        System.out.println("fire.fire:" + keyPath() + "- " + value() + " - " + c);
+        return localizerForContext(c).valueForKeyPath((String)value());
+    }
+}
