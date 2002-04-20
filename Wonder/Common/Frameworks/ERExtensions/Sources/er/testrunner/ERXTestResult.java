e8acb576035ac17bbef2725ddb551542c1f09ae5
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-04-20 00:35:32 +0000

    Adding support for PBX tools from WOExtensions to the unit tests
    git-svn-id: file:///Users/q/working/wondersvn/trunk@265 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/testrunner/ERXTestResult.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/testrunner/ERXTestResult.java
@@ -14,7 +14,6 @@ import java.io.PrintStream;
 import java.util.*;
 
 public class ERXTestResult extends TestResult {
-
     /////////////////////////////////////  log4j category  /////////////////////////////////////
     public static final Category cat = Category.getInstance(ERXTestResult.class);
 
@@ -26,7 +25,7 @@ public class ERXTestResult extends TestResult {
         _errors = new NSMutableArray();
         _failures = new NSMutableArray();
     }
-    
+
     public synchronized void addError(Test test, Throwable t) {
         super.addError(test, t);
         _errors.addObject(fErrors.lastElement());
