583ee4fa21eb4a455ed080a96a910d63fbe56b4b
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-04-30 21:53:56 +0000

    JavaDoc
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@321 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXApplication.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXApplication.java
@@ -14,23 +14,45 @@ import com.webobjects.appserver.*;
 import java.util.*;
 import org.apache.log4j.Category;
 
+/**
+ *  ERXApplication is the abstract superclass of WebObjects applications
+ *  built with the ER frameworks.<br/>
+ *  <br/>
+ *  Useful enhancements include the ability to change the deployed name of
+ *  the application, support for automatic application restarting at given intervals
+ *  and more context information when handling exceptions.
+ */
+
 public abstract class ERXApplication extends WOApplication {
-    
-    //////////////////////////////////////////////  log4j category  ////////////////////////////////////////////
+
+    /** logging support */
     public static final ERXLogger cat = ERXLogger.getLogger(ERXApplication.class);
 
-    ////////////////////////////////////////////// Notification Hooks //////////////////////////////////////////
-    // A few undocumented notifications that are nice ;)
+    // FIXME: Should correct all references to WORequestHandler.DidHandleRequestNotification and then delete this ivar. 
     public static final String WORequestHandlerDidHandleRequestNotification = "WORequestHandlerDidHandleRequestNotification";
-    public static final String ApplicationWillDispatchRequestNotification = "WOApplicationWillDispatchRequestNotification";
-    public static final String ApplicationDidDispatchRequestNotification = "WOApplicationDidDispatchRequestNotification";
-    
 
-    //------------------------------------------------------
-    // Application Cycling
+    /**
+     * The ERXApplication singleton.
+     * @return returns the <code>WOApplication.application()</code> cast as an ERXApplication
+     */
+    public static ERXApplication erxApplication() { return (ERXApplication)WOApplication.application(); }
+    
+    /**
+     *  Adds support for automatic application cycling. Applications can be configured
+     *  to cycle in two ways:<br/>
+     *  <br/>
+     *  The first way is by setting the System property <b>ERTimeToLive</b> to the number
+     *  of seconds that the application should be up before terminating. Note that when
+     *  the application's time to live is up it will quit calling the method <code>killInstance</code>.<br/>
+     *  <br/>
+     *  The second way is by setting the System property <b>ERTimeToDie</b> to the number
+     *  of seconds that the application should be up before starting to refuse new sessions.
+     *  In this case when the application starts to refuse new sessions it will also register
+     *  a kill timer that will terminate the application between 30 minutes and 1:30 minutes.<br/>
+     */
     public void run() {
         int timeToLive=ERXProperties.intForKey("ERTimeToLive");
-        if (timeToLive!=0) {
+        if (timeToLive > 0) {
             cat.info("Instance will live "+timeToLive+" seconds.");
             NSLog.out.appendln("Instance will live "+timeToLive+" seconds.");
             NSTimestamp now=new NSTimestamp();
@@ -39,8 +61,7 @@ public abstract class ERXApplication extends WOApplication {
             t.schedule();
         }
         int timeToDie=ERXProperties.intForKey("ERTimeToDie");
-        if (timeToDie!=0) {
-            // FIXME we could randomize this (30mn?) to help with restart related downtime problems?
+        if (timeToDie > 0) {
             cat.info("Instance will not live past "+timeToDie+":00.");
             NSLog.out.appendln("Instance will not live past "+timeToDie+":00.");
             NSTimestamp now=new NSTimestamp();
@@ -57,6 +78,12 @@ public abstract class ERXApplication extends WOApplication {
         super.run();
     }
 
+    /**
+     *  Stops the application from handling any new requests. Will still handle
+     *  requests from existing sessions. Also registers a kill timer that will
+     *  terminate the application thirty minutes from the time this method is
+     *  called
+     */
     public void startRefusingSessions() {
         cat.info("Refusing new sessions");
         NSLog.out.appendln("Refusing new sessions");
@@ -68,17 +95,29 @@ public abstract class ERXApplication extends WOApplication {
         t.schedule();
     }
 
+    /**
+     *  Killing the instance will log a 'Forcing exit' message and then call <code>System.exit(1)</code>
+     */
     public void killInstance() {
         cat.info("Forcing exit");
         NSLog.out.appendln("Forcing exit");
         System.exit(1);
     }
-
-    //-------------------------------------------------------------------------------------------------
-    // ability to change your name -- useful for training instances
-    // this may not even be needed if you don't use Monitor
+    /** cached name suffix */
     private String _nameSuffix;
+    /** has the name suffix been cached? */
     private boolean _nameSuffixLookedUp=false;
+    /**
+     *  The name suffix is appended to the current name of the application. This adds the ability to
+     *  add a useful suffix to differentuate between different sets of applications on the same machine.<br/>
+     *  <br/>
+     *  The name suffix is set via the System property <b>ERApplicationNameSuffix</b>.<br/>
+     *  <br/>
+     *  For example if the name of an application is Buyer and you want to have a training instance
+     *  appear with the name BuyerTraining then you would set the ERApplicationNameSuffix to Training.<br/>
+     *  <br/>
+     *  @return the System property <b>ERApplicationNameSuffix</b> or <code>null</code>
+     */
     public String nameSuffix() {
         if (!_nameSuffixLookedUp) {
             _nameSuffix=System.getProperty("ERApplicationNameSuffix");
@@ -87,8 +126,14 @@ public abstract class ERXApplication extends WOApplication {
         }
         return _nameSuffix;
     }
-    
+    /** cached computed name */
     private String _userDefaultName;
+    /**
+     *  Adds the ability to completely change the applications name by setting the System property
+     *  <b>ERApplicationName</b>. Will also append the <code>nameSuffix</code> if one is set.<br/>
+     *  <br/>
+     *  @return the computed name of the application.
+     */
     public String name() {
         if (_userDefaultName==null) {
             _userDefaultName=System.getProperty("ERApplicationName");
@@ -102,10 +147,26 @@ public abstract class ERXApplication extends WOApplication {
         return _userDefaultName;
     }
 
+    /**
+     *  This method returns {@link WOApplication}'s <code>name</code> method.<br/>
+     *  @return the name of the application executable. 
+     */
     public String rawName() { return super.name(); }
 
-    public static ERXApplication erxApplication() { return (ERXApplication)WOApplication.application(); }
-
+    /**
+     *  Adds a bunch of useful information relative to the current state when the exception
+     *  occurried. Potentially added information:<br/>
+     * <ol>
+     * <li>the current page name</li>
+     * <li>the current component</li>
+     * <li>the complete hierarchy of nested components</li>
+     * <li>the requested uri</li>
+     * <li>the D2W page configuration</li>
+     * <li>the previous page list (from the WOStatisticsStore)</li>
+     * </ol>
+     * <br/>
+     * @return the WOResponse of the generated exception page.
+     */
     public WOResponse handleException(Exception exception, WOContext context) {
         NSMutableDictionary extraInfo=new NSMutableDictionary();
         if (context!=null && context.page()!=null) {
@@ -136,30 +197,33 @@ public abstract class ERXApplication extends WOApplication {
         cat.warn("Exception caught, " + exception.getMessage() + " extra info: " + extraInfo);
         return super.handleException(exception, context);
     }    
-    
-    // backstop wrapper for notifications
-    public String mailWrapperName() { return "PageWrapper"; }
 
-    // FIXME: Need to get rid of these now.
+    // DELETE ME
+     public String mailWrapperName() { return "PageWrapper"; }
+
+    // DELETE ME
     protected D2WContext _d2wContext;
+    // DELETE ME
     public D2WContext d2wContext() {
         if (_d2wContext == null)
             _d2wContext = new D2WContext();
         return _d2wContext;
     }
 
-    // for subclasses to override
+    // DELETE ME
     public void resetSignificantKeys() {}
+    // DELETE ME
     public void warmUpRuleCache() {
         // Moved to ERDirectToWeb
         //ERD2WModel.erDefaultModel().prepareDataStructures();
     }
 
+    // DELETE ME
     public ERXSimpleHTMLFormatter formatter() { return ERXSimpleHTMLFormatter.formatter(); }
    
+    // DELETE ME
     public WOComponent pageWithName(String name) {
         // Was: pageWithName(name, new WOContext());
         return pageWithName(name, null);
     }
-
 }
