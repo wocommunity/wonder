9d165bdf6f9de3857b9096f22d8bb56f22b73773
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-05-01 02:15:02 +0000

    More JavaDoc
    git-svn-id: file:///Users/q/working/wondersvn/trunk@325 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRetainer.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRetainer.java
@@ -7,17 +7,40 @@
 package er.extensions;
 
 import com.webobjects.foundation.*;
-import org.apache.log4j.Category;
 
-// This is used to to retain pure Java objects on the objC side of things so that they can recieve notifications.
+/**
+ * Used as a generic way to retain a reference to an object so that it will
+ * not be collected by the garbage collector. This class is most often used
+ * to retain objects used to observe {link @NSNotification}s.<br/>
+ * <br/>
+ * Note that the current implementation does not implement reference counting
+ * so calling retain multiple times on the same object does not have any effect
+ * after the first call.
+ */
 public class ERXRetainer {
 
-    ///////////////////////////////////  log4j category  ///////////////////////////////////////////////
-    public final static Category cat = Category.getInstance(ERXRetainer.class);
-
+    /** set used to retain references to objects */
     private static NSMutableSet _retainerSet = new NSMutableSet();
-
-    public static void retain(Object object) { _retainerSet.addObject(object); }
-    public static void release(Object object) { _retainerSet.removeObject(object); }
-    public static boolean isObjectRetained(Object object) { return _retainerSet.containsObject(object); }
+    /**
+     * Retains a reference to the object.
+     * @param object object to be retained.
+     */
+    public static void retain(Object object) {
+        _retainerSet.addObject(object);
+    }
+    /**
+     * Releases the reference to the object.
+     * @param object object to be released.
+     */
+    public static void release(Object object) {
+        _retainerSet.removeObject(object);
+    }
+    /**
+     * Tests if the given object is being retained by the ERXRetainer class.
+     * @param object object to be tested.
+     * @return returns if the given object is currently retained.
+     */
+    public static boolean isObjectRetained(Object object) {
+        return _retainerSet.containsObject(object);
+    }
 }
