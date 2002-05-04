838fb7153615ee12ae366de1b32b7668adf95796
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-05-04 18:44:48 +0000

    ERXThreadLocalMap -> ERXCloneableThreadLocal now works with all objects that implement the cloneable interface.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@338 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXCloneableThreadLocal.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) NetStruxr, Inc. All rights reserved.
+ *
+ * This software is published under the terms of the NetStruxr
+ * Public Software License version 0.5, a copy of which has been
+ * included with this distribution in the LICENSE.NPL file.  */
+package er.extensions;
+
+import java.util.Map;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * <code>ERXClonableThreadLocal</code> extends {@link InheritableThreadLocal}
+ * to bequeath a cloned copy of the parent object to the child thread.<br/>
+ * <br/>
+ * Note: Objects used with this thread local must implement the {@link Cloneable}
+ * interface and have a public <code>clone</code> method.
+ */
+public class ERXCloneableThreadLocal extends InheritableThreadLocal {
+
+    /** logging support */
+    protected static final ERXLogger log = ERXLogger.getLogger(ERXClonableThreadLocal.class);
+
+    /**
+     * Clones a copy of the parent object for the child thread.
+     * The parentValue must implement the {@link Cloneable}
+     * interface and have a public <code>clone</code> method.
+     * @param parentValue local object to the parent thread.
+     * @retrun a cloned value of the parent if not null.
+     */
+    protected Object childValue(Object parentValue) {
+        Object child = null;
+        if (parentValue != null) {
+            if (!(parentValue instanceof Cloneable))
+                throw new IllegalStateException("Using a ERXThreadLocalMap with an object: " 
+                + parentValue.toString() + " that does not implement the Map interface ");
+            // This is very lame. clone() is a protected method off of object and the Cloneable
+            // interface doesn't specify any methods.
+            try {
+                Method m = parentValue.getClass().getMethod("clone", ERXConstant.EmptyClassArray);
+                child = m.invoke(parentValue, ERXConstant.EmptyObjectArray);
+            } catch (InvocationTargetException ite) {
+                log.error("Invocation exception occurred when invoking clone in ERXClonableThreadLocal:" 
+                        + ite.getTargetException() + " backtrace: " + ERXUtilities.stackTrace(ite.getTargetException()));
+            } catch (NoSuchMethodException nsme) {
+                log.error("No clone method for the class: " + parentValue.getClass() + " very strange.");
+            } catch (IllegalAccessException iae) {
+                log.error("Clone method has protected or private access for the object: "
+                        + parentValue + " exception: " + iae.getMessage());
+            }
+        }            
+        return child;
+    }
+}
