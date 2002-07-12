84d503a0cc66c92f6e3d0e4537697f1fcb9fcde8
patrice_gautier <patrice_gautier@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-12 18:29:43 +0000

    Initial revision

---
+++ Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUInspectPage.java
@@ -0,0 +1,24 @@
+//
+// ERNEUInspectPage.java: Class file for WO Component 'ERNEUInspectPage'
+// Project ERNeutralLook
+//
+// Created by patrice on Mon Jun 03 2002
+//
+
+package er.neutral;
+
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+import er.directtoweb.*;
+
+public class ERNEUInspectPage extends ERD2WInspectPage {
+
+    public ERNEUInspectPage(WOContext context) {
+        super(context);
+    }
+
+}
