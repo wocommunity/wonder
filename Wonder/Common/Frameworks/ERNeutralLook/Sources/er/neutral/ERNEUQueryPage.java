c6f3f0a776c31c4f9e494e11fab31c077d47f4d1
(no author) <(no author)@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-12 18:29:43 +0000

    This commit was manufactured by cvs2svn to create branch 'patrice'.
    git-svn-id: file:///Users/q/working/wondersvn/branches/patrice@459 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUQueryPage.java
@@ -0,0 +1,24 @@
+//
+// ERNEUQueryPage.java: Class file for WO Component 'ERNEUQueryPage'
+// Project ERNeutralLook
+//
+// Created by patrice on Mon Jun 03 2002
+//
+
+package er.neutral;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+import er.directtoweb.*;
+import er.extensions.*;
+
+public class ERNEUQueryPage extends ERD2WQueryPage {
+
+    public ERNEUQueryPage(WOContext context) {
+        super(context);
+    }
+
+}
