a694fe979288a2c91ffe4af6d34f88ba7129163f
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-21 15:43:57 +0000

    added @sort operator variants, added @flatten and @unique

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXArrayUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXArrayUtilities.java
@@ -19,6 +19,49 @@ import java.util.Enumeration;
  */
 public class ERXArrayUtilities extends Object {
     /**
+    * Recursively flattens an array of arrays into a single
+     * array of elements.<br/>
+     * <br/>
+     * For example:<br/>
+     * <code>NSArray foos;</code> //Assume exists<br/>
+     * <code>NSArray bars = (NSArray)foos.valueForKey("toBars");</code>
+     * In this case if <code>foos</code> contained five elements
+     * then the array <code>bars</code> will contain five arrays
+     * each corresponding to what <code>aFoo.toBars</code> would
+     * return. To have the entire collection of <code>bars</code>
+     * in one single arra you would call:
+     * <code>NSArray allBars = flatten(bars)</code>
+     * @param array to be flattened
+     * @return an array containing all of the elements from
+     *		all of the arrays contained within the array
+     *		passed in.
+     */
+    // ENHANCEME: Should add option to filter duplicates
+    public static NSArray flatten(NSArray array) {
+        NSMutableArray newArray=null;
+        for (int i=0; i<array.count(); i++) {
+            Object element=array.objectAtIndex(i);
+            if (element instanceof NSArray) {
+                if (newArray==null) {
+                    newArray=new NSMutableArray();
+                    for (int j=0; j<i; j++) {
+                        if(array.objectAtIndex(j)!=null){
+                            newArray.addObject(array.objectAtIndex(j));
+                        }
+                    }
+                }
+                NSArray a=flatten((NSArray)element);
+                for (int j=0; j<a.count();j++) {
+                    if(a.objectAtIndex(j)!=null){
+                        newArray.addObject(a.objectAtIndex(j));
+                    }
+                }
+            }
+        }
+        return (newArray !=null) ? newArray : array;
+    }
+
+    /**
      * Creates an NSArray from a resource associated with a given bundle
      * that is in property list format.<br/>
      * @param name name of the file or resource.
@@ -53,17 +96,18 @@ public class ERXArrayUtilities extends Object {
      * @return mutable clone of sorted array.
      */
     public static NSMutableArray sortedMutableArraySortedWithKey(NSArray array, String key) {
-        return sortedArraySortedWithKey(array, key).mutableClone();
+        return sortedArraySortedWithKey(array, key, null).mutableClone();
     }
     
     /**
      * Sorts a given array with a key in ascending fashion.
      * @param array array to be sorted.
      * @param key sort key.
+     * @param selector sort order selector to use, if null, then sort will be ascending.
      * @return sorted array.
      */
-    public static NSArray sortedArraySortedWithKey(NSArray array, String key) {
-        NSArray order=new NSArray(new Object[] {EOSortOrdering.sortOrderingWithKey(key, EOSortOrdering.CompareAscending)});
+    public static NSArray sortedArraySortedWithKey(NSArray array, String key, NSSelector selector) {
+        NSArray order=new NSArray(new Object[] {EOSortOrdering.sortOrderingWithKey(key, selector == null ? EOSortOrdering.CompareAscending : selector)});
         return EOSortOrdering.sortedArrayUsingKeyOrderArray(array, order);
     }
 
@@ -78,8 +122,12 @@ public class ERXArrayUtilities extends Object {
      */
     static class SortOperator implements NSArray.Operator
     {
+        private NSSelector selector;
+        
         /** public empty constructor */
-	public SortOperator() {}
+	public SortOperator(NSSelector selector) {
+            this.selector = selector;
+        }
 
         /**
          * Sorts the given array by the keypath.
@@ -90,7 +138,7 @@ public class ERXArrayUtilities extends Object {
 	public Object compute(NSArray array, String keypath) {
             if (array.count() < 2)
                 return array;
-	    return sortedArraySortedWithKey(array, keypath);
+	    return sortedArraySortedWithKey(array, keypath, selector);
 	}
     }
 
@@ -120,14 +168,84 @@ public class ERXArrayUtilities extends Object {
                 return array;
             }
             EOEnterpriseObject eo = (EOEnterpriseObject)array.objectAtIndex(0);
-	    return filteredArrayWithFetchSpecificationNamedEntityNamed(array, keypath, eo.entityName());
-	}
+            return filteredArrayWithFetchSpecificationNamedEntityNamed(array, keypath, eo.entityName());
+        }
     }
 
-    /** Registers the operators <b>sort</b> and <b>fetchSpec</b> during class initialization. */
+    /**
+    * Define an {@link NSArray$Operator} for the key <b>flatten</b>.<br/>
+     * <br/>
+     * This allows for key value paths like:<br/>
+     * <br/>
+     * <code>myArray.valueForKey("@flatten");</code><br/>
+     * <br/>
+     * Which in this case would return myArray flattened.
+     */
+    static class FlattenOperator implements NSArray.Operator {
+        /** public empty constructor */
+        public FlattenOperator() {}
+
+        /**
+        * Flattens the given array.
+         * @param array array to be filtered.
+         * @param keypath name of fetch specification.
+         * @return immutable filtered array.
+         */
+        public Object compute(NSArray array, String keypath) {
+            return flatten(array);
+        }
+    }
+
+    /**
+    * Define an {@link NSArray$Operator} for the key <b>flatten</b>.<br/>
+     * <br/>
+     * This allows for key value paths like:<br/>
+     * <br/>
+     * <code>myArray.valueForKey("@flatten");</code><br/>
+     * <br/>
+     * Which in this case would return myArray flattened.
+     */
+    static class UniqueOperator implements NSArray.Operator {
+        /** public empty constructor */
+        public UniqueOperator() {}
+
+        /**
+        * Flattens the given array.
+         * @param array array to be filtered.
+         * @param keypath name of fetch specification.
+         * @return immutable filtered array.
+         */
+        public Object compute(NSArray array, String keypath) {
+            return arrayWithoutDuplicates(array);
+        }
+    }
+
+    /** Registers the operators <b>sort<</b> and <b>fetchSpec</b> during class initialization. */
     static {
-        NSArray.setOperatorForKey("sort", new SortOperator());
+        NSArray.setOperatorForKey("sort", new SortOperator(EOSortOrdering.CompareAscending));
+        NSArray.setOperatorForKey("sortAsc", new SortOperator(EOSortOrdering.CompareAscending));
+        NSArray.setOperatorForKey("sortDesc", new SortOperator(EOSortOrdering.CompareDescending));
+        NSArray.setOperatorForKey("sortInsensitiveAsc", new SortOperator(EOSortOrdering.CompareCaseInsensitiveAscending));
+        NSArray.setOperatorForKey("sortInsensitiveDesc", new SortOperator(EOSortOrdering.CompareCaseInsensitiveDescending));
+        NSArray.setOperatorForKey("flatten", new FlattenOperator());
         NSArray.setOperatorForKey("fetchSpec", new FetchSpecOperator());
+        NSArray.setOperatorForKey("unique", new UniqueOperator());
+    }
+
+
+    /**
+    * Filters out all of the duplicate objects in
+     * a given array.<br/>
+     * Note: The current implementation does not preserve
+     * 		the order of elements in the array.
+     * @param anArray to be filtered
+     * @return filtered array.
+     */
+    // FIXME: Does not preserve array order
+    public static NSArray arrayWithoutDuplicates(NSArray anArray) {
+        NSMutableSet aSet = new NSMutableSet();
+        aSet.addObjectsFromArray(anArray);
+        return aSet.allObjects();
     }
 
     /**
