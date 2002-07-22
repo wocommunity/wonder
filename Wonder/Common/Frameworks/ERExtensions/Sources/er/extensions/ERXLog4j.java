fe1ca6cc536afe9d26ca018379e1817968dba505
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-22 16:25:39 +0000

    JavaDoc
    git-svn-id: file:///Users/q/working/wondersvn/trunk@518 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXLog4j.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXLog4j.java
@@ -7,93 +7,156 @@
 package er.extensions;
 
 import com.webobjects.foundation.*;
-import com.webobjects.eocontrol.*;
-import com.webobjects.eoaccess.*;
-import com.webobjects.appserver.*;
+import com.webobjects.appserver.WOApplication;
 import java.util.*;
 import java.io.*;
 import org.apache.log4j.*;
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// This class is used to configure the log4j system.  By calling ERXLog4j.configureLogging() will cause the log4j
-// configuration file to be loaded.  The file path is specified in a defaults write of ERConfigurationPath.
-// ConfigureLogging can be called many times as it will only be configured the first time.
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This class is used to configure the log4j system.  By calling
+ * ERXLog4j.configureLogging() will cause the log4j configuration
+ * file to be loaded.  The file path is specified in the 
+ * property ERConfigurationPath or the WebObjects.properties found
+ * in the current users home directory. ConfigureLogging can be called many
+ * times as it will only be configured the first time.
+ */
+// CHECKME: Some of this (if not all) can go away now that we are in WO 5 and the property system is
+//	    actually integrated into the runtime.
 public class ERXLog4j {
+    
+    /**
+     * We want to make sure that when this class is called
+     * it will initialize the the class {@link ERXLogger}.
+     */
     static {
         ERXLogger.class.getName();
     }
-    
-    ///////////////////////////////////////////////  log4j category  ////////////////////////////////////////////
+
+    /** logging support */
     public static Category cat;
 
-    /////////////////////////////////////////////// Notification Titles /////////////////////////////////////////
+    /**
+     * holds the notification that is posted after
+     * the configuration system is updated.
+     */
     public static final String ConfigurationDidChangeNotification = "ConfigurationDidChangeNotification";
 
+    /** holds a reference to the current configuration file */
     private static String _configurationFilePath;
+
+    /** records if the logging system has been configured */
+    private static boolean _isLoggingConfigured=false;
+
+    /** records if the logging system has been initialized */
+    private static boolean _isInitialized=false;
+
+    /** records if rapid turnaround has been enabled */
+    private static boolean _initializedRapidTurnAround=false;
+
+    /**
+     * Public inner class used for rapid turn around mode. This
+     * observer listens for changes made to the configuration
+     * file. When changes occur the configuration is reloaded.
+     */
+    public static class Observer {
+
+        /**
+         * Method invoked by the notification center when
+         * the configuration file changes.
+         * @param n current notification
+         */
+        public void reloadConfigurationFile(NSNotification n) {
+            ERXLog4j.loadConfiguration();
+        }
+    }    
+
+    /**
+     * Cover method for returning the current configuration
+     * file path.
+     * @return current configuration file path
+     */
     public static String configurationFilePath() { return _configurationFilePath; }
+
+    /**
+     * Cover method used for setting the current configuration
+     * path to be used.
+     * @param value current configuration file path
+     */
     public static void setConfigurationFilePath(String value) {
         _configurationFilePath = value;
     }
 
-    private static boolean _isLoggingConfigured=false;
+    /**
+     * Cover method that returns if the logging system is configured.
+     * @return if the logging system has been configured
+     */
     public static boolean isLoggingConfigured() { return _isLoggingConfigured; }
+
+    /**
+     * Sets if the logging system has been configured.
+     * @param value if the logging sytem is configured
+     */
     public static void setIsLoggingConfigured(boolean value) { _isLoggingConfigured = value; }
 
-    private static boolean _isInitialized=false;
+    /**
+     * The entry point for setting up the logging system.
+     * This method should be called as early as possible,
+     * the best spot being in a static block of the
+     * application class. Calling this method multiple times
+     * will not have any additional effect after the first call.
+     */
     public static void configureLogging() {
         if (!_isInitialized) {
-            // This must be passed in via the command line.  This will be useful for variable substitution in a
-            // log4j config file
-            if (System.getProperty("ERLogApplicationName") != null)
-                ERXProperties.setStringForKey(System.getProperty("ERLogApplicationName"), "er.applicationName");
-            String configurationPath=ERXProperties.stringForKey("ERConfigurationPath");
+            String configurationPath=System.getProperty("ERConfigurationPath");
             if (configurationPath==null) {
                 File mainProperties=new File(System.getProperty("user.home"),
                                              "WebObjects.properties");
                 configurationPath=mainProperties.getPath();
             }
-            setConfigurationFilePath(configurationPath);
-            loadConfiguration();
-            cat = Category.getInstance("er.utilities.log4j.ERXLog4j");
-            cat.info("Log4j configured.");
+            if (configurationPath != null) {
+                setConfigurationFilePath(configurationPath);
+                loadConfiguration();
+                cat = Category.getInstance(ERXLog4j.class);
+                cat.info("Logging system configured.");
+            } else {
+                // ENHANCEME: Should configure the BasicConfigurator at this point
+            }
             setIsLoggingConfigured(true);
             _isInitialized = true;
         }
     }
 
-    public static class Observer {
-        public void reloadConfigurationFile(NSNotification n) {
-            ERXLog4j.loadConfiguration();
-        }        
-    }
-    // This is used in developement.  If WOCaching is not enabled then an observer will be created and register for the
-    // notification: WORequestHandlerDidHandleRequestNotification
-    // Note that this needs to be called at a latter point after usually called by the notification WOApplicationDidFinishLaunchingNotification
-    private static boolean _initializedRapidTurnAround=false;
+    /**
+     * This method is used to configure the log4j system
+     * for rapid turnaround mode. Rapid turnaround mode
+     * will only be enabled if a configuration file is set
+     * and the system has WOCaching disabled.
+     */
     public static void configureRapidTurnAround() {
         if (!_initializedRapidTurnAround) {
-            if (!WOApplication.application().isCachingEnabled()) {
+            if (!WOApplication.application().isCachingEnabled() && configurationFilePath() != null) {
                 if (cat.isDebugEnabled()) cat.debug("Registering observer for file change.");
-                ERXFileNotificationCenter.defaultCenter().addObserver(new Observer(),
-                                                                new NSSelector("reloadConfigurationFile",
-                                                                               ERXConstant.NotificationClassArray),
-                                                                configurationFilePath());
+                Observer o = new Observer();
+                ERXRetainer.retain(o);
+                ERXFileNotificationCenter.defaultCenter().addObserver(o,
+                                                                      new NSSelector("reloadConfigurationFile",
+                                                                                     ERXConstant.NotificationClassArray),
+                                                                      configurationFilePath());
             }
             _initializedRapidTurnAround = true;
         }
     }
 
-    // This loads and configures the Log4j system and posts the notification: Log4jConfigurationDidChangeNotification
-    //private static long _lastModifiedDate = 0;
-    // Now this is only called when the config file has changed.
+    /**
+     * This method is used to load the current configuration file. When this
+     * method is called it will post the notification ConfigurationDidChangeNotification.
+     * This method is called when rapid turnaround is enabled and the configuration
+     * file changes.
+     */
     public static void loadConfiguration() {
         if (configurationFilePath() != null) {
-            //File configurationFile = new File(configurationFilePath());
-            //if (configurationFile != null && configurationFile.lastModified() > _lastModifiedDate) {
-                //_lastModifiedDate = configurationFile.lastModified();
-                PropertyConfigurator.configure(readPropertiesFromPath(configurationFilePath()));
-                NSNotificationCenter.defaultCenter().postNotification(ConfigurationDidChangeNotification, null);
-            //}
+            PropertyConfigurator.configure(readPropertiesFromPath(configurationFilePath()));
+            NSNotificationCenter.defaultCenter().postNotification(ConfigurationDidChangeNotification, null);
         } else {
             if (isLoggingConfigured())
                 cat.error("Unable to reset logging, configFilePath is null.");
@@ -102,6 +165,14 @@ public class ERXLog4j {
         }
     }
 
+    /**
+     * Simple utility method used to load a properties file
+     * for a given file path.
+     * @param path file path to the properties file
+     * @return properties file with the values from the file
+     *		specified.
+     */
+    // MOVEME: ERXProperties or ERXPropertyUtilities
     public static Properties readPropertiesFromPath(String path) {
         Properties result = new Properties();
         if (path!=null) {
