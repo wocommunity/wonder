263f7e7a4e97fe277c95020382b89db8ecf713eb
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-22 16:25:39 +0000

    JavaDoc

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXPatternLayout.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXPatternLayout.java
@@ -12,12 +12,18 @@ import org.apache.log4j.helpers.PatternConverter;
 import org.apache.log4j.helpers.PatternParser;
 import org.apache.log4j.spi.LoggingEvent;
 import java.util.Enumeration;
-import com.webobjects.appserver.*;
-import com.webobjects.foundation.*;
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableArray;
 
-// This guy allows you to specify '#' when using a log4j pattern layout to get the ProcessId of the
-// current WOApp or '$' to get the name of the WOApp.  Note that processId is a category off of 
-// WOApplication that can be found in the file: ProcessIdApplicationCategory.m.  Need a better solution for 5.0
+/**
+ * The ERXPatternLayout adds two additional (and needed) layout options. The
+ * first is by specifing an '@' character a full backtrace will be logged as part
+ * of the log event. The second is by specifing an '$' char the current application
+ * name of the WOApplication will be logged as part of the log event.
+ */
+// ENHANCEME: Need access to ERXThreadStorage, also need more WO stuff, could opt for a WO char
+//	      and then specify all of the things to log as formatting info for that converter.
 public class ERXPatternLayout extends PatternLayout {
 
     /**
@@ -28,21 +34,47 @@ public class ERXPatternLayout extends PatternLayout {
         this(DEFAULT_CONVERSION_PATTERN);
     }
 
+    /**
+     * Default constructor. Uses the specified conversion
+     * pattern.
+     * @param pattern layout to be used.
+     */
     public ERXPatternLayout(String pattern) {
         super(pattern);
     }
 
+    /**
+     * Creates a pattern parser for the given pattern.
+     * This method is called implicitly by the log4j
+     * logging system.
+     * @param pattern to create the pattern parser for
+     * @return an ERXPatternParser for the given pattern
+     */
     public PatternParser createPatternParser(String pattern) {
         return new ERXPatternParser(pattern == null ? DEFAULT_CONVERSION_PATTERN : pattern);
   }
 }
 
+/**
+ * Pattern parser extension that adds support for WebObjects
+ * specific patterns.
+ */
 class ERXPatternParser extends PatternParser {
 
+    /**
+     * Default constructor for a given pattern
+     * @param pattern to construct the parser for
+     */
     public ERXPatternParser(String pattern) {
         super(pattern);
     }
-    
+
+    /**
+     * Creates a converter for a particular
+     * character. This is the method that
+     * adds the custom converters for WO.
+     * @param c char to add the converter for
+     */
     public void finalizeConverter(char c) {
         if (c == '$') {
             addConverter(new AppNamePatternConverter(formattingInfo));
@@ -54,11 +86,28 @@ class ERXPatternParser extends PatternParser {
             super.finalizeConverter(c);            
         }
     }
-    
+
+    /**
+     * The stack trace pattern is useful for logging full stack traces
+     * when a log event occurs.
+     */
     private class StackTracePatternConverter extends PatternConverter {
+        /**
+         * Package access level constructor.
+         * @param formattingInfo that is currently being used for this
+         *		pattern converter.
+         */
         StackTracePatternConverter(FormattingInfo formattingInfo) {
             super(formattingInfo);
         }
+
+        /**
+         * For a given log event returns the string representation
+         * of a stack trace for the current logging call minus all
+         * of the log4j stack.
+         * @param event current logging event
+         * @return string representation of the current backtrace.
+         */
         public String convert(LoggingEvent event) {
             NSArray parts = NSArray.componentsSeparatedByString(ERXUtilities.stackTrace(), "\n\t");
             NSMutableArray subParts = new NSMutableArray();
@@ -75,16 +124,34 @@ class ERXPatternParser extends PatternParser {
             return "\t" + subParts.componentsJoinedByString("\n\t") + "\n";
         }        
     }
-    
+
+    /**
+     * The application name pattern converter is useful for logging
+     * the current application name in log statements.
+     */
     private class AppNamePatternConverter extends PatternConverter {
+        /** holds a reference to the app name */
         String _appName;
+
+        /**
+         * Default package level constructor
+         * @param formattingInfo current pattern formatting information
+         */
         AppNamePatternConverter (FormattingInfo formattingInfo) {
             super(formattingInfo);
         }
 
+        /**
+         * Returns the current application name for the
+         * current logging event. If the application instance
+         * has not been created yet then "N/A" is logged.
+         * @param event a given logging event
+         * @return the current application name
+         */
         public String convert(LoggingEvent event) {
             if (_appName == null) {
-                if (WOApplication.application() != null) _appName = WOApplication.application().name();
+                if (WOApplication.application() != null)
+                    _appName = WOApplication.application().name();
             }
             return _appName != null ? _appName : "N/A";
         }
