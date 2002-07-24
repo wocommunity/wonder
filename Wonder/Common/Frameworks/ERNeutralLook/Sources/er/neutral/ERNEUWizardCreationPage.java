ccc74cfc01668b37c29a3d969243585b324a79db
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-24 16:29:44 +0000

    adding classes
    git-svn-id: file:///Users/q/working/wondersvn/trunk@543 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUWizardCreationPage.java
@@ -0,0 +1,28 @@
+//
+// ERWizardCreationPage.java: Class file for WO Component 'ERWizardCreationPage'
+// Project ERNeutralLook
+//
+// Created by travis on Mon Jul 01 2002
+//
+
+package er.neutral;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+import er.directtoweb.*;
+
+public class ERNEUWizardCreationPage extends ERD2WWizardCreationPage {
+
+    public ERNEUWizardCreationPage(WOContext context) {
+        super(context);
+    }
+
+    public String defaultRowspan() {
+	return ""+(currentSection()!=null && currentSection().keys!=null ? currentSection().keys.count() : 0)+4;
+	//return ""+(currentSection().keys.count()+4);
+    }
+
+}
