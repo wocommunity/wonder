32e97b3e9f9723bcfe851e4de177ecb802350040
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-24 01:15:14 +0000

    adding

---
+++ Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUQueryAllPage.java
@@ -0,0 +1,21 @@
+//
+// ERNEUQueryAllPage.java: Class file for WO Component 'ERNEUQueryAllPage'
+// Project ERNeutralLook
+//
+// Created by bposokho on Tue Jul 23 2002
+//
+package er.neutral;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+
+public class ERNEUQueryAllPage extends D2WQueryAllEntitiesPage {
+
+    public ERNEUQueryAllPage(WOContext context) {
+        super(context);
+    }
+
+}
