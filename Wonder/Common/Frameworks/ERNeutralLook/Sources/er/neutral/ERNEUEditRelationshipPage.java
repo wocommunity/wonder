dcde11012974bb72b4b35114391c2dcd5e112568
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-24 01:15:14 +0000

    adding
    git-svn-id: file:///Users/q/working/wondersvn/trunk@536 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUEditRelationshipPage.java
@@ -0,0 +1,21 @@
+//
+// ERNEUEditRelationshipPage.java: Class file for WO Component 'ERNEUEditRelationshipPage'
+// Project ERNeutralLook
+//
+// Created by bposokho on Tue Jul 23 2002
+//
+package er.neutral;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+
+public class ERNEUEditRelationshipPage extends D2WEditRelationshipPage {
+
+    public ERNEUEditRelationshipPage(WOContext context) {
+        super(context);
+    }
+
+}
