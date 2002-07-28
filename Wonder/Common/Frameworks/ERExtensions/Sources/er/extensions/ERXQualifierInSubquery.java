f07c49bc3425779238aaa86feb03705e8bd7fe7a
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-28 00:46:09 +0000

    No longer using deprecated api, rewritten subclassing EOQualifier.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@578 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXQualifierInSubquery.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXQualifierInSubquery.java
@@ -10,81 +10,130 @@ import com.webobjects.foundation.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
 
-public class ERXQualifierInSubquery extends EOSQLQualifier {
+// generates a subquery for the qualifier given in argument
+//
+//   ...  t0.ID IN (SELECT t0.ID FROM X WHERE <your qualifier here> ) ..
+//
+//
+// this class can be used to work around the EOF bug where OR
+// queries involving many-to-manies are incorrectly generated
+//
+//
+// It will also generate
+//
+//  ... t0.FOREIGN_KEY_ID in (select t1.ID from X where <your qualifier here>)
+//
+// with the 3 arg constructor
 
-    // generates a subquery for the qualifier given in argument
-    //
-    //   ...  t0.ID in (SELECT t0.ID FROM X WHERE <your qualifier here> ) ..
-    //
-    //
-    // this class can be used to work around the EOF bug where OR
-    // queries involving many-to-manies are incorrectly generated
-    //
-    //
-    // It will also generate
-    //
-    //  ... t0.FOREIGN_KEY_ID in (select t1.ID from X where <your qualifier here>)
-    //
-    // with the 3 arg constructor
+// FIXME: Dues to the way the SQL is generated the three arguement constructor has conflicts in the
+//	  table names used for instance, this bit of code:
+//        EOQualifier q = EOQualifier.qualifierWithQualifierFormat("firstName = 'Max'", null);
+//	  ERXQualifierInSubquery qq = new ERXQualifierInSubquery(q, "User", "groupId");
+//	  EOFetchSpecification fs = new EOFetchSpecification("Group", qq, null);
+//
+// Would generate: "SELECT t0.GROUP_ID, t0.NAME FROM GROUP t0 WHERE t0.GROUP_ID IN ( SELECT t0.GROUP_ID FROM GROUP t0 WHERE t0.NAME = ? ) "
+public class ERXQualifierInSubquery extends EOQualifier implements EOQualifierSQLGeneration, Cloneable {
+
+    /** holds the subqualifier */
+    protected EOQualifier qualifier;
+
+    /** holds the entity name */
+    protected String entityName;
+
+    /** holds the attribute name */
+    protected String attributeName;
+
+    /**
+     * Public single argument constructor. Use
+     * this constructor for sub-qualification
+     * on the same table.
+     * @param q sub-qualifier
+     */
+    public ERXQualifierInSubquery(EOQualifier q) {        
+        this(q, null, null);
+    }
+
+    /**
+     * Public three qrgument constructor. Use
+     * this constructor for for building queries
+     * on foriegn key attributes of the current
+     * entity.
+     * @param q sub qualifier
+     * @param entityName of the sub qualification
+     * @param attributeName foriegn key attribute name
+     */
+    // ENHANCEME: Should be able to just use a relationship key instead of both.
+    public ERXQualifierInSubquery(EOQualifier q, String entityName, String attributeName) {
+        super();
+        this.qualifier = q;
+        this.entityName = entityName;
+        this.attributeName = attributeName;
+    }
+
+    //	===========================================================================
+    //	EOQualifier method(s)
+    //	---------------------------------------------------------------------------
+    // FIXME: Shoudl do something here ...
+    public void addQualifierKeysToSet(NSMutableSet aSet) {
+    }
+
+    public EOQualifier qualifierWithBindings(NSDictionary someBindings, boolean requiresAll) {
+        return (EOQualifier)this.clone();
+    }
+
+    // FIXME: Should do something here ...
+    public void validateKeysWithRootClassDescription(EOClassDescription aClassDescription)
+    {
+    }    
+
     
-    private EOQualifier _qualifier;
-    private EOEntity _entity;
-    private EOAttribute _attribute;
-
-    private ERXQualifierInSubquery(EOQualifier q,
-                                  EOEntity entity) {        
-        super(entity,null,null);
-        _qualifier=q;
-        _entity=entity;
-    }
-
-    public ERXQualifierInSubquery(EOEntity entity,
-                                 EOQualifier q) {
-
-        /* in theory we should be able to accept any qualifier in here
-         but for KeyValueQualifiers, they HAVE to end in an attribute it seems
-         coveredCounties.state=<State mass> throws */
-        this (q, entity);
-        _attribute=(EOAttribute)_entity.primaryKeyAttributes().objectAtIndex(0);
-    }
-
-    public ERXQualifierInSubquery(EOEntity entity,
-                                 EOAttribute att,
-                                 EOQualifier q) {
-        this(q, entity);
-        _attribute=att;
-    }
-
     public String sqlStringForSQLExpression(EOSQLExpression e) {
-        StringBuffer sb=new StringBuffer();
-        sb.append(e.sqlStringForAttribute(_attribute));
+        StringBuffer sb = new StringBuffer();
+        if (attributeName != null)
+            sb.append(e.sqlStringForAttributeNamed(attributeName));
+        else {
+            EOAttribute pk = (EOAttribute)e.entity().primaryKeyAttributes().lastObject();
+            sb.append(e.sqlStringForAttribute(pk));            
+        }
         sb.append(" IN ( ");
-        EOFetchSpecification fs=new EOFetchSpecification(_entity.name(),
-                                                         _qualifier,
+        EOEntity entity;
+        if (entityName == null) {
+            entity = e.entity();
+        } else {
+            entity = e.entity().model().entityNamed(entityName);
+        }
+        EOFetchSpecification fs=new EOFetchSpecification(entity.name(),
+                                                         qualifier,
                                                          null,
                                                          false,
                                                          true,
                                                          null);
-        // FIXME: This will create a new EOAdaptor everytime it is called.
-        EOSQLExpressionFactory factory=EOAdaptor.adaptorWithModel(_entity.model()).expressionFactory();
-        EOSQLExpression expression=factory.selectStatementForAttributes( _entity.primaryKeyAttributes(),
+        // ASSUME: This makes a few assumptions, if anyone can figure out a full proof way that would be nice to get the model
+        //	   Note you can't use: EOAdaptor.adaptorWithModel(e.entity().model()).expressionFactory(); as this creates a
+        //
+        EODatabaseContext context = EODatabaseContext.registeredDatabaseContextForModel(entity.model(), EOObjectStoreCoordinator.defaultCoordinator());
+        EOSQLExpressionFactory factory = context.database().adaptor().expressionFactory();
+        EOSQLExpression expression=factory.selectStatementForAttributes(entity.primaryKeyAttributes(),
                                            false,
                                            fs,
-                                           _entity);
+                                           entity);
 
-        sb.append(expression.statement());
-
+        sb.append(expression.statement());        
         sb.append(" ) ");
         return sb.toString();
     }
 
-    public String toString() { return " <subquery> '"+_qualifier.toString()+"'"; }
+    public EOQualifier schemaBasedQualifierWithRootEntity(EOEntity anEntity) {
+        return (EOQualifier)this.clone();
+    }
 
-    /*
-     EOF seems to be wanting to clone qualifiers when the are inside an and-or qualifier
-     without this method, EOToManyQualifier is cloned into an EOSQLQualifier and the generated SQL is incorrect..
-        */
+    public EOQualifier qualifierMigratedFromEntityRelationshipPath(EOEntity anEntity, String aPath) {
+        return (EOQualifier)this.clone();
+    }    
+    
+    public String toString() { return " <" + getClass().getName() +"> '" + qualifier.toString() + "'"; }
+
     public Object clone() {
-        return new ERXQualifierInSubquery(_entity, _qualifier);
+        return new ERXQualifierInSubquery(qualifier, entityName, attributeName);
     }    
 }
