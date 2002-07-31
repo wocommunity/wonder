30189660ce25c3c4a38705debd3b52e3c004a024
(no author) <(no author)@8dc76679-1c23-de11-85cd-0050568e728b>
2002-07-31 02:22:05 +0000

    This commit was manufactured by cvs2svn to create tag 'WebObjects_5_1_3'.
    git-svn-id: file:///Users/q/working/wondersvn/tags/WebObjects_5_1_3@600 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Validity/Applications/ValidityModeler/Sources/Layout.java
+++
@@ -1,8 +0,0 @@
-import com.webobjects.appserver.*;
-import com.webobjects.appserver.xml.*;
-import com.webobjects.foundation.*;
-import com.webobjects.eocontrol.*;
-import com.webobjects.eoaccess.*;
-public class Layout extends WOComponent {
-
-}
