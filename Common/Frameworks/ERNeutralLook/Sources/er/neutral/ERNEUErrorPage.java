4ab265c123b431dfdc6036978b4933b95c92c9a6
patrice_gautier <patrice_gautier@8dc76679-1c23-de11-85cd-0050568e728b>
2002-08-01 01:11:05 +0000

    Adding Error Page

---
+++ Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUErrorPage.java
@@ -0,0 +1,24 @@
+//
+// ERNEUErrorPage.java: Class file for WO Component 'ERNEUErrorPage'
+// Project ERNeutralLook
+//
+// Created by patrice on Wed Jul 31 2002
+//
+
+package er.neutral;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+import er.directtoweb.*;
+
+
+public class ERNEUErrorPage extends ERDRecoverableErrorPage {
+
+    public ERNEUErrorPage(WOContext context) {
+        super(context);
+    }
+
+}
