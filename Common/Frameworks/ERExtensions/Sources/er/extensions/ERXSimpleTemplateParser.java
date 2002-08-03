b71749c1cad5f7e35012f3cc1c5b59e26c2cc36a
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-08-03 03:43:07 +0000

    JavaDoc
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@631 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXSimpleTemplateParser.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXSimpleTemplateParser.java
@@ -9,7 +9,6 @@ package er.extensions;
 import com.webobjects.foundation.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
-import com.webobjects.appserver.*;
 import org.apache.log4j.Category;
 import java.util.Enumeration;
 
@@ -41,6 +40,9 @@ public class ERXSimpleTemplateParser {
     /**
      * Calculates the set of keys used in a given template
      * for a given delimiter.
+     * @param template to check for keys
+     * @param delimiter for finding keys
+     * @return array of keys
      */
     public NSArray keysInTemplate(String template, String delimiter) {
         NSMutableSet keys = new NSMutableSet();
@@ -61,7 +63,15 @@ public class ERXSimpleTemplateParser {
         }
         return keys.allObjects();
     }    
-    
+
+    /**
+     * Cover method for calling the four argument method
+     * passing in <code>null</code> for the <code>otherObject</code>
+     * parameter. See that method for documentation.
+     * @param template to use to parse
+     * @param delimiter to use to find keys
+     * @param object to resolve keys
+     */
     public String parseTemplateWithObject(String template, String delimiter, Object object) {
         return parseTemplateWithObject(template,
                                        delimiter,
@@ -69,7 +79,20 @@ public class ERXSimpleTemplateParser {
                                        null);
     }
     
-    /* This method replaces the keys enclosed between the delimeter with the values found in object and otherObject. It first looks for a value in object, and then in otherObject if the key is not found in object. Therefore, otherObject is a good place to store default values while other object is a good place to override default values. */
+    /*
+     * This method replaces the keys enclosed between the
+     * delimeter with the values found in object and otherObject.
+     * It first looks for a value in object, and then in otherObject
+     * if the key is not found in object. Therefore, otherObject is
+     * a good place to store default values while other object is a
+     * good place to override default values.
+     *
+     * @param template to use to parse
+     * @param delimiter to use to check for keys
+     * @param object to resolve keys off of
+     * @param otherObject object used to resolve default keys
+     * @return parsed template with keys replaced
+     */
     public String parseTemplateWithObject(String template, String delimiter, Object object, Object otherObject) {
         if (template == null)
             throw new RuntimeException("Attempting to parse null template!");
