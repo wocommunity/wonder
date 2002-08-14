4bfd479ec714aeba78743f3f973b37fd6b71c84e
patrice_gautier <patrice_gautier@8dc76679-1c23-de11-85cd-0050568e728b>
2002-08-14 00:00:59 +0000

    implementing scheme to provide all time stamped EOs commited in the same transaction the same creation/modification date
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@698 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCStampedEnterpriseObject.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCStampedEnterpriseObject.java
@@ -10,6 +10,8 @@ import com.webobjects.foundation.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
 import com.webobjects.appserver.*;
+import er.extensions.*;
+import org.apache.log4j.*;
 
 public abstract class ERCStampedEnterpriseObject extends ERCEnterpriseObject {
 
@@ -17,24 +19,54 @@ public abstract class ERCStampedEnterpriseObject extends ERCEnterpriseObject {
     public abstract String relationshipNameForLogEntry();
     public abstract EOEnterpriseObject logEntryType();
     public EOEnterpriseObject insertionLogEntry=null;
+
+    public static Category cat=Category.getInstance(ERCStampedEnterpriseObject.class);
+
+
+    
+    // FIXME not thread safe..
+    private static NSMutableDictionary _datesPerEcID=new NSMutableDictionary();
+    private static class _Touch {
+        public void touch(NSNotification n) {
+            NSTimestamp now=new NSTimestamp();
+            if (cat.isDebugEnabled()) cat.debug("TimeStamp for "+n.object()+": now");
+            _datesPerEcID.setObjectForKey(ERXConstant.integerForInt(System.identityHashCode(n.object())), now);
+        }
+    }
+    
+    static {
+        NSNotificationCenter center = NSNotificationCenter.defaultCenter();
+        center.addObserver(new _Touch(),
+                           new NSSelector("touch",  ERXConstant.NotificationClassArray),
+                           ERXExtensions.objectsWillChangeInEditingContext,
+                           null);
+    }        
+
+
     
     public void awakeFromInsertion(EOEditingContext ec) {
         super.awakeFromInsertion(ec);
-        NSTimestamp now = new NSTimestamp();
-        setCreated(now);
-        // Shouldn't use touch() here as it can be overridden in subclasses.
-        setLastModified(now);
         if (relationshipNameForLogEntry()!=null && logEntryType() != null) {
             insertionLogEntry=ERCoreBusinessLogic.createLogEntryLinkedToEO(logEntryType(),
                                                                    null,
                                                                    this,
                                                                    relationshipNameForLogEntry());
         }
+        // We now set the date created/last modified in willInsert/Update/Delete
+        // A side effect of this technique is that for new EOs, created/lastModified is null until the EO actually gets saved
+        // which means it'll fail the validation
+        // two options: either we poke a value in those attributes here (even though it will be modified in willInsert,
+        // or we make the property keys not mandatory
+        // I am option for the former.
+        NSTimestamp t=new NSTimestamp();
+        setCreated(t);
+        setLastModified(t);
     }
 
     public void willInsert() {
         super.willInsert();
         touch();
+        setCreated(lastModified());
     }
 
     public void willUpdate() {
@@ -51,7 +83,12 @@ public abstract class ERCStampedEnterpriseObject extends ERCEnterpriseObject {
     }
 
     
-    public void touch() { setLastModified(new NSTimestamp()); }
+    private void touch() {
+        Number n=ERXConstant.integerForInt(System.identityHashCode(editingContext()));
+        NSTimestamp date=(NSTimestamp)_datesPerEcID.objectForKey(n);
+        if (date==null) throw new RuntimeException("Null modification date found in touch() call - EC delegate is probably missing");
+        setLastModified(date);
+    }
     
     public void addObjectToBothSidesOfLogEntryRelationshipWithKey(EOEnterpriseObject object,
                                                                   String key) {
