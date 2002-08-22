ca11a1d81781f50b4b9b5bb1e6fdd80045e458be
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2002-08-22 13:29:12 +0000

    Adding ERXNSPrintWriterLogger to the repository. This NSLog.Logger accepts PrintWrinter so that the character encodeing such as UTF8 and SJIS (Japanese) can be explicitly set.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@752 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXNSPrintWriterLogger.java
@@ -0,0 +1,98 @@
+//
+// ERXNSPrintWriterLogger.java
+// Project ERExtensions
+//
+// Created by tatsuya on Wed Aug 21 2002
+//
+package er.extensions;
+
+import com.webobjects.foundation.*;
+import java.io.PrintWriter;
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * <code>ERXNSPrintWriterLogger</code> is an alternative of 
+ * {@link: com.webobjects.foundation.NSLog.PrintStreamLogger NSLog.PrintStreamLogger} 
+ * but accepts character oriented {@link: java.io.PrintWriter} instead of 
+ * byte oriented {@link: java.io.PrintStream}
+ * so that the character encoding for logging can be 
+ * explicitly set. 
+ * <p>
+ * Usage: 
+ * <pre>
+ * // Create a PrintWriter with Japanese "SJIS" encoding. 
+ * OutputStreamWriter sjisOutWriter = new OutputStreamWriter(new FileOutputStream("app.log"), "SJIS");
+ * PrintWriter sjisPrintWriter = new PrintWriter(sjisOutWriter, true);
+ *
+ * // Create a logger then set it to NSLog.
+ * NSLog.Logger logger = new ERXNSPrintWriterLogger(sjisPrintWriter);
+ * NSLog.setDebug(logger);
+ * NSLog.setOut(logger);
+ * NSLog.setErr(logger);
+ * </pre>
+ */
+public class ERXNSPrintWriterLogger extends NSLog.Logger {
+
+    /** java.io.PrintWriter to print logs */ 
+    private PrintWriter _printWriter; 
+
+    /** Apple built-in logger, used to generate a vervose header "[2002-01-31 09:01:00 EDT] <main> " */ 
+    private NSLog.PrintStreamLogger _vervoseStreamLogger; 
+    
+    /** an output from _vervoseStreamLogger */ 
+    private ByteArrayOutputStream _verboseHeaderStream;
+    
+    public ERXNSPrintWriterLogger() {
+        throw new IllegalArgumentException("ERXNSPrintWriterLogger requires a PrintWriter as an argument.");
+    }
+    
+    public ERXNSPrintWriterLogger(PrintWriter printWriter) {
+        _printWriter = printWriter;
+    }
+    
+    public void appendln() {
+        if (isEnabled())
+            _printWriter.println();
+    }
+    
+    public void appendln(Object object) {
+        if (isEnabled()) {
+            if (isVerbose()) 
+                _printWriter.print(_verboseHeader());
+            _printWriter.println(object);
+        }
+    }
+    
+    public void flush() {
+        _printWriter.flush();
+    }
+
+    private String _verboseHeader() {
+        _verboseHeaderStream().reset();
+
+        // NSLog.PrintStreamLogger will append below to _verboseHeaderStream ByteArrayOutputStream:
+        // "[2002-01-31 09:01:00 EDT] <main> " + " " + "\n" 
+        _vervoseStreamLogger().appendln(" ");  
+        String verboseHeader = _verboseHeaderStream().toString();
+        
+        // remove " \n" at the end of line
+        int lastIndex = verboseHeader.lastIndexOf(" \n");
+        if (lastIndex > 0)   // ignores if " \n" is the first char (lastIndex == 0)
+            verboseHeader = verboseHeader.substring(0, lastIndex);
+        return verboseHeader;
+    }
+
+    private ByteArrayOutputStream _verboseHeaderStream() {
+        if (_verboseHeaderStream == null) 
+            _verboseHeaderStream =  new ByteArrayOutputStream("[2002-01-31 09:01:00 EDT] <main> ".length() + 20);
+        return _verboseHeaderStream;
+    }
+
+    private NSLog.PrintStreamLogger _vervoseStreamLogger() {
+        if (_vervoseStreamLogger == null) 
+            _vervoseStreamLogger = new NSLog.PrintStreamLogger(new PrintStream(_verboseHeaderStream()));
+        return _vervoseStreamLogger;
+    }
+
+}
