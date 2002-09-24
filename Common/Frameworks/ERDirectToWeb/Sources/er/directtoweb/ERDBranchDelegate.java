91dc5aba061ad3e5cb912f3012b14a4a694c5fac
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-09-24 07:12:05 +0000

    JavaDoc plus enhancing to allow the branch delegate to supply the branch choices. By default if they are not specified via the rule system then all of the public methods that can be used for branching are looked up on the class using reflection.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDBranchDelegate.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDBranchDelegate.java
@@ -6,26 +6,53 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.directtoweb;
 
-import com.webobjects.directtoweb.*;
-import com.webobjects.appserver.*;
-import java.lang.reflect.*;
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.directtoweb.NextPageDelegate;
+import com.webobjects.directtoweb.D2WContext;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableArray;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Enumeration;
+
 import er.extensions.ERXLogger;
 import er.extensions.ERXUtilities;
 
-public class ERDBranchDelegate implements NextPageDelegate {
+/**
+ * The branch delegate is used in conjunction with the
+ * {@link ERDMessagePageInterface ERDMessagePageInterface} to allow
+ * flexible branching for message pages. Branch delegates can
+ * only be used with templates that implement the 
+ * {@link ERDBranchInterface ERDBranchInterface}.
+ */
+public abstract class ERDBranchDelegate implements NextPageDelegate {
 
     /** logging support */
-    public final static ERXLogger log = ERXLogger.getERXLogger("er.directtoweb.delegates.ERDBranchDelegate");
+    public final static ERXLogger log = ERXLogger.getERXLogger(ERDBranchDelegate.class);
 
+    /** holds the WOComponent class array used to lookup branch delegate methods */
+    // MOVEME: Should belong in a WO constants class
     public final static Class[] WOComponentClassArray = new Class[] { WOComponent.class };
     
-    public WOComponent nextPage(WOComponent sender) {
+    /**
+     * Implementation of the {@link NextPageDelegate NextPageDelegate}
+     * interface. This method provides the dynamic dispatch based on
+     * the selected branch provided by the sender. Will call the 
+     * method <branchName>(WOComponent) on itself returning the 
+     * result. 
+     * @param sender template invoking the branch delegate
+     * @return result of dynamic method lookup and execution on itself.
+     */
+    public final WOComponent nextPage(WOComponent sender) {
         WOComponent nextPage = null;
         if (sender instanceof ERDBranchInterface) {
             String branchName = ((ERDBranchInterface)sender).branchName();
+            if (log.isDebugEnabled())
+                log.debug("Branching to branch: " + branchName);
             try {
                 Method m = getClass().getMethod(branchName, WOComponentClassArray);
-                nextPage = (WOComponent)m.invoke(this, new Object[] { sender });
+                nextPage = (WOComponent)m.invoke(this, new WOComponent[] { sender });
             } catch (InvocationTargetException ite) {
                 log.error("Invocation exception occurred in ERBranchDelegate: " + ite.getTargetException() + " backtrace: " +
                           ERXUtilities.stackTrace(ite.getTargetException()) + " for branch name: " + branchName);
@@ -36,5 +63,35 @@ public class ERDBranchDelegate implements NextPageDelegate {
             log.warn("Branch delegate being used with a component that does not implement the ERBranchInterface");
         }
         return nextPage;
+    }
+    
+    /**
+     * Calculates which branches to show in the display first
+     * asking the context for the key <b>branchChoices</b> if
+     * this returns null then using reflection all of the 
+     * public methods that take a single WOComponent as a 
+     * parameter are returned.
+     * @param context current D2W context
+     * @return array of branch names.
+     */
+    public NSArray branchChoicesForContext(D2WContext context) {
+        NSArray choices = (NSArray)context.valueForKey("branchChoices");
+        if (choices == null) {
+            try {
+                NSMutableArray methodNames = new NSMutableArray();
+                Method methods[] = getClass().getMethods();
+                for (Enumeration e = new NSArray(methods).objectEnumerator(); e.hasMoreElements();) {
+                    Method method = (Method)e.nextElement();
+                    if (method.getParameterTypes().equals(WOComponentClassArray) && 
+                            !method.getName().equals("nextPage")) {
+                        methodNames.addObject(method.getName());
+                    }
+                }
+            } catch (SecurityException e) {
+                log.error("Caught security exception while calculating the branch choices for delegate: " 
+                        + this + " exception: " + e.getMessage());
+            }
+        }
+        return choices;
     }
 }
