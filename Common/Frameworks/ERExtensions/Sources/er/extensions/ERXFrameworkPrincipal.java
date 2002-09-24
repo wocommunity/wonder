73206489c2ca7af1e1e6cb890ba397eb9f7d0098
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-09-24 05:30:46 +0000

    JavaDoc

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXFrameworkPrincipal.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXFrameworkPrincipal.java
@@ -4,27 +4,43 @@
 //
 // Created by ak on Sat May 04 2002
 //
-
 package er.extensions;
 
-import com.webobjects.foundation.*;
-import com.webobjects.appserver.*;
-import com.webobjects.eocontrol.*;
+import com.webobjects.foundation.NSMutableDictionary;
+import com.webobjects.foundation.NSNotificationCenter;
+import com.webobjects.foundation.NSNotification;
+import com.webobjects.foundation.NSSelector;
+import com.webobjects.appserver.WOApplication;
 
-/** Designated starter class for frameworks.
+/** 
+ * Designated starter class for frameworks.
  * the <code>finishInitialization</code> will be called when the app finishes startup.
- * To use, subclass it and set the NSPrincipalClass of your framework to the subclass. See the ERXCoreBusinessLogic and BTBusinessLogic frameworks for an example of usage.
-*/
-
+ * To use, subclass it and set the NSPrincipalClass of your framework to the subclass. 
+ * See the ERXCoreBusinessLogic and BTBusinessLogic frameworks for an example of usage.
+ */
 public abstract class ERXFrameworkPrincipal {
+
+    /** logging support */
     public static final ERXLogger log = ERXLogger.getERXLogger(ERXFrameworkPrincipal.class);
 
+    /** holds the mapping between framework principals classes and ERXFrameworkPrincipal objects */
     private static NSMutableDictionary initializedFrameworks = new NSMutableDictionary();
 
+    /**
+     * Gets the shared framework principal instance for a given
+     * class.
+     * @param principal class for a given framework
+     * @return framework principal initializer
+     */
     public static ERXFrameworkPrincipal sharedInstance(Class c) {
         return (ERXFrameworkPrincipal)initializedFrameworks.objectForKey(c.getName());
     }
     
+    /**
+     * Sets up a given framework principal class to recieve notification
+     * when it is safe for the framework to be initialized.
+     * @param c principal class
+     */
     public static void setUpFrameworkPrincipalClass(Class c) {
         try {
             if (initializedFrameworks.objectForKey(c.getName()) == null) {
@@ -43,14 +59,32 @@ public abstract class ERXFrameworkPrincipal {
         }
     }
 
-    public void finishInitialization(NSNotification n) {
+    /**
+     * Notification method called when the WOApplication posts
+     * the notification 'ApplicationWillFinishLaunching'. This
+     * method handles de-registering for notifications and releasing
+     * any references to observer so that it can be released for
+     * garbage collection.
+     * @param n notification that is posted after the WOApplication
+     * 		has been constructed, but before the application is
+     *		ready for accepting requests.
+     */
+    public final void finishInitialization(NSNotification n) {
         NSNotificationCenter.defaultCenter().removeObserver(this);
         ERXRetainer.release(this);
+        initializedFrameworks.removeObjectForKey(getClass().getName());
         finishInitialization();
     }
 
+    /**
+     * Overridden by subclasses to provide framework initialization.
+     */
     public abstract void finishInitialization();
     
+    /**
+     * Access to the logging mechanism
+     * @return shared logging instance
+     */
     public ERXLogger log() {
         return log;
     }
