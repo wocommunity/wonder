2058491ccd12e8effe62bbb0ec22952d7a179b0e
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-05 22:01:47 +0000

    using submit button instead of WOActiveImageButton
    git-svn-id: file:///Users/q/working/wondersvn/trunk@899 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditSortedToManyFault.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditSortedToManyFault.java
@@ -11,7 +11,7 @@ import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
 import com.webobjects.directtoweb.*;
 import er.directtoweb.*;
-import er.extensions.*;
+import er.extensions.*; 
 
 public class ERD2WEditSortedToManyFault extends D2WEditToManyFault {
 
@@ -21,9 +21,21 @@ public class ERD2WEditSortedToManyFault extends D2WEditToManyFault {
 
     public NSArray sortedBrowserList()
 {
-        return  ERXArrayUtilities.sortedArraySortedWithKey(browserList(),
-                                                           (String)d2wContext().valueForKey("indexKey"),
-                                                           null);
+        NSArray result = null;
+        EORelationship relationship = entity().relationshipNamed(propertyKey());
+        EOEntity destinationEntity = relationship.destinationEntity();
+        if(destinationEntity.userInfo().valueForKey("isSortedJoinEntity") != null &&
+           ((String)destinationEntity.userInfo().valueForKey("isSortedJoinEntity")).equals("true")){
+            D2WContext d2wContext = new D2WContext();
+            d2wContext.setEntity(destinationEntity);
+            String indexKey = (String)d2wContext.valueForKey("indexKey");
+            result =   ERXArrayUtilities.sortedArraySortedWithKey(browserList(),
+                                                                  indexKey,
+                                                               null);
+        }else{
+            result = browserList();
+        }
+        return result;
 }
 
 }
