1c0f4bd79f1ce16d587edd28b57bcb64ac41427e
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-08 18:30:12 +0000

    adding ERNEUPickListPage

---
+++ Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUPickListPage.java
@@ -0,0 +1,21 @@
+//
+// ERNEUPickListPage.java: Class file for WO Component 'ERNEUPickListPage'
+// Project ERNeutralLook
+//
+// Created by bposokho on Mon Oct 07 2002
+//
+package er.neutral;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import er.directtoweb.*;
+
+public class ERNEUPickListPage extends ERD2WPickListPageTemplate {
+
+    public ERNEUPickListPage(WOContext context) {
+        super(context);
+    }
+
+}
