9d9beab26c62cbadef942343f77312333d772f3c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-09 12:45:31 +0000

    added WOFileUpload check if WOForm's enctype is correct

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXWOForm.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXWOForm.java
@@ -16,26 +16,36 @@ import com.webobjects.appserver.*;
  *  Also, it warns you when you have one Form embedded inside another.
  *  Should be fixed to skip writing out the Form tag, which would make the
  *  ERXOptionalForm obsolete.
+ *  Additionally, it pushes the <code>enctype</code> into the userInfo, so that {@link ERXWOFileUpload}
+ *  can check if it is set correctly.
  *  This subclass is installed when the frameworks loads. 
  */  
 public class ERXWOForm extends com.webobjects.appserver._private.WOForm {
     static final ERXLogger log = ERXLogger.getERXLogger(ERXWOForm.class);
     WOAssociation _formName;
+    WOAssociation _enctype;
     
     public ERXWOForm(String name, NSDictionary associations,
                      WOElement template) {
         super(name, associations, template);
         _formName = (WOAssociation) _associations.removeObjectForKey("name");
+        _enctype = (WOAssociation) _associations.removeObjectForKey("enctype");
     }
 
     public void appendAttributesToResponse(WOResponse response, WOContext context) {
         if(context != null && context instanceof ERXMutableUserInfoHolderInterface && _formName != null) {
+            NSMutableDictionary ui = ((ERXMutableUserInfoHolderInterface)context).mutableUserInfo();
             String formName = (String)_formName.valueInComponent(context.component());
             if(formName != null) {
-                ((ERXMutableUserInfoHolderInterface)context).mutableUserInfo().setObjectForKey(formName, "formName");
+                ui.setObjectForKey(formName, "formName");
                 response._appendTagAttributeAndValue("name", formName, false);
             }
-       }
+            String enctype = (String)_enctype.valueInComponent(context.component());
+            if(enctype != null) {
+                ui.setObjectForKey(enctype.toLowerCase(), "enctype");
+                response._appendTagAttributeAndValue("enctype", enctype, false);
+            }
+        }
         super.appendAttributesToResponse(response, context);
     }
 
@@ -43,7 +53,10 @@ public class ERXWOForm extends com.webobjects.appserver._private.WOForm {
         if(context.isInForm())
             log.warn("This Form is embedded in another Form.");
         super.appendToResponse(response, context);
-        if(context instanceof ERXMutableUserInfoHolderInterface)
-            ((ERXMutableUserInfoHolderInterface)context).mutableUserInfo().removeObjectForKey("formName");
+        if(context instanceof ERXMutableUserInfoHolderInterface) {
+            NSMutableDictionary ui = ((ERXMutableUserInfoHolderInterface)context).mutableUserInfo();
+            ui.removeObjectForKey("formName");
+            ui.removeObjectForKey("enctype");
+        }
     }
 }
