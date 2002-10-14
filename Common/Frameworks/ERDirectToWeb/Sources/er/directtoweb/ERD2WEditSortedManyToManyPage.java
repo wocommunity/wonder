75215fa0b4eedfd8718d24d517e5c3091b68d87e
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-14 16:38:35 +0000

    when removing an element in the list need to resort the remaining objects
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@944 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditSortedManyToManyPage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditSortedManyToManyPage.java
@@ -186,6 +186,15 @@ public class ERD2WEditSortedManyToManyPage extends ERD2WPage implements EditRela
                 dataSource().deleteObject(object);
             }
             relationshipDisplayGroup.fetch(); // updateDisplayedObjects is not doing the trick
+            //Now need to reindex the joins
+            int i = 0;
+            for(Enumeration e = relationshipDisplayGroup.displayedObjects().objectEnumerator();
+                e.hasMoreElements();){
+                EOEnterpriseObject object = (EOEnterpriseObject)e.nextElement();
+                object.takeValueForKey(new Integer(i), indexKey());
+                i++;
+            }
+
         }
         return null;
     }
