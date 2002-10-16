c5c4231950a341dda507957b22744a04b69eff83
msacket <msacket@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-16 14:32:11 +0000

    Simply removed an import statement that was no longer needed.

--- Validity/Frameworks/Validity/Sources/com/gammastream/validity/GSVDateMethods.java
+++ Validity/Frameworks/Validity/Sources/com/gammastream/validity/GSVDateMethods.java
@@ -10,7 +10,6 @@ import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
-import com.gammastream.validity._private.*;
 
 /**
  *	This class provides a set of predefined rules for performing
