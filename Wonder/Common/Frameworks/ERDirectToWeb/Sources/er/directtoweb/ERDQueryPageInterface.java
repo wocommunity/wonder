5f526acca537e15880b50a40c628eeecf48e6a29
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-18 22:58:22 +0000

    adding ERDQueryPageInterface
    git-svn-id: file:///Users/q/working/wondersvn/trunk@982 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryPageInterface.java
@@ -0,0 +1,18 @@
+//
+// ERDQueryPageInterface.java
+// Project ERDirectToWeb
+//
+// Created by bposokho on Fri Oct 18 2002
+//
+package er.directtoweb;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.directtoweb.*;
+
+public interface ERDQueryPageInterface extends QueryPageInterface{
+
+    public void setCancelDelegate(NextPageDelegate cancelDelegate);
+    
+}
