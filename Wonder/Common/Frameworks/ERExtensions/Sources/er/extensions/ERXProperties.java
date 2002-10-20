6c6db30aea75c7ba12dbe46d04bef89d33f071d3
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-20 21:43:08 +0000

    Moving to the new logging configuration architecture:
    - Added a bunch of methods to load properties from file or
      command line arguments, locates properties files under opened
      projects or installed bundles, etc.
    
    
    git-svn-id: file:///Users/q/working/wondersvn/trunk@997 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXProperties.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXProperties.java
@@ -6,10 +6,18 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.extensions;
 
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.appserver.WOResourceManager;
 import com.webobjects.foundation.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
 import java.util.*;
+import java.io.File;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.webobjects.appserver._private.WOProjectBundle;
 
 /**
  * Collection of simple utility methods used to get and set properties
@@ -73,7 +81,7 @@ public class ERXProperties {
     }
 
     // DELETEME: This isn't needed any more
-    public static String stringForKey(String s) { return System.getProperty(s); }
+    //public static String stringForKey(String s) { return System.getProperty(s); }
 
     /**
      * Sets an array in the System properties for
@@ -102,7 +110,258 @@ public class ERXProperties {
      * @param key to be used to get the value
      */
     public static void setStringForKey(String string, String key) {
-        Properties properties = System.getProperties();
-        properties.put(key, string);
+        //Properties properties = System.getProperties();
+        //properties.put(key, string);
+        System.setProperty(key, string);
     }
+    
+    /** 
+     * Copies all properties from source to dest. 
+     * 
+     * @param source  proeprties copied from 
+     * @param dest  properties copied to
+     */
+    public static void transferPropertiesFromSourceToDest(Properties source, Properties dest) {
+        if (source != null)
+            dest.putAll(source);
+    }
+    
+    /**
+     * Reads a Java properties file at the given path 
+     * and returns a {@link java.util.Properties Properties} object 
+     * as the result. If the file does not exist, returns 
+     * an empty properties object. 
+     * 
+     * @param path  file path to the properties file
+     * @return properties object with the values from the file
+     *      specified.
+     */
+    public static Properties propertiesFromPath(String path) {
+        Properties prop = new Properties();
+
+        if (path == null  ||  path.length() == 0) {
+            log.warn("Attempting to read property file for null file path");
+            return prop;
+        }
+
+        File file = new File(path);
+        if (! file.exists()  ||  ! file.isFile()  ||  ! file.canRead()) {
+            log.warn("File " + path + " doesn't exist or can't be read.");
+            return prop;
+        }
+
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(path));
+            prop.load(in);
+            in.close();
+            log.info("Loaded configuration file at path: "+ path);
+        } catch (IOException e) {
+            log.error("Unable to initialize properties from file " + path + "\nError :" + e);
+        }
+        return prop;
+    }
+
+    /**
+     * Sets and returns properties object with the values from 
+     * the given command line arguments string array. 
+     * 
+     * @param argv  string array typically provided by 
+     *               the command line arguments
+     * @return properties object with the values from 
+     *          the argv
+     */
+    public static Properties propertiesFromArgv(String[] argv) {
+        Properties properties = new Properties();
+        NSDictionary argvDict = NSProperties.valuesFromArgv(argv);
+        Enumeration e = argvDict.allKeys().objectEnumerator();
+        while (e.hasMoreElements()) {
+            Object key = e.nextElement();
+            properties.put(key, argvDict.objectForKey(key));
+        }
+        return properties;
+    }
+
+    /** 
+     * Returns an array of paths to the <code>Properties</code> and 
+     * <code>WebObjects.properties</code> files contained in the 
+     * application/framework bundles and home directory. 
+     * <p>
+     * If ProjectBuilder (for Mac OS X) has the project opened, 
+     * it will attempt to get the path to the one in the project 
+     * directory instead of the one in the bundle. 
+     * <p>
+     * This opened project detection feature is pretty fragile and 
+     * will change between versions of the dev-tools.
+     * 
+     * @return paths to Properties files
+     */
+    public static NSArray pathsForUserAndBundleProperties() {
+        return pathsForUserAndBundleProperties(false);
+    }
+
+    public static NSArray pathsForUserAndBundleProperties(boolean reportLoggingEnabled) {
+        NSMutableArray propertiesPaths = new NSMutableArray();
+        NSMutableArray projectsInfo = new NSMutableArray();
+        String projectPath, aPropertiesPath;
+        WOApplication applicaiton = WOApplication.application();
+        if (applicaiton == null) {
+            log.warn("The application is not yet initialized. Returning an empty array.");
+            return NSArray.EmptyArray;
+        }
+        
+        WOResourceManager resourceManager = applicaiton.resourceManager();
+        
+        /* *** Properties for frameworks *** */
+        NSArray frameworkNames = (NSArray)NSBundle.frameworkBundles().valueForKey("name");
+        Enumeration e = frameworkNames.objectEnumerator();
+        while (e.hasMoreElements()) {
+            String frameworkName = (String) e.nextElement();
+            projectPath = aPropertiesPath = null;
+            
+            // Check if the framework project is opened from PBX
+            WOProjectBundle bundle = WOProjectBundle.projectBundleForProject(frameworkName, true);
+            if (bundle != null) 
+                projectPath = bundle.projectPath();
+            else 
+                projectPath = System.getProperty("projects." + frameworkName);
+            
+            if (projectPath != null) {
+                aPropertiesPath = pathForPropertiesUnderProjectPath(projectPath);
+                if (aPropertiesPath != null) {
+                    projectsInfo.addObject("Framework:   " + frameworkName 
+                            + " (opened, developement-mode) " + aPropertiesPath);
+                }
+            }
+            
+            if (aPropertiesPath == null) {
+                // The framework project is not opened from PBX, use the one in the bundle. 
+                aPropertiesPath = resourceManager.pathForResourceNamed("Properties", frameworkName, null);
+                if (aPropertiesPath != null) {
+                    aPropertiesPath = getActualPath(aPropertiesPath);
+                    projectsInfo.addObject("Framework:   " + frameworkName 
+                            + " (not opened, installed) " + aPropertiesPath); 
+                }
+            }
+            
+            if (aPropertiesPath != null) 
+                    propertiesPaths.addObject(aPropertiesPath);
+        } 
+        
+        /* *** Properties for the application (mainBundle) *** */
+        
+        // Check if the application project is opened from PBXs
+        projectPath = aPropertiesPath = null;
+        String mainBundleName = NSBundle.mainBundle().name();
+        // horrendous hack to avoid having to set the NSProjectPath manually.
+            WOProjectBundle mainBundle = WOProjectBundle.projectBundleForProject(mainBundleName, false);
+            if (mainBundle == null) {
+                projectPath = System.getProperty("projects." + mainBundleName);
+                if (projectPath == null)
+                    projectPath = "../..";
+            } else {
+                projectPath = mainBundle.projectPath();
+            }
+            
+        if (projectPath != null) {
+            aPropertiesPath = pathForPropertiesUnderProjectPath(projectPath);
+            if (aPropertiesPath != null) {
+                projectsInfo.addObject("Application: " + mainBundleName 
+                            + " (opened, developement-mode) " + aPropertiesPath); 
+            }
+        }
+
+        if (aPropertiesPath == null) {
+            // The application project is not opened from PBX, use the one in the bundle. 
+            aPropertiesPath = resourceManager.pathForResourceNamed("Properties", "app", null);
+            if (aPropertiesPath != null) {
+               aPropertiesPath = getActualPath(aPropertiesPath);
+               projectsInfo.addObject("Application: " + mainBundleName 
+                            + " (not opened, installed) " + aPropertiesPath);  
+            }
+        }
+
+        if (aPropertiesPath != null) 
+            propertiesPaths.addObject(aPropertiesPath);
+
+
+        /* *** WebObjects.properties in the user home directory *** */
+        String userHome = System.getProperty("user.home");
+        if (userHome != null  &&  userHome.length() > 0) { 
+            File file = new File(userHome, "WebObjects.properties");
+            if (file.exists()  &&  file.isFile()  &&  file.canRead()) {
+                try {
+                    aPropertiesPath = file.getCanonicalPath();
+                    projectsInfo.addObject("User:        WebObjects.properties " + aPropertiesPath);  
+                    propertiesPaths.addObject(aPropertiesPath);
+                } catch (java.io.IOException ex) {
+                    ;
+                }
+            }
+        }
+        
+        /* *** Report the result *** */ 
+        if (reportLoggingEnabled  &&  projectsInfo.count() > 0) {
+            StringBuffer message = new StringBuffer();
+            message.append("\n\n")
+                    .append("ERXProperties has found the following Properties files: \n");
+            e = projectsInfo.objectEnumerator();
+            while (e.hasMoreElements()) 
+                message.append(e.nextElement()).append("\n");
+            
+            log.info(message.toString());
+        }
+
+        return propertiesPaths.immutableClone();
+    }
+
+    /** 
+     * Returns the full path to the Properties file under the 
+     * given project path. At the current implementation, 
+     * it looks for /Properties and /Resources/Properties. 
+     * If the Properties file doesn't exist, returns null.  
+     * 
+     * @param projectPath  string to the project root directory
+     * @return  the path to the Properties file if it exists
+     */
+    public static String pathForPropertiesUnderProjectPath(String projectPath) {
+        String path = null; 
+        final NSArray supportedPropertiesPaths = new NSArray(new Object[] 
+                                        {"/Properties", "/Resources/Properties"});
+        Enumeration e = supportedPropertiesPaths.objectEnumerator();
+        while (e.hasMoreElements()) {
+            File file = new File(projectPath + (String) e.nextElement());
+            if (file.exists()  &&  file.isFile()  &&  file.canRead()) {
+                try {
+                    path = file.getCanonicalPath();
+                } catch (IOException ex) {
+                    log.error(ex.getClass().getName() + ": " + ex.getMessage());
+                }
+                break;
+            }
+        }
+        return path;
+    }
+
+    /**
+     * Returns actual full path to the given file system path  
+     * that could contain symbolic links. For example: 
+     * /Resources will be converted to /Versions/A/Resources
+     * when /Resources is a symbolic link.
+     * 
+     * @param path  path string to a resource that could 
+     *               contain symbolic links
+     * @return actual path to the resource
+     */
+    public static String getActualPath(String path) {
+        String actualPath = null;
+        File file = new File(path);
+        try {
+            actualPath = file.getCanonicalPath();
+        } catch (Exception ex) {
+            log.warn("The file at " + path + " does not seem to exist. Exception: " 
+                + ex.getClass().getName() + ": " + ex.getMessage());
+        }
+        return actualPath;
+    }
+
 }
