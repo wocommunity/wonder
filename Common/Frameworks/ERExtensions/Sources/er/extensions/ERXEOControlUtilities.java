c59e0f0981dce90280e231c7f2735bcd9e5faec3
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-22 03:55:04 +0000

    Moving over some of the primaryKeyFetchSpec methods from Clazz to control utilities.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1013 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOControlUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOControlUtilities.java
@@ -8,13 +8,22 @@ package er.extensions;
 
 import com.webobjects.foundation.NSArray;
 
+import com.webobjects.eoaccess.EOEntity;
+import com.webobjects.eoaccess.EOModelGroup;
+
 import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOFetchSpecification;
 import com.webobjects.eocontrol.EOGlobalID;
+import com.webobjects.eocontrol.EOObjectStoreCoordinator;
+import com.webobjects.eocontrol.EOQualifier;
+
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableArray;
 
 /**
  * Collection of EOF utility method centered around
- * EOControl specific utilities.
+ * EOControl.
  */
 public class ERXEOControlUtilities {
 
@@ -28,5 +37,49 @@ public class ERXEOControlUtilities {
             NSArray gids = new NSArray(ec.globalIDForObject(eo));
             ec.invalidateObjectsWithGlobalIDs(gids);
         }
+    }
+
+    /**
+     * Constructs a fetch specification that will only fetch the primary
+     * keys for a given qualifier.
+     * @param ec editing context, only used to determine the entity
+     * @param entityName name of the entity, only used to determine the entity
+     * @param eoqualifier to construct the fetch spec with
+     * @param sortOrderings array of sort orderings to sort the result set
+     *		with.
+     * @param additionalKeys array of additional key paths to construct the
+     *		raw rows key paths to fetch.
+     * @return fetch specification that can be used to fetch primary keys for
+     * 		a given qualifier and sort orderings.
+     */
+    public static EOFetchSpecification primaryKeyFetchSpecificationForEntity(EOEditingContext ec,
+                                                                      String entityName,
+                                                                      EOQualifier eoqualifier,
+                                                                      NSArray sortOrderings,
+                                                                      NSArray additionalKeys) {
+        EOModelGroup group = EOModelGroup.modelGroupForObjectStoreCoordinator((EOObjectStoreCoordinator)ec.rootObjectStore());
+        EOEntity entity = group.entityNamed(entityName);
+        EOFetchSpecification fs = new EOFetchSpecification(entityName, eoqualifier, sortOrderings);
+        fs.setFetchesRawRows(true);
+        NSMutableArray keys = new NSMutableArray(entity.primaryKeyAttributeNames());
+        if (additionalKeys != null) {
+            keys.addObjectsFromArray(additionalKeys);
+        }
+        fs.setRawRowKeyPaths(keys);
+        return fs;
+    }
+
+    /**
+    * Fetches an array of primary keys matching a given qualifier
+     * and sorted with a given array of sort orderings.
+     * @param ec editing context to fetch into
+     * @param entityName name of the entity
+     * @param eoqualifier to restrict matching primary keys
+     * @param sortOrderings array of sort orders to sort result set
+     * @return array of primary keys matching a given qualifier
+     */
+    public static NSArray primaryKeysMatchingQualifier(EOEditingContext ec, String entityName, EOQualifier eoqualifier, NSArray sortOrderings) {
+        EOFetchSpecification fs = ERXEOControlUtilities.primaryKeyFetchSpecificationForEntity(ec, entityName, eoqualifier, sortOrderings, null);
+        return ec.objectsWithFetchSpecification(fs);
     }
 }
