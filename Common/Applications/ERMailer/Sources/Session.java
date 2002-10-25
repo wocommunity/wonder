ab34978b72179ab0417d21ad21501c6e0516becc
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-25 18:12:27 +0000

    Adding first cut of a batch mailer application, pulls mail messages from ERCMailMessage and sends them using ERJavaMail.

---
+++ Common/Applications/ERMailer/Sources/Session.java
@@ -0,0 +1,20 @@
+//
+// Session.java
+// Project ERMailer
+//
+// Created by max on Tue Oct 22 2002
+//
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+
+public class Session extends WOSession {
+
+    public Session() {
+        super();
+        
+        /* ** Put your per-session initialization code here ** */
+    }
+    
+}
