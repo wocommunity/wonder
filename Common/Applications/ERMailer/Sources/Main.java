f2ed21c58d5e08edfa464e70aa3599a9a23aa0c1
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-10-25 18:12:27 +0000

    Adding first cut of a batch mailer application, pulls mail messages from ERCMailMessage and sends them using ERJavaMail.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1068 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Applications/ERMailer/Sources/Main.java
@@ -0,0 +1,19 @@
+//
+// Main.java: Class file for WO Component 'Main'
+// Project ERMailer
+//
+// Created by max on Tue Oct 22 2002
+//
+ 
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+
+public class Main extends WOComponent {
+
+    public Main(WOContext context) {
+        super(context);
+    }
+
+}
---
+++ Common/Applications/ERMailer/Sources/er/javamail/mailer/ERMailerUtilities.java
@@ -0,0 +1,65 @@
+//
+// ERMailerUtilities.java
+// Project ERMailer
+//
+// Created by Max Muller on Tue Oct 22 2002
+//
+package er.javamail.mailer;
+
+import com.webobjects.foundation.*;
+
+import er.extensions.*;
+import er.corebusinesslogic.*;
+import er.javamail.*;
+
+/**
+ * Collection of utilities methods used to bridge the
+ * worlds of {@link er.corebusinesslogic.ERCMailMessage ERCMailMessage}
+ * and {@link er.javamail.ERMailDelevery ERMailDelevery}.
+ */
+public class ERMailerUtilities {
+
+    /** logging support */
+    public static final ERXLogger log = ERXLogger.getERXLogger(ERMailerUtilities.class);
+
+    /**
+     * Creates a ERMailDelivery for a given
+     * MailMessage.
+     * @param message mail message
+     * @return a mail delevery object
+     */
+    // ENHANCEME: Not handling hidden text, plain text, double byte (Japanese) language or file attachments.
+    public static ERMailDelivery createMailDeliveryForMailMessage(ERCMailMessage message) {
+        ERMailDeliveryHTML mail = new ERMailDeliveryHTML();
+
+        try {
+            // Add all of the addresses
+            mail.setFromAddress(message.fromAddress());
+            if (message.replyToAddress() != null)
+                mail.setReplyToAddress(message.replyToAddress());
+            mail.setToAddresses(message.toAddressesAsArray());
+            if (message.ccAddressesAsArray().count() > 0)
+                mail.setCCAddresses(message.ccAddressesAsArray());
+            if (message.bccAddressesAsArray().count() > 0)
+                mail.setBCCAddresses(message.bccAddressesAsArray());
+
+            // Set the xMailer if one is specified
+            if (message.xMailer() != null)
+                mail.setXMailerHeader(message.xMailer());
+            else if (ERMailer.instance().defaultXMailerHeader() != null)
+                mail.setXMailerHeader(ERMailer.instance().defaultXMailerHeader());
+            
+            // Set the content
+            mail.setSubject(message.title());
+            mail.setHTMLContent(message.text());
+        } catch (Exception e) {
+            log.error("Caught exception constructing mail to delevery: " + ERXUtilities.stackTrace(e));
+            mail = null;
+        }
+        return mail;
+    }
+
+    // IMPLEMENTME
+    public static void reportExceptionForMessage(Exception exception, ERCMailMessage mailMessage) {
+    }    
+}
