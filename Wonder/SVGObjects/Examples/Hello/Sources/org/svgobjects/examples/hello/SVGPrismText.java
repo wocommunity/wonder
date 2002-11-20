d35ee00ed4fbdf77f8c579ced1a52ae67892f3d4
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-11-20 07:35:24 +0000

    Adding Hello Example
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1172 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/SVGObjects/Examples/Hello/Sources/org/svgobjects/examples/hello/SVGPrismText.java
@@ -0,0 +1,19 @@
+/* SVGPrismText.java created by rmendis on Thu 08-Feb-2001 */
+package org.svgobjects.examples.hello;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+
+public class SVGPrismText extends WOComponent {
+
+    public SVGPrismText(WOContext context) {
+        super(context);
+    }
+    
+    /*
+     * non synching component
+     */
+    public boolean synchronizesVariablesWithBindings() {
+        return false;
+    }
+}
---
+++ Wonder/SVGObjects/Examples/Hello/Sources/org/svgobjects/examples/hello/SVGShadowText.java
@@ -0,0 +1,19 @@
+/* SVGShadowText.java created by rmendis on Thu 08-Feb-2001 */
+package org.svgobjects.examples.hello;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+
+public class SVGShadowText extends WOComponent {
+
+    public SVGShadowText(WOContext context) {
+        super(context);
+    }
+
+    /*
+     * non synching component
+     */
+    public boolean synchronizesVariablesWithBindings() {
+        return false;
+    }
+}
