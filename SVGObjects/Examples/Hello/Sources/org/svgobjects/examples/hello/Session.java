5f3cc2ed42d9f733fc2b5d7b39153e5f41ab2877
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-11-20 07:35:24 +0000

    Adding Hello Example
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1172 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ SVGObjects/Examples/Hello/Sources/org/svgobjects/examples/hello/Session.java
@@ -0,0 +1,8 @@
+package org.svgobjects.examples.hello;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+
+public class Session extends WOSession {
+}
