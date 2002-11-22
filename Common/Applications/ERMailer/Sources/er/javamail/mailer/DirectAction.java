db0ca1db01d58628881d08ee761e9cbd40d1a3b9
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-11-22 18:44:05 +0000

    Moving Application and friends to package er.javamail.mailer

--- Common/Applications/ERMailer/Sources/er/javamail/mailer/DirectAction.java
+++ Common/Applications/ERMailer/Sources/er/javamail/mailer/DirectAction.java
@@ -4,10 +4,10 @@
 //
 // Created by max on Tue Oct 22 2002
 //
+package er.javamail.mailer;
 
 import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
-import com.webobjects.eocontrol.*;
 
 public class DirectAction extends WODirectAction {
 
