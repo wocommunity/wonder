c74e5eb2657dd106bbde15ec6dd614f81e7d7d49
tuscland <tuscland@8dc76679-1c23-de11-85cd-0050568e728b>
2002-11-28 10:57:37 +0000

    Cleaned the whole framework and corrected encoding inconsistencies. Added the ability to save session state in ERMailDeliveryComponentBased for future use in a mail (does not seems to work, though).

--- Common/Frameworks/ERJavaMail/Sources/er/javamail/ERMailDeliveryJapanesePlainText.java
+++ Common/Frameworks/ERJavaMail/Sources/er/javamail/ERMailDeliveryJapanesePlainText.java
@@ -13,47 +13,10 @@ import javax.activation.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
-public class ERMailDeliveryJapanesePlainText extends ERMailDelivery {
+public class ERMailDeliveryJapanesePlainText extends ERMailDeliveryPlainText {
 
-    /** String Message content */
-    private String _iso2022jpTextContent;
-
-    /** Sets the subject for the current message instance */
-    public void setSubject (String subject) throws MessagingException {
-        String encoded = null;
-        try {
-            encoded = MimeUtility.encodeText (subject, "ISO-2022-JP", "B");
-        } catch (Exception e) {
-            encoded = subject;
-        }
-        this.mimeMessage ().setSubject  (encoded);
+    public ERMailDeliveryJapanesePlainText() {
+        setCharset("ISO-2022-JP");
     }
-
-    /** Sets the text content of the current message. */
-    public void setTextContent (String text) {
-        _iso2022jpTextContent = unicodeToISO2022JP (text);
-    }
-
-    /** Pre-processes the mail before it gets sent.
-        @see ERMailDelivery#prepareMail */
-    // protected DataHandler prepareMail() {
-    public DataHandler prepareMail() {
-        return new DataHandler(_iso2022jpTextContent, "text/plain; charset=\"ISO-2022-JP\"");
-    }
-
-    /** Encodes Unicode strings into ISO-2022-JP string. */
-    public static String unicodeToISO2022JP (String unicodeString) {
-        String iso2022jpString = null;
-        
-        try {
-            byte[] byteArray = unicodeString.getBytes("ISO2022JP");
-            iso2022jpString = new String(byteArray);
-        } catch (java.io.UnsupportedEncodingException ex) {
-            iso2022jpString = unicodeString;
-        }
-
-        return iso2022jpString;
-    }
-
 }
 
