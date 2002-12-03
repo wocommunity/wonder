75ad2e148b23c1af8d7190e68a5431063d21c890
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2002-12-03 02:52:26 +0000

    Pick now plays nice with embedded page configurations.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1255 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WFactory.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WFactory.java
@@ -4,7 +4,6 @@
  * This software is published under the terms of the NetStruxr
  * Public Software License version 0.5, a copy of which has been
  * included with this distribution in the LICENSE.NPL file.  */
-
 package er.directtoweb;
 
 import com.webobjects.directtoweb.*;
@@ -17,7 +16,6 @@ import er.extensions.*;
 
 
 public class ERD2WFactory extends D2W {
-
 
     private D2WContext _privateContext;
     public D2WContext privateContext(WOSession s) {
@@ -100,8 +98,6 @@ public class ERD2WFactory extends D2W {
         }
         return newPage;
     }    
-
-    
 
 }
 
