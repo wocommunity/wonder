ed1582e9b9fab7264d3cd25cb1c67b7e4db20ae8
tuscland <tuscland@8dc76679-1c23-de11-85cd-0050568e728b>
2002-12-03 12:06:11 +0000

    Added enhanced equivalent of java.lang.String.toLowerCase method that runs faster and that allocates less memory.

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXStringUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXStringUtilities.java
@@ -481,11 +481,50 @@ public class ERXStringUtilities {
     }
 
     /**
-        * Removes the spaces in a given String
+     * Removes the spaces in a given String
      * @return string removing all spaces in it.
      */
     public static String escapeSpace(String aString){
         NSArray parts = NSArray.componentsSeparatedByString(aString," ");
         return parts.componentsJoinedByString("");
+    }
+
+    /** This method runs about 20 times faster than
+     * java.lang.String.toLowerCase (and doesn't waste any storage
+     * when the result is equal to the input).  Warning: Don't use
+     * this method when your default locale is Turkey.
+     * java.lang.String.toLowerCase is slow because (a) it uses a
+     * StringBuffer (which has synchronized methods), (b) it
+     * initializes the StringBuffer to the default size, and (c) it
+     * gets the default locale every time to test for name equal to
+     * "tr".
+     * @see http://www.norvig.com/java-iaq.html#tolower
+     * @author Peter Norvig **/
+    public static String toLowerCase(String str) {
+        int len = str.length();
+        int different = -1;
+        // See if there is a char that is different in lowercase
+        for(int i = len-1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            if (Character.toLowerCase(ch) != ch) {
+                different = i;
+                break;
+            }
+        }
+
+        // If the string has no different char, then return the string as is,
+        // otherwise create a lowercase version in a char array.
+        if (different == -1)
+            return str;
+        else {
+            char[] chars = new char[len];
+            str.getChars(0, len, chars, 0);
+            // (Note we start at different, not at len.)
+            for(int j = different; j >= 0; j--) {
+                chars[j] = Character.toLowerCase(chars[j]);
+            }
+
+            return new String(chars);
+        }
     }
 }
