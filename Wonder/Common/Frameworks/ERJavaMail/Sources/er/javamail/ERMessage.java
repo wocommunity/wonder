9a61a3cf95118aee3e9fdec4946bb8bdb8fbc0e9
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2002-12-18 05:54:18 +0000

    Added bunch of public methods that returns the recipients of the message as well as toString(); mostly logging purpose.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1327 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERJavaMail/Sources/er/javamail/ERMessage.java
+++ Wonder/Common/Frameworks/ERJavaMail/Sources/er/javamail/ERMessage.java
@@ -39,4 +39,92 @@ public class ERMessage extends Object {
         return anObject;
     }
     
+    /**
+     * @param recipientType which can be: 
+     * <code>Message.RecipientType.To</code>, 
+     * <code>Message.RecipientType.CC</code>, or
+     * <code>Message.RecipientType.BCC</code>
+     */
+    public Address[] recipients (Message.RecipientType recipientType) throws MessagingException{
+        if (message == null)  return null;
+        
+        Address[] recipients = null;
+        recipients = message.getRecipients (recipientType);
+        return recipients;
+    }
+    
+    public String recipientsAsString (Message.RecipientType recipientType) 
+            throws MessagingException, AddressException {
+        return recipientsAsString (recipientType, -1);
+    }
+    
+    public String recipientsAsString (Message.RecipientType recipientType, int maxAddresses) 
+            throws MessagingException, AddressException {
+        Address[] allAddresses = recipients (recipientType);
+        if (allAddresses == null  ||  allAddresses.length == 0)  return null;
+        
+        Address[] limitteredAddresses = null;
+
+        if (maxAddresses > 0) {
+            limitteredAddresses = new Address[maxAddresses];
+            System.arraycopy(allAddresses, 0, 
+                    limitteredAddresses, 0, Math.min (allAddresses.length, maxAddresses));
+        } else {
+            limitteredAddresses = allAddresses;
+        }
+        
+        StringBuffer result = new StringBuffer(); 
+        result.append (ERMailUtils.convertInternetAddressesToNSArray((InternetAddress[]) limitteredAddresses)
+                                                        .componentsJoinedByString (", "));
+        if (0 < maxAddresses  &&  maxAddresses < allAddresses.length) {
+            result.append (", and ");
+            result.append (allAddresses.length - maxAddresses);
+            result.append (" other recipients");
+        }
+        
+        return result.toString ();
+    }
+
+    public String allRecipientsAsString () throws MessagingException {
+        return allRecipientsAsString (true, -1);
+    } 
+    
+    public String allRecipientsAsString (boolean includeBcc) throws MessagingException {
+        return allRecipientsAsString (includeBcc, -1);
+    }
+
+    public String allRecipientsAsString (boolean includeBcc, int maxAddresses) throws MessagingException {
+        StringBuffer recipients = new StringBuffer ();
+        String addresses = recipientsAsString(Message.RecipientType.TO, maxAddresses);
+        if (addresses != null  &&  addresses.length() > 0) 
+            recipients.append ("To: ").append (addresses);
+
+        addresses = recipientsAsString(Message.RecipientType.CC, maxAddresses);
+        if (addresses != null  &&  addresses.length() > 0) 
+            recipients.append ("CC: ").append (addresses);
+        
+        if (includeBcc) {
+            addresses = recipientsAsString (Message.RecipientType.BCC, maxAddresses);
+            if (addresses != null  &&  addresses.length () > 0) 
+                recipients.append ("BCC: ").append (addresses);
+        }
+        return recipients.toString ();
+    }
+    
+    public String toString () {
+        StringBuffer sbuf = new StringBuffer ();
+        sbuf.append ("<").append(getClass ().getName ()).append (" ");
+        if (message == null) {
+            sbuf.append("No mime message is set.");
+        } else {
+            try {
+                sbuf.append(allRecipientsAsString ());
+            } catch (MessagingException ex) {
+                ;
+            }
+        }
+        sbuf.append (">");
+        return sbuf.toString ();
+    }
+
 }
