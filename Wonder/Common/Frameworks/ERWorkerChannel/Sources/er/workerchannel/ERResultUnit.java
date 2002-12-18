175f9cf7fe3e4bca81a8026eafb904bc0a5c39e8
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2002-12-18 06:52:00 +0000

    Initial import of ERWorkerChannel, which is an intelligence queue with multi-threaded workers; can ease queue-based, multi-threaded programming. Note that the current implementation is bit rough.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1330 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERWorkerChannel/Sources/er/workerchannel/ERResultUnit.java
@@ -0,0 +1,13 @@
+//
+// ERResultUnit.java
+// Project ERWorkerChannel
+//
+// Created by tatsuya on Mon Jul 29 2002
+//
+package er.workerchannel;
+
+public abstract class ERResultUnit {
+
+    public abstract Object resultValue();
+
+}
