383988cf2201f6197c01ceec6a966e25280eedc2
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2002-12-29 22:38:27 +0000

    Re-factored ERWorkerChannel to prepare future extension called remote worker channel.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1380 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERWorkerChannel/Sources/er/workerchannel/ERWorkerChannel.java
+++ Common/Frameworks/ERWorkerChannel/Sources/er/workerchannel/ERWorkerChannel.java
@@ -8,116 +8,17 @@ package er.workerchannel;
 
 import er.extensions.ERXLogger;
 
-public class ERWorkerChannel {
+public abstract class ERWorkerChannel {
 
-    public static final ERXLogger log = ERXLogger.getERXLogger(ERWorkerChannel.class);
-
-    private final int _queueSize;
-    private ERWorkUnit[] _workUnitQueue;
-    private int _head;
-    private int _tail;
-    private int _count;
-    private boolean _isSuspended;
-
-    private final ERWorkerThread[] _threadPool;
-
-    public ERWorkerChannel(int numberOfThreads, int queueSize) {
-        _queueSize = queueSize; 
-        _initalizeWorkUnitQueue();
-        _isSuspended = false;
-
-        _threadPool = new ERWorkerThread[numberOfThreads];
-        for (int i = 0; i < _threadPool.length; i++) 
-            _threadPool[i] = new ERWorkerThread("Worker-" + i, this);
-    }
-
-    public void startWorkers() {
-        for (int i = 0; i < _threadPool.length; i++) 
-            _threadPool[i].start();
-    }
-
-    public synchronized void shutdownWorkers() {
-        log.info("shutdownWorkers() - count = " + _count);
-        
-        while (_count > 0) {
-            notifyAll();
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                ;
-            }
-        }
-        
-        for (int i = 0; i < _threadPool.length; i++) 
-            _threadPool[i].shutdown();
-
-        for (int i = 0; i < _threadPool.length; i++) {
-            notifyAll();
-            try {
-                _threadPool[i].join();
-            } catch (InterruptedException ex) {
-                ;
-            }
-        }
-    }
-
-    public void suspendWorkers() {
-        _isSuspended = true;
-    }
+    public abstract void startWorkers();
+    public abstract void shutdownWorkers();
+    public abstract void suspendWorkers();
+    public abstract void resumeWorkers();
+    public abstract void cancelScheduledWorkUnits();
     
-    public void resumeWorkers() {
-        _isSuspended = false;
-        notifyAll();
-    }
-
-    public synchronized ERResultUnit scheduleWorkUnit(ERWorkUnit workUnit) {
-        while (_count >= _workUnitQueue.length) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                ;
-            }
-        }
-        ERFutureResult futureResult = new ERFutureResult();
-        workUnit.setFutureResult(futureResult);
-        _workUnitQueue[_tail] = workUnit;
-        _tail = (_tail + 1) % _workUnitQueue.length;
-        _count++;
-        if (_count >= _queueSize * 0.15d) 
-            notifyAll();
-        return futureResult;
-    }
-
-    public synchronized ERWorkUnit dispatchWorkUnit() {
-        while (_count <= 0  ||  _isSuspended) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                ;
-            }
-        }
-        ERWorkUnit workUnit = _workUnitQueue[_head];
-        _head = (_head + 1) % _workUnitQueue.length;
-        _count--;
-        if (_count <= _queueSize * (1.0d - 0.15d)) 
-            notifyAll();
-        return workUnit;
-    }
-
-    public synchronized void cancelScheduledWorkUnits() {
-        _initalizeWorkUnitQueue();
-        _isSuspended = false;
-    }
+    public abstract ERResultUnit scheduleWorkUnit(ERWorkUnit workUnit);
+    public abstract ERWorkUnit dispatchWorkUnit();
     
-    public int currentQueueSize() {
-        return _count;
-    }
+    public abstract int currentQueueSize();
 
-    private void _initalizeWorkUnitQueue() {
-        _workUnitQueue = new ERWorkUnit[_queueSize];
-    	_head = 0;
-    	_tail = 0;
-    	_count = 0;
-    }
-    
 }
