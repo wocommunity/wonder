7970151bccc73ae28b9d7953c8aa9d099e7a2dc6
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-01-03 02:56:49 +0000

    Extending the page and not the template
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1414 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUPickListPage.java
+++ Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUPickListPage.java
@@ -6,13 +6,10 @@
 //
 package er.neutral;
 
-import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
-import com.webobjects.eocontrol.*;
-import com.webobjects.eoaccess.*;
 import er.directtoweb.*;
 
-public class ERNEUPickListPage extends ERD2WPickListPageTemplate {
+public class ERNEUPickListPage extends ERD2WPickListPage {
 
     public ERNEUPickListPage(WOContext context) {
         super(context);
