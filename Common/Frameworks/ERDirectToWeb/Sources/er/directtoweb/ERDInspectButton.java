d36257d1d76fbfaae302bd7f7828c9d5f548f3d1
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2003-01-06 19:13:20 +0000

    adding the ERDInspectButton

---
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDInspectButton.java
@@ -0,0 +1,38 @@
+//
+// ERDInspectButton.java: Class file for WO Component 'ERDInspectButton'
+// Project ERDirectToWeb
+//
+// Created by bposokho on Mon Jan 06 2003
+//
+package er.directtoweb;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+
+public class ERDInspectButton extends ERDCustomEditComponent {
+
+    public ERDInspectButton(WOContext context) {
+        super(context);
+    }
+
+    public boolean isStateless() { return true; }
+    public boolean synchronizesVariablesWithBindings() { return false; }
+
+    public D2WContext d2wContext() { return (D2WContext)valueForBinding("d2wContext"); }
+
+    public WOComponent inspect() {
+        EOEditingContext context = er.extensions.ERXExtensions.newEditingContext();
+        EOEnterpriseObject localObject = EOUtilities.localInstanceOfObject(context, object());
+        String configuration = (String)d2wContext().valueForKey("inspectConfigurationNameForEntity");
+        System.out.println("inspectConfigurationNameForEntity = "+configuration);
+        EditPageInterface epi = (EditPageInterface)D2W.factory().pageForConfigurationNamed(configuration, session());
+        epi.setObject(localObject);
+        epi.setNextPage(context().page());
+        context.hasChanges(); // Ensuring it survives.
+        return (WOComponent)epi;
+    }
+
+}
