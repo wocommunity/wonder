62d6773acc2867a5df5dfe8bb34fc393fb9797a3
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-01-07 00:32:22 +0000

    Adding Processing state
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1421 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERCoreBusinessLogic/Support/populate.sql
+++ Common/Frameworks/ERCoreBusinessLogic/Support/populate.sql
@@ -4,3 +4,4 @@ insert into ERCMAIL_STATE (ID, NAME) values ('rtbs', 'Ready to be sent');
 insert into ERCMAIL_STATE (ID, NAME) values ('sent', 'Sent');
 insert into ERCMAIL_STATE (ID, NAME) values ('rcvd', 'Received');
 insert into ERCMAIL_STATE (ID, NAME) values ('wait', 'Wait');
+insert into ERCMAIL_STATE (ID, NAME) values ('proc', 'Processing');
