62eedddafb77c4460740b563eed9f34707c39dc9
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2003-01-17 23:55:56 +0000

    adding ERXEditDateJavascript component

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEditDateJavascript.java
@@ -0,0 +1,48 @@
+//
+// ERXEditDateJavascript.java: Class file for WO Component 'ERXEditDateJavascript'
+// Project ERExtensions
+//
+// Created by bposokho on Thu Jan 16 2003
+//
+package er.extensions;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+
+public class ERXEditDateJavascript extends WOComponent {
+    static final ERXLogger log = ERXLogger.getERXLogger(ERXEditDateJavascript.class);
+    
+    public ERXEditDateJavascript(WOContext context) {
+        super(context);
+    }
+
+    int i = 0;
+
+    //public boolean isStateless() { return true; }
+    //public boolean synchronizesVariablesWithBindings() { return false; }
+
+    protected static final NSTimestampFormatter DATE_FORMAT = new NSTimestampFormatter("%m/%d/%Y");
+    protected static final NSTimestampFormatter DATE_FORMAT_YEAR_TWO_DIGITS = new NSTimestampFormatter("%m/%d/%y");
+    public String dateString;
+    
+    public String name() { return "datebox"+hashCode(); }
+    //public Object value() {return dateString;}
+
+    private static String _datePickerJavaScriptUrl;
+    public String datePickerJavaScriptUrl() {
+        if (_datePickerJavaScriptUrl==null) {
+            _datePickerJavaScriptUrl= application().resourceManager().urlForResourceNamed("date-picker.js",
+                                                                                          "ERExtensions",
+                                                                                          null,
+                                                                                          context().request());
+        }
+        return _datePickerJavaScriptUrl;
+    }
+
+    public void appendToResponse(WOResponse aResponse, WOContext aContext) {
+        i++;
+        super.appendToResponse(aResponse,aContext);
+    }
+}
