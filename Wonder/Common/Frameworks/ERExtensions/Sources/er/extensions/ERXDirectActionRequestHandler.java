5db94b862d008f1bf39e04c7a56653f4120ed1d9
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-02-18 00:19:13 +0000

    Adding ability to disable automatic message encoding, now getting the message encoding from the browser of the request if using ERXRequest.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1584 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXDirectActionRequestHandler.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXDirectActionRequestHandler.java
@@ -11,6 +11,8 @@ import com.webobjects.appserver.*;
 import com.webobjects.appserver._private.WODirectActionRequestHandler;
 
 /**
+ * Improved direct action request handler. Will automatically handle
+ * character encodings for supported 
  * 
  * NOTE: This class is multi thread safe. 
  */
@@ -18,6 +20,21 @@ public class ERXDirectActionRequestHandler extends WODirectActionRequestHandler 
 
     /** logging support */
     public static final ERXLogger log = ERXLogger.getERXLogger(ERXDirectActionRequestHandler.class);
+
+    /** caches if automatic message encoding is enabled, defaults to true */
+    protected static Boolean automaticMessageEncodingEnabled;
+
+    /**
+     * Allows the disabling of automatic message encoding. Useful for
+     * backend services where you want to just use the default encoding.
+     * @return if automatic message encoding is enabled.
+     */
+    public static boolean automaticMessageEncodingEnabled() {
+        if (automaticMessageEncodingEnabled == null) {
+            automaticMessageEncodingEnabled = ERXProperties.booleanForKeyWithDefault("er.extensions.ERXMessageEncoding.Enabled", true) ? Boolean.TRUE : Boolean.FALSE;
+        }
+        return automaticMessageEncodingEnabled.booleanValue();
+    }
     
     public ERXDirectActionRequestHandler() {
         super();
@@ -68,20 +85,24 @@ public class ERXDirectActionRequestHandler extends WODirectActionRequestHandler 
                 log.error("Caught exception when caching response. Request: " + request + " exception: " + e);
             }
         }
-        
-        ERXMessageEncoding messageEncoding = null;
-        
-        // This should retrieve the session object belonging to the same 
-        // worker thread that's been calling the current handleRequest method. 
-        WOSession session = ERXExtensions.session();   // get it from the thread specific storage 
-        if (session != null  &&  session instanceof ERXSession) {
-            ERXSession erxSession = (ERXSession)session;
-            messageEncoding = erxSession.messageEncoding();
-            erxSession.lastActionWasDA = true;
-        } else {
-            messageEncoding = new ERXMessageEncoding(request.browserLanguages());
-        }
-        messageEncoding.setEncodingToResponse(response);
+        if (automaticMessageEncodingEnabled()) {
+            ERXMessageEncoding messageEncoding = null;
+            
+            // This should retrieve the session object belonging to the same
+            // worker thread that's been calling the current handleRequest method.
+            WOSession session = ERXExtensions.session();   // get it from the thread specific storage
+            if (session != null  &&  session instanceof ERXSession) {
+                ERXSession erxSession = (ERXSession)session;
+                messageEncoding = erxSession.messageEncoding();
+                erxSession.lastActionWasDA = true;
+            } else if (request instanceof ERXRequest) {
+                ERXBrowser browser = ((ERXRequest)request).browser();
+                messageEncoding = browser.messageEncodingForRequest(request);
+            } else {
+                messageEncoding = new ERXMessageEncoding(request.browserLanguages());
+            }
+            messageEncoding.setEncodingToResponse(response);
+        } 
         return response;
     }
 
