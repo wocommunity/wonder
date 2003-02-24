512010c493733a406e7466f154a2d11e7bc8d6ac
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-02-24 19:06:53 +0000

    Moving method used to evaluate an arbitrary SQL statement (not just a select) from ERXExtensions, also improving the method a bit
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1636 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOAccessUtilities.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOAccessUtilities.java
@@ -30,12 +30,38 @@ public class ERXEOAccessUtilities {
                                                      String sequenceName) {
         String sqlString = "select "+sequenceName+".nextVal from dual";
         NSArray array = EOUtilities.rawRowsForSQL(ec, modelNamed, sqlString);
-        if (array.count() == 0)
+        if (array.count() == 0) {
             throw new RuntimeException("Unable to generate value from sequence named: " + sequenceName
-                                       + " in model: " + modelNamed);
+                                       + " in model: " + modelNamed);            
+        }
         NSDictionary dictionary = (NSDictionary)array.objectAtIndex(0);
         NSArray valuesArray = dictionary.allValues();
         return (Number)valuesArray.objectAtIndex(0);
     }
-        
+
+    /**
+     * Utility method used to execute arbitrary SQL. This
+     * has the advantage over the
+     * {@link com.webobjects.eoaccess.EOUtilities EOUtilities}
+     * <code>rawRowsForSQL</code> in that it can be used with
+     * other statements besides just SELECT without throwing
+     * exceptions.
+     * @param ec editing context that determines which model group
+     *		and database context to use.
+     * @param entityName name of an entity in the model connected
+     *		to the database you wish to execute SQL against
+     * @param exp SQL expression
+     */
+    // ENHANCEME: Should support the use of bindings
+    // ENHANCEME: Could also support the option of using a seperate EOF stack so as to execute
+    //		sql in a non-blocking fashion.
+    public static void evaluateSQLWithEntityNamed(EOEditingContext ec, String entityName, String exp) {
+        EOEntity entity = EOUtilities.entityNamed(ec, entityName);
+        EODatabaseContext dbContext = EODatabaseContext.registeredDatabaseContextForModel(entity.model(), ec);
+        EOAdaptorChannel adaptorChannel = dbContext.availableChannel().adaptorChannel();
+        if (!adaptorChannel.isOpen())
+            adaptorChannel.openChannel();
+        EOSQLExpressionFactory factory=adaptorChannel.adaptorContext().adaptor().expressionFactory();
+        adaptorChannel.evaluateExpression(factory.expressionForString(exp));
+    }
 }
