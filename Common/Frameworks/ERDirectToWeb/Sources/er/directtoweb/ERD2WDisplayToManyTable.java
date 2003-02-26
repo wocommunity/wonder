3d1232047ba76fe9d4197bebd16ff6e43cff0da9
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2003-02-26 23:53:37 +0000

    now possible to take advantage of inspectConfigurationNameForEntity using the destination entity

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WDisplayToManyTable.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WDisplayToManyTable.java
@@ -6,12 +6,33 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.directtoweb;
 
-import com.webobjects.appserver.WOContext;
-import com.webobjects.directtoweb.D2WDisplayToManyTable;
+import com.webobjects.appserver.*;
+import com.webobjects.directtoweb.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.eocontrol.*;
 
 public class ERD2WDisplayToManyTable extends D2WDisplayToManyTable {
 
     public ERD2WDisplayToManyTable(WOContext context) {
         super(context);
     }
+
+    public WOComponent inspectAction() {
+        if(item!=null){
+            D2WContext newContext=new D2WContext(session());
+            newContext.takeValueForKey(item, "object");
+            EOEntity entity = EOModelGroup.defaultGroup().entityNamed(item.entityName());
+            newContext.takeValueForKey(entity, "entity");
+            String inspectConfigurationName = (String)newContext.valueForKey("inspectConfigurationNameForEntity");
+            if(inspectConfigurationName!=null && item!=null){
+                InspectPageInterface inspectPage=(InspectPageInterface)D2W.factory().pageForConfigurationNamed(inspectConfigurationName,
+                                                                                                               session());
+                inspectPage.setObject(item);
+                inspectPage.setNextPage(context().page());
+                return (WOComponent)inspectPage;
+            }
+        }
+        return super.inspectAction();
+    }
+    
 }
