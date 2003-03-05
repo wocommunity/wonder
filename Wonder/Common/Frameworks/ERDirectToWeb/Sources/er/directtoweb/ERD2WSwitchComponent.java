a9efc82f92b941882841161fdc26f275b7ffa464
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2003-03-05 22:41:14 +0000

    commenting out the resetting of the caches that breaks the release page
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1684 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WSwitchComponent.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WSwitchComponent.java
@@ -29,14 +29,15 @@ public class ERD2WSwitchComponent extends D2WSwitchComponent  {
      * binding checks that shouldn't be done.
      */
     public void awake() {}
-    
+
+	 //FIXME restting the caches breaks the context in the embedded component
     public void resetCaches() {
-        log.debug("Resetting caches");
-        takeValueForKey(null,"_task"); // this will break in 5.0 :-)
-        takeValueForKey(null,"_entityName");
+        //log.debug("Resetting caches");
+        //takeValueForKey(null,"_task"); // this will break in 5.0 :-)
+        //takeValueForKey(null,"_entityName");
         // Finalizing a context is a protected method, hence the utiltiy.
-        ERD2WUtilities.finalizeContext((D2WContext)valueForKey("_context"));
-        takeValueForKey(null,"_context");
+        //ERD2WUtilities.finalizeContext((D2WContext)valueForKey("subContext"));
+        //takeValueForKey(null,"_context");
     }
 
     private String _pageConfiguration;
