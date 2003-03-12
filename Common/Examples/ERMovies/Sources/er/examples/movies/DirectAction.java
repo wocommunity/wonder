f54fdd90c01b81294d30aa163aab57e2cbcb94c6
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-03-12 22:38:28 +0000

    showing off ERD2WDirectAction
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1716 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Examples/ERMovies/Sources/er/examples/movies/DirectAction.java
+++ Common/Examples/ERMovies/Sources/er/examples/movies/DirectAction.java
@@ -9,9 +9,17 @@ package er.examples.movies;
 import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
 import com.webobjects.eocontrol.*;
+import er.directtoweb.*;
 
-public class DirectAction extends WODirectAction {
+public class DirectAction extends ERD2WDirectAction {
 
+    /**
+     * (ak) WARNING: normally, you would not just subclass ERD2WDirectAction
+     * without taking proper precautions that limit acces to the app.
+     * For example, you could check the name of the action performed
+     * and require that your user has logged in.
+     */
+    
     public DirectAction(WORequest aRequest) {
         super(aRequest);
     }
