0b50933308a9704ce40d9880240f57b36e4ffa2c
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-03-12 19:10:52 +0000

    Adding ERMovies, a D2W app that demonstrates ERNavigation using the standard movies OpenBase DB.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1708 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Examples/ERMovies/Sources/er/examples/movies/PageWrapper.java
@@ -0,0 +1,11 @@
+// Created by Direct to Web's Project Builder Wizard
+package er.examples.movies;
+
+import com.webobjects.appserver.*;
+
+public class PageWrapper extends WOComponent {
+
+    public PageWrapper(WOContext aContext) {
+        super(aContext);
+    }
+}
