f7ee89f8208531567ca59fb5603de2109fb7b3c6
posokhow <posokhow@8dc76679-1c23-de11-85cd-0050568e728b>
2003-03-13 02:01:10 +0000

    allowing for display of index in the sorted browser

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditSortedToManyFault.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditSortedToManyFault.java
@@ -18,29 +18,48 @@ public class ERD2WEditSortedToManyFault extends D2WEditToManyFault {
         super(context);
     }
 
+	public String indexKey(){
+		String indexKey = null;
+		EORelationship relationship = entity().relationshipNamed(propertyKey());
+		if (relationship!=null) {
+			EOEntity destinationEntity = relationship.destinationEntity();
+			if(destinationEntity!=null &&
+		destinationEntity.userInfo().valueForKey("isSortedJoinEntity") != null &&
+		((String)destinationEntity.userInfo().valueForKey("isSortedJoinEntity")).equals("true")) {
+				synchronized (_context) {
+					_context.setEntity(destinationEntity);
+					indexKey = (String)_context.valueForKey("indexKey");
+				}
+			}
+		}
+		return indexKey;
+	}
+
 
     private static D2WContext _context=new D2WContext();
     public NSArray sortedBrowserList() {
         NSArray result = browserList();
-        String indexKey=null;
-        EORelationship relationship = entity().relationshipNamed(propertyKey());
-        if (relationship!=null) {
-            EOEntity destinationEntity = relationship.destinationEntity();
-            if(destinationEntity!=null &&
-               destinationEntity.userInfo().valueForKey("isSortedJoinEntity") != null &&
-               ((String)destinationEntity.userInfo().valueForKey("isSortedJoinEntity")).equals("true")) {
-                synchronized (_context) {
-                    _context.setEntity(destinationEntity);
-                    indexKey = (String)_context.valueForKey("indexKey");
-                }
-            } 
-        }
-        if (indexKey!=null)
-            result =   ERXArrayUtilities.sortedArraySortedWithKey(result,
-                                                                  indexKey,
-                                                                  null);
+        if (indexKey()!=null)
+            result = ERXArrayUtilities.sortedArraySortedWithKey(result,
+																					 indexKey(),
+																					 null);
         
         return result;
-}
+	 }
+
+	 public String browserStringForItem(){
+		 String result = super.browserStringForItem();
+		 if(showIndex()){
+			 Integer index = (Integer)browserItem.valueForKey(indexKey());
+			 if(index != null){
+				 result = (index.intValue()+1)+". "+result;
+			 }
+		 }
+		 return result;
+	 }
+
+	 public boolean showIndex(){
+		 return ERXValueUtilities.booleanValueWithDefault(d2wContext().valueForKey("showIndex"), false);
+	 }
 
 }
