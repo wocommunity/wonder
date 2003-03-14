f9246e6dacf7f4939208f320068e7917558fac3c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-03-14 06:41:39 +0000

    completed commit - sorry for that, dunno what happened??
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@1726 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Applications/Vacation/Sources/Group.java
@@ -0,0 +1,14 @@
+//
+// Group.java
+// Project Vacation
+//
+// Created by mishra on Mon Nov 04 2002
+//
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+
+public class Group extends _Group {
+
+}
