7c601c6131aa189c503a35c15833f4c52ba972f4
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-04-08 13:59:22 +0000

    When you bind the list, but it doesn't have a value, but the dataSource has, the behaviour is now correct.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERXD2WList.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERXD2WList.java
@@ -6,11 +6,15 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.directtoweb;
 
-import com.webobjects.appserver.WOContext;
-import com.webobjects.directtoweb.D2WList;
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.directtoweb.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.eocontrol.*;
 
 // Only difference between this component and D2WList is that this one uses ERD2WSwitchComponent
 public class ERXD2WList extends D2WList {
+    protected EOArrayDataSource _dataSource = null;
 
     public ERXD2WList(WOContext context) {
         super(context);
@@ -20,4 +24,32 @@ public class ERXD2WList extends D2WList {
      * Calling super is a bad thing in 5.2 when used as an embedded list.
      */
     public void awake() {}
+
+    public EODataSource dataSource() {
+        if (this.hasBinding("dataSource") && this.valueForBinding("list") == null)
+            return (EODataSource) this.valueForBinding("dataSource");
+        if (this.hasBinding("list")) {
+            NSArray nsarray = (NSArray) this.valueForBinding("list");
+            EOEditingContext eoeditingcontext
+                = (nsarray != null && nsarray.count() > 0
+                   ? ((EOEnterpriseObject) nsarray.objectAtIndex(0))
+                   .editingContext()
+                   : null);
+            String entityName = (String) this.valueForBinding("entityName");
+            if(entityName == null) {
+                entityName = (nsarray != null && nsarray.count() > 0
+                              ? ((EOEnterpriseObject) nsarray.objectAtIndex(0))
+                              .entityName()
+                              : null);
+            }
+            if(entityName != null) {
+                if (_dataSource == null || !entityName.equals(_dataSource.classDescriptionForObjects().entityName()) || _dataSource.editingContext() != eoeditingcontext)
+                    _dataSource = (eoeditingcontext != null
+                                   ? (new EOArrayDataSource(EOClassDescription.classDescriptionForEntityName(entityName), eoeditingcontext)) : null);
+            }
+            if (_dataSource != null)
+                _dataSource.setArray(nsarray);
+        }
+        return _dataSource;
+    }
 }
