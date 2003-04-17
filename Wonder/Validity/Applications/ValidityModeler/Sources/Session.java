0567e482f9d962b7a4bf959f275f785aa2863353
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2003-04-17 15:15:55 +0000

    Now uses UTF8 as the browser encoding so that Validity can display any Unicode characters such as Japanese (Try not to think about the "Arial, Helvetica" font face tags for now. I'll make it configurable later.)
    git-svn-id: file:///Users/q/working/wondersvn/trunk@1838 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Validity/Applications/ValidityModeler/Sources/Session.java
+++ Wonder/Validity/Applications/ValidityModeler/Sources/Session.java
@@ -36,4 +36,15 @@ public class Session extends WOSession {
         isLoggedIn = newIsLoggedIn;
     }
 
+    public void takeValuesFromRequest(WORequest request, WOContext context) {
+        request.setDefaultFormValueEncoding("UTF8");
+        super.takeValuesFromRequest(request, context);
+    }
+
+    public void appendToResponse(WOResponse response, WOContext context) {
+        response.setContentEncoding("UTF8");
+        super.appendToResponse(response, context);
+        response.setHeader("text/html; charset=UTF-8", "Content-Type");
+    }
+
 }
