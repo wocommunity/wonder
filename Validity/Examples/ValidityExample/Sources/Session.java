0aa81ddb7aa31f3feadfc23eb1f2f299fd00cb64
tatsuya_kawano <tatsuya_kawano@8dc76679-1c23-de11-85cd-0050568e728b>
2003-04-17 15:16:28 +0000

    Now uses UTF8 as the browser encoding so that Validity can display any Unicode characters such as Japanese.

--- Validity/Examples/ValidityExample/Sources/Session.java
+++ Validity/Examples/ValidityExample/Sources/Session.java
@@ -16,5 +16,16 @@ public class Session extends WOSession {
         
         /* ** Put your per-session initialization code here ** */
     }
+
+    public void takeValuesFromRequest(WORequest request, WOContext context) {
+        request.setDefaultFormValueEncoding("UTF8");
+        super.takeValuesFromRequest(request, context);
+    }
+
+    public void appendToResponse(WOResponse response, WOContext context) {
+        response.setContentEncoding("UTF8");
+        super.appendToResponse(response, context);
+        response.setHeader("text/html; charset=UTF-8", "Content-Type");
+    }
     
 }
