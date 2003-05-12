609a4918d7c27f2a55b068b7d7ecb2345da6bd1c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-05-12 20:45:01 +0000

    changed default handling for templates, now the key will be regarded as the default

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXLocalizer.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXLocalizer.java
@@ -396,7 +396,7 @@ public class ERXLocalizer implements NSKeyValueCoding, NSKeyValueCodingAdditions
     }
 
     public String localizedTemplateStringForKeyWithObjectOtherObject(String key, Object o1, Object o2) {
-        String template = localizedStringForKey(key);
+        String template = localizedStringForKeyWithDefault(key);
         if (template != null)
             return ERXSimpleTemplateParser.sharedInstance().parseTemplateWithObject(template, null, o1, o2);
         return key;
