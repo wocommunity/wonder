7b9e72c159e0add6528892e481fac5fcc994d0ae
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-08-02 22:21:08 +0000

    - some fixes and optimizations to the display group - moved actual fetching code to erxaccessutilities
    Inheritance does not work with qualified sub-entities...
    
    
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2034 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXBatchingDisplayGroup.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXBatchingDisplayGroup.java
@@ -10,26 +10,43 @@ import java.util.*;
 
 /**
 * Extends {@link WODisplayGroup} in order to provide real batching.
- * This is done by adding TOP(start, rows) to the select statement
+ * This is done by adding database specific code to the select statement
  * from the {@link EOFetchSpecification} from the {@link WODisplayGroup}'s
  * {@link EODataSource} which <b>must</b> be an {@link EODatabaseDataSource}.
- * The top SQL command is recognized by FrontBase and maybe other DB servers.
- * Future versions will provide a delegate in order to modify the value
- * for lets say Oracle.
  */
 public class ERXBatchingDisplayGroup extends WODisplayGroup {
 
+    /** total number of rows */
+    protected int _rowCount = -1;
+
+    /** cache for the displayed objects */
+    protected NSArray _displayedObjects;
+
+    protected Boolean _isBatching = null;
+
+    public void setIsBatching(boolean value) {
+        _isBatching = value ? Boolean.TRUE : Boolean.FALSE;
+        redisplay();
+    }
+    
     /**
-    * Determines if batching is possible. 
+     * Determines if batching is possible. 
      *
      * @return true if dataSource is an instance of EODatabaseDataSource
      */
     public boolean isBatching() {
-        return dataSource() instanceof EODatabaseDataSource;
+        if(_isBatching == null) {
+            _isBatching = dataSource() instanceof EODatabaseDataSource ? Boolean.TRUE : Boolean.FALSE;
+        }
+        return _isBatching.booleanValue();
     }
 
+    public void setDataSource(EODataSource eodatasource) {
+        _isBatching = null;
+        super.setDataSource(eodatasource);
+    }
     /**
-    * Returns an {@link NSArray} containing the objects from the resulting rows starting
+     * Returns an {@link NSArray} containing the objects from the resulting rows starting
      * at start and stopping at end using a custom SQL, derived from the SQL
      * which the {@link EOFetchSpecification} would use normally {@link EOFetchSpecification.setHints}
      *
@@ -45,20 +62,8 @@ public class ERXBatchingDisplayGroup extends WODisplayGroup {
         spec.setSortOrderings(sortOrderings());
 
         EOEditingContext ec = databaseDataSource().editingContext();
-        String sql = ERXEOAccessUtilities.sqlForFetchSpecificationAndEditingContext(spec, ec);
+        String sql = ERXEOAccessUtilities.sqlForFetchSpecificationAndEditingContext(spec, ec, start, end);
         
-        //add TOP(start, (end - start)) after the SELECT word
-        int index = sql.indexOf("select");
-        if (index == -1) {
-            index = sql.indexOf("SELECT");
-        }
-        index += 6;
-
-        //FIXME: this works for frontbase, might need to be adjusted for other db servers!
-        StringBuffer buf = new StringBuffer();
-        buf.append(sql.substring(0, index)).append(" TOP(").append(start).append(",").append(end - start).append(") ").append(sql.substring(index + 1, sql.length()));
-        sql = buf.toString();
-
         NSDictionary hints = new NSDictionary(sql, "EOCustomQueryExpressionHintKey");
         spec.setHints(hints);
         
@@ -66,67 +71,84 @@ public class ERXBatchingDisplayGroup extends WODisplayGroup {
     }
 
     /**
-    * Overridden in order to use a custom method which determines the number of Objects / rows
+      * Overridden in order to use a custom method which determines the number of Objects / rows
      * for the existing EODatabaseDataSource.
      *
      * @return the number of rows from the EODatabaseDataSource
      */
     public int batchCount() {
-        if(numberOfObjectsPerBatch() == 0) {
-            return 0;
+        if(isBatching()) {
+            if(numberOfObjectsPerBatch() == 0) {
+                return 0;
+            }
+            if(rowCount() == 0) {
+                return 1;
+            } else {
+                return (rowCount() - 1) / numberOfObjectsPerBatch() + 1;
+            }
         }
-        if(rowCount() == 0) {
-            return 1;
-        } else {
-            return (rowCount() - 1) / numberOfObjectsPerBatch() + 1;
-        }
+        return super.batchCount();
     }
-
+    
     /**
-    * Returns the number of rows from the {@link EODatabaseDataSource}.
+     * Returns the number of rows from the {@link EODatabaseDataSource}.
      *
      * @return the number of rows from the {@link EODatabaseDataSource}
      */
     public int rowCount() {
-        EOFetchSpecification spec = databaseDataSource().fetchSpecificationForFetch();
-        EOEditingContext ec = databaseDataSource().editingContext();
-        EOModel model = ERXEOAccessUtilities.modelForFetchSpecificationAndEditingContext(spec, ec);
-        String sql = ERXEOAccessUtilities.sqlForFetchSpecificationAndEditingContext(spec, ec);
-        int index = sql.indexOf("from");
-        if (index == -1) {
-            index = sql.indexOf("FROM");
-        }
-        sql = "select count(*) " + sql.substring(index, sql.length());
-        NSArray result = EOUtilities.rawRowsForSQL(ec, model.name(), sql);
+        if(_rowCount == -1) {
+            EOFetchSpecification spec = databaseDataSource().fetchSpecificationForFetch();
+            EOEditingContext ec = databaseDataSource().editingContext();
+            EOModel model = ERXEOAccessUtilities.modelForFetchSpecificationAndEditingContext(spec, ec);
+            String sql = ERXEOAccessUtilities.sqlForFetchSpecificationAndEditingContext(spec, ec);
+            int index = sql.indexOf("from");
+            if (index == -1) {
+                index = sql.indexOf("FROM");
+            }
+            sql = "select count(*) " + sql.substring(index, sql.length());
+            NSArray result = EOUtilities.rawRowsForSQL(ec, model.name(), sql);
 
-        if (result.count() > 0) {
-            NSDictionary dict = (NSDictionary)result.objectAtIndex(0);
-            NSArray values = dict.allValues();
-            if (values.count() > 0) {
-                Object value = values.objectAtIndex(0);
-                if (value instanceof Number) {
-                    return ((Number)value).intValue();
-                } else {
-                    try {
-                        int c = Integer.parseInt(value.toString());
-                        setObjectArray(new ERFakeNSArray(c));
-                        return c;
-                    } catch (NumberFormatException e) {
-                        throw new IllegalStateException("sql "+sql+" returned a wrong result, could not convert "+value+" into an int!");
+            if (result.count() > 0) {
+                NSDictionary dict = (NSDictionary)result.objectAtIndex(0);
+                NSArray values = dict.allValues();
+                if (values.count() > 0) {
+                    Object value = values.objectAtIndex(0);
+                    if (value instanceof Number) {
+                        return ((Number)value).intValue();
+                    } else {
+                        try {
+                            int c = Integer.parseInt(value.toString());
+                            setObjectArray(new FakeArray(c));
+                            _rowCount = c;
+                        } catch (NumberFormatException e) {
+                            throw new IllegalStateException("sql "+sql+" returned a wrong result, could not convert "+value+" into an int!");
+                        }
                     }
+                } else {
+                    throw new IllegalStateException("sql "+sql+" returned no result!");
                 }
             } else {
                 throw new IllegalStateException("sql "+sql+" returned no result!");
             }
-        } else {
-            throw new IllegalStateException("sql "+sql+" returned no result!");
         }
+        return _rowCount;
     }
     
     public EODatabaseDataSource databaseDataSource() {
         return (EODatabaseDataSource)dataSource();
     }
 
+    public void redisplay() {
+        _rowCount = -1;
+        _displayedObjects = null;
+        super.redisplay();
+    }
+    
+    public void setCurrentBatchIndex(int index) {
+        _displayedObjects = null;
+        super.setCurrentBatchIndex(index);
+    }
+    
     /**
     * Overridden method in order to fetch -only- the rows that are needed. This is
      * different to the editors methods because a {@link WODisplayGroup} would always fetch
@@ -135,30 +157,34 @@ public class ERXBatchingDisplayGroup extends WODisplayGroup {
      * @return the objects that should be diplayed.
      */
     public NSArray displayedObjects() {
-        if (!isBatching()) return super.displayedObjects();
+        if (!isBatching()) {
+            return super.displayedObjects();
+        } else {
+            if(_displayedObjects == null) {
+                //check the start and end based on currentBatchIndex and numberOfObjectsPerBatch()
+                int count = rowCount();
+                int start = (currentBatchIndex()-1) * numberOfObjectsPerBatch();
+                int end = start + numberOfObjectsPerBatch();
 
-        //check the start and end based on currentBatchIndex and numberOfObjectsPerBatch()
-        int count = rowCount();
-        int start = currentBatchIndex() * numberOfObjectsPerBatch();
-        int end = start + numberOfObjectsPerBatch();
+                if (numberOfObjectsPerBatch() == 0) {
+                    start = 0;
+                    end = rowCount();
+                }
 
-        if (numberOfObjectsPerBatch() == 0) {
-            start = 0;
-            end = rowCount();
+                _displayedObjects = objectsInRange(start, end).mutableClone();
+            }
         }
-
-        NSMutableArray objects = objectsInRange(start, end).mutableClone();
-
-        return objects;
+        return _displayedObjects;
     }
-
-    public static class ERFakeNSArray extends NSArray {
+    
+    /** we just fake that we are an array. */
+    class FakeArray extends NSArray {
         int count = 0;
 
-        public ERFakeNSArray(int count) {
+        public FakeArray(int count) {
             this.count = count;
         }
-        private ERFakeNSArray(){}
+        private FakeArray(){}
 
         public int count() {
             return count;
