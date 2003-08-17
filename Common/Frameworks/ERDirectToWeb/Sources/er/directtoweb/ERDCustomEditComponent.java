b40961d1264c51a54c61bc74a97f8313460b5388
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-08-17 21:44:30 +0000

    I removed the need for a wrapping D2WCustomComponent a while ago so we can use ERDXXXCustomComponents in the D2WAssistant.
    However, with some components (ERDEditDateJavascript for example), we will need to pull the name of the key from the "d2wContext.propertyKey" instead of "key" because the component is synching, but does not have a "key" binding set.
    
    I hope this is OK - with my stuff it is...but I'd prefer to have more comments on this.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDCustomEditComponent.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDCustomEditComponent.java
@@ -106,10 +106,14 @@ public abstract class ERDCustomEditComponent extends WOComponent {
     }
 
     public String key() {
-        if (key==null && !synchronizesVariablesWithBindings())
-            key=(String)super.valueForBinding("key");
-        if (key==null && !synchronizesVariablesWithBindings() && d2wContext() != null)
+        if(!synchronizesVariablesWithBindings()) {
+            if (key==null) {
+                key=(String)super.valueForBinding("key");
+            }
+        }
+        if (key==null && d2wContext() != null) {
             key=(String)d2wContext().valueForKey("propertyKey");
+        }
         return key;
     }
 
