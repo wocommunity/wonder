e8fff26189be4306d63670c80a06f9d1433bb06c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-08-17 20:52:04 +0000

    refactored ERD2WQuery to descend from ERD2WPage instead of D2WQuery
    git-svn-id: file:///Users/q/working/wondersvn/trunk@2078 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryPage.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryPage.java
@@ -8,8 +8,10 @@ package er.directtoweb;
 
 import com.webobjects.foundation.*;
 import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
 import com.webobjects.appserver.*;
 import com.webobjects.directtoweb.*;
+import com.webobjects.directtoweb.generation.*;
 import er.extensions.*;
 
 /**
@@ -17,12 +19,30 @@ import er.extensions.*;
  * 
  */
 
-public class ERD2WQueryPage extends D2WQueryPage implements ERXComponentActionRedirector.Restorable  {
+public class ERD2WQueryPage extends ERD2WPage implements QueryPageInterface, DTWGeneration, ERXComponentActionRedirector.Restorable  {
+
+    public WODisplayGroup displayGroup = new WODisplayGroup();
 
     public ERD2WQueryPage(WOContext context) { super(context); }
 
     public String urlForCurrentState() {
         return context().directActionURLForActionNamed(d2wContext().dynamicPage(), null);
+    }
+
+    public boolean isDeep() {
+        return ERXValueUtilities.booleanValue(d2wContext().valueForKey("isDeep"));
+    }
+
+    public boolean usesDistinct() {
+        return ERXValueUtilities.booleanValue(d2wContext().valueForKey("usesDistinct"));
+    }
+
+    public boolean refreshRefetchedObjects() {
+        return ERXValueUtilities.booleanValue(d2wContext().valueForKey("refreshRefetchedObjects"));
+    }
+
+    public int fetchLimit() {
+        return ERXValueUtilities.intValueWithDefault(d2wContext().valueForKey("fetchLimit"), 0);
     }
 
     // debug helpers
@@ -40,7 +60,7 @@ public class ERD2WQueryPage extends D2WQueryPage implements ERXComponentActionRe
     // add the ability to AND the existing qualifier from the DG
     public EOQualifier qualifier() {
         EOQualifier q=displayGroup.qualifier();
-        EOQualifier q2=super.qualifier();
+        EOQualifier q2=displayGroup.qualifierFromQueryValues();
         return q==null ? q2 : (q2==null ? q : new EOAndQualifier(new NSArray(new Object[]{q,q2})));
     }
 
@@ -51,21 +71,27 @@ public class ERD2WQueryPage extends D2WQueryPage implements ERXComponentActionRe
     public boolean showResults = false;
 
     public WOComponent queryAction() {
+        WOComponent nextPage = null;
         if(nextPageDelegate() == null) {
             if(ERXValueUtilities.booleanValue(d2wContext().valueForKey("showListInSamePage"))){
                 showResults = true;
-                return null;
-            }else{
+            } else {
                 String listConfigurationName=(String)d2wContext().valueForKey("listConfigurationName");
+                ListPageInterface listpageinterface = null;
                 if(listConfigurationName!=null){
-                    ListPageInterface listpageinterface = (ListPageInterface)D2W.factory().pageForConfigurationNamed(listConfigurationName, this.session());
-                    listpageinterface.setDataSource(queryDataSource());
-                    listpageinterface.setNextPage(this.context().page());
-                    return (WOComponent) listpageinterface;
+                    listpageinterface = (ListPageInterface)D2W.factory().pageForConfigurationNamed(listConfigurationName, session());
+                } else {
+                    listpageinterface = D2W.factory().listPageForEntityNamed(entity().name(), session());
                 }
+                listpageinterface.setDataSource(queryDataSource());
+                listpageinterface.setNextPage(context().page());
+                nextPage = (WOComponent) listpageinterface;
             }
+        } else {
+            NextPageDelegate nextpagedelegate = this.nextPageDelegate();
+            nextPage = nextpagedelegate.nextPage(this);
         }
-        return super.queryAction();
+        return nextPage;
     }
 
     // returning a null query data source if cancel was clicked
@@ -81,16 +107,46 @@ public class ERD2WQueryPage extends D2WQueryPage implements ERXComponentActionRe
         return result;
     }
 
-    public EODataSource queryDataSource() {
-        return !_wasCancelled ? super.queryDataSource() : null;
-    }
-
     public WOComponent returnPage;
     public WOComponent returnAction(){
         return returnPage;
     }
 
-    public boolean showCancel(){
+    public boolean showCancel() {
         return returnPage != null;
     }
+
+    public EODataSource queryDataSource() {
+        if(_wasCancelled) {
+            return null;
+        }
+        EODataSource ds = dataSource();
+        if (ds == null || !(ds instanceof EODatabaseDataSource)) {
+            ds = new EODatabaseDataSource(session().defaultEditingContext(), entity().name());
+            setDataSource(ds);
+        }
+        EOFetchSpecification fs = ((EODatabaseDataSource)ds).fetchSpecification();
+        fs.setQualifier(qualifier());
+        fs.setIsDeep(isDeep());
+        fs.setUsesDistinct(usesDistinct());
+        fs.setRefreshesRefetchedObjects(refreshRefetchedObjects());
+        int limit = fetchLimit();
+        if (limit != 0)
+            fs.setFetchLimit(limit);
+        return ds;
+    }
+
+    public String fetchSpecOptions() {
+        String fetchSpecOptions
+        = ("_queryDataSource.fetchSpecification().setIsDeep(" + isDeep()
+           + ");\n\t_queryDataSource.fetchSpecification().setUsesDistinct("
+           + usesDistinct()
+           + ");\n\t_queryDataSource.fetchSpecification().setRefreshesRefetchedObjects("
+           + refreshRefetchedObjects() + ");\n");
+        if (fetchLimit() != 0)
+            fetchSpecOptions += ("\t_queryDataSource.fetchSpecification().setFetchLimit("
+                       + fetchLimit() + ");\n");
+        return fetchSpecOptions;
+    }
+
 }
