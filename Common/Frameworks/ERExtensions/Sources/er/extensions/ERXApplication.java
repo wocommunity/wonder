c58c3a6d1cca7f619c0e9dacfbae6578bd91a28a
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-08-19 18:43:17 +0000

    Handling InvocationTargetExceptions just as we do NSForwardExceptions, also now exiting on any java.lang.Error, not just OutOfMemoryErrors.

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXApplication.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXApplication.java
@@ -10,9 +10,9 @@ import com.webobjects.foundation.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.eoaccess.*;
 import com.webobjects.appserver.*;
-//import com.webobjects.appserver._private.ERXSubmitButton;
+
+import java.lang.reflect.*;
 import java.util.*;
-import org.apache.log4j.*;
 
 /**
  *  ERXApplication is the abstract superclass of WebObjects applications
@@ -31,9 +31,6 @@ public abstract class ERXApplication extends WOApplication {
     /** request logging support */
     public static final ERXLogger requestHandlingLog = ERXLogger.getERXLogger("er.extensions.ERXApplication.RequestHandling");
     
-    // FIXME: Should correct all references to WORequestHandler.DidHandleRequestNotification and then delete this ivar.
-    public static final String WORequestHandlerDidHandleRequestNotification = WORequestHandler.DidHandleRequestNotification;
-
     private static boolean _wasERXApplicationMainInvoked = false;
 
     /** 
@@ -427,14 +424,23 @@ public abstract class ERXApplication extends WOApplication {
      * @param exception to check if it is a fatal exception.
      */
     public void handlePotentiallyFatalException(Exception exception) {
-        if( exception instanceof NSForwardException ) {
-            Throwable t = ((NSForwardException) exception).originalException();
-            if( t instanceof OutOfMemoryError ) {
+        Throwable throwable = null;
+        if (exception instanceof InvocationTargetException) {
+            throwable = ((InvocationTargetException)exception).getTargetException();
+        } else if (exception instanceof NSForwardException ) {
+            throwable = ((NSForwardException)exception).originalException();
+        }
+        if (throwable instanceof Error) {
+            if (throwable instanceof OutOfMemoryError) {
                 // We first log just in case the log4j call puts us in a bad state.
                 NSLog.err.appendln("Ran out of memory, killing this instance");
                 log.error("Ran out of memory, killing this instance");
-                Runtime.getRuntime().exit( 1 );
+            } else {
+                // We first log just in case the log4j call puts us in a bad state.
+                NSLog.err.appendln("java.lang.Error \"" + throwable.getClass().getName() + " occurried. Can't recover killing this instance.");
+                log.error("java.lang.Error \"" + throwable.getClass().getName() + " occurried. Can't recover killing this instance.");
             }
+            Runtime.getRuntime().exit( 1 );
         }
     }
 
