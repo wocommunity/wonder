c73167276da647578f0606065b9ff7d25d6b44a6
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-08-31 20:38:43 +0000

    - changed inheritance, now has all the xxxValueForBinding methods - moved debugging, validation and binding resolution from ERDCustomEditComponent to here - documentation

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDCustomComponent.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDCustomComponent.java
@@ -14,56 +14,79 @@ import com.webobjects.directtoweb.*;
 import er.extensions.*;
 
 /**
- * Simpler CustomEditComponent. Work in progress.<br />
- * 
+ * Base class of many custom components.<br />
+ * Has alot of nifty features including resolving bindings against the rule system and inherits all the value pulling methods from {@see ERXNonSychronizingComponent}.
+ * Subclasses should be able to run standalone without a D2W context. This is achieved by pulling values first from the bindings, then from the d2wContext and finally from an "extraBindings" binding.
  */
 
-public abstract class ERDCustomComponent extends WOComponent {
+public abstract class ERDCustomComponent extends ERXNonSychronizingComponent implements ERXExceptionHolder {
 
-    public ERDCustomComponent(WOContext context) {super(context);}
+    public static interface Keys {
+        public static final String key = "key";
+        public static final String localContext = "localContext";
+        public static final String d2wContext = "d2wContext";
+        public static final String extraBindings = "extraBindings";
+        public static final String propertyKey = "propertyKey";
+    }
+    
+    /** logging support */
+    public final static ERXLogger log = ERXLogger.getERXLogger(ERDCustomEditComponent.class);
+    
+    /** Designated constructor */
+    public ERDCustomComponent(WOContext context) {
+        super(context);
+    }
 
-    //////////////////////////////////////// Static Methods //////////////////////////////////////////////////////////////
+    /** Holds the {@see D2WContext}. */
+    private D2WContext d2wContext;
+
+    /** Holds the property key. */
+    private String key;
+
+    /** Holds the extra bindings. */
+    protected Object extraBindings;
+
+    //CHECKME ak: who needs this?
     protected static Integer TRUE = ERXConstant.OneInteger;
     protected static Integer FALSE = ERXConstant.ZeroInteger;
 
-    //////////////////////////////////////// Instance Methods ////////////////////////////////////////////////////////////
-    protected EOEnterpriseObject _object;
-    protected EOEditingContext _editingContext;
-    protected String _key;
-
-    public String key() {
-        return (String)(_key == null && !synchronizesVariablesWithBindings() ? super.valueForBinding("key") : _key);
+    /** Sets the {@see D2WContext}. Applies when used inside a D2WCustomComponent.*/
+    public void setLocalContext(D2WContext value) {
+        setD2wContext(value);
     }
 
-    public EOEnterpriseObject object() {
-        return (EOEnterpriseObject)(_object == null && !synchronizesVariablesWithBindings() ?
-                                    super.valueForBinding("object") : _object);
+    /** Sets the {@see D2WContext}. Applies when used inside a property key repetition.*/
+    public void setD2wContext(D2WContext value) {
+        d2wContext = value;
     }
 
-    public void setObject(EOEnterpriseObject value) {
-        _object = value;
-        if (_object!=null) // making sure the editing context stays alive
-            _editingContext = _object.editingContext();
+    /** The active {@see D2WContext}. Simply calls to {@see d2wContext()}*/
+    public D2WContext localContext() {
+        return d2wContext();
     }
 
-    public Object objectKeyPathValue() {
-        return key() != null && object() != null ? object().valueForKeyPath(key()) : null;
+    /** The active {@see D2WContext}.*/
+    public D2WContext d2wContext() {
+        return d2wContextFromBindings();
     }
 
-
-    public boolean hasBinding(String binding) {
-        // FIXME:  Turn this check off in production
-        if (synchronizesVariablesWithBindings()) {
-            throw new RuntimeException("HasBinding being used in a subclass of CustomEditComponent that synchronizesVariablesWithBindings == true");
+    /** Returns the active d2wContext. If the value was not set via KVC, tries to get the value from the bindings if the component is non-syncing */
+    protected D2WContext d2wContextFromBindings() {
+        if (d2wContext == null && !synchronizesVariablesWithBindings()) {
+            d2wContext = (D2WContext)super.valueForBinding(Keys.localContext);
+            if(d2wContext == null) {
+                d2wContext = (D2WContext)super.valueForBinding(Keys.d2wContext);
+            }
         }
-        return (valueForBinding(binding) != null);
+        return d2wContext;
     }
-
-    // Validation Support
+    
+    /** Validation Support. Passes errors to the parent. */
     public void validationFailedWithException (Throwable e, Object value, String keyPath) {
         parent().validationFailedWithException(e,value,keyPath);
     }
 
+    /** Implementation of the {@see ERXExceptionHolder} interface. Clears exceptions in the parent if possible.*/
     public void clearValidationFailed() {
         // Since this component can be used stand alone, we might not necessarily
         // have an exception holder as our parent --> testing
@@ -71,34 +94,159 @@ public abstract class ERDCustomComponent extends WOComponent {
             ((ERXExceptionHolder)parent()).clearValidationFailed();
     }
 
+    /** @deprecated use booleanValueForBinding() instead */
     public boolean booleanForBinding(String binding) {
-        boolean bool = false;
-        Object booleanBinding = valueForBinding(binding);
-        if (booleanBinding == null) {
-            // Need this because binding NO to a binding results in the component having a binding but the value being null.  Go figure.
-            bool = !super.hasBinding(binding);
-        } else if (booleanBinding instanceof Integer) {
-            bool = ((Integer)booleanBinding).intValue() == 1;
-        } else if (booleanBinding instanceof String && ((String)booleanBinding).equals("YES") ||
-                   ((String)booleanBinding).equals("1")) {
-            bool = true;
-        }
-        return bool;
+        return booleanValueForBinding(binding);
     }
 
+    // CHECKME ak who needs this?
     public Integer integerBooleanForBinding(String binding) {
-        return booleanForBinding(binding) ? ERDCustomComponent.TRUE : ERDCustomComponent.FALSE;
+        return booleanValueForBinding(binding) ? ERDCustomComponent.TRUE : ERDCustomComponent.FALSE;
     }
 
-    public Object valueForBinding(String binding) {
-        Object value=null;
-        if (super.hasBinding(binding)) {
-            value = super.valueForBinding(binding);
-        } else if (parent() instanceof ERDCustomEditComponent || parent() instanceof ERD2WCustomComponentWithArgs) {
+    /** Checks if the binding can be pulled. If the component is synching, throws an Exception. Otherwise checks the superclass and if the value for the binding is not null.*/
+    public boolean hasBinding(String binding) {
+        // FIXME:  Turn this check off in production
+        if (synchronizesVariablesWithBindings()) {
+            throw new IllegalStateException("HasBinding being used in an object of class " + getClass().getName() + " that synchronizesVariablesWithBindings == true");
+        }
+        return (super.hasBinding(binding) || valueForBinding(binding) != null);
+    }
+
+    /** Utility to dump some debug info about this component and its parent */
+    protected void logDebugInfo() {
+        if (log.isDebugEnabled()) {
+            log.debug("***** ERDCustomComponent: this: " + this.getClass().getName());
+            log.debug("***** ERDCustomComponent: parent(): + (" + ((parent() == null) ? "null" : parent().getClass().getName()) + ")");
+            log.debug("                      " + parent());
+            log.debug("***** ERDCustomComponent: parent() instanceof ERDCustomComponent == " + (parent() instanceof ERDCustomComponent));
+            log.debug("***** ERDCustomComponent: parent() instanceof D2WCustomComponentWithArgs == " + (parent() instanceof ERD2WCustomComponentWithArgs));
+            log.debug("***** ERDCustomComponent: parent() instanceof D2WStatelessCustomComponentWithArgs == " + (parent() instanceof ERD2WStatelessCustomComponentWithArgs));
+            log.debug("***** ERDCustomComponent: parent() instanceof D2WCustomQueryComponentWithArgs == " + (parent() instanceof ERDCustomQueryComponentWithArgs));
+        }
+    }
+
+    /** Utility to pull the value from the components parent, if the parent is a D2W wrapper component. */
+    protected Object parentValueForBinding(String binding) {
+        WOComponent parent = parent();
+        if (parent instanceof ERDCustomComponent ||
+            parent instanceof ERD2WCustomComponentWithArgs ||
+            parent instanceof ERD2WStatelessCustomComponentWithArgs) {
+            log.debug("inside the parent instanceof branch");
             // this will eventually bubble up to a D2WCustomComponentWithArgs, where it will (depending on the actual binding)
             // go to the d2wContext
-            value = parent().valueForBinding(binding);
+            return parent.valueForBinding(binding);
+        }
+        return null;
+    }
+
+    /** Utility to pull the value from the components actual bindings. */
+    protected Object originalValueForBinding(String binding) {
+        return super.valueForBinding(binding);
+    }
+
+    /** Utility to pull the value from the {@see D2WContext}. */
+    protected Object d2wContextValueForBinding(String binding) {
+        return d2wContextFromBindings().valueForKey(binding);
+    }
+
+    /** Utility to pull the value from the extra bindings if supplied. */
+    protected Object extraBindingsValueForBinding(String binding) {
+        if(extraBindings() instanceof NSDictionary)
+            return ((NSDictionary)extraBindings()).objectForKey(binding);
+        return null;
+    }
+
+    /**
+     * Fetches an object from the bindings.
+     * Tries the actual supplied bindings, the supplied d2wContext, the parent and finally the extra bindings dictionary.
+     */
+    public Object valueForBinding(String binding) {
+        Object value=null;
+        logDebugInfo();
+        if (super.hasBinding(binding)) {
+            log.debug("super.hasBinding(binding) == true");
+            value = originalValueForBinding(binding);
+        } else if(d2wContextFromBindings() != null) {
+            log.debug("has d2wContext == true");
+            value = d2wContextValueForBinding(binding);
+        } else {
+            value = parentValueForBinding(binding);
+        }
+        if (value == null && binding != null && extraBindings() != null) {
+            log.debug("inside the extraBindings branch");
+            value = extraBindingsValueForBinding(binding);
+        }
+        if (log.isDebugEnabled()) {
+            if (value != null)
+                log.debug("returning " + value.getClass().getName() + ": " + value);
+            else
+                log.debug("returning value: null");
         }
         return value;
+    }
+
+    /** Used by stateless subclasses. */
+    public void reset() {
+        super.reset();
+        extraBindings = null;
+        key = null;
+        d2wContext = null;
+    }
+
+    /** Sets the extra bindings. */
+    public void setExtraBindings(Object value) { extraBindings = value; }
+
+    /** Extra bindings supplied to the component. If this is a dictionary, it will be used for additional bindings.*/
+    public Object extraBindings() {
+        if (extraBindings == null && !synchronizesVariablesWithBindings())
+            extraBindings = super.valueForBinding(Keys.extraBindings);
+        return extraBindings;
+    }
+
+    /** Sets the property key. */
+    public void setKey(String newKey) { key=newKey; }
+
+    /** The active property key. */
+    public String key() {
+        if(!synchronizesVariablesWithBindings()) {
+            if (key==null) {
+                key=(String)super.valueForBinding(Keys.key);
+            }
+        }
+        if (key==null && d2wContext() != null) {
+            key=(String)d2wContext().valueForKey(Keys.propertyKey);
+        }
+        return key;
+    }
+
+    /** Overridden from superclass to turn on component synching, which is the default. */
+    public boolean synchronizesVariablesWithBindings() { return true; }
+
+    /** Is D2W debugging enabled. */
+    public boolean d2wDebuggingEnabled() {
+        return ERDirectToWeb.d2wDebuggingEnabled(session());
+    }
+
+    /** Should the component name be shown. */
+    public boolean d2wComponentNameDebuggingEnabled() {
+        return ERDirectToWeb.d2wComponentNameDebuggingEnabled(session());
+    }
+
+    /** Should the property keys be shown. */
+    public boolean d2wPropertyKeyDebuggingEnabled() {
+        return ERDirectToWeb.d2wPropertyKeyDebuggingEnabled(session());
+    }
+
+    public void appendToResponse(WOResponse r, WOContext c) {
+        try {
+            // in the case where we are non-synchronizing but not stateless, make sure we pull again
+            if (!synchronizesVariablesWithBindings() && !isStateless()) {
+                reset();
+            }
+            super.appendToResponse(r,c);
+        } catch(Exception ex) {
+            ERDirectToWeb.reportException(ex, d2wContext());
+        }
     }
 }
