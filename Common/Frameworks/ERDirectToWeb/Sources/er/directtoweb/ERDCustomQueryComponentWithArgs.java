d33c0575317d85eeecb5a603fcbbb52d76bae841
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-08-31 20:40:06 +0000

    - changed inheritance, now descends from ERDCustomComponent - moved debugging, validation and binding resolution to there, removed duplication
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2154 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDCustomQueryComponentWithArgs.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDCustomQueryComponentWithArgs.java
@@ -12,16 +12,12 @@ import com.webobjects.directtoweb.*;
 import er.extensions.ERXExceptionHolder;
 import er.extensions.ERXLogger;
 
-// Useful component and important bug fix
-// Fixes validation failures being propogated
-// Adds valueForBinding that resolves in the d2wContext.
-
 /**
  * Correctly handles validation exceptions, plus a bunch of other stuff.<br />
  * 
  */
 
-public class ERDCustomQueryComponentWithArgs extends ERDCustomQueryComponent implements ERXExceptionHolder {
+public class ERDCustomQueryComponentWithArgs extends ERDCustomQueryComponent {
 
     public ERDCustomQueryComponentWithArgs(WOContext context) {
         super(context);
@@ -29,47 +25,4 @@ public class ERDCustomQueryComponentWithArgs extends ERDCustomQueryComponent imp
     
     /** logging support */
     public final static ERXLogger log = ERXLogger.getERXLogger(ERDCustomQueryComponentWithArgs.class);
-
-    /* FIXME -- super class has Dictionary as return type
-    private String _extraBindings;
-    public String extraBindings() {
-        _extraBindings=d2wContext().valueForKey("extraBindings");
-        if (_extraBindings!=null) {
-            if (_extraBindings instanceof String && ((String)_extraBindings).length()>0) {
-                // assume it's a dict and parse it
-                _extraBindings=NSPropertyListSerialization.propertyListFromString((String)_extraBindings);
-            }
-        }
-        return _extraBindings;
-    }
-    */
-
-    // Done this way so that subClasses can always get the original valueForBinding.
-    public Object originalValueForBinding(String binding) { return super.valueForBinding(binding); }
-
-    public Object valueForBinding(String binding) {
-        return hasBinding(binding) ? originalValueForBinding(binding) : nonCachingContext().valueForKey(binding);
-    }
-
-    public void validationFailedWithException (Throwable e, Object value, String keyPath) {
-        parent().validationFailedWithException(e,value,keyPath);
-    }
-
-    public void clearValidationFailed(){
-        if (parent() instanceof ERXExceptionHolder)
-            ((ERXExceptionHolder)parent()).clearValidationFailed();
-    }
-
-    public boolean d2wDebuggingEnabled() {
-        return ERDirectToWeb.d2wDebuggingEnabled(session());
-    }
-    public boolean d2wComponentNameDebuggingEnabled() {
-        return ERDirectToWeb.d2wComponentNameDebuggingEnabled(session());
-    }
-    public boolean d2wPropertyKeyDebuggingEnabled() {
-        return ERDirectToWeb.d2wPropertyKeyDebuggingEnabled(session());
-    }
-
-    // Needed to fix a caching bug.
-    public D2WContext nonCachingContext() { return (D2WContext)super.valueForBinding("localContext"); }
 }
