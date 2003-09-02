dab91bb04df765594bddf28d276ddaef1391c382
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-09-02 02:34:57 +0000

    - accounted for changed inheritance - should be deprecated
    git-svn-id: file:///Users/q/working/wondersvn/trunk@2178 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDTrashcan.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDTrashcan.java
@@ -14,7 +14,8 @@ import com.webobjects.directtoweb.*;
 import er.extensions.*;
 
 /**
- * A better trashcan.  Used the gaurded object interface to determine if the object can be deleted.<br />
+ * A better trashcan.  As this component is named inconsistently with the
+ * other buttons that operate on lists, {@see ERDDeleteButton} should be used instead.<br />
  * 
  * @binding object
  * @binding dataSource
@@ -23,34 +24,7 @@ import er.extensions.*;
  * @binding noTrashcanExplanation
  */
 
-public class ERDTrashcan extends ERDCustomEditComponent {
+public class ERDTrashcan extends ERDDeleteButton {
 
     public ERDTrashcan(WOContext context) { super(context); }
-    
-    public boolean isStateless() { return true; }
-    public boolean synchronizesVariablesWithBindings() { return false; }
-
-    public boolean canDelete() {
-        return object() != null && object() instanceof ERXGuardedObjectInterface ? ((ERXGuardedObjectInterface)object()).canDelete() : true;
-    }
-
-    public EODataSource dataSource() { return (EODataSource)valueForBinding("dataSource"); }
-
-    public WOComponent deleteObjectAction() {
-        ConfirmPageInterface nextPage = (ConfirmPageInterface)D2W.factory().pageForConfigurationNamed((String)valueForBinding("confirmDeleteConfigurationName"), session());
-        nextPage.setConfirmDelegate(new ERDDeletionDelegate(object(), dataSource(), context().page()));
-        nextPage.setCancelDelegate(new ERDPageDelegate(context().page()));
-        String message = ERXLocalizer.localizerForSession(session()).localizedTemplateStringForKeyWithObjectOtherObject("ERDTrashcan.confirmDeletionMessage", d2wContext(), object());
-        nextPage.setMessage(message);
-        ((D2WPage)nextPage).setObject(object());
-        return (WOComponent) nextPage;
-    }
-
-    public String onMouseOverTrashcan() {
-        return hasBinding("trashcanExplanation") ? "self.status='" + valueForBinding("trashcanExplanation") + "'; return true" : "";
-    }
-
-    public String onMouseOverNoTrashcan() {
-        return hasBinding("noTrashcanExplanation") ? "self.status='" + valueForBinding("noTrashcanExplanation") + "'; return true" : "";
-    }
 }
