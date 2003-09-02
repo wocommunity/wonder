4dd0efdf49ffe66d1e03fade4c6acd02edffbd67
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-09-02 02:45:33 +0000

    - changed inheritance
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2184 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUQueryAllPage.java
+++ Common/Frameworks/ERNeutralLook/Sources/er/neutral/ERNEUQueryAllPage.java
@@ -8,14 +8,12 @@ package er.neutral;
 
 import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
-import com.webobjects.eocontrol.*;
-import com.webobjects.eoaccess.*;
 import com.webobjects.directtoweb.*;
+import er.directtoweb.*;
 
-public class ERNEUQueryAllPage extends D2WQueryAllEntitiesPage {
+public class ERNEUQueryAllPage extends ERD2WQueryEntitiesPage {
 
     public ERNEUQueryAllPage(WOContext context) {
         super(context);
     }
-
 }
