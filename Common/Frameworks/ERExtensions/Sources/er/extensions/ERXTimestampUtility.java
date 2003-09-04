498f910a49e44c69f9c257bfc0044c84170eb001
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-09-04 23:37:12 +0000

    - first cut of grouping and date stuff - added lotsa utilities even though this is the wrong place (should follow the no-static-methods pattern)

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXTimestampUtility.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXTimestampUtility.java
@@ -11,7 +11,6 @@ import com.webobjects.appserver.*;
 import com.webobjects.eocontrol.*;
 import java.util.*;
 
-// A simle utility for providing deprecated functionality for NSTimestamps
 /**
  * A simle utility for providing deprecated functionality for NSTimestamps<br />
  * 
@@ -26,18 +25,89 @@ public class ERXTimestampUtility {
         return _calendar;
     }
 
+    public static long offsetForDateInCommonEra(NSTimestamp t, int mode) {
+        _calendar.setTime(t);
+        switch(mode) {
+            case GregorianCalendar.YEAR:
+                return _calendar.get(GregorianCalendar.YEAR);
+            case GregorianCalendar.MONTH:
+                return _calendar.get(GregorianCalendar.YEAR) * 12 + _calendar.get(GregorianCalendar.MONTH);
+            case GregorianCalendar.WEEK_OF_YEAR:
+                return _calendar.get(GregorianCalendar.YEAR) * 52 + _calendar.get(GregorianCalendar.WEEK_OF_YEAR);
+            case GregorianCalendar.DAY_OF_MONTH:
+            case GregorianCalendar.DAY_OF_YEAR:
+                return _calendar.get(GregorianCalendar.YEAR) * 365 + _calendar.get(GregorianCalendar.DAY_OF_YEAR);
+            case GregorianCalendar.HOUR_OF_DAY:
+            case GregorianCalendar.HOUR:
+                return (_calendar.get(GregorianCalendar.YEAR) * 365 + _calendar.get(GregorianCalendar.DAY_OF_YEAR)) * 24 + _calendar.get(GregorianCalendar.HOUR_OF_DAY);
+            default:
+                return 0;
+        }
+    }
+
+    public static long differenceByDay(NSTimestamp t1, NSTimestamp t2) {
+        return compareDatesInCommonEra(t1, t2, GregorianCalendar.DAY_OF_YEAR);
+    }
+
+    public static long differenceByWeek(NSTimestamp t1, NSTimestamp t2) {
+        return compareDatesInCommonEra(t1, t2, GregorianCalendar.WEEK_OF_YEAR);
+    }
+
+    public static long differenceByMonth(NSTimestamp t1, NSTimestamp t2) {
+        return compareDatesInCommonEra(t1, t2, GregorianCalendar.MONTH);
+    }
+
+    public static long differenceByYear(NSTimestamp t1, NSTimestamp t2) {
+        return compareDatesInCommonEra(t1, t2, GregorianCalendar.YEAR);
+    }
+
+    public static NSTimestamp firstDateInSameWeek(NSTimestamp value) {
+        return new NSTimestamp(ERXTimestampUtility.yearOfCommonEra(value), ERXTimestampUtility.monthOfYear(value), -ERXTimestampUtility.dayOfWeek(value) + 1, 0, 0, 0, NSTimeZone.defaultTimeZone());
+    }
+
+    public static NSTimestamp firstDateInSameMonth(NSTimestamp value) {
+        return new NSTimestamp(ERXTimestampUtility.yearOfCommonEra(value), ERXTimestampUtility.monthOfYear(value), -ERXTimestampUtility.dayOfMonth(value)+1, 0, 0, 0, NSTimeZone.defaultTimeZone());
+    }
+    public static NSTimestamp firstDateInNextMonth(NSTimestamp value) {
+        return firstDateInSameMonth(value).timestampByAddingGregorianUnits(0, 1, 0, 0, 0, 0);
+    }
+    
+    public static long compareDatesInCommonEra(NSTimestamp t1, NSTimestamp t2, int mode) {
+        return offsetForDateInCommonEra(t2, mode) - offsetForDateInCommonEra(t1, mode);
+    }
+
     public static int dayOfCommonEra(NSTimestamp t) {
         return yearOfCommonEra(t)*365 + dayOfYear(t);
     }
-    
+
+    public static int monthOfCommonEra(NSTimestamp t) {
+        return yearOfCommonEra(t)*12 + monthOfYear(t);
+    }
+
+    public static int weekOfCommonEra(NSTimestamp t) {
+        return yearOfCommonEra(t)*12 + weekOfYear(t);
+    }
+
     public static int dayOfWeek(NSTimestamp t) {
         return calendarForTimestamp(t).get(GregorianCalendar.DAY_OF_WEEK);        
     }
 
+    public static int dayOfMonth(NSTimestamp t) {
+        return calendarForTimestamp(t).get(GregorianCalendar.DAY_OF_MONTH);
+    }
+
+    public static int weekOfYear(NSTimestamp t) {
+        return calendarForTimestamp(t).get(GregorianCalendar.WEEK_OF_YEAR);
+    }    
+
+    public static int weekOfMonth(NSTimestamp t) {
+        return calendarForTimestamp(t).get(GregorianCalendar.WEEK_OF_MONTH);
+    }
+
     public static int dayOfYear(NSTimestamp t) {
         return calendarForTimestamp(t).get(GregorianCalendar.DAY_OF_YEAR);
-    }    
-    
+    }
+
     public static int hourOfDay(NSTimestamp t) {
         return calendarForTimestamp(t).get(GregorianCalendar.HOUR_OF_DAY);
     }
