2fef4b23ed125cc4ed5c79eda02dff83502629d3
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-09-26 01:40:52 +0000

    - putting all the displayNameForFoo in one place
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2341 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDLocalizedAssignment.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDLocalizedAssignment.java
@@ -9,6 +9,7 @@ package er.directtoweb;
 import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
 import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
 import com.webobjects.directtoweb.*;
 import er.extensions.*;
 
@@ -18,7 +19,12 @@ public class ERDLocalizedAssignment extends ERDAssignment implements ERDLocaliza
     static final ERXLogger log = ERXLogger.getERXLogger(ERDLocalizedAssignment.class);
 
     /** holds the dependent keys of the assignment */
-    protected static final NSArray _DEPENDENT_KEYS=new NSArray("propertyKey");
+    protected static final NSArray _pageConfiguration_Keys = new NSArray(new Object[]{"pageConfiguration", "task", "entity.name"});
+    protected static final NSArray _entityName_Keys = new NSArray("entity.name");
+    protected static final NSArray _propertyKey_Keys = new NSArray("propertyKey");
+    protected static final NSArray _sectionKey_Keys = new NSArray("sectionKey");
+    protected static final NSArray _tabKey_Keys = new NSArray("tabKey");
+    protected static final NSArray _destinationEntity_Keys = new NSArray(new Object[]{ "object.entity", "propertyKey"});
 
     /**
      * Static constructor required by the EOKeyValueUnarchiver
@@ -55,28 +61,69 @@ public class ERDLocalizedAssignment extends ERDAssignment implements ERDLocaliza
      * @return array of context keys this assignment depends upon.
      */
     public NSArray dependentKeys(String keyPath) {
-        return _DEPENDENT_KEYS;
+        if(keyPath.equals("displayNameForProperty")) {
+            return _propertyKey_Keys;
+        } else if(keyPath.equals("displayNameForEntity")) {
+            return _entityName_Keys;
+        } else if(keyPath.equals("displayNameForDestinationEntity")) {
+            return _destinationEntity_Keys;
+        } else if(keyPath.equals("displayNameForSectionKey")) {
+            return _sectionKey_Keys;
+        } else if(keyPath.equals("displayNameForTabKey")) {
+            return _tabKey_Keys;
+        } else if(keyPath.equals("displayNameForPageConfiguration")) {
+            return _pageConfiguration_Keys;
+        }
+        return NSArray.EmptyArray;
     }
-    public Object localizedValueForKey(D2WContext c) {
-        return localizedValueForKeyInContext((String)value(), c);
-    }
-    public Object localizedValueForKeyWithDefault(D2WContext c) {
-        return localizedValueForKeyWithDefaultInContext((String)value(), c);
-    }
-    public Object localizedValueForKeyPath(D2WContext c) {
-        return localizerForContext(c).valueForKeyPath((String)value());
+    protected Object localizedValueForDisplayNameOfKeyPath(String keyPath, D2WContext c) {
+        String result = ERXStringUtilities.displayNameForKey((String)c.valueForKeyPath(keyPath));
+        return localizerForContext(c).localizedStringForKeyWithDefault(result);
     }
     public Object displayNameForProperty(D2WContext c) {
-        return localizedValueForKeyWithDefaultInContext((String)value(), c);
+        return localizedValueForDisplayNameOfKeyPath("propertyKey", c);
     }
     public Object displayNameForEntity(D2WContext c) {
-        return localizedValueForKeyWithDefaultInContext((String)value(), c);
+        return localizedValueForDisplayNameOfKeyPath("entity.name", c);
+    }
+    public Object displayNameForDestinationEntity(D2WContext c) {
+        Object result = null;
+        EOEntity destinationEntity = (EOEntity)c.valueForKeyPath("smartRelationship.destinationEntity");
+        if(destinationEntity != null) {
+            EOEntity entity = (EOEntity)c.valueForKey("entity");
+            c.takeValueForKey(destinationEntity, "entity");
+            result = c.valueForKey("displayNameForEntity");
+            c.takeValueForKey(entity, "entity");
+        }
+        return result;
+    }
+    
+    public Object displayNameForPageConfiguration(D2WContext c) {
+        String pageConfiguration = (String)c.valueForKey("pageConfiguration");
+        // do we have task__entityName?
+        if(pageConfiguration.indexOf("__") == 0) {
+            String taskName = (String)c.valueForKey("task");
+            String entityName = (String)c.valueForKeyPath("entity.name");
+            pageConfiguration = taskName.substring(0,1).toUpperCase() + taskName.substring(1) + entityName;
+        }
+        String value = pageConfiguration;
+        if(value != null && value.length() > 0) {
+            value = ERXStringUtilities.displayNameForKey(pageConfiguration);
+        }
+        return localizedValueForKeyWithDefaultInContext(value, c);
+    }
+    public Object displayNameForSectionKey(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("sectionKey", c);
+    }
+    public Object displayNameForTabKey(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("tabKey", c);
     }
     public ERXLocalizer localizerForContext(D2WContext c) {
         return ERXLocalizer.localizerForSession(c.valueForKey("session"));
     }
-    
     public Object fire(D2WContext c) {
-        return localizerForContext(c).localizedValueForKeyWithDefault((String)value());
+        Object result = super.fire(c);
+        log.info(keyForMethodLookup(c) + "-" + result);
+        return result;
     }
 }
