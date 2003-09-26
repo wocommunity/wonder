daed37338beb0fe2973c614eef53995110f25643
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-09-26 01:43:05 +0000

    - putting all the displayNameForFoo in one place - using new displayNameForSectionKey, displayNameForTabKey, changed inheritance to be able to pull from both d2wContext and bindings

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDSectionImage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDSectionImage.java
@@ -6,18 +6,16 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.directtoweb;
 
-import com.webobjects.appserver.WOComponent;
-import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.*;
 
 /**
  * Used to display sections as images instead of text.<br />
- * 
- * @binding d2wContext
  */
 
-public class ERDSectionImage extends WOComponent {
+public class ERDSectionImage extends ERDCustomComponent {
 
     public ERDSectionImage(WOContext context) { super(context); }
 
     public boolean isStateless() { return true; }
+    public boolean synchronizesVariablesWithBindings() { return false; }
 }
