541b9cb8dc0557f41ceaf3b1247db4dafd6a82ec
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-09-27 21:52:33 +0000

    - added new assignments
    git-svn-id: file:///Users/q/working/wondersvn/trunk@2355 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDDefaultDisplayNameAssignment.java
@@ -0,0 +1,152 @@
+package er.directtoweb;
+
+import com.webobjects.directtoweb.*;
+import com.webobjects.foundation.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.directtoweb.*;
+import er.extensions.*;
+
+/**
+ * Beautify the display names for the various keys in D2W a better way.<br />
+ * @author ak
+ */
+
+public class ERDDefaultDisplayNameAssignment extends ERDAssignment implements ERDLocalizableAssignmentInterface {
+
+    /** logging support */
+    static final ERXLogger log = ERXLogger.getERXLogger(ERDDefaultDisplayNameAssignment.class);
+
+    /** holds the dependent keys of the assignment */
+    protected static final NSDictionary keys = ERXDictionaryUtilities.dictionaryWithObjectsAndKeys( new Object [] {
+        new NSArray(new Object[] {"pageConfiguration", "task", "entity.name"}), "displayNameForPageConfiguration",
+        new NSArray(new Object[] {"entity", "object.entityName"}), "displayNameForEntity",
+        new NSArray(new Object[] {"sectionKey"}), "displayNameForSectionKey",
+        new NSArray(new Object[] {"tabKey"}), "displayNameForTabKey",
+        new NSArray(new Object[] {"destinationEntity"}), "displayNameForDestinationEntity",
+        new NSArray(new Object[] {"smartRelationship.destinationEntity"}), "displayNameForDestinationEntity",
+        new NSArray(new Object[] {"editConfigurationName"}), "displayNameForEditConfiguration",
+        new NSArray(new Object[] {"inspectConfigurationName"}), "displayNameForInspectConfiguration",
+        new NSArray(new Object[] {"createConfigurationName"}), "displayNameForCreateConfiguration",
+        new NSArray(new Object[] {"confirmDeleteConfigurationName"}), "displayNameForConfirmDeleteConfiguration"
+    });
+
+    /**
+     * Implementation of the {@link ERDComputingAssignmentInterface}. This array
+     * of keys is used when constructing the
+     * significant keys for the passed in keyPath.
+     * @param keyPath to compute significant keys for.
+     * @return array of context keys this assignment depends upon.
+     */
+    public NSArray dependentKeys(String keyPath) {
+        return (NSArray)keys.valueForKey(keyPath);
+    }
+
+
+    /**
+     * Static constructor required by the EOKeyValueUnarchiver
+     * interface. If this isn't implemented then the default
+     * behavior is to construct the first super class that does
+     * implement this method. Very lame.
+     * @param eokeyvalueunarchiver to be unarchived
+     * @return decoded assignment of this class
+     */
+    public static Object decodeWithKeyValueUnarchiver(EOKeyValueUnarchiver eokeyvalueunarchiver)  {
+        return new ERDDefaultDisplayNameAssignment(eokeyvalueunarchiver);
+    }
+    
+    /** 
+     * Public constructor
+     * @param u key-value unarchiver used when unarchiving
+     *		from rule files. 
+     */    
+    public ERDDefaultDisplayNameAssignment (EOKeyValueUnarchiver u) { super(u); }
+    
+    /** 
+     * Public constructor
+     * @param key context key
+     * @param value of the assignment
+     */
+    public ERDDefaultDisplayNameAssignment (String key, Object value) { super(key,value); }
+
+    /** Helper to get pull the value, pretty-print it and run it through the localizer. */
+    protected Object localizedValueForDisplayNameOfKeyPath(String keyPath, D2WContext c) {
+        String result = ERXStringUtilities.displayNameForKey((String)c.valueForKeyPath(keyPath));
+        if(result != null) {
+            return ERXLocalizer.currentLocalizer().localizedStringForKeyWithDefault(result);
+        }
+        return null;
+    }
+
+    /** @return a beautified, localized display name for the current <code>propertyKey</code> */
+    public Object displayNameForProperty(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("propertyKey", c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>entity.name</code> */
+    public Object displayNameForEntity(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("entity.name", c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>destinationEntity</code> */
+    public Object displayNameForDestinationEntity(D2WContext c) {
+        Object result = null;
+//        if(true) return null;
+        EOEntity destinationEntity = (EOEntity)c.valueForKeyPath("smartRelationship.destinationEntity");
+//        EOEntity destinationEntity = (EOEntity)c.valueForKeyPath("destinationEntity");
+        if(destinationEntity != null) {
+            EOEntity entity = (EOEntity)c.valueForKey("entity");
+            c.takeValueForKey(destinationEntity, "entity");
+            result = c.valueForKey("displayNameForEntity");
+            c.takeValueForKey(entity, "entity");
+        }
+        return result;
+    }
+
+    /** @return a beautified, localized display name for the current <code>sectionKey</code> */
+    public Object displayNameForSectionKey(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("sectionKey", c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>tabKey</code> */
+    public Object displayNameForTabKey(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("tabKey", c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>pageConfiguration</code> */
+    public Object displayNameForPageConfiguration(D2WContext c) {
+        String pageConfiguration = (String)c.valueForKey("pageConfiguration");
+        // do we have task__entityName?
+        if(pageConfiguration.indexOf("__") == 0) {
+            String taskName = (String)c.valueForKey("task");
+            String entityName = (String)c.valueForKeyPath("entity.name");
+            log.info(pageConfiguration + ": task=" + taskName +  ", entity=" + entityName);
+            pageConfiguration = taskName.substring(0,1).toUpperCase() + taskName.substring(1) + entityName;
+        }
+        String value = pageConfiguration;
+        if(value != null && value.length() > 0) {
+            value = ERXStringUtilities.displayNameForKey(pageConfiguration);
+        }
+        return localizedValueForKeyWithDefaultInContext(value, c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>editConfigurationName</code> */
+    public Object displayNameForEditConfiguration(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("editConfigurationName", c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>inspectConfigurationName</code> */
+    public Object displayNameForInspectConfiguration(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("inspectConfigurationName", c);
+    }
+
+    /** @return a beautified, localized display name for the current <code>createConfigurationName</code> */
+    public Object displayNameForCreateConfiguration(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("createConfigurationName", c);
+    }
+    
+    /** @return a beautified, localized display name for the current <code>confirmDeleteConfigurationName</code> */
+    public Object displayNameForConfirmDeleteConfiguration(D2WContext c) {
+        return localizedValueForDisplayNameOfKeyPath("confirmDeleteConfigurationName", c);
+    }
+}
