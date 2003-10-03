61632ea81d263de9ae48846f1e687444baf7024b
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2003-10-03 05:23:30 +0000

    Moving mailer infrastructure to ERCoreBusinessLogic to allow other applications to use it besides ERMailer, also adding automatic compression support for saving on database size in favor of cpu cycles, adding automatic archive support (not finished yet).

---
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/javamail/mailer/ERMailerTimerTask.java
@@ -0,0 +1,29 @@
+//
+// ERMailDaemonTask.java
+// Project ERMailer
+//
+// Created by max on Tue Oct 22 2002
+//
+package er.javamail.mailer;
+
+import java.util.TimerTask;
+
+import er.extensions.ERXLogger;
+
+/**
+ * Timer task used when running the ERMailer
+ * in daemon mode.
+ */
+public class ERMailerTimerTask extends TimerTask {
+
+    /** logging support */
+    public final static ERXLogger log = ERXLogger.getERXLogger(ERMailerTimerTask.class);
+
+    /**
+     * Processes the outgoing mail.
+     */
+    public void run() {
+        if (log.isDebugEnabled()) log.debug("Timer firing to process outgoing mail.");
+        ERMailer.instance().processOutgoingMail();
+    }
+}
