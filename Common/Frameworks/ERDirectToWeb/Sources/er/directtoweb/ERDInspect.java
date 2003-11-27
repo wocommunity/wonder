0a5b1f9804c6326017be954476d6068105b8faae
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2003-11-27 15:15:34 +0000

    ERDInspect now acts like D2WEditToOneFault when showActions is set, displays message when no object is set, editRelationshipConofigurationName is now an assignment, debug helpers added

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDInspect.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDInspect.java
@@ -5,7 +5,9 @@
  * Public Software License version 0.5, a copy of which has been
  * included with this distribution in the LICENSE.NPL file.  */
 package er.directtoweb;
-import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.directtoweb.*;
 
 /**
  * Embedded component that can be used for nesting page configurations, ie ERDInspect can be a customComponentName.<br />
@@ -17,4 +19,16 @@ public class ERDInspect extends ERDCustomEditComponent {
         super(context);
     }
     public boolean synchronizesVariablesWithBindings() { return false; }
+
+    public WOComponent editRelationshipAction() {
+        String editRelationshipConfigurationName = (String)valueForBinding("editRelationshipConfigurationName");
+        EditRelationshipPageInterface epi = (EditRelationshipPageInterface)D2W.factory().pageForConfigurationNamed(editRelationshipConfigurationName, session());
+        epi.setMasterObjectAndRelationshipKey(object(), key());
+        epi.setNextPage(context().page());
+        return (WOComponent)epi;
+    }
+    public WOComponent clearAction() {
+        object().removeObjectFromBothSidesOfRelationshipWithKey((EOEnterpriseObject)object().valueForKey(key()), key());
+        return context().page();
+    }
 }
