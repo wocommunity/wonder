86bc23c5b5e5fda4e049683c91897d2a175731a9
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2004-01-12 10:07:51 +0000

    new method which  works with multiple arguments
    git-svn-id: file:///Users/q/working/wondersvn/trunk@2611 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRuntimeUtilities.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRuntimeUtilities.java
@@ -32,6 +32,79 @@ public class ERXRuntimeUtilities {
         return results;
     }
 
+    /** Excecutes the specified command line commands. If envp is not null the  environment variables are set
+     * before executing the command.
+     *
+     * @param commands the commands to execute like "ls -la" or "cp /tmp/file1 /tmp/file2" or "open /Applications/*.app"
+     * @param envp a <code>String</code> array which represents the environment variables like
+     * <code>String[] envp = new String[]{"PATH=/usr/bin:/bin", "CVS_RSH=ssh"}</code>, can be null
+     * @param dir a <code>File</code> object representing the working directory, can be null
+     *
+     * @return the results from the processes that were executed
+     *
+     * @exception IOException if something went wrong
+     */
+    public final static Result executeCommandLineCommandWithArgumentsWithEnvVarsInWorkingDir(String[] command, String[] envp, File dir) throws IOException {
+
+    	
+    	Runtime rt = Runtime.getRuntime();
+    	Process p = null;
+    	StringBuffer response = new StringBuffer();
+    	String input = "";
+    	String error = "";
+    	try {
+    		if (log.isDebugEnabled()) {
+    			log.debug("will execute command "+command);
+    		}
+    		if (dir == null && envp == null) {
+    			p = rt.exec(command);
+
+    		} else if (dir == null) {
+    			p = rt.exec(command, envp);
+
+    		} else if (envp == null) {
+    			throw new IllegalArgumentException("if dir != null then envp must also be != null");
+
+    		} else {
+    			p = rt.exec(command, envp, dir);
+    		}
+
+    		//wait for the result of identify
+    		try {
+    			p.waitFor();
+
+    		} catch (InterruptedException ex) {}
+
+    		byte[] b = new byte[100];
+
+    		int len = 0;
+    		InputStream is = p.getInputStream();
+
+    		while ((len = is.read(b)) != -1) {
+    			byte[] res = new byte[len];
+    			System.arraycopy(b, 0, res, 0, len);
+    			String s = new String(res);
+    			response.append(s);
+    		}
+    		
+    	} finally {
+    		if (p != null) {
+    			InputStream is = p.getInputStream();
+    			if (is!=null) input = ERXFileUtilities.stringFromInputStream(is);
+    			is = p.getErrorStream();
+    			if (is!=null) error = ERXFileUtilities.stringFromInputStream(is);
+
+    			if (log.isDebugEnabled()) {
+    				log.debug("input = "+input);
+    				log.debug("error = "+error);
+    			}
+    			freeProcessResources(p);
+    		}
+    	}
+    	return new Result(response.toString(), input, error);
+
+    }
+
     /** Excecutes the specified command line command. If envp is not null the  environment variables are set
     * before executing the command.
     *
