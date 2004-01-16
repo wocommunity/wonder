58537651dc938e1919974caf164d29d200d802d0
maxmuller <maxmuller@8dc76679-1c23-de11-85cd-0050568e728b>
2004-01-16 00:10:12 +0000

    Enhancing ERXProperties to extend Properties, adds in automatic defaulting ability. First checks key.AppName then key then key.Default. This way the default values can be specified in a Properties file in a framework and one doesn't have to worry about the value being stomped on due to framework loading order

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXProperties.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXProperties.java
@@ -27,17 +27,32 @@ import com.webobjects.appserver._private.WOProjectBundle;
  * like <code>getBoolean</code> off of Boolean which would resolve
  * the System property as a Boolean object.
  */
-public class ERXProperties {
+public class ERXProperties extends Properties {
 
+    //	===========================================================================
+    //	Class Constant(s)
+    //	---------------------------------------------------------------------------
+    
     /** logging support */
     public final static ERXLogger log = ERXLogger.getERXLogger(ERXProperties.class);
 
+    /** Appended to properties to get the default value */
+    protected final static String DefaultString = ".Default";
+
+    //	===========================================================================
+    //	Class Variable(s)
+    //	---------------------------------------------------------------------------
+    
     /** WebObjects version number as string */
     private static String _webObjectsVersion;
     
     /** WebObjects version number as double */ 
     private static double _webObjectsVersionDouble; 
 
+    //	===========================================================================
+    //	Class Method(s)
+    //	---------------------------------------------------------------------------
+    
     /** 
      * Puts handy properties such as <code>com.webobjects.version</code> 
      * into the system properties. This method is called when 
@@ -615,4 +630,57 @@ public class ERXProperties {
         return actualPath;
     }
 
+    //	===========================================================================
+    //	Instance Variable(s)
+    //	---------------------------------------------------------------------------
+
+    /** caches the application name that is appended to the key for lookup */
+    protected String applicationNameForAppending;
+
+    //	===========================================================================
+    //	Instance Method(s)
+    //	---------------------------------------------------------------------------
+
+    /**
+     * Caches the application name for appending to the key.
+     * Note that for a period when the application is starting up
+     * application() will be null and name() will be null.
+     * @return application name used for appending, for example ".ERMailer"
+     */
+    protected String applicationNameForAppending() {
+        if (applicationNameForAppending == null) {
+            applicationNameForAppending = WOApplication.application() != null ? WOApplication.application().name() : null;
+            if (applicationNameForAppending != null) {
+                applicationNameForAppending = "." + applicationNameForAppending;
+            }
+        }
+        return applicationNameForAppending;
+    }
+
+    /**
+     * Overriding the default getProperty method to first check:
+     * key.<ApplicationName> before checking for key. If nothing
+     * is found then key.Default is checked. 
+     * @param key to check
+     * @return property value
+     */
+    public String getProperty(String key) {
+        String property = null;
+        String application = applicationNameForAppending();
+        if (application != null) {
+            property = super.getProperty(key + application);
+        }
+        if (property == null) {
+            property = super.getProperty(key);
+            if (property == null) {
+                property = super.getProperty(key + DefaultString);                
+            }
+            // We go ahead and set the value to increase the lookup the next time the
+            // property is accessed.
+            if (property != null && application != null) {
+                setProperty(key + application, property);
+            }
+        }
+        return property;
+    }
 }
