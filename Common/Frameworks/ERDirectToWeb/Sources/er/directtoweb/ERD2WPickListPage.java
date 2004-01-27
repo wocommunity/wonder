da3517904012e3895f43f6d5bbbe6f801d863f51
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2004-01-27 23:29:59 +0000

    different formatting, better for debugging

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WPickListPage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WPickListPage.java
@@ -22,10 +22,10 @@ public class ERD2WPickListPage extends ERD2WListPage implements ERDPickPageInter
 
     /** holds the selected objects */
     protected NSMutableArray selectedObjects = new NSMutableArray();
-    
+
     /**
-     * IE sometimes won't submit the form if it only has checkboxes, we
-     * bind this to a WOHiddenField.
+     * IE sometimes won't submit the form if it only has checkboxes, we bind
+     * this to a WOHiddenField.
      */
     public String dummy;
 
@@ -34,59 +34,78 @@ public class ERD2WPickListPage extends ERD2WListPage implements ERDPickPageInter
 
     /**
      * Public constructor.
-     * @param context current context
+     * 
+     * @param context
+     *            current context
      */
-    public ERD2WPickListPage(WOContext context) { super(context); }    
+    public ERD2WPickListPage(WOContext context) {
+        super(context);
+    }
 
     /**
      * Determines if the cancel button should be shown.
+     * 
      * @return if we have a nextPage set
      */
-    public boolean showCancel() { return nextPage()!=null; }
+    public boolean showCancel() {
+        return nextPage() != null;
+    }
 
-    public boolean checked() { return selectedObjects.containsObject(object()); }
-    
+    public boolean checked() {
+        return selectedObjects.containsObject(object());
+    }
+
     public void setChecked(boolean newChecked) {
         if (newChecked) {
-            if (!selectedObjects.containsObject(object()))
-                selectedObjects.addObject(object());
+            if (!selectedObjects.containsObject(object())) selectedObjects.addObject(object());
         } else
             selectedObjects.removeObject(object());
     }
 
-    //public WOComponent cancelPage() 
+    //public WOComponent cancelPage()
     //public void setCancelPage(WOComponent cp);
 
     public NSArray selectedObjects() {
         return selectedObjects;
     }
+
     public void setSelectedObjects(NSArray selectedObjects) {
         this.selectedObjects = selectedObjects.mutableClone();
     }
 
-    // FIXME: Not using cancel page at the moment. Only matters if not using a branch delegate.
+    // FIXME: Not using cancel page at the moment. Only matters if not using a
+    // branch delegate.
     private WOComponent _cancelPage;
-    public WOComponent cancelPage() { return _cancelPage; }
-    public void setCancelPage(WOComponent cp) { _cancelPage = cp; }    
-    
-    // FIXME: Not sure if this makes sense to 
-    public void setChoices(NSArray choices) {}
-    
+
+    public WOComponent cancelPage() {
+        return _cancelPage;
+    }
+
+    public void setCancelPage(WOComponent cp) {
+        _cancelPage = cp;
+    }
+
+    // FIXME: Not sure if this makes sense to
+    public void setChoices(NSArray choices) {
+    }
+
     public WOComponent selectAll() {
         selectedObjects.removeAllObjects();
-        NSArray list=displayGroup().qualifier()==null ? displayGroup().allObjects() :
-            EOQualifier.filteredArrayWithQualifier(displayGroup().allObjects(), displayGroup().qualifier());
-        for (Enumeration e=list.objectEnumerator();e.hasMoreElements();) { selectedObjects.addObject(e.nextElement()); }
+        NSArray list = displayGroup().qualifier() == null ? displayGroup().allObjects() : EOQualifier.filteredArrayWithQualifier(displayGroup().allObjects(),
+                displayGroup().qualifier());
+        for (Enumeration e = list.objectEnumerator(); e.hasMoreElements();) {
+            selectedObjects.addObject(e.nextElement());
+        }
         return null;
     }
-    
+
     public WOComponent unselectAll() {
         selectedObjects.removeAllObjects();
         return null;
     }
 
     public boolean singleSelection() {
-        if (_singleSelection==null) {
+        if (_singleSelection == null) {
             _singleSelection = ERXValueUtilities.booleanValue(d2wContext().valueForKey("singleSelection")) ? Boolean.TRUE : Boolean.FALSE;
         }
         return _singleSelection.booleanValue();
@@ -94,5 +113,5 @@ public class ERD2WPickListPage extends ERD2WListPage implements ERDPickPageInter
 
     public String selectionWidgetName() {
         return singleSelection() ? "WORadioButton" : "WOCheckBox";
-    }        
+    }
 }
