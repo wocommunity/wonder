9c1a1e2714a47bb19b2b1ea2307f7db62a596a84
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-02-10 17:33:57 +0000

    - extended formatter

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXNumberFormatter.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXNumberFormatter.java
@@ -7,17 +7,26 @@
 package er.extensions;
 
 import com.webobjects.foundation.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.*;
 
 /**
- * A simple extension to the number formatter that
+ * An extension to the number formatter. It
  * will strip out the characters '%$,' when parsing
- * a string.
+ * a string and can scale values by setting a pattern like 
+ * <code>(/1024=)0.00 KB</code> which will divide the actual value by 1024 or
+ * <code>(*60=)0 seconds</code> which will multiply the actual value by 60.
  */
 public class ERXNumberFormatter extends NSNumberFormatter {
 
     /** holds a reference to the shared instance */
     protected static ERXNumberFormatter _sharedInstance;
+    private String _ignoredChars = "%$,";
+    private BigDecimal _factor;
+	private String _operator;
+    
     /**
      * Returns the shared instance
      * @return shared instance
@@ -31,29 +40,138 @@ public class ERXNumberFormatter extends NSNumberFormatter {
     /**
      * Public constructor
      */
-    public ERXNumberFormatter() {
+    public ERXNumberFormatter(String pattern) {
+    	super(pattern);
     }
 
     /**
-     * Strips out the characters '$%,' from the string to
-     * be parsed.
+	 * 
+	 */
+	public ERXNumberFormatter() {
+	}
+	
+	public void setIgnoredChars(String value) {
+		_ignoredChars = value;
+	}
+	
+	protected void setFactor(BigDecimal value) {
+		_factor = value;
+	}
+	
+	protected void setOperator(String value) {
+		_operator = value;
+	}
+	
+	/**
+	 * Overridden to search the pattern for operators and factors. The pattern should be
+	 * <code>'(' . operatorChar . factor . '=)' normalFormatterString</code>
+	 * @see com.webobjects.foundation.NSNumberFormatter#setPattern(java.lang.String)
+	 */
+	public void setPattern(String pattern) {
+		int offset = pattern.indexOf("=)");
+		if(offset != -1) {
+			try {
+				setFactor(new BigDecimal(pattern.substring(2, offset)));
+				setOperator(pattern.substring(1, 2));
+				pattern = pattern.substring(offset+2);
+			} catch(NumberFormatException e1) {
+				throw new IllegalArgumentException("ERXNumberFormatter must have a pattern like '(*1024=)#,##0.00', where 1024 is the factor.");
+			} catch(IndexOutOfBoundsException e) {
+				throw new IllegalArgumentException("ERXNumberFormatter must have a pattern like '(*1024=)#,##0.00', where 1024 is the factor.");
+			}
+		} else {
+			setFactor(null);
+			setOperator(null);
+		}
+		super.setPattern(pattern);
+	}
+	
+	
+	/**
+	 * Override this in your subclass to provide for other operations when formatting a value.
+	 * @param value
+	 * @return
+	 */
+	protected BigDecimal performFormat(BigDecimal value) {
+		if("*".equals(_operator)) {
+			value = value.divide(_factor, value.scale(), BigDecimal.ROUND_HALF_EVEN);
+		} else if("/".equals(_operator)) {
+			value = value.multiply(_factor);
+		}
+		return value;
+	}
+	
+	/**
+	 * Override this in your subclass to provide for other operations when parsing a value.
+	 * @param value
+	 * @return
+	 */
+	protected BigDecimal performParse(BigDecimal value) {
+		if("*".equals(_operator)) {
+			value = value.multiply(_factor);
+		} else if("/".equals(_operator)) {
+			value = value.divide(_factor, value.scale(), BigDecimal.ROUND_HALF_EVEN);
+		}
+		return value;
+	}
+	
+	/**
+     * Strips out the ignored characters and optionally performs an operation on the value
+     * from the string to be parsed.
      * @param aString to be parsed
      * @return the parsed object
      */
-    // FIXME: Should provide api to allow for additions to the list of ignored characters
-    // 		also ignoring the , character is not good for localization.
-    // CHECKME: Is this really needed now that we can form locales?
     public Object parseObject(String aString) throws java.text.ParseException {
         char[] chars = aString.toCharArray();
         char[] filteredChars = new char[chars.length];
-        String ignoredChars = "$%,";
         int count = 0;
         for (int i = 0; i < chars.length; i++) {
-            if (ignoredChars.indexOf((int)chars[i]) < 0) {
+            if (_ignoredChars.indexOf((int)chars[i]) < 0) {
                 filteredChars[count++] = chars[i];
             }
         }
         String filteredString = new String(filteredChars, 0, count);
-        return super.parseObject(filteredString);
+        Object result = super.parseObject(filteredString);
+        if(result instanceof Number && _operator != null) {
+        	BigDecimal newValue = null;
+        	if(result instanceof BigDecimal) {
+        		newValue = (BigDecimal)result;
+        	} else {
+        		newValue = new BigDecimal(((Number)result).doubleValue());
+        	}
+        	
+        	newValue = performParse(newValue);
+        	
+        	if(result instanceof BigInteger) {
+        		result = new BigInteger("" + newValue.intValue());
+        	} else {
+        		result = newValue;       		
+        	}
+        }
+        return result;
+    }
+    
+    /**
+     *  Overridden to perform optional conversions on the value given.
+     * @see java.text.Format#format(java.lang.Object, java.lang.StringBuffer, java.text.FieldPosition)
+     */
+    public StringBuffer format(Object value, StringBuffer buffer, FieldPosition position) {
+    	if (value instanceof Number && _operator != null) {
+    		BigDecimal newValue = null;
+    		if(value instanceof BigDecimal) {
+    			newValue = (BigDecimal)value;
+    		} else {
+    			newValue = new BigDecimal(((Number)value).doubleValue());
+    		}
+    		
+    		newValue = performFormat(newValue);
+
+    		if(value instanceof BigInteger) {
+    			value = new BigInteger("" + newValue.intValue());
+    		} else {
+    			value = newValue;       		
+    		}
+    	}
+    	return super.format(value, buffer, position);
     }
 }
