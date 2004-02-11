f34e0d8e75baf7cd2c3d245d8ece6f24b2c62b62
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-02-11 12:50:14 +0000

    - calendar can now handle localized date
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2696 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryDateRangeJavascript.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryDateRangeJavascript.java
@@ -14,37 +14,46 @@ import er.extensions.*;
 
 /**
  * Used for building date queries with javascript.<br />
- * 
+ * WARNING: the format string used by this app is in plain java format (yyyy/MM/dd) instead of (%Y/%M/%D)
  */
 
 public class ERDQueryDateRangeJavascript extends ERDCustomQueryComponent {
 
     public ERDQueryDateRangeJavascript(WOContext context) { super(context); }
 
-    protected static final NSTimestampFormatter DATE_FORMAT =
-    new NSTimestampFormatter("%m/%d/%Y");
-    protected static final NSTimestampFormatter DATE_FORMAT_YEAR_TWO_DIGITS =
-        new NSTimestampFormatter("%m/%d/%y") ;
-
+    protected static String _datePickerJavaScriptUrl;
     protected String key;
-
+    protected NSTimestampFormatter _formatter;
+    protected String _minValue;
+    protected String _maxValue;
+    protected String _minName;
+    protected String formatterString;
+    protected String _maxName;
+    
     public String propertyKey() {
         if(key == null)
             key = (String)valueForBinding("propertyKey");
         return key;
     }
 
-    private String stringForDate(NSTimestamp d) {
+    public NSTimestampFormatter formatter() {
+    	if(_formatter == null) {
+    		_formatter = new NSTimestampFormatter(formatterString());
+    	}
+    	return _formatter;
+    }
+    
+    protected String stringForDate(NSTimestamp d) {
         String result=null;
         if(d != null) {
             try {
-                result = DATE_FORMAT.format(d);
-            } catch(IllegalArgumentException nsfe) {}
+            	result =  formatter().format(d);
+            } catch(IllegalArgumentException nsfe) {
+            }
         }
         return result;
     }
 
-    private String _minValue;
     public Object minValue() {
         if(_minValue == null){
             _minValue=stringForDate((NSTimestamp)displayGroup().queryMin().valueForKey(propertyKey()));
@@ -52,7 +61,6 @@ public class ERDQueryDateRangeJavascript extends ERDCustomQueryComponent {
         return _minValue;
     }
 
-    private String _maxValue;
     public Object maxValue() {
         if (_maxValue == null) {
             _maxValue=stringForDate((NSTimestamp)displayGroup().queryMax().valueForKey(propertyKey()));
@@ -63,28 +71,9 @@ public class ERDQueryDateRangeJavascript extends ERDCustomQueryComponent {
     public NSTimestamp dateForString(String dateString) {
         NSTimestamp date = null;
         try {
-            if(dateString!=null) {
-                boolean dateIsValid = false;
-                NSMutableArray components = new NSMutableArray(NSArray.componentsSeparatedByString(dateString, "/"));
-                if (components.count() == 3) {
-                    String monthString = (String)components.objectAtIndex(0);
-                    if (monthString.length() == 1)
-                        components.replaceObjectAtIndex("0" + monthString, 0);
-                    String dayString = (String)components.objectAtIndex(1);
-                    if (dayString.length() == 1)
-                        components.replaceObjectAtIndex("0" +dayString, 1);
-                    String yearString = (String)components.objectAtIndex(2);
-                    //String yearString = dateString.substring(dateString.lastIndexOf("/")+1, dateString.length());
-                    String modifiedDateString = components.componentsJoinedByString("/");
-                    java.text.Format formatter=yearString.length()==2 ? DATE_FORMAT_YEAR_TWO_DIGITS : DATE_FORMAT;
-                    date = (NSTimestamp) formatter.parseObject(modifiedDateString);
-                    String reformattedDate=formatter.format(date);
-                    dateIsValid = reformattedDate.equals(modifiedDateString);
-                }
-                if (!dateIsValid)
-                    throw ERXValidationFactory.defaultFactory().createException(null, propertyKey(), dateString, "InvalidDateFormatException");
-
-            }
+        	if(dateString!=null) {
+        		date = (NSTimestamp)formatter().parseObject(dateString);
+        	}
         } catch (java.text.ParseException nspe) {
             NSValidation.ValidationException v =
             ERXValidationFactory.defaultFactory().createException(null, propertyKey(), dateString, "InvalidDateFormatException");
@@ -116,7 +105,6 @@ public class ERDQueryDateRangeJavascript extends ERDCustomQueryComponent {
         	displayGroup().queryMax().removeObjectForKey(propertyKey());
     }
 
-    private static String _datePickerJavaScriptUrl;
     public String datePickerJavaScriptUrl() {
         if (_datePickerJavaScriptUrl==null) {
             _datePickerJavaScriptUrl= application().resourceManager().urlForResourceNamed("date-picker.js", "ERExtensions", null, context().request());
@@ -124,20 +112,29 @@ public class ERDQueryDateRangeJavascript extends ERDCustomQueryComponent {
         return _datePickerJavaScriptUrl;
     }
 
-    private String _minName;
     public String minName() {
         if (_minName==null) _minName="min"+hashCode();
         return _minName;
     }
     public String minHREF() {
-        return "javascript:show_calendar('QueryForm." + minName() + "')"; 
+        return "javascript:show_calendar('QueryForm." + minName() + "',null,null,'"+formatterString()+"')"; 
     }
-    private String _maxName;
+
+    public String formatterString() {
+		if(formatterString == null) {
+			formatterString = (String)valueForBinding("formatter");
+			if(formatterString == null) {
+				formatterString = "MM/dd/yyyy";
+			}
+		}
+		return formatterString;
+	}
+
     public String maxName() {
         if (_maxName==null) _maxName="max"+hashCode();
         return _maxName;
     }
     public String maxHREF() {
-        return "javascript:show_calendar('QueryForm." + maxName() + "')";
+        return "javascript:show_calendar('QueryForm." + maxName() + "',null,null,'"+formatterString()+"')";
     }
 }
