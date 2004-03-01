f18d6862d6c00c1895b8e05ab3f984c640548953
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-03-01 10:10:43 +0000

    - explaination component default is now inspect on "ConfirmDelete*", not "Confirm*" as before - explaination component can now be un-set by providing a rule with higher prio and "" string - configuration name is now indirected, use explanationConfigurationName instead of inspectConfigurationName (although it still works)

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WConfirmPage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WConfirmPage.java
@@ -3,8 +3,12 @@ package er.directtoweb;
 import com.webobjects.directtoweb.*;
 import com.webobjects.appserver.*;
 
-/** Unless you need the special shouldProvideConfirmMessage, one should use ERD2WMessagePage instead */
+/** 
+ * Unless you need the special shouldProvideConfirmMessage, one should use ERD2WMessagePage instead 
+ */
 public class ERD2WConfirmPage extends ERD2WMessagePage {
 
-    public ERD2WConfirmPage(WOContext context) { super(context); }
+    public ERD2WConfirmPage(WOContext context) { 
+    	super(context); 
+    }
 }
