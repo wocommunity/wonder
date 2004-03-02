67f40c38040a341be623fbd25386b96d0a7f32fd
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2004-03-02 10:07:06 +0000

    zipping a string / byte array makes no sense, one uses inflater / deflater to 'zip' a bytearray.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2757 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXCompressionUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXCompressionUtilities.java
@@ -10,18 +10,20 @@ import er.extensions.*;
 
 public class ERXCompressionUtilities {
 
+    public static final ERXLogger log = ERXLogger.getERXLogger(ERXCompressionUtilities.class);
+
     public static byte[] gzipByteArray(byte[] input) {
         try {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
-        GZIPOutputStream out = new GZIPOutputStream(bos);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
+            GZIPOutputStream out = new GZIPOutputStream(bos);
 
-        out.write(input, 0, input.length);
+            out.write(input, 0, input.length);
 
-        out.finish();
-        out.close();
+            out.finish();
+            out.close();
 
-        byte[] compressedData = bos.toByteArray();
-        return compressedData;
+            byte[] compressedData = bos.toByteArray();
+            return compressedData;
         } catch (IOException e) {
             return null;
         }
@@ -38,7 +40,7 @@ public class ERXCompressionUtilities {
             return null;
         }
     }
-    
+
     public static String gzipString(String source) {
         try {
             return new String(gzipByteArray(source.getBytes("UTF-8")), "UTF-8");
@@ -64,7 +66,7 @@ public class ERXCompressionUtilities {
         } catch (UnsupportedEncodingException e) {
             return null;
         }
-    }    
+    }
 
     public static byte[] gzipStringAsByteArray(String source) {
         try {
@@ -73,65 +75,90 @@ public class ERXCompressionUtilities {
             return null;
         }
     }
-    
+
     public static byte[] zipByteArray(byte[] input) {
         try {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
-        ZipOutputStream out = new ZipOutputStream(bos);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
+            ZipOutputStream out = new ZipOutputStream(bos);
 
-        out.write(input, 0, input.length);
+            out.write(input, 0, input.length);
 
-        out.finish();
-        out.close();
+            out.finish();
+            out.close();
 
-        byte[] compressedData = bos.toByteArray();
-        return compressedData;
+            byte[] compressedData = bos.toByteArray();
+            return compressedData;
         } catch (IOException e) {
             return null;
         }
     }
 
-    public static byte[] unzipByteArray(byte[] input) {
+    public static File unzipByteArrayIntoDirectory(byte[] input, File directory, boolean overwrite) {
         try {
-        ByteArrayInputStream bos = new ByteArrayInputStream(input);
-        ZipInputStream in = new ZipInputStream(bos);
+            if (!directory.exists()) {
+                directory.mkdirs();
+                directory.mkdir();
+                if (!directory.exists()) { throw new IllegalStateException("could not create directory " + directory); }
+            } else {
+                if (!overwrite) {
+                    throw new IllegalStateException("overwrite is false and file " + directory + " does exist");
+                } else if (!directory.isDirectory()) { throw new IllegalArgumentException("file " + directory + " is NOT an directory"); }
+            }
 
-        byte[] uncompressedData = ERXFileUtilities.bytesFromInputStream(in);
-        return uncompressedData;
+            long start = System.currentTimeMillis();
+            ByteArrayInputStream bos = new ByteArrayInputStream(input);
+            ZipInputStream in = new ZipInputStream(bos);
+            ZipEntry entry = null;
+            while ((entry = in.getNextEntry()) != null) {
+                long start1 = System.currentTimeMillis();
+
+                String oriName = entry.getName();
+                String filename = directory.getAbsolutePath() + File.separator + oriName;
+
+                if (entry.isDirectory()) {
+                    if (log.isDebugEnabled()) log.debug("creating directory " + oriName);
+
+                    File f = new File(filename);
+                    f.mkdirs();
+                    f.mkdir();
+
+                } else {
+                    int uncompressedSize = (int) entry.getSize();
+
+                    if (uncompressedSize > -1) {
+                        byte[] b = new byte[uncompressedSize];
+                        in.read(b);
+                        FileOutputStream fis = new FileOutputStream(filename);
+                        fis.write(b);
+                        fis.flush();
+                        fis.close();
+
+                        if (log.isDebugEnabled()) log.debug("unzipped entry " + filename);
+                    }
+                }
+                long end1 = System.currentTimeMillis();
+
+            }
+            long end = System.currentTimeMillis();
+            if (log.isDebugEnabled()) log.debug("whole decompression took " + (end - start));
+            return directory;
         } catch (IOException e) {
-            return null;
-        }
-    }
-
-    public static String zipString(String source) {
-        try {
-        return new String(zipByteArray(source.getBytes("UTF-8")), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            return null;
-        }
-    }
-
-    public static String unzipString(String source) {
-        try {
-        byte[] b = unzipByteArray(source.getBytes("UTF-8"));
-        return new String(b, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
             return null;
         }
     }
 
     public static byte[] deflateByteArray(byte[] input) {
         try {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
-        DeflaterOutputStream out = new DeflaterOutputStream(bos);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream(input.length);
+            DeflaterOutputStream out = new DeflaterOutputStream(bos);
 
-        out.write(input, 0, input.length);
+            out.write(input, 0, input.length);
 
-        out.finish();
-        out.close();
+            out.finish();
+            out.close();
 
-        byte[] compressedData = bos.toByteArray();
-        return compressedData;
+            byte[] compressedData = bos.toByteArray();
+            return compressedData;
         } catch (IOException e) {
             return null;
         }
@@ -139,11 +166,11 @@ public class ERXCompressionUtilities {
 
     public static byte[] inflateByteArray(byte[] input) {
         try {
-        ByteArrayInputStream bos = new ByteArrayInputStream(input);
-        InflaterInputStream in = new InflaterInputStream(bos);
+            ByteArrayInputStream bos = new ByteArrayInputStream(input);
+            InflaterInputStream in = new InflaterInputStream(bos);
 
-        byte[] uncompressedData = ERXFileUtilities.bytesFromInputStream(in);
-        return uncompressedData;
+            byte[] uncompressedData = ERXFileUtilities.bytesFromInputStream(in);
+            return uncompressedData;
         } catch (IOException e) {
             return null;
         }
@@ -151,7 +178,7 @@ public class ERXCompressionUtilities {
 
     public static String deflateString(String source) {
         try {
-        return new String(deflateByteArray(source.getBytes("UTF-8")), "UTF-8");
+            return new String(deflateByteArray(source.getBytes("UTF-8")), "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return null;
         }
@@ -159,11 +186,11 @@ public class ERXCompressionUtilities {
 
     public static String inflateString(String source) {
         try {
-        byte[] b = inflateByteArray(source.getBytes("UTF-8"));
-        return new String(b, "UTF-8");
+            byte[] b = inflateByteArray(source.getBytes("UTF-8"));
+            return new String(b, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return null;
         }
     }
-    
+
 }
