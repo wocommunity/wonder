ea3a9ef58a9086d08310564cebf910974fa3636c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-03-08 13:05:58 +0000

    - adding table generation (unfinished)
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@2800 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXKeyValueCodingUtilities.java
@@ -0,0 +1,38 @@
+/*
+ * Created on 08.03.2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package er.extensions;
+
+import java.lang.reflect.Field;
+
+import com.webobjects.foundation.NSForwardException;
+
+/**
+ * Utilities for use with key value coding.
+ * @author ak
+ */
+public class ERXKeyValueCodingUtilities {
+	
+    /**
+     * Extends key-value coding to a class. You can currently only
+     * get the static fields of the class, not method results.
+     * @param clazz
+     * @param key
+     * @return
+     */
+    public static Object classValueForKey(Class clazz, String key) {
+    	Object result = null;
+    	if(key != null) {
+    		try {
+    			Field f = clazz.getDeclaredField(key);
+    			result = f.get(clazz);
+    		} catch (Exception e) {
+    			throw new NSForwardException(e);
+    		}
+    	}
+    	return result;
+    }
+}
