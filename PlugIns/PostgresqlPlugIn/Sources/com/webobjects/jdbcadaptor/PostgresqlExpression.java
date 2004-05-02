4f92aabeb70cc6553dcf3b80a979ab0f8ef1551a
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-05-02 06:07:02 +0000

    Added correct JOIN clause handling (Arturo Perez)
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@3008 8dc76679-1c23-de11-85cd-0050568e728b

--- PlugIns/PostgresqlPlugIn/Sources/com/webobjects/jdbcadaptor/PostgresqlExpression.java
+++ PlugIns/PostgresqlPlugIn/Sources/com/webobjects/jdbcadaptor/PostgresqlExpression.java
@@ -4,13 +4,16 @@ import com.webobjects.foundation.*;
 import com.webobjects.eoaccess.*;
 
 /**
-* Postgres needs special handling of NSData conversion, special
- * escape char and has a regex query selector.
- * @author ak
+ * Postgres needs special handling of NSData conversion, special
+ * escape char, has a regex query selector and handles JOIN clauses correctly.
+ * @author ak: Regex, NSData
+ * @author Giorgio Valito: refactoring, typecasting, schema building
+ * @author Arturo Perez: JOIN clauses
  */
 public class PostgresqlExpression extends JDBCExpression {
 
     static private final String _DEFERRABLE_MODIFIER = " INITIALLY DEFERRED";
+
     /**
     * Lookup table for conversion of bytes -> hex.
      */
@@ -25,7 +28,7 @@ public class PostgresqlExpression extends JDBCExpression {
     }
 
     /**
-        * Overridden because the original version throws when the
+     * Overridden because the original version throws when the
      * data contains negative byte values.
      */
     public String formatValueForAttribute(Object obj, EOAttribute eoattribute) {
@@ -39,7 +42,7 @@ public class PostgresqlExpression extends JDBCExpression {
     }
 
     /**
-        * Overrides the parent implementation to add an <code>INITIALLY DEFERRED</code> to the generated statement.
+     * Overrides the parent implementation to add an <code>INITIALLY DEFERRED</code> to the generated statement.
      * Useful you want to generate the schema-building SQL from a pure java environment.
      */
     public void prepareConstraintStatementForRelationship( EORelationship relationship, NSArray sourceColumns, NSArray destinationColumns ) {
@@ -48,14 +51,15 @@ public class PostgresqlExpression extends JDBCExpression {
     }
 
     /**
-        * Overridden because Postgres uses "|" instead of "\" as any other database system.
+     * Overridden because Postgres uses "|" instead of "\" like any
+     * other database system.
      */
     public char sqlEscapeChar() {
         return _SQL_ESCAPE_CHAR;
     }
 
     /**
-        * Overriden so we can put a regex-match qualifier in the display groups
+     * Overriden so we can put a regex-match qualifier in the display groups
      * query bindings. You can bind '~*' to queryOperator.someKey and '.*foo' to
      * queryMatch.someKey and will get the correct results.
      */
@@ -67,7 +71,7 @@ public class PostgresqlExpression extends JDBCExpression {
     }
 
     /**
-        * Overridden because the original version throws when the
+     * Overridden because the original version throws when the
      * data contains negative byte values.
      */
     public String sqlStringForData(NSData data) {
@@ -92,5 +96,184 @@ public class PostgresqlExpression extends JDBCExpression {
         }
         return super.sqlStringForValue(v,kp);
     }
-*/    
+*/
+
+    /** Helper class to store a join definition */
+    private class JoinClause {
+        String table1;
+        String op;
+        String table2;
+        String joinCondition;
+        public String toString() {
+            return table1 + op + table2 + joinCondition;
+        }
+    }
+
+    /** Holds array of JoinClause. */
+    private NSMutableArray _alreadyJoined = new NSMutableArray();
+
+    /**
+     * Overriden to handle correct placements of join conditions.
+     */
+    public String assembleSelectStatementWithAttributes(NSArray attributes,
+                                                        boolean lock,
+                                                        com.webobjects.eocontrol.EOQualifier qualifier,
+                                                        NSArray fetchOrder,
+                                                        String selectString,
+                                                        String columnList,
+                                                        String tableList,
+                                                        String whereClause,
+                                                        String joinClause,
+                                                        String orderByClause,
+                                                        String lockClause) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT ");
+        sb.append(columnList);
+        sb.append(" FROM ");
+
+        if (_alreadyJoined.count() > 0) {
+            sb.append(" ");
+            sb.append(joinClauseString());
+        } else {
+            sb.append(tableList);
+        }
+        if (whereClause != null && whereClause.length() > 0) {
+            sb.append(" WHERE ");
+            sb.append(whereClause);
+        }
+        if (orderByClause != null && orderByClause.length() > 0) {
+            sb.append(" ORDER BY ");
+            sb.append(orderByClause);
+        }
+        if (lockClause != null && lockClause.length() > 0) {
+            sb.append(" ");
+            sb.append(lockClause);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Overridden to compose the final string expression for the join clause
+     * from the array we constructed previously.
+     */
+    public String joinClauseString() {
+        NSMutableDictionary seenIt = new NSMutableDictionary();
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < _alreadyJoined.count(); i++) {
+            JoinClause jc = (JoinClause)_alreadyJoined.objectAtIndex(i);
+
+            if (seenIt.objectForKey(jc.table1) == null &&
+                seenIt.objectForKey(jc.table2) == null) {
+                sb.append(jc.table1);
+                sb.append(jc.op);
+                sb.append(jc.table2);
+            } else if (seenIt.objectForKey(jc.table1) == null) {
+                sb.append(jc.op);
+                sb.append(jc.table1);
+            } else {
+                sb.append(jc.op);
+                sb.append(jc.table2);
+            }
+            sb.append(jc.joinCondition);
+            sb.append(" ");
+
+            seenIt.setObjectForKey(jc.table1, jc.table1);
+            seenIt.setObjectForKey(jc.table2, jc.table2);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Overriden to not call the super implementation.
+     */
+    public void addJoinClause(String leftName,
+                              String rightName,
+                              int semantic) {
+        String clause = assembleJoinClause(leftName, rightName, semantic);
+        _alreadyJoined.insertObjectAtIndex(clause, 0);
+    }
+
+    /**
+     * Utility that traverses a key path to find the last destination entity
+     */
+    private EOEntity entityForKeyPath(String keyPath) {
+        NSArray keys = NSArray.componentsSeparatedByString(keyPath, ".");
+        EOEntity ent = entity();
+        for (int i = 0; i < keys.count(); i++) {
+            String k = (String)keys.objectAtIndex(i);
+            EORelationship rel = ent.anyRelationshipNamed(k);
+            if (rel == null) {
+                throw new IllegalArgumentException("relationship " + k + " relationship was null");
+            }
+            ent = rel.destinationEntity();
+        }
+        return ent;
+    }
+
+    /**
+     * Overriden to contruct a valid SQL92 JOIN clause as opposed to
+     * the Oracle-like SQL the superclass produces.
+     */
+    public String assembleJoinClause(String leftName,
+                                     String rightName,
+                                     int semantic) {
+        StringBuffer sb = new StringBuffer();
+        String leftAlias = leftName.substring(0, leftName.indexOf("."));
+        String rightAlias = rightName.substring(0, rightName.indexOf("."));
+
+        String leftCol = leftName.substring(leftName.indexOf(".") + 1);
+        String rightCol = rightName.substring(rightName.indexOf(".") + 1);
+
+        NSArray k;
+        EOEntity rightEntity;
+        EOEntity leftEntity;
+
+        if (leftAlias.equals("t0")) {
+            leftEntity = entity();
+        } else {
+            k = aliasesByRelationshipPath().allKeysForObject(leftAlias);
+            String leftKey = k.count()>0? (String)k.lastObject() : "";
+            leftEntity = entityForKeyPath(leftKey);
+        }
+
+        if (rightAlias.equals("t0")) {
+            rightEntity = entity();
+        } else {
+            k = aliasesByRelationshipPath().allKeysForObject(rightAlias);
+            String rightKey = k.count()>0? (String)k.lastObject() : "";
+            rightEntity = entityForKeyPath(rightKey);
+        }
+        String rightTable = rightEntity.externalName();
+        String leftTable = leftEntity.externalName();
+
+        JoinClause jc = new JoinClause();
+
+        jc.table1 = leftTable + " " + leftAlias;
+
+        switch (semantic) {
+            case EORelationship.LeftOuterJoin:
+                jc.op = " LEFT OUTER JOIN ";
+                break;
+            case EORelationship.RightOuterJoin:
+                jc.op = " RIGHT OUTER JOIN ";
+                break;
+            case EORelationship.FullOuterJoin:
+                jc.op = " FULL OUTER JOIN ";
+                break;
+            case EORelationship.InnerJoin:
+                jc.op = " INNER JOIN ";
+                break;
+        }
+
+        jc.table2 = rightTable + " " + rightAlias;
+
+        if (leftCol.equals(rightCol)) {
+            jc.joinCondition = " USING (" + leftCol + ")";
+        } else {
+            jc.joinCondition = " ON " + leftName + " = " + rightName;
+        }
+        return jc.toString();
+    }
 }
