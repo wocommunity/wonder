089ebfbb050b826c5965073a79bcb09a517e3926
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-05-05 08:19:16 +0000

    added fix for disabled links and uncalled child actions, javadoc
    git-svn-id: file:///Users/q/working/wondersvn/trunk@3013 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXHyperlink.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXHyperlink.java
@@ -12,12 +12,25 @@ import com.webobjects.foundation.*;
 
 
 /**
- * @author david
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Enhancement to WOHyperlink. Don't use this class directly, it is patched 
+ * automatically into the runtime system on application startup. Just use WOHyperlink.
+ * <ul>
+ * <li>Puts a description of the action into the session under the key <code>ERXActionLogging</code>
+ * <li>When the <code>disabled</code> is true, then returns <code>context().page()</code>
+ * instead of the normal WONoContentElement. The rationale is that in almost all cases 
+ * you don't want your users to see an empty page, especially not in the typical case when
+ * you show a list paginator and disable the link to the current item.
+ * <li>When you have action targets inside of the hyperlink, then their invokeAction() 
+ * method is normally never called. This is probably an optimization, but breaks the case when you have
+ * - say - onClick elements inside of the hyperlink. This subclass will instead propagate the
+ * invokeAction to the children if the senderID() starts with the elementID() (which should indicate an
+ * action inside of a hyperlink).  
+ * </ul>
+ * @author david Logging
+ * @author ak WONoContentElement fix, senderID fix
  */
 public class ERXHyperlink extends WOHyperlink {
+    
     public static ERXLogger log = ERXLogger.getERXLogger(ERXHyperlink.class);
     
     /**
@@ -29,11 +42,22 @@ public class ERXHyperlink extends WOHyperlink {
         super(arg0, arg1, arg2);
     }
 
-    
-    /* logs the action name into session's dictionary with a key = ERXActionLogging 
+    /**
+     * Overriden to perform the logging, propagating the action to subelements and returning the
+     * current page if an empty page is returned from super.
      */
-    public WOActionResults invokeAction(WORequest arg0, WOContext arg1) {
-        WOActionResults result = super.invokeAction(arg0, arg1);
+    public WOActionResults invokeAction(WORequest request, WOContext context) {
+        WOActionResults result = super.invokeAction(request, context);
+        if(result != null && (result instanceof WONoContentElement)) {
+            result = context.page();
+        }
+        if(result == null) {
+            String sender = context.senderID();
+            String element = context.elementID();
+            if(sender.startsWith(element) && !element.equals(sender)) {
+                result = invokeChildrenAction(request, context);
+            }
+        }
         if (result != null && ERXExtensions.session() != null) {
             ERXExtensions.session().setObjectForKey(this.toString(), "ERXActionLogging");
         }
