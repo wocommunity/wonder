18ee87d076c35f07737da6b267b55ae39e073a22
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2004-05-19 23:59:30 +0000

    NEW FEATURE: EOObjectStoreCoordinator subclass which provies automatic release of JDBC Connections
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@3027 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXObjectStoreCoordinator.java
@@ -0,0 +1,38 @@
+package er.extensions;
+
+import com.webobjects.eocontrol.*;
+
+
+/**
+ * @author david
+ *
+ * Adds functionality to automatically close all related JDBC Connections.
+ */
+public class ERXObjectStoreCoordinator extends EOObjectStoreCoordinator {
+    public static ERXLogger log = ERXLogger.getERXLogger(ERXObjectStoreCoordinator.class);
+    
+    public boolean _didClose = false;
+    public boolean _shouldClose = false;
+    
+    /** @see com.webobjects.eocontrol.EOObjectStoreCoordinator
+     * 
+     */
+    public ERXObjectStoreCoordinator() {
+        super();
+    }
+
+    public ERXObjectStoreCoordinator(boolean shouldClose) {
+        this();
+        _shouldClose = shouldClose;
+    }
+    
+    public void dispose() {
+        if (_shouldClose) {
+            _didClose = ERXEOAccessUtilities.closeDatabaseConnections(this);
+            if (!_didClose && _shouldClose) {
+                log.error("shouldClose was true but could not close all Connections!");
+            }
+        }
+        super.dispose();
+    }
+}
