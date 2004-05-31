7dd6eb0bfdebcf885be95c43f73496c393ae2722
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2004-05-31 07:29:14 +0000

    FEATURE: now implements Map except two methods: entrySet and keySet

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXMutableDictionary.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXMutableDictionary.java
@@ -2,6 +2,7 @@ package er.extensions;
 
 
 import java.io.*;
+import java.util.*;
 
 import com.webobjects.foundation.*;
 
@@ -9,7 +10,7 @@ import com.webobjects.foundation.*;
 usefull class in to automatically en- and decode an NSMutableDictionary
  as blob into a database. ERPrototype name = mutableDictionary
 */
-public class ERXMutableDictionary extends NSMutableDictionary {
+public class ERXMutableDictionary extends NSMutableDictionary implements Map {
     public static final long serialVersionUID = 8091318522043166356L;
 
     public static NSData toBlob(ERXMutableDictionary d) throws Exception {
@@ -42,4 +43,86 @@ public class ERXMutableDictionary extends NSMutableDictionary {
     public Object clone() {
         return new ERXMutableDictionary(this);
     }
+    
+    /* (non-Javadoc)
+     * @see java.util.Map#clear()
+     */
+    public void clear() {
+        removeAllObjects();
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#containsKey(java.lang.Object)
+     */
+    public boolean containsKey(Object arg0) {
+        return objectForKey(arg0) != null;
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#containsValue(java.lang.Object)
+     */
+    public boolean containsValue(Object arg0) {
+        return allValues().containsObject(arg0);
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#entrySet()
+     */
+    public Set entrySet() {
+        throw new IllegalAccessError("not implemented");
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#get(java.lang.Object)
+     */
+    public Object get(Object arg0) {
+        return objectForKey(arg0);
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#isEmpty()
+     */
+    public boolean isEmpty() {
+        return count() == 0;
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#keySet()
+     */
+    public Set keySet() {
+        throw new IllegalAccessError("not implemented");
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#put(java.lang.Object, java.lang.Object)
+     */
+    public Object put(Object arg0, Object arg1) {
+        Object prev = objectForKey(arg0);
+        setObjectForKey(arg1, arg0);
+        return prev;
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#putAll(java.util.Map)
+     */
+    public void putAll(Map arg0) {
+        throw new IllegalAccessError("not implemented");
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#remove(java.lang.Object)
+     */
+    public Object remove(Object arg0) {
+        return removeObjectForKey(arg0);
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#size()
+     */
+    public int size() {
+        return count();
+    }
+    /* (non-Javadoc)
+     * @see java.util.Map#values()
+     */
+    public Collection values() {
+        return (Collection)allValues();
+    }
+    
+    public NSArray allValues() {
+        NSArray av = super.allValues();
+        return new ERXMutableArray(av);
+    }
+    
+    
 }
