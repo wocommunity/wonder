630b50ed8ab47a0eaecc404269133d459c28074b
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2004-07-19 19:09:52 +0000

    API: moved isNewObject to ERXEOControlUtilities

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOControlUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOControlUtilities.java
@@ -941,7 +941,28 @@ public class ERXEOControlUtilities {
             a.addObject(eo);
         }
         return a;
-    }    
+    }  
+    
+    /**
+     * Tests if an enterprise object is a new object by
+     * looking to see if it is in the list of inserted
+     * objects for the editing context or if the editing
+     * context is null.<br/>
+     * <br/>
+     * Note: An object that has been deleted will have it's
+     * editing context set to null which means this method
+     * would report true for an object that has been deleted
+     * from the database.
+     * @param eo enterprise object to check
+     * @return true or false depending on if the object is a
+     *		new object.
+     */
+    public static boolean isNewObject(EOEnterpriseObject eo) {
+        if (eo.editingContext() == null) return true;
+        
+        EOGlobalID gid = eo.editingContext().globalIDForObject(eo);
+        return gid.isTemporary();
+    }
 
     
     /**
