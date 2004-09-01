f40dad0da0417f20cd867552d734c3c7734d95ee
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-09-01 13:33:27 +0000

    Javadoc, ec from session
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@3190 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryIsContainedInArray.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryIsContainedInArray.java
@@ -1,18 +1,25 @@
 package er.directtoweb;
 
-import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
-import com.webobjects.eoaccess.EOUtilities;
+import com.webobjects.eoaccess.*;
 import com.webobjects.eocontrol.*;
-import com.webobjects.directtoweb.*;
 import er.extensions.*;
 
 /**
- * Class for DirectToWeb Component ERDQueryIsContainedInArray.
+ * Allows you to query for objects that have a one or more of a set of related objects.
+ * Example: given Child->School, you could select a few schools and find all the children that are in those schools.
  *
- * @binding sample sample binding explanation
- * @d2wKey sample sample d2w key
- *
+ * @binding displayGroup displayGroup to set the queryMatch in (queryOperator = isContainedInArray)
+ * @binding key relationship key ("school")
+ * @binding destinationEntityName name of the destination entity ("School")
+ * @binding restrictedChoiceKey keypath returning an array of preselections (session.user.district.schools)
+ * @binding restrictingFetchSpecification name of a fetchspec (elementarySchoools)
+ * @binding keyWhenRelationship display key for destination ("name"->school.name)
+ * @binding sortOrderAttributeName sort key for destination ("name"->school.name)
+ * @binding toManyUIStyle "browser" or "checkbox"
+ * @binding numCols number of columns when "checkbox" is the UIStyle
+ * @binding size number of rows for "checkbox" or "browser" UIStyle
+ * 
  * @created ak on Wed Apr 07 2004
  * @project ERDirectToWeb
  */
@@ -44,13 +51,8 @@ public class ERDQueryIsContainedInArray extends ERDCustomQueryComponent {
             return valueForKeyPath(restrictedChoiceKey);
         String fetchSpecName=(String)valueForBinding("restrictingFetchSpecification");
         if(fetchSpecName != null &&  fetchSpecName.length()>0) {
-            EOEditingContext ec = ERXEC.newEditingContext();
-            ec.lock();
-            try {
-                return EOUtilities.objectsWithFetchSpecificationAndBindings(ec, (String)valueForBinding("destinationEntityName"),fetchSpecName,null);
-            } finally {
-                ec.unlock();
-            }
+            EOEditingContext ec = session().defaultEditingContext();
+            return EOUtilities.objectsWithFetchSpecificationAndBindings(ec, (String)valueForBinding("destinationEntityName"),fetchSpecName,null);
         }
         return null;
     }
