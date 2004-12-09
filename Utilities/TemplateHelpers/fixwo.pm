d2d50785aa935918f18b7babe7a443dca5c4a1cf
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2004-12-09 11:05:29 +0000

    <foo /> handling, error reporting, can now switch between ' and " as a tag delimiter, now parses when in the same directory, namespaced attributes handled better

--- Utilities/TemplateHelpers/fixwo.pm
+++ Utilities/TemplateHelpers/fixwo.pm
@@ -19,7 +19,7 @@ my $skipWrite = 1;
 
 sub parseScript {
     my ($script, $encode) = (@_);
-    my $notFound = "====";
+    my $notFound = "\0";
     my $GT = $notFound . "GT" . $notFound;
     my $LT = $notFound . "LT" . $notFound;
 
@@ -37,23 +37,41 @@ sub parseScript {
     return "<$script>";
 }
 
+sub parseValue {
+    my ($value, $encode) = (@_);
+    if($encode) {
+        $value =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
+    } else {
+        $value =~ s/\%([A-Fa-f0-9]{2})/pack('C', hex($1))/seg;
+        if($value =~ m|\"|) {
+            $value = "'" . $value . "'";
+        } else {
+            $value = "\"" . $value . "\"";
+        }
+    }
+    return $value;
+}
+
 sub parseTag {
     my ($end, $tag, $rest) = (@_);
-    my $notFound = "###&###";
     $rest =~ s|nowrap([^="]?)|nowrap=nowrap$1|gsi;
     $rest =~ s|noshade|noshade=noshade|gsi;
     $rest =~ s|^\s*(.*?)\s*$|$1|gsi;
-    $rest =~ s|\s+|$notFound|gsi;
-    $rest =~ s|=\s*"(.*?)"|=$1|gsi;
-    $rest =  "$notFound$rest";
-    $rest =~ s|$notFound([a-zA-Z0-9_\-]+)\=| '" ' . lc($1) . '="'|egsi;
-    $rest =~ s|^\"\s*||gsi;
-    $rest =~ s|$notFound| |gsi;
-    $rest =~ s|\s*$||gsi;
+    $rest =~ s|=\s*"(.*?)"|'=' . parseValue($1,1) . "\0"|gesi;
+    $rest =~ s|=\s*'(.*?)'|'=' . parseValue($1,1) . "\0"|gesi;
+    $rest =~ s|\s+|\0|gsi;
+    $rest =~ s|\s+|\0|gsi;
+    $rest = "$rest\0";
+    $rest =~ s|\0+|\0|gsi;
+    $rest =~ s#([a-zA-Z0-9_\-\:]+)\=(.*?)\0# lc($1) . '=' . parseValue($2,0) . ' ' #egsi;
     $tag = lc($tag);
-    $rest = " $rest\"" if($rest ne "" && $rest !~ m|\s*/\s*$|i);
-    $rest = " $rest/" if($rest !~ m|\s*/\s*$|i && ($tag eq 'spacer' || $tag eq 'link' || $tag eq 'meta' || $tag eq 'br' || $tag eq 'img' || $tag eq 'hr' || $tag eq 'input'));
-    return $end . lc($tag) . $rest;
+    $rest = " $rest" if($rest ne "" && $rest !~ m|\s*/\s*$|i);
+    $rest = "$rest /" if($rest !~ m|\s*/\s*$|i && ($tag eq 'spacer' || $tag eq 'meta' || $tag eq 'link' || $tag eq 'meta' || $tag eq 'br' || $tag eq 'img' || $tag eq 'hr' || $tag eq 'input'));
+    $rest =~ s|\0| |gsi;
+    $rest =~ s|\s+$||gis;
+    my $result = $end . lc($tag) . $rest;
+
+    return $result;
 }
 
 sub lowerCaseBinding {
@@ -85,7 +103,7 @@ sub fixwohtml {
     return $in if($skipConvert != 0);
 
     $out =~ s|<(script.*?/script)>|parseScript($1,1)|igse;
-    $out =~ s|<\s*(/?)(\w+)(.*?)>|"<" . parseTag($1,$2,$3) .'>'|igse;
+    $out =~ s|<\s*(/?)\s*(\w+)(.*?)\s*(/?)>|"<" . parseTag($1,$2,$3) .'>'|igse;
     $out =~ s|<(script.*?/script)>|parseScript($1,0)|igse;
     return $out;
 }
@@ -131,7 +149,7 @@ sub run {
     die ("perl -mfixwo -e fixwo::run WebObjectsFile1.wo WebObjectsFile2.wo") if($#ARGV < 0);
 
     foreach $component (@ARGV) {
-   	my $debug = 0;
+   	    my $debug = 0;
         if($component =~ m|^\+(\w+)|i) {
             $skipWrite = 0 if($1 eq "write");
             $skipConvert = 0 if($1 eq "convert");
@@ -139,20 +157,25 @@ sub run {
             next;
         }
         $component =~ s|\.wo(.*?)$||;
-        my $base = $1 if($component =~ m|/([^/]+?)$|);
-        $base = $component if(!defined($base));
+        my $base = $component;
+        $base = $component if(!length($base) > 0);
 #        warn("Processing $base\n");
         my $wod = "$component.wo/$base.wod";
         my $html = "$component.wo/$base.html";
         $xhtml = fixwohtml(readFromFile($html));
         if(!$skipConvert) {
-            my $parser = new XML::Parser(NoExpand => 'true', Style => $debug ? 'Debug' : undef);
+            my $parser = new XML::Parser(NoExpand => 'true', Style => $debug ? 'Debug' : undef, ErrorContext => 3);
             my $xhtmlToCheck = $xhtml;
+            # a real check just takes too much time, so we just
+            # strip comments, text and processing instructions and add a dummy root element
+            # the error context is not the actual html, but an abbreviated version that 
+            # should at least point you to the source of the problem
+            $xhtmlToCheck =~ s|<\!--(.*?)-->||isg;
             $xhtmlToCheck =~ s|<\!.*?>||isg;
-            $xhtmlToCheck =~ s|>(.*?)<|><|isg;
+            $xhtmlToCheck =~ s|>(.*?)<|>\n<|isg;
             $xhtmlToCheck =~ s|^[^<]*?(<.*?>)[^>]*?$|$1|is;
             eval{ $parser->parse("<xml>" . qq{$xhtmlToCheck} . "</xml>") };
-            warn("Error in doc: $html\n$xhtml\n\n") if($evalReply = $@);
+            warn("Error in doc: $html ($evalReply)\n$xhtml\n\n") if($evalReply = $@);
         }
         writeToFile($html, $xhtml);
         writeToFile($wod , fixwowod(readFromFile($wod)));
