8268aa07322847c5b2fb9478e5245c76fe9cd6d4
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2005-01-04 18:24:40 +0000

    entityCode return the userInfo's value for entityCode key, used by ERXLongPrimaryKeyFactory
    git-svn-id: file:///Users/q/working/wondersvn/trunk@3354 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXModelGroup.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXModelGroup.java
@@ -12,18 +12,19 @@ import com.webobjects.eoaccess.*;
 import com.webobjects.foundation.*;
 
 /**
- * The reason that this model group is needed is because the
- * regular EOModelGroup will fail to load a model if it has
- * an entity name conflict. While normally this could be considered
- * a 'good thing' in the case of EOPrototypes multiple EOModels
- * might all need there own prototype entities (in fact EOM requires it).
- * This model group subclass will only print warning messages when
- * duplicate entity names are found.
+ * The reason that this model group is needed is because the regular
+ * EOModelGroup will fail to load a model if it has an entity name conflict.
+ * While normally this could be considered a 'good thing' in the case of
+ * EOPrototypes multiple EOModels might all need there own prototype entities
+ * (in fact EOM requires it). This model group subclass will only print warning
+ * messages when duplicate entity names are found.
  */
-public class ERXModelGroup extends EOModelGroup {
+public class ERXModelGroup extends
+        EOModelGroup {
 
     /** logging support */
-    public static ERXLogger log  = ERXLogger.getERXLogger(ERXModelGroup.class);
+    public static ERXLogger log = ERXLogger.getERXLogger(ERXModelGroup.class);
+    private Hashtable       cache;
 
     /**
      * Default public constructor
@@ -32,14 +33,14 @@ public class ERXModelGroup extends EOModelGroup {
     }
 
     /**
-     * The only reason this method is needed is so our model group
-     * subclass is used. Other than that it does the exact same thing
-     * as EOModelGroup's implementation.
+     * The only reason this method is needed is so our model group subclass is
+     * used. Other than that it does the exact same thing as EOModelGroup's
+     * implementation.
+     * 
      * @return ERXModelGroup for all of the loaded bundles
      */
     public static EOModelGroup modelGroupForLoadedBundles() {
         EOModelGroup eomodelgroup = new ERXModelGroup();
-        System.out.println("ERX: eomodelgroup id = "+System.identityHashCode(eomodelgroup));
         NSArray nsarray = NSBundle.frameworkBundles();
         int i = nsarray.count() + 1;
 
@@ -54,40 +55,38 @@ public class ERXModelGroup extends EOModelGroup {
             int i8 = nsarray7.count();
             for (int i9 = 0; i9 < i8; i9++) {
                 String string = (String) nsarray7.objectAtIndex(i9);
-                String string10
-                    = (NSPathUtilities.stringByDeletingPathExtension
-                       (NSPathUtilities.lastPathComponent(string)));
+                String string10 = (NSPathUtilities.stringByDeletingPathExtension(NSPathUtilities
+                        .lastPathComponent(string)));
                 EOModel eomodel = eomodelgroup.modelNamed(string10);
                 if (eomodel == null)
                     eomodelgroup.addModelWithPath(string);
                 else if (NSLog.debugLoggingAllowedForLevelAndGroups(1, 32768L))
-                    NSLog.debug.appendln
-                        ("Ignoring model at path \"" + string
-                         + "\" because the model group " + eomodelgroup
-                         + " already contains the model from the path \""
-                         + eomodel.path() + "\"");
+                        NSLog.debug
+                                .appendln("Ignoring model at path \"" + string + "\" because the model group "
+                                        + eomodelgroup + " already contains the model from the path \""
+                                        + eomodel.path() + "\"");
             }
         }
         // correcting an EOF Inheritance bug
-        ((ERXModelGroup)eomodelgroup).checkInheritanceRelationships();
+        ((ERXModelGroup) eomodelgroup).checkInheritanceRelationships();
         return eomodelgroup;
     }
 
     /**
-     * This implementation will load models that have
-     * entity name conflicts, removing the offending
-     * entity. The reason this is needed is because
-     * multiple models might have JDBC prototype entities
-     * which would cause problems for the model group.
-     * @param eomodel model to be added
+     * This implementation will load models that have entity name conflicts,
+     * removing the offending entity. The reason this is needed is because
+     * multiple models might have JDBC prototype entities which would cause
+     * problems for the model group.
+     * 
+     * @param eomodel
+     *            model to be added
      */
     public void addModel(EOModel eomodel) {
         Enumeration enumeration = _modelsByName.objectEnumerator();
         String name = eomodel.name();
         if (_modelsByName.objectForKey(name) != null) {
-            log.warn("The model '" + name + "' (path: " + eomodel.path()
-             + ") cannot be added to model group " + this
-             + " because it already contains a model with that name.");
+            log.warn("The model '" + name + "' (path: " + eomodel.path() + ") cannot be added to model group " + this
+                    + " because it already contains a model with that name.");
             return;
         }
         NSMutableSet nsmutableset = new NSMutableSet(128);
@@ -97,48 +96,83 @@ public class ERXModelGroup extends EOModelGroup {
             nsmutableset.addObjectsFromArray(eomodel1.entityNames());
         }
         NSSet intersection = nsmutableset.setByIntersectingSet(nsset);
-        if(intersection.count() != 0) {
+        if (intersection.count() != 0) {
             log.warn("The model '" + name + "' (path: " + eomodel.path()
-                     + ") has an entity name conflict with the entities "
-                     + intersection + " already in the model group " + this);
+                    + ") has an entity name conflict with the entities " + intersection
+                    + " already in the model group " + this);
             Enumeration e = intersection.objectEnumerator();
-            while(e.hasMoreElements()) {
-                String entityName = (String)e.nextElement();
+            while (e.hasMoreElements()) {
+                String entityName = (String) e.nextElement();
                 log.debug("Removing entity " + entityName + " from model " + name);
                 eomodel.removeEntity(eomodel.entityNamed(entityName));
             }
         }
         eomodel.setModelGroup(this);
         _modelsByName.setObjectForKey(eomodel, eomodel.name());
-        NSNotificationCenter.defaultCenter()
-            .postNotification("EOModelAddedNotification", eomodel);
+        NSNotificationCenter.defaultCenter().postNotification("EOModelAddedNotification", eomodel);
     }
 
     /**
-     * Corrects a strange EOF inheritance issue where if a model
-     * gets loaded and an entity that has children located in a
-     * different model that hasn't been loaded yet will not be
-     * setup correctly. Specifically when those child entities
-     * are loaded they will not have their parentEntity relationship
+     * Corrects a strange EOF inheritance issue where if a model gets loaded and
+     * an entity that has children located in a different model that hasn't been
+     * loaded yet will not be setup correctly. Specifically when those child
+     * entities are loaded they will not have their parentEntity relationship
      * set correctly.
      */
     public void checkInheritanceRelationships() {
         if (_subEntitiesCache != null && _subEntitiesCache.count() > 0) {
-            for (Enumeration parentNameEnumerator = _subEntitiesCache.keyEnumerator(); parentNameEnumerator.hasMoreElements();) {
-                String parentName = (String)parentNameEnumerator.nextElement();
-                NSArray children = (NSArray)_subEntitiesCache.objectForKey(parentName);
+            for (Enumeration parentNameEnumerator = _subEntitiesCache.keyEnumerator(); parentNameEnumerator
+                    .hasMoreElements();) {
+                String parentName = (String) parentNameEnumerator.nextElement();
+                NSArray children = (NSArray) _subEntitiesCache.objectForKey(parentName);
                 EOEntity parent = entityNamed(parentName);
                 for (Enumeration childrenEnumerator = children.objectEnumerator(); childrenEnumerator.hasMoreElements();) {
-                    String childName = (String)childrenEnumerator.nextElement();
+                    String childName = (String) childrenEnumerator.nextElement();
                     EOEntity child = entityNamed(childName);
-                    
+
                     if (child.parentEntity() != parent && !parent.subEntities().containsObject(child)) {
-                        log.debug("Found entity: " + child.name() + " which should have: " + parent.name() + " as it's parent.");
+                        log.debug("Found entity: " + child.name() + " which should have: " + parent.name()
+                                + " as it's parent.");
                         parent.addSubEntity(child);
                     }
                 }
             }
         }
     }
-    
+
+    /**
+     * Looks up the userInfo for the Entity with the specified entityName and
+     * returns it if the code could be found.
+     * 
+     * @param ename
+     *            the name from the Entity for which we want to the get
+     *            entityCode
+     * @return either the userInfo.entityCode or 0 if no entry could be found
+     */
+    public int entityCode(String ename) {
+        return entityCode(entityNamed(ename));
+    }
+
+    /**
+     * Looks up the userInfo for the Entity with the specified entityName and
+     * returns it if the code could be found.
+     * 
+     * @param entity
+     *            the Entity for which we want to the get entityCode
+     *            
+     * @return either the userInfo.entityCode or 0 if no entry could be found
+     */
+    public int entityCode(EOEntity entity) {
+        Integer cachedValue = (Integer) cache.get(entity);
+        if (cachedValue == null) {
+            NSDictionary d = entity.userInfo();
+            if (d == null) d = NSDictionary.EmptyDictionary;
+            cachedValue = (Integer) d.objectForKey("entityCode");
+            if (cachedValue == null) {
+                cachedValue = new Integer(0);
+                cache.put(entity, cachedValue);
+            }
+        }
+        return cachedValue.intValue();
+    }
 }
