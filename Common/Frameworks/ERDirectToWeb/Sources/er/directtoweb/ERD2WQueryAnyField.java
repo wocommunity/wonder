b62ab39d41e42503f4c587a4b43bd6959f5bc586
giorgio_v <giorgio_v@8dc76679-1c23-de11-85cd-0050568e728b>
2005-02-22 15:06:03 +0000

    Added ERD2WQueryAnyField, ERD2WQueryNumberRange, ERD2WQueryStringWithChoices, ERD2WQueryToManyField, ERD2WQueryToOneField, ERDAnyField components
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@3427 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryAnyField.java
@@ -0,0 +1,20 @@
+//
+// ERD2WQueryAnyField.java: Class file for WO Component 'ERD2WQueryAnyField'
+// Project ERDirectToWeb
+//
+// Created by giorgio on 07/10/04
+//
+package er.directtoweb;
+
+import com.webobjects.foundation.*;
+import com.webobjects.appserver.*;
+import com.webobjects.directtoweb.*;
+import er.extensions.*;
+
+public class ERD2WQueryAnyField extends D2WQueryAnyField {
+
+    public ERD2WQueryAnyField(WOContext context) {
+        super(context);
+    }
+
+}
