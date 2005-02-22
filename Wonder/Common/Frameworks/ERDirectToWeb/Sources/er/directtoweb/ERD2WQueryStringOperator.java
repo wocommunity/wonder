e26c602ae4d58e06c28a697efb205cfc43810bf7
giorgio_v <giorgio_v@8dc76679-1c23-de11-85cd-0050568e728b>
2005-02-22 14:35:53 +0000

    Localization support
    git-svn-id: file:///Users/q/working/wondersvn/trunk@3423 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryStringOperator.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryStringOperator.java
@@ -10,24 +10,49 @@ import com.webobjects.appserver.*;
 import com.webobjects.directtoweb.*;
 import com.webobjects.eocontrol.*;
 import com.webobjects.foundation.*;
+import er.extensions.*;
 
 public class ERD2WQueryStringOperator extends D2WQueryStringOperator {
-
+    public ERXKeyValuePair currentElement;
+    
     public ERD2WQueryStringOperator(WOContext context) {
         super(context);
     }
-
+    
     public NSArray allQualifierOperators(){
-        return qualifierOperatorsOverrideFromRules() != null ? qualifierOperatorsOverrideFromRules() : _allQualifierOperators;
+        NSArray operators = qualifierOperatorsOverrideFromRules() != null ? qualifierOperatorsOverrideFromRules() : _allQualifierOperators;
+        int count = operators.count();
+        NSMutableArray result = new NSMutableArray( count );
+        for( int i = 0; i < count; i++ ) {
+            String currentOperatorString = (String)operators.objectAtIndex(i);
+            String value = (String)ERXLocalizer.currentLocalizer().localizedValueForKeyWithDefault(currentOperatorString);
+            result.addObject(new ERXKeyValuePair(currentOperatorString, value));
+        }
+        return result;
     }
-
+    
     private static NSArray _stringQualifierOperators;
     private static NSArray _allQualifierOperators;
     
     public NSArray qualifierOperatorsOverrideFromRules(){
         return (NSArray)d2wContext().valueForKey("qualifierOperators");
     }
-
+    
+    public ERXKeyValuePair selectedElement() {
+        return new ERXKeyValuePair
+        (super.anOperator(), (String)ERXLocalizer.currentLocalizer().localizedValueForKeyWithDefault
+         (super.anOperator()));
+    }
+    
+    public void  setSelectedElement(ERXKeyValuePair newSelection) {
+        setAnOperator(newSelection != null ? (String) newSelection.key() : null );
+    }
+    
+    public void reset() {
+        super.reset();
+        currentElement = null;
+    }
+    
     static {
         _stringQualifierOperators = new NSArray(new String[]{"starts with", "contains", "ends with", "is", "like"});
         _allQualifierOperators = _stringQualifierOperators.arrayByAddingObjectsFromArray(EOQualifier.relationalQualifierOperators());
