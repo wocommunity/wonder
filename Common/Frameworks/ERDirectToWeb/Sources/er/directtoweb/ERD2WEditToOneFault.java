fdc8b12bb0c952af7de2c06c9390d0dce3e9d575
giorgio_v <giorgio_v@8dc76679-1c23-de11-85cd-0050568e728b>
2005-07-18 15:22:40 +0000

    Added ERD2WEditToOneFault

---
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditToOneFault.java
@@ -0,0 +1,40 @@
+//
+// ERD2WEditToOneFault.java: Class file for WO Component 'ERD2WEditToOneFault'
+// Project ERDirectToWeb
+//
+// Created by giorgio on 13/07/05
+//
+
+package er.directtoweb;
+
+import com.webobjects.appserver.*;
+import com.webobjects.directtoweb.*;
+import com.webobjects.eoaccess.*;
+import com.webobjects.eocontrol.*;
+import er.extensions.*;
+
+public class ERD2WEditToOneFault extends D2WEditToOneFault {
+    /** logging support */
+    public static final ERXLogger log = ERXLogger.getERXLogger(ERD2WEditToOneFault.class);
+    
+    
+    public ERD2WEditToOneFault(WOContext context) {
+        super(context);
+    }
+
+    public WOComponent editRelationship() {
+        WOComponent editPage = null;
+        String editRelationshipConfigurationName = (String)d2wContext().valueForKey("editRelationshipConfigurationName");
+        if (editRelationshipConfigurationName != null && editRelationshipConfigurationName.length() > 0) {
+            editPage = D2W.factory().pageForConfigurationNamed(editRelationshipConfigurationName, session());
+            if (editPage instanceof EditRelationshipPageInterface) {
+                ((EditRelationshipPageInterface)editPage).setMasterObjectAndRelationshipKey(object(),propertyKey());
+                ((EditRelationshipPageInterface)editPage).setNextPage(context().page());
+            } else {
+                log.warn("Unsupported relationship editing page: " + editPage.getClass().getName());
+            }
+        }
+        return editPage != null ? editPage : super.editRelationship();
+        
+    }
+}
