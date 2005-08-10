d41048f4556ea7dcb8c622075d56ba57d278ab5a
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2005-08-10 07:13:37 +0000

    refactoring and adding support for timeouts which prevents waiting forever cause sometimes a Process.waitFor() does never return because of whatever reasons.

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRuntimeUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRuntimeUtilities.java
@@ -1,7 +1,9 @@
 package er.extensions;
 
 import java.io.*;
+import java.util.*;
 
+import com.sun.rsasign.*;
 import com.webobjects.foundation.*;
 
 public class ERXRuntimeUtilities {
@@ -16,6 +18,10 @@ public class ERXRuntimeUtilities {
      * @param commands
      *            the commands to execute like "ls -la" or "cp /tmp/file1
      *            /tmp/file2" or "open /Applications/*.app"
+     *            new String[]{"ls", "-la"}
+     *            new String[]{"cp", "/tmp/file1", "/tmp/file2"}
+     *            new String[]{"open", "/Applications/*.app"}
+     *            
      * @param envp
      *            a <code>String</code> array which represents the environment
      *            variables like
@@ -30,15 +36,13 @@ public class ERXRuntimeUtilities {
      * @exception IOException
      *                if something went wrong
      */
-    public final static Result[] executeCommandLineCommandsWithEnvVarsInWorkingDir(String[] commands, String[] envp, File dir)
-            throws IOException {
-        Result[] results = new Result[commands.length];
-
-        for (int i = 0; i < commands.length; i++) {
-            results[i] = executeCommandLineCommandWithEnvVarsInWorkingDir(commands[i], envp, dir);
+    public final static Result executeCommandLineCommandWithArgumentsWithEnvVarsInWorkingDir(String[] command, String[] envp, File dir) throws IOException {
+        try {
+            return execute(command, envp, dir, 0);
+        } catch (ERXTimeoutException e) {
+            // this will never happen so we can return null here.
+            return null;
         }
-
-        return results;
     }
 
     /**
@@ -46,8 +50,16 @@ public class ERXRuntimeUtilities {
      * environment variables are set before executing the command.
      * 
      * @param commands
-     *            the commands to execute like "ls -la" or "cp /tmp/file1
-     *            /tmp/file2" or "open /Applications/*.app"
+     *            the commands to execute, this is an String array with two dimensions
+     *            the following commands <br>
+     *            "ls -la" or "cp /tmp/file1 /tmp/file2" or "open /Applications/*.app"<br>
+     *            would be as String arrays<br>
+     *            <pre>new String[]{
+     *              new String[] {"ls", "-la"},
+     *              new String[] {"cp", "/tmp/file1", "/tmp/file2"},
+     *              new String[] {"open", "/Applications/*.app"}
+     *            }
+     *            </pre>
      * @param envp
      *            a <code>String</code> array which represents the environment
      *            variables like
@@ -56,33 +68,75 @@ public class ERXRuntimeUtilities {
      * @param dir
      *            a <code>File</code> object representing the working
      *            directory, can be null
-     * @param useOutputFile
-     *            defines if the process should write its output to a file which
-     *            in turn is read from the filesystem instead of reading
-     *            directly from the process output stream
      * 
      * @return the results from the processes that were executed
      * 
      * @exception IOException
      *                if something went wrong
      */
-    public final static Result executeCommandLineCommandWithArgumentsWithEnvVarsInWorkingDir(String[] command, String[] envp, File dir,
-            boolean writeToOutputFile) throws IOException {
-        File outputFile = null;
-        if (writeToOutputFile) {
+    public final static Result[] executeCommandLineCommandsWithEnvVarsInWorkingDir(String[][] commands, String[] envp, File dir)
+            throws IOException {
+        Result[] results = new Result[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
             try {
-                outputFile = File.createTempFile("Wonder", ".tmp");
-            } catch (IOException e1) {
-                throw new NSForwardException(e1, "could not create temp file");
+                results[i] = execute(commands[i], envp, dir, 0);
+            } catch (ERXTimeoutException e) {
+                // will never happen
+                return null;
             }
-            String[] newEnvp = new String[envp.length + 2];
-            System.arraycopy(newEnvp, 0, newEnvp, 0, envp.length);
-            newEnvp[newEnvp.length - 2] = ">";
-            newEnvp[newEnvp.length - 2] = outputFile.getAbsolutePath();
         }
+
+        return results;
+    }
+
+    /**
+     * Excecutes the specified command line commands. If envp is not null the
+     * environment variables are set before executing the command. This method supports
+     * timeout's. This is quite important because its -always- possible that a UNIX or WINDOWS
+     * process does not return, even with simple shell scripts. This is due to whatever bugs
+     * and hence every invocation of <code>Process.waitFor()</code> should be observed and stopped
+     * if a certain amount of time is over.
+     * 
+     * @param commands
+     *            the commands to execute, this is an String array with two dimensions
+     *            the following commands <br>
+     *            "ls -la" or "cp /tmp/file1 /tmp/file2" or "open /Applications/*.app"<br>
+     *            would be as String arrays<br>
+     *            <pre>new String[]{
+     *              new String[] {"ls", "-la"},
+     *              new String[] {"cp", "/tmp/file1", "/tmp/file2"},
+     *              new String[] {"open", "/Applications/*.app"}
+     *            }
+     *            </pre>
+     * @param envp
+     *            a <code>String</code> array which represents the environment
+     *            variables like
+     *            <code>String[] envp = new String[]{"PATH=/usr/bin:/bin", "CVS_RSH=ssh"}</code>,
+     *            can be null
+     * @param dir
+     *            a <code>File</code> object representing the working
+     *            directory, can be null
+     *            
+     * @param timeout
+     *            a <code>long</code> which can be either <code>0</code> indicating this
+     *            method call waits until the process exits or any <code>long</code> number
+     *            larger than <code>0</code> which means if the process does not exit after 
+     *            <code>timeout</code> seconds then this method throws an ERXTimeoutException
+     *            
+     * 
+     * @return the results from the processes that were executed
+     * 
+     * @exception IOException
+     *                if something went wrong
+     */
+    public final static Result execute(String[] command, String[] envp, File dir, long timeout)
+            throws IOException, ERXTimeoutException {
+        File outputFile = null;
 
         Runtime rt = Runtime.getRuntime();
         Process p = null;
+        int exitValue = 0;
         StringBuffer response = new StringBuffer();
         String input = "";
         String error = "";
@@ -103,88 +157,49 @@ public class ERXRuntimeUtilities {
                 p = rt.exec(command, envp, dir);
             }
 
-            // wait for the result of identify
-            try {
-                p.waitFor();
-
-            } catch (InterruptedException ex) {
-            }
-
-            if (writeToOutputFile) {
-                response.append( ERXStringUtilities.stringWithContentsOfFile(outputFile) );
+            if (timeout > 0) {
+                final Process pp = p;
+                TimeoutTimerTask task = new TimeoutTimerTask(p);
+                Timer timer = new Timer();
+                timer.schedule(task, timeout);
+                try {
+                    p.waitFor();
+                } catch (InterruptedException ex) {
+                }
+                timer.cancel();
+                if (task.hasTimeout()) {
+                    throw new ERXTimeoutException("process did't exit after " + timeout + " milliseconds");
+                }
             } else {
-                response.append( ERXFileUtilities.stringFromInputStream(p.getInputStream()));
+                // wait for the result of the process
+                try {
+                    p.waitFor();
+                } catch (InterruptedException ex) {
+                }
             }
+            response.append(ERXFileUtilities.stringFromInputStream(p.getInputStream()));
 
         } finally {
-            if (p != null) {
 
-                InputStream is = p.getErrorStream();
-                if (is != null) error = ERXFileUtilities.stringFromInputStream(is);
-
-                if (log.isDebugEnabled()) {
-                    log.debug("response = " + response);
-                    log.debug("error = " + error);
+            try {
+                if (p != null && exitValue == 0) {
+                    InputStream is = p.getErrorStream();
+                    if (is != null) error = ERXFileUtilities.stringFromInputStream(is);
+    
+                    if (log.isDebugEnabled()) {
+                        log.debug("response = " + response);
+                        log.debug("error = " + error);
+                    }
+                    freeProcessResources(p);
                 }
-                freeProcessResources(p);
+            } catch (NullPointerException e) {
+                // p was null
             }
+
             if (outputFile != null) outputFile.delete();
         }
         return new Result(response.toString(), error);
 
-    }
-
-    /**
-     * Excecutes the specified command line commands. If envp is not null the
-     * environment variables are set before executing the command.
-     * 
-     * @param commands
-     *            the commands to execute like "ls -la" or "cp /tmp/file1
-     *            /tmp/file2" or "open /Applications/*.app"
-     * @param envp
-     *            a <code>String</code> array which represents the environment
-     *            variables like
-     *            <code>String[] envp = new String[]{"PATH=/usr/bin:/bin", "CVS_RSH=ssh"}</code>,
-     *            can be null
-     * @param dir
-     *            a <code>File</code> object representing the working
-     *            directory, can be null
-     * 
-     * @return the results from the processes that were executed
-     * 
-     * @exception IOException
-     *                if something went wrong
-     */
-    public final static Result executeCommandLineCommandWithArgumentsWithEnvVarsInWorkingDir(String[] command, String[] envp, File dir)
-            throws IOException {
-        return executeCommandLineCommandWithArgumentsWithEnvVarsInWorkingDir(command, envp, dir, false);
-    }
-
-    /**
-     * Excecutes the specified command line command. If envp is not null the
-     * environment variables are set before executing the command.
-     * 
-     * @param command
-     *            the command to execute like "ls -la" or "cp /tmp/file1
-     *            /tmp/file2" or "open /Applications/*.app"
-     * @param envp
-     *            a <code>String</code> array which represents the environment
-     *            variables like
-     *            <code>String[] envp = new String[]{"PATH=/usr/bin:/bin", "CVS_RSH=ssh"}</code>,
-     *            can be null
-     * @param dir
-     *            a <code>File</code> object representing the working
-     *            directory, can be null
-     * 
-     * @return the result from the process that was executed
-     * 
-     * @exception IOException
-     *                if something went wrong
-     */
-    public final static Result executeCommandLineCommandWithEnvVarsInWorkingDir(String command, String[] envp, File dir) throws IOException {
-        String[] commands = new String[1];
-        commands[0] = command;
-        return executeCommandLineCommandWithArgumentsWithEnvVarsInWorkingDir(commands, envp, dir);
     }
 
     /**
@@ -198,17 +213,21 @@ public class ERXRuntimeUtilities {
         if (p != null) {
             try {
                 if (p.getInputStream() != null) p.getInputStream().close();
-                if (p.getOutputStream() != null) p.getOutputStream().close();
-                if (p.getErrorStream() != null) p.getErrorStream().close();
-                p.destroy();
             } catch (IOException e) {
+                // do nothing here
             }
+            if (p.getOutputStream() != null) try {
+                p.getOutputStream().close();
+            } catch (IOException e) {
+                // do nothing here
+            }
+            if (p.getErrorStream() != null) try {
+                p.getErrorStream().close();
+            } catch (IOException e) {
+                // do nothing here
+            }
+            p.destroy();
         }
-    }
-
-    public static String escapePath(String path) {
-        path = ERXStringUtilities.replaceStringByStringInString(" ", "\\ ", path);
-        return path;
     }
 
     public static class Result {
@@ -219,5 +238,32 @@ public class ERXRuntimeUtilities {
             this.response = response;
             this.error = error;
         }
+    }
+
+    public static class ERXTimeoutException extends Exception {
+
+        public ERXTimeoutException(String string) {
+            super(string);
+        }
+    }
+
+    public static class TimeoutTimerTask extends TimerTask {
+        Process p;
+        boolean hasTimeout = false;
+        
+        public TimeoutTimerTask(Process p) {
+            this.p = p;
+        }
+        public boolean hasTimeout() {
+            return hasTimeout;
+        }
+        public void run() {
+            try {
+                p.exitValue();
+            } catch (IllegalThreadStateException e) {
+                hasTimeout = true;
+                p.destroy();
+            }
+        }   
     }
 }
