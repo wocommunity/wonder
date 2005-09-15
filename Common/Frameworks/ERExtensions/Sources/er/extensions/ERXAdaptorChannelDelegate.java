5fbd5b0c645bfd4f74e4182c2e676bc8f9f892b3
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2005-09-15 16:56:57 +0000

    better adaptor logging

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAdaptorChannelDelegate.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAdaptorChannelDelegate.java
@@ -1,5 +1,7 @@
 package er.extensions;
 
+import org.apache.log4j.*;
+
 import com.webobjects.foundation.*;
 import com.webobjects.eoaccess.*;
 
@@ -39,6 +41,7 @@ public class ERXAdaptorChannelDelegate {
         if(log == null) {
             log = ERXLogger.getERXLogger("er.extensions.ERXAdaptorChannelDelegate.sqlLogging");
         }
+        //log.setLevel(Level.DEBUG);
         String entityMatchPattern = ERXProperties.stringForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.entityMatchPattern", ".*");
         long millisecondsNeeded = System.currentTimeMillis() - _lastMilliseconds;
         String entityName = (expression.entity() != null ? expression.entity().name() : "Unknown");
@@ -48,23 +51,73 @@ public class ERXAdaptorChannelDelegate {
             long warnMilliseconds = ERXProperties.longForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.milliSeconds.warn", 500);
             long errorMilliseconds = ERXProperties.longForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.milliSeconds.error", 5000);
             int maxLength = ERXProperties.intForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.maxLength", 3000);
-
-            String description = "\"" + entityName  + "\"@" + channel.adaptorContext().hashCode() + " expression took " + millisecondsNeeded + " ms: " + expression.statement();
-            if(description.length() > maxLength) {
-                description = description.substring(0, maxLength);
-            }
+            boolean needsLog = false;
             if(millisecondsNeeded > errorMilliseconds) {
-                log.error(description, new RuntimeException("Statement running too long"));
+            	needsLog = true;
             } else if(millisecondsNeeded > warnMilliseconds) {
-                log.warn(description);
+            	needsLog = true;
             } else if(millisecondsNeeded > infoMilliseconds) {
-                if (log.isInfoEnabled()) {
-                    log.info(description);
-                }
+            	if (log.isInfoEnabled()) {
+            		needsLog = true;
+            	}
             } else if(millisecondsNeeded > debugMilliseconds) {
-                if (log.isDebugEnabled()) {
-                    log.debug(description);
-                }
+            	if (log.isDebugEnabled()) {
+            		needsLog = true;
+            	}
+            }
+            if(needsLog) {
+            	String description = "\"" + entityName  + "\"@" + channel.adaptorContext().hashCode() 
+            	+ " expression took " + millisecondsNeeded + " ms: " 
+            	+ expression.statement();
+            	StringBuffer sb = new StringBuffer();
+            	NSArray variables = expression.bindVariableDictionaries();
+            	int cnt = variables != null ? variables.count() : 0;
+            	if(cnt > 0) {
+            		sb.append(" withBindings: ");
+            		for(int i = 0; i < cnt; i++) {
+            			NSDictionary nsdictionary = (NSDictionary)variables.objectAtIndex(i);
+            			Object obj = nsdictionary.valueForKey("BindVariableValue");
+            			String attributeName = (String) nsdictionary.valueForKey("BindVariableName");
+            			if(obj instanceof String) {
+            				obj = EOSQLExpression.sqlStringForString((String) obj);
+            			} else if(obj instanceof NSData) {
+            				obj = expression.sqlStringForData((NSData)obj);
+            			} else {
+            				if(expression.entity() != null) {
+            					EOAttribute attribute = expression.entity().anyAttributeNamed(attributeName);
+            					if(attribute != null) {
+            						obj = expression.formatValueForAttribute(obj, attribute);
+            					}
+            				}
+            			}
+            			if(i != 0)
+            				sb.append(", ");
+            			sb.append(i + 1);
+            			sb.append(":");
+            			sb.append(obj);
+            			sb.append("[");
+            			sb.append(attributeName);
+            			sb.append("]");
+            		}
+            	}
+            	description = description + sb.toString();
+            	
+            	if(description.length() > maxLength) {
+            		description = description.substring(0, maxLength);
+            	}
+            	if(millisecondsNeeded > errorMilliseconds) {
+            		log.error(description, new RuntimeException("Statement running too long"));
+            	} else if(millisecondsNeeded > warnMilliseconds) {
+            		log.warn(description);
+            	} else if(millisecondsNeeded > infoMilliseconds) {
+            		if (log.isInfoEnabled()) {
+            			log.info(description);
+            		}
+            	} else if(millisecondsNeeded > debugMilliseconds) {
+            		if (log.isDebugEnabled()) {
+            			log.debug(description);
+            		}
+            	}
             }
         }
     }
