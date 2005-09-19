7c246ce109f4a966564addaa893e210076a4e655
davidteran <davidteran@8dc76679-1c23-de11-85cd-0050568e728b>
2005-09-19 18:00:14 +0000

    new framework EROraclePlugIn which adds some features to the original Oracle PlugIn like milliseconds for timestamps

---
+++ PlugIns/EROraclePlugIn/Sources/com/webobjects/jdbcadaptor/EROraclePlugIn.java
@@ -0,0 +1,37 @@
+package com.webobjects.jdbcadaptor;
+
+import com.webobjects.eoaccess.*;
+import com.webobjects.foundation.*;
+
+
+/** Overrides OraclePlugIn in order to provide the modified
+ * EROracleExpression class to EOF.
+ * 
+ * @author David Teran
+ *
+ */
+public class EROraclePlugIn extends OraclePlugIn {
+
+    public EROraclePlugIn(JDBCAdaptor jdbcadaptor) {
+        super(jdbcadaptor);
+    }
+
+    
+    /* (non-Javadoc)
+     * @see com.webobjects.jdbcadaptor.JDBCPlugIn#defaultExpressionClass()
+     */
+    public Class defaultExpressionClass() {
+        return EROracleExpression.class;
+    }
+
+    /* (non-Javadoc)
+     * @see com.webobjects.jdbcadaptor.JDBCPlugIn#createSynchronizationFactory()
+     */
+    public EOSynchronizationFactory createSynchronizationFactory() {
+        try {
+            return new EROracleSynchronizationFactory(adaptor());
+        } catch ( Exception e ) {
+            throw new NSForwardException(e, "Couldn't create synchronization factory");
+        }
+    }
+}
