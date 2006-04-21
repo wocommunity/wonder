17f69119f9f6f3913b9247a9045cc744b2c726a5
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-04-21 20:34:18 +0000

    ERIMAdaptor and the changes to generic.xml to build it as part of the main build.  Also fixed a reference to 'AjaxFramework' in the clean target that looks like it should have been 'Ajax' while I was in there.

---
+++ WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/BuddyOfflineException.java
@@ -0,0 +1,13 @@
+package er.imadaptor;
+
+public class BuddyOfflineException extends MessageException {
+
+  public BuddyOfflineException(String _message) {
+    super(_message);
+  }
+
+  public BuddyOfflineException(String _message, Throwable _cause) {
+    super(_message, _cause);
+  }
+
+}
