ab22f00216bd1aa2993d41529959de41ac0c2d2c
(no author) <(no author)@8dc76679-1c23-de11-85cd-0050568e728b>
2006-04-21 20:34:18 +0000

    This commit was manufactured by cvs2svn to create branch 'Wonder_3_0_0_branch'.

--- WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/IInstantMessengerFactory.java
+++
@@ -1,5 +0,0 @@
-package er.imadaptor;
-
-public interface IInstantMessengerFactory {
-  public IInstantMessenger createInstantMessenger(String _screenName, String _password);
-}
--- WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/IMessageListener.java
+++
@@ -1,5 +0,0 @@
-package er.imadaptor;
-
-public interface IMessageListener {
-  public void messageReceived(IInstantMessenger _instantMessenger, String _buddyName, String _message);
-}
