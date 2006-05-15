955aaa193b7ab8a3e3d37ee07cfc0f04714547c6
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-05-15 11:52:48 +0000

    Moved SQL time logging to ERXEOAccessUtilities and adaptor operation handling to ERXJDBCAdaptor

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAdaptorChannelDelegate.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAdaptorChannelDelegate.java
@@ -1,9 +1,5 @@
 package er.extensions;
 
-import java.io.*;
-
-import org.apache.log4j.*;
-
 import com.webobjects.foundation.*;
 import com.webobjects.eoaccess.*;
 
@@ -12,7 +8,7 @@ import com.webobjects.eoaccess.*;
  * used exceed the time specified in the system property
  * <code>er.extensions.ERXSQLExpressionTracker.trace.milliSeconds.[debug|info|warn|error]</code>,
  * and the entity name matches the regular expression
- * <code>er.extensions.ERXAdaptorChannelDelegate.trace.entityMatchPattern</code>
+ * <code>er.extensions.ERXSQLExpressionTracker.trace.entityMatchPattern</code>
  * then the SQL expression is logged together with the time used and the
  * parameters. <br />
  * NOTE: to get patched into EOF, this class registers itself for the
@@ -34,16 +30,9 @@ public class ERXAdaptorChannelDelegate {
 
 	private static ERXLogger log = ERXLogger.getERXLogger(ERXAdaptorChannelDelegate.class);
 
-	private static Boolean postAdaptorOperationNotifications = null;
-
 	private static ERXAdaptorChannelDelegate _delegate;
 
-	public static final String disabledForThreadKey = "DISABLED_FOR_THREAD";
-	
-    public static final String AdaptorOperationsDidPerformNotification = "AdaptorOperationsDidPerform";
- 
-	private ERXLogger sqlLoggingLogger = null;
-	private long _lastMilliseconds;
+    private long _lastMilliseconds;
 	
 
 	public static void setupDelegate() {
@@ -58,107 +47,7 @@ public class ERXAdaptorChannelDelegate {
 	}
 
 	public void adaptorChannelDidEvaluateExpression(EOAdaptorChannel channel, EOSQLExpression expression) {
-		if (sqlLoggingLogger == null) {
-			sqlLoggingLogger = ERXLogger.getERXLogger("er.extensions.ERXAdaptorChannelDelegate.sqlLogging");
-		}
-		// sqlLoggingLogger.setLevel(Level.DEBUG);
-		String entityMatchPattern = ERXProperties.stringForKeyWithDefault(
-				"er.extensions.ERXAdaptorChannelDelegate.trace.entityMatchPattern", ".*");
-		long millisecondsNeeded = System.currentTimeMillis() - _lastMilliseconds;
-		String entityName = (expression.entity() != null ? expression.entity().name() : "Unknown");
-		if (entityName.matches(entityMatchPattern)) {
-			long debugMilliseconds = ERXProperties.longForKeyWithDefault(
-					"er.extensions.ERXAdaptorChannelDelegate.trace.milliSeconds.debug", 5);
-			long infoMilliseconds = ERXProperties.longForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.milliSeconds.info",
-					100);
-			long warnMilliseconds = ERXProperties.longForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.milliSeconds.warn",
-					500);
-			long errorMilliseconds = ERXProperties.longForKeyWithDefault(
-					"er.extensions.ERXAdaptorChannelDelegate.trace.milliSeconds.error", 5000);
-			int maxLength = ERXProperties.intForKeyWithDefault("er.extensions.ERXAdaptorChannelDelegate.trace.maxLength", 3000);
-			boolean needsLog = false;
-			if (millisecondsNeeded > errorMilliseconds) {
-				needsLog = true;
-			} else if (millisecondsNeeded > warnMilliseconds) {
-				needsLog = true;
-			} else if (millisecondsNeeded > infoMilliseconds) {
-				if (sqlLoggingLogger.isInfoEnabled()) {
-					needsLog = true;
-				}
-			} else if (millisecondsNeeded > debugMilliseconds) {
-				if (sqlLoggingLogger.isDebugEnabled()) {
-					needsLog = true;
-				}
-			}
-			if (needsLog) {
-				String description = "\"" + entityName + "\"@" + channel.adaptorContext().hashCode() + " expression took "
-						+ millisecondsNeeded + " ms: " + expression.statement();
-				StringBuffer sb = new StringBuffer();
-				NSArray variables = expression.bindVariableDictionaries();
-				int cnt = variables != null ? variables.count() : 0;
-				if (cnt > 0) {
-					sb.append(" withBindings: ");
-					for (int i = 0; i < cnt; i++) {
-						NSDictionary nsdictionary = (NSDictionary) variables.objectAtIndex(i);
-						Object obj = nsdictionary.valueForKey("BindVariableValue");
-						String attributeName = (String) nsdictionary.valueForKey("BindVariableName");
-						if (obj instanceof String) {
-							obj = EOSQLExpression.sqlStringForString((String) obj);
-						} else if (obj instanceof NSData) {
-							// ak: this is just for logging, however we would
-							// like to get readable data
-							// in particular for PKs and with postgres this
-							// works.
-							// plain EOF is broken, though
-							try {
-								if (((NSData) obj).length() < 50) {
-									obj = expression.sqlStringForData((NSData) obj);
-								}
-							} catch (ArrayIndexOutOfBoundsException ex) {
-								// ignore, this is a bug in EOF
-							}
-							if (obj instanceof NSData) {
-								// produces very yucky output
-								obj = obj.toString();
-							}
-						} else {
-							if (expression.entity() != null) {
-								EOAttribute attribute = expression.entity().anyAttributeNamed(attributeName);
-								if (attribute != null) {
-									obj = expression.formatValueForAttribute(obj, attribute);
-								}
-							}
-						}
-						if (i != 0)
-							sb.append(", ");
-						sb.append(i + 1);
-						sb.append(":");
-						sb.append(obj);
-						sb.append("[");
-						sb.append(attributeName);
-						sb.append("]");
-					}
-				}
-				description = description + sb.toString();
-
-				if (description.length() > maxLength) {
-					description = description.substring(0, maxLength);
-				}
-				if (millisecondsNeeded > errorMilliseconds) {
-					sqlLoggingLogger.error(description, new RuntimeException("Statement running too long"));
-				} else if (millisecondsNeeded > warnMilliseconds) {
-					sqlLoggingLogger.warn(description);
-				} else if (millisecondsNeeded > infoMilliseconds) {
-					if (sqlLoggingLogger.isInfoEnabled()) {
-						sqlLoggingLogger.info(description);
-					}
-				} else if (millisecondsNeeded > debugMilliseconds) {
-					if (sqlLoggingLogger.isDebugEnabled()) {
-						sqlLoggingLogger.debug(description);
-					}
-				}
-			}
-		}
+		ERXEOAccessUtilities.logExpression(channel, expression, _lastMilliseconds);
 	}
 
 	public boolean adaptorChannelShouldEvaluateExpression(EOAdaptorChannel channel, EOSQLExpression expression) {
@@ -179,44 +68,4 @@ public class ERXAdaptorChannelDelegate {
 			channel.adaptorChannel().setDelegate(this);
 		}
 	}
-
-    /**
-     * Posts a AdaptorOperationsDidPerformNotification notification in case the operations were successfull.
-     * You can register a listener for this notification and stream the operations to a file or some other
-     * destination.
-     * @param channel
-     * @param adaptorOps
-     * @param exception
-     * @return
-     */
-	public Throwable adaptorChannelDidPerformOperations(EOAdaptorChannel channel, NSArray adaptorOps, Throwable exception) {
-		if (log.isDebugEnabled()) {
-			log.debug("adaptorChannelDidPerformOperations: count=" + adaptorOps.count() + ", exception = " + exception);
-		}
-        
-		if (exception == null) {
-			if (postAdaptorOperationNotifications()) {
-				NSNotificationCenter.defaultCenter().postNotification(AdaptorOperationsDidPerformNotification, adaptorOps);
-			}
-
-		}
-		return exception;
-	}
-
-
-	/**
-	 * @return <code>true</code> if the system property
-	 *         <code>er.extensions.ERXDatabaseContextDelegate.postAdaptorOperationNotifications</code>
-	 *         is set to true and if ERXThreadStorage.valueForKey(disabledForThreadKey) returns false or null, false otherwise.
-	 */
-	private boolean postAdaptorOperationNotifications() {
-		if (postAdaptorOperationNotifications == null) {
-			postAdaptorOperationNotifications = ERXProperties.booleanForKeyWithDefault(
-					"er.extensions.ERXAdaptorChannelDelegate.postAdaptorOperationNotifications", false) ? Boolean.TRUE : Boolean.FALSE;
-		}
-		Boolean b = (Boolean) ERXThreadStorage.valueForKey(disabledForThreadKey);
-		
-		return postAdaptorOperationNotifications.booleanValue() && (b == null ? true : !b.booleanValue());
-	}
-
 }
