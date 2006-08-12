2757c80e1f4a7544bbdb3e84ca41d2cf732c2ac1
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-08-12 05:09:13 +0000

    Added DragResize from Timo Hoepfner

--- Ajax/Ajax/Sources/er/ajax/AjaxComponent.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxComponent.java
@@ -41,7 +41,19 @@ public abstract class AjaxComponent extends WOComponent {
     }
     
     protected void addScriptResourceInHead(WOResponse _response, String _fileName) {
-      AjaxUtils.addScriptResourceInHead(context(), _response, _fileName);
+	AjaxUtils.addScriptResourceInHead(context(), _response, _fileName);
+    }
+
+    protected void addScriptResourceInHead(WOResponse _response, String _framework, String _fileName) {
+	AjaxUtils.addScriptResourceInHead(context(), _response, _framework, _fileName);
+    }
+    
+    protected void addStylesheetResourceInHead(WOResponse _response, String _fileName) {
+      AjaxUtils.addStylesheetResourceInHead(context(), _response, _fileName);
+    }
+    
+    protected void addStylesheetResourceInHead(WOResponse _response, String _framework, String _fileName) {
+	AjaxUtils.addStylesheetResourceInHead(context(), _response, _framework, _fileName);
     }
 
     /**
--- Ajax/AjaxExample/Sources/Application.java
+++ Ajax/AjaxExample/Sources/Application.java
@@ -2,6 +2,9 @@
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+
 import er.extensions.*;
 
 public class Application extends ERXApplication {
@@ -10,10 +13,16 @@ public class Application extends ERXApplication {
         ERXApplication.main(argv, Application.class);
     }
 
-    public Application() {
+    public WOComponent pageWithName(String s, WOContext wocontext) {
+		return super.pageWithName(s, wocontext);
+	}
+
+	public Application() {
         super();
         log.info("Welcome to " + this.name() + " !");
+        log.info("Welcome to " + System.getProperty("java.class.path").replace(':', '\n') + " !");
         /* ** put your initialization code in here ** */
         setAllowsConcurrentRequestHandling(true);
+        setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
     }
 }
