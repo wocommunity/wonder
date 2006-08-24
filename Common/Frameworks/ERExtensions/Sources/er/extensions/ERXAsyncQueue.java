f565f61159ad417429258c0e1a0d95e83b32aa05
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-08-24 20:15:44 +0000

    AsyncQueue added

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAsyncQueue.java
@@ -0,0 +1,43 @@
+package er.extensions;
+
+import java.util.*;
+
+/**
+ * Simple queue class that runs in a thread, waits and pulls jobs from a queue.
+ * To use it, implement the process(Object) method in your subclass.
+ * @author ak
+ *
+ */
+public abstract class ERXAsyncQueue extends Thread {
+
+	private Queue _jobs = new LinkedList();
+
+	public void enqueue(Object o) {
+		synchronized (_jobs) {
+			_jobs.add(o);
+			_jobs.notify();
+		}
+	}
+	
+	public final void run() {
+		boolean done = false;
+		try {
+			while(!done) {
+				Object o = null;
+				synchronized (_jobs) {
+					_jobs.wait();
+					if(!_jobs.isEmpty()) {
+						o = _jobs.remove();
+					}
+				}
+				if(o != null) {
+					process(o);
+				}
+			}
+		} catch (InterruptedException e) {
+			done = true;
+		}
+	}
+
+	public abstract void process(Object object);
+}
