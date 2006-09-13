e747bb0c8c2289c169681f41eab17ad458e84998
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-09-13 21:27:47 +0000

    Frameworks/ERCoreBusinessLogic/Resources/ERCoreBusinessLogic.eomodeld/ERCHelpText.fspec
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@4382 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/_ERCMessageAttachment.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/_ERCMessageAttachment.java
@@ -3,45 +3,45 @@
 // Created by eogenerator
 // DO NOT EDIT.  Make changes to ERCMessageAttachment.java instead.
 package er.corebusinesslogic;
+import com.webobjects.foundation.*;
+import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.*;
 import er.extensions.*;
+import java.util.*;
+import java.math.BigDecimal;
 
 public abstract class _ERCMessageAttachment extends ERXGenericRecord {
 
-    public _ERCMessageAttachment() {
-        super();
+    public interface Key  {
+        public static final String MIME_TYPE = "mimeType";
+        public static final String MAIL_MESSAGE = "mailMessage";
+        public static final String FILE_PATH = "filePath";  
     }
 
-    public static abstract class _ERCMessageAttachmentClazz extends er.extensions.ERXGenericRecord.ERXGenericRecordClazz {
+    public static abstract class _ERCMessageAttachmentClazz extends ERXGenericRecord.ERXGenericRecordClazz {
 
     }
 
 
     public String filePath() {
-        return (String)storedValueForKey("filePath");
+        return (String)storedValueForKey(Key.FILE_PATH);
     }
     public void setFilePath(String aValue) {
-        takeStoredValueForKey(aValue, "filePath");
+        takeStoredValueForKey(aValue, Key.FILE_PATH);
     }
 
     public String mimeType() {
-        return (String)storedValueForKey("mimeType");
+        return (String)storedValueForKey(Key.MIME_TYPE);
     }
     public void setMimeType(String aValue) {
-        takeStoredValueForKey(aValue, "mimeType");
+        takeStoredValueForKey(aValue, Key.MIME_TYPE);
     }
 
-    public ERCMailMessage mailMessage() {
-        return (ERCMailMessage)storedValueForKey("mailMessage");
+    public er.corebusinesslogic.ERCMailMessage mailMessage() {
+        return (er.corebusinesslogic.ERCMailMessage)storedValueForKey(Key.MAIL_MESSAGE);
     }
-
-    public void setMailMessage(ERCMailMessage aValue) {
-        takeStoredValueForKey(aValue, "mailMessage");
-    }
-    public void addToBothSidesOfMailMessage(ERCMailMessage object) {
-        addObjectToBothSidesOfRelationshipWithKey(object, "mailMessage");
-    }
-    public void removeFromBothSidesOfMailMessage(ERCMailMessage object) {
-        removeObjectFromBothSidesOfRelationshipWithKey(object, "mailMessage");
+    public void setMailMessage(er.corebusinesslogic.ERCMailMessage object) {
+        takeStoredValueForKey(object, Key.MAIL_MESSAGE);
     }
 
 }
