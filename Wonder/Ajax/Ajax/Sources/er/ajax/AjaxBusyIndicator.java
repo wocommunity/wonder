61ec726e17b631480702d178623e4339f31b8a75
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-09-18 03:03:40 +0000

    added support for default display style and custom busy image
    git-svn-id: file:///Users/q/working/wondersvn/trunk@4418 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Ajax/Ajax/Sources/er/ajax/AjaxBusyIndicator.java
+++ Wonder/Ajax/Ajax/Sources/er/ajax/AjaxBusyIndicator.java
@@ -3,31 +3,38 @@ package er.ajax;
 import com.webobjects.appserver.*;
 
 public class AjaxBusyIndicator extends AjaxComponent {
+  public AjaxBusyIndicator(WOContext context) {
+    super(context);
+  }
 
-    public AjaxBusyIndicator(WOContext context) {
-        super(context);
-    }
+  public boolean isStateless() {
+    return true;
+  }
 
-    public boolean isStateless() {
-        return true;
-    }
+  public boolean synchronizesVariablesWithBindings() {
+    return false;
+  }
 
-    public boolean synchronizesVariablesWithBindings() {
-        return false;
-    }
+  protected void addRequiredWebResources(WOResponse res) {
+    addScriptResourceInHead(res, "prototype.js");
+    addScriptResourceInHead(res, "scriptaculous.js");
+    addScriptResourceInHead(res, "effects.js");
+  }
 
-	protected void addRequiredWebResources(WOResponse res) {
-        addScriptResourceInHead(res, "prototype.js");
-        addScriptResourceInHead(res, "scriptaculous.js");
-        addScriptResourceInHead(res, "effects.js");
-	}
+  public String busyImage() {
+    return (String) valueForBinding("busyImage");
+  }
 
-	public String divID(){
-		return (String) valueForBinding("divID", "busy");
-	}
-	
-	protected WOActionResults handleRequest(WORequest request, WOContext context) {
-		return null;
-	}
+  public String busyImageFramework() {
+    return (String) valueForBinding("busyImageFramework");
+  }
+
+  public String divID() {
+    return (String) valueForBinding("divID", "busy");
+  }
+
+  protected WOActionResults handleRequest(WORequest request, WOContext context) {
+    return null;
+  }
 
 }
