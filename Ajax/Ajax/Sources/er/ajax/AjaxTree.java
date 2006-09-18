7649eb13bcbe34b7ad5eb2c2db3bf4d890907e23
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-09-18 03:04:55 +0000

    fixed tree example (uses nested divs instead of tables)

--- Ajax/Ajax/Sources/er/ajax/AjaxTree.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxTree.java
@@ -2,14 +2,14 @@ package er.ajax;
 
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSMutableSet;
 
 public class AjaxTree extends WOComponent {
   private AjaxTreeModel _treeModel;
-  private String _treeNodeRenderer;
 
   public AjaxTree(WOContext context) {
     super(context);
-    _treeNodeRenderer = AjaxSimpleTreeNodeRenderer.class.getName();
+    _treeModel = new AjaxTreeModel();
   }
 
   public void setTreeModel(AjaxTreeModel treeModel) {
@@ -20,21 +20,75 @@ public class AjaxTree extends WOComponent {
     return _treeModel;
   }
 
-  public void setRootNode(ITreeNode rootNode) {
-    if (_treeModel == null || _treeModel.rootTreeNode() != rootNode) {
-      _treeModel = new AjaxTreeModel(rootNode);
-    }
+  public String collapsedImage() {
+    return _treeModel.collapsedImage();
   }
 
-  public ITreeNode rootNode() {
-    return (_treeModel != null) ? _treeModel.rootTreeNode() : null;
+  public String collapsedImageFramework() {
+    return _treeModel.collapsedImageFramework();
+  }
+
+  public String expandedImage() {
+    return _treeModel.expandedImage();
+  }
+
+  public String expandedImageFramework() {
+    return _treeModel.expandedImageFramework();
+  }
+
+  public String leafImage() {
+    return _treeModel.leafImage();
+  }
+
+  public String leafImageFramework() {
+    return _treeModel.leafImageFramework();
+  }
+
+  public ITreeNode rootTreeNode() {
+    return _treeModel.rootTreeNode();
+  }
+
+  public void setCollapsedImage(String collapsedImage) {
+    _treeModel.setCollapsedImage(collapsedImage);
+  }
+
+  public void setCollapsedImageFramework(String collapsedImageFramework) {
+    _treeModel.setCollapsedImageFramework(collapsedImageFramework);
+  }
+
+  public void setExpanded(ITreeNode treeNode, boolean expanded) {
+    _treeModel.setExpanded(treeNode, expanded);
+  }
+
+  public void setExpandedImage(String expandedImage) {
+    _treeModel.setExpandedImage(expandedImage);
+  }
+
+  public void setExpandedImageFramework(String expandedImageFramework) {
+    _treeModel.setExpandedImageFramework(expandedImageFramework);
+  }
+
+  public void setExpandedTreeNodes(NSMutableSet expandedTreeNodes) {
+    _treeModel.setExpandedTreeNodes(expandedTreeNodes);
+  }
+
+  public void setLeafImage(String leafImage) {
+    _treeModel.setLeafImage(leafImage);
+  }
+
+  public void setLeafImageFramework(String leafImageFramework) {
+    _treeModel.setLeafImageFramework(leafImageFramework);
+  }
+
+  public void setRootTreeNode(ITreeNode rootTreeNode) {
+    _treeModel.setRootTreeNode(rootTreeNode);
   }
 
   public void setTreeNodeRenderer(String treeNodeRenderer) {
-    _treeNodeRenderer = treeNodeRenderer;
+    _treeModel.setTreeNodeRenderer(treeNodeRenderer);
   }
 
   public String treeNodeRenderer() {
-    return _treeNodeRenderer;
+    return _treeModel.treeNodeRenderer();
   }
 }
--- Ajax/Ajax/Sources/er/ajax/AjaxTreeModel.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxTreeModel.java
@@ -5,20 +5,40 @@ import com.webobjects.foundation.NSMutableSet;
 public class AjaxTreeModel {
   private ITreeNode _rootTreeNode;
   private NSMutableSet _expandedTreeNodes;
+  private String _treeNodeRenderer;
+  private String _collapsedImage;
+  private String _collapsedImageFramework;
+  private String _expandedImage;
+  private String _expandedImageFramework;
+  private String _leafImage;
+  private String _leafImageFramework;
 
-  public AjaxTreeModel(ITreeNode rootTreeNode) {
-    _rootTreeNode = rootTreeNode;
+  public AjaxTreeModel() {
     _expandedTreeNodes = new NSMutableSet();
+    _collapsedImage = "collapsed.gif";
+    _collapsedImageFramework = "Ajax";
+    _expandedImage = "expanded.gif";
+    _expandedImageFramework = "Ajax";
+    _leafImage = "leaf.gif";
+    _leafImageFramework = "Ajax";
+    _treeNodeRenderer = AjaxSimpleTreeNodeRenderer.class.getName();
   }
-  
+
+  public void setRootTreeNode(ITreeNode rootTreeNode) {
+    if (rootTreeNode != _rootTreeNode) {
+      _rootTreeNode = rootTreeNode;
+      _expandedTreeNodes.removeAllObjects();
+    }
+  }
+
   public ITreeNode rootTreeNode() {
     return _rootTreeNode;
   }
-  
+
   public boolean isExpanded(ITreeNode treeNode) {
     return _expandedTreeNodes.containsObject(treeNode);
   }
-  
+
   public void setExpanded(ITreeNode treeNode, boolean expanded) {
     if (expanded) {
       _expandedTreeNodes.addObject(treeNode);
@@ -26,5 +46,73 @@ public class AjaxTreeModel {
     else {
       _expandedTreeNodes.removeObject(treeNode);
     }
+  }
+  
+  public void collapseAll() {
+    _expandedTreeNodes.removeAllObjects();
+  }
+  
+  public void setTreeNodeRenderer(String treeNodeRenderer) {
+    _treeNodeRenderer = treeNodeRenderer;
+  }
+
+  public String treeNodeRenderer() {
+    return _treeNodeRenderer;
+  }
+
+  public String collapsedImage() {
+    return _collapsedImage;
+  }
+
+  public void setCollapsedImage(String collapsedImage) {
+    _collapsedImage = collapsedImage;
+  }
+
+  public String collapsedImageFramework() {
+    return _collapsedImageFramework;
+  }
+
+  public void setCollapsedImageFramework(String collapsedImageFramework) {
+    _collapsedImageFramework = collapsedImageFramework;
+  }
+
+  public String expandedImage() {
+    return _expandedImage;
+  }
+
+  public void setExpandedImage(String expandedImage) {
+    _expandedImage = expandedImage;
+  }
+
+  public String expandedImageFramework() {
+    return _expandedImageFramework;
+  }
+
+  public void setExpandedImageFramework(String expandedImageFramework) {
+    _expandedImageFramework = expandedImageFramework;
+  }
+
+  public NSMutableSet expandedTreeNodes() {
+    return _expandedTreeNodes;
+  }
+
+  public void setExpandedTreeNodes(NSMutableSet expandedTreeNodes) {
+    _expandedTreeNodes = expandedTreeNodes;
+  }
+
+  public String leafImage() {
+    return _leafImage;
+  }
+
+  public void setLeafImage(String leafImage) {
+    _leafImage = leafImage;
+  }
+
+  public String leafImageFramework() {
+    return _leafImageFramework;
+  }
+
+  public void setLeafImageFramework(String leafImageFramework) {
+    _leafImageFramework = leafImageFramework;
   }
 }
