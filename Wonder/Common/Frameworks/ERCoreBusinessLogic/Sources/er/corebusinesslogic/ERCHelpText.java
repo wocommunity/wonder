82a3fd159036c928c22117e9487e91a174376a41
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-09-19 18:08:07 +0000

    Display help text added
    git-svn-id: file:///Users/q/working/wondersvn/trunk@4435 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCHelpText.java
+++ Wonder/Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCHelpText.java
@@ -1,16 +1,28 @@
 package er.corebusinesslogic;
 import com.webobjects.eocontrol.EOEditingContext;
 
+import er.extensions.EOEnterpriseObjectClazz;
+import er.extensions.ERXEnterpriseObjectCache;
+
 public class ERCHelpText extends _ERCHelpText {
 
     private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ERCHelpText.class);
 
-    public static final ERCHelpTextClazz clazz = new ERCHelpTextClazz();
-    public static class ERCHelpTextClazz extends _ERCHelpTextClazz {/* more clazz methods here */}
-
     public final static String ENTITY = "ERCHelpText";
+    public static final ERCHelpTextClazz clazz = (ERCHelpTextClazz)EOEnterpriseObjectClazz.clazzForEntityNamed(ENTITY); 
+    
+    public static class ERCHelpTextClazz extends _ERCHelpTextClazz {
+    	
+       	ERXEnterpriseObjectCache cache = new ERXEnterpriseObjectCache(ENTITY, Key.KEY);
+       	
+       	public ERCHelpText helpTextForKey(EOEditingContext ec, String key) {
+       		return (ERCHelpText) cache.objectForKey(ec, key);
+       	}
+    }
 
-    public interface Key extends _ERCHelpText.Key {}
+    public interface Key extends _ERCHelpText.Key {
+    }
+
 
     /**
      * Intitializes the EO. This is called when an EO is created, not when it is 
