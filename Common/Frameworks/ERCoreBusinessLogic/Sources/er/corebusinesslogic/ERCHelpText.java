f0cccd930076713cb2712bd77d371dd74bf8935e
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-09-21 11:39:48 +0000

    help text now resolves

--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCHelpText.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCHelpText.java
@@ -1,7 +1,11 @@
 package er.corebusinesslogic;
+import com.webobjects.eoaccess.EOUtilities;
 import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOGlobalID;
 
 import er.extensions.EOEnterpriseObjectClazz;
+import er.extensions.ERXEC;
 import er.extensions.ERXEnterpriseObjectCache;
 
 public class ERCHelpText extends _ERCHelpText {
@@ -12,12 +16,28 @@ public class ERCHelpText extends _ERCHelpText {
     public static final ERCHelpTextClazz clazz = (ERCHelpTextClazz)EOEnterpriseObjectClazz.clazzForEntityNamed(ENTITY); 
     
     public static class ERCHelpTextClazz extends _ERCHelpTextClazz {
-    	
-       	ERXEnterpriseObjectCache cache = new ERXEnterpriseObjectCache(ENTITY, Key.KEY);
-       	
-       	public ERCHelpText helpTextForKey(EOEditingContext ec, String key) {
-       		return (ERCHelpText) cache.objectForKey(ec, key);
-       	}
+
+        ERXEnterpriseObjectCache cache = new ERXEnterpriseObjectCache(ENTITY, Key.KEY) {
+
+            protected void handleUnsuccessfulQueryForKey(Object key) {
+                EOEditingContext ec = ERXEC.newEditingContext();
+                EOGlobalID gid = NO_GID_MARKER;
+                ec.lock();
+                try {
+                    EOEnterpriseObject eo = EOUtilities.objectMatchingKeyAndValue(ec, ENTITY, Key.KEY, key);
+                    if(eo != null) {
+                        gid = ec.globalIDForObject(eo);
+                    }
+                } finally {
+                    ec.unlock();
+                }
+                cache().put(key, gid);
+            }
+        };
+        
+        public ERCHelpText helpTextForKey(EOEditingContext ec, String key) {
+            return (ERCHelpText) cache.objectForKey(ec, key);
+        }
     }
 
     public interface Key extends _ERCHelpText.Key {
