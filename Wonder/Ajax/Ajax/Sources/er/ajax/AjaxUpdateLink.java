14bb58506425faba00f0ca89b74de80b328e4e77
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-09-22 04:27:29 +0000

    replaceID binding on AjaxUpdateLink that lets you target the actionresults to a specific div
    git-svn-id: file:///Users/q/working/wondersvn/trunk@4457 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Ajax/Ajax/Sources/er/ajax/AjaxUpdateLink.java
+++ Wonder/Ajax/Ajax/Sources/er/ajax/AjaxUpdateLink.java
@@ -9,6 +9,7 @@ import com.webobjects.appserver.WOResponse;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSMutableDictionary;
+
 /**
  * Updates a region on the screen by creating a request to an action, then returning a script that in turn
  * creates an Ajax.Updater for the area.  If you do not provide an action binding, it will just update the
@@ -21,6 +22,8 @@ import com.webobjects.foundation.NSMutableDictionary;
  * @binding insertion JavaScript function to evaluate when the update takes place.
  * @binding ignoreActionResponse boolean defining if the action's response should be thrown away 
  *                  (useful when the same action has both Ajax and plain links)
+ * @binding updateContainerID the id of the AjaxUpdateContainer to update after performing this action 
+ * @binding replaceID the ID of the div (or other html element) whose contents will be replaced with the results of this action
  * @binding title title of the link
  * @binding style css style of the link
  * @binding class css class of the link
@@ -31,7 +34,7 @@ import com.webobjects.foundation.NSMutableDictionary;
 public class AjaxUpdateLink extends AjaxDynamicElement {
 
   public AjaxUpdateLink(String name, NSDictionary associations, WOElement children) {
-      super(name, associations, children);
+    super(name, associations, children);
   }
 
   public String onClick(WOContext context) {
@@ -39,22 +42,30 @@ public class AjaxUpdateLink extends AjaxDynamicElement {
     StringBuffer sb = new StringBuffer();
     if (associations().valueForKey("action") == null) {
       String updateContainerID = (String) valueForBinding("updateContainerID", context.component());
-      sb.append("new Ajax.Updater('"+updateContainerID+"', $('"+updateContainerID+"').getAttribute('updateUrl'), ");
+      sb.append("new Ajax.Updater('" + updateContainerID + "', $('" + updateContainerID + "').getAttribute('updateUrl'), ");
       AjaxOptions.appendToBuffer(options, sb, context);
       sb.append(")");
     }
     else {
       String actionUrl = context.componentActionURL();
-      sb.append("new Ajax.Request('");
-      sb.append(actionUrl);
-      sb.append("', ");
-      AjaxOptions.appendToBuffer(options, sb, context);
-      sb.append(")");
+      String replaceID = (String) valueForBinding("replaceID", context.component());
+      if (replaceID == null) {
+        sb.append("new Ajax.Request('");
+        sb.append(actionUrl);
+        sb.append("', ");
+        AjaxOptions.appendToBuffer(options, sb, context);
+        sb.append(")");
+      }
+      else {
+        sb.append("new Ajax.Updater('" + replaceID + "', '" + actionUrl + "', ");
+        AjaxOptions.appendToBuffer(options, sb, context);
+        sb.append(")");
+      }
     }
-    String onClick = (String)valueForBinding("onClick", context.component());
-    if(onClick != null) {
-        sb.append(";");
-        sb.append(onClick);
+    String onClick = (String) valueForBinding("onClick", context.component());
+    if (onClick != null) {
+      sb.append(";");
+      sb.append(onClick);
     }
     return sb.toString();
   }
@@ -72,56 +83,53 @@ public class AjaxUpdateLink extends AjaxDynamicElement {
 
     return options;
   }
-  
+
   public void appendToResponse(WOResponse response, WOContext context) {
-      WOComponent component = context.component();
-      boolean disabled = booleanValueForBinding("disabled", false, component);
-      Object stringValue = valueForBinding("string", component);
-      if(!disabled) {
-          response.appendContentString("<a ");
-          appendTagAttributeToResponse(response, "href", "#");
-          appendTagAttributeToResponse(response, "onclick", onClick(context) + "; return false;");
-          appendTagAttributeToResponse(response, "title", valueForBinding("title", component ));
-          appendTagAttributeToResponse(response, "value", valueForBinding("value", component ));
-          appendTagAttributeToResponse(response, "class", valueForBinding("class", component ));
-          appendTagAttributeToResponse(response, "style", valueForBinding("style", component ));
-          appendTagAttributeToResponse(response, "id", valueForBinding("id", component ));
-          // appendTagAttributeToResponse(response, "onclick", onClick(context));
-          response.appendContentString(">");
-      }
-      if(stringValue != null) {
-          response.appendContentHTMLString(stringValue.toString());
-      }
-      appendChildrenToResponse(response, context);
-      if(!disabled) {
-          response.appendContentString("</a>");
-      }
-      super.appendToResponse(response, context);
+    WOComponent component = context.component();
+    boolean disabled = booleanValueForBinding("disabled", false, component);
+    Object stringValue = valueForBinding("string", component);
+    if (!disabled) {
+      response.appendContentString("<a ");
+      appendTagAttributeToResponse(response, "href", "#");
+      appendTagAttributeToResponse(response, "onclick", onClick(context) + "; return false;");
+      appendTagAttributeToResponse(response, "title", valueForBinding("title", component));
+      appendTagAttributeToResponse(response, "value", valueForBinding("value", component));
+      appendTagAttributeToResponse(response, "class", valueForBinding("class", component));
+      appendTagAttributeToResponse(response, "style", valueForBinding("style", component));
+      appendTagAttributeToResponse(response, "id", valueForBinding("id", component));
+      // appendTagAttributeToResponse(response, "onclick", onClick(context));
+      response.appendContentString(">");
+    }
+    if (stringValue != null) {
+      response.appendContentHTMLString(stringValue.toString());
+    }
+    appendChildrenToResponse(response, context);
+    if (!disabled) {
+      response.appendContentString("</a>");
+    }
+    super.appendToResponse(response, context);
   }
 
   protected void addRequiredWebResources(WOResponse res, WOContext context) {
-      addScriptResourceInHead(context, res, "prototype.js");
-      addScriptResourceInHead(context, res, "scriptaculous.js");
+    addScriptResourceInHead(context, res, "prototype.js");
+    addScriptResourceInHead(context, res, "scriptaculous.js");
   }
-  
+
   protected WOActionResults handleRequest(WORequest request, WOContext context) {
-      WOComponent component = context.component();
-      WOActionResults results = (WOActionResults) valueForBinding("action", component);
-      if (results == null || booleanValueForBinding("ignoreActionResponse", false, component)) {
-          String updateContainerID = (String) valueForBinding("updateContainerID", component);
-          if(updateContainerID != null) {
-              WOResponse response = AjaxUtils.createResponse(context);
-              response.setHeader("text/javascript", "content-type");
-              response.setContent("new Ajax.Updater('"+updateContainerID+"', $('"+updateContainerID+"').getAttribute('updateUrl'), {"+
-                      " evalScripts: " + valueForBinding("evalScripts", "false", context.component()) + ", " +
-                      " insertion: " + valueForBinding("insertion", "function(receiver, response) {Element.update(receiver, response);}", context.component()) + " " +
-              "})");
-              results = response;
-              if(log.isDebugEnabled()) {
-                  log.debug("Response: " + response.contentString());
-              }
-          }
+    WOComponent component = context.component();
+    WOActionResults results = (WOActionResults) valueForBinding("action", component);
+    if (results == null || booleanValueForBinding("ignoreActionResponse", false, component)) {
+      String updateContainerID = (String) valueForBinding("updateContainerID", component);
+      if (updateContainerID != null) {
+        WOResponse response = AjaxUtils.createResponse(context);
+        response.setHeader("text/javascript", "content-type");
+        response.setContent("new Ajax.Updater('" + updateContainerID + "', $('" + updateContainerID + "').getAttribute('updateUrl'), {" + " evalScripts: " + valueForBinding("evalScripts", "false", context.component()) + ", " + " insertion: " + valueForBinding("insertion", "function(receiver, response) {Element.update(receiver, response);}", context.component()) + " " + "})");
+        results = response;
+        if (log.isDebugEnabled()) {
+          log.debug("Response: " + response.contentString());
+        }
       }
-      return results;
+    }
+    return results;
   }
 }
