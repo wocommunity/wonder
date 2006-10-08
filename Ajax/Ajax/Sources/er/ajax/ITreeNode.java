7c906aa79af3d5008b65d9b1aa973aa5562aaa64
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-10-08 18:23:08 +0000

    ajax tree rearchitected based on Anjo's ideas

--- Ajax/Ajax/Sources/er/ajax/ITreeNode.java
+++
@@ -1,9 +0,0 @@
-package er.ajax;
-
-import com.webobjects.foundation.NSArray;
-
-public interface ITreeNode {
-  public boolean isLeaf();
-  public ITreeNode parentTreeNode();
-  public NSArray childrenTreeNodes();
-}
