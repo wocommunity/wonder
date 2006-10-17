f517658df67f17a87e911de9dfc8a4ecf71ab132
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-10-17 13:16:54 +0000

    localization with query all pages

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryAnyField.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryAnyField.java
@@ -1,18 +1,28 @@
-//
-// ERD2WQueryAnyField.java: Class file for WO Component 'ERD2WQueryAnyField'
-// Project ERDirectToWeb
-//
-// Created by giorgio on 07/10/04
-//
 package er.directtoweb;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.directtoweb.D2WQueryAnyField;
 
+/**
+ * Localized QueryAnyField for the query all pages.
+ * 
+ *
+ * @author giorgio
+ */
 public class ERD2WQueryAnyField extends D2WQueryAnyField {
 
     public ERD2WQueryAnyField(WOContext context) {
         super(context);
+    }
+
+    public String relationshipKey() {
+        String result = propertyKey();
+        // AK: for whatever reason, the relationship key is "()" in this component and as
+        // I couldn't find the cause, I fixed it here
+        if("()".equals(result)) {
+            result = null;
+        }
+        return result;
     }
 
 }
