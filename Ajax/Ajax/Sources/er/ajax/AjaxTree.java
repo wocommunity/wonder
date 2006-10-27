00ba31ebae2c4ccf93d3bb3e4fb5d726bf26cdfb
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-10-27 15:26:13 +0000

    tree has an option delegate (AjaxTreeModel.Delegate) and no longer auto synchronizes bindings
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@4638 8dc76679-1c23-de11-85cd-0050568e728b

--- Ajax/Ajax/Sources/er/ajax/AjaxTree.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxTree.java
@@ -8,245 +8,185 @@ import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSMutableArray;
 
 public class AjaxTree extends WOComponent {
-  private String _id;
-  private AjaxTreeModel _treeModel;
-  private String _collapsedImage;
-  private String _collapsedImageFramework;
-  private String _expandedImage;
-  private String _expandedImageFramework;
-  private String _leafImage;
-  private String _leafImageFramework;
+	private AjaxTreeModel _treeModel;
 
-  private NSArray _nodes;
-  private int _nodeIndex;
-  private int _level;
-  private int _closeCount;
-  private Object _lastParent;
-  private Object _item;
+	private NSArray _nodes;
+	private int _nodeIndex;
+	private int _level;
+	private int _closeCount;
+	private Object _lastParent;
+	private Object _item;
 
-  public AjaxTree(WOContext context) {
-    super(context);
-    _collapsedImage = "collapsed.gif";
-    _collapsedImageFramework = "Ajax";
-    _expandedImage = "expanded.gif";
-    _expandedImageFramework = "Ajax";
-    _leafImage = "leaf.gif";
-    _leafImageFramework = "Ajax";
-  }
+	public AjaxTree(WOContext context) {
+		super(context);
+	}
 
-  public NSArray nodes() {
-    if (_nodes == null) {
-      NSMutableArray nodes = new NSMutableArray();
-      _fillInOpenNodes(treeModel().rootTreeNode(), nodes);
-      _nodes = nodes;
-    }
-    return _nodes;
-  }
+	public boolean synchronizesVariablesWithBindings() {
+		return false;
+	}
 
-  public void _setNodeIndex(int nodeIndex) {
-    _nodeIndex = nodeIndex;
-  }
+	public NSArray nodes() {
+		if (_nodes == null) {
+			NSMutableArray nodes = new NSMutableArray();
+			_fillInOpenNodes(treeModel().rootTreeNode(), nodes);
+			_nodes = nodes;
+		}
+		return _nodes;
+	}
 
-  public int _nodeIndex() {
-    return _nodeIndex;
-  }
+	public void _setNodeIndex(int nodeIndex) {
+		_nodeIndex = nodeIndex;
+	}
 
-  public void setAllExpanded(boolean allExpanded) {
-    treeModel().setAllExpanded(allExpanded);
-  }
+	public int _nodeIndex() {
+		return _nodeIndex;
+	}
 
-  public boolean isAllExpanded() {
-    return treeModel().isAllExpanded();
-  }
+	protected void _fillInOpenNodes(Object node, NSMutableArray nodes) {
+		nodes.add(node);
+		if (treeModel().isExpanded(node)) {
+			NSArray childrenTreeNodes = treeModel().childrenTreeNodes(node);
+			if (childrenTreeNodes != null) {
+				int childTreeNodeCount = childrenTreeNodes.count();
+				for (int childTreeNodeNum = 0; childTreeNodeNum < childTreeNodeCount; childTreeNodeNum++) {
+					Object childNode = childrenTreeNodes.objectAtIndex(childTreeNodeNum);
+					_fillInOpenNodes(childNode, nodes);
+				}
+			}
+		}
+	}
 
-  public void setRootExpanded(boolean rootExpanded) {
-    treeModel().setRootExpanded(rootExpanded);
-  }
+	public void appendToResponse(WOResponse aResponse, WOContext aContext) {
+		_level = 0;
+		_closeCount = 0;
 
-  public boolean isRootExpanded() {
-    return treeModel().isRootExpanded();
-  }
+		treeModel().setDelegate(valueForBinding("delegate"));
+		if (hasBinding("allExpanded")) {
+			treeModel().setAllExpanded(((Boolean) valueForBinding("allExpanded")).booleanValue());
+		}
+		if (hasBinding("rootExpanded")) {
+			treeModel().setRootExpanded(((Boolean) valueForBinding("rootExpanded")).booleanValue());
+		}
+		treeModel().setIsLeafKeyPath(stringValueForBinding("isLeafKeyPath", null));
+		treeModel().setParentTreeNodeKeyPath(stringValueForBinding("parentKeyPath", null));
+		treeModel().setChildrenTreeNodesKeyPath(stringValueForBinding("childrenKeyPath", null));
+		treeModel().setRootTreeNode(valueForBinding("root"));
 
-  protected void _fillInOpenNodes(Object node, NSMutableArray nodes) {
-    nodes.add(node);
-    if (treeModel().isExpanded(node)) {
-      NSArray childrenTreeNodes = treeModel().childrenTreeNodes(node);
-      if (childrenTreeNodes != null) {
-        int childTreeNodeCount = childrenTreeNodes.count();
-        for (int childTreeNodeNum = 0; childTreeNodeNum < childTreeNodeCount; childTreeNodeNum++) {
-          Object childNode = childrenTreeNodes.objectAtIndex(childTreeNodeNum);
-          _fillInOpenNodes(childNode, nodes);
-        }
-      }
-    }
-  }
+		super.appendToResponse(aResponse, aContext);
+	}
 
-  public void appendToResponse(WOResponse aResponse, WOContext aContext) {
-    _level = 0;
-    _closeCount = 0;
-    super.appendToResponse(aResponse, aContext);
-  }
+	public void setItem(Object item) {
+		if (item != _item) {
+			Object parent = treeModel().parentTreeNode(item);
+			int level;
+			if (parent == null) {
+				level = 0;
+			}
+			else if (parent == _item) {
+				level = _level + 1;
+			}
+			else if (parent != _lastParent) {
+				level = treeModel().level(item);
+			}
+			else {
+				level = _level;
+			}
+			if (_level > level) {
+				_closeCount = (_level - level);
+			}
+			else {
+				_closeCount = 0;
+			}
+			_lastParent = parent;
+			_level = level;
+			_item = item;
+			setValueForBinding(item, "item");
+		}
+	}
 
-  public void setItem(Object item) {
-    if (item != _item) {
-      Object parent = treeModel().parentTreeNode(item);
-      int level;
-      if (parent == null) {
-        level = 0;
-      }
-      else if (parent == _item) {
-        level = _level + 1;
-      }
-      else if (parent != _lastParent) {
-        level = treeModel().level(item);
-      }
-      else {
-        level = _level;
-      }
-      if (_level > level) {
-        _closeCount = (_level - level);
-      }
-      else {
-        _closeCount = 0;
-      }
-      _lastParent = parent;
-      _level = level;
-      _item = item;
-      setValueForBinding(item, "item");
-    }
-  }
+	public Object item() {
+		return _item;
+	}
 
-  public Object item() {
-    return _item;
-  }
+	public boolean isLeaf() {
+		return treeModel().isLeaf(_item);
+	}
 
-  public boolean isLeaf() {
-    return treeModel().isLeaf(_item);
-  }
+	public boolean isExpanded() {
+		return treeModel().isExpanded(_item);
+	}
 
-  public int _closeCount() {
-    return _closeCount;
-  }
+	public int _closeCount() {
+		return _closeCount;
+	}
 
-  public void setTreeModel(AjaxTreeModel treeModel) {
-    _treeModel = treeModel;
-  }
+	public void setTreeModel(AjaxTreeModel treeModel) {
+		_treeModel = treeModel;
+	}
 
-  public AjaxTreeModel treeModel() {
-    if (_treeModel == null) {
-      _treeModel = new AjaxTreeModel();
-    }
-    return _treeModel;
-  }
+	public AjaxTreeModel treeModel() {
+		if (_treeModel == null) {
+			_treeModel = new AjaxTreeModel();
+		}
+		return _treeModel;
+	}
 
-  public void setIsLeafKeyPath(String keyPath) {
-    treeModel().setIsLeafKeyPath(keyPath);
-  }
-  
-  public String isLeafKeyPath() {
-    return treeModel().isLeafKeyPath();
-  }
-  
-  public void setParentKeyPath(String keyPath) {
-    treeModel().setParentTreeNodeKeyPath(keyPath);
-  }
+	public String id() {
+		String id;
+		if (hasBinding("id")) {
+			id = (String) valueForBinding("id");
+		}
+		else {
+			id = AjaxUtils.toSafeElementID(context().elementID());
+		}
+		return id;
+	}
 
-  public String parentKeyPath() {
-    return treeModel().parentTreeNodeKeyPath();
-  }
+	protected String stringValueForBinding(String bindingName, String defaultValue) {
+		String value = defaultValue;
+		if (hasBinding(bindingName)) {
+			value = (String) valueForBinding(bindingName);
+		}
+		return value;
+	}
 
-  public void setChildrenKeyPath(String keyPath) {
-    treeModel().setChildrenTreeNodesKeyPath(keyPath);
-  }
+	public String collapsedImage() {
+		return stringValueForBinding("collapsedImage", "collapsed.gif");
+	}
 
-  public String childrenKeyPath() {
-    return treeModel().childrenTreeNodesKeyPath();
-  }
+	public String collapsedImageFramework() {
+		return stringValueForBinding("collapsedImageFramework", "Ajax");
+	}
 
-  public void setRoot(Object root) {
-    treeModel().setRootTreeNode(root);
-  }
+	public String expandedImage() {
+		return stringValueForBinding("expandedImage", "expanded.gif");
+	}
 
-  public Object root() {
-    return treeModel().rootTreeNode();
-  }
+	public String expandedImageFramework() {
+		return stringValueForBinding("expandedImageFramework", "Ajax");
+	}
 
-  public boolean isExpanded() {
-    return treeModel().isExpanded(_item);
-  }
+	public String leafImage() {
+		return stringValueForBinding("leafImage", "leaf.gif");
+	}
 
-  public void setId(String id) {
-    _id = id;
-  }
+	public String leafImageFramework() {
+		return stringValueForBinding("leafImageFramework", "Ajax");
+	}
 
-  public String id() {
-    if (_id == null) {
-      _id = AjaxUtils.toSafeElementID(context().elementID());
-    }
-    return _id;
-  }
+	public String _toggleFunctionName() {
+		return id() + "Toggle";
+	}
 
-  public String collapsedImage() {
-    return _collapsedImage;
-  }
+	public WOActionResults expand() {
+		treeModel().setExpanded(_item, true);
+		_nodes = null;
+		return null;
+	}
 
-  public void setCollapsedImage(String collapsedImage) {
-    _collapsedImage = collapsedImage;
-  }
-
-  public String collapsedImageFramework() {
-    return _collapsedImageFramework;
-  }
-
-  public void setCollapsedImageFramework(String collapsedImageFramework) {
-    _collapsedImageFramework = collapsedImageFramework;
-  }
-
-  public String expandedImage() {
-    return _expandedImage;
-  }
-
-  public void setExpandedImage(String expandedImage) {
-    _expandedImage = expandedImage;
-  }
-
-  public String expandedImageFramework() {
-    return _expandedImageFramework;
-  }
-
-  public void setExpandedImageFramework(String expandedImageFramework) {
-    _expandedImageFramework = expandedImageFramework;
-  }
-
-  public String leafImage() {
-    return _leafImage;
-  }
-
-  public void setLeafImage(String leafImage) {
-    _leafImage = leafImage;
-  }
-
-  public String leafImageFramework() {
-    return _leafImageFramework;
-  }
-
-  public void setLeafImageFramework(String leafImageFramework) {
-    _leafImageFramework = leafImageFramework;
-  }
-
-  public String _toggleFunctionName() {
-    return _id + "Toggle";
-  }
-
-  public WOActionResults expand() {
-    treeModel().setExpanded(_item, true);
-    _nodes = null;
-    return null;
-  }
-
-  public WOActionResults collapse() {
-    treeModel().setExpanded(_item, false);
-    _nodes = null;
-    return null;
-  }
+	public WOActionResults collapse() {
+		treeModel().setExpanded(_item, false);
+		_nodes = null;
+		return null;
+	}
 }
