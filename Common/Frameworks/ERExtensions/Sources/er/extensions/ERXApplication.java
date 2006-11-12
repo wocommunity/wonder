e87b248e9e28f28b746af3e87dfee0955696f946
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-11-12 21:39:32 +0000

    small changes to support Ajax optimizations

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXApplication.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXApplication.java
@@ -697,14 +697,37 @@ public abstract class ERXApplication extends WOApplication implements ERXGracefu
     }
 
     /**
+     * Checks if the page should not be stored in the cache 
+     */
+    public static boolean shouldNotStorePage(WOMessage message) {
+  	  return (message != null && (message.headerForKey(ERXSession.DONT_STORE_PAGE) != null || (message.userInfo() != null && message.userInfo().objectForKey(ERXSession.DONT_STORE_PAGE) != null)));
+    }
+
+    /**
+     * Checks if the page should not be stored in the cache 
+     */
+    public static boolean shouldNotStorePage(WOContext context) {
+      WORequest request = context.request();
+      WOResponse response = context.response();
+      // MS: The "AJAX_SUBMIT_BUTTON_NAME" check is a total hack, but if your page structure changes such that the form that
+      // is being submitted to is hidden, it ends up not notifying the system not to cache the page.
+      return (shouldNotStorePage(response) || shouldNotStorePage(request) || request.formValueForKey("AJAX_SUBMIT_BUTTON_NAME") != null);
+    }
+
+    /**
      * Overridden to allow for redirected responses.
      * @param request object
      * @param context object
      */
     public WOActionResults invokeAction(WORequest request, WOContext context) {
         WOActionResults results = super.invokeAction(request, context);
-        if(useComponentActionRedirection())
+        // MS: This is to support AjaxUpdateContainer.
+        if (results == null && ERXApplication.shouldNotStorePage(context)) {
+        	results = context.response();
+        }
+        if(useComponentActionRedirection()) {
             ERXComponentActionRedirector.createRedirector(results);
+        }
         return results;
     }
 
