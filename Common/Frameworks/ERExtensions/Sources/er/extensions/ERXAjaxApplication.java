c4b1793d2e67ba0bf7d962c73c3edd5c9330cb5d
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-11-13 16:58:09 +0000

    split out the ajax parts of application and session to make it easier for people to know what parts to copy if they aren't using ERXSession and ERXApplication
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@4713 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAjaxApplication.java
@@ -0,0 +1,52 @@
+package er.extensions;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOMessage;
+import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WOResponse;
+
+/**
+ * The ERXAjaxApplication is the part of ERXApplication that is modified to
+ * handle Ajax requests.  If you want to use the Ajax framework without
+ * using other parts of Project Wonder (i.e. ERXSession or ERXApplication),
+ * you should steal all of the code in ERXAjaxSession and ERXAjaxApplication.
+ * 
+ * @author mschrag
+ */
+public abstract class ERXAjaxApplication extends WOApplication {
+    /**
+     * Checks if the page should not be stored in the cache 
+     */
+    public static boolean shouldNotStorePage(WOMessage message) {
+  	  return (message != null && (message.headerForKey(ERXAjaxSession.DONT_STORE_PAGE) != null || (message.userInfo() != null && message.userInfo().objectForKey(ERXAjaxSession.DONT_STORE_PAGE) != null)));
+    }
+
+    /**
+     * Checks if the page should not be stored in the cache 
+     */
+    public static boolean shouldNotStorePage(WOContext context) {
+      WORequest request = context.request();
+      WOResponse response = context.response();
+      // MS: The "AJAX_SUBMIT_BUTTON_NAME" check is a total hack, but if your page structure changes such that the form that
+      // is being submitted to is hidden, it ends up not notifying the system not to cache the page.
+      return (shouldNotStorePage(response) || shouldNotStorePage(request) || request.formValueForKey("AJAX_SUBMIT_BUTTON_NAME") != null);
+    }
+
+    /**
+     * Overridden to allow for redirected responses.
+     * @param request object
+     * @param context object
+     */
+    public WOActionResults invokeAction(WORequest request, WOContext context) {
+        WOActionResults results = super.invokeAction(request, context);
+        // MS: This is to support AjaxUpdateContainer.
+        if (results == null && ERXAjaxApplication.shouldNotStorePage(context)) {
+        	results = context.response();
+        }
+        return results;
+    }
+}
+
+
