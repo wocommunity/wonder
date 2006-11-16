916c7fedc6f34356fd3edc89c2a23fd2ca948a8c
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-11-16 03:40:19 +0000

    removed requires for now ... I'm not sure we actually need it here and it was causing Chuck problems
    git-svn-id: file:///Users/q/working/wondersvn/trunk@4734 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Ajax/Ajax/Sources/er/ajax/Ajax.java
+++ Wonder/Ajax/Ajax/Sources/er/ajax/Ajax.java
@@ -2,21 +2,13 @@ package er.ajax;
 
 import com.webobjects.appserver.WOApplication;
 
-import er.extensions.ERXExtensions;
 import er.extensions.ERXFrameworkPrincipal;
 
 public class Ajax extends ERXFrameworkPrincipal {
 	
-	public static Class[] REQUIRES;
+	public static Class[] REQUIRES = new Class[0];
 
     static {
-    	try {
-    		REQUIRES = new Class[] { ERXExtensions.class };
-    	}
-    	catch (Throwable t) {
-    		// If you don't have ERExtesnsions
-    		REQUIRES = new Class[0];
-    	}
         setUpFrameworkPrincipalClass(Ajax.class);
     }
 
