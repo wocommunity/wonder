99ed800c5965eaa61c3b30e2c5afc857f5798bc0
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2006-11-24 12:35:32 +0000

    regex qualifier now also works with key paths

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRegExQualifier.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRegExQualifier.java
@@ -1,11 +1,11 @@
 package er.extensions;
 
-import com.webobjects.eoaccess.EOAttribute;
 import com.webobjects.eoaccess.EOEntity;
 import com.webobjects.eoaccess.EOQualifierSQLGeneration;
 import com.webobjects.eoaccess.EOSQLExpression;
 import com.webobjects.eocontrol.EOKeyValueQualifier;
 import com.webobjects.eocontrol.EOQualifier;
+import com.webobjects.eocontrol.EOQualifierVariable;
 import com.webobjects.foundation.NSKeyValueCodingAdditions;
 import com.webobjects.foundation.NSSelector;
 
@@ -19,57 +19,62 @@ import com.webobjects.foundation.NSSelector;
  */
 public class ERXRegExQualifier extends EOKeyValueQualifier {
 
-    public static String MatchesSelectorName = "matches";
-    public static NSSelector MatchesSelector = new NSSelector("matches", new Class[] {String.class});
+	public static String MatchesSelectorName = "matches";
+	public static NSSelector MatchesSelector = new NSSelector("matches", new Class[] {String.class});
 
-    static {
-        EOQualifierSQLGeneration.Support.setSupportForClass(new ERXRegExQualifier.Support(), ERXRegExQualifier.class);
-    }
+	static {
+		EOQualifierSQLGeneration.Support.setSupportForClass(new ERXRegExQualifier.Support(), ERXRegExQualifier.class);
+	}
 
-    public ERXRegExQualifier(String aKey, String aValue) {
-        super(aKey, MatchesSelector, aValue);
-    }
+	public ERXRegExQualifier(String aKey, String aValue) {
+		super(aKey, MatchesSelector, aValue);
+	}
 
-    public boolean evaluateWithObject(Object object) {
-        Object objectValue = NSKeyValueCodingAdditions.Utility.valueForKeyPath(object, key());
-        if(objectValue instanceof String) {
-            return ((String)objectValue).matches((String)value());
-        }
-        return false;
-    }
+	public boolean evaluateWithObject(Object object) {
+		Object objectValue = NSKeyValueCodingAdditions.Utility.valueForKeyPath(object, key());
+		if(objectValue instanceof String) {
+			return ((String)objectValue).matches((String)value());
+		}
+		return false;
+	}
 
-    public static class Support extends EOQualifierSQLGeneration._KeyValueQualifierSupport {
+	public static class Support extends EOQualifierSQLGeneration._KeyValueQualifierSupport {
 
-        public String sqlStringForSQLExpression(EOQualifier eoqualifier, EOSQLExpression e) {
-            String key = ((EOKeyValueQualifier)eoqualifier).key();
-            String value = (String) ((EOKeyValueQualifier)eoqualifier).value();
-            EOAttribute attribute = e.entity().attributeNamed(key);
-            String columnName = attribute.valueForSQLExpression(e);
-            value = e.formatStringValue(value);
-            String result = null;
-            //AK: yucky way to provide support for different DBs...
-            String clazzName = e.getClass().getName().toLowerCase();
-            if(clazzName.indexOf("mysql") > 0 ) {
-                result = columnName + " REGEXP " + value + "";
-            } else if(clazzName.indexOf("postgres") > 0 ) {
-                result = columnName + " ~* " + value + "";
-            } else if(clazzName.indexOf("oracle") > 0 ) {
-                result = "REGEXP_LIKE(" + columnName + ", " + value + ")";
-            } else  {
-                throw new UnsupportedOperationException("Regex not known for class: " + e.getClass().getName());
-            }
-            return result;
-        }
+		// AK: Should go somewhere else so we can create other enhanced qualifiers, too
+		
+		private String sqlStringForKeyValueQualifier(EOKeyValueQualifier qualifier, EOSQLExpression e) {
+			String key = qualifier.key();
+			String keyString = e.sqlStringForAttributeNamed(key);
+			if (keyString == null) {
+				throw new IllegalStateException("sqlStringForKeyValueQualifier: attempt to generate SQL for " + qualifier.getClass().getName() + " " + qualifier + " failed because attribute identified by key '" + key + "' was not reachable from from entity '" + e.entity().name() + "'");
+			}
+			Object qualifierValue = qualifier.value();
+			if (qualifierValue instanceof EOQualifierVariable) {
+				throw new IllegalStateException("sqlStringForKeyValueQualifier: attempt to generate SQL for " + qualifier.getClass().getName() + " " + qualifier + " failed because the qualifier variable '$" + ((EOQualifierVariable) qualifierValue).key() + "' is unbound.");
+			}
+			keyString = e.formatSQLString(keyString, e.entity()._attributeForPath(key).readFormat());
+			Object value = qualifierValue;
+			String valueString = e.sqlStringForValue(value, key);
+			
+			String result =  ERXSQLHelper.newSQLHelper(e).sqlForRegularExpressionQuery(keyString, valueString);
+			return result;
+		}
 
-        public EOQualifier schemaBasedQualifierWithRootEntity(EOQualifier eoqualifier, EOEntity eoentity) {
-            EOQualifier result = super.schemaBasedQualifierWithRootEntity(eoqualifier, eoentity);
-            return result;
-        }
 
-        public EOQualifier qualifierMigratedFromEntityRelationshipPath(EOQualifier eoqualifier, EOEntity eoentity, String s) {
-            EOQualifier result = super.qualifierMigratedFromEntityRelationshipPath(eoqualifier, eoentity, s);
-            return result;
-        }
-    }
+		public String sqlStringForSQLExpression(EOQualifier eoqualifier, EOSQLExpression e) {
+			String result = sqlStringForKeyValueQualifier((EOKeyValueQualifier)eoqualifier, e);
+			return result;
+		}
+
+		public EOQualifier schemaBasedQualifierWithRootEntity(EOQualifier qualifier, EOEntity entity) {
+			EOQualifier result = super.schemaBasedQualifierWithRootEntity(qualifier, entity);
+			return result;
+		}
+
+		public EOQualifier qualifierMigratedFromEntityRelationshipPath(EOQualifier qualifier, EOEntity entity, String s) {
+			EOQualifier result = super.qualifierMigratedFromEntityRelationshipPath(qualifier, entity, s);
+			return result;
+		}
+	}
 
 }
