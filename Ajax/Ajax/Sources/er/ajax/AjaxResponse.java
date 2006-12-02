c0198747cb8f06bdd6463a9e799f598a14fe6914
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-12-02 04:05:07 +0000

    added Ajax response appenders

--- Ajax/Ajax/Sources/er/ajax/AjaxResponse.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxResponse.java
@@ -1,10 +1,13 @@
 package er.ajax;
 
+import java.util.Enumeration;
+
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOApplication;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
+import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSMutableDictionary;
 
 /**
@@ -15,6 +18,28 @@ import com.webobjects.foundation.NSMutableDictionary;
  */
 public class AjaxResponse extends WOResponse {
 	public static final String AJAX_UPDATE_PASS = "_ajaxUpdatePass";
+	private static NSMutableArray _responseAppenders; 
+	
+	/**
+	 * Add a response appender to the list of response appender.  At the end of
+	 * every AjaxResponse, the AjaxResponseAppenders are given an opportunity to
+	 * tag along. For instance, if you have an area at the top of your pages that
+	 * show errors or notifications, you may want all of your ajax responses to have
+	 * a chance to trigger an update of this area, so you could register an 
+	 * AjaxResponseAppender that renders a javascript block that calls 
+	 * MyNotificationsUpdate() only if there are notifications to be shown. Without
+	 * response appenders, you would have to include a check in all of your 
+	 * components to do this. 
+	 * 
+	 * @param responseAppender the appender to add
+	 */
+	public static void addAjaxResponseAppender(AjaxResponseAppender responseAppender) {
+		if (_responseAppenders == null) {
+			_responseAppenders = new NSMutableArray();
+		}
+		_responseAppenders.addObject(responseAppender);
+	}
+	
 	private WORequest _request;
 	private WOContext _context;
 
@@ -34,6 +59,13 @@ public class AjaxResponse extends WOResponse {
 				userInfo.setObjectForKey(Boolean.TRUE, AjaxResponse.AJAX_UPDATE_PASS);
 				WOActionResults woactionresults = WOApplication.application().invokeAction(_request, _context);
 				_content.append(content);
+				if (_responseAppenders != null) {
+					Enumeration responseAppendersEnum = _responseAppenders.objectEnumerator();
+					while (responseAppendersEnum.hasMoreElements()) {
+						AjaxResponseAppender responseAppender = (AjaxResponseAppender) responseAppendersEnum.nextElement();
+						responseAppender.appendToResponse(this, _context);
+					}
+				}
 			}
 			finally {
 				_context._setSenderID(originalSenderID);
