69254884074daae84c29cc49fadbbb4eb73569ca
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-12-05 13:35:34 +0000

    Form.serialize in prototype serializes submit buttons.  This is totally wrong.  Without this fix, if you had an ajax response that contained a WOSubmitButton, it would always appear to be pressed when the next ajax response submitted
    git-svn-id: file:///Users/q/working/wondersvn/trunk@4812 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Ajax/Ajax/WebServerResources/wonder.js
@@ -0,0 +1,17 @@
+Object.extend(Form, {
+  serializeWithoutSubmits: function(form) {
+    var elements = Form.getElements($(form));
+    var queryComponents = new Array();
+
+    for (var i = 0; i < elements.length; i++) {
+			if (elements[i].type != 'submit') {
+	      var queryComponent = Form.Element.serialize(elements[i]);
+	      if (queryComponent) {
+	        queryComponents.push(queryComponent);
+				}
+			}
+    }
+
+    return queryComponents.join('&');
+  }
+});
