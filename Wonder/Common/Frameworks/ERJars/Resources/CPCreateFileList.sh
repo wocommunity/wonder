05bdab619da5eadf1f6f921efe4ae8d26c523bd8
(no author) <(no author)@8dc76679-1c23-de11-85cd-0050568e728b>
2006-12-05 22:30:59 +0000

    This commit was manufactured by cvs2svn to create tag 'Wonder_2_0_0_Branch_EXPORT'.
    git-svn-id: file:///Users/q/working/wondersvn/tags/Wonder_2_0_0_Branch_EXPORT@4814 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERJars/Resources/CPCreateFileList.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "Sources" ]
-then
-        find Sources -name '*.java' | xargs egrep -H '^package(.*);' |sed -e 's/package //' |sed -e 's/;//' > CPFileList.txt
-        find Sources -name '*.java' | xargs egrep -L '^package(.*);' |sed -e 's/$/:/' >> CPFileList.txt
-else
-        find . -name '*.java' | xargs egrep -H '^package(.*);' |sed -e 's/package //' |sed -e 's/;//' > CPFileList.txt
-        find . -name '*.java' | xargs egrep -L '^package(.*);' |sed -e 's/$/:/' >> CPFileList.txt
-fi
