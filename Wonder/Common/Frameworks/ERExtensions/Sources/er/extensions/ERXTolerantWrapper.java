84e7446d1fd9b004b46c2c1ad71de80212546855
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-12-11 18:08:16 +0000

    formatted and added support for explicitly specifying whether or not the wrapper should be tolerant
    git-svn-id: file:///Users/q/working/wondersvn/trunk@4848 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXTolerantWrapper.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXTolerantWrapper.java
@@ -4,68 +4,82 @@ import org.apache.log4j.Logger;
 
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOApplication;
+import com.webobjects.appserver.WOAssociation;
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 import com.webobjects.appserver._private.WODynamicGroup;
 import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableDictionary;
 
 /**
- * Wrapper for areas that might throw exceptions and catches them.
- * This is very useful when developing or other complex wraps of switch components, 
- * as most of the time, you are stuck with 
- * an exception and have no idea where it might come from.
+ * Wrapper for areas that might throw exceptions and catches them. This is very useful when developing or other complex
+ * wraps of switch components, as most of the time, you are stuck with an exception and have no idea where it might come
+ * from.
+ * 
  * @author ak
- *
  */
 public class ERXTolerantWrapper extends WODynamicGroup {
-
 	private static final Logger log = Logger.getLogger(ERXTolerantWrapper.class);
+	private WOAssociation _tolerant;
 
-	public ERXTolerantWrapper(String arg0, NSDictionary arg1, WOElement arg2) {
-		super(arg0, arg1, arg2);
+	public ERXTolerantWrapper(String name, NSDictionary associations, WOElement template) {
+		super(name, associations, template);
+		_tolerant = (WOAssociation) ((NSMutableDictionary) associations).removeObjectForKey("tolerant");
 	}
 
-	private boolean isTolerant() {
-		return !WOApplication.application().isCachingEnabled();
+	private boolean isTolerant(WOComponent component) {
+		boolean tolerant;
+		if (_tolerant != null) {
+			tolerant = _tolerant.booleanValueInComponent(component);
+		}
+		else {
+			tolerant = !WOApplication.application().isCachingEnabled();
+		}
+		return tolerant;
 	}
 
-	public void appendToResponse(WOResponse arg0, WOContext arg1) {
-		if(isTolerant()) {
+	public void appendToResponse(WOResponse response, WOContext context) {
+		if (isTolerant(context.component())) {
 			try {
-				super.appendToResponse(arg0, arg1);
-			} catch (RuntimeException ex) {
-				arg0.appendContentString(ex.toString());
+				super.appendToResponse(response, context);
+			}
+			catch (Throwable ex) {
+				response.appendContentString(ex.toString());
 				log.error(ex, ex);
 			}
-		} else {
-			super.appendToResponse(arg0, arg1);
+		}
+		else {
+			super.appendToResponse(response, context);
 		}
 	}
 
-	public WOActionResults invokeAction(WORequest arg0, WOContext arg1) {
-		if(isTolerant()) {
+	public WOActionResults invokeAction(WORequest request, WOContext context) {
+		if (isTolerant(context.component())) {
 			try {
-				return super.invokeAction(arg0, arg1);
-			} catch (RuntimeException ex) {
+				return super.invokeAction(request, context);
+			}
+			catch (Throwable ex) {
 				log.error(ex, ex);
 			}
 			return null;
-		} else {
-			return super.invokeAction(arg0, arg1);
 		}
+		return super.invokeAction(request, context);
 	}
 
-	public void takeValuesFromRequest(WORequest arg0, WOContext arg1) {
-		if(isTolerant()) {
+	public void takeValuesFromRequest(WORequest request, WOContext context) {
+		if (isTolerant(context.component())) {
 			try {
-				super.takeValuesFromRequest(arg0, arg1);
-			} catch (RuntimeException ex) {
+				super.takeValuesFromRequest(request, context);
+			}
+			catch (Throwable ex) {
 				log.error(ex, ex);
 			}
-		} else {
-			super.takeValuesFromRequest(arg0, arg1);
+		}
+		else {
+			super.takeValuesFromRequest(request, context);
 		}
 	}
 
