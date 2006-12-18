aae491ea00689b6ec37cd8b86f7e10b373449332
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-12-18 22:17:33 +0000

    added Google Maps example
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@4905 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Ajax/AjaxExample/Sources/AjaxGMapExample.java
@@ -0,0 +1,17 @@
+
+
+import com.webobjects.appserver.*;
+
+public class AjaxGMapExample extends WOComponent {
+
+	public String _address = "3741 Westerre Parkway Suite A, Richmond VA, 23233";
+	
+    public AjaxGMapExample(WOContext context) {
+        super(context);
+    }
+    
+    public WOActionResults search() {
+    	return null;
+    }
+    
+}
