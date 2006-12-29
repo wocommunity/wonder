5fa9a354a4634c1f48ffa081b403794f49d4d429
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2006-12-29 23:00:11 +0000

    bunch of fixes for CopyTask

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXJDBCUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXJDBCUtilities.java
@@ -18,13 +18,18 @@ import java.util.Enumeration;
 import org.apache.log4j.Logger;
 
 import com.webobjects.appserver.WOApplication;
+import com.webobjects.eoaccess.EOAdaptor;
 import com.webobjects.eoaccess.EOAdaptorChannel;
 import com.webobjects.eoaccess.EOAttribute;
 import com.webobjects.eoaccess.EOEntity;
 import com.webobjects.eoaccess.EOModel;
 import com.webobjects.eoaccess.EOModelGroup;
+import com.webobjects.eoaccess.EOQualifierSQLGeneration;
+import com.webobjects.eoaccess.EOSQLExpression;
+import com.webobjects.eoaccess.EOSQLExpressionFactory;
 import com.webobjects.eoaccess.EOSchemaGeneration;
 import com.webobjects.eoaccess.EOSynchronizationFactory;
+import com.webobjects.eocontrol.EOQualifier;
 import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableArray;
@@ -41,8 +46,12 @@ public class ERXJDBCUtilities {
 	public static NSTimestampFormatter TIMESTAMP_FORMATTER = new NSTimestampFormatter("%Y-%m-%d %H:%M:%S.%F");
 
 	public static class CopyTask {
+		protected NSDictionary sourceDictionary;
+		protected NSDictionary destDictionary;
 		protected Connection source;
 		protected Connection dest;
+		protected boolean _quoteSource;
+		protected boolean _quoteDestination;
 
 		protected NSMutableArray entities = new NSMutableArray();
 
@@ -58,14 +67,22 @@ public class ERXJDBCUtilities {
 			addEntity(anEntity);
 		}
 
+		public CopyTask() {
+		}
+
 		public void connect(NSDictionary aSourceConnectionDict, NSDictionary aDestConnectionDict) throws SQLException {
+			sourceDictionary = aSourceConnectionDict;
+			destDictionary = aDestConnectionDict;
 			source = connectionWithDictionary(aSourceConnectionDict);
 			dest = connectionWithDictionary(aDestConnectionDict);
+			_quoteSource = Boolean.valueOf((String)aSourceConnectionDict.objectForKey("quote")).booleanValue();
+			_quoteDestination = Boolean.valueOf((String)aDestConnectionDict.objectForKey("quote")).booleanValue();
 		}
 
 		public void connect(String sourcePrefix, String destPrefix) throws SQLException {
-			source = connectionWithDictionary(dictionaryFromPrefix(sourcePrefix));
-			dest = connectionWithDictionary(dictionaryFromPrefix(destPrefix));
+			sourceDictionary = dictionaryFromPrefix(sourcePrefix);
+			destDictionary = dictionaryFromPrefix(destPrefix);
+			connect(sourceDictionary, destDictionary);
 		}
 
 		private NSDictionary dictionaryFromPrefix(String prefix) {
@@ -87,7 +104,7 @@ public class ERXJDBCUtilities {
 			}
 		}
 
-		protected void addEntity(EOEntity entity) {
+		public void addEntity(EOEntity entity) {
 			entities.addObject(entity);
 		}
 
@@ -98,7 +115,9 @@ public class ERXJDBCUtilities {
 		public void run(boolean commitAtEnd) throws SQLException {
 			for (Enumeration models = entities.objectEnumerator(); models.hasMoreElements();) {
 				EOEntity entity = (EOEntity) models.nextElement();
-				copyEntity(entity);
+				if (!entity.isAbstractEntity()) {
+					copyEntity(entity);
+				}
 			}
 			if (commitAtEnd) {
 				commit();
@@ -113,8 +132,18 @@ public class ERXJDBCUtilities {
 			String username = (String) dict.objectForKey("username");
 			String password = (String) dict.objectForKey("password");
 			String driver = (String) dict.objectForKey("driver");
-			String url = (String) dict.objectForKey("url");
-			Boolean autoCommit = (Boolean) dict.objectForKey("autoCommit");
+			String url = (String) dict.objectForKey("URL");
+			if (url == null) {
+				url = (String) dict.objectForKey("url");
+			}
+			Boolean autoCommit;
+			Object autoCommitObj = dict.objectForKey("autoCommit");
+			if (autoCommitObj instanceof String) {
+				autoCommit = Boolean.valueOf((String)autoCommitObj);
+			}
+			else {
+				autoCommit = (Boolean)autoCommitObj;
+			}
 			boolean ac = autoCommit == null ? true : autoCommit.booleanValue();
 			// Boolean readOnly = (Boolean) dict.objectForKey("readOnly");
 			// boolean ro = readOnly == null ? false : readOnly.booleanValue();
@@ -155,7 +184,7 @@ public class ERXJDBCUtilities {
 				String column = att.columnName();
 				if (!ERXStringUtilities.stringIsNullOrEmpty(column)) {
 					if (quoteNames) {
-						columns.addObject("" + column + "");
+						columns.addObject("\"" + column + "\"");
 					}
 					else {
 						columns.addObject(column);
@@ -189,18 +218,39 @@ public class ERXJDBCUtilities {
 			String tableName = entity.externalName();
 			String[] columnNames = columnsFromAttributes(attributes, true);
 			String[] columnNamesWithoutQuotes = columnsFromAttributes(attributes, false);
-			String columns = columnsFromAttributesAsArray(attributes, true).componentsJoinedByString(", ");
-
+		
 			// build the select statement, this selects -all- rows
 			StringBuffer selectBuf = new StringBuffer();
 			selectBuf.append("select ");
-			selectBuf.append(columns).append(" from ").append(tableName).append(";");
+			selectBuf.append(columnsFromAttributesAsArray(attributes, _quoteSource).componentsJoinedByString(", ")).append(" from ");
+			if (_quoteSource) {
+				selectBuf.append("\"" + tableName + "\"");
+			}
+			else {
+				selectBuf.append(tableName);
+			}
+			EOQualifier qualifier = entity.restrictingQualifier();
+			if (qualifier != null) {
+			    EOAdaptor adaptor = EOAdaptor.adaptorWithName("JDBC");
+			    adaptor.setConnectionDictionary(sourceDictionary);
+				EOSQLExpressionFactory factory = adaptor.expressionFactory();
+				EOSQLExpression sqlExpression = factory.createExpression(entity);
+				String sqlString = EOQualifierSQLGeneration.Support._sqlStringForSQLExpression(qualifier, sqlExpression);
+				selectBuf.append(" where ").append(sqlString);
+			}
+			selectBuf.append(";");
 			String sql = selectBuf.toString();
-
 			Statement stmt = source.createStatement();
 
 			StringBuffer insertBuf = new StringBuffer();
-			insertBuf.append("insert into ").append("" + tableName + "").append(" (").append(columns).append(") values (");
+			insertBuf.append("insert into ");
+			if (_quoteDestination) {
+				insertBuf.append("\"" + tableName + "\"");
+			}
+			else {
+				insertBuf.append(tableName);
+			}
+			insertBuf.append(" (").append(columnsFromAttributesAsArray(attributes, _quoteDestination).componentsJoinedByString(", ")).append(") values (");
 			for (int i = columnNames.length; i-- > 0;) {
 				insertBuf.append("?");
 				if (i > 0) {
@@ -210,97 +260,94 @@ public class ERXJDBCUtilities {
 			insertBuf.append(");");
 
 			String insertSql = insertBuf.toString();
+			System.out.println("CopyTask.copyEntity: " + insertSql);
 			PreparedStatement upps = dest.prepareStatement(insertSql);
 
-			try {
-				ResultSet rows = stmt.executeQuery(sql);
-				// transfer each row by first setting the values
-				int rowsCount = 0;
-				while (rows.next()) {
-					rowsCount++;
-					if (rows.getRow() % 1000 == 0) {
-						log.info("table " + tableName + ", inserted " + rows.getRow() + " rows");
-					}
+			ResultSet rows = stmt.executeQuery(sql);
+			// transfer each row by first setting the values
+			int rowsCount = 0;
+			while (rows.next()) {
+				rowsCount++;
+				if (rows.getRow() % 1000 == 0) {
+					System.out.println("CopyTask.copyEntity: table " + tableName + ", inserted " + rows.getRow() + " rows");
+					log.info("table " + tableName + ", inserted " + rows.getRow() + " rows");
+				}
 
-					NSMutableSet tempfilesToDelete = new NSMutableSet();
-					// call upps.setInt, upps.setBinaryStream, ...
-					for (int i = 0; i < columnNamesWithoutQuotes.length; i++) {
-						// first we need to get the type
-						String columnName = columnNamesWithoutQuotes[i];
-						int type = rows.getMetaData().getColumnType(i + 1);
+				NSMutableSet tempfilesToDelete = new NSMutableSet();
+				// call upps.setInt, upps.setBinaryStream, ...
+				for (int i = 0; i < columnNamesWithoutQuotes.length; i++) {
+					// first we need to get the type
+					String columnName = columnNamesWithoutQuotes[i];
+					int type = rows.getMetaData().getColumnType(i + 1);
 
-						Object o = rows.getObject(columnName);
-						if (log.isDebugEnabled()) {
-							if (o != null) {
-								log.info("column=" + columnName + ", value class=" + o.getClass().getName() + ", value=" + o);
-							}
-							else {
-								log.info("column=" + columnName + ", value class unknown, value is null");
-							}
-						}
-
-						if (o instanceof Blob) {
-							Blob b = (Blob) o;
-							InputStream bis = b.getBinaryStream();
-							// stream this to a file, we need the length...
-							File tempFile = null;
-							try {
-								tempFile = File.createTempFile("TempJDBC", ".blob");
-								ERXFileUtilities.writeInputStreamToFile(bis, tempFile);
-							}
-							catch (IOException e5) {
-								log.error("could not create tempFile for row " + rows.getRow() + " and column " + columnName + ", setting column value to null!");
-								upps.setNull(i + 1, type);
-								if (tempFile != null)
-									if (!tempFile.delete())
-										tempFile.delete();
-
-								continue;
-							}
-							FileInputStream fis;
-							try {
-								fis = new FileInputStream(tempFile);
-							}
-							catch (FileNotFoundException e6) {
-								log.error("could not create FileInputStream from tempFile for row " + rows.getRow() + " and column " + columnName + ", setting column value to null!");
-								upps.setNull(i + 1, type);
-								if (tempFile != null)
-									if (!tempFile.delete())
-										tempFile.delete();
-
-								continue;
-							}
-							upps.setBinaryStream(i + 1, fis, (int) tempFile.length());
-							tempfilesToDelete.addObject(tempFile);
-						}
-						else if (o != null) {
-							upps.setObject(i + 1, o);
+					Object o = rows.getObject(columnName);
+					if (log.isDebugEnabled()) {
+						if (o != null) {
+							log.info("column=" + columnName + ", value class=" + o.getClass().getName() + ", value=" + o);
 						}
 						else {
-							upps.setNull(i + 1, type);
+							log.info("column=" + columnName + ", value class unknown, value is null");
 						}
 					}
-					upps.executeUpdate();
-					upps.clearParameters();
-					for (Enumeration e = tempfilesToDelete.objectEnumerator(); e.hasMoreElements();) {
-						File f = (File) e.nextElement();
-						if (!f.delete())
-							f.delete();
-					}
 
-					if (rows.getRow() % 1000 == 0) {
-						log.info("committing at count=" + rowsCount);
-						dest.commit();
-						log.info("committing done");
-					}
+					if (o instanceof Blob) {
+						Blob b = (Blob) o;
+						InputStream bis = b.getBinaryStream();
+						// stream this to a file, we need the length...
+						File tempFile = null;
+						try {
+							tempFile = File.createTempFile("TempJDBC", ".blob");
+							ERXFileUtilities.writeInputStreamToFile(bis, tempFile);
+						}
+						catch (IOException e5) {
+							log.error("could not create tempFile for row " + rows.getRow() + " and column " + columnName + ", setting column value to null!");
+							upps.setNull(i + 1, type);
+							if (tempFile != null)
+								if (!tempFile.delete())
+									tempFile.delete();
 
+							continue;
+						}
+						FileInputStream fis;
+						try {
+							fis = new FileInputStream(tempFile);
+						}
+						catch (FileNotFoundException e6) {
+							log.error("could not create FileInputStream from tempFile for row " + rows.getRow() + " and column " + columnName + ", setting column value to null!");
+							upps.setNull(i + 1, type);
+							if (tempFile != null)
+								if (!tempFile.delete())
+									tempFile.delete();
+
+							continue;
+						}
+						upps.setBinaryStream(i + 1, fis, (int) tempFile.length());
+						tempfilesToDelete.addObject(tempFile);
+					}
+					else if (o != null) {
+						upps.setObject(i + 1, o);
+					}
+					else {
+						upps.setNull(i + 1, type);
+					}
 				}
-				log.info("table " + tableName + ", inserted " + rowsCount + " rows");
-				rows.close();
+				upps.executeUpdate();
+				upps.clearParameters();
+				for (Enumeration e = tempfilesToDelete.objectEnumerator(); e.hasMoreElements();) {
+					File f = (File) e.nextElement();
+					if (!f.delete())
+						f.delete();
+				}
+
+				//if (rows.getRow() % 1000 == 0) {
+					//log.info("committing at count=" + rowsCount);
+					//dest.commit();
+					//log.info("committing done");
+				//}
+
 			}
-			catch (SQLException e2) {
-				log.error("could not get next from resultset", e2);
-			}
+			log.info("table " + tableName + ", inserted " + rowsCount + " rows");
+			rows.close();
 		}
 	}
 
@@ -312,6 +359,30 @@ public class ERXJDBCUtilities {
 
 	/**
 	 * Copies all rows from one database to another database. The tables must exist before calling this method.
+	 *
+	 * Example:
+	 * <pre>
+	 *     NSMutableDictionary sourceDict = new NSMutableDictionary();
+	 *     sourceDict.setObjectForKey("YourPassword", "password");
+	 *     sourceDict.setObjectForKey("YourUserName", "username");
+	 *     sourceDict.setObjectForKey("jdbc:FrontBase://127.0.0.1/YourSourceDatabase", "URL");
+	 *     sourceDict.setObjectForKey("com.frontbase.jdbc.FBJDriver", "driver");
+	 *     sourceDict.setObjectForKey(Boolean.FALSE.toString(), "autoCommit");
+	 *     sourceDict.setObjectForKey(Boolean.TRUE.toString(), "readOnly");
+	 *     sourceDict.setObjectForKey(Boolean.TRUE.toString(), "quote");
+	 *     
+	 *     NSMutableDictionary destDict = sourceDict.mutableClone();
+	 *     destDict.setObjectForKey("jdbc:postgresql://localhost/YourDestinationDatabase", "URL");
+	 *     destDict.setObjectForKey("YourPassword", "password");
+	 *     destDict.setObjectForKey("YourUserName", "username");
+	 *     destDict.setObjectForKey("org.postgresql.Driver", "driver");
+	 *     destDict.setObjectForKey(Boolean.FALSE.toString(), "autoCommit");
+	 *     destDict.setObjectForKey(Boolean.FALSE.toString(), "readOnly");
+	 *     destDict.setObjectForKey(Boolean.FALSE.toString(), "quote");
+	 *     
+	 *     EOModel model = EOModelGroup.defaultGroup().modelNamed("YourModelName");
+	 *     ERXJDBCUtilities._copyDatabaseDefinedByEOModelAndConnectionDictionaryToDatabaseWithConnectionDictionary(model, sourceDict, destDict);
+	 * </pre>
 	 * 
 	 * @param entity
 	 *            the EOEntity which is related to the table which we want to copy
@@ -327,6 +398,7 @@ public class ERXJDBCUtilities {
 	 *            <code>com.frontbase.jdbc.FBJDriver</code> , for PostgreSQL its <code>org.postgresql.Driver</code>
 	 *            <li>autoCommit, a Boolean defining if autoCommit should be on or off, default is true
 	 *            <li>readOnly, a Boolean defining if the Connection is readOnly or not, default is false. Its a good
+	 *            <li>quote, a Boolean defining if the table and field names should be "quoted"
 	 *            idea to make the sourceDict readOnly, because one does not write.
 	 *            </ol>
 	 * @param destDict
@@ -335,7 +407,6 @@ public class ERXJDBCUtilities {
 	 *            a boolean which defines if the destination Connection should get a commit at the end. Set this to
 	 *            false if you want to transfer multiple tables with primary key - foreign key dependencies
 	 */
-
 	public static void _copyDatabaseDefinedByEOModelAndConnectionDictionaryToDatabaseWithConnectionDictionary(EOModel m, NSDictionary sourceDict, NSDictionary destDict) {
 		try {
 			CopyTask task = new CopyTask(m);
