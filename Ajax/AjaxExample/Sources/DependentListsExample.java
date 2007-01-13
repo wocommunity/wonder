179a831afd66bc3b35d85f824103648003d46aa7
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-01-13 23:44:22 +0000

    multiple dependent lists
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5006 8dc76679-1c23-de11-85cd-0050568e728b

--- Ajax/AjaxExample/Sources/DependentListsExample.java
+++ Ajax/AjaxExample/Sources/DependentListsExample.java
@@ -6,64 +6,145 @@ import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSMutableArray;
 
 public class DependentListsExample extends WOComponent {
-  private NSMutableArray _states;
-  public State _repetitionState;
-  public County _repetitionCounty;
-  public State _selectedState;
-  public State _selectedState2;
-  public County _selectedCounty;
-  public County _selectedCounty2;
-  public County _selectedCounty3;
-  public String _address;
+	private NSMutableArray _states;
+	public State _repetitionState;
+	public County _repetitionCounty;
+	public Street _repetitionStreet;
 
-  public DependentListsExample(WOContext context) {
-    super(context);
-    _states = new NSMutableArray();
-    for (int stateNum = 0; stateNum < 30; stateNum++) {
-      State state = new State("State " + stateNum);
-      for (int countyNum = 0; countyNum < 30; countyNum++) {
-        County county = new County("State " + state.name() + " - County " + countyNum);
-        state.addCounty(county);
-      }
-      _states.addObject(state);
-    }
-  }
+	private State _selectedState;
+	private County _selectedCounty;
+	private Street _selectedStreet;
 
-  public NSArray states() {
-    return _states;
-  }
+	public State _selectedState2;
+	public County _selectedCounty2;
 
-  public static class State {
-    private String _name;
-    private NSMutableArray _counties;
+	public String _address;
 
-    public State(String name) {
-      _name = name;
-      _counties = new NSMutableArray();
-    }
+	public DependentListsExample(WOContext context) {
+		super(context);
+		_states = new NSMutableArray();
+		for (int stateNum = 0; stateNum < 30; stateNum++) {
+			State state = new State("State " + stateNum);
+			for (int countyNum = 0; countyNum < 30; countyNum++) {
+				County county = new County(state, "State " + state.name() + " - County " + countyNum);
+				for (int streetNum = 0; streetNum < 30; streetNum++) {
+					Street street = new Street(county, "State " + state.name() + " - County " + countyNum + " - Street " + streetNum);
+					county.addStreet(street);
+				}
+				state.addCounty(county);
+			}
+			_states.addObject(state);
+		}
+	}
 
-    public String name() {
-      return _name;
-    }
+	public void setSelectedState(State selectedState) {
+		_selectedState = selectedState;
+		System.out.println("DependentListsExample.setSelectedState: " + _selectedState);
+	}
 
-    public void addCounty(County county) {
-      _counties.addObject(county);
-    }
+	public State getSelectedState() {
+		return _selectedState;
+	}
 
-    public NSArray counties() {
-      return _counties;
-    }
-  }
+	public void setSelectedCounty(County selectedCounty) {
+		_selectedCounty = selectedCounty;
+		System.out.println("DependentListsExample.setSelectedCounty: " + _selectedCounty);
+	}
 
-  public static class County {
-    public String _name;
+	public County getSelectedCounty() {
+		return _selectedCounty;
+	}
 
-    public County(String name) {
-      _name = name;
-    }
+	public void setSelectedStreet(Street selectedStreet) {
+		_selectedStreet = selectedStreet;
+		System.out.println("DependentListsExample.setSelectedStreet: " + _selectedStreet);
+	}
 
-    public String name() {
-      return _name;
-    }
-  }
+	public Street getSelectedStreet() {
+		return _selectedStreet;
+	}
+
+	public NSArray states() {
+		return _states;
+	}
+
+	public static class State {
+		private String _name;
+		private NSMutableArray _counties;
+
+		public State(String name) {
+			_name = name;
+			_counties = new NSMutableArray();
+		}
+
+		public String name() {
+			return _name;
+		}
+
+		public void addCounty(County county) {
+			_counties.addObject(county);
+		}
+
+		public NSArray counties() {
+			return _counties;
+		}
+		
+		public String toString() {
+			return "[State: name = " + _name + "]"; 
+		}
+	}
+
+	public static class County {
+		private State _state;
+		private String _name;
+		private NSMutableArray _streets;
+
+		public County(State state, String name) {
+			_state = state;
+			_name = name;
+			_streets = new NSMutableArray();
+		}
+		
+		public State state() {
+			return _state;
+		}
+
+		public String name() {
+			return _name;
+		}
+
+		public void addStreet(Street street) {
+			_streets.addObject(street);
+		}
+
+		public NSArray streets() {
+			return _streets;
+		}
+		
+		public String toString() {
+			return "[County: state = " + _state + "; name = " + _name + "]"; 
+		}
+	}
+
+	public static class Street {
+		private County _county;
+		private String _name;
+
+		public Street(County county, String name) {
+			_county = county;
+			_name = name;
+		}
+		
+		public County county() {
+			return _county;
+		}
+
+		public String name() {
+			return _name;
+		}
+		
+		public String toString() {
+			return "[Street: county = " + _county + "; name = " + _name + "]"; 
+		}
+	}
 }
