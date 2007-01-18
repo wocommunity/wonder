d0d01e870b8d813222e72b9f58fe4df2f2a57890
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-01-18 18:01:06 +0000

    reformat
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5059 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXComponentUtilities.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXComponentUtilities.java
@@ -20,8 +20,7 @@ import com.webobjects.foundation.NSMutableDictionary;
  */
 public class ERXComponentUtilities {
 	/**
-	 * Returns a query parameter dictionary from a set of ?key=association
-	 * WOAssociation dictionary.
+	 * Returns a query parameter dictionary from a set of ?key=association WOAssociation dictionary.
 	 * 
 	 * @param associations
 	 *            the set of associations
@@ -35,8 +34,7 @@ public class ERXComponentUtilities {
 	}
 
 	/**
-	 * Returns a query parameter dictionary from a set of ?key=association
-	 * WOAssociation dictionary.
+	 * Returns a query parameter dictionary from a set of ?key=association WOAssociation dictionary.
 	 * 
 	 * @param associations
 	 *            the set of associations
@@ -66,8 +64,7 @@ public class ERXComponentUtilities {
 	}
 
 	/**
-	 * Returns the set of ?key=value associations from an associations
-	 * dictionary.
+	 * Returns the set of ?key=value associations from an associations dictionary.
 	 * 
 	 * @param associations
 	 *            the associations to enumerate
@@ -77,10 +74,8 @@ public class ERXComponentUtilities {
 	}
 
 	/**
-	 * Returns the set of ?key=value associations from an associations
-	 * dictionary. If removeQueryParameterAssociations is true, the
-	 * corresponding entries will be removed from the associations dictionary
-	 * that was passed in.
+	 * Returns the set of ?key=value associations from an associations dictionary. If removeQueryParameterAssociations
+	 * is true, the corresponding entries will be removed from the associations dictionary that was passed in.
 	 * 
 	 * @param associations
 	 *            the associations to enumerate
@@ -114,79 +109,91 @@ public class ERXComponentUtilities {
 	/**
 	 * Returns the boolean value of a binding.
 	 * 
-	 * @param component the component
-	 * @param bindingName the name of the boolean binding
+	 * @param component
+	 *            the component
+	 * @param bindingName
+	 *            the name of the boolean binding
 	 * @return a boolean
 	 */
 	public static boolean booleanValueForBinding(WOComponent component, String bindingName) {
-		Boolean value = (Boolean)component.valueForBinding(bindingName);
+		Boolean value = (Boolean) component.valueForBinding(bindingName);
 		return value != null && value.booleanValue();
 	}
-	
+
 	/**
 	 * Returns the URL of the html template for the given component name.
 	 * 
-	 * @param componentName the name of the component to load a template for (without the .wo)
-	 * @param languages the list of languages to use for finding components
+	 * @param componentName
+	 *            the name of the component to load a template for (without the .wo)
+	 * @param languages
+	 *            the list of languages to use for finding components
 	 * @return the URL to the html template (or null if there isn't one)
 	 */
 	public static URL htmlTemplateUrl(String componentName, NSArray languages) {
 		return ERXComponentUtilities.templateUrl(componentName, "html", languages);
 	}
-	
+
 	/**
 	 * Returns the URL of the template for the given component name.
 	 * 
-	 * @param componentName the name of the component to load a template for (without the .wo)
-	 * @param extension the file extension of the template (without the dot -- i.e. "html") 
-	 * @param languages the list of languages to use for finding components
+	 * @param componentName
+	 *            the name of the component to load a template for (without the .wo)
+	 * @param extension
+	 *            the file extension of the template (without the dot -- i.e. "html")
+	 * @param languages
+	 *            the list of languages to use for finding components
 	 * @return the URL to the template (or null if there isn't one)
 	 */
 	public static URL templateUrl(String componentName, String extension, NSArray languages) {
-        String htmlPathName = componentName + ".wo/" + componentName + "." + extension;
-        WOResourceManager resourcemanager = WOApplication.application().resourceManager();
-        URL templateUrl = resourcemanager.pathURLForResourceNamed(htmlPathName, null, languages);
-        if (templateUrl == null) {
-          NSArray frameworkBundles = NSBundle.frameworkBundles();
-          if (frameworkBundles != null) {
-            Enumeration frameworksEnum = frameworkBundles.objectEnumerator();
-            while (templateUrl == null && frameworksEnum.hasMoreElements()) {
-              NSBundle frameworkBundle = (NSBundle) frameworksEnum.nextElement();
-              templateUrl = resourcemanager.pathURLForResourceNamed(htmlPathName, frameworkBundle.name(), languages);
-            }
-          }
-        }
-        return templateUrl;
+		String htmlPathName = componentName + ".wo/" + componentName + "." + extension;
+		WOResourceManager resourcemanager = WOApplication.application().resourceManager();
+		URL templateUrl = resourcemanager.pathURLForResourceNamed(htmlPathName, null, languages);
+		if (templateUrl == null) {
+			NSArray frameworkBundles = NSBundle.frameworkBundles();
+			if (frameworkBundles != null) {
+				Enumeration frameworksEnum = frameworkBundles.objectEnumerator();
+				while (templateUrl == null && frameworksEnum.hasMoreElements()) {
+					NSBundle frameworkBundle = (NSBundle) frameworksEnum.nextElement();
+					templateUrl = resourcemanager.pathURLForResourceNamed(htmlPathName, frameworkBundle.name(), languages);
+				}
+			}
+		}
+		return templateUrl;
 	}
-	
+
 	/**
 	 * Returns the contents of the html template for the given component name as a string.
 	 * 
-	 * @param componentName the name of the component to load a template for (without the .wo)
-	 * @param languages the list of languages to use for finding components
+	 * @param componentName
+	 *            the name of the component to load a template for (without the .wo)
+	 * @param languages
+	 *            the list of languages to use for finding components
 	 * @return the string contents of the html template (or null if there isn't one)
 	 */
 	public static String htmlTemplate(String componentName, NSArray languages) throws IOException {
 		return ERXComponentUtilities.template(componentName, "html", languages);
 	}
-	
+
 	/**
 	 * Returns the contents of the template for the given component name as a string.
 	 * 
-	 * @param componentName the name of the component to load a template for (without the .wo)
-	 * @param extension the file extension of the template (without the dot -- i.e. "html") 
-	 * @param languages the list of languages to use for finding components
+	 * @param componentName
+	 *            the name of the component to load a template for (without the .wo)
+	 * @param extension
+	 *            the file extension of the template (without the dot -- i.e. "html")
+	 * @param languages
+	 *            the list of languages to use for finding components
 	 * @return the string contents of the template (or null if there isn't one)
 	 */
 	public static String template(String componentName, String extension, NSArray languages) throws IOException {
 		String template;
 		URL templateUrl = ERXComponentUtilities.templateUrl(componentName, extension, languages);
-        if (templateUrl == null) {
-          template = null;
-        }
-        else {
-          template = ERXStringUtilities.stringFromURL(templateUrl);
-        }
-        return template;
+		if (templateUrl == null) {
+			template = null;
+		}
+		else {
+			template = ERXStringUtilities.stringFromURL(templateUrl);
+		}
+		return template;
 	}
 }
