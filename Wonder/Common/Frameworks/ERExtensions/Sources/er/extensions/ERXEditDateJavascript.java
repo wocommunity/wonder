bd8c89938f72c1c351b6b7aad5905fac797f15cb
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-01-21 13:00:28 +0000

    made stateless
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5068 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEditDateJavascript.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEditDateJavascript.java
@@ -8,55 +8,70 @@ package er.extensions;
 
 import org.apache.log4j.Logger;
 
-import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
-import com.webobjects.appserver.WOResponse;
 
-public class ERXEditDateJavascript extends WOComponent {
-    static final Logger log = Logger.getLogger(ERXEditDateJavascript.class);
-    private static String _datePickerJavaScriptUrl;
-    
-    protected int i = 0;
-    public String dateString;
-    public String dateformat = ERXTimestampFormatter.DEFAULT_PATTERN;
+public class ERXEditDateJavascript extends ERXStatelessComponent {
+	static final Logger log = Logger.getLogger(ERXEditDateJavascript.class);
+	private static String _datePickerJavaScriptUrl;
 
-    public ERXEditDateJavascript(WOContext context) {
-        super(context);
-    }
+	private String elementID;
+	
+	public ERXEditDateJavascript(WOContext context) {
+		super(context);
+	}
 
-    
-    public String name() { 
-    	return "datebox"+hashCode(); 
-    }
-    public String href() {
-        String formName = ERXWOForm.formName(context(), "EditForm");
-        return "show_calendar('"+formName+"."+name()+ "', null, null, '"+formatterStringForScript()+"'); return false;";
-    }
+	public void awake() {
+		elementID = context().elementID().replace('.', '_');
+	}
+	
+	public String dateformat() {
+		String format = (String) stringValueForBinding("dateformat");
+		if (format == null) {
+			format = ERXTimestampFormatter.DEFAULT_PATTERN;
+		}
+		return format;
+	}
 
-    public String datePickerJavaScriptUrl() {
-        if (_datePickerJavaScriptUrl==null) {
-            _datePickerJavaScriptUrl= application().resourceManager().urlForResourceNamed("date-picker.js", "ERExtensions", null, context().request());
-        }
-        return _datePickerJavaScriptUrl;
-    }
+	public void setDateformat(String value) {
+		setValueForBinding(value, "dateformat");
+	}
 
-    public void appendToResponse(WOResponse aResponse, WOContext aContext) {
-        i++;
-        super.appendToResponse(aResponse,aContext);
-    }
-    
-    public String formatterStringForScript() {
-        String format = ERXLocalizer.currentLocalizer().localizedStringForKeyWithDefault(dateformat);
-        return ERXEditDateJavascript.formatterStringForScript(format);
-    }
-    
-    public static String formatterStringForScript(String format) {
-    	String result = format;
-    	result = ERXStringUtilities.replaceStringByStringInString("%Y", "yyyy", result);
-    	result = ERXStringUtilities.replaceStringByStringInString("%y", "yy", result);
-    	result = ERXStringUtilities.replaceStringByStringInString("%m", "MM", result);
-    	result = ERXStringUtilities.replaceStringByStringInString("%d", "dd", result);
-    	result = ERXStringUtilities.replaceStringByStringInString("%b", "MON", result);
-    	return result;
-    }
+	public String dateString() {
+		return (String) stringValueForBinding("dateString");
+	}
+
+	public void setDateString(String value) {
+		setValueForBinding(value, "dateString");
+	}
+
+	public String name() {
+		return "datebox" + elementID;
+	}
+
+	public String href() {
+		String formName = ERXWOForm.formName(context(), "EditForm");
+		return "show_calendar('" + formName + "." + name() + "', null, null, '" + formatterStringForScript() + "'); return false;";
+	}
+
+	public String datePickerJavaScriptUrl() {
+		if (_datePickerJavaScriptUrl == null) {
+			_datePickerJavaScriptUrl = application().resourceManager().urlForResourceNamed("date-picker.js", "ERExtensions", null, context().request());
+		}
+		return _datePickerJavaScriptUrl;
+	}
+
+	public String formatterStringForScript() {
+		String format = ERXLocalizer.currentLocalizer().localizedStringForKeyWithDefault(dateformat());
+		return ERXEditDateJavascript.formatterStringForScript(format);
+	}
+
+	public static String formatterStringForScript(String format) {
+		String result = format;
+		result = ERXStringUtilities.replaceStringByStringInString("%Y", "yyyy", result);
+		result = ERXStringUtilities.replaceStringByStringInString("%y", "yy", result);
+		result = ERXStringUtilities.replaceStringByStringInString("%m", "MM", result);
+		result = ERXStringUtilities.replaceStringByStringInString("%d", "dd", result);
+		result = ERXStringUtilities.replaceStringByStringInString("%b", "MON", result);
+		return result;
+	}
 }
