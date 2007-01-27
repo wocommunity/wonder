2e9decb8eb0dd446d443a01b663d57da329d1f08
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-01-27 19:16:10 +0000

    not hooked into the build system yet, but i hate carrying around a bunch of uncommitted stuff ... beginnings of the ERXObjectStoreCoordinatorSynchronizer test cases

---
+++ Common/Frameworks/ERExtensions/Tests/ERXObjectStoreCoordinatorSynchronizerTest/Sources/ERXObjectStoreCoordinatorSynchronizerTest0.java
@@ -0,0 +1,20 @@
+import com.webobjects.eoaccess.EOAdaptorChannel;
+import com.webobjects.eoaccess.EOModel;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSArray;
+
+import er.extensions.ERXJDBCUtilities;
+import er.extensions.migration.IERXMigration;
+
+public class ERXObjectStoreCoordinatorSynchronizerTest0 implements IERXMigration {
+  public void downgrade(EOEditingContext editingContext, EOAdaptorChannel channel, EOModel model) throws Throwable {
+  }
+
+  public NSArray modelDependencies() {
+    return NSArray.EmptyArray;
+  }
+
+  public void upgrade(EOEditingContext editingContext, EOAdaptorChannel channel, EOModel model) throws Throwable {
+    ERXJDBCUtilities.createTablesForModel(channel, model);
+  }
+}
---
+++ Common/Frameworks/ERExtensions/Tests/ERXObjectStoreCoordinatorSynchronizerTest/Sources/Employee.java
@@ -0,0 +1,9 @@
+
+import org.apache.log4j.Logger;
+
+public class Employee extends _Employee {
+	private static Logger log = Logger.getLogger( Employee.class );
+	
+	public Employee() {
+	}
+}
