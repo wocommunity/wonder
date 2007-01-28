e3adbc8aa12661cb06cacadc1645b5332b289ea3
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-01-28 00:16:37 +0000

    more tests

--- Common/Frameworks/ERExtensions/Tests/ERXObjectStoreCoordinatorSynchronizerTest/Sources/ERXObjectStoreCoordinatorSynchronizerTestCase.java
+++ Common/Frameworks/ERExtensions/Tests/ERXObjectStoreCoordinatorSynchronizerTest/Sources/ERXObjectStoreCoordinatorSynchronizerTestCase.java
@@ -1,3 +1,5 @@
+import java.util.Enumeration;
+
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
@@ -45,7 +47,7 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     EOEditingContext editingContext_osc2 = ERXEC.newEditingContext(ERXObjectStoreCoordinatorPool._pool().nextObjectStore());
     Company company_osc2 = Company.fetchCompany(editingContext_osc2, Company.NAME_KEY, companyName);
 
-    for (int i = 0; i < 10; i ++) {
+    for (int i = 0; i < 10; i++) {
       // Change Company1's name in OSC1
       String companyName_1 = "Company" + ERXRandomGUID.newGid() + "_osc1";
       company_osc1.setName(companyName_1);
@@ -53,15 +55,15 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
       sleep();
       assertEquals(companyName_1, company_osc2.name());
       assertEquals(companyName_1, company_osc1.name());
-  
+
       // Fetch Company1 by name in OSC1
       Company company_osc1_with_companyName1 = Company.fetchCompany(editingContext_osc1, Company.NAME_KEY, companyName_1);
       assertEquals(company_osc1, company_osc1_with_companyName1);
-  
+
       // Fetch Company1 by name in OSC2
       Company company_osc2_with_companyName1 = Company.fetchCompany(editingContext_osc2, Company.NAME_KEY, companyName_1);
       assertEquals(company_osc2, company_osc2_with_companyName1);
-  
+
       // Change Company1's name in OSC2
       String companyName_2 = "Company" + ERXRandomGUID.newGid() + "_osc2";
       company_osc2.setName(companyName_2);
@@ -69,11 +71,11 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
       sleep();
       assertEquals(companyName_2, company_osc1.name());
       assertEquals(companyName_2, company_osc2.name());
-      
+
       // Fetch Company1 by name in OSC1
       Company company_osc1_with_companyName2 = Company.fetchCompany(editingContext_osc1, Company.NAME_KEY, companyName_2);
       assertEquals(company_osc1, company_osc1_with_companyName2);
-  
+
       // Fetch Company1 by name in OSC2
       Company company_osc2_with_companyName2 = Company.fetchCompany(editingContext_osc2, Company.NAME_KEY, companyName_2);
       assertEquals(company_osc2, company_osc2_with_companyName2);
@@ -92,12 +94,12 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     EOEditingContext editingContext_osc2 = ERXEC.newEditingContext(ERXObjectStoreCoordinatorPool._pool().nextObjectStore());
     Company company_osc2 = Company.fetchCompany(editingContext_osc2, Company.NAME_KEY, companyName);
 
-    for (int i = 0; i < 10; i ++) {
+    for (int i = 0; i < 10; i++) {
       // Change Company1's name in OSC1
       String companyName_1 = "Company" + ERXRandomGUID.newGid() + "_osc1";
       String companyAddress1_1 = company_osc1.address1();
       company_osc1.setName(companyName_1);
-      
+
       // Change Company1's address in OSC2
       String companyAddress1_2 = "Address" + ERXRandomGUID.newGid() + "_osc2";
       company_osc2.setAddress1(companyAddress1_2);
@@ -109,17 +111,17 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
       assertEquals(companyName_1, company_osc1.name());
       assertEquals(companyAddress1_2, company_osc2.address1());
       assertEquals(companyAddress1_1, company_osc1.address1());
-  
+
       // Fetch Company1 by name in OSC1
       Company company_osc1_with_companyName1 = Company.fetchCompany(editingContext_osc1, Company.NAME_KEY, companyName_1);
       assertEquals(company_osc1, company_osc1_with_companyName1);
       assertEquals(companyAddress1_1, company_osc1_with_companyName1.address1());
-  
+
       // Fetch Company1 by name in OSC2
       Company company_osc2_with_companyName1 = Company.fetchCompany(editingContext_osc2, Company.NAME_KEY, companyName_1);
       assertEquals(company_osc2, company_osc2_with_companyName1);
       assertEquals(companyAddress1_2, company_osc2_with_companyName1.address1());
-  
+
       // Save Company1's new address in OSC2
       editingContext_osc2.saveChanges();
       sleep();
@@ -147,14 +149,10 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     editingContext_osc1.saveChanges();
 
     // Fetch employees relationship of Company1 in OSC1
-    NSArray employees_osc1 = company_osc1.employees();
-    assertEquals(1, employees_osc1.count());
-    assertEquals(employee_osc1, employees_osc1.objectAtIndex(0));
+    assertContainsExactlyEOs(new NSArray(employee_osc1), company_osc1.employees());
 
     // Fetch employees relationship of Company1 in OSC2
-    NSArray employees_osc2 = company_osc2.employees();
-    assertEquals(1, employees_osc2.count());
-    assertEOEquals(employee_osc1, employees_osc1.objectAtIndex(0));
+    assertContainsExactlyEOs(new NSArray(employee_osc1), company_osc2.employees());
   }
 
   public void testAddToFaultedToMany() {
@@ -181,15 +179,11 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     sleep();
 
     // Check employees for Company1 in OSC1
-    NSArray employees_osc1_AfterInsert = company_osc1.employees();
-    assertEquals(1, employees_osc1_AfterInsert.count());
-    assertEquals(employee_osc1, employees_osc1_AfterInsert.objectAtIndex(0));
+    assertContainsExactlyEOs(new NSArray(employee_osc1), company_osc1.employees());
 
     // Check employees for Company1 in OSC2
-    NSArray employees_osc2_AfterInsert = company_osc2.employees();
-    assertEquals(1, employees_osc2_AfterInsert.count());
-    assertEOEquals(employee_osc1, employees_osc2_AfterInsert.objectAtIndex(0));
-    
+    assertContainsExactlyEOs(new NSArray(employee_osc1), company_osc2.employees());
+
     // Create employee for Company1 in OSC2 and Save
     String employeeName2 = "Employee" + ERXRandomGUID.newGid();
     Employee employee_osc2 = Employee.createEmployee(editingContext_osc2, Boolean.FALSE, employeeName2, company_osc2);
@@ -197,18 +191,10 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     sleep();
 
     // Check employees for Company1 in OSC1
-    NSArray employees_osc1_AfterInsert2 = company_osc1.employees();
-    assertEquals(2, employees_osc1_AfterInsert2.count());
-    assertEOEquals(employee_osc1, employees_osc1_AfterInsert2.objectAtIndex(0));
-    assertEOEquals(employee_osc2, employees_osc1_AfterInsert2.objectAtIndex(1));
-    assertEquals(employeeName2, ((Employee) employees_osc1_AfterInsert2.objectAtIndex(1)).name());
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee_osc1, employee_osc2 }), company_osc1.employees());
 
     // Check employees for Company1 in OSC2
-    NSArray employees_osc2_AfterInsert2 = company_osc2.employees();
-    assertEquals(2, employees_osc2_AfterInsert2.count());
-    assertEOEquals(employee_osc1, employees_osc2_AfterInsert2.objectAtIndex(0));
-    assertEOEquals(employee_osc2, employees_osc2_AfterInsert2.objectAtIndex(1));
-    assertEquals(employeeName2, ((Employee) employees_osc2_AfterInsert2.objectAtIndex(1)).name());
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee_osc1, employee_osc2 }), company_osc2.employees());
   }
 
   public void testAddToFaultedToManyWithUncommittedToManyEntries() {
@@ -231,7 +217,7 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     NSArray employees_osc2_BeforeInsert = company_osc2.employees();
     assertEquals(1, employees_osc2_BeforeInsert.count());
     assertEOEquals(employee1_osc2, employees_osc2_BeforeInsert.objectAtIndex(0));
-    
+
     // Create and save Employee2 for Company1 in OSC1
     String employeeName2 = "Employee" + ERXRandomGUID.newGid();
     Employee employee2_osc1 = Employee.createEmployee(editingContext_osc1, Boolean.FALSE, employeeName2, company_osc1);
@@ -239,37 +225,86 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
     sleep();
 
     // Check employees for Company1 in OSC2 (should contain both Employees)
-    NSArray employees_osc2_AfterInsert = company_osc2.employees();
-    assertEquals(2, employees_osc2_AfterInsert.count());
-    assertEOEquals(employee2_osc1, employees_osc2_AfterInsert.objectAtIndex(0));
-    assertEOEquals(employee1_osc2, employees_osc2_AfterInsert.objectAtIndex(1));
-    
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee1_osc2, employee2_osc1 }), company_osc2.employees());
+
     // Check employees for Company1 in OSC1 (should contain only Employee2)
-    NSArray employees_osc1_AfterInsert = company_osc1.employees();
-    assertEquals(1, employees_osc1_AfterInsert.count());
-    assertEquals(employee2_osc1, employees_osc1_AfterInsert.objectAtIndex(0));
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee2_osc1 }), company_osc1.employees());
 
     // Save Employee1 in OSC2
     editingContext_osc2.saveChanges();
     sleep();
 
     // Check employees for Company1 in OSC2 (should contain both Employees)
-    NSArray employees_osc2_AfterInsert2 = company_osc2.employees();
-    assertEquals(2, employees_osc2_AfterInsert2.count());
-    assertEOEquals(employee2_osc1, employees_osc2_AfterInsert2.objectAtIndex(0));
-    assertEOEquals(employee1_osc2, employees_osc2_AfterInsert2.objectAtIndex(1));
-    
-    // Check employees for Company1 in OSC1 (should contain only Employee2)
-    NSArray employees_osc1_AfterInsert2 = company_osc1.employees();
-    assertEquals(2, employees_osc1_AfterInsert2.count());
-    assertEOEquals(employee2_osc1, employees_osc1_AfterInsert2.objectAtIndex(0));
-    assertEOEquals(employee1_osc2, employees_osc1_AfterInsert2.objectAtIndex(1));
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee1_osc2, employee2_osc1 }), company_osc2.employees());
+
+    // Check employees for Company1 in OSC1 (should contain both Employees)
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee1_osc2, employee2_osc1 }), company_osc1.employees());
   }
-  
+
+  public void testRemoveFromFaultedToMany() {
+    // Create Company1 in OSC1
+    String companyName = "Company" + ERXRandomGUID.newGid();
+    EOEditingContext editingContext_osc1 = ERXEC.newEditingContext(ERXObjectStoreCoordinatorPool._pool().nextObjectStore());
+    Company company_osc1 = Company.createCompany(editingContext_osc1, companyName);
+    editingContext_osc1.saveChanges();
+    sleep();
+
+    // Create and Save Employee1 and Employee2 for Company1 in OSC1
+    String employee1Name = "Employee" + ERXRandomGUID.newGid();
+    Employee employee1_osc1 = Employee.createEmployee(editingContext_osc1, Boolean.FALSE, employee1Name, company_osc1);
+    String employee2Name = "Employee" + ERXRandomGUID.newGid();
+    Employee employee2_osc1 = Employee.createEmployee(editingContext_osc1, Boolean.FALSE, employee2Name, company_osc1);
+    editingContext_osc1.saveChanges();
+    sleep();
+
+    // Fetch employees for Company1 in OSC1
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee1_osc1, employee2_osc1 }), company_osc1.employees());
+
+    // Fetch Company1 in OSC2
+    EOEditingContext editingContext_osc2 = ERXEC.newEditingContext(ERXObjectStoreCoordinatorPool._pool().nextObjectStore());
+    Company company_osc2 = Company.fetchCompany(editingContext_osc2, Company.NAME_KEY, companyName);
+    // Fetch and check employees for Company1 in OSC2
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee1_osc1, employee2_osc1 }), company_osc2.employees());
+
+    // gonna break
+    NSArray employees_osc2 = company_osc2.employees();
+    Employee employee2_osc2 = (Employee) employees_osc2.objectAtIndex(0);
+    Employee employee1_osc2 = (Employee) employees_osc2.objectAtIndex(1);
+
+    // Delete Employee1 in OSC1 and Save
+    editingContext_osc1.deleteObject(employee1_osc1);
+    editingContext_osc1.saveChanges();
+    sleep();
+
+    // Fetch and check employees for Company1 in OSC1
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee2_osc1 }), company_osc1.employees());
+
+    // Fetch and check employees for Company1 in OSC2
+    assertContainsExactlyEOs(new NSArray(new Object[] { employee2_osc1 }), company_osc2.employees());
+
+    // ... Do someting with the deleted object in OSC2
+
+    // Delete the remaining employee in OSC2 and Save
+    if (ERXEOControlUtilities.eoEquals(employee1_osc1, employee1_osc2)) {
+      editingContext_osc2.deleteObject(employee2_osc2);
+    }
+    else {
+      editingContext_osc2.deleteObject(employee1_osc2);
+    }
+    editingContext_osc2.saveChanges();
+    sleep();
+
+    // Fetch and check employees for Company1 in OSC1
+    assertContainsExactlyEOs(NSArray.EmptyArray, company_osc1.employees());
+
+    // Fetch and check employees for Company1 in OSC2
+    assertContainsExactlyEOs(NSArray.EmptyArray, company_osc2.employees());
+  }
+
   public static void assertEOEquals(Object obj1, Object obj2) {
     if (obj1 != null && obj2 != null && obj1 instanceof EOEnterpriseObject && obj2 instanceof EOEnterpriseObject) {
-      EOEnterpriseObject eo1 = (EOEnterpriseObject)obj1;
-      EOEnterpriseObject eo2 = (EOEnterpriseObject)obj2;
+      EOEnterpriseObject eo1 = (EOEnterpriseObject) obj1;
+      EOEnterpriseObject eo2 = (EOEnterpriseObject) obj2;
       if (!ERXEOControlUtilities.eoEquals(eo1, eo2)) {
         throw new AssertionFailedError("Expected " + eo1.editingContext().globalIDForObject(eo1) + ", but got " + eo2.editingContext().globalIDForObject(eo2));
       }
@@ -278,9 +313,32 @@ public class ERXObjectStoreCoordinatorSynchronizerTestCase extends TestCase {
       assertEquals(obj1, obj2);
     }
   }
-  
+
+  public void assertContainsExactlyEOs(NSArray expectedEOs, NSArray actualEOs) {
+    int expectedCount = expectedEOs.count();
+    int actualCount = actualEOs.count();
+    assertEquals(expectedCount, actualCount);
+    Enumeration expectedEOsEnum = expectedEOs.objectEnumerator();
+    while (expectedEOsEnum.hasMoreElements()) {
+      EOEnterpriseObject expectedEO = (EOEnterpriseObject) expectedEOsEnum.nextElement();
+      boolean containsEO = false;
+      Enumeration actualEOsEnum = actualEOs.objectEnumerator();
+      while (!containsEO && actualEOsEnum.hasMoreElements()) {
+        EOEnterpriseObject actualEO = (EOEnterpriseObject) actualEOsEnum.nextElement();
+        containsEO = ERXEOControlUtilities.eoEquals(expectedEO, actualEO);
+      }
+      if (!containsEO) {
+        throw new AssertionFailedError("Expected " + expectedEOs.valueForKey("__globalID") + ", but got " + actualEOs.valueForKey("__globalID"));
+      }
+    }
+  }
+
   protected void sleep() {
-    try { Thread.sleep(50); } catch (Throwable t) { }
+    try {
+      Thread.sleep(50);
+    }
+    catch (Throwable t) {
+    }
   }
 
   public static void main(String[] args) {
