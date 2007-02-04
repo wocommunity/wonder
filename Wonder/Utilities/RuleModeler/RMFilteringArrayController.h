e4a5b8933c32862495578b4af4994f5c964727ff
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-04 11:10:56 +0000

    Added focus on selection and unfocus. Parsing of search string as NSPredicate is now optional. Added case-sensitivity and OR matching options, for search. Fixed bug when parsing as NSPredicate. Refactoring of undo and insertion/deletion.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5139 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Utilities/RuleModeler/RMFilteringArrayController.h
+++ Wonder/Utilities/RuleModeler/RMFilteringArrayController.h
@@ -9,11 +9,27 @@
 #import <Foundation/Foundation.h>
 
 @interface RMFilteringArrayController : NSArrayController {
-    NSMutableArray *_newObjects;
+    NSArray  *focussedObjects;
+    NSImage  *focusImage;
+    NSImage  *emptyImage;
+    IBOutlet NSSearchField   *searchField;
+    BOOL    searchIsCaseSensitive;
+    BOOL    searchMatchesAnyWord;
 }
 
 - (void)search:(id)sender;
-- (NSUndoManager *)undoManager;
-- (void)_setActionName:(NSString *)format old:(id)oldValue new:(id)newValue;
+
+- (IBAction)focus:(id)sender;
+- (IBAction)unfocus:(id)sender;
+
+- (BOOL)focussing;
+- (BOOL)canFocus;
+- (BOOL)canUnfocus;
+
+- (NSSearchField *) searchField;
+- (void) setSearchField:(NSSearchField *)value;
+
+- (IBAction)changeCaseSensitivityOption:(id)sender;
+- (IBAction)changeWordMatchingOption:(id)sender;
 
 @end
