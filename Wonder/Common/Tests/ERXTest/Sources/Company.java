a482d52ea1c05b2eb56e36a646544d6cda20db2d
(no author) <(no author)@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-04 12:17:34 +0000

    This commit was manufactured by cvs2svn to create tag 'RuleModeler_1_3'.
    git-svn-id: file:///Users/q/working/wondersvn/tags/RuleModeler_1_3@5162 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Tests/ERXTest/Sources/Company.java
+++
@@ -1,9 +0,0 @@
-
-import org.apache.log4j.Logger;
-
-public class Company extends _Company {
-	private static Logger log = Logger.getLogger( Company.class );
-	
-	public Company() {
-	}
-}
--- Wonder/Common/Tests/ERXTest/Sources/Employee.java
+++
@@ -1,9 +0,0 @@
-
-import org.apache.log4j.Logger;
-
-public class Employee extends _Employee {
-	private static Logger log = Logger.getLogger( Employee.class );
-	
-	public Employee() {
-	}
-}
