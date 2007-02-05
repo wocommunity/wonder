010b3e5177a4f5da6bac3fed29857c48d67f0cda
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-05 21:20:50 +0000

    default button also moved to top -1000px
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5165 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXWOForm.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXWOForm.java
@@ -248,7 +248,7 @@ public class ERXWOForm extends com.webobjects.appserver._private.WOHTMLDynamicEl
         	if(_multipleSubmit != null && _multipleSubmit.booleanValueInComponent(context.component())) {
         		if(_addDefaultSubmitButton != null && _addDefaultSubmitButton.booleanValueInComponent(context.component())
         				|| (_addDefaultSubmitButton == null  && addDefaultSubmitButtonDefault)) {
-        			response._appendContentAsciiString("<input type=\"submit\" style=\"position: absolute; left: -10000px;\" name=\"WOFormDummySubmit\" value=\"WOFormDummySubmit\" />");
+        			response._appendContentAsciiString("<input type=\"submit\" style=\"position: absolute; left: -100px; top: -1000px; \" name=\"WOFormDummySubmit\" value=\"WOFormDummySubmit\" />");
         		}
         	}
         	appendChildrenToResponse(response, context);
