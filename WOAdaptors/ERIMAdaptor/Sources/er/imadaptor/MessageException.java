19790d201da119a40210b5cfcfee0d84679179d6
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-15 20:57:31 +0000

    reformatted with proper PW formatters + switched to JOscar library as the default implementation (seems to be much nicer)
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5241 8dc76679-1c23-de11-85cd-0050568e728b

--- WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/MessageException.java
+++ WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/MessageException.java
@@ -1,11 +1,11 @@
 package er.imadaptor;
 
 public class MessageException extends InstantMessengerException {
-  public MessageException(String message) {
-    super(message);
-  }
+	public MessageException(String message) {
+		super(message);
+	}
 
-  public MessageException(String message, Throwable cause) {
-    super(message, cause);
-  }
+	public MessageException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
