8ae342a8a45d969a7d3cb9af9c5afe2111f6074d
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-15 20:57:31 +0000

    reformatted with proper PW formatters + switched to JOscar library as the default implementation (seems to be much nicer)

--- WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/BuddyOfflineException.java
+++ WOAdaptors/ERIMAdaptor/Sources/er/imadaptor/BuddyOfflineException.java
@@ -2,12 +2,12 @@ package er.imadaptor;
 
 public class BuddyOfflineException extends MessageException {
 
-  public BuddyOfflineException(String message) {
-    super(message);
-  }
+	public BuddyOfflineException(String message) {
+		super(message);
+	}
 
-  public BuddyOfflineException(String message, Throwable cause) {
-    super(message, cause);
-  }
+	public BuddyOfflineException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
