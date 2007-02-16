5ba0fdff95882e0301eb6f68fce82526a2ad03fe
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-16 11:24:51 +0000

    update from chuck
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5251 8dc76679-1c23-de11-85cd-0050568e728b

--- Ajax/AjaxExample/Sources/AjaxGridExample.java
+++ Ajax/AjaxExample/Sources/AjaxGridExample.java
@@ -7,51 +7,43 @@ import com.webobjects.foundation.*;
 
 public class AjaxGridExample extends WOComponent {
 
-	 private NSMutableDictionary configData;
-	 public WODisplayGroup displayGroup;
-	 
-	 
-	 public AjaxGridExample(WOContext context) 
-	 {
-        super(context);
-        displayGroup = new WODisplayGroup();
-        displayGroup().setObjectArray(sampleData());
-        
-        // We preset the batch size so that our x / N display is updated when the page first renders
-        displayGroup().setNumberOfObjectsPerBatch(Integer.parseInt((String)configData().valueForKey("batchSize")));
-	 }
+	private NSMutableDictionary configData;
+	public WODisplayGroup displayGroup;
 
-	 
-    private NSMutableArray sampleData()
-    {
-    	NSData data = new NSData(application().resourceManager().bytesForResourceNamed("AjaxGridExampleTestData.plist", null, NSArray.EmptyArray));
-    	NSMutableArray sampleData = new NSMutableArray((NSArray)NSPropertyListSerialization.propertyListFromData(data, "UTF-8"));
-    	Random random = new Random(new NSTimestamp().getNanos());
-    	for (int i = 0; i < sampleData.count(); i++)
-    	{
-    		NSMutableDictionary row = (NSMutableDictionary)sampleData.objectAtIndex(i);
-    		row.setObjectForKey(new Integer(random.nextInt()), "number");
-    		row.setObjectForKey(new NSTimestamp(random.nextLong()), "date");
+	public AjaxGridExample(WOContext context) {
+		super(context);
+		displayGroup = new WODisplayGroup();
+		displayGroup().setObjectArray(sampleData());
+
+		// We preset the batch size so that our x / N display is updated when
+		// the page first renders
+		displayGroup().setNumberOfObjectsPerBatch(Integer.parseInt((String) configData().valueForKey("batchSize")));
+	}
+
+	private NSMutableArray sampleData() {
+		NSData data = new NSData(application().resourceManager().bytesForResourceNamed("AjaxGridExampleTestData.plist", null, NSArray.EmptyArray));
+		NSMutableArray sampleData = new NSMutableArray((NSArray) NSPropertyListSerialization.propertyListFromData(data, "UTF-8"));
+		Random random = new Random(new NSTimestamp().getNanos());
+		for (int i = 0; i < sampleData.count(); i++) {
+			NSMutableDictionary row = (NSMutableDictionary) sampleData.objectAtIndex(i);
+			row.setObjectForKey(new Integer(random.nextInt()), "number");
+			row.setObjectForKey(new NSTimestamp(random.nextLong()), "date");
+			row.setObjectForKey(new Integer(i + 1).toString(), "level");
 		}
-    	
-    	return sampleData;
-    	
-    }
-    
-    
-    public NSMutableDictionary configData()
-    {
-    	if (configData == null)
-    	{
-    		NSData data = new NSData(application().resourceManager().bytesForResourceNamed("AjaxGridExampleConfiguration.plist", null, NSArray.EmptyArray));
-    		configData = new NSMutableDictionary((NSDictionary)NSPropertyListSerialization.propertyListFromData(data, "UTF-8"));
-     	}
-    	
-    	return configData;
-    }
 
-    
-    
+		return sampleData;
+
+	}
+
+	public NSMutableDictionary configData() {
+		if (configData == null) {
+			NSData data = new NSData(application().resourceManager().bytesForResourceNamed("AjaxGridExampleConfiguration.plist", null, NSArray.EmptyArray));
+			configData = new NSMutableDictionary((NSDictionary) NSPropertyListSerialization.propertyListFromData(data, "UTF-8"));
+		}
+
+		return configData;
+	}
+
 	/**
 	 * @return the disply group containing the data
 	 */
@@ -59,20 +51,17 @@ public class AjaxGridExample extends WOComponent {
 		return displayGroup;
 	}
 
-	
 	public void updateBatchSize() {
-		NSLog.out.appendln("updateBatchSize");
 	}
-	
+
 	public void nextBatch() {
 		displayGroup().displayNextBatch();
 		displayGroup().setSelectedObject(null);
 	}
-	
+
 	public void previousBatch() {
 		displayGroup().displayPreviousBatch();
 		displayGroup().setSelectedObject(null);
 	}
-
 
 }
