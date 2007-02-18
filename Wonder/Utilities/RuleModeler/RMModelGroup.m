f6862dce7f5b3e2a787c680254447745be1ebb44
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-18 19:54:35 +0000

    Fixed bug when closing documents, due to dependencies between models and model groups.
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5259 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Utilities/RuleModeler/RMModelGroup.m
+++ Wonder/Utilities/RuleModeler/RMModelGroup.m
@@ -1,10 +1,36 @@
-//
-//  RMModelGroup.m
-//  RuleModeler
-//
-//  Created by Dave Lopper on 8/14/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/*
+ RMModelGroup.m
+ RuleModeler
+
+ Created by davelopper on 8/14/06.
+
+
+ Copyright (c) 2004-2007, Project WONDER <http://wonder.sourceforge.net/>
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+  * Neither the name of the Project WONDER nor the names of its contributors may
+    be used to endorse or promote products derived from this software without
+    specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #import "RMModelGroup.h"
 #import "RMModelGroupEditor.h"
@@ -375,17 +401,15 @@ NSString *RMModelGroupType = @"Rule Model Group";
     RMDocumentController    *dc = [NSDocumentController sharedDocumentController];
     NSEnumerator            *docEnum = [[dc documents] objectEnumerator];
     id                      eachDoc;
-    NSMutableArray          *docsToClose = [NSMutableArray arrayWithArray:[self valueForKeyPath:@"modelContainers.model"]];
+    NSMutableArray          *modelsToClose = [NSMutableArray arrayWithArray:[self valueForKeyPath:@"modelContainers.model"]];
     
     while (eachDoc = [docEnum nextObject]) {
         if (eachDoc != self) {
-            if ([eachDoc isKindOfClass:[RMModelGroup class]]) {
-                if ([[eachDoc valueForKeyPath:@"modelContainers.model"] containsObject:self]) {
-                    [docsToClose removeObject:eachDoc];
-                }
-            } else if ([eachDoc isKindOfClass:[RMModel class]]) {
+            if ([eachDoc isKindOfClass:[RMModelGroup class]]) { // Don't close models referenced by other model groups
+                [modelsToClose removeObjectsInArray:[eachDoc valueForKeyPath:@"modelContainers.model"]];
+            } else if ([eachDoc isKindOfClass:[RMModel class]]) { // Don't close an opened model, i.e. a model which has a GUI
                 if ([[eachDoc windowControllers] count]) {
-                    [docsToClose removeObject:eachDoc];
+                    [modelsToClose removeObject:eachDoc];
                 }
             }
         }
@@ -396,7 +420,7 @@ NSString *RMModelGroupType = @"Rule Model Group";
     // The following code allows us to avoid adding models to recent list,
     // when models where only referenced in group document.
     [dc setDontAddRecentDocument:YES];
-    [docsToClose makeObjectsPerformSelector:@selector(close)];
+    [modelsToClose makeObjectsPerformSelector:@selector(close)];
     [dc setDontAddRecentDocument:NO];
 }
 
@@ -413,28 +437,26 @@ NSString *RMModelGroupType = @"Rule Model Group";
     [super showWindows];
 }
 
-- (void)close {
-    // We don't want that model documents which are still referenced by model group documents
-    // be really closed. Closing it would remove it from documentController's list.
-//    NSLog(@"Should close %@?", [self fileName]);
+- (BOOL)isReferencedInSomeModelGroup {
     NSEnumerator    *docEnum = [[[NSDocumentController sharedDocumentController] documents] objectEnumerator];
     id              eachDoc;
-    BOOL            doClose = YES;
     
     while (eachDoc = [docEnum nextObject]) {
         if ([eachDoc isKindOfClass:[RMModelGroup class]]) {
             if ([[eachDoc valueForKeyPath:@"modelContainers.model"] containsObject:self]) {
-                doClose = NO;
-                break;
+                return YES;
             }
         }
     }
     
-    if (doClose) {
-//        NSLog(@"YES");
+    return NO;
+}
+
+- (void)close {
+    // We don't want that model documents which are still referenced by model group documents
+    // be really closed. Closing it would remove it from documentController's list.
+    if (![self isReferencedInSomeModelGroup]) {
         [super close];
-    } else {
-//        NSLog(@"NO");
     }
 }
 
