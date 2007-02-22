be5b779a488d2f7e38cf541482b1fb99e7569f05
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-22 11:45:07 +0000

    when "multiple" is true, the query to one/many can now search with an OR, meaning, if you have a user with roles and select "admin" and "customer", all users that are admins or customers are found.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryToOneRelationship.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryToOneRelationship.java
@@ -8,18 +8,24 @@ package er.directtoweb;
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.directtoweb.D2WQueryToOneRelationship;
 import com.webobjects.eoaccess.EOEntity;
 import com.webobjects.eoaccess.EORelationship;
 import com.webobjects.eoaccess.EOUtilities;
 import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSArray;
 
 import er.extensions.ERXEC;
+import er.extensions.ERXPrimaryKeyListQualifier;
+import er.extensions.ERXValueUtilities;
 
 /**
- * Same as original but used ERToOneRelationship.<br />
- * 
+ * Enhanced relationship query component to to-one relationships.
+ * @d2wKey multiple when true, the user can choose multiple items and they get selected as <code>OR</code>
+ * @d2wKey restrictedChoiceKey keypath off the component that returns the list of objects to display
+ * @d2wKey restrictingFetchSpecification name of the fetchSpec to use for the list of objects.
  */
 
 public class ERD2WQueryToOneRelationship extends D2WQueryToOneRelationship {
@@ -29,16 +35,48 @@ public class ERD2WQueryToOneRelationship extends D2WQueryToOneRelationship {
         super(context);
     }
 
+    public boolean hasMultipleSelection() {
+        return ERXValueUtilities.booleanValue(d2wContext().valueForKey("multiple"));
+    }
+    
+    public String componentName() {
+        return !hasMultipleSelection() ? "ERXToOneRelationship" :  "ERXToManyRelationship";
+    }
+    
+    public WOComponent self() {
+        return this;
+    }
+
+    public void setValue(Object newValue) {
+        if(hasMultipleSelection()) {
+            if (newValue instanceof NSArray) {
+                NSArray array = (NSArray) newValue;
+                if(array.count() == 0) {
+                    newValue = null;
+                }
+            }
+            displayGroup().queryOperator().takeValueForKey(ERXPrimaryKeyListQualifier.IsContainedInArraySelectorName, propertyKey());
+        }
+        super.setValue(newValue);
+    }
+
     public Object restrictedChoiceList() {
-        String restrictedChoiceKey=(String)d2wContext().valueForKey("restrictedChoiceKey");
-        if( restrictedChoiceKey!=null &&  restrictedChoiceKey.length()>0 )
+        String restrictedChoiceKey = (String) d2wContext().valueForKey("restrictedChoiceKey");
+        if (restrictedChoiceKey != null && restrictedChoiceKey.length() > 0) {
             return valueForKeyPath(restrictedChoiceKey);
-        String fetchSpecName=(String)d2wContext().valueForKey("restrictingFetchSpecification");
-        if(fetchSpecName != null) {
+        }
+        String fetchSpecName = (String) d2wContext().valueForKey("restrictingFetchSpecification");
+        if (fetchSpecName != null) {
             EOEditingContext ec = ERXEC.newEditingContext();
             EOEntity entity = d2wContext().entity();
-            EORelationship relationship = entity.relationshipNamed((String)d2wContext().valueForKey("propertyKey"));
-            return EOUtilities.objectsWithFetchSpecificationAndBindings(ec, relationship.destinationEntity().name(),fetchSpecName,null);
+            EORelationship relationship = entity.relationshipNamed((String) d2wContext().valueForKey("propertyKey"));
+            ec.lock();
+            try {
+                return EOUtilities.objectsWithFetchSpecificationAndBindings(ec,
+                        relationship.destinationEntity().name(), fetchSpecName, null);
+            } finally {
+                ec.unlock();
+            }
         }
         return null;
     }
