9577b05dc589a3b58cd53dbcd6f633a4324a6b3c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-22 11:45:07 +0000

    when "multiple" is true, the query to one/many can now search with an OR, meaning, if you have a user with roles and select "admin" and "customer", all users that are admins or customers are found.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5288 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryToManyRelationship.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WQueryToManyRelationship.java
@@ -2,20 +2,24 @@ package er.directtoweb;
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.directtoweb.D2WQueryToManyRelationship;
 import com.webobjects.eoaccess.EOEntity;
 import com.webobjects.eoaccess.EORelationship;
 import com.webobjects.eoaccess.EOUtilities;
 import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSArray;
 
 import er.extensions.ERXEC;
+import er.extensions.ERXPrimaryKeyListQualifier;
+import er.extensions.ERXValueUtilities;
 
 /**
- * Same as original but used ERXToOneRelationship and allows you to restrict the objects shown.<br />
- * 
- * @created ak on Wed Apr 07 2004
- * @project ERDirectToWeb
+ * Enhanced relationship query component to to-many relationships.
+ * @d2wKey multiple when true, the user can choose multiple items and they get selected as <code>OR</code>
+ * @d2wKey restrictedChoiceKey keypath off the component that returns the list of objects to display
+ * @d2wKey restrictingFetchSpecification name of the fetchSpec to use for the list of objects.
  */
 
 public class ERD2WQueryToManyRelationship extends D2WQueryToManyRelationship {
@@ -30,12 +34,32 @@ public class ERD2WQueryToManyRelationship extends D2WQueryToManyRelationship {
     public ERD2WQueryToManyRelationship(WOContext context) {
         super(context);
     }
-    
-    public void awake() {
-        super.awake();
-        //displayGroup().queryOperator().takeValueForKey(ERXPrimaryKeyListQualifier.IsContainedInSelectorName, propertyKey());
+
+    public boolean hasMultipleSelection() {
+        return ERXValueUtilities.booleanValue(d2wContext().valueForKey("multiple"));
     }
     
+    public String componentName() {
+        return !hasMultipleSelection() ? "ERXToOneRelationship" :  "ERXToManyRelationship";
+    }
+    
+    public WOComponent self() {
+        return this;
+    }
+
+    public void setValue(Object newValue) {
+        if(hasMultipleSelection()) {
+            if (newValue instanceof NSArray) {
+                NSArray array = (NSArray) newValue;
+                if(array.count() == 0) {
+                    newValue = null;
+                }
+            }
+            displayGroup().queryOperator().takeValueForKey(ERXPrimaryKeyListQualifier.IsContainedInArraySelectorName, propertyKey());
+        }
+        super.setValue(newValue);
+    }
+
     public Object restrictedChoiceList() {
         String restrictedChoiceKey=(String)d2wContext().valueForKey("restrictedChoiceKey");
         if( restrictedChoiceKey!=null &&  restrictedChoiceKey.length() > 0 )
