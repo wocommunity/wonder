f8f5bf7426ccfc9352d7b655b57f483d33047e02
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-25 10:37:43 +0000

    Added option (local + global) to display lhs in a formatted form, one comparison qualifier per line. Added option to put parentheses around comparison qualifiers. Refactoring of assignment tooltips. Changed rhs value text size to match other fields text size.

--- Utilities/RuleModeler/RMModelEditor.h
+++ Utilities/RuleModeler/RMModelEditor.h
@@ -1,18 +1,30 @@
-//  Copyright (c) 2004 King Chung Huang
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+/*
+ RMModelEditor.h
+ RuleModeler
 
-//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ Created by King Chung Huang on 1/29/04.
 
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-//
-//  RMModelEditor.h
-//  RuleModeler
-//
-//  Created by King Chung Huang on Thu Jan 29 2004.
-//  Copyright (c) 2004 King Chung Huang. All rights reserved.
-//
+ Copyright (c) 2004 King Chung Huang
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
 #import <AppKit/AppKit.h>
 
@@ -48,6 +60,7 @@ typedef enum {
     
     IBOutlet RMFilteringArrayController  *rulesController;
     IBOutlet NSTextField    *lhsValueTextField;
+    IBOutlet NSButton       *lhsFormatCheckbox;
     IBOutlet NSTextView     *rhsValueTextView;
     IBOutlet NSTextField    *rhsValueHelpField;
     
@@ -84,6 +97,7 @@ typedef enum {
 
 - (IBAction) focus:(id)sender;
 - (IBAction) unfocus:(id)sender;
+- (void) showRules:(NSArray *)rules;
 
 - (void) setFirstResponderInPart:(RMWindowPart)part;
 
