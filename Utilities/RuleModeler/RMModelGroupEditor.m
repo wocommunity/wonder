424dd776c4fa744a4fdd590028f2e2e789d58c22
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-25 10:38:51 +0000

    Moved code to superclass

--- Utilities/RuleModeler/RMModelGroupEditor.m
+++ Utilities/RuleModeler/RMModelGroupEditor.m
@@ -1,10 +1,36 @@
-//
-//  RMModelGroupEditor.m
-//  RuleModeler
-//
-//  Created by Dave Lopper on 8/14/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/*
+ RMModelGroupEditor.m
+ RuleModeler
+
+ Created by davelopper on 8/14/06.
+
+
+ Copyright (c) 2004-2007, Project WONDER <http://wonder.sourceforge.net/>
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+  * Neither the name of the Project WONDER nor the names of its contributors may
+    be used to endorse or promote products derived from this software without
+    specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #import "RMModelGroupEditor.h"
 #import "RMModelGroup.h"
@@ -14,31 +40,7 @@
 
 //#define EDITABLE_MODELS
 
-@interface RMEnabledColorTransformer : NSValueTransformer {
-}
-@end
-
-@implementation RMEnabledColorTransformer
-
-+ (BOOL)allowsReverseTransformation {
-    return NO;
-}
-
-- (id)transformedValue:(id)value {
-    if ([value boolValue]) {
-        return [NSColor textColor];
-    } else {
-        return [NSColor disabledControlTextColor];
-    }    
-}
-
-@end
-
 @implementation RMModelGroupEditor
-
-+ (void)initialize {
-    [NSValueTransformer setValueTransformer:[[RMEnabledColorTransformer alloc] init] forName:@"RMEnabledColorTransformer"];
-}
 
 - (id)init {
     if (self = [self initWithWindowNibName:@"RMModelGroupEditor"]) {
