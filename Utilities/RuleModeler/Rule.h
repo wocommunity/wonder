49f58cc001f805bf48643a52d25cff203186aa52
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-02-25 10:40:23 +0000

    Added option to set default rule priority (now set to 100). Added option (local + global) to display lhs in a formatted form, one comparison qualifier per line.

--- Utilities/RuleModeler/Rule.h
+++ Utilities/RuleModeler/Rule.h
@@ -1,10 +1,30 @@
-//
-//  Rule.h
-//  RuleModeler
-//
-//  Created by King Chung Huang on Thu Jan 29 2004.
-//  Copyright (c) 2004 King Chung Huang. All rights reserved.
-//
+/*
+ Rule.h
+ RuleModeler
+
+ Created by King Chung Huang on 1/29/04.
+
+
+ Copyright (c) 2004 King Chung Huang
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
 #import <Foundation/Foundation.h>
 
@@ -20,11 +40,15 @@
     int		    _author;
     EOQualifier	    *_lhs;
     NSString	    *_lhsDescription;
+    NSString	    *_lhsFormattedDescription;
     Assignment	    *_rhs;
     BOOL	    _enabled;
 	RMModel		*_model; // Back-pointer - not retained
     BOOL        isNewRule; // FIXME Dependency on RMFilteringArrayController
 }
+
++ (void)setDefaultRulePriority:(int)priority;
++ (int)defaultRulePriority;
 
 + (NSArray *)rulesFromMutablePropertyList:(id)plist;
 
@@ -39,6 +63,7 @@
 - (EOQualifier *)lhs;
 - (void)setLhs:(EOQualifier *)lhs;
 - (NSString *)lhsDescription;
+- (NSString *)lhsFormattedDescription;
 
 - (int)author;
 - (void)setAuthor:(int)value;
@@ -55,5 +80,7 @@
 - (void)_setActionName:(NSString *)format old:(id)oldValue new:(id)newValue;
 
 - (BOOL)isEqualToRule:(Rule *)rule;
+
+- (void)resetDescriptionCaches;
 
 @end
