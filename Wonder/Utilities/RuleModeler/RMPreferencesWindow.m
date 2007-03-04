99e7695a9de6a8af7ee0aa694403b4f135db5729
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-03-04 18:43:09 +0000

    New pref pane
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5375 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Utilities/RuleModeler/RMPreferencesWindow.m
+++ Wonder/Utilities/RuleModeler/RMPreferencesWindow.m
@@ -1,10 +1,30 @@
-//
-//  RMPreferencesWindow.m
-//  RuleModeler
-//
-//  Created by King Chung Huang on Fri Jan 30 2004.
-//  Copyright (c) 2004 King Chung Huang. All rights reserved.
-//
+/*
+ RMPreferencesWindow.m
+ RuleModeler
+
+ Created by King Chung Huang on 1/30/04.
+
+
+ Copyright (c) 2004 King Chung Huang
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
 #import "RMPreferencesWindow.h"
 
@@ -29,6 +49,7 @@
     
  
     [self addPreferencePane:generalPane identifier:@"GeneralPane" icon:[NSImage imageNamed:@"NSApplicationIcon"] label:NSLocalizedString(@"General", @"Toolbar item label") toolTip:NSLocalizedString(@"General Preferences", @"Toolbar item tooltip") target:self action:@selector(toolbarAction:) menu:NULL];
+    [self addPreferencePane:colorPane identifier:@"ColorPane" icon:[NSImage imageNamed:@"ColorPane"] label:NSLocalizedString(@"Colors", @"Toolbar item label") toolTip:NSLocalizedString(@"Color Preferences", @"Toolbar item tooltip") target:self action:@selector(toolbarAction:) menu:NULL];
     
     [self prepareToolbar];
     
@@ -129,6 +150,7 @@
     
     [currentIdentifier release];
     currentIdentifier = [[NSString stringWithString:identifier] retain];
+    [[[self window] toolbar] setSelectedItemIdentifier:identifier];
 }
 
 - (IBAction)addPath:(id)sender {
