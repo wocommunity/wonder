f62c91954a803ddd9aa19dabbb948c816979dc68
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-03-04 18:50:09 +0000

    Fixed update of rule order in files
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5379 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Utilities/RuleModeler/RMModel.m
+++ Wonder/Utilities/RuleModeler/RMModel.m
@@ -1,10 +1,30 @@
-//
-//  RMModel.m
-//  RuleModeler
-//
-//  Created by King Chung Huang on Thu Jan 29 2004.
-//  Copyright (c) 2004 King Chung Huang. All rights reserved.
-//
+/*
+ RMModel.m
+ RuleModeler
+
+ Created by King Chung Huang on 1/29/04.
+
+
+ Copyright (c) 2004 King Chung Huang
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
 #import "RMModel.h"
 #import "RMModelEditor.h"
@@ -76,6 +96,13 @@ static NSArray * _sortDescriptors = nil;
 	}
     
 	return _sortDescriptors;
+}
+
++ (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"values.useRuleEditorRuleOrdering"]) {
+        [_sortDescriptors release];
+        _sortDescriptors = nil;
+    }
 }
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
@@ -237,6 +264,7 @@ static NSArray * _sortDescriptors = nil;
 + (void)initialize {
     // Do not call super - see +initialize documentation
     [self setKeys:[NSArray arrayWithObject:@"fileURL"] triggerChangeNotificationsForDependentKey:@"displayName"];
+    [[NSUserDefaultsController sharedUserDefaultsController] addObserver:self forKeyPath:@"values.useRuleEditorRuleOrdering" options:0 context:NULL];
 }
 
 - (NSString *)displayName {
