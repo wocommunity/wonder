d619b21425fe1dcae2398068c126cca1d3f096b9
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-03-17 20:27:10 +0000

    added uniquing wrapper to prevent caching issues in the tab panel
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5433 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXUniquingWrapper.java
@@ -0,0 +1,62 @@
+package er.extensions;
+
+import org.apache.log4j.Logger;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WOAssociation;
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.appserver._private.WODynamicElementCreationException;
+import com.webobjects.appserver._private.WODynamicGroup;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableDictionary;
+
+/**
+ * Needed when you have a component content and the state of the wrapper changes.
+ * In that case, you might get the same element id and then switch components will get the
+ * wrong value from their cache. It's very hard to explain, take a look at the ERXTabPanel :)
+ * @binding id is to append to the element ID
+ * @author ak
+ */
+public class ERXUniquingWrapper extends WODynamicGroup {
+	
+	private static final Logger log = Logger.getLogger(ERXTolerantWrapper.class);
+	
+	private WOAssociation _id;
+
+	public ERXUniquingWrapper(String name, NSDictionary associations, WOElement template) {
+		super(name, associations, template);
+		_id = (WOAssociation) ((NSMutableDictionary) associations).removeObjectForKey("id");
+		if(_id == null) {
+			throw new WODynamicElementCreationException("ERXUniquingWrapper needs an 'id' binding." );
+		}
+	}
+
+	private String id(WOComponent component) {
+		Object value = _id.valueInComponent(component);
+		return value != null ? value.toString() : "" + hashCode();
+	}
+
+	public void appendToResponse(WOResponse response, WOContext context) {
+		context.appendElementIDComponent(id(context.component()));
+		super.appendToResponse(response, context);
+		context.deleteLastElementIDComponent();
+	}
+
+	public WOActionResults invokeAction(WORequest request, WOContext context) {
+		context.appendElementIDComponent(id(context.component()));
+		WOActionResults result = super.invokeAction(request, context);
+		context.deleteLastElementIDComponent();
+		return result;
+	}
+
+	public void takeValuesFromRequest(WORequest request, WOContext context) {
+		context.appendElementIDComponent(id(context.component()));
+		super.takeValuesFromRequest(request, context);
+		context.deleteLastElementIDComponent();
+	}
+
+}
