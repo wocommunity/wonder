24be7c4b2cf54c51f5352efe0f198c5e0a765e32
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-03-29 01:58:27 +0000

    split out ajax file upload into ajax progress bar, added a lot more controls to both

--- Ajax/AjaxExample/Sources/FileUploadExample.java
+++ Ajax/AjaxExample/Sources/FileUploadExample.java
@@ -5,17 +5,49 @@ import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.foundation.NSData;
 
+import er.ajax.AjaxProgress;
+import er.ajax.AjaxUploadProgress;
+
 public class FileUploadExample extends WOComponent {
 	public NSData _data;
+	public AjaxUploadProgress _uploadProgress;
+	public AjaxProgress _progress;
 
 	public FileUploadExample(WOContext context) {
 		super(context);
+		_progress = new AjaxProgress(100);
+		Thread progressThread = new Thread(new Runnable() {
+			public void run() {
+				for (int i = 0; i < _progress.maximum(); i++) {
+					_progress.setValue(i);
+					try {
+						Thread.sleep(200);
+					}
+					catch (Throwable t) {
+					}
+				}
+				_progress.setDone(true);
+			}
+		});
+		progressThread.start();
 	}
 
+	public long now() {
+		return System.currentTimeMillis();
+	}
+	
 	public String test() {
 		return null;
 	}
 
+	public WOActionResults resetUpload() {
+		if (_uploadProgress != null) {
+			_uploadProgress.reset();
+			_uploadProgress = null;
+		}
+		return null;
+	}
+	
 	public WOActionResults uploadFinished() {
 		System.out.println("FileUploadExample.uploadFinished: FINISHED!");
 		if (_data != null) {
