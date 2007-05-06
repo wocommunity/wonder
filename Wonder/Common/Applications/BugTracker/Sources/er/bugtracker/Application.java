81d0429065daf599f29eef3dbedf5b2d5cc029e7
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-06 00:36:54 +0000

    using ERXNavigation instead of standard D2W
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5580 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Applications/BugTracker/Sources/er/bugtracker/Application.java
+++ Wonder/Common/Applications/BugTracker/Sources/er/bugtracker/Application.java
@@ -5,13 +5,23 @@
  * Public Software License version 0.5, a copy of which has been
  * included with this distribution in the LICENSE.NPL file.  */
 package er.bugtracker;
-import com.webobjects.appserver.*;
-import com.webobjects.directtoweb.*;
-import com.webobjects.eocontrol.*;
-import com.webobjects.eoaccess.*;
-import com.webobjects.foundation.*;
-import java.util.*;
-import er.extensions.*;
+
+import java.util.Enumeration;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOMailDelivery;
+import com.webobjects.eoaccess.EOModel;
+import com.webobjects.eoaccess.EOModelGroup;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSLog;
+
+import er.extensions.ERXApplication;
+import er.extensions.ERXEC;
+import er.extensions.ERXNavigationManager;
+import er.extensions.ERXProperties;
+import er.extensions.ERXWOContext;
 
 public class Application extends ERXApplication {
 
@@ -22,63 +32,67 @@ public class Application extends ERXApplication {
     }
 
     private String _userDefaultName;
+
     public String name() {
-        if (_userDefaultName==null) {
-            _userDefaultName=System.getProperty("BTApplicationName");
-            if (_userDefaultName==null) _userDefaultName=super.name();
+        if (_userDefaultName == null) {
+            _userDefaultName = System.getProperty("BTApplicationName");
+            if (_userDefaultName == null)
+                _userDefaultName = super.name();
         }
         return _userDefaultName;
     }
 
     public Application() {
+        ERXNavigationManager.manager().configureNavigation();
         setContextClassName("er.extensions.ERXWOContext");
         setPageRefreshOnBacktrackEnabled(true);
         // http://myhost:aPort/cgi-bin/WebObjects/MyApp.woa/wa/WOEventSetup
         setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
-        setTimeOut(8*60*60); //set the timeout to 8 hours.
-        Class core = er.corebusinesslogic.ERCoreBusinessLogic.class;
-        Class bug = er.bugtracker.BTBusinessLogic.class;
+        setTimeOut(8 * 60 * 60); // set the timeout to 8 hours.
     }
 
     public void finishInitialization() {
         NSLog.debug.appendln("finishInitialization called.");
         try {
             adjustConnectionDictionary(EOModelGroup.defaultGroup().modelNamed("BugTracker"));
-            boolean runBatchReport=ERXProperties.booleanForKey("BTRunBatchReport");
+            boolean runBatchReport = ERXProperties.booleanForKey("BTRunBatchReport");
             if (runBatchReport) {
                 runBatchReport();
                 System.exit(0);
             }
         } catch (ExceptionInInitializerError e) {
-            NSLog.err.appendln("Original exception "+e.getException());
+            NSLog.err.appendln("Original exception " + e.getException());
         }
     }
 
     public void adjustConnectionDictionary(EOModel model) {
         try {
-            databaseName=(String)model.connectionDictionary().objectForKey("databaseName");
+            databaseName = (String) model.connectionDictionary().objectForKey("databaseName");
         } catch (Exception ex) {
-            NSLog.err.appendln("Original exception : "+ex);
+            NSLog.err.appendln("Original exception : " + ex);
         }
     }
 
-
-    /** we run over all people in the DB and send them a summary email if they have unread bugs */
+    /**
+     * we run over all people in the DB and send them a summary email if they
+     * have unread bugs
+     */
     public void runBatchReport() {
-        EOEditingContext ec=ERXEC.newEditingContext();
+        EOEditingContext ec = ERXEC.newEditingContext();
         try {
-            NSArray everybody=People.clazz.allObjects(ec);
-            for (Enumeration e=everybody.objectEnumerator(); e.hasMoreElements();) {
-                People person=(People)e.nextElement();
-                NSDictionary bindings = new NSDictionary(new Object[] {person}, new Object[] {"user"});
-                NSArray unreadBugs=person.unreadBugs();
-                String email=person.email();
-                if (unreadBugs.count()>0 && email!=null && email.length()!=0) {
+            NSArray everybody = People.clazz.allObjects(ec);
+            for (Enumeration e = everybody.objectEnumerator(); e.hasMoreElements();) {
+                People person = (People) e.nextElement();
+                NSDictionary bindings = new NSDictionary(new Object[] { person }, new Object[] { "user" });
+                NSArray unreadBugs = person.unreadBugs();
+                String email = person.email();
+                if (unreadBugs.count() > 0 && email != null && email.length() != 0) {
                     WOComponent emailBody = pageWithName("BugReportEmail", ERXWOContext.newContext());
-                    emailBody.takeValueForKey(unreadBugs,"unreadBugs");
-                    emailBody.takeValueForKey(person,"owner");
-                    WOMailDelivery.sharedInstance().composeComponentEmail("bugtracker@netstruxr.com", new NSArray(email), null, "You have "+unreadBugs.count()+" unread bug(s)", emailBody, true);
-                    NSLog.debug.appendln("Sending report to "+email+": "+unreadBugs.count()+" unread bugs");
+                    emailBody.takeValueForKey(unreadBugs, "unreadBugs");
+                    emailBody.takeValueForKey(person, "owner");
+                    WOMailDelivery.sharedInstance().composeComponentEmail("bugtracker@netstruxr.com", new NSArray(email), null,
+                            "You have " + unreadBugs.count() + " unread bug(s)", emailBody, true);
+                    NSLog.debug.appendln("Sending report to " + email + ": " + unreadBugs.count() + " unread bugs");
                 }
             }
         } finally {
@@ -86,4 +100,3 @@ public class Application extends ERXApplication {
         }
     }
 }
-
