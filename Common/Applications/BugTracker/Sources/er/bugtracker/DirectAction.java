3be076a58c459df7c846edaa34300c56c9f18ee5
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-06 00:36:54 +0000

    using ERXNavigation instead of standard D2W

--- Common/Applications/BugTracker/Sources/er/bugtracker/DirectAction.java
+++ Common/Applications/BugTracker/Sources/er/bugtracker/DirectAction.java
@@ -6,6 +6,7 @@
  * included with this distribution in the LICENSE.NPL file.  */
 
 package er.bugtracker;
+
 import com.webobjects.foundation.*;
 import com.webobjects.appserver.*;
 import com.webobjects.eocontrol.*;
@@ -21,22 +22,14 @@ public class DirectAction extends ERD2WDirectAction {
         super(aRequest);
     }
 
-    private static EOEditingContext _pingEditingContext;
-    protected static EOEditingContext pingEditingContext() {
-        if (_pingEditingContext == null)
-            _pingEditingContext = ERXEC.newEditingContext();
-        return _pingEditingContext;
-    }
     public WOComponent pingAction() {
         WOComponent result = null;
-        EOEditingContext ec = pingEditingContext();
+        EOEditingContext ec = ERXEC.newEditingContext();
         ec.lock();
-        try{
-            EOUtilities.rawRowsForSQL(ec,
-                                      "bug",
-                                      "select count(*) from PRIORITY");
-            result=pageWithName("ERXSuccess");
-        } catch(Exception e) {
+        try {
+            EOUtilities.rawRowsForSQL(ec, "bug", "select count(*) from PRIORITY", null);
+            result = pageWithName("ERXSuccess");
+        } catch (Exception e) {
         } finally {
             ec.unlock();
         }
@@ -44,24 +37,20 @@ public class DirectAction extends ERD2WDirectAction {
     }
 
     public static People userFromRequest(WORequest r, EOEditingContext ec) {
-        People user=null;
-        //This gets us the encrypted ID stored for the login cookie
-        String  encryptedPrimaryKey=r.cookieValueForKey("BTL");
-        if (encryptedPrimaryKey !=null &&
-            !encryptedPrimaryKey.equals("") &&
-            !encryptedPrimaryKey.equals("-")) {
+        People user = null;
+        // This gets us the encrypted ID stored for the login cookie
+        String encryptedPrimaryKey = r.cookieValueForKey("BTL");
+        if (encryptedPrimaryKey != null && !encryptedPrimaryKey.equals("") && !encryptedPrimaryKey.equals("-")) {
             String clearPrimaryKey = ERXCrypto.blowfishDecode(encryptedPrimaryKey);
-            if (clearPrimaryKey!=null) {
+            if (clearPrimaryKey != null) {
                 clearPrimaryKey = clearPrimaryKey.trim();
                 ec.lock();
-                try{
+                try {
                     Integer clearPrimaryKeyInt = new Integer(clearPrimaryKey);
                     NSDictionary dictionary = new NSDictionary(clearPrimaryKeyInt, new String("id"));
-                    user = (People)EOUtilities.objectWithPrimaryKey(ec,
-                                                                    "People",
-                                                                    dictionary);
-                } catch(NumberFormatException NFe){
-                    //WOApplication.application().logString(NFe.toString());
+                    user = (People) EOUtilities.objectWithPrimaryKey(ec, "People", dictionary);
+                } catch (NumberFormatException NFe) {
+                    // WOApplication.application().logString(NFe.toString());
                 } finally {
                     ec.unlock();
                 }
@@ -73,18 +62,20 @@ public class DirectAction extends ERD2WDirectAction {
     private static WOComponent errorPage(String message, Session session) {
         ErrorPageInterface error = D2W.factory().errorPage(session);
         error.setMessage(message);
-        error.setNextPage(WOApplication.application().pageWithName("Main",session.context()));
-        return (WOComponent)error;
+        error.setNextPage(WOApplication.application().pageWithName("Main", session.context()));
+        return (WOComponent) error;
     }
 
     public static class BugActionCallback implements ERXUtilities.Callback {
         public String numberFromRequest;
+
         public BugActionCallback(String numFromReq) {
             super();
             numberFromRequest = numFromReq;
         }
+
         public Object invoke(Object ctx) {
-            Session session = (Session)ctx;
+            Session session = (Session) ctx;
             WOComponent result = null;
             if ((numberFromRequest == null) || (numberFromRequest.equals(""))) {
                 result = errorPage("Invalid Request", session);
@@ -94,13 +85,13 @@ public class DirectAction extends ERD2WDirectAction {
                 try {
                     Integer bugId = new Integer(numberFromRequest);
                     NSArray bugs = EOUtilities.objectsMatchingKeyAndValue(ec, "Bug", "bugid", bugId);
-                    EOEnterpriseObject bug = bugs.count() > 0 ? (EOEnterpriseObject)bugs.objectAtIndex(0) : null;
+                    EOEnterpriseObject bug = bugs.count() > 0 ? (EOEnterpriseObject) bugs.objectAtIndex(0) : null;
                     if (bug == null) {
                         result = errorPage("Bug not found", session);
                     } else {
-                        EditBug eb = (EditBug)Application.application().pageWithName("EditBug",session.context());
+                        EditBug eb = (EditBug) Application.application().pageWithName("EditBug", session.context());
                         eb.setObject(bug);
-                        eb.setNextPage(Application.application().pageWithName("HomePage",session.context()));
+                        eb.setNextPage(Application.application().pageWithName("HomePage", session.context()));
                         result = eb;
                     }
                 } catch (NumberFormatException nfe) {
@@ -117,61 +108,71 @@ public class DirectAction extends ERD2WDirectAction {
             return result;
         }
     }
+
     public WOComponent bugAction() {
-        BugActionCallback result = new BugActionCallback((String)request().formValueForKey("number"));
+        BugActionCallback result = new BugActionCallback((String) request().formValueForKey("number"));
         return entranceTemplate(result);
     }
 
     private WOComponent entranceTemplate(ERXUtilities.Callback successComponent) {
-        WOComponent result=null;
-        People u = userFromRequest(request(),session().defaultEditingContext());
-        if (u!=null) {
-            ((Session)session()).setUser(u);
-            return (WOComponent)successComponent.invoke(session());
+        WOComponent result = null;
+        People u = userFromRequest(request(), session().defaultEditingContext());
+        if (u != null) {
+            ((Session) session()).setUser(u);
+            return (WOComponent) successComponent.invoke(session());
         } else {
-            Main loginPage = (Main)pageWithName("Main");
+            Main loginPage = (Main) pageWithName("Main");
             loginPage.setNextPageCallback(successComponent);
-            result=loginPage;
+            result = loginPage;
         }
         return result;
     }
 
     public static class EntranceActionCallback implements ERXUtilities.Callback {
         public Object invoke(Object ctx) {
-            return WOApplication.application().pageWithName("HomePage",((Session)ctx).context()) ;
+            return WOApplication.application().pageWithName("HomePage", ((Session) ctx).context());
         }
     }
+
     public WOComponent entranceAction() {
         EntranceActionCallback result = new EntranceActionCallback();
         return entranceTemplate(result);
     }
 
+    public WOComponent homeAction() {
+        EntranceActionCallback result = new EntranceActionCallback();
+        return entranceTemplate(result);
+    }
+
     public WOActionResults defaultAction() {
-        // the reason for this redirect is that the session cookie and the 'remember my login cookie'
+        // the reason for this redirect is that the session cookie and the
+        // 'remember my login cookie'
         // are in xxx/cgi-bin/WebObjects/x.woa domain
         // if you hit xxx.cgi-bin/WebObjects/x, you do not get them!..
-        WORedirect page=(WORedirect)pageWithName("WORedirect");
-        page.setUrl(session().context().directActionURLForActionNamed("entrance",null));
+        WORedirect page = (WORedirect) pageWithName("WORedirect");
+        page.setUrl(session().context().directActionURLForActionNamed("entrance", null));
         return page;
     }
 
     public WOActionResults logoutAction() {
-        WORedirect redirectPage=(WORedirect)pageWithName("WORedirect");
+        WORedirect redirectPage = (WORedirect) pageWithName("WORedirect");
         redirectPage.setUrl("/");
-        if (existingSession()!=null) existingSession().terminate();
+        if (existingSession() != null)
+            existingSession().terminate();
         return redirectPage;
     }
 
-    //CHECKME: this doesn't make sense? Where is "BugsToTransfert" and what is it?
-    public WOComponent transfertDescriptionAction(){
+    // CHECKME: this doesn't make sense? Where is "BugsToTransfert" and what is
+    // it?
+    public WOComponent transfertDescriptionAction() {
         EOEditingContext ec = ERXEC.newEditingContext();
         ec.lock();
         try {
             NSArray bugs = EOUtilities.objectsWithFetchSpecificationAndBindings(ec, "Bug", "BugsToTransfert", null);
-            for(Enumeration e = bugs.objectEnumerator(); e.hasMoreElements();){
-                Bug bug = (Bug)e.nextElement();
-                System.out.println("bug = "+bug.valueForKey("subject"));
-                String description = (String)bug.valueForKey("textDescription");
+            for (Enumeration e = bugs.objectEnumerator(); e.hasMoreElements();) {
+                Bug bug = (Bug) e.nextElement();
+                System.out.println("bug = " + bug.valueForKey("subject"));
+                String description = (String) bug.valueForKey("textDescription");
                 bug.takeValueForKey(description, "textDescription");
                 ec.saveChanges();
             }
