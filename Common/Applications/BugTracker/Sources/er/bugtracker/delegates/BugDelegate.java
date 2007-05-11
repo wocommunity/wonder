c1321fd8dba2cb10cff2aeff6b93200a9dbc6ef6
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-11 13:15:51 +0000

    added some workflow action examples
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@5678 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Applications/BugTracker/Sources/er/bugtracker/delegates/BugDelegate.java
+++ Common/Applications/BugTracker/Sources/er/bugtracker/delegates/BugDelegate.java
@@ -16,11 +16,22 @@ public class BugDelegate extends BranchDelegate {
 
     protected NSArray defaultBranchChoices(D2WContext context) {
         NSArray result = super.defaultBranchChoices(context);
-        Bug bug = (Bug)object(context);
+        log.info("in: " + result);
+         Bug bug = (Bug)object(context);
         // AK: this is just an illustration
         if(!bug.state().equals(State.ANALYZE)) {
             result = choiceByRemovingKeys(new NSArray("delete"), result);
         }
+        if(!bug.state().equals(State.CLOSED)) {
+        	result = choiceByRemovingKeys(new NSArray(new Object[] {"reopen", "view"}), result);
+        }
+        if(bug.state().equals(State.CLOSED)) {
+        	result = choiceByRemovingKeys(new NSArray(new Object[] {"resolve", "edit"}), result);
+        }
+        if(!bug.state().equals(State.VERIFY)) {
+            result = choiceByRemovingKeys(new NSArray("reject"), result);
+        }
+        log.info("out: " + result + " -> " + bug.state().textDescription());
         return result;
     }
 
@@ -34,6 +45,45 @@ public class BugDelegate extends BranchDelegate {
             bug = (Bug) bug.localInstanceIn(peer);
             bug.close();
             epi=(EditPageInterface)D2W.factory().pageForConfigurationNamed("EditBugToClose",session);
+            epi.setObject(bug);
+            epi.setNextPage(sender.context().page());
+        } finally {
+            peer.unlock();
+        }
+
+        return (WOComponent)epi;
+    }
+
+    public WOComponent reopen(WOComponent sender) {
+        Bug bug = (Bug) object(sender);
+        Session session = session(sender);
+        EOEditingContext peer = ERXEC.newEditingContext(bug.editingContext().parentObjectStore());
+        EditPageInterface epi = null;
+        peer.lock();
+        try {
+            bug = (Bug) bug.localInstanceIn(peer);
+            bug.reopen();
+            epi=(EditPageInterface)D2W.factory().pageForConfigurationNamed("EditBugToReopen",session);
+            epi.setObject(bug);
+            epi.setNextPage(sender.context().page());
+        } finally {
+            peer.unlock();
+        }
+
+        return (WOComponent)epi;
+    }
+
+
+    public WOComponent reject(WOComponent sender) {
+        Bug bug = (Bug) object(sender);
+        Session session = session(sender);
+        EOEditingContext peer = ERXEC.newEditingContext(bug.editingContext().parentObjectStore());
+        EditPageInterface epi = null;
+        peer.lock();
+        try {
+            bug = (Bug) bug.localInstanceIn(peer);
+            bug.rejectVerification();
+            epi=(EditPageInterface)D2W.factory().pageForConfigurationNamed("EditBugToRejectVerification",session);
             epi.setObject(bug);
             epi.setNextPage(sender.context().page());
         } finally {
