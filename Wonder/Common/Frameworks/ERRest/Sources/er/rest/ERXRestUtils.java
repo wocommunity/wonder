0d3b4362bfd82f5b5f5cca23a6bca866736adad7
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-21 01:04:18 +0000

    support for String primary keys (sort of)
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5796 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERRest/Sources/er/rest/ERXRestUtils.java
+++ Wonder/Common/Frameworks/ERRest/Sources/er/rest/ERXRestUtils.java
@@ -1,9 +1,11 @@
 package er.rest;
 
+import com.webobjects.eoaccess.EOAttribute;
 import com.webobjects.eoaccess.EOEntity;
 import com.webobjects.eocontrol.EOEnterpriseObject;
 import com.webobjects.eocontrol.EOGlobalID;
 import com.webobjects.eocontrol.EOKeyGlobalID;
+import com.webobjects.foundation.NSArray;
 
 import er.extensions.ERXStringUtilities;
 
@@ -15,13 +17,49 @@ import er.extensions.ERXStringUtilities;
 public class ERXRestUtils {
 	/**
 	 * Returns whether or not the given key value is the primary key of
-	 * an EO.
+	 * an EO.  This is crazy -- It tries to guess if it's looking at
+	 * a key or not.
 	 * 
 	 * @param key the possible EO key
 	 * @return true if key is a primary key
 	 */
-	public static boolean isEOID(String key) {
-		return key != null && ERXStringUtilities.isDigitsOnly(key);
+	public static boolean isEOID(ERXRestKey restKey) {
+		boolean isID = false;
+		String key = restKey.key();
+		if (key != null) {
+			EOEntity entity = restKey.entity();
+			if (ERXStringUtilities.isDigitsOnly(key)) {
+				isID = true;
+			}
+			else if (restKey.previousKey() == null) {
+				if (!restKey.isKeyAll()) {
+					isID = true;
+				}
+			}
+			else {
+				if (restKey.previousKey().isKeyAll()) {
+					isID = true;
+				}
+				else {
+					try {
+						Object previousValue = restKey.previousKey()._value(false);
+						if (previousValue instanceof NSArray) {
+							isID = true;
+						}
+					}
+					catch (ERXRestException e) {
+						throw new RuntimeException("Failed to check key '" + key + "'.", e);
+					}
+					catch (ERXRestSecurityException e) {
+						throw new RuntimeException("Failed to check key '" + key + "'.", e);
+					}
+					catch (ERXRestNotFoundException e) {
+						throw new RuntimeException("Failed to check key '" + key + "'.", e);
+					}
+				}
+			}
+		}
+		return isID;
 	}
 
 	/**
@@ -32,7 +70,20 @@ public class ERXRestUtils {
 	 * @return the globalID
 	 */
 	public static EOGlobalID gidForID(EOEntity entity, String key) {
-		return EOKeyGlobalID.globalIDWithEntityName(entity.name(), new Object[] { Integer.valueOf(key) });
+		EOGlobalID gid;
+		if (ERXStringUtilities.isDigitsOnly(key)) {
+			gid = EOKeyGlobalID.globalIDWithEntityName(entity.name(), new Object[] { Integer.valueOf(key) });
+		}
+		else {
+			NSArray primaryKeyAttributes = entity.primaryKeyAttributes();
+			if (primaryKeyAttributes.count() > 1) {
+				throw new IllegalArgumentException("Compound primary keys (" + entity + ") are not currently supported.");
+			}
+			EOAttribute primaryKeyAttribute = (EOAttribute) primaryKeyAttributes.objectAtIndex(0);
+			String valueType = primaryKeyAttribute.valueType();
+			gid = EOKeyGlobalID.globalIDWithEntityName(entity.name(), new Object[] { key });
+		}
+		return gid;
 	}
 	
 	/**
@@ -42,8 +93,16 @@ public class ERXRestUtils {
 	 * @return the primary key
 	 */
 	public static String idForEO(EOEnterpriseObject eo) {
-		EOKeyGlobalID gid = (EOKeyGlobalID) eo.editingContext().globalIDForObject(eo);
-		Object id = gid.keyValues()[0];
+		EOGlobalID gid = eo.editingContext().globalIDForObject(eo);
+		if (!(gid instanceof EOKeyGlobalID)) {
+			throw new IllegalArgumentException("Unsupported primary key type '" + gid + "'.");
+		}
+		EOKeyGlobalID keyGID = (EOKeyGlobalID) gid;
+		Object[] keyValues = keyGID.keyValues();
+		if (keyValues.length > 1) {
+			throw new IllegalArgumentException("Compound primary keys (" + eo.entityName() + ") are not currently supported.");
+		}
+		Object id = keyValues[0];
 		return String.valueOf(id);
 	}
 }
