c2761ee0ce9a340a81efa7e2fbff491786f19ae0
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-22 02:36:47 +0000

    add convenience bindings for calling common javascript functions

--- Ajax/Ajax/Sources/er/ajax/AjaxFunctionLink.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxFunctionLink.java
@@ -1,23 +1,92 @@
 package er.ajax;
 
+import com.webobjects.appserver.WOAssociation;
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WOResponse;
 import com.webobjects.appserver._private.WOConstantValueAssociation;
+import com.webobjects.appserver._private.WODynamicElementCreationException;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableDictionary;
 
 import er.extensions.ERXHyperlink;
 
 /**
- * AjaxFunctionLink is just like a WOHyperlink except that you 
- * only set onclick, and its href automatically becomes "javascript:void(0)".
- *
- *  @binding onclick the javascript to execute when the link is clicked.
- *  
+ * AjaxFunctionLink is a convenience for calling javascript functions in response to
+ * an onclick.  When the link is inside of an AjaxUpdateContainer, you do not need to
+ * specify the ID of the container.
+ * 
+ * So for instance, if you are inside of an AjaxInPlace with the id "blogForm" you can do
+ * 
+ * <pre>
+ * &lt:wo:AjaxFunctionLink type = "edit"&gt;
+ * </pre>
+ * 
+ * to go into edit mode.
+ * 
+ * @binding onclick the javascript to execute when the link is clicked.
+ * @binding type the type of event to fire ("update", "save", "edit", "cancel")
+ * @binding updateContainerID the id of the container to fire the event to (optional if inside of the container)
+ * 
  * @author mschrag
  */
 public class AjaxFunctionLink extends ERXHyperlink {
+	private WOAssociation _type;
+	private WOAssociation _updateContainerID;
+
 	public AjaxFunctionLink(String aName, NSDictionary associations, WOElement template) {
 		super(aName, AjaxFunctionLink.processAssociations(associations), template);
+		_type = (WOAssociation) _associations.removeObjectForKey("type");
+		_updateContainerID = (WOAssociation) _associations.removeObjectForKey("updateContainerID");
+		if (_associations.objectForKey("onclick") != null && _type != null) {
+			throw new WODynamicElementCreationException("You cannot bind both 'type' and 'onclick' at the same time.");
+		}
+		if (_updateContainerID != null && _type == null) {
+			throw new WODynamicElementCreationException("If you bind 'updateContainerID', you must also bind 'type'.");
+		}
+	}
+
+	public void appendAttributesToResponse(WOResponse response, WOContext context) {
+		super.appendAttributesToResponse(response, context);
+		AjaxFunctionLink._appendAttributesToResponse(response, context, _type, _updateContainerID);
+	}
+	
+	public static void _appendAttributesToResponse(WOResponse response, WOContext context, WOAssociation typeAssociation, WOAssociation updateContainerIDAssociation) {
+		WOComponent component = context.component();
+		String type = (String) typeAssociation.valueInComponent(component);
+		if (type != null) {
+			String updateContainerID;
+			if (updateContainerIDAssociation != null) {
+				updateContainerID = (String) updateContainerIDAssociation.valueInComponent(component);
+			}
+			else {
+				updateContainerID = AjaxUpdateContainer.currentUpdateContainerID();
+			}
+
+			if (updateContainerID == null) {
+				throw new WODynamicElementCreationException("You must either set the 'updateContainerID' binding or the link must be contained inside of an AjaxUpdateContainer.");
+			}
+
+			response.appendContentString(" onclick = \"");
+			response.appendContentString(updateContainerID);
+			if ("edit".equalsIgnoreCase(type)) {
+				response.appendContentString("Edit");
+			}
+			else if ("cancel".equalsIgnoreCase(type)) {
+				response.appendContentString("Cancel");
+			}
+			else if ("save".equalsIgnoreCase(type)) {
+				response.appendContentString("Save");
+			}
+			else if ("update".equalsIgnoreCase(type)) {
+				response.appendContentString("Update");
+			}
+			else {
+				throw new WODynamicElementCreationException("Unknown AjaxInPlace type '" + type + "'.  Must be one of 'edit', 'cancel', 'save', or 'Update'.");
+			}
+			response.appendContentString("()\"");
+		}
 	}
 
 	protected static NSDictionary processAssociations(NSDictionary associations) {
