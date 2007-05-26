8144420d076d7bcbb5dfc1a247579e0db45da974
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-26 17:04:41 +0000

    regenerated with fixed templates

--- Common/Applications/BugTracker/Frameworks/BTBusinessLogic/Sources/er/bugtracker/_Release.java
+++ Common/Applications/BugTracker/Frameworks/BTBusinessLogic/Sources/er/bugtracker/_Release.java
@@ -21,6 +21,12 @@ public abstract class _Release extends ERXGenericRecord {
 
     public static abstract class _ReleaseClazz extends ERXGenericRecord.ERXGenericRecordClazz {
 
+        public NSArray objectsForFetchAll(EOEditingContext context) {
+            EOFetchSpecification spec = EOFetchSpecification.fetchSpecificationNamed("FetchAll", "Release");
+
+            return context.objectsWithFetchSpecification(spec);
+        }
+
     }
 
 
