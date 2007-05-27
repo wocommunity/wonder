6305f32ff40d28d34da6c8319fe060cf200a1e6a
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-27 13:06:13 +0000

    ERD2WQueryPage now implements ERDQueryPageInterface
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5888 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryPageInterface.java
+++ Wonder/Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDQueryPageInterface.java
@@ -8,9 +8,24 @@ package er.directtoweb;
 
 import com.webobjects.directtoweb.NextPageDelegate;
 import com.webobjects.directtoweb.QueryPageInterface;
+import com.webobjects.eocontrol.EODataSource;
 
-public interface ERDQueryPageInterface extends QueryPageInterface{
+public interface ERDQueryPageInterface extends QueryPageInterface {
 
+    // CHECKME AK: what's this for?
     public void setCancelDelegate(NextPageDelegate cancelDelegate);
+    
+    /**
+     * Preselect a query match value
+     * @param value
+     * @param selector
+     * @param key
+     */
+    public void setQueryMatchForKey(Object value, String selector, String key);
+    
+    /**
+     * Returns the query datasource. Not having this is annoying...
+     */
+    public EODataSource queryDataSource();
     
 }
