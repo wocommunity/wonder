780bc7c439cd57307e77ce3f09df17bee696bb33
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-28 14:03:29 +0000

    current user can't delete himself
    git-svn-id: file:///Users/q/working/wondersvn/trunk@5915 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Applications/BugTracker/Sources/er/bugtracker/delegates/PeopleDelegate.java
+++ Wonder/Common/Applications/BugTracker/Sources/er/bugtracker/delegates/PeopleDelegate.java
@@ -3,9 +3,18 @@ package er.bugtracker.delegates;
 import com.webobjects.directtoweb.D2WContext;
 import com.webobjects.foundation.NSArray;
 
+import er.bugtracker.People;
+import er.extensions.ERXEOControlUtilities;
+
 public class PeopleDelegate extends BranchDelegate {
 
     protected NSArray defaultBranchChoices(D2WContext context) {
-        return super.defaultBranchChoices(context);
+        NSArray result = super.defaultBranchChoices(context);
+        People people = (People) object(context);
+        if(ERXEOControlUtilities.eoEquals(people, People.clazz.currentUser(people.editingContext()))) {
+            result = choiceByRemovingKeys(new NSArray("delete"), result);
+            result = choiceByRemovingKeys(new NSArray("view"), result);
+        }
+        return result;
     }
 }
