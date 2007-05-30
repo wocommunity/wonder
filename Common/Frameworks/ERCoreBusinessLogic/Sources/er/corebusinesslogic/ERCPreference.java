de8331b08414858888042ff7cdc2855aa69255b6
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-05-30 00:29:47 +0000

    better UI display

--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCPreference.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCPreference.java
@@ -4,12 +4,16 @@ package er.corebusinesslogic;
 import org.apache.log4j.Logger;
 
 import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOKeyValueUnarchiver;
 import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSPropertyListSerialization;
 
 import er.extensions.EOEnterpriseObjectClazz;
 
 public class ERCPreference extends _ERCPreference {
     static final Logger log = Logger.getLogger(ERCPreference.class);
+    private final static String VALUE="_V";
 
     public ERCPreference() {
         super();
@@ -20,6 +24,16 @@ public class ERCPreference extends _ERCPreference {
     }
     
     
+    public String userPresentableDescription() {
+        return key() + ": " + decodedValue();
+    }
+    
+    protected Object decodedValue() {
+        NSDictionary d = (NSDictionary )NSPropertyListSerialization.propertyListFromString(value());
+        EOKeyValueUnarchiver u = new EOKeyValueUnarchiver(d);
+        return u.decodeObjectForKey(VALUE);
+    }    
+
     // Class methods go here
     
     public static class ERCPreferenceClazz extends _ERCPreferenceClazz {
