768d1170b6e0606feeb6dce3789a666bdbd42c82
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-06-04 21:47:56 +0000

    Enable POJO state support

--- Common/Applications/BugTracker/Frameworks/BTBusinessLogic/Sources/er/bugtracker/State.java
+++ Common/Applications/BugTracker/Frameworks/BTBusinessLogic/Sources/er/bugtracker/State.java
@@ -7,34 +7,47 @@ import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOSharedEditingContext;
 import com.webobjects.foundation.NSArray;
 
-public class State extends _State {
+import er.corebusinesslogic.ERCMailState;
+import er.extensions.ERXConstant;
+
+/**
+ * State is an example of a POJO. As it is a string in the DB, you need to provide a factory method.
+ * @author ak
+ *
+ */
+public class State extends ERXConstant.StringConstant {
+    
     static final Logger log = Logger.getLogger(State.class);
 
-    public static State ANALYZE;
-    public static State BUILD;
-    public static State VERIFY;
-    public static State DOCUMENT;
-    public static State CLOSED;
+    public static State ANALYZE = new State("anzl", "Analyze", 1);
+    public static State BUILD = new State("buld", "Build", 2);
+    public static State VERIFY = new State("vrfy", "Verify", 3);
+    public static State DOCUMENT = new State("dcmt", "Document", 4);
+    public static State CLOSED = new State("clsd", "Closed", 5);
 
-    public State() {
-        super();
-    }
-
-    public void init(EOEditingContext ec) {
-        super.init(ec);
-    }
-
-
-    // Class methods go here
+    private int _sortOrder;
     
-    public static class StateClazz extends _StateClazz {
+    public State(String value, String name, int sortOrder) {
+        super(value, name);
+        _sortOrder = sortOrder;
+    }
+    
+    public int sortOrder() {
+        return _sortOrder;
+    }
+    
+    public String textDescription() {
+        return name();
+    }
+    
+    public static class StateClazz {
 
     	public NSArray allObjects(EOEditingContext ec) {
     		return new NSArray(new Object[] {ANALYZE, BUILD, VERIFY, DOCUMENT, CLOSED});
     	}
 
     	public State sharedStateForKey(String key) {
-            return (State)objectWithPrimaryKeyValue(EOSharedEditingContext.defaultSharedEditingContext(), key);
+            return (State) ERXConstant.constantForClassNamed(key, State.class.getName());
         }
 
         public void initializeSharedData() {
@@ -44,6 +57,10 @@ public class State extends _State {
             State.DOCUMENT = sharedStateForKey("dcmt");
             State.CLOSED = sharedStateForKey("clsd");
         }
+    }
+    
+    public static State state(String key) {
+        return (State) constantForClassNamed(key, State.class.getName());
     }
 
     public static final StateClazz clazz = new StateClazz();
