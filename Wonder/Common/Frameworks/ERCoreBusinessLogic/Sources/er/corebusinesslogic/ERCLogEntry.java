1b5618f86d3797a4000fad52d8d43afc199e0709
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-06-05 18:40:39 +0000

    - xxxClazz()->clazz- static methods in Clazz now non-static- more sensible check for ERCLogEntry interface
    git-svn-id: file:///Users/q/working/wondersvn/trunk@6021 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCLogEntry.java
+++ Wonder/Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCLogEntry.java
@@ -1,6 +1,7 @@
 // ERCLogEntry.java
 // (c) by Anjo Krank (ak@kcmedia.ag)
 package er.corebusinesslogic;
+
 import org.apache.log4j.Logger;
 
 import com.webobjects.eocontrol.EOEditingContext;
@@ -21,36 +22,33 @@ public class ERCLogEntry extends _ERCLogEntry {
         super.init(ec);
         setCreated(new NSTimestamp());
         EOEnterpriseObject actor = ERCoreBusinessLogic.actor(ec);
-        if (actor!=null) {
-            setUserID((Integer)((ERXGenericRecord)actor).rawPrimaryKey());
+        if (actor != null) {
+            setUserID((Integer) ((ERXGenericRecord) actor).rawPrimaryKey());
         }
     }
-    
-    
+
     // Class methods go here
-    
+
     public static class ERCLogEntryClazz extends _ERCLogEntryClazz {
-        
+        // Logging support
+        public ERCLogEntry createLogEntryLinkedToEO(EOEnterpriseObject type, String text, 
+                EOEnterpriseObject eo, String relationshipKey) {
+            EOEditingContext editingContext = eo.editingContext();
+            ERCLogEntry logEntry = (ERCLogEntry) ERCLogEntry.clazz.createAndInsertObject(editingContext);
+            if (type != null) {
+                // CHECKME: (ak) what's type supposed to do??
+                // logEntry.addObjectToBothSidesOfRelationshipWithKey(type,"type");
+            }
+            if (relationshipKey != null) {
+                // CHECKME: (ak) what's relationshipKey supposed to do??
+                // logEntry.addObjectToBothSidesOfRelationshipWithKey(eo,relationshipKey);
+            }
+            logEntry.setText(text);
+            return logEntry;
+        }
+
     }
 
-    public static ERCLogEntryClazz logEntryClazz() { return (ERCLogEntryClazz)EOEnterpriseObjectClazz.clazzForEntityNamed("ERCLogEntry"); }
+    public static ERCLogEntryClazz clazz = new ERCLogEntryClazz();
 
-    // Logging support
-    public static ERCLogEntry createLogEntryLinkedToEO(EOEnterpriseObject type,
-                                                       String text,
-                                                       EOEnterpriseObject eo,
-                                                       String relationshipKey) {
-        EOEditingContext editingContext=eo.editingContext();
-        ERCLogEntry logEntry = (ERCLogEntry)ERCLogEntry.logEntryClazz().createAndInsertObject(editingContext);
-        if(type != null) {
-            // CHECKME: (ak) what's type supposed to do??
-            // logEntry.addObjectToBothSidesOfRelationshipWithKey(type,"type");
-        }
-        if(relationshipKey != null) {
-            // CHECKME: (ak) what's relationshipKey supposed to do??
-            // logEntry.addObjectToBothSidesOfRelationshipWithKey(eo,relationshipKey);
-        }
-        logEntry.setText(text);
-        return logEntry;
-    }    
 }
