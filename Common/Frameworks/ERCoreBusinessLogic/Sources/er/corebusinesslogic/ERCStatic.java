2cc841443b5c20e63a34180e51fdef766df3c210
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-06-05 18:50:25 +0000

    removed model-based fetch spec
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6022 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCStatic.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/ERCStatic.java
@@ -3,8 +3,11 @@
 package er.corebusinesslogic;
 import org.apache.log4j.Logger;
 
+import com.webobjects.eoaccess.EOUtilities;
 import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOFetchSpecification;
+import com.webobjects.eocontrol.EOGlobalID;
 import com.webobjects.eocontrol.EOObjectStoreCoordinator;
 import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSKeyValueCoding;
@@ -30,24 +33,22 @@ public class ERCStatic extends _ERCStatic {
             return objectMatchingKey(ec, key, false);
         }
         
-        public NSArray preferencesWithKey(EOEditingContext ec, String key) {
-        	return objectsForPreferences(ec, key);
+        public NSArray staticsForKey(EOEditingContext ec, String key) {
+            return EOUtilities.objectsMatchingKeyAndValue(ec, "ERCStatic", Key.KEY, key);
         }
         
         public ERCStatic objectMatchingKey(EOEditingContext ec, String key, boolean noCache) {
             // If noCache is true we always go to the database
             Object result = noCache ? null : _staticsPerKey.objectForKey(key);
             if (result == null) {
-                NSArray arr = preferencesWithKey(ec, key);
+                NSArray arr = staticsForKey(ec, key);
                 if (arr.count() > 1)
                     throw new IllegalStateException("Found " + arr.count() + " rows for key " + key);
                 result = arr.count() == 1 ? arr.objectAtIndex(0) : NSKeyValueCoding.NullValue;
                 if (result instanceof EOEnterpriseObject) {
                     privateEditingContext().lock();
                     try {
-                        _staticsPerKey.setObjectForKey(ERXEOControlUtilities.localInstanceOfObject(privateEditingContext(),
-                                                                                                   (ERCStatic)result),
-                                                       key);                        
+                        _staticsPerKey.setObjectForKey(ERXEOControlUtilities.localInstanceOfObject(privateEditingContext(), (ERCStatic) result), key);
                     } finally {
                         privateEditingContext().unlock();
                     }
@@ -56,14 +57,14 @@ public class ERCStatic extends _ERCStatic {
             } else if (result instanceof EOEnterpriseObject) {
                 privateEditingContext().lock();
                 try {
-                    result = ERXEOControlUtilities.localInstanceOfObject(ec, (ERCStatic)result);
+                    result = ERXEOControlUtilities.localInstanceOfObject(ec, (ERCStatic) result);
                 } finally {
                     privateEditingContext().unlock();
-                }                
+                }
             } else if (result.equals(NSKeyValueCoding.NullValue)) {
                 result = null;
             }
-            return (ERCStatic)result;
+            return (ERCStatic) result;
         }
 
         public void invalidateCache() { _staticsPerKey.removeAllObjects(); }
@@ -91,7 +92,7 @@ public class ERCStatic extends _ERCStatic {
         }
         
         public String staticStoredValueForKey(EOEditingContext ec, String key, boolean noCache) {
-            ERCStatic entry = ERCStatic.clazz.objectMatchingKey(ec, key, noCache);
+            ERCStatic entry = objectMatchingKey(ec, key, noCache);
             return entry != null ? entry.value() : null;
         }
 
@@ -157,6 +158,7 @@ public class ERCStatic extends _ERCStatic {
                                                        String value,
                                                        String key) {
             ERCStatic entry = ERCStatic.clazz.objectMatchingKey(editingContext,key);
+            // AK: this is totally thread unsafe...
             if (entry==null) {
                 entry=(ERCStatic)ERXEOControlUtilities.createAndInsertObject(editingContext, "ERCStatic");
                 entry.setKey(key);
