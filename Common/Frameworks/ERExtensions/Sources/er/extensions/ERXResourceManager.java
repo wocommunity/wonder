57f5d3b6ad2e5d437a90dcc8bdfb75d47aa8faf0
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-06-18 14:40:45 +0000

    WO 5.4/5.3 compatibility

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXResourceManager.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXResourceManager.java
@@ -110,8 +110,10 @@ public class ERXResourceManager extends WOResourceManager {
 			String encoded = WOURLEncoder.encode(fileURL);
 			WOContext context = null;
 			String key = WOApplication.application().resourceRequestHandlerKey();
-			//AK: 5.4
-			// key =  "_wr_";
+			if (ERXApplication.isWO54()) {
+				// AK: 5.4
+				key =  "_wr_";
+			}
 			if(request != null) {
 				context = (WOContext) request.valueForKey("context");
 			}
