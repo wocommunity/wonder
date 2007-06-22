a4d9d4f1acec1f9d741d4604bdc113e44161bbab
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2007-06-22 00:43:37 +0000

    Backported changes from trunk to get better implementations of queryAction and selectAction, along with a few other assoted improvements.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditRelationshipPage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditRelationshipPage.java
@@ -7,20 +7,28 @@
 
 package er.directtoweb;
 
-import com.webobjects.foundation.*;
-import com.webobjects.appserver.*;
-import com.webobjects.eocontrol.*;
-import com.webobjects.eoaccess.*;
-import com.webobjects.directtoweb.*;
-import er.extensions.*;
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2W;
+import com.webobjects.directtoweb.D2WEditRelationshipPage;
+import com.webobjects.directtoweb.EditPageInterface;
+import com.webobjects.directtoweb.Services;
+import com.webobjects.eoaccess.EOUtilities;
+import com.webobjects.eocontrol.EODetailDataSource;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOEnterpriseObject;
+
+import er.extensions.ERXEC;
+import er.extensions.ERXValueUtilities;
+import er.extensions.ERXLogger;
 
 public class ERD2WEditRelationshipPage extends D2WEditRelationshipPage {
-	
-	/** logging support */
+
+    /** logging support */
     public final static ERXLogger log = ERXLogger.getERXLogger(ERD2WEditRelationshipPage.class);
 
-	
-	/** interface for all the keys used in this pages code */
+
+    /** interface for all the keys used in this pages code */
     public static interface Keys {
         public static final String isEntityEditable = "isEntityEditable";
         public static final String readOnly = "readOnly";
@@ -36,28 +44,52 @@ public class ERD2WEditRelationshipPage extends D2WEditRelationshipPage {
         super(c);
     }
 
+    /**
+     * Overridden because the action bound should not return null.
+     */
+    public WOComponent queryAction() {
+        WOComponent nextPage = super.queryAction();
+        if(nextPage == null) {
+            nextPage = context().page();
+        }
+        return nextPage;
+    }
 
-    
+    /**
+     * Overridden because the action bound should not return null.
+     */
+    public WOComponent selectAction() {
+        WOComponent nextPage = super.selectAction();
+        if(nextPage == null) {
+            nextPage = context().page();
+        }
+        return nextPage;
+    }
+
     public WOComponent editObjectInRelationship(){
         WOComponent result = null;
-        if (log.isDebugEnabled()) { log.debug("browserSelections = " + browserSelections); }
-        if(browserSelections != null && browserSelections.count() == 1)
-        {
+        if(browserSelections != null && browserSelections.count() == 1) {
+            String editConfigurationName = (String)d2wContext().valueForKey("editConfigurationName");
+            EditPageInterface epi;
+            if(editConfigurationName != null && editConfigurationName.length() > 0) {
+                epi = (EditPageInterface)D2W.factory().pageForConfigurationNamed(editConfigurationName,session());
+            } else {
+                epi = D2W.factory().editPageForEntityNamed(object().entityName(),session());
+            }
             EOEnterpriseObject eo = (EOEnterpriseObject)browserSelections.objectAtIndex(0);
-            EditPageInterface epi = D2W.factory().editPageForEntityNamed(eo.entityName(), session());
             epi.setObject(eo);
             epi.setNextPage(context().page());
             result = (WOComponent)epi;
         }
         return result;
     }
-    
+
     public void setMasterObjectAndRelationshipKey(EOEnterpriseObject eo, String relationshipKey) {
-        EOEditingContext ec = ERXExtensions.newEditingContext(eo.editingContext(), false); // no validation;
+        EOEditingContext ec = ERXEC.newEditingContext(eo.editingContext(), false); // no validation;
         setEditingContext(ec);
         EOEnterpriseObject localEO = EOUtilities.localInstanceOfObject(ec, eo);
         setObject(localEO);
-        _relationshipKey = relationshipKey; 
+        _relationshipKey = relationshipKey;
         if (object().isToManyKey(relationshipKey))
             isRelationshipToMany = true;
         else
@@ -79,32 +111,32 @@ public class ERD2WEditRelationshipPage extends D2WEditRelationshipPage {
         relationshipDisplayGroup.fetch();
         return null;
     }
-	
-	/** 
-	 * Checks if the entity is read-only, meaning that you can't edit its objects.
+
+    /**
+     * Checks if the entity is read-only, meaning that you can't edit its objects.
      *
-	 * Three factors influence this evaluation:
-	 * <ol>
-	 * 	<li>The default implementation of {@link com.webobjects.directtoweb.D2WComponent#isEntityReadOnly isEntityReadOnly}</li>
-	 * 	<li>The value of the <code>isEntityEditable</code> rule from the D2WContext.</li>
-	 * 	<li>The value of the <code>readOnly</code> rule from the D2WContext (with no rule-engine inference).</li>
-	 * </ol>
-	 *
+     * Three factors influence this evaluation:
+     * <ol>
+     *  <li>The default implementation of {@link com.webobjects.directtoweb.D2WComponent#isEntityReadOnly isEntityReadOnly}</li>
+     *  <li>The value of the <code>isEntityEditable</code> rule from the D2WContext.</li>
+     *  <li>The value of the <code>readOnly</code> rule from the D2WContext (with no rule-engine inference).</li>
+     * </ol>
+     *
      * Use <code>isEntityEditable</code> or <code>readOnly</code> rules to override the default behavior.
      *
-	 * @return true if the entity is considered read-only
-	 */
-	public boolean isEntityReadOnly() {
-		boolean flag = super.isEntityReadOnly(); // First, check super's implementation.
-        
+     * @return true if the entity is considered read-only
+     */
+    public boolean isEntityReadOnly() {
+        boolean flag = super.isEntityReadOnly(); // First, check super's implementation.
+
         // Check isEntityEditable.  Use ! because isReadOnly and isEditable are mutually exclusive.
         flag = !ERXValueUtilities.booleanValueWithDefault(d2wContext().valueForKey(Keys.isEntityEditable), !flag);
-        
+
         // Check readOnly.
         // Need no inference else context computes value by querying super's method isEntityReadOnly again.
         flag = ERXValueUtilities.booleanValueWithDefault(d2wContext().valueForKeyNoInference(Keys.readOnly), flag);
-        
+
         return flag;
     }
-    
+
 }
