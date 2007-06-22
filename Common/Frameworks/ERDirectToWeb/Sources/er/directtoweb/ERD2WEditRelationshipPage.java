dc40d83f5d96a219f1a65d492e9ead543d75af78
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2007-06-22 00:24:18 +0000

    Added isEntityReadOnly method to ERD2WEditRelationshipPage in order to have more control over its behavior--specifically when the edit and new buttons are available to the user.\n\nAdded a WOConditional around the Edit button in the ERNEUEditRelationshipPage that controls visibility of the button based on the isEntityReadOnly method.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditRelationshipPage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WEditRelationshipPage.java
@@ -15,6 +15,16 @@ import com.webobjects.directtoweb.*;
 import er.extensions.*;
 
 public class ERD2WEditRelationshipPage extends D2WEditRelationshipPage {
+	
+	/** logging support */
+    public final static ERXLogger log = ERXLogger.getERXLogger(ERD2WEditRelationshipPage.class);
+
+	
+	/** interface for all the keys used in this pages code */
+    public static interface Keys {
+        public static final String isEntityEditable = "isEntityEditable";
+        public static final String readOnly = "readOnly";
+    }
 
     private String _relationshipKey;
 
@@ -30,7 +40,7 @@ public class ERD2WEditRelationshipPage extends D2WEditRelationshipPage {
     
     public WOComponent editObjectInRelationship(){
         WOComponent result = null;
-        System.out.println("browserSelections = "+browserSelections);
+        if (log.isDebugEnabled()) { log.debug("browserSelections = " + browserSelections); }
         if(browserSelections != null && browserSelections.count() == 1)
         {
             EOEnterpriseObject eo = (EOEnterpriseObject)browserSelections.objectAtIndex(0);
@@ -68,6 +78,33 @@ public class ERD2WEditRelationshipPage extends D2WEditRelationshipPage {
         dataSource().deleteObject((EOEnterpriseObject) object().valueForKeyPath(_relationshipKey));
         relationshipDisplayGroup.fetch();
         return null;
+    }
+	
+	/** 
+	 * Checks if the entity is read-only, meaning that you can't edit its objects.
+     *
+	 * Three factors influence this evaluation:
+	 * <ol>
+	 * 	<li>The default implementation of {@link com.webobjects.directtoweb.D2WComponent#isEntityReadOnly isEntityReadOnly}</li>
+	 * 	<li>The value of the <code>isEntityEditable</code> rule from the D2WContext.</li>
+	 * 	<li>The value of the <code>readOnly</code> rule from the D2WContext (with no rule-engine inference).</li>
+	 * </ol>
+	 *
+     * Use <code>isEntityEditable</code> or <code>readOnly</code> rules to override the default behavior.
+     *
+	 * @return true if the entity is considered read-only
+	 */
+	public boolean isEntityReadOnly() {
+		boolean flag = super.isEntityReadOnly(); // First, check super's implementation.
+        
+        // Check isEntityEditable.  Use ! because isReadOnly and isEditable are mutually exclusive.
+        flag = !ERXValueUtilities.booleanValueWithDefault(d2wContext().valueForKey(Keys.isEntityEditable), !flag);
+        
+        // Check readOnly.
+        // Need no inference else context computes value by querying super's method isEntityReadOnly again.
+        flag = ERXValueUtilities.booleanValueWithDefault(d2wContext().valueForKeyNoInference(Keys.readOnly), flag);
+        
+        return flag;
     }
     
 }
