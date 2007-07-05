63adfd03b248629af9ed63e10e3d86b5d83a9c30
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2007-07-05 22:34:25 +0000

    Fixes for inheritance related bugs, refactoring
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6145 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/eoaccess/entityordering/ERXEntityOrder.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/eoaccess/entityordering/ERXEntityOrder.java
@@ -9,9 +9,10 @@ import com.webobjects.eoaccess.EOEntity;
 import com.webobjects.eoaccess.EOModelGroup;
 import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSComparator;
+import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSMutableDictionary;
-import com.webobjects.foundation.NSMutableSet;
+import com.webobjects.foundation.NSSet;
 
 import er.extensions.ERXArrayUtilities;
 import er.extensions.ERXModelGroup;
@@ -19,10 +20,11 @@ import er.extensions.ERXModelGroup;
 
 /**
  * Abstract class defining an ordering of EOEntities that also provides NSComparators to sort entities based on this ordering.
+ * The ordering is based on groups of entities with entities within each group having no defined order.
  *
  * <p>This is implemented by creating a dictionary of entity name to group. Group 1 is entities with no dependencies. Group 2 is entities with
  * dependencies on entities in group 1. Group 3 is entities with dependencies on entities in groups 1 and 2. etc.
- * The dependencies between entities are determined by the abstract <code>buildDependancyList(NSMutableArray)</code>.</p>
+ * The dependencies between entities are determined by the abstract <code>NSDictionary dependenciesByEntity()</code>.</p>
  *
  * @author chill
  */
@@ -42,12 +44,12 @@ public abstract class ERXEntityOrder
     public ERXEntityOrder(EOModelGroup modelGroup) {
         super();
         createListOfEntities(modelGroup);
-        generateOrder();
+        generateOrdering();
     }
 
 
     /**
-     * Convenience constructor for implementing classes.  Uses EOModelGroup.defaultGroup().
+     * Convenience constructor for implementing classes.  Uses <code>EOModelGroup.defaultGroup()</code>.
      */
     public ERXEntityOrder() {
         this(EOModelGroup.defaultGroup());
@@ -55,10 +57,12 @@ public abstract class ERXEntityOrder
 
 
     /**
-     * Returns dictionary of entity name to group. Group 1 is entities with no dependencies. Group 2 is entities with
-     * dependencies on entities in group 1. Group 3 is entities with dependencies on entities in groups 1 and 2. etc
+     * Returns dictionary of group numbers (<code>java.lang.Integer</code>) to entity names.
+     * Group 1 is entities with no dependencies. Group 2 is entities with
+     * dependencies on entities in group 1. Group 3 is entities with
+     * dependencies on entities in groups 1 and 2. etc
      *
-     * @return the groupedEntities dictionary of entity name to group
+     * @return dictionary of group numbers to entity names
      */
     public NSMutableDictionary groupedEntities() {
         return groupedEntities;
@@ -66,92 +70,91 @@ public abstract class ERXEntityOrder
 
 
     /**
-     * Processes <code>allEntities()</code> to determine the dependencies between them.
+     * Processes <code>allEntities()</code> and returns a dictionary keyed on
+     * <code>dependencyKeyFor(EOEntity)</code>. The keys are usually <code>entity.name()</code> but are
+     * not required to be. The value associated with each key is an NSSet of the
+     * entity names that have a dependency on the key. This dictionary is used
+     * to determine the dependency ordering.
+     *
+     * @return a dictionary keyed on dependencyKeyFor(EOEntity)
      */
-    protected abstract void buildDependancyList();
+    protected abstract NSDictionary dependenciesByEntity();
 
 
     /**
-     * @return the key into an EOEntity's <code>userInfo()</code> dictionary where the dependency information
-     * for this class will be stored
+     * Calls <code>dependenciesByEntity()</code> to determine dependencies and processes entities
+     * in <code>allEntities()</code> to generate the <code>groupedEntities()</code> dictionary.
      */
-    protected abstract String userInfoKey();
+    protected void generateOrdering() {
 
-
-    /**
-     * Processes all entities in all models and generates the <code>groupedEntities()</code> dictionary.
-     */
-    protected void generateOrder() {
-        buildDependancyList();
-
+        NSDictionary dependencies = dependenciesByEntity();
         NSMutableArray entities = allEntities().mutableClone();
         int groupNum = 1;
         while (entities.count() > 0) {
-            // Entities that are eligible for this group are not added to the master list
-            // immediately to avoid dependencies within the group
+            // Entities that are eligible for this group are NOT added to the master list
+            // immediately to avoid dependencies between entities in the same group
             NSMutableDictionary groupDictionary = new NSMutableDictionary();
 
             Integer group = new Integer(groupNum++);
             logger.trace("Building group " + group);
+
+            // Examine each entity not already in a group and add it to this group if
+            // all of its dependencies are in previously processed groups.
             int index = 0;
             while (index < entities.count()) {
                 EOEntity entity = (EOEntity) entities.objectAtIndex(index);
                 logger.trace("Processing entity " + entity.name());
 
-                // Ignore Prototypes
-                if (ERXModelGroup.isPrototypeEntity(entity)) {
-                    logger.trace("Ignoring prototype entity " + entity.name());
-                    entities.removeObjectAtIndex(index);
-                }
-                else if (hasDependenciesForEntity(entity)) {
+                if (hasDependenciesForEntity(dependencies, entity)) {
                     logger.trace("Adding entity " + entity.name() + " to group " + group);
                     groupDictionary.setObjectForKey(group, entity.name());
                     entities.removeObjectAtIndex(index);
                 }
                 else {
-                    // This entity can't be processed yet, it will get added to a later group
+                    // This entity still has unresolved dependencies, it will get added to a later group
                     index++;
                 }
             }
 
+            // If an error is found, log out information to make debugging easier
             if (groupDictionary.count() == 0) {
                 logger.error("Stopping, circular relationships found for " + entities.valueForKey("name"));
+                NSSet remainingEntities = new NSSet((NSArray)entities.valueForKey("name"));
+                for (int i = 0; i < entities.count(); i++) {
+                    EOEntity entity = (EOEntity) entities.objectAtIndex(i);
+                    NSSet remainingDependencies = dependentEntities(dependencies, entity).setByIntersectingSet(remainingEntities);
+                    logger.error(entity.name() + " has dependencies on " + remainingDependencies);
+                }
                 throw new RuntimeException("Circular relationships found for " + entities.valueForKey("name"));
             }
 
             groupedEntities().addEntriesFromDictionary(groupDictionary);
         }
-    }
 
-
-    /**
-     * Returns the list of the names of the entities that reference (depend on) this entity. This list
-     * is populated by buildDependancyList(). If buildDependancyList() has not finished executing, the list returned by
-     * this method may not be complete. The list is stored in the entity's userInfo dictionary. This is a convenience
-     * for accessing <code>entity.userInfo().objectForKey(userInfoKey())</code>.
-     *
-     * @param entity
-     *            EOEntity to return list of referencing entities for
-     * @return list of names of entities previously recorded as referencing this entity
-     */
-    protected NSMutableSet entitiesDependentOn(EOEntity entity) {
-        NSMutableSet referencingEntities = (NSMutableSet) entity.userInfo().objectForKey(userInfoKey());
-        if (referencingEntities == null) {
-            referencingEntities = new NSMutableSet();
-            NSMutableDictionary userInfo = new NSMutableDictionary(entity.userInfo());
-            userInfo.setObjectForKey(referencingEntities, userInfoKey());
-            entity.setUserInfo(userInfo);
+        if (logger.isTraceEnabled()) {
+            logger.trace("Entity groups in dependency order:");
+            for (int i = 1; i < groupNum; i++) {
+                logger.trace("Group " + i + ": " + groupedEntities().allKeysForObject(new Integer(i)));
+                logger.trace("");
+            }
         }
-        return referencingEntities;
-    }
+     }
 
 
     /**
+     * @param dependencies dictionary from <code>dependenciesByEntity()</code>
      * @param entity entity to check for dependencies
-     * @return true if <code>groupedEntities()</code> has all the entities named in <code>entitiesWithConstraintsOn(entity)</code>.
+     *
+     * @return true if <code>groupedEntities()</code> has all the entities named in <code>dependentEntities(dependencies, entity)</code>.
      */
-    protected boolean hasDependenciesForEntity(EOEntity entity) {
-        for (Iterator nameIterator = entitiesDependentOn(entity).iterator(); nameIterator.hasNext();) {
+    protected boolean hasDependenciesForEntity(NSDictionary dependencies, EOEntity entity) {
+        // Abstract entities etc may not have an entry
+        if (dependentEntities(dependencies, entity) == null)
+        {
+            return true;
+        }
+
+        for (Iterator nameIterator = dependentEntities(dependencies, entity).iterator(); nameIterator.hasNext();) {
             String entityName = (String) nameIterator.next();
             if (groupedEntities().objectForKey(entityName) == null) {
                 return false;
@@ -159,6 +162,28 @@ public abstract class ERXEntityOrder
         }
 
         return true;
+    }
+
+
+    /**
+     * @param dependencies result from <code>dependenciesByEntity()</code>
+     * @param entity EOEntity to return dependencies set for
+     *
+     * @return set of names of entities that are dependent on entity
+     */
+    protected NSSet dependentEntities(NSDictionary dependencies, EOEntity entity) {
+        return (NSSet) dependencies.objectForKey(dependencyKeyFor(entity));
+    }
+
+
+    /**
+     * This implementation returns <code>entity.name()</code>.
+     * @param entity EOEntity to return key into dependency dictionary for
+     *
+     * @return key for <code>entity</code> into dependency dictionary returned by <code>dependenciesByEntity()</code>
+     */
+    protected String dependencyKeyFor(EOEntity entity) {
+        return entity.name();
     }
 
 
@@ -188,7 +213,6 @@ public abstract class ERXEntityOrder
     public NSArray allEntities() {
         return allEntities;
     }
-
 
 
     /**
