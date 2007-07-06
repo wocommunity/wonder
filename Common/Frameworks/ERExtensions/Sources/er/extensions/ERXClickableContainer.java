3a8c46f314e6ece4f97efbb7e538a93cdfee4e2e
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-07-06 21:39:32 +0000

    ERXClickableContainer

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXClickableContainer.java
@@ -0,0 +1,49 @@
+package er.extensions;
+
+import com.webobjects.appserver.WOAssociation;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.appserver._private.WOConstantValueAssociation;
+import com.webobjects.appserver._private.WOGenericContainer;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableDictionary;
+
+/**
+ * ERXClickableContainer is a WOGenericContainer with easier support for the
+ * javascript onclick event. For instance,
+ * 
+ * &ltwo:ERXClickableContainer action = "$someAction"&gt;
+ * 
+ * would create a div (by default) with an onclick action that executes the
+ * given component action and replaces the page with the results.
+ * 
+ * @binding elementName the HTML element name to use (div by default)
+ * @binding action the action to perform
+ * 
+ * @author mschrag
+ */
+public class ERXClickableContainer extends WOGenericContainer {
+	public ERXClickableContainer(String name, NSDictionary associations, WOElement template) {
+		super(name, ERXClickableContainer._processAssociations(associations), template);
+	}
+
+	protected static NSDictionary _processAssociations(NSDictionary associations) {
+		NSMutableDictionary mutableAssociations = (NSMutableDictionary) associations;
+		WOAssociation action = (WOAssociation) mutableAssociations.removeObjectForKey("action");
+		if (action != null) {
+			mutableAssociations.setObjectForKey(action, "invokeAction");
+		}
+		if (!mutableAssociations.containsKey("elementName")) {
+			mutableAssociations.setObjectForKey(new WOConstantValueAssociation("div"), "elementName");
+		}
+		return mutableAssociations;
+	}
+
+	public void appendAttributesToResponse(WOResponse response, WOContext context) {
+		super.appendAttributesToResponse(response, context);
+		response.appendContentString(" onclick = \"location.href='");
+		response.appendContentString(context.componentActionURL());
+		response.appendContentString("'\" style = \"cursor: pointer;\"");
+	}
+}
