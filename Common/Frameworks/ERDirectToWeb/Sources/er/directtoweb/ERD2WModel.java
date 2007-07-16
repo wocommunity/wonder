9547ca8889703f3c705f2e104b5664e6d5620eff
leffert <leffert@8dc76679-1c23-de11-85cd-0050568e728b>
2007-07-16 18:23:15 +0000

    adding URL-based constructor

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WModel.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WModel.java
@@ -91,6 +91,9 @@ public class ERD2WModel extends D2WModel {
     protected ERD2WModel(File file) {
         super(file);
     }
+    public ERD2WModel(URL url) {
+        super(url);
+    }
     protected ERD2WModel(EOKeyValueUnarchiver unarchiver) {
         super(unarchiver);
     }
