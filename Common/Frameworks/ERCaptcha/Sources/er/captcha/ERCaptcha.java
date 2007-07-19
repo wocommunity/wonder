7d56970b8dffec377c1d610e23149956d62de17a
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-07-19 15:10:12 +0000

    readable captchas + settable captcha engine
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6238 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERCaptcha/Sources/er/captcha/ERCaptcha.java
+++ Common/Frameworks/ERCaptcha/Sources/er/captcha/ERCaptcha.java
@@ -5,7 +5,9 @@ import java.io.ByteArrayOutputStream;
 
 import org.apache.log4j.Logger;
 
+import com.octo.captcha.engine.CaptchaEngine;
 import com.octo.captcha.service.CaptchaServiceException;
+import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
 import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 import com.octo.captcha.service.image.ImageCaptchaService;
 import com.sun.image.codec.jpeg.JPEGCodec;
@@ -17,20 +19,37 @@ import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 import com.webobjects.foundation.NSData;
 
+import er.extensions.ERXProperties;
+
 /**
- * ERCaptcha presents a captcha image to the use along with a text field
- * for the user to identify the image.
+ * ERCaptcha presents a captcha image to the use along with a text field for the user to identify the image.
  * 
  * @binding validated returns whether or not the user properly validated the image
  * @binding resetText if set, a submit button will be added with this text that resets the image
- *  
+ * 
+ * @property er.captcha.captchaEngineClass the class name of the CaptchaEngine to use
+ * 
  * @author mschrag
  */
 public class ERCaptcha extends WOComponent {
 	private static final Logger log = Logger.getLogger(ERCaptcha.class);
-	private static ImageCaptchaService _captchaService = new DefaultManageableImageCaptchaService();
+	private static ImageCaptchaService _captchaService;
 	private NSData _captcha;
 	private String _response;
+
+	public static ImageCaptchaService captchaService() {
+		try {
+			if (_captchaService == null) {
+				String captchaEngineClass = ERXProperties.stringForKeyWithDefault("er.captcha.captchaEngineClass", ERCaptchaEngine.class.getName());
+				CaptchaEngine captchaEngine = (CaptchaEngine) Class.forName(captchaEngineClass).newInstance();
+				_captchaService = new DefaultManageableImageCaptchaService(new FastHashMapCaptchaStore(), captchaEngine, 180, 100000, 75000);
+			}
+			return _captchaService;
+		}
+		catch (Throwable t) {
+			throw new RuntimeException("Failed to initialize captcha engine.", t);
+		}
+	}
 
 	public ERCaptcha(WOContext context) {
 		super(context);
@@ -65,7 +84,7 @@ public class ERCaptcha extends WOComponent {
 		if (context._wasFormSubmitted()) {
 			Boolean validated = Boolean.FALSE;
 			try {
-				validated = _captchaService.validateResponseForID(context.elementID(), _response);
+				validated = ERCaptcha.captchaService().validateResponseForID(context.elementID(), _response);
 			}
 			catch (CaptchaServiceException e) {
 				ERCaptcha.log.error("Captcha service failed.", e);
@@ -82,7 +101,7 @@ public class ERCaptcha extends WOComponent {
 			byte[] captchaChallengeAsJpeg = null;
 			ByteArrayOutputStream captchaOutputStream = new ByteArrayOutputStream();
 			try {
-				BufferedImage challenge = _captchaService.getImageChallengeForID(context.elementID());
+				BufferedImage challenge = ERCaptcha.captchaService().getImageChallengeForID(context.elementID());
 				JPEGImageEncoder captchaEncoder = JPEGCodec.createJPEGEncoder(captchaOutputStream);
 				captchaEncoder.encode(challenge);
 				_captcha = new NSData(captchaOutputStream.toByteArray());
