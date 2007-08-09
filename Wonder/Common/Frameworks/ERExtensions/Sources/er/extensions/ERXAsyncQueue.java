b48aa227d7b66502d15a792ed62245162894339c
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-08-09 17:46:59 +0000

    made generic + added thread name ... this should probably have-a thread vs be-a thread
    git-svn-id: file:///Users/q/working/wondersvn/trunk@6335 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAsyncQueue.java
+++ Wonder/Common/Frameworks/ERExtensions/Sources/er/extensions/ERXAsyncQueue.java
@@ -5,40 +5,51 @@ import java.util.LinkedList;
 /**
  * Simple queue class that runs in a thread, waits and pulls jobs from a queue.
  * To use it, implement the process(Object) method in your subclass.
+ * 
  * @author ak
- *
+ * 
  */
-public abstract class ERXAsyncQueue extends Thread {
+public abstract class ERXAsyncQueue<T> extends Thread {
+	private LinkedList<T> _jobs = new LinkedList<T>();
 
-	private LinkedList _jobs = new LinkedList();
-
-	public void enqueue(Object o) {
-		synchronized (_jobs) {
-			_jobs.addFirst(o);
-			_jobs.notify();
-		}
+  public ERXAsyncQueue() {
+	  super("ERXAsyncQueue");
+  }
+  
+	public ERXAsyncQueue(String name) {
+	  super(name);
 	}
 	
+	public void enqueue(T o) {
+		synchronized (_jobs) {
+			_jobs.addFirst(o);
+			_jobs.notifyAll();
+		}
+	}
+
+	@Override
 	public final void run() {
 		boolean done = false;
 		try {
-			while(!done) {
-				Object o = null;
+			while (!done) {
+				T o = null;
 				synchronized (_jobs) {
-				    if(!_jobs.isEmpty()) {
-				        o = _jobs.removeLast();
-				    } else {
-				        _jobs.wait();
-				    }
+					if (!_jobs.isEmpty()) {
+						o = _jobs.removeLast();
+					}
+					else {
+						_jobs.wait();
+					}
 				}
-				if(o != null) {
+				if (o != null) {
 					process(o);
 				}
 			}
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			done = true;
 		}
 	}
 
-	public abstract void process(Object object);
+	public abstract void process(T object);
 }
