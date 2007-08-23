c1fae990df6e1c0aeef8d0bbfec15b5bff2445fa
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-08-23 12:30:39 +0000

    Added AjaxLongResponse, changed example to use it
    git-svn-id: file:///Users/q/working/wondersvn/trunk@6386 8dc76679-1c23-de11-85cd-0050568e728b

--- Wonder/Ajax/AjaxExample/Sources/LongResponseExample.java
+++ Wonder/Ajax/AjaxExample/Sources/LongResponseExample.java
@@ -7,61 +7,58 @@ import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 
 public class LongResponseExample extends WOComponent {
-    
-    static Logger log = Logger.getLogger(LongResponseExample.class);
 
-    public static class Task extends Thread {
-        int stage = 0;
-        String status = "Idle";
+	static Logger log = Logger.getLogger(LongResponseExample.class);
 
-        public void run() {
+	public static class Task extends Thread {
+		int stage = 0;
+		String status = "Idle";
 
-            try {
-                setStatus("Starting up");
-                sleep(2000);
-                while(stage < 20) {
-                    sleep(1000);
-                    stage++;
-                    setStatus("Currently at stage: " + stage);
-                 }
-                setStatus("Finished");
-            } catch (InterruptedException e) {
-                status = "Interrupted";
-            }
-        }
+		public void run() {
 
-        private void setStatus(String value) {
-            status = value;
-            log.info(status);
-        }
-        
-        public String getStatus() {
-            return status;
-        }
-    }
-    
-    public Task task;
-    
-    public LongResponseExample(WOContext context) {
-        super(context);
-    }
+			try {
+				setStatus("Starting up");
+				sleep(2000);
+				while (stage < 20) {
+					sleep(1000);
+					stage++;
+					setStatus("Currently at stage: " + stage);
+				}
+				setStatus("Finished");
+			}
+			catch (InterruptedException e) {
+				status = "Interrupted";
+			}
+		}
 
-    public long test() {
-        return System.currentTimeMillis();
-    }
+		private void setStatus(String value) {
+			status = value;
+			log.info(status);
+		}
 
-    public WOComponent startTask() {
-      task= new Task();
-      task.start();
-      return null;
-    }
-    
-    public WOActionResults stopTask() {
-      task.interrupt();
-      return null;
-    }
+		public String getStatus() {
+			return status;
+		}
+	}
 
-    public String onClickServer() {
-        return "new Ajax.Updater('taskInfo', $(taskInfo).updateUrl, {})};";
-    }
+	public Task task;
+
+	public LongResponseExample(WOContext context) {
+		super(context);
+	}
+
+	public long test() {
+		return System.currentTimeMillis();
+	}
+
+	public WOComponent startTask() {
+		task = new Task();
+		task.start();
+		return null;
+	}
+
+	public WOActionResults stopTask() {
+		task.interrupt();
+		return null;
+	}
 }
