6731a93dbee2a2a5d8f4e6051b69b2813c91f19f
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-09-13 14:26:08 +0000

    5.3/5.4 WODynamicURL fix
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6479 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXDynamicURL.java
@@ -0,0 +1,50 @@
+package er.extensions;
+
+import com.webobjects.foundation.NSForwardException;
+import com.webobjects.foundation._NSDelegate;
+
+/**
+ * 5.3/5.4-safe wrapper around a WODynamicURL (which changed classes).
+ * 
+ * @author mschrag
+ */
+public class ERXDynamicURL {
+	private _NSDelegate _delegate;
+	private Object _dynamicUrl;
+
+	/**
+	 * Construct an ERXDynamicURL.
+	 * 
+	 * @param dynamicUrl a WODynamicURL (either from the 5.3 or the 5.4 package)
+	 */
+	public ERXDynamicURL(Object dynamicUrl) {
+		try {
+			if (ERXApplication.isWO54()) {
+				_delegate = new _NSDelegate(Class.forName("com.webobjects.appserver.WODynamicURL"));
+			}
+			else {
+				_delegate = new _NSDelegate(Class.forName("com.webobjects.appserver._private.WODynamicURL"));
+			}
+			_dynamicUrl = dynamicUrl;
+		}
+		catch (Throwable t) {
+			throw new NSForwardException("Failed to create ERXDynamicURL.", t);
+		}
+	}
+
+	/**
+	 * @see com.webobjects.appserver(\._private|)WODynamicURL.requestHandlerPath()
+	 * @return
+	 */
+	public String requestHandlerPath() {
+		return (String) _delegate.perform("requestHandlerPath");
+	}
+
+	/**
+	 * @see com.webobjects.appserver(\._private|)WODynamicURL.queryString()
+	 * @return
+	 */
+	public String queryString() {
+		return (String) _delegate.perform("queryString");
+	}
+}
