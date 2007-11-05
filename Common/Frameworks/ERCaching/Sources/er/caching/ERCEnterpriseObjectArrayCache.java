53d30b8b17f8658c05ef88fdd0daae13fbbf72cd
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2007-11-05 21:15:59 +0000

    extremely rudimentary memcached support
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6615 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERCaching/Sources/er/caching/ERCEnterpriseObjectArrayCache.java
@@ -0,0 +1,55 @@
+package er.caching;
+
+import java.util.Map;
+
+import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOGlobalID;
+import com.webobjects.foundation.NSArray;
+
+import er.extensions.ERXEnterpriseObjectArrayCache;
+/**
+ Usage example:
+ 
+            ERCEnterpriseObjectArrayCache&lt;Bug&gt; cache = new ERCEnterpriseObjectArrayCache&lt;Bug&gt;("Bug");
+            cache.setObjectsForKey(bugs, "all");
+            NSArray&lt;Bug&gt; objects = cache.objectsForKey("all");
+ 
+ * @author ak
+ *
+ * @param <T>
+ */
+public class ERCEnterpriseObjectArrayCache<T extends EOEnterpriseObject> extends ERXEnterpriseObjectArrayCache<EOEnterpriseObject> {
+
+    protected Map<String, NSArray<EOGlobalID>> _cache;
+
+    public ERCEnterpriseObjectArrayCache(String entityName) {
+        this(entityName, 0L);
+    }
+
+    public ERCEnterpriseObjectArrayCache(Class<? extends EOEnterpriseObject> c) {
+        this(entityNameForClass(c));
+    }
+
+    public ERCEnterpriseObjectArrayCache(String entityName, long timeout) {
+        super(entityName, timeout);
+    }
+
+    private synchronized Map<String, NSArray<EOGlobalID>> cache() {
+        if (_cache == null) {
+            _cache = new ERCachingMap<String, NSArray<EOGlobalID>>();
+        }
+        return _cache;
+    }
+
+    protected void setCachedArrayForKey(NSArray<EOGlobalID> gids, Object key) {
+        cache().put(key.toString(), gids);
+    }
+
+    protected NSArray<EOGlobalID> cachedArrayForKey(Object key) {
+        return (NSArray<EOGlobalID>) cache().get(key);
+    }
+
+    public synchronized void reset() {
+        // can't do any thing here...
+    }
+}
