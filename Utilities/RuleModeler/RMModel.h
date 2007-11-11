f880056b737c95f9538f8adccd67689defd0a536
davelopper <davelopper@8dc76679-1c23-de11-85cd-0050568e728b>
2007-11-11 22:05:09 +0000

    Added support for multiple window controllers
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6637 8dc76679-1c23-de11-85cd-0050568e728b

--- Utilities/RuleModeler/RMModel.h
+++ Utilities/RuleModeler/RMModel.h
@@ -1,10 +1,30 @@
-//
-//  RMModel.h
-//  RuleModeler
-//
-//  Created by King Chung Huang on Thu Jan 29 2004.
-//  Copyright (c) 2004 King Chung Huang. All rights reserved.
-//
+/*
+ RMModel.h
+ RuleModeler
+
+ Created by King Chung Huang on 1/29/04.
+
+
+ Copyright (c) 2004 King Chung Huang
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+*/
 
 #import <AppKit/AppKit.h>
 #import "NSPropertyListSerializationAdditions.h"
@@ -25,6 +45,8 @@
 - (void)insertRules:(NSArray *)rules atIndexes:(NSIndexSet *)indexes;
 - (void)removeRulesAtIndexes:(NSIndexSet *)indexes;
 
-+ (NSArray *) sortDescriptors;
+- (id)makeNewWindowController;
+
++ (NSArray *)sortDescriptors;
 
 @end
