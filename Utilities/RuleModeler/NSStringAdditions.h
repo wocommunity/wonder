7aaf77caebf4ca27f66c3295b30b883400bc0e84
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2007-12-12 21:51:10 +0000

    added support for refreshing wolips on-the-fly via the new HTTP WOLips service

--- Utilities/RuleModeler/NSStringAdditions.h
+++ Utilities/RuleModeler/NSStringAdditions.h
@@ -13,4 +13,21 @@
 
 - (NSString *)pathRelativeToPath:(NSString *)path;
 
+- (NSString *)encodePercentEscapes;
+- (NSString *)encodePercentEscapesPerRFC2396 ;
+- (NSString *)encodePercentEscapesStrictlyPerRFC2396 ;
+// Decodes any existing percent escapes which should not be encoded per RFC 2396 sec. 2.4.3
+// Encodes any characters which should be encoded per RFC 2396 sec. 2.4.3.
+- (NSString *)encodePercentEscapesPerRFC2396ButNot:(NSString*)butNot butAlso:(NSString*)butAlso ;
+- (NSString *)decodeAllPercentEscapes ;
+// I did an experiment to find out which ASCII characters are encoded,
+// by encoding a string with all the nonalphanumeric characters available on the
+// Macintosh keyboard, with and without the shift key down. There were fourteen:
+// ` # % ^ [ ] { } \ | " < >
+// You only see thirteen because the fourtheenth one is the space character, " ".
+// This agrees with the lists of "space" "delims" and "unwise" in by RFC 2396 sec. 2.4.3
+// Also, I found that all of the non-ASCII characters available on the Macintosh
+// keyboard by using option or shift+option are also encoded. Some of these have
+// two bytes of unicode to encode, for example %C2%A4 for 0xC2A4
+
 @end
