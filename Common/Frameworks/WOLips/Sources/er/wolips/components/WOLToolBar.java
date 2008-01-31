b863a34c93282c5e90b93c5feab2255f2b68d5f9
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-01-31 07:59:00 +0000

    better toolbar toggling; added "expanded" option to WOLToolBar; added API files;
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6870 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/WOLips/Sources/er/wolips/components/WOLToolBar.java
+++ Common/Frameworks/WOLips/Sources/er/wolips/components/WOLToolBar.java
@@ -9,9 +9,28 @@ import com.webobjects.appserver.WOContext;
  * WOLips.
  * 
  * @author mschrag
+ * @binding expanded if true, the toolbar is expanded by default; defaults to false 
  */
 public class WOLToolBar extends WOComponent {
   public WOLToolBar(WOContext context) {
     super(context);
+  }
+  
+  @Override
+  public boolean synchronizesVariablesWithBindings() {
+    return false;
+  }
+  
+  public String style() {
+    String style;
+    Object expandedObj = valueForBinding("expanded");
+    boolean expanded = ((expandedObj instanceof Boolean && ((Boolean)expandedObj).booleanValue()) || (expandedObj instanceof String && Boolean.valueOf((String)expandedObj).booleanValue()));
+    if (expanded) {
+      style = null;
+    }
+    else {
+      style = "display: none";
+    }
+    return style;
   }
 }
