1fa15bea2f68f08d35bbf80aa42568ae903b018b
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-02-01 23:28:29 +0000

    javadoc

--- Ajax/Ajax/Sources/er/ajax/AjaxSelectionList.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxSelectionList.java
@@ -10,6 +10,29 @@ import com.webobjects.foundation.NSMutableArray;
 
 import er.extensions.ERXComponentUtilities;
 
+/**
+ * AjaxSelectionList provides a list component that supports keyboard navigation and component renderers. Externally,
+ * the component behaves like a form field, similar to WOPopUpButton except that it uses its component content as the
+ * renderer for each item instead of taking a displayString.
+ * 
+ * The javascript wrapper exposes events of the selection changing, the item being selected (via double-click or the
+ * enter key), and an item being deleted (with the delete key).
+ * 
+ * @author mschrag
+ * @binding elementName (optional) the type of element for the wrapper (ul, ol, table)
+ * @binding list (required) the list to render
+ * @binding item (required) bound for each item of the list
+ * @binding selection (optional) the current selection
+ * @binding mandatory (optional) if false, a null value will be prepended to the list. It is up to your component
+ *          content to handle the null value to show a "No Selection" value. mandatory defaults to true. Mandatory does
+ *          not imply any validation, it is the semantic equivalent of noSelectionString when compared to WOPopUpButton.
+ * @binding id (optional) the id of the list
+ * @binding class (optional) the css class of the list
+ * @binding style (optional) the css style of the list
+ * @binding onchange (optional) the javascript to execute when the selection changes
+ * @binding onselect (optional) the javascript to execute when the user presses enter or double-clicks
+ * @binding ondelete (optional) the javascript to execute when backspace or delete is pressed
+ */
 public class AjaxSelectionList extends AjaxComponent {
 	private String _id;
 	private String _value;
@@ -22,7 +45,7 @@ public class AjaxSelectionList extends AjaxComponent {
 	public boolean synchronizesVariablesWithBindings() {
 		return false;
 	}
-	
+
 	public void setItem(Object item) {
 		if (item instanceof NSKeyValueCoding.Null) {
 			setValueForBinding(null, "item");
@@ -31,7 +54,7 @@ public class AjaxSelectionList extends AjaxComponent {
 			setValueForBinding(item, "item");
 		}
 	}
-	
+
 	public Object item() {
 		return valueForBinding("item");
 	}
@@ -68,7 +91,7 @@ public class AjaxSelectionList extends AjaxComponent {
 	public String fieldID() {
 		return id() + "_Field";
 	}
-	
+
 	public String id() {
 		if (_id == null) {
 			_id = (String) valueForBinding("id");
