432ef04755418a1d29a280cd6499e0042b78e119
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-02-05 22:43:10 +0000

    subclass to expose the EOAttribute(EOEntity, String) constructor
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6928 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXEOAttribute.java
@@ -0,0 +1,25 @@
+package er.extensions;
+
+import com.webobjects.eoaccess.EOAttribute;
+import com.webobjects.eoaccess.EOEntity;
+import com.webobjects.foundation.NSDictionary;
+
+/**
+ * ERXEOAttribute exists only to make the EOAttribute(EOEntity, String) 
+ * constructor public.
+ * 
+ * @author mschrag
+ */
+public class ERXEOAttribute extends EOAttribute {
+	public ERXEOAttribute() {
+		super();
+	}
+
+	public ERXEOAttribute(NSDictionary plist, Object owner) {
+		super(plist, owner);
+	}
+
+	public ERXEOAttribute(EOEntity entity, String definition) {
+		super(entity, definition);
+	}
+}
