592c861e2bc4a24bb0742bbaba8297614427cdbd
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-02-08 23:25:42 +0000

    basic RSS page
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@6968 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXRssPage.java
@@ -0,0 +1,31 @@
+package er.extensions;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import com.webobjects.appserver.*;
+
+/**
+ * Simple RSS feed provider.
+ * @binding feedTitle
+ * @binding feedUrl
+ * @binding feedDescription
+ * @binding list
+ * @binding item
+ * @binding itemTitle
+ * @binding itemLink
+ * @binding itemPubDate
+ * @binding feedDescription
+ * @author ak
+ */
+
+public class ERXRssPage extends ERXStatelessComponent {
+	 
+	public ERXRssPage(WOContext context) {
+		super(context);
+	}
+
+	public Object dateFormatter() {
+		return new SimpleDateFormat("E, dd MMM yyyy hh:mm:ss Z", Locale.ENGLISH);
+	}
+}
