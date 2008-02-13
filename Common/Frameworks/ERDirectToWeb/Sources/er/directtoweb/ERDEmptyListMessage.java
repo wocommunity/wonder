8e694a8993ba8e7d42de3ade39029cea52fb7301
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2008-02-13 02:52:43 +0000

    Brought back ERDEmptyListMessage from trunk.  Fixed misspelled reference to excel button component name in list and pick list pages.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDEmptyListMessage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERDEmptyListMessage.java
@@ -6,9 +6,14 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.directtoweb;
 
-import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOComponent;
-import er.extensions.ERXStatelessComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2W;
+import com.webobjects.directtoweb.EditPageInterface;
+import com.webobjects.directtoweb.ErrorPageInterface;
+import com.webobjects.foundation.NSKeyValueCoding;
+
+import er.extensions.ERXValueUtilities;
 
 /**
  * Default component shown when a D2W list is empty.<br />
@@ -16,8 +21,57 @@ import er.extensions.ERXStatelessComponent;
 
 public class ERDEmptyListMessage extends ERDCustomComponent {
 
-    public ERDEmptyListMessage(WOContext context) { super(context); }
+	public ERDEmptyListMessage(WOContext context) {
+		super(context);
+	}
 
-    public final boolean isStateless() { return true; }
-    public final boolean synchronizesVariablesWithBindings() { return false; }
+	public NSKeyValueCoding bindings() {
+		return new NSKeyValueCoding() {
+			public void takeValueForKey(Object obj, String s) {
+				// nothing
+			}
+
+			public Object valueForKey(String s) {
+				return valueForBinding(s);
+			}
+		};
+	}
+
+	public final boolean isStateless() {
+		return true;
+	}
+
+	public final boolean synchronizesVariablesWithBindings() {
+		return false;
+	}
+	
+    /**
+     * Returns whether the "create new" link should be shown, depends on a rule like:<br><br>
+     * pageConfiguration = 'ListEntity' => showCreateObjectLink = true [prio]
+     * 
+     */
+    public boolean showCreateObjectLink () {
+    	boolean enabledFromRule = ERXValueUtilities.booleanValue(this.d2wContext().valueForKey("showCreateObjectLink"));
+    	boolean entityExists = (this.d2wContext().entity() != null && this.d2wContext().entity().name() != null);
+    	
+    	return entityExists && enabledFromRule;
+    }
+    
+    /**
+     * @return a new create page for the current entity
+     */
+    public WOComponent createObject () {
+        WOComponent nextPage = null;
+        try {
+            EditPageInterface epi = D2W.factory().editPageForNewObjectWithEntityNamed(this.d2wContext().entity().name(), session());
+            epi.setNextPage(context().page());
+            nextPage = (WOComponent) epi;
+        } catch (IllegalArgumentException e) {
+            ErrorPageInterface epf = D2W.factory().errorPage(session());
+            epf.setMessage(e.toString());
+            epf.setNextPage(context().page());
+            nextPage = (WOComponent) epf;
+        }
+        return nextPage;
+    }
 }
