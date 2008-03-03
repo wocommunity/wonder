279cc1d1f0ee690761993b2755a4690f7e0c1046
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-03 23:42:00 +0000

    fix for .@ keys that aren't actually property operator keys (ported back from HEAD)

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXProperties.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXProperties.java
@@ -1054,9 +1054,11 @@ public class ERXProperties {
               }
               else {
                 int keyIndex = key.indexOf(operatorKeyWithAt + ".");
-                operator = (ERXProperties.Operator) ERXProperties.operators.objectForKey(operatorKey);
-                computedProperties = operator.compute(key.substring(0, keyIndex), value, key.substring(keyIndex + operatorKeyWithAt.length() + 1));
-                break;
+                if (keyIndex != -1) {
+                  operator = (ERXProperties.Operator) ERXProperties.operators.objectForKey(operatorKey);
+                  computedProperties = operator.compute(key.substring(0, keyIndex), value, key.substring(keyIndex + operatorKeyWithAt.length() + 1));
+                  break;
+                }
               }
             }
 
