58cd931da2234d6bfdecb7c8c2b83284592b6ff6
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-05 05:48:51 +0000

    Bug fix: was selecting first item instead of none

--- Ajax/Ajax/WebServerResources/wonder.js
+++ Ajax/Ajax/WebServerResources/wonder.js
@@ -118,7 +118,7 @@ Object.extend(Form, {
     	
     	selects = $(form).getElementsByTagName('select');
     	for (selectIdx=0; selectIdx < selects.length; selectIdx++) {
-    		selects[selectIdx].selectedIndex = 0;
+    		selects[selectIdx].selectedIndex = -1;
     	}
     }
 });  
