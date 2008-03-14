2a02b9c01457dd755ccdcb3cb8303548da10b3a3
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-14 04:47:25 +0000

    Demo of retaining which tab is selected

--- Ajax/AjaxExample/Sources/Session.java
+++ Ajax/AjaxExample/Sources/Session.java
@@ -1,11 +1,15 @@
-import er.extensions.ERXSession;
+import er.extensions.*;
 
 /**
  * Everything related to saving pages is handled now in ERXSession.
- * 
+ *
  *
  * @author ak
  */
 
 public class Session extends ERXSession {
+
+	public String selectedTab = "InlineContent";
+
+
 }
