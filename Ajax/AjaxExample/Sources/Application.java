ee287000e1829bad5f842397d0c23199c23218f2
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-18 00:04:55 +0000

    much simpler JSON examples

--- Ajax/AjaxExample/Sources/Application.java
+++ Ajax/AjaxExample/Sources/Application.java
@@ -2,19 +2,23 @@
 
 import org.apache.log4j.Logger;
 
+import er.ajax.json.JSONRequestHandler;
 import er.extensions.ERXApplication;
 
 public class Application extends ERXApplication {
-    static Logger log = Logger.getLogger(Application.class);
-    public static void main(String argv[]) {
-        ERXApplication.main(argv, Application.class);
-    }
+	static Logger log = Logger.getLogger(Application.class);
+
+	public static void main(String argv[]) {
+		ERXApplication.main(argv, Application.class);
+	}
 
 	public Application() {
-        super();
-        log.info("Welcome to " + this.name() + " !");
-        /* ** put your initialization code in here ** */
-        setAllowsConcurrentRequestHandling(true);
-        setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
-    }
+		super();
+		log.info("Welcome to " + this.name() + " !");
+		/* ** put your initialization code in here ** */
+		setAllowsConcurrentRequestHandling(true);
+		setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
+		JSONRequestHandler requestHandler = JSONRequestHandler.register();
+		requestHandler.registerService("exampleService", new ExampleService());
+	}
 }
