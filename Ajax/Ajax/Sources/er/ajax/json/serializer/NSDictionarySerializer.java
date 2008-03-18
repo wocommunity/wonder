ac822f9fad722fcd5c2aa276c1cfb31f59275d24
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-18 20:29:02 +0000

    new nsdictionary serializer

--- Ajax/Ajax/Sources/er/ajax/json/serializer/NSDictionarySerializer.java
+++ Ajax/Ajax/Sources/er/ajax/json/serializer/NSDictionarySerializer.java
@@ -1,28 +1,9 @@
-/*
- * JSON-RPC-Java - a JSON-RPC to Java Bridge with dynamic invocation
- *
- * $Id$
- *
- * Copyright Metaparadigm Pte. Ltd. 2004.
- * Michael Clark <michael@metaparadigm.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public (LGPL)
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details: http://www.gnu.org/
- *
- */
-
 package er.ajax.json.serializer;
 
 import java.util.Enumeration;
 import java.util.Iterator;
 
+import org.jabsorb.JSONSerializer;
 import org.jabsorb.serializer.AbstractSerializer;
 import org.jabsorb.serializer.MarshallException;
 import org.jabsorb.serializer.ObjectMatch;
@@ -31,129 +12,196 @@ import org.jabsorb.serializer.UnmarshallException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSKeyValueCoding;
 import com.webobjects.foundation.NSMutableDictionary;
 
 /**
- * Transforms NSDictionaries between JavaScript and Java.
+ * Serialises NSDictionaries
  * 
- * @author Jean-François Veillette <jfveillette@os.ca>
- * @version $Revision$, $Date$ <br>
- *          &copy; 2005 OS communications informatiques, inc. Tous droits réservés.
+ * TODO: if this serialises a superclass does it need to also specify the subclasses?
  */
 public class NSDictionarySerializer extends AbstractSerializer {
+	/**
+	 * Unique serialisation id.
+	 */
+	private final static long serialVersionUID = 2;
 
+	/**
+	 * Classes that this can serialise.
+	 */
 	private static Class[] _serializableClasses = new Class[] { NSDictionary.class, NSMutableDictionary.class };
 
+	/**
+	 * Classes that this can serialise to.
+	 */
 	private static Class[] _JSONClasses = new Class[] { JSONObject.class };
 
-	public Class[] getSerializableClasses() {
-		return _serializableClasses;
+	public boolean canSerialize(Class clazz, Class jsonClazz) {
+		return (super.canSerialize(clazz, jsonClazz) || ((jsonClazz == null || jsonClazz == JSONObject.class) && NSDictionary.class.isAssignableFrom(clazz)));
 	}
 
 	public Class[] getJSONClasses() {
 		return _JSONClasses;
 	}
 
-	public boolean canSerialize(Class clazz, Class jsonClazz) {
-		return (super.canSerialize(clazz, jsonClazz) || ((jsonClazz == null || jsonClazz == JSONObject.class) && NSDictionary.class.isAssignableFrom(clazz)));
+	public Class[] getSerializableClasses() {
+		return _serializableClasses;
+	}
+
+	public Object marshall(SerializerState state, Object p, Object o) throws MarshallException {
+		NSDictionary dictionary = (NSDictionary) o;
+		JSONObject obj = new JSONObject();
+		JSONObject dictionarydata = new JSONObject();
+		if (ser.getMarshallClassHints()) {
+			try {
+				obj.put("javaClass", o.getClass().getName());
+			}
+			catch (JSONException e) {
+				throw new MarshallException("javaClass not found!");
+			}
+		}
+		try {
+			obj.put("nsdictionary", dictionarydata);
+			state.push(o, dictionarydata, "nsdictionary");
+		}
+		catch (JSONException e) {
+			throw new MarshallException("Could not add nsdictionary to object: " + e.getMessage());
+		}
+		Object key = null;
+		try {
+			Enumeration keyEnum = dictionary.allKeys().objectEnumerator();
+			while (keyEnum.hasMoreElements()) {
+				key = keyEnum.nextElement();
+				Object value = dictionary.objectForKey(key);
+				String keyString = key.toString(); // only support String keys
+
+				Object json = ser.marshall(state, dictionarydata, value, keyString);
+
+				// omit the object entirely if it's a circular reference or duplicate
+				// it will be regenerated in the fixups phase
+				if (JSONSerializer.CIRC_REF_OR_DUPLICATE != json) {
+					dictionarydata.put(keyString, json);
+				}
+			}
+		}
+		catch (MarshallException e) {
+			throw (MarshallException) new MarshallException("nsdictionary key " + key + " " + e.getMessage()).initCause(e);
+		}
+		catch (JSONException e) {
+			throw (MarshallException) new MarshallException("nsdictionary key " + key + " " + e.getMessage()).initCause(e);
+		}
+		finally {
+			state.pop();
+		}
+		return obj;
 	}
 
 	public ObjectMatch tryUnmarshall(SerializerState state, Class clazz, Object o) throws UnmarshallException {
+		JSONObject jso = (JSONObject) o;
+		String java_class;
 		try {
-			JSONObject jso = (JSONObject) o;
-			String java_class = jso.getString("javaClass");
-			if (java_class == null) {
-				throw new UnmarshallException("no type hint");
-			}
-			if (!(java_class.equals("com.webobjects.foundation.NSDictionary") || java_class.equals("com.webobjects.foundation.NSMutableDictionary"))) {
-				throw new UnmarshallException("not a NSDictionary");
-			}
-			JSONObject jsonmap = jso.getJSONObject("dict");
-			if (jsonmap == null) {
-				throw new UnmarshallException("dict missing");
-			}
-			ObjectMatch m = new ObjectMatch(-1);
-			Iterator i = jsonmap.keys();
-			String key = null;
-			try {
-				while (i.hasNext()) {
-					key = (String) i.next();
-					m = ser.tryUnmarshall(state, null, jsonmap.get(key)).max(m);
-				}
-			}
-			catch (UnmarshallException e) {
-				throw new UnmarshallException("key " + key + " " + e.getMessage());
-			}
-			return m;
+			java_class = jso.getString("javaClass");
 		}
 		catch (JSONException e) {
-			throw new UnmarshallException("Failed to unmarshall NSDictionary.", e);
+			throw new UnmarshallException("Could not read javaClass", e);
 		}
+		if (java_class == null) {
+			throw new UnmarshallException("no type hint");
+		}
+		if (!(java_class.equals("com.webobjects.foundation.NSDictionary") || java_class.equals("com.webobjects.foundation.NSMutableDictionary"))) {
+			throw new UnmarshallException("not an NSDictionary");
+		}
+		JSONObject jsondictionary;
+		try {
+			jsondictionary = jso.getJSONObject("nsdictionary");
+		}
+		catch (JSONException e) {
+			throw new UnmarshallException("Could not read dictionary: " + e.getMessage(), e);
+		}
+		if (jsondictionary == null) {
+			throw new UnmarshallException("nsdictionary missing");
+		}
+		ObjectMatch m = new ObjectMatch(-1);
+		Iterator i = jsondictionary.keys();
+		String key = null;
+		state.setSerialized(o, m);
+		try {
+			while (i.hasNext()) {
+				key = (String) i.next();
+				m.setMismatch(ser.tryUnmarshall(state, null, jsondictionary.get(key)).max(m).getMismatch());
+			}
+		}
+		catch (UnmarshallException e) {
+			throw new UnmarshallException("key " + key + " " + e.getMessage(), e);
+		}
+		catch (JSONException e) {
+			throw new UnmarshallException("key " + key + " " + e.getMessage(), e);
+		}
+		return m;
 	}
 
 	public Object unmarshall(SerializerState state, Class clazz, Object o) throws UnmarshallException {
+		JSONObject jso = (JSONObject) o;
+		String java_class;
 		try {
-			JSONObject jso = (JSONObject) o;
-			String java_class = jso.getString("javaClass");
-			if (java_class == null) {
-				throw new UnmarshallException("no type hint");
-			}
-			NSMutableDictionary abmap = null;
-			if (java_class.equals("com.webobjects.foundation.NSDictionary") || java_class.equals("com.webobjects.foundation.NSMutableDictionary")) {
-				abmap = new NSMutableDictionary();
-			}
-			else {
-				throw new UnmarshallException("not a NSDictionary");
-			}
-			JSONObject jsonmap = jso.getJSONObject("dict");
-			if (jsonmap == null) {
-				throw new UnmarshallException("dict missing");
-			}
-			Iterator i = jsonmap.keys();
-			String key = null;
-			try {
-				while (i.hasNext()) {
-					key = (String) i.next();
-					Object value = ser.unmarshall(state, null, jsonmap.get(key));
-					abmap.setObjectForKey(value, key);
-				}
-			}
-			catch (UnmarshallException e) {
-				throw new UnmarshallException("key " + key + " " + e.getMessage());
-			}
-			return abmap;
+			java_class = jso.getString("javaClass");
 		}
 		catch (JSONException e) {
-			throw new UnmarshallException("Failed to unmarshall NSDictionary.", e);
+			throw new UnmarshallException("Could not read javaClass", e);
+		}
+		if (java_class == null) {
+			throw new UnmarshallException("no type hint");
+		}
+		boolean immutableClone = false;
+		NSMutableDictionary abdictionary;
+		if (java_class.equals("com.webobjects.foundation.NSDictionary")) {
+			abdictionary = new NSMutableDictionary();
+			immutableClone = true;
+		}
+		else if (java_class.equals("com.webobjects.foundation.NSMutableDictionary")) {
+			abdictionary = new NSMutableDictionary();
+		}
+		else {
+			throw new UnmarshallException("not an NSDictionary");
+		}
+		JSONObject jsondictionary;
+		try {
+			jsondictionary = jso.getJSONObject("nsdictionary");
+		}
+		catch (JSONException e) {
+			throw new UnmarshallException("Could not read dictionary: " + e.getMessage(), e);
+		}
+		if (jsondictionary == null) {
+			throw new UnmarshallException("nsdictionary missing");
+		}
+		Iterator i = jsondictionary.keys();
+		String key = null;
+		try {
+			while (i.hasNext()) {
+				key = (String) i.next();
+				Object value = ser.unmarshall(state, null, jsondictionary.get(key));
+				if (value != null) {
+					abdictionary.setObjectForKey(value, key);
+				}
+				else {
+					abdictionary.setObjectForKey(NSKeyValueCoding.NullValue, key);
+				}
+			}
+			NSDictionary finalDictionary = abdictionary;
+			if (immutableClone) {
+				finalDictionary = abdictionary.immutableClone();
+			}
+			state.setSerialized(o, finalDictionary);
+			return finalDictionary;
+		}
+		catch (UnmarshallException e) {
+			throw new UnmarshallException("key " + key + " " + e.getMessage(), e);
+		}
+		catch (JSONException e) {
+			throw new UnmarshallException("key " + key + " " + e.getMessage(), e);
 		}
 	}
 
-	/**
-	 * @see com.metaparadigm.jsonrpc.Serializer#doMarshall(java.lang.Object)
-	 */
-	public Object marshall(SerializerState state, Object p, Object o) throws MarshallException {
-		try {
-			NSDictionary map = (NSDictionary) o;
-			JSONObject obj = new JSONObject();
-			JSONObject mapdata = new JSONObject();
-			obj.put("javaClass", o.getClass().getName());
-			obj.put("dict", mapdata);
-			Object key = null;
-			try {
-				for (Enumeration e = map.keyEnumerator(); e.hasMoreElements();) {
-					key = e.nextElement();
-					Object value = ser.marshall(state, o, map.objectForKey(key), key);
-					mapdata.put(String.valueOf(key), value);
-				}
-			}
-			catch (MarshallException e) {
-				throw new MarshallException("map key " + key + " " + e.getMessage());
-			}
-			return obj;
-		}
-		catch (JSONException e) {
-			throw new MarshallException("Failed to marshall NSDictionary.", e);
-		}
-	}
 }
