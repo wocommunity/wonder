994354334416147b50fddd6b4666394ff5afd06f
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-18 03:01:38 +0000

    added a wo json client factory

---
+++ Ajax/Ajax/Sources/er/ajax/json/JavaJSONClient.java
@@ -0,0 +1,43 @@
+package er.ajax.json;
+
+import java.lang.reflect.Field;
+
+import org.jabsorb.JSONSerializer;
+import org.jabsorb.client.Client;
+import org.jabsorb.client.HTTPSession;
+import org.jabsorb.client.TransportRegistry;
+
+/**
+ * If you are trying to communicate with a WO JSON server from another Java app, JavaJSONClient provides a factory for
+ * creating the appropiate JSON Client class.
+ * 
+ * @author mschrag
+ */
+public class JavaJSONClient {
+	/**
+	 * Creates and returns a JSON Client.
+	 * 
+	 * @param jsonUrl
+	 *            the JSON service URL
+	 * @param useHttpClient
+	 *            if true, Commons HTTPClient will be used instead of URLConnection (much better, but requires
+	 *            HttpClient)
+	 * @return the JSON client
+	 * @throws Exception
+	 *             if the client creation fails
+	 */
+	public static Client create(String jsonUrl, boolean useHttpClient) throws Exception {
+		if (useHttpClient) {
+			HTTPSession.register(TransportRegistry.i());
+		}
+		Client client = new Client(TransportRegistry.i().createSession(jsonUrl));
+		Field serializerField = client.getClass().getDeclaredField("serializer");
+		serializerField.setAccessible(true);
+		JSONSerializer serializer = (JSONSerializer) serializerField.get(client);
+		serializer.registerSerializer(new JSONEnterpriseObjectSerializer());
+		serializer.registerSerializer(new NSArraySerializer());
+		serializer.registerSerializer(new NSDictionarySerializer());
+		serializer.registerSerializer(new NSTimestampSerializer());
+		return client;
+	}
+}
