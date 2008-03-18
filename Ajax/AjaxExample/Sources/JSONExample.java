7d66fe04fe5a9bf9231ccd3ea5fbb0bc8079f82b
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-18 11:19:20 +0000

    test for broken refs
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7190 8dc76679-1c23-de11-85cd-0050568e728b

--- Ajax/AjaxExample/Sources/JSONExample.java
+++ Ajax/AjaxExample/Sources/JSONExample.java
@@ -4,10 +4,22 @@ import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSPropertyListSerialization;
 import com.webobjects.foundation.NSTimestamp;
 
+import er.extensions.ERXConstant;
+
 public class JSONExample extends com.webobjects.appserver.WOComponent {
 	private JSONProxy _proxy;
 	private NSMutableArray<Person> _people;
 
+	private static class Constant extends ERXConstant.StringConstant {
+
+		public Constant(String value, String name) {
+			super(value, name);
+		}
+	}
+	
+	private static Constant  TEST1 = new Constant("test1", "Test 1");
+	private static Constant  TEST2 = new Constant("test2", "Test 2");
+	
 	public JSONExample(WOContext context) {
 		super(context);
 		_proxy = new JSONProxy();
@@ -32,7 +44,9 @@ public class JSONExample extends com.webobjects.appserver.WOComponent {
 		}
 
 		private NSArray<Object> someData = new NSArray<Object>(
-				new Object[] { new Integer(0), "test", new NSTimestamp(), NSPropertyListSerialization.propertyListFromString("<0000c0a8004a0000d2f5480400000113c81c0584c55806fa>") });
+				// AK: new json doesn't handle this
+				new Object[] { TEST1, TEST2, TEST1, new Integer(0), "test", new NSTimestamp(), NSPropertyListSerialization.propertyListFromString("<0000c0a8004a0000d2f5480400000113c81c0584c55806fa>"
+				) });
 
 		public NSArray someData() {
 			return someData;
