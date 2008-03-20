6b9630bd1df7a29814b5402062604d89cb222d10
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-20 15:27:28 +0000

    easier way to completely remove a tag

--- Common/Frameworks/ERTaggable/Sources/er/taggable/model/ERTag.java
+++ Common/Frameworks/ERTaggable/Sources/er/taggable/model/ERTag.java
@@ -2,6 +2,13 @@ package er.taggable.model;
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.eoaccess.EOEntity;
+import com.webobjects.eoaccess.EOModelGroup;
+import com.webobjects.eocontrol.EOEditingContext;
+
+import er.extensions.ERXEC;
+import er.taggable.ERTaggableEntity;
+
 /**
  * ERTag represents a single String shared tag.
  * 
@@ -18,4 +25,47 @@ public class ERTag extends _ERTag {
   }
 
   private static Logger log = Logger.getLogger(ERTag.class);
+
+  @Override
+  public void willDelete() throws ValidationException {
+    untagAllTaggables();
+    super.willDelete();
+  }
+
+  /**
+   * Removes this tag from all taggables.  This is automatically
+   * called prior to deleting an ERTag.
+   */
+  public void untagAllTaggables() {
+    EOEditingContext editingContext = editingContext();
+    EOModelGroup modelGroup = entity().model().modelGroup();
+    for (EOEntity entity : ERTaggableEntity.taggableEntities()) {
+      ERTaggableEntity taggableEntity = ERTaggableEntity.taggableEntity(entity);
+      taggableEntity.removeTags(editingContext, this);
+    }
+  }
+
+  /**
+   * Deletes the tag with the given name and removes the tag from all taggables.
+   * 
+   * @param tagName the tag name to delete
+   */
+  public static void deleteTagNamed(String tagName) {
+    EOEditingContext editingContext = ERXEC.newEditingContext();
+    ERTag.deleteTagNamed(editingContext, tagName);
+    editingContext.saveChanges();
+  }
+
+  /**
+   * Deletes the tag with the given name and removes the tag from all taggables.
+   * 
+   * @param editingContext the editing context to delete in
+   * @param tagName the tag name to delete
+   */
+  public static void deleteTagNamed(EOEditingContext editingContext, String tagName) {
+    ERTag tag = ERTag.fetchERTag(editingContext, ERTag.NAME_KEY, tagName);
+    if (tag != null) {
+      tag.delete();
+    }
+  }
 }
