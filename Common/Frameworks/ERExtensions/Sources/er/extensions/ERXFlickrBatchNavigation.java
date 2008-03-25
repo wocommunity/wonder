0e9dd162be3ed460690a20b1e7a72f4ec060e84a
cug <cug@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-25 22:54:53 +0000

    Can paginate plain array with POJOs now

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXFlickrBatchNavigation.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXFlickrBatchNavigation.java
@@ -1,5 +1,7 @@
 package er.extensions;
 
+
+
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
@@ -17,19 +19,31 @@ import com.webobjects.foundation.NSMutableArray;
  * Include ERXFlickrBatchNavigation.css in ERExtensions for a default stylesheet
  * that looks (very) similar to Flickr.
  * </p>
+ * <p>Can also be used for pagination on the parent component, where the objects being paginated may be POJOs in an array,
+ * or where paging all the objects in the allObjects array is not feasible due to memory requirements.</p>
  * 
  * @author mschrag
+ * @author rob, cug (non displayGroup batching)
+ * 
  * @binding displayGroup the display group to paginate
  * @binding displayName the name of the items that are being display ("photo", "bug", etc)
  * @binding showPageRange if true, the page of items on the page is shown, for example "(1-7 of 200 items)"
  * @binding small if true, a compressed page count style is used 
+ * 
+ * @binding parentActionName (if you don't provide a displayGroup) the action to be executed on the parent component to get the next batch of items.
+ * @binding currentBatchIndex (if you don't provide a displayGroup) used to get and set on the parent component the selected page index
+ * @binding maxNumberOfObjects (if you don't provide a displayGroup) used to get the total number of objects that are being paginated.
+ * @binding numberOfObjectsPerBatch (if you don't provide a displayGroup) the number of objects per batch (page)
  */
 public class ERXFlickrBatchNavigation extends WOComponent {
 	private int _lastPageCount;
 	private int _lastPageSize;
 	private int _lastCurrentPageNumber;
 	private NSMutableArray<PageNumber> _pageNumbers;
-	private PageNumber _repetitionPageNumber;
+	private PageNumber _repetitionPageNumber; 
+	
+	//Note: Lazily Cached
+	private String _parentActionName;
 
 	public ERXFlickrBatchNavigation(WOContext context) {
 		super(context);
@@ -44,11 +58,16 @@ public class ERXFlickrBatchNavigation extends WOComponent {
 	}
 
 	public NSArray objects() {
-		if (displayGroup() instanceof ERXDisplayGroup) {
-			ERXDisplayGroup dg = (ERXDisplayGroup) displayGroup();
-			return dg.filteredObjects();
-		}
-		return displayGroup().allObjects();
+		NSArray objects = null;
+		if(displayGroup() != null){
+			if (displayGroup() instanceof ERXDisplayGroup) {
+				ERXDisplayGroup dg = (ERXDisplayGroup) displayGroup();
+				objects =  dg.filteredObjects();
+			} else {
+				objects =  displayGroup().allObjects();
+			}
+		} 
+		return objects;
 	}
 	
 	public WODisplayGroup displayGroup() {
@@ -62,41 +81,64 @@ public class ERXFlickrBatchNavigation extends WOComponent {
 	public PageNumber repetitionPageNumber() {
 		return _repetitionPageNumber;
 	}
-
-	public boolean hasMultiplePages() {
-    WODisplayGroup displayGroup = displayGroup();
-	  return displayGroup.batchCount() > 1;
-	}
 	
+	public boolean hasMultiplePages() {
+		return batchCount() > 1;
+	}
+		
 	public boolean hasPreviousPage() {
-		WODisplayGroup displayGroup = displayGroup();
-		return displayGroup.currentBatchIndex() > 1;
+		return currentBatchIndex() > 1;
 	}
 
 	public WOActionResults previousPage() {
-		WODisplayGroup displayGroup = displayGroup();
-		displayGroup.displayPreviousBatch();
-		return null;
+		WOActionResults previousPage = null;
+		if(displayGroup() != null){
+			WODisplayGroup displayGroup = displayGroup();
+			displayGroup.displayPreviousBatch();
+		} else if(parentActionName() != null){
+			Integer previousBatchIndex = new Integer((currentBatchIndex() - 1));
+			if(!(previousBatchIndex.intValue()  > 0)){
+				previousBatchIndex = new Integer(1);
+			} 
+			setValueForBinding(previousBatchIndex, "currentBatchIndex");
+			previousPage = performParentAction(parentActionName());
+		}
+		return previousPage;
 	}
 
 	public boolean hasNextPage() {
-		WODisplayGroup displayGroup = displayGroup();
-		return displayGroup.currentBatchIndex() < displayGroup.batchCount();
+		return currentBatchIndex() < batchCount();
 	}
 
 	public WOActionResults nextPage() {
-		WODisplayGroup displayGroup = displayGroup();
-		displayGroup.displayNextBatch();
-		return null;
+		WOActionResults nextPage = null;
+		if(displayGroup() != null){
+			WODisplayGroup displayGroup = displayGroup();
+			displayGroup.displayNextBatch();
+		} else if(parentActionName() != null){
+			Integer nextBatchIndex = new Integer((currentBatchIndex() + 1));
+			int pageCount  = batchCount();
+			if((nextBatchIndex.intValue()  > pageCount)){
+				nextBatchIndex = new Integer(pageCount);
+			} 
+			setValueForBinding(nextBatchIndex, "currentBatchIndex");
+			nextPage = performParentAction(parentActionName());
+		}
+		return nextPage;
 	}
 
 	public WOActionResults selectPage() {
-		WODisplayGroup displayGroup = displayGroup();
+		WOActionResults selectPage = null;
 		Integer pageNumber = _repetitionPageNumber.pageNumber();
 		if (pageNumber != null) {
-			displayGroup.setCurrentBatchIndex(pageNumber.intValue());
+			if (displayGroup() != null) {
+				displayGroup().setCurrentBatchIndex(pageNumber.intValue());
+			} else {
+				setValueForBinding(pageNumber, "currentBatchIndex");
+				selectPage = performParentAction(parentActionName());
+			}
 		}
-		return null;
+		return selectPage;
 	}
 
 	public String displayName() {
@@ -106,6 +148,20 @@ public class ERXFlickrBatchNavigation extends WOComponent {
 		}
 		return displayName;
 	}
+	
+	public Integer displayNameCount(){
+		Integer displayNameCount = new Integer(0);
+		if(displayGroup() != null){
+			NSArray objects = objects();
+			if(objects != null && objects.count() > 0){
+				displayNameCount = new Integer(objects.count());
+			}
+		} else {
+			displayNameCount = new Integer(maxNumberOfObjects());
+		}
+		
+		return displayNameCount;
+	}
 
 	public boolean isCurrentPageNumber() {
 		Integer pageNumber = _repetitionPageNumber.pageNumber();
@@ -113,10 +169,9 @@ public class ERXFlickrBatchNavigation extends WOComponent {
 	}
 
 	public NSArray<PageNumber> pageNumbers() {
-		WODisplayGroup displayGroup = displayGroup();
-		int pageCount = displayGroup.batchCount();
-		int currentPageNumber = displayGroup.currentBatchIndex();
-		int pageSize = displayGroup.numberOfObjectsPerBatch();
+		int pageCount = batchCount();
+		int currentPageNumber = currentBatchIndex();
+		int pageSize = numberOfObjectsPerBatch();
 		if (_lastPageCount != pageCount || _lastCurrentPageNumber != currentPageNumber || _lastPageSize != pageSize) {
 			_pageNumbers = new NSMutableArray<PageNumber>();
 
@@ -167,7 +222,6 @@ public class ERXFlickrBatchNavigation extends WOComponent {
 			_lastCurrentPageNumber = currentPageNumber;
 			_lastPageSize = pageSize;
 		}
-
 		return _pageNumbers;
 	}
 	
@@ -197,5 +251,90 @@ public class ERXFlickrBatchNavigation extends WOComponent {
 		public boolean isEllipsis() {
 			return _ellipsis;
 		}
+	}
+	
+	public int batchCount(){
+		int batchCount = 0;
+		if(displayGroup() != null){
+			batchCount = displayGroup().batchCount();
+		} else {
+			int numberOfObjectsPerBatch = numberOfObjectsPerBatch();
+			int maxNumberOfObjects = maxNumberOfObjects();
+			if (!(numberOfObjectsPerBatch == 0)){	
+				if (maxNumberOfObjects == 0)
+					batchCount = 1;
+				else
+					batchCount = (maxNumberOfObjects - 1) / numberOfObjectsPerBatch + 1;
+			}
+		}
+		return batchCount;	
+	}
+	
+	public int numberOfObjectsPerBatch(){
+		int numberOfObjects = 0;
+		if(displayGroup() != null){
+			numberOfObjects = displayGroup().numberOfObjectsPerBatch();
+		} else if(hasBinding("numberOfObjectsPerBatch")){
+			Integer numberOfObjectsPerBatch = (Integer) valueForBinding("numberOfObjectsPerBatch");
+			if(numberOfObjectsPerBatch != null && numberOfObjectsPerBatch.intValue() > 0){
+				numberOfObjects = numberOfObjectsPerBatch.intValue();
+			} 
+		}
+		return numberOfObjects;
+	}
+	
+	public int maxNumberOfObjects() {
+		int maxNumber = 0; 
+		if (hasBinding("maxNumberOfObjects")) {
+			Integer maxNumberOfObjects = (Integer) valueForBinding("maxNumberOfObjects");
+			if (maxNumberOfObjects != null && maxNumberOfObjects.intValue() > 0) {
+				maxNumber = maxNumberOfObjects.intValue();
+			} 
+		} 
+		return maxNumber;
+	}
+	
+	public int currentBatchIndex(){
+		int index = 1;
+		if(displayGroup() != null){
+			index = displayGroup().currentBatchIndex();
+		} else if(hasBinding("currentBatchIndex")){
+			Integer currentBatchIndex = (Integer) valueForBinding("currentBatchIndex");
+			if(currentBatchIndex != null && currentBatchIndex.intValue() > 1){
+				index = currentBatchIndex.intValue();
+			}
+		}
+		return index;
+	}
+	
+	public String parentActionName(){
+		if(_parentActionName == null){
+			_parentActionName = (String) valueForBinding("parentActionName");
+		}
+		return _parentActionName;
+	}
+	
+	public int firstIndex(){
+		int firstIndex = 0;
+		if(displayGroup() != null){
+			firstIndex  = displayGroup().indexOfFirstDisplayedObject();
+		} else {
+			int currentBatchIndex = currentBatchIndex();
+			int numberOfObjectsPerBatch = numberOfObjectsPerBatch();
+			firstIndex = (currentBatchIndex * numberOfObjectsPerBatch) - (numberOfObjectsPerBatch - 1); 
+		}
+		return firstIndex;
+	}
+	
+	public int lastIndex(){
+		int lastIndex = 0;
+		if(displayGroup() != null){
+			lastIndex  = displayGroup().indexOfLastDisplayedObject();
+		} else {
+			int currentBatchIndex = currentBatchIndex();
+			int numberOfObjectsPerBatch = numberOfObjectsPerBatch();
+			lastIndex = currentBatchIndex * numberOfObjectsPerBatch;
+		}
+		return lastIndex;
 	}
 }
