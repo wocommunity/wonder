7704b09e8e2931353a689f1669bb2bf4da68bb9c
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-29 22:49:41 +0000

    Added implementation for databaseContextShouldUpdateCurrentSnapshot.  The previous implementation resulted in the fetchTimestamp of an editing context being ignored - the snapshot was never updated.  This implementation matches EOF's when this method is not implemented.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7269 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/eoaccess/ERXDatabaseContextMulticastingDelegate.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/eoaccess/ERXDatabaseContextMulticastingDelegate.java
@@ -3,6 +3,7 @@ package er.extensions.eoaccess;
 import java.lang.reflect.Method;
 
 import com.webobjects.eoaccess.EOAdaptorChannel;
+import com.webobjects.eoaccess.EODatabase;
 import com.webobjects.eoaccess.EODatabaseChannel;
 import com.webobjects.eoaccess.EODatabaseContext;
 import com.webobjects.eoaccess.EOEntity;
@@ -29,14 +30,16 @@ import er.extensions.foundation.ERXMulticastingDelegate;
 public class ERXDatabaseContextMulticastingDelegate extends ERXMulticastingDelegate implements EODatabaseContext.Delegate {
 
     private Method orderAdaptorOperations;
-
-
+    private Method currentEditingContext;
 
     public ERXDatabaseContextMulticastingDelegate() {
         super();
         try {
             orderAdaptorOperations = EODatabaseContext.class.getDeclaredMethod("orderAdaptorOperations", new Class[] {});
             orderAdaptorOperations.setAccessible(true);
+
+            currentEditingContext = EODatabaseChannel.class.getDeclaredMethod("currentEditingContext", new Class[] {});
+            currentEditingContext.setAccessible(true);
         }
         catch (Exception e) {
             throw NSForwardException._runtimeExceptionForThrowable(e);
@@ -59,7 +62,7 @@ public class ERXDatabaseContextMulticastingDelegate extends ERXMulticastingDeleg
      * added, then <code>newDelegate</code> is added at the end of the delegate chain.</li>
      * </ol>
      *
-     * @param newDelegate
+     * @param newDelegate object to include as delegate
      */
     public static void addDefaultDelegate(Object newDelegate) {
         ERXDatabaseContextMulticastingDelegate multiDelegate;
@@ -178,9 +181,53 @@ public class ERXDatabaseContextMulticastingDelegate extends ERXMulticastingDeleg
 
     /**
      * @see com.webobjects.eoaccess.EODatabaseContext.Delegate#databaseContextShouldUpdateCurrentSnapshot(com.webobjects.eoaccess.EODatabaseContext, com.webobjects.foundation.NSDictionary, com.webobjects.foundation.NSDictionary, com.webobjects.eocontrol.EOGlobalID, com.webobjects.eoaccess.EODatabaseChannel)
+     * @see EODatabase#snapshotForGlobalID(EOGlobalID, long)
      */
-    public NSDictionary databaseContextShouldUpdateCurrentSnapshot(EODatabaseContext dbCtxt, NSDictionary dic, NSDictionary dic2, EOGlobalID gid, EODatabaseChannel dbChannel) {
-        return (NSDictionary)perform("databaseContextShouldUpdateCurrentSnapshot", new Object[] {dbCtxt, dic, dic2, gid, dbChannel}, null);
+    public NSDictionary databaseContextShouldUpdateCurrentSnapshot(EODatabaseContext dbCtxt, NSDictionary existingSnapshot, NSDictionary fetchedRow, EOGlobalID gid, EODatabaseChannel dbChannel) {
+    	/* There is no way for this delegate method to say "do what you normally do". Our two choices for a default return value are 
+    	   never update the snapshot or always update it.  What we want is neither, we want the unchanged EOF behavior.  So we 
+    	   re-implement what EODatabaseChannel would so in the absence of this delegate method: updating of the snapshot depends 
+    	   whether we are refreshing and on the snapshot's age and the fetchTimestamp of the EC doing the fetching.
+         */  	
+    	try {
+        	// Again with the object rape.  EODatabaseChannel.currentEditingContext() is private and the underlying instance variable
+        	// is protected.  We need the ec, so we do what we have to...
+        	EOEditingContext ec = (EOEditingContext) currentEditingContext.invoke(dbChannel, new Object[] {});
+        	
+        	// Get the snapshot if it has not expired.  cachedSnapshot will be null if it has expired
+        	// If not null, it should be the same as the existingSnapshot parameter
+        	NSDictionary cachedSnapshot = dbCtxt.database().snapshotForGlobalID(gid, ec.fetchTimestamp());
+
+        	// If we are refreshing or the snapshot in the cache has timed out, but the fetched row 
+        	// matches the cached snapshot, reset the time stamp by recording the existing snapshot again.
+        	if (existingSnapshot.equals(fetchedRow) && (dbChannel.isRefreshingObjects() || cachedSnapshot == null))
+        	{
+        		dbCtxt.database().recordSnapshotForGlobalID(existingSnapshot, gid);
+        	}
+        	
+        	// Handle refreshing fetches.  If the fetched data is the same, we return
+        	// existingSnapshot to avoid a bug in EODatabaseChannel where == is used 
+        	// instead of equals() when this delegate method is called.  Without this, 
+        	// EOObjectStore.ObjectsChangedInStoreNotification will be sent when  there are no real changes.
+        	// This should be fixed in 5.4.2
+        	if (dbChannel.isRefreshingObjects())
+        	{
+        		return  existingSnapshot.equals(fetchedRow) ? existingSnapshot : fetchedRow;
+        	}
+
+        	// Same as for refreshing, if the snapshot has expired but is unchanged in the database,
+        	// return the existing snapshot
+        	if (cachedSnapshot == null && existingSnapshot.equals(fetchedRow))
+        	{
+        		return existingSnapshot;
+        	}
+
+        	// Return the existing snapshot unless it has expired
+        	return cachedSnapshot != null ? existingSnapshot : fetchedRow;
+        }
+        catch (Exception e) {
+            throw NSForwardException._runtimeExceptionForThrowable(e);
+        }
     }
 
 
