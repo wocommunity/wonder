430575ae91735d9a0a257c71cc70ee27fe663c03
thoepfner <thoepfner@8dc76679-1c23-de11-85cd-0050568e728b>
2008-03-31 21:58:44 +0000

    WOConstantValueAssociations were swallowed by superclass and always translated into HTML attributes.

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXDataHyperlink.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXDataHyperlink.java
@@ -6,8 +6,10 @@ import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
 import com.webobjects.appserver.WORequest;
+import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSKeyValueCoding;
+import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSMutableDictionary;
 
 /**
@@ -15,35 +17,46 @@ import com.webobjects.foundation.NSMutableDictionary;
  * unknown bindings into tag attributes, it instead passes them to the page
  * specified in pageName. For instance, if you have a pageName = "PersonPage"
  * and person = "$currentPerson", when the link is clicked, the next page will
- * be a PersonPage with setPerson(currentPerson) passed into it. You can pass in
- * as many bindings as you need.
+ * be a PersonPage with setPerson(currentPerson) passed into it.
+ * 
+ * You can pass in as many bindings as you need, but there are some bindings
+ * that are used by this element by itself and thus are not passed to the page:
+ * The ones defined by WOHyperlink and additionally "id", "class", "onclick" and
+ * "otherTagString", which are handled like bindings on a stock WOHyperlink.
  * 
  * @author mschrag
+ * @author timo
  */
 public class ERXDataHyperlink extends ERXHyperlink {
-	public ERXDataHyperlink(String s, NSDictionary nsdictionary, WOElement woelement) {
-		super(s, nsdictionary, woelement);
-	}
-
-	@Override
+	private final static NSArray<String> BINDINGS_HANDLED_BY_SUPER_CLASS = new NSMutableArray<String>("id", "class", "onclick", "otherTagString");
+	private NSMutableDictionary<String,WOAssociation> _bindingAssociations=new NSMutableDictionary<String,WOAssociation>();
+	
 	@SuppressWarnings("unchecked")
-	public NSMutableDictionary<String, WOAssociation> nonUrlAttributeAssociations() {
-		NSMutableDictionary<String, WOAssociation> nonUrlAttributeAssociations = super.nonUrlAttributeAssociations();
-		if (_pageName != null) {
-			nonUrlAttributeAssociations = new NSMutableDictionary();
+	public ERXDataHyperlink(String s, NSDictionary associations, WOElement woelement) {
+		// The WOHyperlink's constructor will remove all associations from
+		// super._associations that directly affect WOHyperlink
+		super(s, associations, woelement);
+		NSMutableDictionary<String, WOAssociation> superAssociations = super._associations;
+		for (String key : superAssociations.allKeys()) {
+			// leave anything listed in BINDINGS_HANDLED_BY_SUPERCLASS in
+			// super._associations to be handled by the superclass.
+			if (!BINDINGS_HANDLED_BY_SUPER_CLASS.containsObject(key)) {
+				// Move all other associations to our own _bindingAssociations,
+				// so they can later be used as bindings for the page specified
+				// in pageName.
+				_bindingAssociations.setObjectForKey(superAssociations.removeObjectForKey(key), key);
+			}
 		}
-		return nonUrlAttributeAssociations;
 	}
-
+	
 	@Override
 	@SuppressWarnings("unchecked")
 	public WOActionResults invokeAction(WORequest worequest, WOContext wocontext) {
 		WOActionResults results = super.invokeAction(worequest, wocontext);
-		NSMutableDictionary<String, WOAssociation> nonUrlAttributeAssociations = super.nonUrlAttributeAssociations();
 		if (results != null && _pageName != null) {
 			WOComponent component = wocontext.component();
-			for (String bindingName : nonUrlAttributeAssociations.allKeys()) {
-				WOAssociation association = nonUrlAttributeAssociations.objectForKey(bindingName);
+			for (String bindingName : _bindingAssociations.allKeys()) {
+				WOAssociation association = _bindingAssociations.objectForKey(bindingName);
 				Object value = association.valueInComponent(component);
 				NSKeyValueCoding.Utility.takeValueForKey(results, value, bindingName);
 			}
