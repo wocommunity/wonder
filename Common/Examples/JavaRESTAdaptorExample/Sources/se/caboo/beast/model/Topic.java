0098d713b2ee26dacd89c0d3eba8e41c8f299dee
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-04-12 22:12:33 +0000

    JavaRESTAdaptorExample

---
+++ Common/Examples/JavaRESTAdaptorExample/Sources/se/caboo/beast/model/Topic.java
@@ -0,0 +1,7 @@
+package se.caboo.beast.model;
+
+import org.apache.log4j.Logger;
+
+public class Topic extends _Topic {
+  private static Logger log = Logger.getLogger(Topic.class);
+}
---
+++ Common/Examples/JavaRESTAdaptorExample/Sources/se/caboo/beast/model/User.java
@@ -0,0 +1,7 @@
+package se.caboo.beast.model;
+
+import org.apache.log4j.Logger;
+
+public class User extends _User {
+  private static Logger log = Logger.getLogger(User.class);
+}
