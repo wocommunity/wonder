2c0ade3028aad9437badd9438840f994854c6775
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2008-04-19 03:10:49 +0000

    Wonder-100 Add refresh on select tab to AjaxTabbedPanel Fix bug in PeriodicalExecuter usage

--- Ajax/Ajax/Sources/er/ajax/AjaxTabbedPanel.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxTabbedPanel.java
@@ -13,7 +13,7 @@ import com.webobjects.foundation.*;
  * demand so only the selected tab is rendered when the page is displayed. If
  * tabs can take a while to load, a hidden div containing a "working" message
  * can temporarily replace the panel content while it loads.
- * 
+ *
  * <h3>CSS Classes Used by AjaxTabbedPanel and AjaxTabbedPanelTab</h3>
  * <table>
  * <tr>
@@ -45,55 +45,55 @@ import com.webobjects.foundation.*;
  * <td>The LI representing the unselected panel(s).</td>
  * </tr>
  * </table
- * 
+ *
  * @binding id required, String the id of the UL that wraps the tabs
  * @binding busyDiv optional, String the id of a div that should be shown when a
  *          tab is loading
- * 
+ *
  * @author Chuck Hill
  */
 public class AjaxTabbedPanel extends AjaxDynamicElement {
-    
+
     private WOElement content;
     private NSMutableArray tabs = new NSMutableArray();
     private WOAssociation id;
     private WOAssociation busyDiv;
-    
-    
+
+
     public AjaxTabbedPanel(String name, NSDictionary associations, WOElement template) {
         super(name, associations, template);
         content = template;
         id = (WOAssociation) associations.objectForKey("id");
         busyDiv = (WOAssociation) associations.objectForKey("busyDiv");
         findTabs((WODynamicGroup)template);
-        
+
         if (id == null)
         {
         	throw new RuntimeException("id binding is required");
         }
     }
 
-    
+
     /**
      * Looks through the child components to local the AjaxTabbedPanelTabs that are controlled by this panel.
      * Tabs without an explicit id attributed are assigned a caculated one.
-     * 
+     *
      * @param template the graph of elements passed to the constructor.
      */
     private void findTabs(WODynamicGroup template)  {
     	if (template == null) return;
-    	
+
         NSArray children = template.childrenElements();
         for (int i = 0; i < children.count(); i++) {
             WOElement child = (WOElement)children.objectAtIndex(i);
             if (child instanceof AjaxTabbedPanelTab) {
             	AjaxTabbedPanelTab childTab = (AjaxTabbedPanelTab)child;
-            	
+
             	// The tabs need to have an id attribute so we assign one if needed
                 if (childTab.id() == null) {
                     childTab.setId(new WOConstantValueAssociation(id.valueInComponent(null) + "_pane_" + tabs.count()));
                 }
-                
+
                 tabs.addObject(childTab);
             }
             else if (child instanceof WODynamicGroup) {
@@ -102,7 +102,7 @@ public class AjaxTabbedPanel extends AjaxDynamicElement {
         }
     }
 
-    
+
     /**
      * Creates the tabs and pane control.
      */
@@ -120,7 +120,7 @@ public class AjaxTabbedPanel extends AjaxDynamicElement {
         response.appendContentString(">\n");
 
         String paneControlID = idString + "_panecontrol";
-        
+
         for (int i = 0; i < tabs.count(); i++) {
             String index = new Integer(i).toString();
             String tabID = idString + "_tab_" + index;
@@ -136,15 +136,15 @@ public class AjaxTabbedPanel extends AjaxDynamicElement {
             response.appendContentString("<a ");
             appendTagAttributeToResponse(response, "id", panelTabID);
             response.appendContentString(" href=\"javascript:void(0)\" onclick=\"");
-            
-            if ( ! isSelectedTab) {
-                response.appendContentString("AjaxTabbedPanel.loadPanel('");
-                response.appendContentString(panelID);
-                response.appendContentString("', '");
-                response.appendContentString((busyDiv != null) ? (String)busyDiv.valueInComponent(component) : "");
-                response.appendContentString("'); ");
-            }
-            
+
+            response.appendContentString("AjaxTabbedPanel.loadPanel('");
+            response.appendContentString(panelID);
+            response.appendContentString("', '");
+            response.appendContentString((busyDiv != null) ? (String)busyDiv.valueInComponent(component) : "");
+            response.appendContentString("', ");
+            response.appendContentString(tab.refreshesOnSelect(context.component()).toString());
+            response.appendContentString("); ");
+
             response.appendContentString("AjaxTabbedPanel.selectTab('");
             response.appendContentString(idString);
             response.appendContentString("', '");
@@ -158,14 +158,14 @@ public class AjaxTabbedPanel extends AjaxDynamicElement {
             response.appendContentString("</a>\n");
             response.appendContentString("</li>\n");
         }
-        
+
         response.appendContentString("</ul>\n");
 
         // UL for panes
         response.appendContentString("<ul class=\"ajaxTabbedPanelPanes\" ");
         appendTagAttributeToResponse(response, "id", paneControlID);
         response.appendContentString(">\n");
-        // The tabs render themself as pane
+        // The tabs render themselves as panes
         if (content !=  null)
         {
         	content.appendToResponse(response, context);
