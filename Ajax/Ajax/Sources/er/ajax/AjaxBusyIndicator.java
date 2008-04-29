a8dee51080bbb1eb50ca2333cc8efee069f9a753
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-04-29 14:48:45 +0000

    support for per-element busy indicators (via watchContainerID) as well as onCreate and onComplete functions

--- Ajax/Ajax/Sources/er/ajax/AjaxBusyIndicator.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxBusyIndicator.java
@@ -5,42 +5,52 @@ import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 
+/**
+ * AjaxBusyIndicator provides various ways of performing operations when an Ajax requst is in process.
+ * 
+ * @binding busyClass (optional) the CSS class to apply to the updating element during the request
+ * @binding divID (optional) the id of the div to show and hide during the request
+ * @binding onCreate (optional) the function to execute when the request starts
+ * @binding onComplete (optional) the function to execute when the request ends
+ * @binding busyImage (optional) if set, a busy div will be automatically created with this image in it
+ * @binding busyImageFramework (optional) the framework that contains the busy image
+ * @binding watchContainerID (optional) if set, the other bindings will only apply when this container ID is being
+ *          updated, which provides for per-element busy controls
+ *          
+ * @author mschrag
+ */
 public class AjaxBusyIndicator extends AjaxComponent {
-  public AjaxBusyIndicator(WOContext context) {
-    super(context);
-  }
+	public AjaxBusyIndicator(WOContext context) {
+		super(context);
+	}
 
-  public boolean isStateless() {
-    return true;
-  }
+	public boolean isStateless() {
+		return true;
+	}
 
-  public boolean synchronizesVariablesWithBindings() {
-    return false;
-  }
+	public boolean synchronizesVariablesWithBindings() {
+		return false;
+	}
 
-  protected void addRequiredWebResources(WOResponse res) {
-    addScriptResourceInHead(res, "prototype.js");
-	addScriptResourceInHead(res, "effects.js");
-  }
+	protected void addRequiredWebResources(WOResponse res) {
+		addScriptResourceInHead(res, "prototype.js");
+		addScriptResourceInHead(res, "effects.js");
+	}
 
-  public String busyClass() {
-    return (String) valueForBinding("busyClass");
-  }
+	public String divID() {
+		return (String) valueForBinding("divID", "busy");
+	}
 
-  public String busyImage() {
-    return (String) valueForBinding("busyImage");
-  }
+	public String onCreate() {
+		return (String) valueForBinding("onCreate", "null");
+	}
 
-  public String busyImageFramework() {
-    return (String) valueForBinding("busyImageFramework");
-  }
+	public String onComplete() {
+		return (String) valueForBinding("onComplete", "null");
+	}
 
-  public String divID() {
-    return (String) valueForBinding("divID", "busy");
-  }
-
-  public WOActionResults handleRequest(WORequest request, WOContext context) {
-    return null;
-  }
+	public WOActionResults handleRequest(WORequest request, WOContext context) {
+		return null;
+	}
 
 }
