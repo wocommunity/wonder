8301bbc93f0760a954b597a6356eaadc9a43bc35
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-05 15:51:09 +0000

    converted from SQL scripts to Migration API

--- Common/Frameworks/ERAttachment/Sources/er/attachment/migrations/ERAttachment1.java
+++ Common/Frameworks/ERAttachment/Sources/er/attachment/migrations/ERAttachment1.java
@@ -1,14 +1,27 @@
 package er.attachment.migrations;
 
-import er.extensions.migration.ERXMigration;
+import java.util.TimeZone;
+
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSTimestamp;
+
+import er.extensions.migration.ERXMigrationDatabase;
 
 /**
  * Add creation date and available boolean.
  * 
  * @author mschrag
  */
-public class ERAttachment1 extends ERXMigration {
-  public ERAttachment1() {
-    super(true);
+public class ERAttachment1 extends ERXMigrationDatabase.Migration {
+  @Override
+  public void downgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+    database.existingTableNamed("ERAttachment").existingColumnNamed("available").delete();
+    database.existingTableNamed("ERAttachment").existingColumnNamed("creationDate").delete();
+  }
+
+  @Override
+  public void upgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+    database.existingTableNamed("ERAttachment").newBooleanColumn("available", false, Boolean.TRUE);
+    database.existingTableNamed("ERAttachment").newTimestampColumn("creationDate", false, new NSTimestamp(2007, 8, 1, 0, 0, 0, TimeZone.getDefault()));
   }
 }
