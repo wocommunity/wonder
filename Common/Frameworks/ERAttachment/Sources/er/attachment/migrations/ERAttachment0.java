1d99dc9a92882bd69423d9d9db3fa220a7532c23
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-05 15:51:09 +0000

    converted from SQL scripts to Migration API
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7544 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERAttachment/Sources/er/attachment/migrations/ERAttachment0.java
+++ Common/Frameworks/ERAttachment/Sources/er/attachment/migrations/ERAttachment0.java
@@ -1,14 +1,52 @@
 package er.attachment.migrations;
 
-import er.extensions.migration.ERXMigration;
+import com.webobjects.eocontrol.EOEditingContext;
+
+import er.extensions.migration.ERXMigrationDatabase;
+import er.extensions.migration.ERXMigrationTable;
 
 /**
  * Performs the initial database table creation.
  * 
  * @author mschrag
  */
-public class ERAttachment0 extends ERXMigration {
-  public ERAttachment0() {
-    super(true);
+public class ERAttachment0 extends ERXMigrationDatabase.Migration {
+  @Override
+  public void downgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+    database.existingTableNamed("ERAttachment").drop();
+    database.existingTableNamed("ERAttachmentData").drop();
+  }
+
+  @Override
+  public void upgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+    ERXMigrationTable attachmentTable = database.newTableNamed("ERAttachment");
+    attachmentTable.newIntegerColumn("height", true);
+    attachmentTable.newIntegerColumn("id", false);
+    attachmentTable.newStringColumn("mimeType", 100, false);
+    attachmentTable.newStringColumn("configurationName", 100, true);
+    attachmentTable.newStringColumn("ownerID", 16, true);
+    attachmentTable.newStringColumn("originalFileName", 255, false);
+    attachmentTable.newIntegerColumn("parentID", true);
+    attachmentTable.newStringColumn("proxied", 5, false);
+    attachmentTable.newIntegerColumn("size", false);
+    attachmentTable.newStringColumn("storageType", 10, true);
+    attachmentTable.newBlobColumn("smallData", true);
+    attachmentTable.newStringColumn("thumbnail", 10, true);
+    attachmentTable.newStringColumn("webPath", 1000, false);
+    attachmentTable.newIntegerColumn("width", true);
+    attachmentTable.newIntegerColumn("attachmentDataID", true);
+    attachmentTable.newStringColumn("filesystemPath", 255, true);
+    attachmentTable.newStringColumn("s3Path", 1000, true);
+    attachmentTable.create();
+    attachmentTable.setPrimaryKey("id");
+    attachmentTable.addUniqueIndex("ERAttachmentWebPath", "webPath");
+
+    ERXMigrationTable attachmentDataTable = database.newTableNamed("ERAttachmentData");
+    attachmentDataTable.newBlobColumn("data", true);
+    attachmentDataTable.newIntegerColumn("id", false);
+    attachmentDataTable.create();
+    attachmentDataTable.setPrimaryKey("id");
+
+    attachmentTable.addForeignKey("attachmentDataID", "ERAttachmentData", "id");
   }
 }
