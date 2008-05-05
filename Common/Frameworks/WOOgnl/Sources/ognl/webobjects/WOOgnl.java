e8e08c4e0a39751f73a50ff132bee24353fad333
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-05 23:39:32 +0000

    Added support for determining the destination class of keypaths that have null components so they can pass to helper functions properly ... this required a NASTY patch in WOOgnl because it appears that OGNL does not LIKE to traverse keypaths with null components (whereas NSKVC will just return null if any part of the path evaluates to null)
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7552 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/WOOgnl/Sources/ognl/webobjects/WOOgnl.java
+++ Common/Frameworks/WOOgnl/Sources/ognl/webobjects/WOOgnl.java
@@ -39,160 +39,180 @@ import com.webobjects.foundation._NSUtilities;
 public class WOOgnl {
 	public static Logger log = Logger.getLogger(WOOgnl.class);
 
-    public static final String DefaultWOOgnlBindingFlag = "~";
+	public static final String DefaultWOOgnlBindingFlag = "~";
 
-    protected static NSMutableArray _retainerArray = new NSMutableArray();
-    static {
-        try {
-            Observer o = new Observer();
-            _retainerArray.addObject(o);
-            NSNotificationCenter.defaultCenter().addObserver(o,
-                                                             new NSSelector("configureWOOgnl", new Class[]
-                                                                            { com.webobjects.foundation.NSNotification.class }),
-                                                             WOApplication.ApplicationWillFinishLaunchingNotification,
-                                                             null);
-        } catch (Exception e) {
-        	WOOgnl.log.error("Failed to configure WOOgnl.", e);
-        }
-    }
+	protected static NSMutableArray _retainerArray = new NSMutableArray();
+	static {
+		try {
+			Observer o = new Observer();
+			_retainerArray.addObject(o);
+			NSNotificationCenter.defaultCenter().addObserver(o, new NSSelector("configureWOOgnl", new Class[] { com.webobjects.foundation.NSNotification.class }), WOApplication.ApplicationWillFinishLaunchingNotification, null);
+		}
+		catch (Exception e) {
+			WOOgnl.log.error("Failed to configure WOOgnl.", e);
+		}
+	}
 
-    private static Hashtable associationMappings = new Hashtable();
-    
-    public static void setAssociationClassForPrefix(Class clazz, String prefix) {
-    	associationMappings.put(prefix, clazz);
-    }
-    
-    private WOAssociation createAssociationForClass(Class clazz, String value, boolean isConstant) {
-    	return (WOAssociation) _NSUtilities.instantiateObject(clazz, new Class[]{Object.class, boolean.class}, new Object[]{value, Boolean.valueOf(isConstant)}, true, false);
-    }
+	private static Hashtable associationMappings = new Hashtable();
 
-    public static class Observer {
-        public void configureWOOgnl(NSNotification n) {
-            WOOgnl.factory().configureWOForOgnl();
-            NSNotificationCenter.defaultCenter().removeObserver(this);
-            _retainerArray.removeObject(this);
-        }
-    }
+	public static void setAssociationClassForPrefix(Class clazz, String prefix) {
+		associationMappings.put(prefix, clazz);
+	}
 
-    protected static WOOgnl _factory;
-    public static WOOgnl factory() {
-        if (_factory == null) {
-            _factory = new WOOgnl();
-        }
-        return _factory;
-    }
-    public static void setFactory(WOOgnl factory) { _factory = factory; }
-    
-    public ClassResolver classResolver() { return NSClassResolver.sharedInstance(); }
+	private WOAssociation createAssociationForClass(Class clazz, String value, boolean isConstant) {
+		return (WOAssociation) _NSUtilities.instantiateObject(clazz, new Class[] { Object.class, boolean.class }, new Object[] { value, Boolean.valueOf(isConstant) }, true, false);
+	}
 
-    public String ognlBindingFlag() { return DefaultWOOgnlBindingFlag; }
+	public static class Observer {
+		public void configureWOOgnl(NSNotification n) {
+			WOOgnl.factory().configureWOForOgnl();
+			NSNotificationCenter.defaultCenter().removeObserver(this);
+			_retainerArray.removeObject(this);
+		}
+	}
 
-    public Hashtable newDefaultContext() {
-        Hashtable h = new Hashtable();
-        if (classResolver() != null) {
-            h.put("classResolver", classResolver());
-        }
-        return h;
-    }
-    
-    public void configureWOForOgnl() {
-        // Configure runtime.
-        // Configure foundation classes.
-        OgnlRuntime.setPropertyAccessor(Object.class, new NSObjectPropertyAccessor());
-        OgnlRuntime.setPropertyAccessor(NSArray.class, new NSArrayPropertyAccessor());
-        OgnlRuntime.setPropertyAccessor(NSDictionary.class, new NSDictionaryPropertyAccessor());
+	protected static WOOgnl _factory;
 
-        NSFoundationElementsAccessor e = new NSFoundationElementsAccessor();
-        OgnlRuntime.setElementsAccessor(NSArray.class, e);
-        OgnlRuntime.setElementsAccessor(NSDictionary.class, e);
-        OgnlRuntime.setElementsAccessor(NSSet.class, e);
-        // Register template parser
-        if (!"false".equals(System.getProperty("ognl.active"))) {
-        	String parserClassName = System.getProperty("ognl.parserClassName", "ognl.helperfunction.WOHelperFunctionHTMLTemplateParser");
-        	WOMiddleManParser.setWOHTMLTemplateParserClassName(parserClassName);
-    	    if ("true".equalsIgnoreCase(System.getProperty("ognl.inlineBindings"))) {
-    	    	WOHelperFunctionHTMLTemplateParser.setAllowInlineBindings(true);
-    	    }
-    	    if ("true".equalsIgnoreCase(System.getProperty("ognl.parseStandardTags"))) {
-    	    	WOHelperFunctionHTMLParser.setParseStandardTags(true);
-    	    }
-        }
-    }
+	public static WOOgnl factory() {
+		if (_factory == null) {
+			_factory = new WOOgnl();
+		}
+		return _factory;
+	}
 
-    public void convertOgnlConstantAssociations(NSMutableDictionary associations) {
-        for (Enumeration e = associations.keyEnumerator(); e.hasMoreElements();) {
-        	String name = (String)e.nextElement();
-        	WOAssociation association = (WOAssociation) associations.objectForKey(name);
-        	boolean isConstant = false;
-        	String keyPath = null;
-        	if (association instanceof WOConstantValueAssociation) {
+	public static void setFactory(WOOgnl factory) {
+		_factory = factory;
+	}
+
+	public ClassResolver classResolver() {
+		return NSClassResolver.sharedInstance();
+	}
+
+	public String ognlBindingFlag() {
+		return DefaultWOOgnlBindingFlag;
+	}
+
+	public Hashtable newDefaultContext() {
+		Hashtable h = new Hashtable();
+		if (classResolver() != null) {
+			h.put("classResolver", classResolver());
+		}
+		return h;
+	}
+
+	public void configureWOForOgnl() {
+		// Configure runtime.
+		// Configure foundation classes.
+		OgnlRuntime.setPropertyAccessor(Object.class, new NSObjectPropertyAccessor());
+		OgnlRuntime.setPropertyAccessor(NSArray.class, new NSArrayPropertyAccessor());
+		OgnlRuntime.setPropertyAccessor(NSDictionary.class, new NSDictionaryPropertyAccessor());
+
+		NSFoundationElementsAccessor e = new NSFoundationElementsAccessor();
+		OgnlRuntime.setElementsAccessor(NSArray.class, e);
+		OgnlRuntime.setElementsAccessor(NSDictionary.class, e);
+		OgnlRuntime.setElementsAccessor(NSSet.class, e);
+		// Register template parser
+		if (!"false".equals(System.getProperty("ognl.active"))) {
+			String parserClassName = System.getProperty("ognl.parserClassName", "ognl.helperfunction.WOHelperFunctionHTMLTemplateParser");
+			WOMiddleManParser.setWOHTMLTemplateParserClassName(parserClassName);
+			if ("true".equalsIgnoreCase(System.getProperty("ognl.inlineBindings"))) {
+				WOHelperFunctionHTMLTemplateParser.setAllowInlineBindings(true);
+			}
+			if ("true".equalsIgnoreCase(System.getProperty("ognl.parseStandardTags"))) {
+				WOHelperFunctionHTMLParser.setParseStandardTags(true);
+			}
+		}
+	}
+
+	public void convertOgnlConstantAssociations(NSMutableDictionary associations) {
+		for (Enumeration e = associations.keyEnumerator(); e.hasMoreElements();) {
+			String name = (String) e.nextElement();
+			WOAssociation association = (WOAssociation) associations.objectForKey(name);
+			boolean isConstant = false;
+			String keyPath = null;
+			if (association instanceof WOConstantValueAssociation) {
 				WOConstantValueAssociation constantAssociation = (WOConstantValueAssociation) association;
 				// AK: this sucks, but there is no API to get at the value
 				Object value = constantAssociation.valueInComponent(null);
 				keyPath = value != null ? value.toString() : null;
 				isConstant = true;
-			} else if(association instanceof WOKeyValueAssociation) {
+			}
+			else if (association instanceof WOKeyValueAssociation) {
 				keyPath = association.keyPath();
-			} else if(association instanceof WOBindingNameAssociation) {
-				WOBindingNameAssociation b = (WOBindingNameAssociation)association;
+			}
+			else if (association instanceof WOBindingNameAssociation) {
+				WOBindingNameAssociation b = (WOBindingNameAssociation) association;
 				// AK: strictly speaking, this is not correct, as we only get the first part of 
 				// the path. But take a look at WOBindingNameAssociation for a bit of fun...
 				keyPath = "^" + b._parentBindingName;
 			}
-        	if(keyPath != null) {
-        		if(associationMappings.size() != 0) {
-        			int index = name.indexOf(':');
-        			if(index > 0) {
-        				String prefix = name.substring(0, index);
-        				if(prefix != null) {
-        					Class c = (Class) associationMappings.get(prefix);
-        					if(c != null) {
-        						String postfix = name.substring(index+1);
-        						WOAssociation newAssociation = createAssociationForClass(c, keyPath, isConstant);
-        						associations.removeObjectForKey(name);
-        						associations.setObjectForKey(newAssociation, postfix);
-        					}
-        				}
-        			}
-        		}
-        		if(isConstant && keyPath != null && keyPath.startsWith(ognlBindingFlag())) {
-        			String ognlExpression = keyPath.substring(ognlBindingFlag().length(),
-        					keyPath.length());
-        			if (ognlExpression.length() > 0) {
-        				WOAssociation newAssociation = new WOOgnlAssociation(ognlExpression);
-        				NSArray keys = associations.allKeysForObject(association);
-        				//if (log.isDebugEnabled())
-        				//    log.debug("Constructing Ognl association for binding key(s): "
-        				//              + (keys.count() == 1 ? keys.lastObject() : keys) + " expression: " + ognlExpression);
-        				if (keys.count() == 1) {
-        					associations.setObjectForKey(newAssociation, keys.lastObject());
-        				} else {
-        					for (Enumeration ee = keys.objectEnumerator(); ee.hasMoreElements();) {
-        						associations.setObjectForKey(newAssociation, e.nextElement());    
-        					}
-        				}
-        			}
-        		}
-            }
-        }
-    }
+			if (keyPath != null) {
+				if (associationMappings.size() != 0) {
+					int index = name.indexOf(':');
+					if (index > 0) {
+						String prefix = name.substring(0, index);
+						if (prefix != null) {
+							Class c = (Class) associationMappings.get(prefix);
+							if (c != null) {
+								String postfix = name.substring(index + 1);
+								WOAssociation newAssociation = createAssociationForClass(c, keyPath, isConstant);
+								associations.removeObjectForKey(name);
+								associations.setObjectForKey(newAssociation, postfix);
+							}
+						}
+					}
+				}
+				if (isConstant && keyPath != null && keyPath.startsWith(ognlBindingFlag())) {
+					String ognlExpression = keyPath.substring(ognlBindingFlag().length(), keyPath.length());
+					if (ognlExpression.length() > 0) {
+						WOAssociation newAssociation = new WOOgnlAssociation(ognlExpression);
+						NSArray keys = associations.allKeysForObject(association);
+						//if (log.isDebugEnabled())
+						//    log.debug("Constructing Ognl association for binding key(s): "
+						//              + (keys.count() == 1 ? keys.lastObject() : keys) + " expression: " + ognlExpression);
+						if (keys.count() == 1) {
+							associations.setObjectForKey(newAssociation, keys.lastObject());
+						}
+						else {
+							for (Enumeration ee = keys.objectEnumerator(); ee.hasMoreElements();) {
+								associations.setObjectForKey(newAssociation, e.nextElement());
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
-    public Object getValue(String expression, Object obj) {
-        Object value = null;
-        try {
-            value = Ognl.getValue(expression, newDefaultContext(), obj);
-        } catch (OgnlException ex) {
-            throw new RuntimeException("Failed to get value '" + expression + "' on " + obj, ex);
-        }
-        return value;
-    } 
-    
-    public void setValue(String expression, Object obj, Object value) {
-        try {
-            Ognl.setValue(expression, newDefaultContext(), obj, value);
-        } catch (OgnlException ex) {
-            throw new RuntimeException("Failed to set value '" + expression + "' on " + obj, ex);
-        }
-    } 
-    
-}	
+	public Object getValue(String expression, Object obj) {
+		Object value = null;
+		try {
+			value = Ognl.getValue(expression, newDefaultContext(), obj);
+		}
+		catch (OgnlException ex) {
+			String message = ex.getMessage();
+			// MS: This is SUPER SUPER LAME, but I don't see any other way in OGNL to
+			// make keypaths with null components behave like NSKVC (i.e. returning null
+			// vs throwing an exception).  They have something called nullHandlers 
+			// in OGNL, but it appears that you have to register it per-class and you
+			// can't override the factory.
+			if (message != null && message.startsWith("source is null for getProperty(null, ")) {
+				value = null;
+			}
+			else {
+				throw new RuntimeException("Failed to get value '" + expression + "' on " + obj, ex);
+			}
+		}
+		return value;
+	}
+
+	public void setValue(String expression, Object obj, Object value) {
+		try {
+			Ognl.setValue(expression, newDefaultContext(), obj, value);
+		}
+		catch (OgnlException ex) {
+			throw new RuntimeException("Failed to set value '" + expression + "' on " + obj, ex);
+		}
+	}
+
+}
