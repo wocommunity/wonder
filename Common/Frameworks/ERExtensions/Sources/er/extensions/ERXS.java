53d212feaa0715f9e5d3019a1ead818d381ad2c3
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-14 19:25:22 +0000

    ERXSortOrdering/ERXSortOrderings -- adds chaining support for EOSortOrdering

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXS.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXS.java
@@ -17,55 +17,55 @@ public class ERXS {
 	public static final NSSelector INS_ASC = EOSortOrdering.CompareCaseInsensitiveAscending;
 	public static final NSSelector INS_DESC = EOSortOrdering.CompareCaseInsensitiveDescending;
 
-	public static EOSortOrdering sortOrder(String key, NSSelector comparison) {
-		return EOSortOrdering.sortOrderingWithKey(key, comparison);
+	public static ERXSortOrdering sortOrder(String key, NSSelector comparison) {
+		return ERXSortOrdering.sortOrderingWithKey(key, comparison);
 	}
 
-	public static EOSortOrdering desc(String key) {
-		return EOSortOrdering.sortOrderingWithKey(key, ERXS.DESC);
+	public static ERXSortOrdering desc(String key) {
+		return ERXSortOrdering.sortOrderingWithKey(key, ERXS.DESC);
 	}
 
-	public static NSArray<EOSortOrdering> descs(String... keys) {
+	public static ERXSortOrdering.ERXSortOrderings descs(String... keys) {
 		return sortOrders(DESC, keys);
 	}
 
-	public static EOSortOrdering asc(String key) {
-		return EOSortOrdering.sortOrderingWithKey(key, ERXS.ASC);
+	public static ERXSortOrdering asc(String key) {
+		return ERXSortOrdering.sortOrderingWithKey(key, ERXS.ASC);
 	}
 
-	public static NSArray<EOSortOrdering> ascs(String... keys) {
+	public static ERXSortOrdering.ERXSortOrderings ascs(String... keys) {
 		return sortOrders(ASC, keys);
 	}
 
-	public static EOSortOrdering descInsensitive(String key) {
-		return EOSortOrdering.sortOrderingWithKey(key, ERXS.INS_DESC);
+	public static ERXSortOrdering descInsensitive(String key) {
+		return ERXSortOrdering.sortOrderingWithKey(key, ERXS.INS_DESC);
 	}
 
-	public static NSArray<EOSortOrdering> descInsensitives(String... keys) {
+	public static ERXSortOrdering.ERXSortOrderings descInsensitives(String... keys) {
 		return sortOrders(INS_DESC, keys);
 	}
 
-	public static EOSortOrdering ascInsensitive(String key) {
-		return EOSortOrdering.sortOrderingWithKey(key, ERXS.INS_ASC);
+	public static ERXSortOrdering ascInsensitive(String key) {
+		return ERXSortOrdering.sortOrderingWithKey(key, ERXS.INS_ASC);
 	}
 
-	public static NSArray<EOSortOrdering> ascInsensitives(String... keys) {
+	public static ERXSortOrdering.ERXSortOrderings ascInsensitives(String... keys) {
 		return sortOrders(INS_ASC, keys);
 	}
 
-	public static NSArray<EOSortOrdering> sortOrders(String key, NSSelector comparison) {
-		return new NSArray<EOSortOrdering>(EOSortOrdering.sortOrderingWithKey(key, comparison));
+	public static ERXSortOrdering.ERXSortOrderings sortOrders(String key, NSSelector comparison) {
+		return new ERXSortOrdering(key, comparison).array();
 	}
 
-	public static NSArray<EOSortOrdering> sortOrders(String key1, NSSelector comparison1, String key2, NSSelector comparison2) {
-		return new NSArray<EOSortOrdering>(new EOSortOrdering[] { EOSortOrdering.sortOrderingWithKey(key1, comparison1), EOSortOrdering.sortOrderingWithKey(key2, comparison2) });
+	public static ERXSortOrdering.ERXSortOrderings sortOrders(String key1, NSSelector comparison1, String key2, NSSelector comparison2) {
+		return new ERXSortOrdering.ERXSortOrderings(new EOSortOrdering[] { ERXSortOrdering.sortOrderingWithKey(key1, comparison1), ERXSortOrdering.sortOrderingWithKey(key2, comparison2) });
 	}
 
 
-	public static NSArray<EOSortOrdering> sortOrders(NSSelector sel, String...keys) {
-		NSMutableArray<EOSortOrdering> result = new NSMutableArray<EOSortOrdering>();
+	public static ERXSortOrdering.ERXSortOrderings sortOrders(NSSelector sel, String...keys) {
+	  ERXSortOrdering.ERXSortOrderings result = new ERXSortOrdering.ERXSortOrderings();
 		for (String key : keys) {
-			result.addObject(new EOSortOrdering(key, sel));
+			result.addObject(ERXSortOrdering.sortOrderingWithKey(key, sel));
 		}
 		return result;
 	}
@@ -87,12 +87,12 @@ public class ERXS {
 		return EOSortOrdering.sortedArrayUsingKeyOrderArray(array, orderings);
 	}
 
-	public static NSArray<EOSortOrdering> chain(EOSortOrdering... sortOrderings) {
-		return new NSArray<EOSortOrdering>(sortOrderings);
+	public static ERXSortOrdering.ERXSortOrderings chain(EOSortOrdering... sortOrderings) {
+		return new ERXSortOrdering.ERXSortOrderings(sortOrderings);
 	}
 
-	public static NSArray<EOSortOrdering> chain(NSArray<EOSortOrdering>... arr) {
-		NSMutableArray<EOSortOrdering> result = new NSMutableArray<EOSortOrdering>();
+	public static ERXSortOrdering.ERXSortOrderings chain(NSArray<EOSortOrdering>... arr) {
+	  ERXSortOrdering.ERXSortOrderings result = new ERXSortOrdering.ERXSortOrderings();
 		for (NSArray<EOSortOrdering> value : arr) {
 			result.addObjects(value.objects());
 		}
