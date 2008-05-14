17e8d02e9ae7d3a3cee991dcaaa483ba20a05d73
dfrolov <dfrolov@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-14 15:45:50 +0000

    Test case for ERXRuntimeUtilities

---
+++ Common/Tests/ERXTest/Sources/er/erxtest/tests/ERXRuntimeUtilitiesTest.java
@@ -0,0 +1,18 @@
+package er.erxtest.tests;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+import er.extensions.ERXRuntimeUtilities;
+import er.extensions.ERXRuntimeUtilities.Result;
+import er.extensions.ERXRuntimeUtilities.TimeoutException;
+
+public class ERXRuntimeUtilitiesTest extends TestCase {
+
+	public void testExecuteMultipleFastCommands() throws IOException, TimeoutException {
+		for (int i = 0; i < 100; ++i) {
+		    Result result = ERXRuntimeUtilities.execute(new String[] {"echo", String.valueOf(i)}, null, null, 0);
+		    assertEquals(i + "\n", result.getResponseAsString());
+		}
+	}
+}
