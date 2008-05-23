49feb39b174b1b0e805663dfa6d63970f000b16e
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-23 16:50:42 +0000

    added (untested) ERXKeyGlobalID

---
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXKeyGlobalID.java
@@ -0,0 +1,78 @@
+package er.extensions;
+
+import com.webobjects.eocontrol.EOKeyGlobalID;
+import com.webobjects.eocontrol._EOVectorKeyGlobalID;
+import com.webobjects.foundation.NSData;
+import com.webobjects.foundation.NSPropertyListSerialization;
+
+/**
+ * Serializable Global ID class. You can use this as a custom attribute value type.
+ *  
+ * @author ak
+ *
+ */
+public class ERXKeyGlobalID extends EOKeyGlobalID {
+
+	private EOKeyGlobalID _gid;
+	
+	public ERXKeyGlobalID(String entityName, Object values[]) {
+		super(entityName, 0);
+		_gid = EOKeyGlobalID.globalIDWithEntityName(entityName, values);
+	}
+
+	public String asString() {
+		return entityName() + "." + ERXEOControlUtilities.primaryKeyStringForGlobalID(this);
+	}
+
+	public NSData asData() {
+		return new NSData(asString().getBytes());
+	}
+
+	public static ERXKeyGlobalID fromString(String value) {
+		if(value == null) {
+			return null;
+		}
+		String entityName = ERXStringUtilities.firstPropertyKeyInKeyPath(value);
+		String valueString = ERXStringUtilities.keyPathWithoutFirstProperty(value);
+		Object pkValue = NSPropertyListSerialization.propertyListFromString(valueString);
+		return null;
+	}
+
+	public static ERXKeyGlobalID fromData(NSData value) {
+		return fromString(new String(value.bytes()));
+	}
+
+	public static ERXKeyGlobalID globalIDForGID(EOKeyGlobalID gid) {
+		return new ERXKeyGlobalID(gid.entityName(), gid.keyValues());
+	}
+
+	@Override
+	public Object[] _keyValuesNoCopy() {
+		return _gid._keyValuesNoCopy();
+	}
+
+	@Override
+	public int keyCount() {
+		return _gid.keyCount();
+	}
+
+	@Override
+	public String toString() {
+		return _gid.toString();
+	}
+
+	@Override
+	public Object[] keyValues() {
+		return _gid.keyValues();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return _gid.equals(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		return _gid.hashCode();
+	}
+}
