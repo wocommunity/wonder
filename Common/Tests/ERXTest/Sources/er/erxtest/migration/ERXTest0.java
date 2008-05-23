546bf4748e8b0d6812bc6fda6af9c6fde82d81c4
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-23 21:17:00 +0000

    switch to proper migrations

--- Common/Tests/ERXTest/Sources/er/erxtest/migration/ERXTest0.java
+++ Common/Tests/ERXTest/Sources/er/erxtest/migration/ERXTest0.java
@@ -1,23 +1,66 @@
 package er.erxtest.migration;
-import com.webobjects.eoaccess.EOAdaptorChannel;
-import com.webobjects.eoaccess.EOModel;
+
 import com.webobjects.eocontrol.EOEditingContext;
-import com.webobjects.foundation.NSArray;
 
-import er.extensions.ERXJDBCUtilities;
-import er.extensions.migration.ERXModelVersion;
-import er.extensions.migration.IERXMigration;
+import er.extensions.migration.ERXMigrationDatabase;
+import er.extensions.migration.ERXMigrationTable;
+import er.extensions.migration.ERXMigrationDatabase.Migration;
 
-public class ERXTest0 implements IERXMigration {
-  public void downgrade(EOEditingContext editingContext, EOAdaptorChannel channel, EOModel model) throws Throwable {
+public class ERXTest0 extends Migration {
+  @Override
+  public void downgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+    // TODO Auto-generated method stub
+
   }
 
-  @SuppressWarnings("unchecked")
-  public NSArray<ERXModelVersion> modelDependencies() {
-    return NSArray.EmptyArray;
-  }
+  @Override
+  public void upgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+    ERXMigrationTable companyTable = database.newTableNamed("Company");
+    companyTable.newStringColumn("address1", 255, true);
+    companyTable.newStringColumn("address2", 255, true);
+    companyTable.newStringColumn("city", 255, true);
+    companyTable.newIntegerColumn("id", false);
+    companyTable.newStringColumn("name", 255, false);
+    companyTable.newStringColumn("state", 255, true);
+    companyTable.newStringColumn("zipcode", 255, true);
+    companyTable.create();
+    companyTable.setPrimaryKey("id");
 
-  public void upgrade(EOEditingContext editingContext, EOAdaptorChannel channel, EOModel model) throws Throwable {
-    ERXJDBCUtilities.createTablesForModel(channel, model);
+    ERXMigrationTable employeeTable = database.newTableNamed("Employee");
+    employeeTable.newStringColumn("address1", 255, true);
+    employeeTable.newStringColumn("address2", 255, true);
+    employeeTable.newStringColumn("city", 255, true);
+    employeeTable.newIntegerColumn("companyID", false);
+    employeeTable.newIntegerColumn("id", false);
+    employeeTable.newBooleanColumn("manager", false);
+    employeeTable.newStringColumn("name", 255, false);
+    employeeTable.newStringColumn("state", 255, true);
+    employeeTable.newStringColumn("zipcode", 255, true);
+    employeeTable.create();
+    employeeTable.setPrimaryKey("id");
+    employeeTable.addForeignKey("companyID", "Company", "id");
+
+    ERXMigrationTable roleTable = database.newTableNamed("Role");
+    roleTable.newIntegerColumn("id", false);
+    roleTable.create();
+    roleTable.setPrimaryKey("id");
+
+    ERXMigrationTable employeeRoleTable = database.newTableNamed("EmployeeRole");
+    employeeRoleTable.newIntegerColumn("employeeId", false);
+    employeeRoleTable.newIntegerColumn("roleId", false);
+    employeeRoleTable.create();
+    employeeRoleTable.setPrimaryKey("employeeId", "roleId");
+    employeeRoleTable.addForeignKey("employeeId", "Employee", "id");
+    employeeRoleTable.addForeignKey("roleId", "Role", "id");
+
+    ERXMigrationTable paycheckTable = database.newTableNamed("Paycheck");
+    paycheckTable.newBigDecimalColumn("amount", 38, 2, false);
+    paycheckTable.newBooleanColumn("cashed", false);
+    paycheckTable.newIntegerColumn("employeeID", false);
+    paycheckTable.newIntegerColumn("id", false);
+    paycheckTable.newTimestampColumn("paymentDate", false);
+    paycheckTable.create();
+    paycheckTable.setPrimaryKey("id");
+    paycheckTable.addForeignKey("employeeID", "Employee", "id");
   }
 }
