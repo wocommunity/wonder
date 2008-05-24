e02a4c31127bdd83f44fb8b5a6699ae29cb16ebc
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-24 00:38:05 +0000

    actually saving change logs
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7630 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/audittrail/ERCAuditTrail.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/audittrail/ERCAuditTrail.java
@@ -1,47 +1,50 @@
 package er.corebusinesslogic.audittrail;
 
-
-import com.webobjects.eoaccess.EOAttribute;
-import com.webobjects.eoaccess.EOProperty;
 import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOEnterpriseObject;
 import com.webobjects.eocontrol.EOGlobalID;
 import com.webobjects.eocontrol.EOKeyGlobalID;
-import com.webobjects.foundation.NSNotificationCenter;
-import com.webobjects.foundation.NSSelector;
+import com.webobjects.eocontrol.EOQualifier;
 
 import er.extensions.ERXEOControlUtilities;
 import er.extensions.ERXGenericRecord;
 import er.extensions.ERXKeyGlobalID;
-import er.extensions.ERXModelGroup;
-import er.extensions.ERXProperties;
 import er.extensions.ERXQ;
-import er.extensions.ERXSelectorUtilities;
-import er.extensions.ERXStringUtilities;
 
 /**
- * Bracket for all single audit trail actions. It serves as a "shadow" of the object in question.
+ * Bracket for all single audit trail actions. It serves as a "shadow" of the
+ * object in question.
+ * 
  * @author ak
- *
+ * 
  */
 public class ERCAuditTrail extends _ERCAuditTrail {
-    
+
     @SuppressWarnings("unused")
     static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ERCAuditTrail.class);
 
     public static final ERCAuditTrailClazz clazz = new ERCAuditTrailClazz();
 
-    public interface Delegate {
-    }
-
     public static class ERCAuditTrailClazz extends _ERCAuditTrail._ERCAuditTrailClazz {
 
         public ERCAuditTrail auditTrailForObject(EOEditingContext ec, EOEnterpriseObject eo) {
-            return auditTrailForGlobalID(ec, ec.globalIDForObject(eo));
+            EOKeyGlobalID gid = null;
+            if (eo instanceof ERXGenericRecord) {
+                gid = ((ERXGenericRecord) eo).permanentGlobalID();
+            } else {
+                throw new IllegalArgumentException("Can't handle non ERXGenericRecord");
+            }
+
+            ERCAuditTrail trail = (ERCAuditTrail) ERXEOControlUtilities.objectWithQualifier(ec, ENTITY_NAME, ERXQ.equals(Key.GID, ERXKeyGlobalID.globalIDForGID(gid)));
+            if (trail == null) {
+                trail = (ERCAuditTrail) EOQualifier.filteredArrayWithQualifier(ec.insertedObjects(),
+                        ERXQ.equals("entityName", ENTITY_NAME).and(ERXQ.equals(Key.GID + ".globalID", gid))).lastObject();
+            }
+            return trail;
         }
 
         public ERCAuditTrail auditTrailForGlobalID(EOEditingContext ec, EOGlobalID gid) {
-            return (ERCAuditTrail) ERXEOControlUtilities.objectWithQualifier(ec, ENTITY_NAME, ERXQ.equals(Key.GID, gid));
+            throw new IllegalArgumentException("Can't handle non ERXGenericRecord");
         }
 
         public ERCAuditTrail createAuditTrailForObject(EOEditingContext ec, EOEnterpriseObject eo) {
@@ -56,8 +59,13 @@ public class ERCAuditTrail extends _ERCAuditTrail {
 
     }
 
+    public ERCAuditTrail() {
+        super();
+    }
+    
     public void init(EOEditingContext ec) {
         super.init(ec);
+        setIsDeleted(false);
     }
 
     public void setObject(EOEnterpriseObject eo) {
@@ -76,5 +84,15 @@ public class ERCAuditTrail extends _ERCAuditTrail {
     public EOEnterpriseObject object() {
         EOKeyGlobalID gid = gid().globalID();
         return editingContext().faultForGlobalID(gid, editingContext());
+    }
+
+    public void createEntry(ERCAuditTrailType type, String keyPath, Object oldValue, Object newValue) {
+        ERCAuditTrailEntry entry = ERCAuditTrailEntry.clazz.createAndInsertObject(editingContext());
+        entry.setTrail(this);
+        addToEntries(entry);
+        entry.setKeyPath(keyPath);
+        entry.setType(type);
+        entry.setOldValue(oldValue);
+        entry.setNewValue(newValue);
     }
 }
--- Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/audittrail/ERCAuditTrailEntry.java
+++ Common/Frameworks/ERCoreBusinessLogic/Sources/er/corebusinesslogic/audittrail/ERCAuditTrailEntry.java
@@ -1,6 +1,15 @@
 package er.corebusinesslogic.audittrail;
 
-import com.webobjects.eocontrol.*;
+import com.webobjects.eoaccess.EOObjectNotAvailableException;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOKeyGlobalID;
+import com.webobjects.foundation.NSPropertyListSerialization;
+import com.webobjects.foundation.NSTimestamp;
+
+import er.corebusinesslogic.ERCoreBusinessLogic;
+import er.extensions.ERXGenericRecord;
+import er.extensions.ERXKeyGlobalID;
 
 public class ERCAuditTrailEntry extends _ERCAuditTrailEntry {
 
@@ -8,19 +17,43 @@ public class ERCAuditTrailEntry extends _ERCAuditTrailEntry {
     private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ERCAuditTrailEntry.class);
 
     public static final ERCAuditTrailEntryClazz clazz = new ERCAuditTrailEntryClazz();
+
     public static class ERCAuditTrailEntryClazz extends _ERCAuditTrailEntry._ERCAuditTrailEntryClazz {
         /* more clazz methods here */
     }
 
-    public interface Key extends _ERCAuditTrailEntry.Key {}
+    public interface Key extends _ERCAuditTrailEntry.Key {
+    }
 
-    /**
-     * Initializes the EO. This is called when an EO is created, not when it is 
-     * inserted into an EC.
-     */
     public void init(EOEditingContext ec) {
         super.init(ec);
+        EOEnterpriseObject user = ERCoreBusinessLogic.actor(ec);
+        if (user != null && user instanceof ERXGenericRecord) {
+            ERXKeyGlobalID gid = ERXKeyGlobalID.globalIDForGID(((ERXGenericRecord) user).permanentGlobalID());
+            setUserGlobalID(gid);
+        }
+        setCreated(new NSTimestamp());
     }
-    
-    // more EO methods here
+
+    public EOEnterpriseObject user() {
+        if (userGlobalID() == null) {
+            return null;
+        }
+        EOKeyGlobalID gid = userGlobalID().globalID();
+        EOEnterpriseObject eo = editingContext().faultForGlobalID(gid, editingContext());
+        try {
+            eo.willRead();
+            return eo;
+        } catch (EOObjectNotAvailableException e) {
+            return null;
+        }
+    }
+
+    public void setOldValue(Object value) {
+        setOldValues(NSPropertyListSerialization.stringFromPropertyList(value));
+    }
+
+    public void setNewValue(Object value) {
+        setNewValues(NSPropertyListSerialization.stringFromPropertyList(value));
+    }
 }
