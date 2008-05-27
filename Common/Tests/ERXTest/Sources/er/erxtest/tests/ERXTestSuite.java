3523f91f1be37d371e42d9f540156a24ec7904e3
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-05-27 15:56:53 +0000

    switch to use new test cases init methods

--- Common/Tests/ERXTest/Sources/er/erxtest/tests/ERXTestSuite.java
+++ Common/Tests/ERXTest/Sources/er/erxtest/tests/ERXTestSuite.java
@@ -2,8 +2,14 @@ package er.erxtest.tests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import er.erxtest.Application;
+import er.extensions.ERXExtensions;
 
 public class ERXTestSuite {
+  static {
+    ERXExtensions.initApp(Application.class, new String[0]);
+  }
+
   public static Test suite() {
     TestSuite suite = new TestSuite();
     suite.addTestSuite(ERXExpiringCacheTestCase.class);
