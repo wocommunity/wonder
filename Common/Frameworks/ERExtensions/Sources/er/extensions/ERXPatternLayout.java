bb50e9aed28e5e8de4a30160c90d0d670b23980b
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-02 19:41:29 +0000

    deprecations
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7673 8dc76679-1c23-de11-85cd-0050568e728b

--- Common/Frameworks/ERExtensions/Sources/er/extensions/ERXPatternLayout.java
+++ Common/Frameworks/ERExtensions/Sources/er/extensions/ERXPatternLayout.java
@@ -1,5 +1,10 @@
 package er.extensions;
-
+/**
+ * Do not use in property files, use er.extensions.logging.ERXPatternLayout instead.
+ * @deprecated
+ * @author ak
+ *
+ */
 public class ERXPatternLayout extends er.extensions.logging.ERXPatternLayout {
 	public ERXPatternLayout() {
 		System.err.println("You are referencing er.extensions.ERXPatternLayout but should use er.extensions.logging.ERXPatternLayout instead");
