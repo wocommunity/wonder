0bcad92adc2b14891f6dd1ed5281cf7dff726224
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-02 14:46:11 +0000

    added support for component content inside busy indicator if you set id, class, or style bindings

--- Ajax/Ajax/Sources/er/ajax/AjaxBusyIndicator.java
+++ Ajax/Ajax/Sources/er/ajax/AjaxBusyIndicator.java
@@ -16,7 +16,9 @@ import com.webobjects.appserver.WOResponse;
  * @binding busyImageFramework (optional) the framework that contains the busy image
  * @binding watchContainerID (optional) if set, the other bindings will only apply when this container ID is being
  *          updated, which provides for per-element busy controls
- *          
+ * @binding id (optional) if bound, you can provide a custom style for the generated busy image div
+ * @binding class (optional) if bound, you can provide a custom style for the generated busy image div 
+ * @binding style (optional) if bound, you can provide a custom style for the generated busy image div 
  * @author mschrag
  */
 public class AjaxBusyIndicator extends AjaxComponent {
@@ -37,8 +39,24 @@ public class AjaxBusyIndicator extends AjaxComponent {
 		addScriptResourceInHead(res, "effects.js");
 	}
 
+	public boolean customStyle() {
+		return hasBinding("id") || hasBinding("class") || hasBinding("style");
+	}
+
+	public String style() {
+		String style = (String) valueForBinding("style", "display: none");
+		return style;
+	}
+
 	public String divID() {
-		return (String) valueForBinding("divID", "busy");
+		String id = (String) valueForBinding("divID");
+		if (id == null) {
+			id = (String) valueForBinding("id");
+			if (id == null) {
+				id = "busy";
+			}
+		}
+		return id;
 	}
 
 	public String onCreate() {
