c8d79b717509f17e8e4a5be1303334ce52d8e8f3
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-12 05:11:09 +0000

    don't look. nothing to see.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7794 8dc76679-1c23-de11-85cd-0050568e728b

--- Frameworks/EOF/ERIndexing/Sources/er/indexing/eof/ERIDirectory.java
+++ Frameworks/EOF/ERIndexing/Sources/er/indexing/eof/ERIDirectory.java
@@ -7,6 +7,11 @@ import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.store.IndexOutput;
 
 import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableDictionary;
+
+import er.extensions.eof.ERXEC;
+import er.extensions.foundation.ERXMutableDictionary;
 
 public class ERIDirectory extends _ERIDirectory {
 
@@ -14,168 +19,113 @@ public class ERIDirectory extends _ERIDirectory {
     private static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ERIDirectory.class);
 
     public static final ERIDirectoryClazz clazz = new ERIDirectoryClazz();
-    
-    public static class EOFIndexOutput extends IndexOutput {
+   
+    private class EOFDirectory extends Directory {
 
         @Override
         public void close() throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void flush() throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public long getFilePointer() {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public long length() throws IOException {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public void seek(long l) throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void writeByte(byte byte0) throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void writeBytes(byte[] abyte0, int i, int j) throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-    }
-    
-    public static class EOFIndexInput extends IndexInput {
-
-        @Override
-        public void close() throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public long getFilePointer() {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public long length() {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public byte readByte() throws IOException {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public void readBytes(byte[] abyte0, int i, int j) throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-
-        @Override
-        public void seek(long l) throws IOException {
-            // TODO Auto-generated method stub
-            
-        }
-    }
-    
-    public static class EOFDirectory extends Directory {
-
-        @Override
-        public void close() throws IOException {
-            // TODO Auto-generated method stub
-            
+            editingContext().saveChanges();
         }
 
         @Override
         public IndexOutput createOutput(String s) throws IOException {
-            // TODO Auto-generated method stub
-            return null;
+            return fileForName(s).createOutput();
         }
 
         @Override
         public void deleteFile(String s) throws IOException {
-            // TODO Auto-generated method stub
-            
+            ERIFile file = fileForName(s);
+            if(file != null) {
+                file.delete();
+                editingContext().saveChanges();
+            }
         }
 
         @Override
         public boolean fileExists(String s) throws IOException {
-            // TODO Auto-generated method stub
-            return false;
+            return fileForName(s) != null;
         }
 
         @Override
         public long fileLength(String s) throws IOException {
-            // TODO Auto-generated method stub
-            return 0;
+            return fileForName(s).length();
         }
 
         @Override
         public long fileModified(String s) throws IOException {
-            // TODO Auto-generated method stub
-            return 0;
+            return fileForName(s).timestamp();
         }
 
         @Override
         public String[] list() throws IOException {
-            // TODO Auto-generated method stub
-            return null;
+            return ((NSArray<String>)files().valueForKeyPath(ERIFile.Key.NAME)).objects();
         }
 
         @Override
         public IndexInput openInput(String s) throws IOException {
-            // TODO Auto-generated method stub
-            return null;
+            return fileForName(s).openInput();
         }
 
         @Override
         public void renameFile(String s, String s1) throws IOException {
-            // TODO Auto-generated method stub
-            
+            fileForName(s).setName(s1);
+            editingContext().saveChanges();
         }
 
         @Override
         public void touchFile(String s) throws IOException {
-            // TODO Auto-generated method stub
-            
+            fileForName(s).touch();
+            editingContext().saveChanges();
         }
         
     }
     
     public static class ERIDirectoryClazz extends _ERIDirectory._ERIDirectoryClazz {
-        /* more clazz methods here */
+
+        private NSMutableDictionary<String,ERIDirectory> directories = ERXMutableDictionary.synchronizedDictionary();
+        
+        public Directory directoryForName(String store) {
+            ERIDirectory result = directories.objectForKey(store);
+            if(result == null) {
+                ERXEC ec = (ERXEC) ERXEC.newEditingContext();
+
+                ec.setUseAutoLock(true);
+                ERIDirectory directory = objectMatchingKeyAndValue(ec, Key.NAME, store);
+                if(directory == null) {
+                    directory = createAndInsertObject(ec);
+                    directory.setName(store);
+                    ec.saveChanges();
+                }
+                result = directory;
+                directories.setObjectForKey(directory, store);
+            }
+            return result.directory();
+        }
+
     }
 
     public interface Key extends _ERIDirectory.Key {}
 
-    /**
-     * Initializes the EO. This is called when an EO is created, not when it is 
-     * inserted into an EC.
-     */
     public void init(EOEditingContext ec) {
         super.init(ec);
     }
+
+    public ERIFile fileForName(String name) {
+        for (ERIFile file : files()) {
+            if(file.name().equals(name)) {
+                return file;
+            }
+        }
+        return null;
+    }
+
+    private Directory _directory;
     
-    // more EO methods here
+    public Directory directory() {
+
+        if(_directory == null) {
+            _directory = new EOFDirectory();
+        }
+        return _directory;
+    }
 }
