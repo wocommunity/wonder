f5a94032e344c9c99bc41c4abb613119781af59c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-18 12:23:54 +0000

    temp commit, dont wanna lose this

---
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/SCUtilities.java
@@ -0,0 +1,13 @@
+package er.sproutcore;
+
+import java.util.LinkedHashMap;
+import java.util.Stack;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.appserver.ERXResponse;
+import er.extensions.appserver.ERXWOContext;
+import er.extensions.foundation.ERXThreadStorage;
+
+public class SCUtilities {
+}
