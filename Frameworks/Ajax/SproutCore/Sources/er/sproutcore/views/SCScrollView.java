4d3097fb6cc716da2a4ac1cc1c2e3e1e60b7541f
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-19 16:35:56 +0000

    more views added, photo example set up first cut

--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCScrollView.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCScrollView.java
@@ -4,9 +4,9 @@ import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
 import com.webobjects.foundation.NSDictionary;
 
-public class SCButton extends SCView {
+public class SCScrollView extends SCView {
 
-    public SCButton(String arg0, NSDictionary arg1, WOElement arg2) {
+    public SCScrollView(String arg0, NSDictionary arg1, WOElement arg2) {
         super(arg0, arg1, arg2);
     }
 }
---
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCSliderView.java
@@ -0,0 +1,12 @@
+package er.sproutcore.views;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOElement;
+import com.webobjects.foundation.NSDictionary;
+
+public class SCSliderView extends SCView {
+
+    public SCSliderView(String arg0, NSDictionary arg1, WOElement arg2) {
+        super(arg0, arg1, arg2);
+    }
+}
