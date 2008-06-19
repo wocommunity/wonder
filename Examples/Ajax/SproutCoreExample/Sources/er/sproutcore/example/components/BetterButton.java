8a603d841440cbae44d7cee216dd193c390e3f18
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-19 14:25:33 +0000

    components should now work too (well, in the furture that is)

---
+++ Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/BetterButton.java
@@ -0,0 +1,12 @@
+package er.sproutcore.example.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.sproutcore.views.SCComponent;
+
+public class BetterButton extends SCComponent {
+    
+    public BetterButton(WOContext context) {
+        super(context);
+    }
+}
---
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCComponent.java
@@ -0,0 +1,39 @@
+package er.sproutcore.views;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.foundation.NSArray;
+
+import er.extensions.components.ERXNonSynchronizingComponent;
+import er.sproutcore.views.SCView.Item;
+
+public class SCComponent extends ERXNonSynchronizingComponent {
+
+    public SCComponent(WOContext arg0) {
+        super(arg0);
+    }
+
+    @Override
+    public final void appendToResponse(WOResponse response, WOContext context) {
+        Item item = SCView.pushItem(stringValueForBinding("id"), className());
+        for (String key : ((NSArray<String>) bindingKeys())) {
+            Object value = valueForBinding(key);
+            value = value == null ? "null" : value;
+            if (key.startsWith("?")) {
+                item.addBinding(key.substring(1), value);
+            } else {
+                item.addProperty(key, value);
+            }
+        }
+        doAppendToResponse(response, context);
+        SCView.popItem();
+    }
+
+    protected void doAppendToResponse(WOResponse response, WOContext context) {
+        super.appendToResponse(response, context);
+    }
+
+    protected String className() {
+        return name();
+    }
+}
