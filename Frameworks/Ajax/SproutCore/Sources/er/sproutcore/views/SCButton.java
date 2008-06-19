ba09b83ef2f85d5750799bfd88fa5e2a414f84f8
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-19 15:24:27 +0000

    readability

--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCButton.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCButton.java
@@ -1,5 +1,6 @@
 package er.sproutcore.views;
 
+import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
 import com.webobjects.foundation.NSDictionary;
 
@@ -8,5 +9,4 @@ public class SCButton extends SCView {
     public SCButton(String arg0, NSDictionary arg1, WOElement arg2) {
         super(arg0, arg1, arg2);
     }
-
 }
