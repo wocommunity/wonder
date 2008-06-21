597405edb5a78a6ecf3e0b676d9fe72389f224c5
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-21 12:05:54 +0000

    rest of the views added

--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCSourceListView.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCSourceListView.java
@@ -2,11 +2,18 @@ package er.sproutcore.views;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSDictionary;
 
-public class SCSourceListView extends SCView {
+public class SCSourceListView extends SCCollectionView {
 
     public SCSourceListView(String arg0, NSDictionary arg1, WOElement arg2) {
         super(arg0, arg1, arg2);
+    }
+
+    @Override
+    protected void doAppendToResponse(WOResponse response, WOContext context) {
+        super.doAppendToResponse(response, context);
     }
 }
