136cae5ba07e8dad6072b79cdbd689bb10691ee6
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-21 16:23:39 +0000

    moved to packages

--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/collection/SCCollectionView.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/collection/SCCollectionView.java
@@ -1,4 +1,4 @@
-package er.sproutcore.views;
+package er.sproutcore.views.collection;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
@@ -6,9 +6,11 @@ import com.webobjects.appserver.WOResponse;
 import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSDictionary;
 
-public class SCGridView extends SCCollectionView {
+import er.sproutcore.views.SCView;
 
-    public SCGridView(String arg0, NSDictionary arg1, WOElement arg2) {
+public class SCCollectionView extends SCView {
+
+    public SCCollectionView(String arg0, NSDictionary arg1, WOElement arg2) {
         super(arg0, arg1, arg2);
     }
 
