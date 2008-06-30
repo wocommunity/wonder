f9d4e0e6344e961cc83cca9f1e77d3deb99da89a
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-06-30 22:53:52 +0000

    rudimentary request hanlder

---
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/SCRequestHandler.java
@@ -0,0 +1,15 @@
+package er.sproutcore;
+
+import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WORequestHandler;
+import com.webobjects.appserver.WOResponse;
+
+public class SCRequestHandler extends WORequestHandler {
+
+    @Override
+    public WOResponse handleRequest(WORequest arg0) {
+        WOResponse result = new WOResponse();
+        return result;
+    }
+
+}
