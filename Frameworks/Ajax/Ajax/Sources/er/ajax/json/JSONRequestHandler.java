3911ede3a70f91f85e0da743ef45b207299b73e7
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-09 18:58:48 +0000

    added stateful JSON endpoints (still experimental)

--- Frameworks/Ajax/Ajax/Sources/er/ajax/json/JSONRequestHandler.java
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/json/JSONRequestHandler.java
@@ -1,5 +1,7 @@
 package er.ajax.json;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.jabsorb.JSONRPCBridge;
@@ -14,44 +16,165 @@ import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WORequestHandler;
 import com.webobjects.appserver.WOResponse;
 import com.webobjects.appserver.WOSession;
+import com.webobjects.foundation._NSUtilities;
 
+import er.extensions.components.ERXDynamicURL;
 import er.extensions.foundation.ERXMutableURL;
+import er.extensions.foundation.ERXProperties;
 
+/**
+ * JSONRequestHandler provides support for JSON RPC services that can be both 
+ * stateless or stateful (using JSON Components). 
+ *   
+ * @author mschrag
+ * @property er.ajax.json.globalBacktrackCacheSize the maximum number of global components that can be in the session (defaults to backtrack cache size) 
+ * @property er.ajax.json.backtrackCacheSize the maximum number of non-global components that can be in the session  (defaults to backtrack cache size)
+ */
 public class JSONRequestHandler extends WORequestHandler {
 	public static final String RequestHandlerKey = "json";
 
-	private JSONRPCBridge _jsonBridge;
+	private JSONRPCBridge _sharedBridge;
 
+	/**
+	 * Registers the JSONRequestHandler with your application using the default key.
+	 * 
+	 * @return the request handler instance 
+	 */
 	public static JSONRequestHandler register() {
 		JSONRequestHandler requestHandler = new JSONRequestHandler();
 		WOApplication.application().registerRequestHandler(requestHandler, JSONRequestHandler.RequestHandlerKey);
 		return requestHandler;
 	}
 
+	/**
+	 * Creates a new JSONRequestHandler.
+	 */
 	public JSONRequestHandler() {
-		_jsonBridge = JSONBridge.createBridge();
+		_sharedBridge = JSONBridge.createBridge();
 	}
 
+	/**
+	 * Returns the shared JSON Bridge for this request handler.
+	 * 
+	 * @return the shared JSON Bridge for this request handler
+	 */
 	public JSONRPCBridge getJSONBridge() {
-		return _jsonBridge;
+		return _sharedBridge;
 	}
 
-	public void registerSerializer(Serializer serializer) throws Exception {
+	/**
+	 * Registers a custom serializer into the global JSON serializers (see JSONRPCBridge).
+	 *  
+	 * @param serializer the serializer to register
+	 * @throws Exception if the registration fails
+	 */
+	public static void registerSerializer(Serializer serializer) throws Exception {
 		JSONRPCBridge.getSerializer().registerSerializer(serializer);
 	}
 
+	/**
+	 * Registers all of the methods of the given class to be available for services to call (see JSONRPCBridge).
+	 * 
+	 * @param clazz the class to register
+	 * @throws Exception if the registration fails
+	 */
 	public static void registerClass(Class clazz) throws Exception {
 		JSONRequestHandler.registerClass(clazz.getSimpleName(), clazz);
 	}
 
+	/**
+	 * Registers all of the methods of the given class to be available for services to call (see JSONRPCBridge).
+	 *
+	 * @param name the namespace to register the methods under
+	 * @param clazz the class to register
+	 * @throws Exception if the registration fails
+	 */
 	public static void registerClass(String name, Class clazz) throws Exception {
 		JSONRPCBridge.getGlobalBridge().registerClass(name, clazz);
 	}
 
+	/**
+	 * Registers the given object in the shared JSON bridge.  The shared JSON
+	 * bridge is used for stateless JSON services.  As an example, if you call
+	 * registerService("myExampleService", new ExampleService()) you can then 
+	 * call json.myExampleService.someMethodInExampleService from your Javascript.
+	 * The same instance is shared across all of your service users, so you should
+	 * not store any state in this class.
+	 * 
+	 * @param name the name to register the object as 
+	 * @param serviceObject the instance to register
+	 */
 	public void registerService(String name, Object serviceObject) {
-		_jsonBridge.registerObject(name, serviceObject);
+		_sharedBridge.registerObject(name, serviceObject);
 	}
 
+	/**
+	 * Returns a URL pointing to the JSON request handler.  This variant
+	 * should be used for the shared web service endpoint.
+	 * 
+	 * @param context the current WOContext
+	 * @param queryString the query string to append
+	 * @return a JSON request handler URL
+	 */
+	public static String jsonUrl(WOContext context, String queryString) {
+		return JSONRequestHandler.jsonUrl(context, JSONRequestHandler.RequestHandlerKey, null, null, queryString);
+	}
+
+	/**
+	 * Returns a URL pointing to the JSON request handler.  This variant
+	 * should be used for the shared web service endpoint.
+	 * 
+	 * @param context the current WOContext
+	 * @param requestHandlerKey if you registered a custom JSON request handler key
+	 * @param queryString the query string to append
+	 * @return a JSON request handler URL
+	 */
+	public static String jsonUrl(WOContext context, String requestHandlerKey, String queryString) {
+		return JSONRequestHandler.jsonUrl(context, requestHandlerKey, null, null, queryString);
+	}
+
+	/**
+	 * Returns a URL pointing to the JSON request handler for a JSON component.
+	 * 
+	 * @param context the current WOContext
+	 * @param componentName the name of the component to lookup
+	 * @param instance the instance identifier (any value) to create a unique instance (or null for a session-global)
+	 * @param queryString the query string to append
+	 * @return a JSON request handler URL
+	 */
+	public static String jsonUrl(WOContext context, String componentName, String instance, String queryString) {
+		return JSONRequestHandler.jsonUrl(context, JSONRequestHandler.RequestHandlerKey, componentName, instance, queryString);
+	}
+
+	/**
+	 * Returns a URL pointing to the JSON request handler.
+	 * 
+	 * @param context the current WOContext
+	 * @param requestHandlerKey if you registered a custom JSON request handler key
+	 * @param componentName the name of the component to lookup (or null for the shared bridge)
+	 * @param instance the instance identifier (any value) to create a unique instance (or null for a session-global)
+	 * @param queryString the query string to append
+	 * @return a JSON request handler URL
+	 */
+	public static String jsonUrl(WOContext context, String requestHandlerKey, String componentName, String instance, String queryString) {
+		String requestHandlerPath;
+		if (componentName == null) {
+			requestHandlerPath = "";
+		}
+		else {
+			if (instance == null) {
+				requestHandlerPath = componentName;
+			}
+			else {
+				requestHandlerPath = componentName + "/" + instance;
+			}
+		}
+
+		String jsonUrl = context.urlWithRequestHandlerKey(JSONRequestHandler.RequestHandlerKey, requestHandlerPath, queryString);
+		return jsonUrl;
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public WOResponse handleRequest(WORequest request) {
 		WOApplication application = WOApplication.application();
@@ -82,7 +205,57 @@ public class JSONRequestHandler extends WORequestHandler {
 					session.awake();
 				}
 				try {
-					output = _jsonBridge.call(new Object[] { request, response, context }, input);
+					ERXDynamicURL url = new ERXDynamicURL(request._uriDecomposed());
+					String requestHandlerPath = url.requestHandlerPath();
+					JSONRPCBridge jsonBridge;
+					if (requestHandlerPath != null && requestHandlerPath.length() > 0) {
+						String componentNameAndInstance = requestHandlerPath;
+						String componentName;
+						String componentInstance;
+						int slashIndex = componentNameAndInstance.indexOf('/');
+						if (slashIndex == -1) {
+							componentName = componentNameAndInstance;
+							componentInstance = null;
+						}
+						else {
+							componentName = componentNameAndInstance.substring(0, slashIndex);
+							componentInstance = componentNameAndInstance.substring(slashIndex + 1);
+						}
+
+						if (session == null) {
+							session = context.session();
+						}
+
+						String bridgesKey = (componentInstance == null) ? "_JSONGlobalBridges" : "_JSONInstanceBridges";
+						Map<String, JSONRPCBridge> componentBridges = (Map<String, JSONRPCBridge>) session.objectForKey(bridgesKey);
+						if (componentBridges == null) {
+							int limit = ERXProperties.intForKeyWithDefault((componentInstance == null) ? "er.ajax.json.globalBacktrackCacheSize" : "er.ajax.json.backtrackCacheSize", WOApplication.application().pageCacheSize());
+							componentBridges = new LRUMap<String, JSONRPCBridge>(limit);
+							session.setObjectForKey(componentBridges, bridgesKey);
+						}
+						jsonBridge = componentBridges.get(componentNameAndInstance);
+						if (jsonBridge == null) {
+							Class componentClass = _NSUtilities.classWithName(componentName);
+							JSONComponent component;
+							if (JSONComponent.class.isAssignableFrom(componentClass)) {
+								component = (JSONComponent) _NSUtilities.instantiateObject(componentClass, new Class[] { WOContext.class }, new Object[] { context }, true, false);
+							}
+							else {
+								throw new SecurityException("There is no JSON component named '" + componentName + "'.");
+							}
+							jsonBridge = JSONBridge.createBridge();
+							jsonBridge.registerObject("component", component);
+							componentBridges.put(componentNameAndInstance, jsonBridge);
+						}
+						JSONComponent component = (JSONComponent) jsonBridge.getBridgeState().getObjectMap().get("component");
+						component.checkAccess();
+						component._setContext(context);
+					}
+					else {
+						jsonBridge = _sharedBridge;
+					}
+
+					output = jsonBridge.call(new Object[] { request, response, context }, input);
 					if (context._session() != null) {
 						WOSession contextSession = context._session();
 						// If this is a new session, then we have to force it to be a cookie session
@@ -139,6 +312,20 @@ public class JSONRequestHandler extends WORequestHandler {
 		}
 		finally {
 			application.sleep();
+		}
+	}
+
+	protected static class LRUMap<U, V> extends LinkedHashMap<U, V> {
+		private int _maxSize;
+
+		public LRUMap(int maxSize) {
+			super(16, 0.75f, true);
+			_maxSize = maxSize;
+		}
+
+		@Override
+		protected boolean removeEldestEntry(Map.Entry<U, V> eldest) {
+			return size() > _maxSize;
 		}
 	}
 }
--- Frameworks/Ajax/Ajax/Sources/er/ajax/json/client/AjaxJSONClient.java
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/json/client/AjaxJSONClient.java
@@ -1,16 +1,12 @@
 package er.ajax.json.client;
 
-import com.webobjects.appserver.WOActionResults;
-import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
-import com.webobjects.appserver.WOElement;
-import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
-import com.webobjects.foundation.NSDictionary;
 
-import er.ajax.AjaxDynamicElement;
 import er.ajax.AjaxUtils;
 import er.ajax.json.JSONRequestHandler;
+import er.extensions.components.ERXComponent;
+import er.extensions.foundation.ERXRandomGUID;
 
 /**
  * AjaxJSONClient renders a "new JSONRpcClient('...')" with a URL back to your application (along with a session ID if
@@ -22,21 +18,63 @@ import er.ajax.json.JSONRequestHandler;
  * 
  * @author mschrag
  * @binding callback the initialization callback
+ * @binding componentName the JSON component to use
+ * @binding global if true, a single component instance will be shared for the session (defaults false)
+ * @binding instance if global is false, you can set a specific instance identifier (leave out for a generated value)
  */
-public class AjaxJSONClient extends AjaxDynamicElement {
-	public AjaxJSONClient(String name, NSDictionary associations, WOElement element) {
-		super(name, associations, element);
+public class AjaxJSONClient extends ERXComponent {
+	private String _instance;
+
+	public AjaxJSONClient(WOContext context) {
+		super(context);
 	}
 
+	@Override
+	public boolean synchronizesVariablesWithBindings() {
+		return false;
+	}
+
+	public boolean global() {
+		return booleanValueForBinding("global", false);
+	}
+
+	public String jsonComponent() {
+		return stringValueForBinding("component");
+	}
+
+	public String jsonInstance() {
+		String instance = stringValueForBinding("instance");
+		if (instance == null) {
+			if (_instance == null && !global()) {
+				_instance = ERXRandomGUID.newGid();
+				instance = _instance;
+			}
+			instance = _instance;
+		}
+		return instance;
+	}
+
+	@Override
 	public void appendToResponse(WOResponse woresponse, WOContext wocontext) {
+		AjaxUtils.addScriptResourceInHead(wocontext, woresponse, "jsonrpc.js");
+
 		String queryString = null;
 		if (wocontext.request().sessionID() != null && wocontext.session().storesIDsInURLs()) {
 			queryString = "wosid=" + wocontext.request().sessionID();
 		}
-		String jsonUrl = wocontext.urlWithRequestHandlerKey(JSONRequestHandler.RequestHandlerKey, "", queryString);
+
+		String componentName = jsonComponent();
+		String instance;
+		if (componentName == null) {
+			instance = null;
+		}
+		else {
+			instance = jsonInstance();
+		}
+
+		String jsonUrl = JSONRequestHandler.jsonUrl(wocontext, componentName, instance, queryString);
 		woresponse.appendContentString("new JSONRpcClient(");
-		WOComponent component = wocontext.component();
-		String callback = (String) valueForBinding("callback", component);
+		String callback = stringValueForBinding("callback");
 		if (callback != null) {
 			woresponse.appendContentString(callback);
 			woresponse.appendContentString(",");
@@ -44,13 +82,5 @@ public class AjaxJSONClient extends AjaxDynamicElement {
 		woresponse.appendContentString("'");
 		woresponse.appendContentString(jsonUrl);
 		woresponse.appendContentString("')");
-	}
-
-	protected void addRequiredWebResources(WOResponse response, WOContext context) {
-		AjaxUtils.addScriptResourceInHead(context, response, "jsonrpc.js");
-	}
-
-	public WOActionResults handleRequest(WORequest request, WOContext context) {
-		return null;
 	}
 }
