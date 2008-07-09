352633f6a508460d002930a8a86df7745ee0e6bb
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-09 18:58:48 +0000

    added stateful JSON endpoints (still experimental)
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7910 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/json/JSONComponent.java
@@ -0,0 +1,57 @@
+package er.ajax.json;
+
+import com.webobjects.appserver.WOContext;
+
+/**
+ * The base class for JSON "Components".  JSONComponents provide a framework
+ * for implementing stateful JSON services.  If you are providing a JSON
+ * service that is only used on a page, you can use AjaxProxy to provide
+ * a stateful JSON interface to a page.  However, if you would like to
+ * provide stateful services to non-page-based clients, AjaxProxy isn't
+ * sufficient.  By extending JSONComponent, all of your public methods
+ * are exposed as RPC calls to your clients that can be accessed by 
+ * constructing a URL to a JSON Request Handler.  For instance, 
+ * /json/MyComponentName will give you a stateful service endpoint     
+ * 
+ * @author mschrag
+ */
+public class JSONComponent {
+	private WOContext _context;
+
+	/**
+	 * Constructs a new JSONComponent.
+	 * 
+	 * @param context the context that created the component
+	 */
+	public JSONComponent(WOContext context) {
+		_context = context;
+	}
+
+	/**
+	 * Returns the current context.
+	 * 
+	 * @return the current context.
+	 */
+	protected WOContext context() {
+		return _context;
+	}
+	
+	/**
+	 * Sets the current context.
+	 * 
+	 * @param context the current context.
+	 */
+	protected void _setContext(WOContext context) {
+		_context = context;
+	}
+	
+	/**
+	 * Called prior to issuing any calls to
+	 * the component.  Throw a SecurityException
+	 * if the Session user's credentials are
+	 * insufficient. 
+	 */
+	protected void checkAccess() {
+		// override to provide an implementation
+	}
+}
