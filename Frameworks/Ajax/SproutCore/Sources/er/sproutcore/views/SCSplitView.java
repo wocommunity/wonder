2edfa196d087072cd481a292f6718b6f90e62f12
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-11 21:04:29 +0000

    hello fixed, isEnabled->enabled again

--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCSplitView.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/SCSplitView.java
@@ -1,5 +1,6 @@
 package er.sproutcore.views;
 
+import com.webobjects.appserver.WOAssociation;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
 import com.webobjects.appserver.WOResponse;
@@ -9,6 +10,7 @@ public class SCSplitView extends SCView {
 
     public SCSplitView(String arg0, NSDictionary arg1, WOElement arg2) {
         super(arg0, arg1, arg2);
+        moveProperty("direction", "layoutDirection");
     }
 
     @Override
