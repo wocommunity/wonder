0fa2cf367cfc6a187f670ea5b99d55c6e2490560
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-11 13:19:51 +0000

    contacts added

---
+++ Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Contacts.java
@@ -0,0 +1,10 @@
+package er.sproutcore.example.components;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+
+public class Contacts extends WOComponent {
+    public Contacts(WOContext context) {
+        super(context);
+    }
+}
