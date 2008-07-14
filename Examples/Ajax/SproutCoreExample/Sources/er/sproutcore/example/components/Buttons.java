4cd8f97b79a98950da62c29a1308b8b74eabc5b7
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-14 20:30:16 +0000

    fixed warnings
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7971 8dc76679-1c23-de11-85cd-0050568e728b

--- Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Buttons.java
+++ Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Buttons.java
@@ -4,21 +4,20 @@ import com.webobjects.appserver.WOContext;
 import com.webobjects.foundation.NSArray;
 
 import er.extensions.components.ERXComponent;
-import er.sproutcore.views.SCComponent;
 
 public class Buttons extends ERXComponent {
-    
-    public String segment;
-    
-    public Buttons(WOContext context) {
-        super(context);
-    }
-    
-    public NSArray segments() {
-        return new NSArray(new Object[]{"one", "two", "three"});
-    }
 
-    public NSArray values() {
-        return new NSArray(new Object[]{ });
-    }
+	public String _segment;
+
+	public Buttons(WOContext context) {
+		super(context);
+	}
+
+	public NSArray<String> segments() {
+		return new NSArray<String>(new String[] { "one", "two", "three" });
+	}
+
+	public NSArray<Object> values() {
+		return new NSArray<Object>(new Object[] {});
+	}
 }
