c2709305b2653d24248e77aa5422469200a01ab4
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-15 20:52:41 +0000

    photos now show
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8022 8dc76679-1c23-de11-85cd-0050568e728b

--- Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/photos/Picker.java
+++ Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/photos/Picker.java
@@ -7,5 +7,6 @@ import er.sproutcore.views.SCComponent;
 public class Picker extends SCComponent {
     public Picker(WOContext context) {
         super(context);
+        setClassName("SC.View");
     }
 }
