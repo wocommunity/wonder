2f18df661c01e68b17e85cef1f520791db439ee2
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-15 11:21:01 +0000

    more sensible start page with DAs
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@7994 8dc76679-1c23-de11-85cd-0050568e728b

--- Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Main.java
+++ Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Main.java
@@ -1,11 +1,8 @@
 package er.sproutcore.example.components;
 
-import org.apache.log4j.Logger;
-
 import com.webobjects.appserver.WOContext;
 
 import er.extensions.components.ERXComponent;
-import er.sproutcore.SCUtilities;
 
 public class Main extends ERXComponent {
 
