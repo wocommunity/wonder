2c02c276c5cfedcccadef40937867c3f04bf0962
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-15 12:21:17 +0000

    added tab view

--- Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Buttons.java
+++ Examples/Ajax/SproutCoreExample/Sources/er/sproutcore/example/components/Buttons.java
@@ -3,9 +3,9 @@ package er.sproutcore.example.components;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.foundation.NSArray;
 
-import er.extensions.components.ERXComponent;
+import er.sproutcore.views.SCComponent;
 
-public class Buttons extends ERXComponent {
+public class Buttons extends SCComponent {
 
 	public String _segment;
 
