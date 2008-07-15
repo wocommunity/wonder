0aab2fb814cd5ab1ef7d9e59b603c812a2ffb814
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-15 01:07:32 +0000

    work on checkbox field views

--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/button/SCRadioView.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/button/SCRadioView.java
@@ -7,34 +7,24 @@ import com.webobjects.foundation.NSDictionary;
 
 public class SCRadioView extends SCButtonView {
 
-    public SCRadioView(String arg0, NSDictionary arg1, WOElement arg2) {
-        super(arg0, arg1, arg2);
-    }
+	public SCRadioView(String arg0, NSDictionary arg1, WOElement arg2) {
+		super(arg0, arg1, arg2);
+	}
 
-    @Override
-    public String cssName(WOContext context) {
-        return "sc-button-view ";
-    }
+	@Override
+	public String cssName(WOContext context) {
+		return "sc-button-view ";
+	}
 
-    @Override
+	@Override
 	public String defaultTheme(WOContext context) {
-    	return "radio";
-    }
-    
-    @Override
-    protected void doAppendToResponse(WOResponse response, WOContext context) {
-        String url = blankUrl();
-        response.appendContentString("<img class=\"button\" src=\"" + url + "\">" );
-    	
-        String label = label(context);
-        if (label != null) {
-            response.appendContentString("<span class=\"label\">" );
-        }
-        
-        super.doAppendToResponse(response, context);
-        
-        if (label != null) {
-            response.appendContentString("</span>");
-        }
-    }
+		return "radio";
+	}
+
+	@Override
+	protected void doAppendToResponse(WOResponse response, WOContext context) {
+		String url = blankUrl();
+		response.appendContentString("<img class=\"button\" src=\"" + url + "\">");
+		super.doAppendToResponse(response, context);
+	}
 }
--- Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/field/SCCheckboxFieldView.java
+++ Frameworks/Ajax/SproutCore/Sources/er/sproutcore/views/field/SCCheckboxFieldView.java
@@ -7,17 +7,29 @@ import com.webobjects.foundation.NSDictionary;
 
 public class SCCheckboxFieldView extends SCFieldView {
 
-    public SCCheckboxFieldView(String arg0, NSDictionary arg1, WOElement arg2) {
-        super(arg0, arg1, arg2);
-    }
+	public SCCheckboxFieldView(String arg0, NSDictionary arg1, WOElement arg2) {
+		super(arg0, arg1, arg2);
+	}
+	
+	@Override
+	public String cssName(WOContext context) {
+		return null;
+	}
 
-    @Override
-    public String type() {
-        return "checkbox";
-    }
+	@Override
+	public String css(WOContext context) {
+		String css = super.css(context);
+		css += (booleanValueForBinding("enabled", true, context.component()) ? "" : " disabled");
+		return css;
+	}
 
-    @Override
-    protected void doAppendToResponse(WOResponse response, WOContext context) {
-        super.doAppendToResponse(response, context);
-    }
+	@Override
+	public String type() {
+		return "checkbox";
+	}
+
+	@Override
+	protected void doAppendToResponse(WOResponse response, WOContext context) {
+		super.doAppendToResponse(response, context);
+	}
 }
