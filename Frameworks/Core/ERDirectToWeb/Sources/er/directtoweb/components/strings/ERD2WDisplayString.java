9d0d939ec33dcb52c66f2a8bad67877dc23fe565
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-07-31 12:06:25 +0000

    now uses eo.userPresentableDescription() which should make more sense than a simply toString that is used by default.
    I am prepared to roll that back if someone convinces me why this is a bad idea
    
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8108 8dc76679-1c23-de11-85cd-0050568e728b

--- Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/strings/ERD2WDisplayString.java
+++ Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/strings/ERD2WDisplayString.java
@@ -8,15 +8,26 @@ package er.directtoweb.components.strings;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.directtoweb.D2WDisplayString;
+import com.webobjects.eocontrol.EOEnterpriseObject;
 
 /**
- * Full blown dislay string with all the bells and whistles.<br />
- * 
+ * Full blown display string with all the bells and whistles.<br />
+ * Of the value displayed is an EO, uses the userPresentableDescription()
  */
-
+//FIXME AK: should actually use keyWhenRelationship
 public class ERD2WDisplayString extends D2WDisplayString {
 
     public ERD2WDisplayString(WOContext context) {
         super(context);
+    }
+    
+    @Override
+    public Object objectPropertyValue() {
+    	Object object = super.objectPropertyValue();
+    	if (object instanceof EOEnterpriseObject) {
+			EOEnterpriseObject eo = (EOEnterpriseObject) object;
+			return eo.userPresentableDescription();
+		}
+    	return super.objectPropertyValue();
     }
 }
