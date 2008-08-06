a17d4a060f9c74880c2a321641da084cf3718328
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2008-08-06 20:23:45 +0000

    JD, simpler API (still not ready)

--- Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXTemporaryGlobalID.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXTemporaryGlobalID.java
@@ -4,99 +4,105 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import com.webobjects.appserver.WOApplication;
-import com.webobjects.eocontrol.EOTemporaryGlobalID;
+import com.webobjects.eocontrol.EOGlobalID;
+import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSForwardException;
 
 /**
- * Experimental class to have quasi-GUIDs that fit into a long. Use as an
- * alternative to the ERXLongPrimaryKeyFactory or the ultra-long built-in keys.
- * 
+ * Experimental class to have quasi-GUIDs that fit into a long. Used as an
+ * alternative to the ERXLongPrimaryKeyFactory or the 24 byte built-in keys.
+ * To have it installed, set your pk prototype to "longId".
  * @author ak
  * 
  */
-public class ERXTemporaryGlobalID extends EOTemporaryGlobalID {
+public class ERXTemporaryGlobalID extends EOGlobalID {
 
 	private static int _cnt;
 	private static int _lastTime;
 
+	private static int _identifier;
+	
 	private long _value;
-
-	/**
-	 * The delegate returns an identifier for the current app instance.
-	 * 
-	 * @author ak
-	 * 
-	 */
-	public static interface Delegate {
-		public int identifier();
-	}
-
-	private static Delegate _delegate;
-
+   
 	public ERXTemporaryGlobalID() {
 		synchronized (ERXTemporaryGlobalID.class) {
+			if(_identifier == 0) {
+				setDefaultAppIdentifier();
+			}
 			_value = 0L;
 			int current = (int) System.currentTimeMillis();
 			if (_lastTime < current) {
 				_cnt = 0;
 				_lastTime = current;
 			}
-			_value |= delegate().identifier() << 40;
-			_value |= _cnt << 32;
-			_value |= _lastTime;
-			if (++_cnt == Short.MAX_VALUE) {
+			_value |= ((identifier() << 40) & 0xffffff0000000000L);
+			_value |= ((_cnt << 32)         & 0x000000ff00000000L);
+			_value |= ((_lastTime )         & 0x00000000ffffffffL);
+			if (++_cnt == Byte.MAX_VALUE) {
 				_lastTime++;
 				_cnt = 0;
 			}
 		}
 	}
 
+	private static void setDefaultAppIdentifier() {
+		int result = hostIdentifier();
+		result |= appIdentifier();
+		_identifier = result;
+	}
+
+	private static int hostIdentifier() {
+		try {
+			byte[] address = InetAddress.getLocalHost().getAddress();
+			int result = address[3] << 16;
+			result |= address[4] << 8;
+			return result;
+		}
+		catch (UnknownHostException e) {
+			throw NSForwardException._runtimeExceptionForThrowable(e);
+		}
+	}
+
+	private static int appIdentifier() {
+		return WOApplication.application().port().intValue() & 0xff;
+	}
+
+	public static void setIdentifier(int value) {
+		_identifier = value;
+	}
+
+	private int identifier() {
+		return _identifier;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ERXTemporaryGlobalID) {
+			ERXTemporaryGlobalID gid = (ERXTemporaryGlobalID) obj;
+			return gid._value == _value;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean isTemporary() {
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return (int)_value;
+	}
+	
+	public long value() {
+		return _value;
+	}
+	
 	/**
-	 * The default delegate returns the last two byte of the local host OR the
-	 * port % 0xff.
-	 * 
-	 * @author ak
-	 * 
+	 * Returns a pk-ready dictionary with the supplied key.
+	 * @param key
 	 */
-	public static class DefaultDelegate implements Delegate {
-
-		private int _identifier;
-
-		DefaultDelegate() {
-			int result = getHostIdentifier();
-			result |= getAppIdentifier();
-			_identifier = result;
-		}
-
-		protected int getHostIdentifier() {
-			try {
-				byte[] address = InetAddress.getLocalHost().getAddress();
-				int result = address[3] << 16;
-				result |= address[4] << 8;
-				return result;
-			}
-			catch (UnknownHostException e) {
-				throw NSForwardException._runtimeExceptionForThrowable(e);
-			}
-		}
-
-		protected int getAppIdentifier() {
-			return WOApplication.application().port().intValue() & 0xff;
-		}
-
-		public int identifier() {
-			return _identifier;
-		}
-	}
-
-	private Delegate delegate() {
-		if (_delegate == null) {
-			_delegate = new DefaultDelegate();
-		}
-		return _delegate;
-	}
-
-	public static void setDelegate(Delegate delegate) {
-		_delegate = delegate;
+	public NSDictionary dictionary(String key) {
+		return new NSDictionary(value(), key);
 	}
 }
