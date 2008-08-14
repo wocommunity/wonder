d04e51797a8e4347e4e3b94c2694e35c9081b39b
thoepfner <thoepfner@8dc76679-1c23-de11-85cd-0050568e728b>
2008-08-14 09:44:04 +0000

    Fix for "unmappable character for encoding UTF8"

--- Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/JSAlertPanel.java
+++ Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/JSAlertPanel.java
@@ -1,6 +1,6 @@
 /*
  * JSAlertPanel.java
- * © Copyright 2001 Apple Computer, Inc. All rights reserved.
+ * (c) Copyright 2001 Apple Computer, Inc. All rights reserved.
  * This a modified version.
  * Original license: http://www.opensource.apple.com/apsl/
  */
