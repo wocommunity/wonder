77a6b222876ead2f53463200bbf79f29c56b0a5f
thoepfner <thoepfner@8dc76679-1c23-de11-85cd-0050568e728b>
2008-08-14 09:44:04 +0000

    Fix for "unmappable character for encoding UTF8"
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8174 8dc76679-1c23-de11-85cd-0050568e728b

--- Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/JSComponent.java
+++ Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/JSComponent.java
@@ -1,6 +1,6 @@
 /*
  * JSComponent.java
- * © Copyright 2001 Apple Computer, Inc. All rights reserved.
+ * (c) Copyright 2001 Apple Computer, Inc. All rights reserved.
  * This a modified version.
  * Original license: http://www.opensource.apple.com/apsl/
  */
