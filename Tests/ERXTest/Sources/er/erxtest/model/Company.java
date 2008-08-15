7bcd4ca813242d958871c29d40b01dfad6008125
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-08-15 15:48:45 +0000

    fixed eogen file, regen'd with latest templates + added @Overrides

--- Tests/ERXTest/Sources/er/erxtest/model/Company.java
+++ Tests/ERXTest/Sources/er/erxtest/model/Company.java
@@ -5,10 +5,12 @@ import org.apache.log4j.Logger;
 public class Company extends _Company {
   private static Logger log = Logger.getLogger(Company.class);
   
+  @Override
   public void addToEmployees(er.erxtest.model.Employee object) {
     includeObjectIntoPropertyWithKey(object, Company.EMPLOYEES_KEY);
   }
 
+  @Override
   public void removeFromEmployees(er.erxtest.model.Employee object) {
     excludeObjectFromPropertyWithKey(object, Company.EMPLOYEES_KEY);
   }
