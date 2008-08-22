78c92d7d6a64fefb8ba6edec17ca5c0069faa95c
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-08-22 14:11:25 +0000

    GoogleChart framework
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8221 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Frameworks/Ajax/GoogleChart/Sources/er/googlechart/components/GCQRCode.java
@@ -0,0 +1,80 @@
+package er.googlechart.components;
+
+import com.webobjects.appserver.WOAssociation;
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.foundation.NSDictionary;
+
+import er.extensions.foundation.ERXMutableURL;
+
+/**
+ * QR Codes (see http://code.google.com/apis/chart/#qrcodes).
+ * 
+ * @binding text the text to encode
+ * @binding outputEncoding the output encoding to use (defaults to UTF-8)
+ * @binding ecLevel the error correction level ("L", "M", "Q", or "H")
+ * @binding margin the margin around the code
+ * @binding size "wxh" format chart size ("300x400")
+ * @binding width the width of the chart
+ * @binding height the height of the chart
+ * @binding custom custom query string parameters to append
+ * @binding id the id of the img tag
+ * @binding class the class of the img tag
+ * @binding alt the alt text of the img tag
+ *  
+ * @author mschrag
+ */
+public class GCQRCode extends GCAbstractChart {
+  protected WOAssociation _text;
+  protected WOAssociation _outputEncoding;
+  protected WOAssociation _ecLevel;
+  protected WOAssociation _margin;
+
+  public GCQRCode(String name, NSDictionary associations, WOElement element) {
+    super(name, associations, element);
+    _text = (WOAssociation) associations.objectForKey("text");
+    _outputEncoding = (WOAssociation) associations.objectForKey("outputEncoding");
+  }
+
+  @Override
+  protected void addQueryParameters(ERXMutableURL chartUrl, WOResponse response, WOContext context) {
+    super.addQueryParameters(chartUrl, response, context);
+
+    WOComponent component = context.component();
+    chartUrl.setQueryParameter("cht", "qr");
+
+    String text = null;
+    if (_text != null) {
+      text = (String) _text.valueInComponent(component);
+    }
+    if (text != null) {
+      chartUrl.setQueryParameter("chl", text);
+    }
+
+    String outputEncoding = null;
+    if (_outputEncoding != null) {
+      outputEncoding = (String) _outputEncoding.valueInComponent(component);
+    }
+    if (outputEncoding != null) {
+      chartUrl.setQueryParameter("choe", outputEncoding);
+    }
+
+    if (_ecLevel != null || _margin != null) {
+      StringBuffer chld = new StringBuffer();
+      if (_ecLevel != null) {
+        String ecLevel = (String) _ecLevel.valueInComponent(component);
+        chld.append(ecLevel);
+      }
+      if (_margin != null) {
+        if (chld.length() > 0) {
+          chld.append("|");
+        }
+        String margin = (String) _margin.valueInComponent(component);
+        chld.append(margin);
+      }
+      chartUrl.setQueryParameter("chld", chld.toString());
+    }
+  }
+}
---
+++ Frameworks/Ajax/GoogleChart/Sources/er/googlechart/components/GCSparkline.java
@@ -0,0 +1,62 @@
+package er.googlechart.components;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.foundation.NSDictionary;
+
+import er.extensions.foundation.ERXMutableURL;
+
+/**
+ * A sparkline chart (see http://code.google.com/apis/chart/#sparkline).
+ * 
+ * @binding data the array, or array of arrays, of data
+ * @binding size "wxh" format chart size ("300x400")
+ * @binding width the width of the chart
+ * @binding height the height of the chart
+ * @binding colors an array of color values (for lines, bars, pie slices) 
+ * @binding title the title of the chart
+ * @binding titleColor the color of the chart title
+ * @binding titleSize the size of the chart title
+ * @binding backgroundStyle "solid", "gradient", or "stripes"
+ * @binding background the solid color of the background
+ * @binding chartBackgroundStyle "solid", "gradient", or "stripes"
+ * @binding chartBackground the solid color of the chart background 
+ * @binding transparency the transparency color of the chart background
+ * @binding legend an array of legend values
+ * @binding labeledAxes an array of axes that have labels ("x,y,r") (see http://code.google.com/apis/chart/#multiple_axes_labels)
+ * @binding axisLabels an array of array of axis labels
+ * @binding custom custom query string parameters to append
+ * @binding id the id of the img tag
+ * @binding class the class of the img tag
+ * @binding alt the alt text of the img tag
+ * @binding encoding the explicit chart encoding to use ("simple", "extended", "text")
+ * @binding normalize if true, values will be normalized relative to the max value
+ * @binding maxValue if false, normalization is off or set to a number to override the max value 
+ * @binding scaling if true, numbers will be scaled with an automatic min/max, or set to min/max string values (see http://code.google.com/apis/chart/#data_scaling)
+ * @binding fillArea the fill area (see http://code.google.com/apis/chart/#fill_area_marker)
+ * @binding lineStyles the line styles (see http://code.google.com/apis/chart/#line_styles)
+ * @binding rangeMarkers the string that specifies range markers (see http://code.google.com/apis/chart/#hor_line_marker)
+ * @binding shapeMarkers the string that specifies shape markers (see http://code.google.com/apis/chart/#shape_markers2)
+ * @binding gridLines the override for specifying all gridline values (see http://code.google.com/apis/chart/#grid)
+ * @binding gridXStep the number of steps on the x axis between grid lines
+ * @binding gridYStep the number of steps on the y axis between grid lines
+ * @binding gridLineSize the number of pixels in the line of the dash part of the grid line
+ * @binding gridBlankSize the numer of pixels in the spacing between dashes in the grid line
+ *  
+ * @author mschrag
+ */
+public class GCSparkline extends GCLineChart {
+  public GCSparkline(String name, NSDictionary associations, WOElement element) {
+    super(name, associations, element);
+  }
+
+  @Override
+  protected void addQueryParameters(ERXMutableURL chartUrl, WOResponse response, WOContext context) {
+    super.addQueryParameters(chartUrl, response, context);
+    
+    WOComponent component = context.component();
+    chartUrl.setQueryParameter("cht", "ls");
+  }
+}
