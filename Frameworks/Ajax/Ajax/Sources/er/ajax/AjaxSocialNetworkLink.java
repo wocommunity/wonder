721bb633f2a49a5e6a6f7cfa1c01c765335bf564
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-11-11 11:45:56 +0000

    support for icons in different frameworks + javadoc

--- Frameworks/Ajax/Ajax/Sources/er/ajax/AjaxSocialNetworkLink.java
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/AjaxSocialNetworkLink.java
@@ -4,6 +4,16 @@ import com.webobjects.appserver.WOContext;
 
 import er.extensions.components.ERXComponent;
 
+/**
+ * AjaxSocialNetworkLink creates a link to the submission URL for 
+ * a social network around the social network's icon.
+ * 
+ * @author mschrag
+ * @binding name the name of the social network (@see er.ajax.AjaxSocialNetwork.socialNetworkNamed)
+ * @binding url the URL to submit
+ * @binding title the title to submit
+ * @binding alt the alt tag (defaults to the name of the network)
+ */
 public class AjaxSocialNetworkLink extends ERXComponent {
 	public AjaxSocialNetworkLink(WOContext context) {
 		super(context);
