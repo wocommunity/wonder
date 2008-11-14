f820f60ca2ed4ed03b1c74eef1a544b997698aa6
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2008-11-14 00:45:57 +0000

    Made ID unique, more debug logging

--- Examples/Ajax/AjaxExample/Sources/Session.java
+++ Examples/Ajax/AjaxExample/Sources/Session.java
@@ -1,5 +1,6 @@
-import er.extensions.*;
-import er.extensions.appserver.ERXSession;
+import com.webobjects.appserver.*;
+
+import er.extensions.appserver.*;
 
 /**
  * Everything related to saving pages is handled now in ERXSession.
@@ -11,6 +12,14 @@ import er.extensions.appserver.ERXSession;
 public class Session extends ERXSession {
 
 	public String selectedTab = "InlineContent";
+	
+	public void savePage(WOComponent page) {
+//		NSLog.out.appendln("Session shouldNotStorePage: " + ERXAjaxApplication.shouldNotStorePage(context()));
+//		NSLog.out.appendln("rquest key: " + context().request().headerForKey(ERXAjaxSession.PAGE_REPLACEMENT_CACHE_LOOKUP_KEY));
+//		NSLog.out.appendln("response key: " + context().response().headerForKey(ERXAjaxSession.PAGE_REPLACEMENT_CACHE_LOOKUP_KEY));
+	      
+		super.savePage(page);
+	}
 
 
 }
