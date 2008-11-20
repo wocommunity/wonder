a54f5fbfda4ae1194be38db01379ec3403f9cf29
thoepfner <thoepfner@8dc76679-1c23-de11-85cd-0050568e728b>
2008-11-20 14:24:48 +0000

    Fix for "unmappable character for encoding UTF8" in ant build. Sometimes these broke the ant build for me, but not always...
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8655 8dc76679-1c23-de11-85cd-0050568e728b

--- Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/WXBar.java
+++ Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/WXBar.java
@@ -1,6 +1,6 @@
 /*
  * WXBar.java
- * © Copyright 2001 Apple Computer, Inc. All rights reserved.
+ * (c) Copyright 2001 Apple Computer, Inc. All rights reserved.
  * This a modified version.
  * Original license: http://www.opensource.apple.com/apsl/
  */
