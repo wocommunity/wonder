111a0aa87b97c630bb442926396e113e68256fd9
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2008-12-05 04:08:57 +0000

    Add taglet for @properties and tie into JavaDoc.  Add build.xml for taglets. Maybe having this documentation in the JavaDocs will help.

---
+++ Build/taglets/PropertyTaglet.java
@@ -0,0 +1,47 @@
+import com.sun.tools.doclets.Taglet;
+import com.sun.javadoc.*;
+import java.util.Map;
+
+/**
+ * Taglet representing @property. This tag can be used in any kind of
+ * {@link com.sun.javadoc.Doc}.  It is not an inline tag. For
+ * example, "@property er.migration.skipModelNames a comma-separated list of 
+ * model names to NOT be migrated." would be shown as:
+ * <DL>
+ * <DT>
+ * <B>Properties:</B>
+ * <DD><table><tr><th>er.migration.skipModelNames</th><td>
+ * a comma-separated list of model names
+ * to NOT be migrated.</td></tr></table></DD>
+ * </DL>
+ *
+ * @author chill
+ */
+
+public class PropertyTaglet extends AbstractTaglet  {
+    
+    private static final String NAME = "property";
+    private static final String HEADER = "Properties";
+    
+    public String getName() {
+        return NAME;
+    }
+    
+    public String getHeader() {
+        return HEADER;
+    }
+    
+    /**
+     * Register this Taglet.
+     * @param tagletMap  the map to register this tag to.
+     */
+    public static void register(Map tagletMap) {
+       PropertyTaglet tag = new PropertyTaglet();
+       Taglet t = (Taglet) tagletMap.get(tag.getName());
+       if (t != null) {
+           tagletMap.remove(tag.getName());
+       }
+       tagletMap.put(tag.getName(), tag);
+    }
+}
+
