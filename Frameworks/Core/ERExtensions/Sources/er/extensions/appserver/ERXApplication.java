f64b3374b9538577feb1462e88feb70a5e30dad9
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-12-11 04:43:30 +0000

    Support for DirectConnect SSL

--- Frameworks/Core/ERExtensions/Sources/er/extensions/appserver/ERXApplication.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/appserver/ERXApplication.java
@@ -49,6 +49,7 @@ import com.webobjects.appserver.WOResponse;
 import com.webobjects.appserver.WOSession;
 import com.webobjects.appserver.WOTimer;
 import com.webobjects.appserver._private.WOComponentDefinition;
+import com.webobjects.appserver._private.WOProperties;
 import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOTemporaryGlobalID;
 import com.webobjects.foundation.NSArray;
@@ -205,6 +206,21 @@ public abstract class ERXApplication extends ERXAjaxApplication implements ERXGr
 	 * The path rewriting replacement to apply to the matched pattern (@see _rewriteURL) 
 	 */
 	protected String _replaceApplicationPathReplace;
+
+	/**
+	 * The SSL host used by this application.
+	 */
+	protected String _sslHost;
+
+	/**
+	 * The SSL port used by this application.
+	 */
+	protected Integer _sslPort;
+
+	/**
+	 * Tracks whether or not _addAdditionalAdaptors has been called yet.
+	 */
+	protected boolean _initializedAdaptors = false;
 
 	/**
 	 * Copies the props from the command line to the static dict
@@ -2174,5 +2190,110 @@ public abstract class ERXApplication extends ERXAjaxApplication implements ERXGr
 			}
 
 		};
+	}
+
+	/**
+	 * Returns whether or not DirectConnect SSL should be enabled.  If you set this, please
+	 * review the DirectConnect SSL section of the ERExtensions sample Properties file to
+	 * learn more about how to properly configure it.
+	 *  
+	 * @return whether or not DirectConnect SSL should be enabled
+	 * @property er.extensions.ERXApplication.ssl.enabled
+	 */
+	public boolean sslEnabled() {
+		return ERXProperties.booleanForKey("er.extensions.ERXApplication.ssl.enabled");
+	}
+
+	/**
+	 * Returns the host name that will be used to bind the SSL socket to (defaults to host()).
+	 * 
+	 * @return the SSL socket host
+	 * @property er.extensions.ERXApplication.ssl.host
+	 */
+	public String sslHost() {
+		String sslHost = _sslHost;
+		if (sslHost == null) {
+			sslHost = ERXProperties.stringForKeyWithDefault("er.extensions.ERXApplication.ssl.host", host());
+		}
+		return sslHost;
+	}
+	
+	/**
+	 * Sets an SSL host override.
+	 * 
+	 * @param sslHost an SSL host override
+	 */
+	public void _setSslHost(String sslHost) {
+		_sslHost = sslHost;
+	}
+
+	/**
+	 * Returns the SSL port that will be used for DirectConnect SSL (defaults to 443).  A value of
+	 * 0 will cause WO to autogenerate an SSL port number.
+	 * 
+	 * @return the SSL port that will be used for DirectConnect SSL
+	 * @property er.extensions.ERXApplication.ssl.port
+	 */
+	public int sslPort() {
+		int sslPort;
+		if (_sslPort != null) {
+			sslPort = _sslPort.intValue();
+		}
+		else {
+			sslPort = ERXProperties.intForKeyWithDefault("er.extensions.ERXApplication.ssl.port", 443);
+		}
+		return sslPort;
+	}
+	
+	/**
+	 * Sets an SSL port override (called back by the ERXSecureAdaptor)
+	 * 
+	 * @param sslPort an ssl port override
+	 */
+	public void _setSslPort(int sslPort) {
+		_sslPort = sslPort;
+	}
+
+	/**
+	 * Injects additional adaptors into the WOAdditionalAdaptors setting.  Subclasses can extend this
+	 * method, but should call super._addAdditionalAdaptors.
+	 * 
+	 * @param additionalAdaptors the mutable adaptors array
+	 */
+	protected void _addAdditionalAdaptors(NSMutableArray<NSDictionary<String, Object>> additionalAdaptors) {
+		if (sslEnabled()) {
+			boolean sslAdaptorConfigured = false;
+			for (NSDictionary<String, Object> adaptor : additionalAdaptors) {
+				if (ERXSecureDefaultAdaptor.class.getName().equals(adaptor.objectForKey(WOProperties._AdaptorKey))) {
+					sslAdaptorConfigured = true;
+				}
+			}
+			ERXSecureDefaultAdaptor.checkSSLConfig();
+			if (!sslAdaptorConfigured) {
+				NSMutableDictionary<String, Object> sslAdaptor = new NSMutableDictionary<String, Object>();
+				sslAdaptor.setObjectForKey(ERXSecureDefaultAdaptor.class.getName(), WOProperties._AdaptorKey);
+				sslAdaptor.setObjectForKey(sslHost(), WOProperties._HostKey);
+				sslAdaptor.setObjectForKey(Integer.valueOf(sslPort()), WOProperties._PortKey);
+				additionalAdaptors.addObject(sslAdaptor);
+			}
+		}
+	}
+	
+	/**
+	 * Returns the additionalAdaptors, but calls _addAdditionalAdaptors to give the runtime an opportunity to
+	 * programmatically force adaptors into the list.  
+	 */
+	@Override
+	@SuppressWarnings("deprecation")
+	public NSArray<NSDictionary<String, Object>> additionalAdaptors() {
+		NSArray<NSDictionary<String, Object>> additionalAdaptors = super.additionalAdaptors();
+		if (!_initializedAdaptors) {
+			NSMutableArray<NSDictionary<String, Object>> mutableAdditionalAdaptors = additionalAdaptors.mutableClone();
+			_addAdditionalAdaptors(mutableAdditionalAdaptors);
+			_initializedAdaptors = true;
+			additionalAdaptors = mutableAdditionalAdaptors;
+			setAdditionalAdaptors(mutableAdditionalAdaptors);
+		}
+		return additionalAdaptors;
 	}
 }
