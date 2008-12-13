338a11db2556496e2884a7ccd3c54abc9d55cd04
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2008-12-13 15:50:52 +0000

    simple top-level-page binding debug ... fancier coming

--- Frameworks/Misc/WOLips/Sources/er/wolips/components/WOLToolBar.java
+++ Frameworks/Misc/WOLips/Sources/er/wolips/components/WOLToolBar.java
@@ -1,7 +1,12 @@
 package er.wolips.components;
 
+import java.lang.reflect.Method;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WOApplication;
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSLog;
 
 import er.extensions.components.ERXComponentUtilities;
 
@@ -14,20 +19,39 @@ import er.extensions.components.ERXComponentUtilities;
  * @binding expanded if true, the toolbar is expanded by default; defaults to false 
  */
 public class WOLToolBar extends WOComponent {
+  private boolean _debugEnabled;
+
   public WOLToolBar(WOContext context) {
     super(context);
   }
-  
+
   @Override
   public boolean isStateless() {
     return true;
   }
-  
+
   public String style() {
     String style = null;
     if (!ERXComponentUtilities.booleanValueForBinding(this, "expanded", false)) {
       style = "display: none";
     }
     return style;
+  }
+
+  public boolean isDebugEnabled() {
+    return _debugEnabled;
+  }
+  
+  public WOActionResults toggleDebugEnabled() {
+    try {
+      WOApplication application = WOApplication.application();
+      Method setDebugMethod = application.getClass().getMethod("setDebugEnabledForComponent", boolean.class, WOComponent.class);
+      _debugEnabled = !_debugEnabled;
+      setDebugMethod.invoke(application, _debugEnabled, context().page());
+    }
+    catch (Throwable t) {
+      NSLog.debug.appendln("Your application does not have a setDebugEnabledForComopnent(boolean, WOComponent) method.");
+    }
+    return context().page();
   }
 }
