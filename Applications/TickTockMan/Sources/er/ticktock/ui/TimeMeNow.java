2bf287218353836c9f724b4225dae9828409157b
kiddyr <kiddyr@8dc76679-1c23-de11-85cd-0050568e728b>
2009-01-08 04:52:57 +0000

    Very first (extremely simple) version of an app that will do various time-related tasks. Hopefully, this can become a resource for people trying to understand the use of time, time intervals, time zones and associated objects in WebObjects applications.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8792 8dc76679-1c23-de11-85cd-0050568e728b

---
+++ Applications/TickTockMan/Sources/er/ticktock/ui/TimeMeNow.java
@@ -0,0 +1,31 @@
+package er.ticktock.ui;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSTimestamp;
+
+public class TimeMeNow extends WOComponent {
+
+	public TimeMeNow(WOContext context) {
+		super(context);
+	}
+
+	public NSTimestamp startTime = new NSTimestamp();
+	long startMillis = System.currentTimeMillis();
+
+	public NSTimestamp endTime = null;
+	long endMillis = 0L;
+
+	public WOComponent clickNow() {
+		endTime = new NSTimestamp();
+		endMillis = System.currentTimeMillis();
+		return null;
+	}
+
+	public int difference() {
+		if (endMillis == 0L)
+			return 0;
+		else
+			return (new Long((endMillis - startMillis) / 1000)).intValue() + 1;
+	}
+}
