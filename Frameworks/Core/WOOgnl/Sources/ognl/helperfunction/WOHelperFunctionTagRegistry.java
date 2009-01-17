6c16b913c582a22f571a3202f58d92784d86560b
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-01-17 18:49:30 +0000

    add all the shortcuts that 5.4 has so we have better parity

--- Frameworks/Core/WOOgnl/Sources/ognl/helperfunction/WOHelperFunctionTagRegistry.java
+++ Frameworks/Core/WOOgnl/Sources/ognl/helperfunction/WOHelperFunctionTagRegistry.java
@@ -40,27 +40,77 @@ public class WOHelperFunctionTagRegistry {
 	static {
 		WOHelperFunctionTagRegistry.log.setLevel(Level.WARN);
 
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOString", "string");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOString", "str");
+        // aliases for convenience
+        // Note that aliases are case insensitive.
+		WOHelperFunctionTagRegistry.registerTagShortcut("ERXLocalizedString", "localized"); // not in 5.4
+
 		WOHelperFunctionTagRegistry.registerTagShortcut("ERXElse", "else");
 		WOHelperFunctionTagRegistry.registerTagShortcut("ERXWOConditional", "if");
-		WOHelperFunctionTagRegistry.registerTagShortcut("ERXWOConditional", "condition");
 		WOHelperFunctionTagRegistry.registerTagShortcut("ERXWOConditional", "conditional");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOHyperlink", "link");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WORepetition", "loop");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOTextField", "textfield");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOCheckBox", "checkbox");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOHiddenField", "hidden");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOPopUpButton", "select");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WORadioButton", "radio");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOPasswordField", "password");
+		WOHelperFunctionTagRegistry.registerTagShortcut("ERXWOConditional", "condition"); // not in 5.4
+		WOHelperFunctionTagRegistry.registerTagShortcut("WORepetition", "foreach");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WORepetition", "repeat");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WORepetition", "repetition");
+		WOHelperFunctionTagRegistry.registerTagShortcut("WORepetition", "loop"); // not in 5.4
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOComponentContent", "content");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOComponentContent", "componentContent");
+		WOHelperFunctionTagRegistry.registerTagShortcut("WOString", "str"); // not in 5.4
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOString", "string");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOSwitchComponent", "switchComponent");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOSwitchComponent", "switch");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOXMLNode", "XMLNode");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WONestedList", "nestedList");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOParam", "param");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOApplet", "applet");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOQuickTime", "quickTime");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOHTMLCommentString", "commentString");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOHTMLCommentString", "comment");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WONoContentElement", "noContentElement");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WONoContentElement", "noContent");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOBody", "body");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOEmbeddedObject", "embeddedObject");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOEmbeddedObject", "embedded");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOFrame", "frame");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOImage", "image");
+		WOHelperFunctionTagRegistry.registerTagShortcut("WOImage", "img"); // not in 5.4
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOForm", "form");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOJavaScript", "javaScript");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOVBScript", "VBScript");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOResourceURL", "resourceURL");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOGenericElement", "genericElement");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOGenericElement", "element");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOGenericContainer", "genericContainer");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOGenericContainer", "container");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOActiveImage", "activeImage");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOCheckBox", "checkBox");
+		WOHelperFunctionTagRegistry.registerTagShortcut("WOCheckBox", "checkbox"); // not in 5.4 (5.4 is case insensitive)
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOFileUpload", "fileUpload");
 		WOHelperFunctionTagRegistry.registerTagShortcut("WOFileUpload", "upload");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOText", "text");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOForm", "form");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOSubmitButton", "submit");
-		WOHelperFunctionTagRegistry.registerTagShortcut("ERXLocalizedString", "localized");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOImage", "img");
-		WOHelperFunctionTagRegistry.registerTagShortcut("WOImage", "image");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOHiddenField", "hiddenField");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOHiddenField", "hidden"); // not in 5.4
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOImageButton", "imageButton");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOInputList", "inputList");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOBrowser", "browser");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOCheckBoxList", "checkBoxList");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOPopUpButton", "popUpButton");
+		WOHelperFunctionTagRegistry.registerTagShortcut("WOPopUpButton", "select"); // not in 5.4
+        WOHelperFunctionTagRegistry.registerTagShortcut("WORadioButtonList", "radioButtonList");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOPasswordField", "passwordField");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOPassword", "password");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WORadioButton", "radioButton");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WORadioButton", "radio");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOResetButton", "resetButton");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOResetButton", "reset");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOSubmitButton", "submitButton");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOSubmitButton", "submit");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOText", "text");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOTextField", "textField");
+		WOHelperFunctionTagRegistry.registerTagShortcut("WOTextField", "textfield"); // not in 5.4 (5.4 is case insensitive)
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOSearchField", "search");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOSearchField", "searchfield");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOHyperlink", "hyperlink");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOHyperlink", "link");
+        WOHelperFunctionTagRegistry.registerTagShortcut("WOActionURL", "actionURL");
 
 		WOHelperFunctionTagRegistry.registerTagProcessorForElementType(new NotTagProcessor(), "not");
 	}
