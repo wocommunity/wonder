3050aba3446252ef49262179605a9b5ebb979dcc
mbushkov <mbushkov@8dc76679-1c23-de11-85cd-0050568e728b>
2009-01-20 11:17:44 +0000

    Sorting test files by absolute path - it results in test files grouped by folder names and then by file names

--- Frameworks/Misc/ERSelenium/Sources/er/selenium/DefaultSeleniumTestFilesFinder.java
+++ Frameworks/Misc/ERSelenium/Sources/er/selenium/DefaultSeleniumTestFilesFinder.java
@@ -43,7 +43,7 @@ public class DefaultSeleniumTestFilesFinder implements SeleniumTestFilesFinder {
 			}
 		}
 		
-		ERXArrayUtilities.sortArrayWithKey(result, "name");
+		ERXArrayUtilities.sortArrayWithKey(result, "absolutePath");
 		return result;
 	}
 	
