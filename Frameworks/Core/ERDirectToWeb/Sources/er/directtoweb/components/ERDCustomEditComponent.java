8869cc22ae2ede3b6182b19baac08453a8aa22e5
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2009-01-27 23:57:23 +0000

    Adding some utility methods for common operations and a method for getting the destination entity's sort orderings.

--- Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/ERDCustomEditComponent.java
+++ Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/ERDCustomEditComponent.java
@@ -9,8 +9,16 @@ package er.directtoweb.components;
 import org.apache.log4j.Logger;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2WContext;
+import com.webobjects.eoaccess.EOEntity;
+import com.webobjects.eoaccess.EOUtilities;
 import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOSortOrdering;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableArray;
+
+import er.extensions.foundation.ERXArrayUtilities;
 
 /**
  * Superclass for most of the custom edit components.  <br />
@@ -28,6 +36,8 @@ public abstract class ERDCustomEditComponent extends ERDCustomComponent {
         public static final String localContext = "localContext";
         public static final String permissionToEdit = "permissionToEdit";
     }
+
+    private NSArray _defaultSortOrderingsForDestinationEntity;
 
     /**
      * Public constructor
@@ -68,15 +78,67 @@ public abstract class ERDCustomEditComponent extends ERDCustomComponent {
     public boolean isStateless() { return false; }
     public boolean synchronizesVariablesWithBindings() { return true; }
 
+    /** Used by stateful but non-synching subclasses */
+    public void resetCachedBindingsInStatefulComponent() {
+        super.resetCachedBindingsInStatefulComponent();
+        object = null;
+        editingContext = null;
+        _defaultSortOrderingsForDestinationEntity = null;
+    }
+
     // Used by stateless subclasses
     public void reset() {
         super.reset();
         object = null;
         editingContext = null;
+        _defaultSortOrderingsForDestinationEntity = null;
     }
 
     // Defaults to true when not used in a D2W component.  In the rules the default is false.
     public boolean permissionToEdit() {
         return hasBinding(Keys.permissionToEdit) ? booleanValueForBinding(Keys.permissionToEdit) : true;
     }
+
+    /**
+     * Computes the destination entity that we're editing.  Hits the "destinationEntityName"
+     * rule.
+     *
+     * @return destination entity
+     */
+    public EOEntity destinationEntity() {
+        final String destinationEntityName = (String)valueForBinding("destinationEntityName");
+        return destinationEntityName != null ? EOUtilities.entityNamed(object().editingContext(), destinationEntityName) : null;
+    }
+
+    /**
+     * Hits the destinationEntityName rule to get the destination entity.  Grabs a private D2WContext, sets the
+     * destination entity and asks for the defaultSortOrdering rule.
+     *
+     * @return an array of sort orderings for the destination entity.
+     */
+    public NSArray defaultSortOrderingsForDestinationEntity() {
+        if (_defaultSortOrderingsForDestinationEntity == null) {
+            final D2WContext context = new D2WContext();
+            final NSArray sortOrderingDefinition;
+            final int sortOrderingDefinitionCount;
+            NSMutableArray sortOrderings = null;
+            int i = 0;
+
+            context.setEntity(destinationEntity());
+            sortOrderingDefinition = (NSArray)context.valueForKey("defaultSortOrdering");
+            sortOrderingDefinitionCount = sortOrderingDefinition != null ? sortOrderingDefinition.count() : 0;
+
+            while (i < sortOrderingDefinitionCount) {
+                final String key = (String)sortOrderingDefinition.objectAtIndex(i++);
+                final String selectorKey = (String)sortOrderingDefinition.objectAtIndex(i++);
+                final EOSortOrdering sortOrdering = new EOSortOrdering(key, ERXArrayUtilities.sortSelectorWithKey(selectorKey));
+                (sortOrderings != null ? sortOrderings : (sortOrderings = new NSMutableArray())).addObject(sortOrdering);
+            }
+
+            _defaultSortOrderingsForDestinationEntity = sortOrderings != null ? sortOrderings.immutableClone() : NSArray.EmptyArray;
+        }
+
+        return _defaultSortOrderingsForDestinationEntity;
+    }
+
 }
