10babb7fe7873866274e8ddc0428948397a3f4f7
bob_schwarzmann <bob_schwarzmann@8dc76679-1c23-de11-85cd-0050568e728b>
2009-02-10 23:08:05 +0000

    Merged revisions 8909 via svnmerge from https://wonder.svn.sourceforge.net/svnroot/wonder/branches/Wonder_2_0_0_Branch/Wonder
    ........
      r8909 | dscheck | 2009-02-07 15:11:49 -0800 (Sat, 07 Feb 2009) | 1 line
      
      Added callback support via d2wContext delegate to get the displayedObjects and allObjects lists in case they need to be something other than what the displayGroup has
    ........

--- Common/Frameworks/ERExcelLook/Sources/er/directtoweb/excel/ERExcelExportButton.java
+++ Common/Frameworks/ERExcelLook/Sources/er/directtoweb/excel/ERExcelExportButton.java
@@ -21,6 +21,10 @@ import com.webobjects.directtoweb.ListPageInterface;
 import com.webobjects.eocontrol.EOArrayDataSource;
 import com.webobjects.eocontrol.EOClassDescription;
 import er.extensions.ERXValueUtilities;
+
+import java.io.*;
+import java.lang.reflect.*;
+
 /**
  * Component used in the List page to provide a generic excel export.<br />
  * 
@@ -29,6 +33,9 @@ import er.extensions.ERXValueUtilities;
  */
 
 public class ERExcelExportButton extends ERDCustomQueryComponent {
+
+    public static NSSelector DisplayedObjectsForExcelSelector = new NSSelector("displayedObjectsForExcelExport");
+    public static NSSelector AllObjectsForExcelSelector = new NSSelector("allObjectsForExcelExport");
 
     public ERExcelExportButton(WOContext context) { super(context); }
 
@@ -42,19 +49,81 @@ public class ERExcelExportButton extends ERDCustomQueryComponent {
     }
 
     public WOComponent excelExportDisplayed() {
+        NSArray array = null;
+
         EOArrayDataSource ds=new EOArrayDataSource(EOClassDescription.classDescriptionForEntityName(d2wContext().entity().name()), displayGroup().dataSource().editingContext());
-        ds.setArray(displayGroup().displayedObjects());
+
+        Object delegate = d2wContext().valueForKey("excelExportDelegate");
+
+        if (DisplayedObjectsForExcelSelector.implementedByObject(delegate)) {
+            // Needed this for custom template pages that display more item than whats in the standard displayGroup
+            try {
+                array = (NSArray) DisplayedObjectsForExcelSelector.invoke(delegate);
+            }
+            catch (IllegalAccessException e) {
+
+            }
+            catch (IllegalArgumentException e) {
+
+            }
+            catch (InvocationTargetException e) {
+
+            }
+            catch (NoSuchMethodException e) {
+
+            }
+        }
+        else {
+            array = displayGroup().displayedObjects();
+        }
+
+        if (log.isDebugEnabled()) log.debug("displayedObjectsForExcel = "+array);
+
+        ds.setArray(array);
+
         return excelExport(ds);
     }
 
     public WOComponent excelExportAll() {
+        NSArray array = null;
+
         EOArrayDataSource ds=new EOArrayDataSource(EOClassDescription.classDescriptionForEntityName(d2wContext().entity().name()), displayGroup().dataSource().editingContext());
-        ds.setArray(displayGroup().allObjects());
+
+        Object delegate = d2wContext().valueForKey("excelExportDelegate");
+
+        if (AllObjectsForExcelSelector.implementedByObject(delegate)) {
+            // Needed this for custom template pages that display more item than whats in the standard displayGroup
+            try {
+                array = (NSArray) AllObjectsForExcelSelector.invoke(delegate);
+            }
+            catch (IllegalAccessException e) {
+
+            }
+            catch (IllegalArgumentException e) {
+
+            }
+            catch (InvocationTargetException e) {
+
+            }
+            catch (NoSuchMethodException e) {
+
+            }
+        }
+        else {
+            array = displayGroup().allObjects();
+        }
+
+        if (log.isDebugEnabled()) log.debug("allObjectsForExcel = "+array);
+
+        ds.setArray(array);
+
         return excelExport(ds);
     }
 
     public WOComponent excelExport(EODataSource ds){
         D2WContext context=d2wContext();
+
+        if (log.isDebugEnabled()) log.debug("ds all objects = "+ds.fetchObjects());
 
         D2WContext newContext=new D2WContext(context);
         newContext.takeValueForKey(context.task(),"task");
