1ec9238166d6b5868f2f277f4cd79390b59d2923
qdolan <qdolan@8dc76679-1c23-de11-85cd-0050568e728b>
2009-02-23 12:20:40 +0000

    Update WOJavaRebel to support JavaRebel 2.0RC1 plugin signature.
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@8960 8dc76679-1c23-de11-85cd-0050568e728b

--- Frameworks/Misc/WOJavaRebel/Sources/org/zeroturnaround/javarebel/WOJavaRebelIntegrationPlugin.java
+++ Frameworks/Misc/WOJavaRebel/Sources/org/zeroturnaround/javarebel/WOJavaRebelIntegrationPlugin.java
@@ -1,8 +1,5 @@
 package org.zeroturnaround.javarebel;
 
-import org.zeroturnaround.javarebel.IntegrationFactory;
-import org.zeroturnaround.javarebel.Plugin;
-import org.zeroturnaround.javarebel.support.PackageClassFilter;
 
 /**
  * JavaRebel WebObjects Integration Plugin
@@ -14,14 +11,26 @@ public class WOJavaRebelIntegrationPlugin implements Plugin {
 	public void preinit() {
 	    IntegrationFactory.getInstance()
 	      .addIntegrationProcessor(WOJavaRebelBytecodeProcessor.IDEPATCH_CLASS, new WOJavaRebelBytecodeProcessor());
-		// Reduce performance penalty by excluding packages that will not be changing
-		ConfigurationFactory.getInstance()
-			.addExcludeManagedFilter(new PackageClassFilter(new String[]{
-				"com.webobjects", "com.apple", "org.apache", "javax.xml",
-				"org.w3c", "org.xml", "ognl", "org.zeroturnaround" }));
+//		 Reduce performance penalty by excluding packages that will not be changing
+//		ConfigurationFactory.getInstance()
+//			.addExcludeManagedFilter(new PackageClassFilter(new String[]{
+//				"com.webobjects", "com.apple", "org.apache", "javax.xml",
+//				"org.w3c", "org.xml", "ognl", "org.zeroturnaround" }));
 	}
 
   public boolean checkDependencies(ClassLoader arg0, ClassResourceSource arg1) {
     return false;
+  }
+
+  public String getDescription() {
+    return "WebObjects JavaRebel Plugin";
+  }
+
+  public String getId() {
+    return "WOJR";
+  }
+
+  public String getName() {
+    return "WOJavaRebel";
   }
 }
