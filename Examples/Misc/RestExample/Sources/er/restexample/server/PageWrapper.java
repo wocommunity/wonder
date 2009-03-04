8202b93f84fc0734f6a67b9ab7748a8fccf3892c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2009-03-04 23:06:21 +0000

    first (non-working) version of D2W based REST

---
+++ Examples/Misc/RestExample/Sources/er/restexample/server/PageWrapper.java
@@ -0,0 +1,10 @@
+package er.restexample.server;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOComponent;
+
+public class PageWrapper extends WOComponent {
+    public PageWrapper(WOContext context) {
+        super(context);
+    }
+}
---
+++ Examples/Misc/RestExample/Sources/er/restexample/server/ServerForum.java
@@ -0,0 +1,7 @@
+package er.restexample.server;
+
+import org.apache.log4j.Logger;
+
+public class ServerForum extends _ServerForum {
+  private static Logger log = Logger.getLogger(ServerForum.class);
+}
