4e671c71721d205231b9317a7389b3edd8692f60
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-03-10 17:36:12 +0000

    move prototype.js into appendToResponse -- this was breaking in Safari 4 ... messed up ordering or something
    git-svn-id: file:///Users/q/working/wondersvn/trunk/Wonder@9034 8dc76679-1c23-de11-85cd-0050568e728b

--- Applications/Uber/Sources/er/uber/components/REST.java
+++ Applications/Uber/Sources/er/uber/components/REST.java
@@ -1,9 +1,18 @@
 package er.uber.components;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOResponse;
+
+import er.ajax.AjaxUtils;
 
 public class REST extends UberComponent {
     public REST(WOContext context) {
         super(context);
+    }
+    
+    @Override
+    public void appendToResponse(WOResponse response, WOContext context) {
+    	AjaxUtils.addScriptResourceInHead(context, response, "prototype.js");
+    	super.appendToResponse(response, context);
     }
 }
