89d251c5ed9b5a365c586021dbad50c63f986434
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-03-19 13:56:25 +0000

    WONDER-247	patch for ERXModel and ERXModelGroup$Model for ERXModel to optionally share extended prototypes logic

--- Frameworks/Core/ERExtensions/Sources/com/webobjects/eoaccess/ERXModel.java
+++ Frameworks/Core/ERExtensions/Sources/com/webobjects/eoaccess/ERXModel.java
@@ -5,22 +5,129 @@ package com.webobjects.eoaccess;
 
 import java.net.URL;
 
+import org.apache.log4j.Logger;
+
 import com.webobjects.eoaccess.EOModel;
+import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableDictionary;
+import com.webobjects.jdbcadaptor.JDBCAdaptor;
 
+import er.extensions.eof.ERXKey;
+import er.extensions.eof.ERXModelGroup;
 import er.extensions.foundation.ERXProperties;
 
 /**
- * This EOModel subclass simply provides the opportunity to subclass EOEntity.
+ * This EOModel subclass primarily provides the opportunity to subclass EOEntity.
  * 
  * <p><b>Note</b> the package <code>com.webobjects.eoaccess</code> is used to
  * allow any protected or default access superclass instance methods to resolve
  * at runtime.
+ * 
+ * <p>To allow for extended prototypes set
+ * <code>er.extensions.ERXModel.useExtendedPrototypes=true</code>.
  *  
  * @author ldeck
  */
 public class ERXModel extends EOModel {
+	
+	private static final Logger log = Logger.getLogger(ERXModel.class);
+	
+	/**
+	 * Utility to add attributes to the prototype cache. As the attributes are chosen by name, replace already
+	 * existing ones.
+	 * 
+	 * @param entity
+	 */
+	private static void addAttributesToPrototypesCache(EOEntity entity) {
+		addAttributesToPrototypesCache(entity.model(), attributesFromEntity(entity));
+	}
+
+	/**
+	 * Utility to add attributes to the prototype cache for a given model. As the attributes are chosen by name, replace already
+	 * existing ones.
+	 * 
+	 * @param model
+	 * @param attributes
+	 */
+	private static void addAttributesToPrototypesCache(EOModel model, NSArray<? extends EOAttribute> attributes) {
+		if (attributes.count() != 0) {
+			NSArray keys = namesForAttributes(attributes);
+			NSDictionary temp = new NSDictionary(attributes, keys);
+			model._prototypesByName.addEntriesFromDictionary(temp);
+		}
+	}
+	
+	/**
+	 * Utility for getting all the attributes off an entity. If the entity is null, an empty array is returned.
+	 * 
+	 * @param entity
+	 * @return array of attributes from the given entity
+	 */
+	private static NSArray<EOAttribute> attributesFromEntity(EOEntity entity) {
+		NSArray<EOAttribute> result = NSArray.emptyArray();
+		if (entity != null) {
+			result = entity.attributes();
+			log.info("Attributes from " + entity.name() + ": " + result);
+		}
+		return result;
+	}
+	
+	/**
+	 * Create the prototype cache for the given model by walking a search order.
+	 * @param model 
+	 */
+	public static void createPrototypes(EOModel model) {
+		log.info("Creating prototypes for model: " + model.name() + "->" + model.connectionDictionary());
+		synchronized (_EOGlobalModelLock) {
+			model._prototypesByName = new NSMutableDictionary();
+			String name = model.name();
+			NSArray adaptorPrototypes = NSArray.EmptyArray;
+			EOAdaptor adaptor = EOAdaptor.adaptorWithModel(model);
+			try {
+				adaptorPrototypes = adaptor.prototypeAttributes();
+			}
+			catch (Exception e) {
+				log.error(e, e);
+			}
+			addAttributesToPrototypesCache(model, adaptorPrototypes);
+			NSArray prototypesToHide = attributesFromEntity(model._group.entityNamed("EOPrototypesToHide"));
+			model._prototypesByName.removeObjectsForKeys(namesForAttributes(prototypesToHide));
+
+			String plugin = null;
+			if (adaptor instanceof JDBCAdaptor && !model.name().equalsIgnoreCase("erprototypes")) {
+				plugin = (String) model.connectionDictionary().objectForKey("plugin");
+			}
+
+			addAttributesToPrototypesCache(model._group.entityNamed("EOPrototypes"));
+			addAttributesToPrototypesCache(model._group.entityNamed("EO" + model.adaptorName() + "Prototypes"));
+			if (plugin != null) {
+				addAttributesToPrototypesCache(model._group.entityNamed("EOJDBC" + plugin + "Prototypes"));
+			}
+
+			addAttributesToPrototypesCache(model._group.entityNamed("EOCustomPrototypes"));
+			addAttributesToPrototypesCache(model._group.entityNamed("EO" + model.adaptorName() + "CustomPrototypes"));
+			if (plugin != null) {
+				addAttributesToPrototypesCache(model._group.entityNamed("EOJDBC" + plugin + "CustomPrototypes"));
+			}
+
+			addAttributesToPrototypesCache(model._group.entityNamed("EO" + name + "Prototypes"));
+			addAttributesToPrototypesCache(model._group.entityNamed("EO" + model.adaptorName() + name + "Prototypes"));
+			if (plugin != null) {
+				addAttributesToPrototypesCache(model._group.entityNamed("EOJDBC" + plugin + name + "Prototypes"));
+			}
+		}
+	}
+	
+	/**
+	 * Utility for getting all names from an array of attributes.
+	 * 
+	 * @param attributes
+	 * @return array of attribute names
+	 */
+	private static NSArray<String> namesForAttributes(NSArray<? extends EOAttribute> attributes) {
+		return new ERXKey<String>("name").arrayValueInObject(attributes);
+	}
 
 	/**
 	 * Creates and returns a new ERXModel.
@@ -77,6 +184,50 @@ public class ERXModel extends EOModel {
 			list.setObjectForKey(eoEntityClassName, "entityClass" );
 		}
 		return super._addEntityWithPropertyList(list);
+	}
+	
+	/**
+	 * Overridden to use our prototype creation method if
+	 * <code>er.extensions.ERXModelGroup.useExtendedPrototypes=true</code>.
+	 */
+	@Override
+	public NSArray availablePrototypeAttributeNames() {
+		synchronized (_EOGlobalModelLock) {
+			if (_prototypesByName == null && useExtendedPrototypes()) {
+				createPrototypes(this);
+			}
+		}
+		return super.availablePrototypeAttributeNames();
+	}
+	
+	/**
+	 * Overridden to use our prototype creation method if
+	 * <code>er.extensions.ERXModel.useExtendedPrototypes=true</code>.
+	 */
+	@Override
+	public EOAttribute prototypeAttributeNamed(String name) {
+		synchronized (_EOGlobalModelLock) {
+			if (_prototypesByName == null && useExtendedPrototypes()) {
+				createPrototypes(this);
+			}
+		}
+		return super.prototypeAttributeNamed(name);
+	}
+	
+	@Override
+	public void setModelGroup(EOModelGroup modelGroup) {
+		super.setModelGroup(modelGroup);
+		if (modelGroup instanceof ERXModelGroup) {
+			((ERXModelGroup) modelGroup).resetConnectionDictionaryInModel(this);
+		}
+	}
+	
+	/**
+	 * Defaults to false.
+	 * @return the boolean property value for <code>er.extensions.ERXModel.useExtendedPrototypes</code>.
+	 */
+	protected boolean useExtendedPrototypes() {
+		return ERXProperties.booleanForKeyWithDefault("er.extensions.ERXModel.useExtendedPrototypes", false);
 	}
 
 }
