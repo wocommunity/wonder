90aa809ce401b3f01f3783d06b1bbc17afa295f7
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-03-23 14:16:56 +0000

    properly parser "3/22 10pm" -- times were not being handled properly

--- Frameworks/Misc/ERChronic/Sources/er/chronic/handlers/SmSdHandler.java
+++ Frameworks/Misc/ERChronic/Sources/er/chronic/handlers/SmSdHandler.java
@@ -14,9 +14,21 @@ public class SmSdHandler implements IHandler {
   public Span handle(List<Token> tokens, Options options) {
     int month = tokens.get(0).getTag(ScalarMonth.class).getType().intValue();
     int day = tokens.get(1).getTag(ScalarDay.class).getType().intValue();
-    Calendar start = Time.construct(options.getNow().get(Calendar.YEAR), month, day);
-    Calendar end = Time.cloneAndAdd(start, Calendar.DAY_OF_MONTH, 1);
-    return new Span(start, end);
+
+    // MS: properly parse time in this format
+    Span span;
+    try {
+      List<Token> timeTokens = tokens.subList(2, tokens.size());
+      Calendar dayStart = Time.construct(options.getNow().get(Calendar.YEAR), month, day);
+      span = Handler.dayOrTime(dayStart, timeTokens, options);
+    }
+    catch (IllegalArgumentException e) {
+      if (options.isDebug()) {
+        e.printStackTrace(System.out);
+      }
+      span = null;
+    }
+    return span;
   }
 
 }
