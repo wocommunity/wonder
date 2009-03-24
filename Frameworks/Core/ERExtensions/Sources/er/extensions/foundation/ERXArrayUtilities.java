c4728867a1a2640e0dc695eaf6afd8efc5762566
qdolan <qdolan@8dc76679-1c23-de11-85cd-0050568e728b>
2009-03-24 06:01:48 +0000

    Performance improvement to arrayByAddingObjectsFromArrayWithoutDuplcates when using large arrays.

--- Frameworks/Core/ERExtensions/Sources/er/extensions/foundation/ERXArrayUtilities.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/foundation/ERXArrayUtilities.java
@@ -610,15 +610,18 @@ public class ERXArrayUtilities extends Object {
      *		the second array.
      */
     public static NSArray arrayByAddingObjectsFromArrayWithoutDuplicates(NSArray a1, NSArray a2) {
-        // FIXME this is n2 -- could be made n lg n
         NSArray result=null;
         if (a2.count()==0)
             result=a1;
         else {
             NSMutableArray mutableResult=new NSMutableArray(a1);
+            NSMutableSet resultSet=new NSMutableSet(a1);
             for (Enumeration e=a2.objectEnumerator(); e.hasMoreElements();) {
                 Object elt=e.nextElement();
-                if (!mutableResult.containsObject(elt)) mutableResult.addObject(elt);
+                if (!resultSet.containsObject(elt)) { 
+                	mutableResult.addObject(elt);
+                	resultSet.addObject(elt);
+                }
             }
             result=mutableResult;
         }
