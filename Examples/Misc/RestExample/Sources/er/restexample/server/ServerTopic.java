5cfef1bed98882925811ccb1ea4be06e92a4f432
thoepfner <thoepfner@8dc76679-1c23-de11-85cd-0050568e728b>
2009-04-23 10:40:05 +0000

    Adapted to refactored ERRest (untested, but compiles again)

--- Examples/Misc/RestExample/Sources/er/restexample/server/ServerTopic.java
+++ Examples/Misc/RestExample/Sources/er/restexample/server/ServerTopic.java
@@ -3,5 +3,6 @@ package er.restexample.server;
 import org.apache.log4j.Logger;
 
 public class ServerTopic extends _ServerTopic {
-  private static Logger log = Logger.getLogger(ServerTopic.class);
+  @SuppressWarnings("unused")
+private static Logger log = Logger.getLogger(ServerTopic.class);
 }
