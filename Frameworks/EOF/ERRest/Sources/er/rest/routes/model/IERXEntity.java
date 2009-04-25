cb291a7d7d7d92977baf73db0448e568cc9de8e5
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-04-25 21:09:46 +0000

    if the entity object is a Class, then just return a class proxy for it

--- Frameworks/EOF/ERRest/Sources/er/rest/routes/model/IERXEntity.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/routes/model/IERXEntity.java
@@ -15,15 +15,15 @@ public interface IERXEntity {
 
 	public static class Factory {
 		private static Map<String, IERXEntity> _entityRegistry = new ConcurrentHashMap<String, IERXEntity>();
-		
+
 		public static void registerEntityNamed(IERXEntity entity, String name) {
 			_entityRegistry.put(name, entity);
 		}
-		
+
 		public static IERXEntity registeredEntityNamed(String name) {
 			return _entityRegistry.get(name);
 		}
-		
+
 		public static IERXEntity entityNamed(EOEditingContext editingContext, String name) {
 			IERXEntity entity = IERXEntity.Factory.registeredEntityNamed(name);
 			if (entity == null) {
@@ -72,7 +72,13 @@ public interface IERXEntity {
 				entity = new EOEntityProxy(ERXEOAccessUtilities.entityForEo((EOEnterpriseObject) obj));
 			}
 			else {
-				Class clazz = obj.getClass();
+				Class clazz;
+				if (obj instanceof Class) {
+					clazz = (Class) obj;
+				}
+				else {
+					clazz = obj.getClass();
+				}
 				entity = IERXEntity.Factory.registeredEntityNamed(clazz.getName());
 				if (entity == null) {
 					entity = IERXEntity.Factory.registeredEntityNamed(clazz.getSimpleName());
@@ -88,9 +94,9 @@ public interface IERXEntity {
 	public String name();
 
 	public String shortName();
-	
+
 	public IERXEntity parentEntity();
-	
+
 	public IERXRelationship relationshipNamed(String name);
 
 	public IERXAttribute attributeNamed(String name);
@@ -106,10 +112,10 @@ public interface IERXEntity {
 	public NSArray<IERXRelationship> relationships();
 
 	public IERXEntity siblingEntityNamed(String name);
-	
+
 	public Object primaryKeyValue(Object obj);
-	
+
 	public Object createInstance(EOEditingContext editingContext);
-	
+
 	public Object objectWithPrimaryKeyValue(EOEditingContext editingContext, Object pkValue);
 }
