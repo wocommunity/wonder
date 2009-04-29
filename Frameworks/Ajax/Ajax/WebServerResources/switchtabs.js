65a65390e5d7301294ae15013fb19f44d5f1abde
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2009-04-29 23:58:00 +0000

    Flicker prevention when loading panel contents and onSelect script used

--- Frameworks/Ajax/Ajax/WebServerResources/switchtabs.js
+++ Frameworks/Ajax/Ajax/WebServerResources/switchtabs.js
@@ -7,7 +7,7 @@ var AjaxTabbedPanel = {
 	},
 	
     // Change which tab appears in selected state
-    selectTab : function(tabControlID, selectedTabID) {
+    selectTab : function(tabControlID, selectedTabID, paneID, busyDivID) {
       var selectedTab = $(selectedTabID);
       var tablist = this.getChildrenByTagName($(tabControlID), 'li');
       var nodes = $A(tablist);
@@ -19,7 +19,12 @@ var AjaxTabbedPanel = {
           }
       });
       selectedTab.removeClassName('ajaxTabbedPanelTab-unselected').addClassName('ajaxTabbedPanelTab-selected');
-      AjaxTabbedPanel.runOnSelect($(tabControlID)); 
+      
+      // Only call runOnSelect if the panel contents have previously been loaded.
+      // If the panel contents are getting loaded the loadPanel function will call this
+      var pane = $(paneID);
+      if (pane.innerHTML!='' && pane.innerHTML!=busyContent(busyDivID))
+          AjaxTabbedPanel.runOnSelect($(tabControlID)); 
     },
 
     // Change which panel appears
@@ -46,16 +51,9 @@ var AjaxTabbedPanel = {
 
     // Loads the panel contents if not already loaded
     loadPanel : function(tabControlID, paneID, busyDivID, shouldReload) {
-
-      // Determine what to show if the panel takes a while to  load
-      var busyContent = 'Loading, please wait...';
-      if (busyDivID != '') {
-          busyContent = $('busydiv').innerHTML;
-      }
-
       var pane = $(paneID);
-      if (pane.innerHTML=='' || pane.innerHTML==busyContent || shouldReload) {
-         pe = new PeriodicalExecuter(function(pe) { pane.innerHTML=busyContent; pe.stop()}, 0.25);
+      if (pane.innerHTML=='' || pane.innerHTML==busyContent(busyDivID) || shouldReload) {
+         pe = new PeriodicalExecuter(function(pe) { pane.innerHTML=busyContent(busyDivID); pe.stop()}, 0.25);
          new Ajax.Updater(pane, pane.getAttribute('updateUrl'), {asynchronous: 1, 
          														 evalScripts: true, 
          														 onComplete: function(a, b) {pe.stop(); 
@@ -78,6 +76,15 @@ var AjaxTabbedPanel = {
 		}
     },
         
+    // Determines what to show if the panel takes a while to  load  
+    busyContent : function(busyDivID) {
+      var busyContent = 'Loading, please wait...';
+      if (busyDivID != '') {
+          busyContent = $(busyDivID).innerHTML;
+      }
+      return busyContent;
+    },   
+    
     // Returns an element's children that have a specific tag name as an array
     getChildrenByTagName : function(element, tag_name) {
         child_array      = new Array();
