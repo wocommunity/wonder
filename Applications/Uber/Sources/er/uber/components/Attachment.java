1645dadd9259acb5037aaa5fa4a5e85fb0757a5e
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-05-19 00:59:45 +0000

    support for clearing example attachments + use of the new width/height configuration to thumbnail down to 50px

--- Applications/Uber/Sources/er/uber/components/Attachment.java
+++ Applications/Uber/Sources/er/uber/components/Attachment.java
@@ -3,28 +3,41 @@ package er.uber.components;
 import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WODisplayGroup;
+import com.webobjects.eocontrol.EOEditingContext;
 
+import er.attachment.model.ERAttachment;
+import er.extensions.eof.ERXEC;
 import er.uber.model.Company;
 
 public class Attachment extends UberComponent {
-  public WODisplayGroup _companies;
-  public Company _company;
+	public WODisplayGroup _companies;
+	public Company _company;
 
-  public Attachment(WOContext context) {
-    super(context);
-  }
+	public Attachment(WOContext context) {
+		super(context);
+	}
 
-  public Company selectedCompany() {
-    return (Company) _companies.selectedObject();
-  }
+	public Company selectedCompany() {
+		return (Company) _companies.selectedObject();
+	}
 
-  public WOActionResults selectCompany() {
-    _companies.setSelectedObject(_company);
-    return null;
-  }
+	public WOActionResults selectCompany() {
+		_companies.setSelectedObject(_company);
+		return null;
+	}
 
-  public WOActionResults save() {
-    editingContext().saveChanges();
-    return null;
-  }
+	public WOActionResults clearAttachment() {
+		EOEditingContext editingContext = ERXEC.newEditingContext();
+		Company localCompany = selectedCompany().localInstanceIn(editingContext);
+		ERAttachment attachment = localCompany.logo();
+		attachment.delete();
+		localCompany.setLogo(null);
+		editingContext.saveChanges();
+		return null;
+	}
+
+	public WOActionResults save() {
+		editingContext().saveChanges();
+		return null;
+	}
 }
