44dc8411c9ed76fe7c4977adbd6d59f781f89114
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2009-05-21 06:36:00 +0000

    cleanup, removed one dict, javadoc

--- Frameworks/Core/ERExtensions/Sources/er/extensions/components/_private/ERXSwitchComponent.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/components/_private/ERXSwitchComponent.java
@@ -14,86 +14,79 @@ import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableDictionary;
 
 /**
- * Variant of WOSwitchComponent that replaces the plain-text names in the
- * element ID  with a number, in case you don't like advertising your
- * component names or like shorter URLs.
- * 
+ * Variant of WOSwitchComponent that replaces the plain-text component names in
+ * the element ID with a number, in case you don't like advertising your
+ * component names or just like shorter URLs.<br>
+ * Gets installed automatically by ERXPatcher.
  * @author ak
  */
 public class ERXSwitchComponent extends WODynamicElement {
 	WOAssociation componentName;
 	public NSMutableDictionary componentAttributes;
 	NSMutableDictionary componentCache;
+	private NSMutableDictionary<String, String> elementIDByName;
 	WOElement template;
 
 	public ERXSwitchComponent(String paramString, NSDictionary paramNSDictionary, WOElement paramWOElement) {
 		super(null, null, null);
-		this.componentName = ((WOAssociation) paramNSDictionary.objectForKey("WOComponentName"));
-		if (this.componentName == null) {
-			this.componentName = ((WOAssociation) paramNSDictionary.objectForKey("_componentName"));
-			if (this.componentName == null)
+		componentName = ((WOAssociation) paramNSDictionary.objectForKey("WOComponentName"));
+		if (componentName == null) {
+			componentName = ((WOAssociation) paramNSDictionary.objectForKey("_componentName"));
+			if (componentName == null)
 				throw new WODynamicElementCreationException("<" + getClass().getName() + "> : '" + "WOComponentName" + "' attribute missing.");
 		}
 
-		if (paramNSDictionary != null) {
-			this.componentAttributes = paramNSDictionary.mutableClone();
-			this.componentAttributes.removeObjectForKey("WOComponentName");
-			this.componentAttributes.removeObjectForKey("_componentName");
-		}
-		else {
-			this.componentAttributes = new NSMutableDictionary();
-		}
-
-		this.componentCache = new NSMutableDictionary();
-
-		this.template = paramWOElement;
+		componentAttributes = paramNSDictionary.mutableClone();
+		componentAttributes.removeObjectForKey("WOComponentName");
+		componentAttributes.removeObjectForKey("_componentName");
+		componentCache = new NSMutableDictionary();
+		template = paramWOElement;
+		elementIDByName = new NSMutableDictionary<String, String>();
 	}
 
 	public String toString() {
-		return "<ERXSwitchComponent  componentName: " + componentName 
+		return "<" + getClass().getName() + " componentName: " + componentName 
 		+ " componentAttributes: " + componentAttributes 
 		+ " componentCache: " + componentCache + " children: " + template + ">";
 	}
 
-	private NSMutableDictionary<String, String> _namesByID = new NSMutableDictionary<String, String>();
-	private NSMutableDictionary<String, String> _idsByName = new NSMutableDictionary<String, String>();
-
-	public String _elementNameInContext(WOContext paramWOContext) {
-		WOComponent localWOComponent = paramWOContext.component();
+	private String componentNameInContext(WOComponent localWOComponent) {
 		String name = null;
-		Object localObject = this.componentName.valueInComponent(localWOComponent);
+		Object localObject = componentName.valueInComponent(localWOComponent);
 		if (localObject != null)
 			name = localObject.toString();
 
 		if ((name == null) || (name.length() == 0)) {
 			throw new IllegalStateException("<" + getClass().getName() + "> : componentName not specified or componentName association evaluated to null.");
 		}
+		return name;
+	}
+
+	public String _elementNameInContext(String name, WOContext paramWOContext) {
 		synchronized (this) {
-			String id = _idsByName.objectForKey(name);
+			String id = elementIDByName.objectForKey(name);
 			if (id == null) {
-				id = _namesByID.count() + "";
-				_idsByName.setObjectForKey(id, name);
-				_namesByID.setObjectForKey(name, id);
+				id = elementIDByName.count() + "";
+				elementIDByName.setObjectForKey(id, name);
 			}
 			name = id;
 		}
 		return name;
 	}
 
-	public WOElement _realComponentWithName(String paramString, WOContext paramWOContext) {
+	public WOElement _realComponentWithName(String name, String elementID, WOContext paramWOContext) {
 		WOElement localWOElement;
 		synchronized (this) {
-			paramString = _namesByID.objectForKey(paramString);
-			localWOElement = (WOElement) this.componentCache.objectForKey(paramString);
+			localWOElement = (WOElement) componentCache.objectForKey(elementID);
 
 			if (localWOElement == null) {
-				localWOElement = WOApplication.application().dynamicElementWithName(paramString, this.componentAttributes, this.template, paramWOContext._languages());
+				localWOElement = WOApplication.application().dynamicElementWithName(name, componentAttributes, template, paramWOContext._languages());
 
 				if (localWOElement == null) {
-					throw new WODynamicElementCreationException("<" + getClass().getName() + "> : cannot find component or dynamic element named " + paramString);
+					throw new WODynamicElementCreationException("<" + getClass().getName() + "> : cannot find component or dynamic element named " + elementID);
 				}
 
-				this.componentCache.setObjectForKey(localWOElement, paramString);
+				componentCache.setObjectForKey(localWOElement, elementID);
 			}
 		}
 
@@ -101,11 +94,12 @@ public class ERXSwitchComponent extends WODynamicElement {
 	}
 
 	public void takeValuesFromRequest(WORequest paramWORequest, WOContext paramWOContext) {
-		String str = _elementNameInContext(paramWOContext);
+		String name = componentNameInContext(paramWOContext.component());
+		String id = _elementNameInContext(name, paramWOContext);
 
-		paramWOContext.appendElementIDComponent(str);
+		paramWOContext.appendElementIDComponent(id);
 
-		WOElement localWOElement = _realComponentWithName(str, paramWOContext);
+		WOElement localWOElement = _realComponentWithName(name, id, paramWOContext);
 
 		localWOElement.takeValuesFromRequest(paramWORequest, paramWOContext);
 
@@ -113,11 +107,12 @@ public class ERXSwitchComponent extends WODynamicElement {
 	}
 
 	public WOActionResults invokeAction(WORequest paramWORequest, WOContext paramWOContext) {
-		String str = _elementNameInContext(paramWOContext);
+		String name = componentNameInContext(paramWOContext.component());
+		String id = _elementNameInContext(name, paramWOContext);
 
-		paramWOContext.appendElementIDComponent(str);
+		paramWOContext.appendElementIDComponent(id);
 
-		WOElement localWOElement = _realComponentWithName(str, paramWOContext);
+		WOElement localWOElement = _realComponentWithName(name, id, paramWOContext);
 
 		WOActionResults localWOActionResults = localWOElement.invokeAction(paramWORequest, paramWOContext);
 
@@ -127,11 +122,12 @@ public class ERXSwitchComponent extends WODynamicElement {
 	}
 
 	public void appendToResponse(WOResponse paramWOResponse, WOContext paramWOContext) {
-		String str = _elementNameInContext(paramWOContext);
+		String name = componentNameInContext(paramWOContext.component());
+		String id = _elementNameInContext(name, paramWOContext);
 
-		paramWOContext.appendElementIDComponent(str);
+		paramWOContext.appendElementIDComponent(id);
 
-		WOElement localWOElement = _realComponentWithName(str, paramWOContext);
+		WOElement localWOElement = _realComponentWithName(name, id, paramWOContext);
 
 		localWOElement.appendToResponse(paramWOResponse, paramWOContext);
 
