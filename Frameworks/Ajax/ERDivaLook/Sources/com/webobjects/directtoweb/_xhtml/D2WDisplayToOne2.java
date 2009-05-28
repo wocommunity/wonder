e73b40c8bc14dd7f26765cb686ad76f199dda597
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-05-28 13:14:06 +0000

    Add support for noSelectionString

--- Frameworks/Ajax/ERDivaLook/Sources/com/webobjects/directtoweb/_xhtml/D2WDisplayToOne2.java
+++ Frameworks/Ajax/ERDivaLook/Sources/com/webobjects/directtoweb/_xhtml/D2WDisplayToOne2.java
@@ -2,7 +2,9 @@ package com.webobjects.directtoweb._xhtml;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.directtoweb.D2WDisplayToOne;
+
 import er.extensions.foundation.ERXStringUtilities;
+import er.extensions.localization.ERXLocalizer;
 
 public class D2WDisplayToOne2 extends D2WDisplayToOne {
     public D2WDisplayToOne2(WOContext context) {
@@ -13,5 +15,11 @@ public class D2WDisplayToOne2 extends D2WDisplayToOne {
     public String classString() {
     	String classString = (String) d2wContext().valueForKey("class");
     	return classString != null ? ERXStringUtilities.safeIdentifierName(classString) : null;
+    }
+    
+    @Override
+    public Object toOneDescription() {
+        Object description = super.toOneDescription();
+        return description != null ? description : ERXLocalizer.currentLocalizer().localizedStringForKeyWithDefault((String) d2wContext().valueForKey("noSelectionString"));
     }
 }
