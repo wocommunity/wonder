a0a09b2a105a728eb64c1b04c7ee0a9b4ce714a6
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-05-29 12:22:19 +0000

    WONDER-296  ERXDateGrouper's setSelectedDate needs to have a broader argument class

--- Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXDateGrouper.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXDateGrouper.java
@@ -1,5 +1,6 @@
 package er.extensions.components;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Enumeration;
 
 import org.apache.log4j.Logger;
@@ -107,7 +108,8 @@ public class ERXDateGrouper extends WODisplayGroup {
         _datesForWeeksForCurrentMonth = null;
     }
     public NSTimestamp selectedDate() { return _selectedDate == null ? today() : _selectedDate; }
-    public void setSelectedDate(NSTimestamp value) {
+    public void setSelectedDate(Date date) {
+    	NSTimestamp value = (date instanceof NSTimestamp)?(NSTimestamp)date:new NSTimestamp(date);
         _reset();
         _selectedDate = value;
     }
