f6c3d39ff420143f776460ec7c3c89e617a96eb5
qdolan <qdolan@8dc76679-1c23-de11-85cd-0050568e728b>
2009-06-06 16:40:42 +0000

    and the rest of the files would help

---
+++ Frameworks/Misc/ERPDFGeneration/Sources/er/pdf/builder/UJACImpl.java
@@ -0,0 +1,35 @@
+package er.pdf.builder;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ujac.print.DocumentPrinter;
+import org.ujac.util.io.HttpResourceLoader;
+
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSForwardException;
+
+public class UJACImpl implements PDFBuilder {
+  private DocumentPrinter documentPrinter;
+  private Map<String, String> documentProperties = new HashMap<String, String>();
+  
+  public void createDocument(OutputStream os) {
+    try {
+      documentPrinter.printDocument(os);
+    } catch (Exception e) {
+      NSForwardException._runtimeExceptionForThrowable(e);
+    }
+  }
+
+  public void setSource(String document, String encoding, String urlPrefix, NSDictionary<String, Object> configuration) throws UnsupportedEncodingException {
+    InputStream is = new ByteArrayInputStream(document.getBytes(encoding));
+    documentPrinter = new DocumentPrinter(is, documentProperties);
+    documentPrinter.setResourceLoader(new HttpResourceLoader(urlPrefix));    
+  }
+
+  
+}
---
+++ Frameworks/Misc/ERPDFGeneration/Sources/er/pdf/components/UJACResource.java
@@ -0,0 +1,54 @@
+package er.pdf.components;
+
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.appserver.WOAssociation;
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WOResourceManager;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.appserver._private.WODynamicElementCreationException;
+import com.webobjects.appserver._private.WOHTMLDynamicElement;
+import com.webobjects.foundation.NSDictionary;
+
+/**
+ * UJAC resource component with resource manager support
+ * @binding href href to the icon
+ * @binding filename filename of the icon
+ * @binding framework framework of the icon
+ * @author q
+ */
+public abstract class UJACResource extends WOHTMLDynamicElement {
+
+  protected WOAssociation _source;
+  protected WOAssociation _framework;
+
+  public UJACResource(String name, NSDictionary<String, WOAssociation> associations, WOElement template) {
+    super(name, associations, template);
+    _source = (WOAssociation) _associations.removeObjectForKey("source");
+    _framework = (WOAssociation) _associations.removeObjectForKey("framework");
+    if(_source == null) {
+      throw new WODynamicElementCreationException("'source' must be bound: " + this);
+    }
+  }
+
+  public void appendAttributesToResponse(WOResponse response, WOContext context) {
+    WOComponent component = context.component();
+    String href = (String)_source.valueInComponent(component);
+
+    String framework = "app";
+    if(_framework != null) {
+      Object val = _framework.valueInComponent(component);
+      if(val != null) {
+        framework = val.toString();
+      }
+    }
+    
+    WOResourceManager rs = WOApplication.application().resourceManager();
+    if (!href.startsWith("http://") && !href.startsWith("https://"))
+      href = rs.urlForResourceNamed(href, framework, null, context.request());
+    response._appendTagAttributeAndValue("source", href, false);
+    super.appendAttributesToResponse(response, context);
+  }
+  
+}
