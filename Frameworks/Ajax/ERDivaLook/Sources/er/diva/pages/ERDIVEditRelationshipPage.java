d0b3039840c2e1e677afe7fee5fb2382452546aa
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-06-11 12:44:36 +0000

    Reverting use of ERXStylesheet in the main pages temporarily - to avoid a bug where the CSS is added twice sometimes

--- Frameworks/Ajax/ERDivaLook/Sources/er/diva/pages/ERDIVEditRelationshipPage.java
+++ Frameworks/Ajax/ERDivaLook/Sources/er/diva/pages/ERDIVEditRelationshipPage.java
@@ -96,4 +96,15 @@ public class ERDIVEditRelationshipPage extends ERD2WEditRelationshipPage impleme
         
         return flag;
     }
+    
+    // R/R
+    @Override
+	public void appendToResponse(WOResponse response, WOContext context) {
+    	super.appendToResponse(response, context);
+
+    	// add page style sheet
+    	if (stylesheet() != null) {
+    		AjaxUtils.addStylesheetResourceInHead(context, response, "app", stylesheet());
+    	}
+    }
 }
