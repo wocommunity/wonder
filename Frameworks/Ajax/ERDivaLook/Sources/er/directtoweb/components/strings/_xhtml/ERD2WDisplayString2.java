a7407e6c4c2ac86895e43f017c7f712b0d6cc893
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-07-03 12:24:38 +0000

    Deprecating ERD2WDisplayString2 in favour of updated ERD2WDisplayString

--- Frameworks/Ajax/ERDivaLook/Sources/er/directtoweb/components/strings/_xhtml/ERD2WDisplayString2.java
+++ Frameworks/Ajax/ERDivaLook/Sources/er/directtoweb/components/strings/_xhtml/ERD2WDisplayString2.java
@@ -3,16 +3,15 @@ package er.directtoweb.components.strings._xhtml;
 import com.webobjects.appserver.WOContext;
 
 import er.directtoweb.components.strings.ERD2WDisplayString;
-import er.extensions.foundation.ERXStringUtilities;
 
+/**
+ * @Deprecated in favour of parent component (with added bindings)
+ * @author mendis
+ *
+ */
+@Deprecated
 public class ERD2WDisplayString2 extends ERD2WDisplayString {
     public ERD2WDisplayString2(WOContext context) {
         super(context);
-    }
-    
-    // accessors 
-    public String classString() {
-    	Object classValue = d2wContext().valueForKey("class");
-    	return classValue != null ? ERXStringUtilities.safeIdentifierName(classValue.toString()) : null;
     }
 }
