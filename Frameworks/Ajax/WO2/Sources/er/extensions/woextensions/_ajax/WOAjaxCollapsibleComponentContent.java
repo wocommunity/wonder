6420197960b04de72b465789dda92e31b6d833d1
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-07-23 10:14:19 +0000

    Adding ajax version of collapsible (and removing XX2 version)

---
+++ Frameworks/Ajax/WO2/Sources/er/extensions/woextensions/_ajax/WOAjaxCollapsibleComponentContent.java
@@ -0,0 +1,32 @@
+package er.extensions.woextensions._ajax;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.woextensions.WOCollapsibleComponentContent;
+
+/**
+ * Ajax/XHTML collapsible component
+ * @see WOCollapsibleComponentContent
+ * 
+ * @binding container
+ * 
+ * @author dchonen
+ */
+public class WOAjaxCollapsibleComponentContent extends WOCollapsibleComponentContent {
+
+	
+    public WOAjaxCollapsibleComponentContent(WOContext context) {
+        super(context);
+    }
+    
+    public String toggleButtonClass() {
+    	return isVisible() ? "open" : "closed";
+    }
+    
+    // actions (ajax)
+    public WOComponent toggleVisibilityClicked()  {
+        super.toggleVisibilityClicked();
+        return this;
+    }
+}
