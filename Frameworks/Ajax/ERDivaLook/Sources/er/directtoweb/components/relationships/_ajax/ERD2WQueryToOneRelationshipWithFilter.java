0b908f2f86f5639cc4684f0524c9e41278db1d84
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-07-28 15:58:29 +0000

    Adding query to one filter dynamic D2W component + optimization and better support for ajax D2W components

---
+++ Frameworks/Ajax/ERDivaLook/Sources/er/directtoweb/components/relationships/_ajax/ERD2WQueryToOneRelationshipWithFilter.java
@@ -0,0 +1,58 @@
+package er.directtoweb.components.relationships._ajax;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2WCustomQueryComponent;
+
+import er.directtoweb.components.relationships._xhtml.ERD2WQueryToOneRelationship2;
+import er.extensions.foundation.ERXValueUtilities;
+
+/**
+ * Adds a filter for the select list.
+ * How to use: Set a delayed boolean assignment on restrictedChoiceKey or restrictedFetchSpecification based on the condition isFiltered
+ * 
+ * @see ERD2WQueryToOneRelationship2
+ * 
+ * 
+ * @author mendis
+ *
+ */
+public class ERD2WQueryToOneRelationshipWithFilter extends D2WCustomQueryComponent {
+	
+	public ERD2WQueryToOneRelationshipWithFilter(WOContext context) {
+		super(context);
+	}
+	
+	public String filterKey() {
+		return (String) d2wContext().valueForKey("filterKey");
+	}
+
+	private boolean isFiltered() {
+    	return ERXValueUtilities.booleanValue(d2wContext().valueForKey(filterKey()));
+	}
+	
+	private void setIsFiltered(boolean flag) {
+		d2wContext().takeValueForKey(flag, filterKey());
+	}
+	
+	public String container() {
+		return d2wContext().valueForKey("id") + "_container";
+	} 
+	
+	/*
+	 * button
+	 */
+	public String value() {
+		return isFiltered() ? "Show All" : "Filter";
+	}
+	
+	public String buttonName() {
+		return d2wContext().valueForKey("name") + "_filter_button";
+	}
+
+	// actions (ajax)
+	public WOComponent toggleFilter() {
+		setIsFiltered(!isFiltered());
+		return this;
+	}
+}
