05a701205fe7b6b4cfc9c558c72a80256591f7b0
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-07-29 09:29:44 +0000

    FIXME note

--- Frameworks/Ajax/WO2/Sources/er/extensions/components/_xhtml/ERXPluralString2.java
+++ Frameworks/Ajax/WO2/Sources/er/extensions/components/_xhtml/ERXPluralString2.java
@@ -3,6 +3,7 @@ package er.extensions.components._xhtml;
 import com.webobjects.appserver.WOContext;
 import er.extensions.components.ERXPluralString;
 
+// FIXME: RM: try to consolidate with parent component ERXPluralString
 public class ERXPluralString2 extends ERXPluralString {
     public ERXPluralString2(WOContext context) {
         super(context);
