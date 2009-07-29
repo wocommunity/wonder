0fcea3aadff4db07d1da1f103fc8d80bee1d7fe8
kiddyr <kiddyr@8dc76679-1c23-de11-85cd-0050568e728b>
2009-07-29 21:11:49 +0000

    Changes to ant build system to enable testing. See http://wiki.objectstyle.org/confluence/display/WONDER/Testing

---
+++ Frameworks/Core/ERExtensions/Tests/er/extensions/statistics/ERXMetricsTest.java
@@ -0,0 +1,15 @@
+
+package er.extensions.statistics;
+
+import junit.framework.TestCase;
+
+public class ERXMetricsTest extends TestCase {
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  public void testEvents() {
+  }
+}
