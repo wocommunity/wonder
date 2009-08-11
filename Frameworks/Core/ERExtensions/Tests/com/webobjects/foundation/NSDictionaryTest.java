0ad6bc76186f95413be99d1528b3434dd828fd8e
qdolan <qdolan@8dc76679-1c23-de11-85cd-0050568e728b>
2009-08-11 08:12:39 +0000

    Added unit tests for WO foundation classes

---
+++ Frameworks/Core/ERExtensions/Tests/com/webobjects/foundation/NSDictionaryTest.java
@@ -0,0 +1,345 @@
+package com.webobjects.foundation;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSKeyValueCoding;
+import com.webobjects.foundation.NSMutableDictionary;
+
+public class NSDictionaryTest extends BaseTestCase {
+
+	public void testNSDictionary() {
+		NSDictionary<Object, Object> dict = new NSDictionary<Object, Object>();
+		assertTrue(dict.isEmpty());
+	}
+
+	 public void testNSDictionaryIsImmutable() {
+	    NSDictionary<Object, Object> dict = new NSDictionary<Object, Object>();
+	    try {
+	      dict.put("abc", "def");
+	      fail("NSDictionary is not immutable");
+	    } catch (UnsupportedOperationException e) {
+	    }
+	 }
+	
+	public void testNSDictionaryMapOfKV() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key", "value");
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(map);
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testNSDictionaryMapOfKVNull() {
+	  try {
+	    new NSDictionary<String, String>((Map<String, String>)null);
+	    fail("NullPointerException expected");
+	  } catch (NullPointerException e) {
+	  }
+	}
+
+	public void testNSDictionaryMapOfKVInvalid() {
+	  Map<String, String> map = new HashMap<String, String>();
+	  map.put("key", null);
+	  try {
+	    new NSDictionary<String, String>(map);
+	    fail("IllegalArgumentException expected");
+	  } catch (IllegalArgumentException e) {
+	  }
+	}
+
+
+	public void testNSDictionaryNSArrayOfVNSArrayOfK() {
+		NSArray<String> keys = new NSArray<String>(new String[] { "key1", "key2" });
+		NSArray<String> values = new NSArray<String>(new String[] { "value1", "value2" });
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		assertEquals("value1", dict.get("key1"));
+		assertEquals("value2", dict.get("key2"));
+	}
+	
+	 public void testNSDictionaryNSArrayOfVNSArrayOfKNull() {
+	    NSArray<String> array = new NSArray<String>(new String[] { "value1", "value2" });
+	    try {
+	      new NSDictionary<String, String>((NSArray<String>)null, array);
+	    } catch (IllegalArgumentException e) {
+	    }
+      try {
+        new NSDictionary<String, String>(array, (NSArray<String>)null);
+      } catch (IllegalArgumentException e) {
+      }
+      try {
+        new NSDictionary<String, String>((NSArray<String>)null, (NSArray<String>)null);
+      } catch (IllegalArgumentException e) {
+      }
+	  }
+
+	public void testNSDictionaryNSDictionaryOfKV() {
+		NSMutableDictionary<String, String> mutableDict = new NSMutableDictionary<String, String>("value", "key");
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(mutableDict);
+		NSDictionary<String, String> dict2 = new NSDictionary<String, String>(dict);
+		assertEquals(mutableDict, dict);
+    assertEquals(mutableDict, dict2);
+		mutableDict.put("key", "newValue");
+		assertEquals("value", dict.get("key"));
+    assertEquals("value", dict2.get("key"));
+	}
+	
+	public void testNSDictionaryNSDictionaryOfKVNull() {
+	  try {
+	    new NSDictionary<String, String>((NSDictionary<String, String>) null);
+	    fail("NullPointerException expected");
+	  } catch (NullPointerException e) {
+	  }
+  }
+
+	public void testNSDictionaryVArrayKArray() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		assertEquals(2, dict.size());
+		assertEquals("value1", dict.get("key1"));
+		assertEquals("value2", dict.get("key2"));
+	}
+
+	public void testNSDictionaryVArrayKArrayNull() {
+    String[] values = new String[] {"value1", "value2"};
+    try {
+      new NSDictionary<String, String>((String[])null, values);
+      fail("IllegalArgumentException expected");
+    } catch (IllegalArgumentException e) {
+    }
+    
+    try {
+      new NSDictionary<String, String>(values, (String[])null);
+      fail("IllegalArgumentException expected");
+    } catch (IllegalArgumentException e) {
+    }
+
+    NSDictionary<String, String> dict = new NSDictionary<String, String>((String[])null, (String[])null);
+    assertTrue(dict.isEmpty());
+	}
+
+	
+	public void testNSDictionaryVK() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("value", "key");
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testNSDictionaryVKNull() {
+	  try {
+	    new NSDictionary<String, String>((String) null, "key");
+	    fail("IllegalArgumentException expected");
+	  } catch (IllegalArgumentException e) {
+	  }
+	  
+    try {
+      new NSDictionary<String, String>("value", (String) null);
+      fail("IllegalArgumentException expected");
+    } catch (IllegalArgumentException e) {
+    }
+	  
+    try {
+      new NSDictionary<String, String>((String) null, (String) null);
+      fail("IllegalArgumentException expected");
+    } catch (IllegalArgumentException e) {
+    }
+	}
+
+	
+	public void testAllKeys() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSArray<String> keyArray = dict.allKeys();
+		assertEquals(2, keyArray.size());
+		assertTrue(keyArray.contains("key1"));
+		assertTrue(keyArray.contains("key2"));
+	}
+
+	public void testAllKeysForObject() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value", "value"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSArray<String> keyArray = dict.allKeysForObject("value");
+		assertEquals(2, keyArray.size());
+		assertTrue(keyArray.contains("key1"));
+		assertTrue(keyArray.contains("key2"));
+	}
+	
+	public void testObjectsForKeys() {
+    String[] keys = new String[] {"key1", "key2"};
+    String[] values = new String[] {"value1", "value2"};
+    NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+    NSArray<String> objectArray = dict.objectsForKeys(new NSArray<String>(new String[] { "key1", "key2", "key3" }), "null");
+    assertEquals(3, objectArray.size());
+    assertTrue(objectArray.contains("value1"));
+    assertTrue(objectArray.contains("value2"));
+    assertTrue(objectArray.contains("null"));
+	}
+
+	public void testAllValues() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSArray<String> keyArray = dict.allValues();
+		assertEquals(2, keyArray.size());
+		assertTrue(keyArray.contains("value1"));
+		assertTrue(keyArray.contains("value2"));
+	}
+
+	public void testCount() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		assertEquals(2, dict.count());
+	}
+
+	public void testEmptyDictionary() {
+		assertTrue(NSDictionary.EmptyDictionary.isEmpty());
+		assertTrue(NSDictionary.emptyDictionary().isEmpty());
+	}
+
+	public void testHashMap() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		Map<String, String> hashMap = dict.hashMap();
+		assertEquals(2, hashMap.size());
+		assertEquals("value1", hashMap.get("key1"));
+		assertEquals("value2", dict.get("key2"));
+	}
+
+	public void testIsEqualToDictionary() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSDictionary<String, String> dict2 = new NSDictionary<String, String>(values, keys);
+		assertTrue(dict.isEqualToDictionary(dict2));
+	}
+
+	public void testImmutableClone() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSDictionary<String, String> clone = dict.immutableClone();
+		
+		assertEquals(NSDictionary.class, clone.getClass());
+		assertEquals(clone, dict);
+	}
+
+	public void testMutableClone() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSDictionary<String, String> clone = dict.mutableClone();
+		
+		assertEquals(NSMutableDictionary.class, clone.getClass());
+		assertEquals(clone, dict);
+	}
+
+	public void testKeyEnumerator() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("value", "key");	
+		Enumeration<String> e = dict.keyEnumerator();
+		
+		assertTrue(e.hasMoreElements());
+		assertEquals("key", e.nextElement());
+		assertFalse(e.hasMoreElements());
+		try {
+			e.nextElement();
+			fail("Expected NoSuchElementException");
+		} catch (NoSuchElementException ex) {} 
+	}
+	
+	public void testObjectEnumerator() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("value", "key");	
+		Enumeration<String> e = dict.objectEnumerator();
+		
+		assertTrue(e.hasMoreElements());
+		assertEquals("value", e.nextElement());
+		assertFalse(e.hasMoreElements());
+		try {
+			e.nextElement();
+			fail("Expected NoSuchElementException");
+		} catch (NoSuchElementException ex) {} 
+	}
+	
+	public void testObjectForKey() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("value", "key");
+		assertEquals("value", dict.objectForKey("key"));
+	}
+
+	public void testValueForKey() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("value", "key");
+		assertEquals("value", dict.valueForKey("key"));
+	}
+
+	public void testValueForKeyPath() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("value", "key");
+		assertEquals("value", dict.valueForKeyPath("key"));
+		
+		dict = new NSDictionary<String, String>("value", "key.path");
+		assertEquals("value", dict.valueForKeyPath("key.path"));
+		
+		NSDictionary<String, String> subDict = new NSDictionary<String, String>("value", "path");
+		NSDictionary<String, NSDictionary<String,String>> dict2 = new NSDictionary<String, NSDictionary<String,String>>(subDict, "key");
+		assertEquals("value", dict2.valueForKeyPath("key.path"));
+	}
+
+  public void testClone() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSDictionary<String, String> clone = (NSDictionary<String, String>) dict.clone();
+		
+		assertEquals(NSDictionary.class, clone.getClass());
+		assertEquals(clone, dict);
+	}
+
+	public void testPutObjectObject() {
+		try {
+			NSDictionary.emptyDictionary().put("key", "value");
+			fail("Put should throw UnsupportedOperationException");
+		} catch (UnsupportedOperationException e) {
+		}
+	}
+
+	public void testPutAllMap() {
+		try {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("key", "value");
+			NSDictionary.emptyDictionary().putAll(map);
+			fail("PutAll should throw UnsupportedOperationException");
+		} catch (UnsupportedOperationException e) {
+		}	
+	}
+
+	public void testRemoveObject() {
+		try {
+			NSDictionary.emptyDictionary().remove("abc");
+			fail("RemoveObject should throw UnsupportedOperationException");
+		} catch (UnsupportedOperationException e) {
+		}
+	}
+
+	public void testClear() {
+		try {
+			NSDictionary.EmptyDictionary.clear();
+			fail("Clear should throw UnsupportedOperationException");
+		} catch (UnsupportedOperationException e) {
+		}
+	}
+
+	public void testUnknownKeyException() {
+		NSDictionary<String, String> dict = new NSDictionary<String, String>("John", "name");
+		try {
+			throw new NSKeyValueCoding.UnknownKeyException("error", dict, "name");
+		} catch (NSKeyValueCoding.UnknownKeyException e) {
+		  return;
+		} catch (Exception e ) {
+		  fail("Unable to throw unknown key exception");
+		}
+	}
+}
---
+++ Frameworks/Core/ERExtensions/Tests/com/webobjects/foundation/NSMutableDictionaryTest.java
@@ -0,0 +1,169 @@
+package com.webobjects.foundation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableDictionary;
+
+public class NSMutableDictionaryTest extends BaseTestCase {
+
+	public void testNSMutableDictionary() {
+		NSMutableDictionary<?, ?> dict = new NSMutableDictionary<Object, Object>();
+		assertTrue(dict.isEmpty());
+	}
+
+	public void testNSMutableDictionaryInt() {
+		NSMutableDictionary<?, ?> dict = new NSMutableDictionary<Object, Object>(5);
+		assertTrue(dict.isEmpty());
+	}
+
+	public void testNSMutableDictionaryMapOfKV() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key", "value");
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(map);
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testNSMutableDictionaryNSArrayOfVNSArrayOfK() {
+		NSArray<String> keys = new NSArray<String>(new String[] { "key1", "key2" });
+		NSArray<String> values = new NSArray<String>(new String[] { "value1", "value2" });
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(values, keys);
+		assertEquals("value1", dict.get("key1"));
+		assertEquals("value2", dict.get("key2"));
+	}
+
+	public void testNSMutableDictionaryNSMutableDictionaryOfKV() {
+		NSMutableDictionary<String, String> mutableDict = new NSMutableDictionary<String, String>("value", "key");
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(mutableDict);
+		assertFalse(dict.isEmpty());
+		mutableDict.put("key", "newValue");
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testNSMutableDictionaryVArrayKArray() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(values, keys);
+		assertEquals("value1", dict.get("key1"));
+		assertEquals("value2", dict.get("key2"));
+	}
+
+	public void testNSMutableDictionaryVK() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testPutObjectObject() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>();
+		dict.put("key", "value");
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testSetObjectForKey() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>();
+		dict.setObjectForKey("value", "key");
+		assertEquals("value", dict.get("key"));
+	}
+
+	public void testPutAllMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key", "value");
+		map.put("key2", "value2");
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>();
+		dict.putAll(map);
+		assertEquals("value", dict.get("key"));
+		assertEquals("value2", dict.get("key2"));
+	}
+
+	public void testAddEntriesFromDictionary() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		
+		NSMutableDictionary<String, String> mutableDict = new NSMutableDictionary<String, String>();
+		mutableDict.addEntriesFromDictionary(dict);
+		
+		assertEquals("value1", dict.get("key1"));
+		assertEquals("value2", dict.get("key2"));
+	}
+	
+	public void testRemoveObject() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+		dict.remove("key");
+		assertTrue(dict.isEmpty());
+	}
+
+	public void testClear() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+		dict.clear();
+		assertTrue(dict.isEmpty());
+	}
+	
+	public void testRemoveAllObjects() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+		dict.removeAllObjects();
+		assertTrue(dict.isEmpty());
+	}
+
+	public void testRemoveObjectForKey() {
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+		dict.removeObjectForKey("key");
+		assertTrue(dict.isEmpty());
+	}
+
+	public void testRemoveObjectsForKeys() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(values, keys);
+		dict.removeObjectsForKeys(new NSArray<String>(new String[] { "key1", "key2" }));
+		assertTrue(dict.isEmpty());
+	}
+
+	public void testSetDictionary() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(values, keys);
+		dict.setDictionary(new NSDictionary<String, String>("value3", "key3"));
+		assertEquals("value3", dict.get("key3"));
+	}
+
+	public void testImmutableClone() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSDictionary<String, String> dict = new NSDictionary<String, String>(values, keys);
+		NSDictionary<String, String> clone = dict.immutableClone();
+		
+		assertEquals(NSDictionary.class, clone.getClass());
+		assertEquals(clone, dict);
+	}
+
+  public void testClone() {
+		String[] keys = new String[] {"key1", "key2"};
+		String[] values = new String[] {"value1", "value2"};
+		NSMutableDictionary<String, String> dict = new NSMutableDictionary<String, String>(values, keys);
+		NSMutableDictionary<String, String> clone = (NSMutableDictionary<String, String>) dict.clone();
+		
+		assertEquals(NSMutableDictionary.class, clone.getClass());
+		assertEquals(clone, dict);
+	}
+
+	public void testTakeValueForKeyPath() {
+	  NSDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+	  dict.takeValueForKeyPath("newValue", "key");
+	  assertEquals("newValue", dict.get("key"));
+
+	  NSDictionary<String, String> subDict = new NSMutableDictionary<String, String>("value", "path");
+	  NSDictionary<String, ?> dict2 = new NSDictionary<String, NSDictionary<?, ?>>(subDict, "key");
+	  dict2.takeValueForKeyPath("newValue", "key.path");
+	  assertEquals("newValue", dict2.valueForKeyPath("key.path"));
+	}
+	
+  public void testTakeValueForKey() {
+    NSDictionary<String, String> dict = new NSMutableDictionary<String, String>("value", "key");
+    dict.takeValueForKey("newValue", "key");
+    assertEquals("newValue", dict.get("key"));
+  }
+
+}
