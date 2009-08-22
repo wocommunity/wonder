f2f1e140c6af299924c0863754255b9c90aa3938
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2009-08-22 00:55:05 +0000

    Added support for 'displayVariant' D2W key as another option for controlling property component rendering.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WPage.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/ERD2WPage.java
@@ -124,6 +124,8 @@ public abstract class ERD2WPage extends D2WPage implements ERXExceptionHolder, E
         public static final String tabSectionsContents = "tabSectionsContents";
 
         public static final String alternateKeyInfo = "alternateKeyInfo";
+
+        public static final String displayVariant = "displayVariant";
         
         public static final String clickToOpenEnabled = "clickToOpenEnabled";
 
@@ -882,6 +884,26 @@ public abstract class ERD2WPage extends D2WPage implements ERXExceptionHolder, E
     // moved them here to avoid too much subclassing
     public boolean isEmbedded() {
         return ERXComponentUtilities.booleanValueForBinding(this, "isEmbedded", false);
+    }
+
+    /**
+     * Gets the <code>displayVariant</code> for the current property key.  The intention is that the display variant
+     * allows variation in the display method of property keys without needing different, slightly varying,
+     * <code>displayPropertyKeys</code> or <code>tabSectionsContents</code> rules.  Template support has been added for
+     * the <code>omit</code> and <code>blank</code> variants.  One could imagine others, such as <code>collapsed</code>,
+     * <code>ajax</code>, etc.
+     * @return the display variant, if specified
+     */
+    public String displayVariant() {
+        return (String)d2wContext().valueForKey(Keys.displayVariant);
+    }
+
+    /**
+     * Determines if display of the current property key should be <code>omitted</code>.
+     * @return true if key should be omitted
+     */
+    public boolean isKeyOmitted() {
+        return "omit".equals(displayVariant());
     }
 
     /*
