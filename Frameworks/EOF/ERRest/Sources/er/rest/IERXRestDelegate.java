e67d03e849be51d9d37671d9ad2796d17d7095b5
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-09-07 00:42:56 +0000

    added a factory for registering and creating entity rest delegates

--- Frameworks/EOF/ERRest/Sources/er/rest/IERXRestDelegate.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/IERXRestDelegate.java
@@ -1,6 +1,9 @@
 package er.rest;
 
 import com.webobjects.eocontrol.EOClassDescription;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSMutableDictionary;
+import com.webobjects.foundation._NSUtilities;
 
 /**
  * The delegate interface used to convert objects to and from request nodes.
@@ -8,6 +11,21 @@ import com.webobjects.eocontrol.EOClassDescription;
  * @author mschrag
  */
 public interface IERXRestDelegate {
+	/**
+	 * Sets the editing context fort his rest delegate (which might not be necessary, but EO delegates require it).
+	 * 
+	 * @param editingContext
+	 *            the editing context for this delegate
+	 */
+	public void setEditingContext(EOEditingContext editingContext);
+
+	/**
+	 * Returns the primary key for the specified object.
+	 * 
+	 * @param obj
+	 *            the object to return a pk for
+	 * @return the primary key of the object
+	 */
 	public Object primaryKeyForObject(Object obj);
 
 	/**
@@ -49,4 +67,60 @@ public interface IERXRestDelegate {
 	 * @return the object with the given entity and ID
 	 */
 	public Object objectOfEntityWithID(EOClassDescription entity, Object id);
+
+	/**
+	 * A Factory for creating IERXRestDelegates. Right now it's just hard-coded, but this is being added for a later
+	 * extension point.
+	 * 
+	 * @author mschrag
+	 */
+	public static class Factory {
+		private static NSMutableDictionary<String, Class<? extends IERXRestDelegate>> _delegates = new NSMutableDictionary<String, Class<? extends IERXRestDelegate>>();
+
+		/**
+		 * Registers a rest delegate for the given entity name.
+		 * 
+		 * @param delegateClass
+		 *            the delegate class to register
+		 * @param entityName
+		 *            the entity name to register for
+		 */
+		public static void setDelegateForEntityNamed(Class<? extends IERXRestDelegate> delegateClass, String entityName) {
+			_delegates.setObjectForKey(delegateClass, entityName);
+		}
+
+		/**
+		 * Returns a rest delegate for the given entity name.
+		 * 
+		 * @param entityName
+		 *            the name o the entity to lookup
+		 * @param editingContext
+		 *            the current editingcontext
+		 * @return a rest delegate
+		 */
+		public static IERXRestDelegate delegateForEntityNamed(String entityName, EOEditingContext editingContext) {
+			IERXRestDelegate delegate;
+			Class<? extends IERXRestDelegate> delegateClass = _delegates.objectForKey(entityName);
+			if (delegateClass == null) {
+				Class<?> possibleDelegateClass = _NSUtilities.classWithName(entityName + "RestDelegate");
+				if (possibleDelegateClass != null) {
+					delegateClass = possibleDelegateClass.asSubclass(IERXRestDelegate.class);
+				}
+			}
+
+			if (delegateClass != null) {
+				try {
+					delegate = delegateClass.newInstance();
+				}
+				catch (Throwable t) {
+					throw new RuntimeException("Failed to create a delegate for the entity '" + entityName + "'.", t);
+				}
+			}
+			else {
+				delegate = new ERXEORestDelegate();
+			}
+			delegate.setEditingContext(editingContext);
+			return delegate;
+		}
+	}
 }
