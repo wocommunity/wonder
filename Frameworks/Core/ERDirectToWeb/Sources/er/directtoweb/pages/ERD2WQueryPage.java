8ee3c54a79aa2111d6e7c4588f43350f5be9a188
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2009-09-28 20:29:57 +0000

    No longer sets the default query validation delegate if not defined.  Fixed some Javadoc.

--- Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/pages/ERD2WQueryPage.java
+++ Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/pages/ERD2WQueryPage.java
@@ -383,9 +383,6 @@ public class ERD2WQueryPage extends ERD2WPage implements ERDQueryPageInterface {
     public ERDQueryValidationDelegate queryValidationDelegate() {
         if (null == queryValidationDelegate) {
             queryValidationDelegate = (ERDQueryValidationDelegate)d2wContext().valueForKey("queryValidationDelegate");
-            if (null == queryValidationDelegate) {
-                queryValidationDelegate = new ERDQueryValidationDelegate.DefaultQueryValidationDelegate();
-            }
         }
         return queryValidationDelegate;
     }
