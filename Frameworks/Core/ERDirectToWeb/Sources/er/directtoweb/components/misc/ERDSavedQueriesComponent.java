2a6d1b0a7eecd449259c3cbc87feaa0d71347389
tcripps <tcripps@8dc76679-1c23-de11-85cd-0050568e728b>
2009-10-01 04:00:00 +0000

    Extended saved query component to work with regular EOs as well as shared EOs.  Removed redundant methods from ERNEUSavedQueriesComponent class.

--- Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/misc/ERDSavedQueriesComponent.java
+++ Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/misc/ERDSavedQueriesComponent.java
@@ -26,6 +26,7 @@ import com.webobjects.foundation.NSSelector;
 import com.webobjects.foundation.NSTimestamp;
 import com.webobjects.foundation.NSTimestampFormatter;
 
+import er.extensions.appserver.ERXSession;
 import er.extensions.eof.ERXConstant;
 import er.extensions.eof.ERXEOAccessUtilities;
 import er.extensions.eof.ERXEOControlUtilities;
@@ -34,7 +35,8 @@ import er.extensions.foundation.ERXPatcher;
 import er.extensions.foundation.ERXProperties;
 import er.extensions.foundation.ERXSelectorUtilities;
 import er.extensions.foundation.ERXValueUtilities;
-import er.extensions.logging.ERXLogger;
+
+import org.apache.log4j.Logger;
 
 /**
  * This Component will store the forms values in the displayGroup of a Query Page into user preferences
@@ -48,10 +50,15 @@ import er.extensions.logging.ERXLogger;
  * @author dscheck
  */
 public abstract class ERDSavedQueriesComponent extends WOComponent {
-	public static final ERXLogger log = ERXLogger.getERXLogger(ERDSavedQueriesComponent.class);
+	public static final Logger log = Logger.getLogger(ERDSavedQueriesComponent.class);
 
-	public static EOKeyValueArchiving.Support originalEOKVArchiningTimestampSupport = new EOKeyValueArchiving._TimestampSupport();
-	public static EOKeyValueArchiving.Support newEOKVArchiningTimestampSupport = new ERDSavedQueriesComponent._TimestampSupport();
+    public static EOKeyValueArchiving.Support originalEOKVArchivingTimestampSupport = new EOKeyValueArchiving._TimestampSupport();
+    public static EOKeyValueArchiving.Support newEOKVArchivingTimestampSupport = new ERDSavedQueriesComponent._TimestampSupport();
+
+    /** @deprecated  use {@link #originalEOKVArchivingTimestampSupport} */
+    public static EOKeyValueArchiving.Support originalEOKVArchiningTimestampSupport = originalEOKVArchivingTimestampSupport;
+    /** @deprecated  use {@link #newEOKVArchivingTimestampSupport} */
+    public static EOKeyValueArchiving.Support newEOKVArchiningTimestampSupport = newEOKVArchivingTimestampSupport;
 
 	public ERDSavedQueriesComponent(WOContext context) {
 		super(context);
@@ -77,7 +84,7 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 		return _userPreferences;
 	}
 
-	public static class SavedQuery extends Object implements NSKeyValueCoding, EOKeyValueArchiving {
+    public static class SavedQuery implements NSKeyValueCoding, EOKeyValueArchiving {
 		private NSMutableDictionary dict = null;
 		public final String NAME_KEY = "name";
 		public final String QUERY_MIN_KEY = "queryMin";
@@ -86,18 +93,23 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 		public final String QUERY_OPERATOR_KEY = "queryOperator";
 		public final String QUERY_BINDINGS_KEY = "queryBindings";
 
+        public static interface SerializationKeys {
+            public static final String EncodedEO = "encodedEO";
+            public static final String EncodedSharedEO = "encodedSharedEO";
+
+            public static final String EntityName = "entityName";
+            public static final String PrimaryKey = "pk";
+        }
+
 		public SavedQuery() {
 			super();
-
 			dict = new NSMutableDictionary();
 		}
 
 		// Use this when creating from the current values in the displayGroup
 		public SavedQuery(String name, WODisplayGroup displayGroup) {
 			this();
-
 			setName(name);
-
 			getValuesFromDisplayGroup(displayGroup);
 		}
 
@@ -268,11 +280,11 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 
 		public void encodeWithKeyValueArchiver(EOKeyValueArchiver archiver) {
 			archiver.encodeObject(name(), "name");
-			archiver.encodeObject(encodeDictionaryWithSharedEOs(queryMin().mutableClone()), "queryMin");
-			archiver.encodeObject(encodeDictionaryWithSharedEOs(queryMax().mutableClone()), "queryMax");
-			archiver.encodeObject(encodeDictionaryWithSharedEOs(queryMatch().mutableClone()), "queryMatch");
-			archiver.encodeObject(encodeDictionaryWithSharedEOs(queryOperator().mutableClone()), "queryOperator");
-			archiver.encodeObject(encodeDictionaryWithSharedEOs(queryBindings().mutableClone()), "queryBindings");
+            archiver.encodeObject(encodeDictionaryWithEOs(queryMin().mutableClone()), "queryMin");
+            archiver.encodeObject(encodeDictionaryWithEOs(queryMax().mutableClone()), "queryMax");
+            archiver.encodeObject(encodeDictionaryWithEOs(queryMatch().mutableClone()), "queryMatch");
+            archiver.encodeObject(encodeDictionaryWithEOs(queryOperator().mutableClone()), "queryOperator");
+            archiver.encodeObject(encodeDictionaryWithEOs(queryBindings().mutableClone()), "queryBindings");
 		}
 
 		public static Object decodeWithKeyValueUnarchiver(EOKeyValueUnarchiver unarchiver) {
@@ -282,28 +294,36 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 		// Use this when resetting from a stored preference
 		private SavedQuery(EOKeyValueUnarchiver unarchiver) {
 			this();
-			setName((String) unarchiver.decodeObjectForKey("name"));
-			setQueryMin((NSDictionary) decodeDictionaryWithSharedEOs((NSDictionary) unarchiver.decodeObjectForKey("queryMin")));
-			setQueryMax((NSDictionary) decodeDictionaryWithSharedEOs((NSDictionary) unarchiver.decodeObjectForKey("queryMax")));
-			setQueryMatch((NSDictionary) decodeDictionaryWithSharedEOs((NSDictionary) unarchiver.decodeObjectForKey("queryMatch")));
-			setQueryOperator((NSDictionary) decodeDictionaryWithSharedEOs((NSDictionary) unarchiver.decodeObjectForKey("queryOperator")));
-			setQueryBindings((NSDictionary) decodeDictionaryWithSharedEOs((NSDictionary) unarchiver.decodeObjectForKey("queryBindings")));
+            setName((String)unarchiver.decodeObjectForKey("name"));
+            setQueryMin((NSDictionary) decodeDictionaryWithEOs((NSDictionary)unarchiver.decodeObjectForKey("queryMin")));
+            setQueryMax((NSDictionary) decodeDictionaryWithEOs((NSDictionary)unarchiver.decodeObjectForKey("queryMax")));
+            setQueryMatch((NSDictionary) decodeDictionaryWithEOs((NSDictionary)unarchiver.decodeObjectForKey("queryMatch")));
+            setQueryOperator((NSDictionary) decodeDictionaryWithEOs((NSDictionary)unarchiver.decodeObjectForKey("queryOperator")));
+            setQueryBindings((NSDictionary) decodeDictionaryWithEOs((NSDictionary)unarchiver.decodeObjectForKey("queryBindings")));
 		}
 
 		public String toString() {
 			return "SavedQuery: " + hashCode() + " dict=" + dict;
 		}
+        
+        private boolean isDictionaryAnEncodedEO(NSDictionary dictionary) {
+            return dictionary != null && dictionary.objectForKey(SerializationKeys.EncodedEO) != null;
+        }
 
 		private boolean isDictionaryAnEncodedSharedEO(NSDictionary dictionary) {
-			return dictionary != null && dictionary.objectForKey("encodedSharedEO") != null;
+			return dictionary != null && dictionary.objectForKey(SerializationKeys.EncodedSharedEO) != null;
 		}
 
-		private NSMutableDictionary encodeSharedEO(EOEnterpriseObject eo) {
+		private NSMutableDictionary encodeEO(EOEnterpriseObject eo) {
 			NSMutableDictionary encodedDict = new NSMutableDictionary(3);
 
-			encodedDict.setObjectForKey(Boolean.TRUE, "encodedSharedEO");
-			encodedDict.setObjectForKey(eo.entityName(), "entityName");
-			encodedDict.setObjectForKey(ERXEOControlUtilities.primaryKeyStringForObject(eo), "pk");
+            if (eo.editingContext() instanceof EOSharedEditingContext) {
+                encodedDict.setObjectForKey(Boolean.TRUE, SerializationKeys.EncodedSharedEO);
+            } else {
+                encodedDict.setObjectForKey(Boolean.TRUE, SerializationKeys.EncodedEO);
+            }
+            encodedDict.setObjectForKey(eo.entityName(), SerializationKeys.EntityName);
+            encodedDict.setObjectForKey(ERXEOControlUtilities.primaryKeyStringForObject(eo), SerializationKeys.PrimaryKey);
 
 			if (log.isDebugEnabled())
 				log.debug("encodeSharedEO, eo=" + eo + ",  encodedDict=" + encodedDict);
@@ -311,63 +331,72 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 			return encodedDict;
 		}
 
-		private EOEnterpriseObject decodeSharedEO(NSDictionary dictionary) {
-			String entityName = (String) dictionary.objectForKey("entityName");
-			String pk = (String) dictionary.objectForKey("pk");
-			EOEntity entity = ERXEOAccessUtilities.entityNamed(null, entityName);
-			EOAttribute primaryKeyAttribute = (EOAttribute) ERXArrayUtilities.firstObject(entity.primaryKeyAttributes());
-			Object primaryKeyObject = pk;
-			EOEnterpriseObject eo = null;
+        private EOEnterpriseObject decodeEO(NSDictionary dictionary) {
+            String entityName = (String)dictionary.objectForKey(SerializationKeys.EntityName);
+            String pk = (String)dictionary.objectForKey(SerializationKeys.PrimaryKey);
+            EOEntity entity = ERXEOAccessUtilities.entityNamed(null, entityName);
+            EOAttribute primaryKeyAttribute;
+            EOEnterpriseObject eo = null;
+            if (entity != null) {
+                primaryKeyAttribute = ERXArrayUtilities.firstObject(entity.primaryKeyAttributes());
+                Object primaryKeyObject = pk;
+                if (log.isDebugEnabled()) log.debug("decodeEO with dict: " + dictionary);
 
-			if (log.isDebugEnabled())
-				log.debug("decodeSharedEO, dict=" + dictionary);
+                if (primaryKeyAttribute != null && !String.class.getName().equals(primaryKeyAttribute.className())) {
+                    primaryKeyObject = ERXConstant.integerForString(pk);
+                }
 
-			if (primaryKeyAttribute != null && !String.class.getName().equals(primaryKeyAttribute.className()))
-				primaryKeyObject = ERXConstant.integerForString(pk);
-
-			try {
-				eo = ERXEOControlUtilities.sharedObjectWithPrimaryKey(entityName, primaryKeyObject);
-			} catch (EOObjectNotAvailableException e) {
-
-			}
-			return eo;
-		}
+                try {
+                    if (isDictionaryAnEncodedSharedEO(dictionary)) {
+                        eo = ERXEOControlUtilities.sharedObjectWithPrimaryKey(entityName, primaryKeyObject);
+                    } else {
+                        eo = ERXEOControlUtilities.objectWithPrimaryKeyValue(ERXSession.session().defaultEditingContext(), entityName, primaryKeyObject, null);
+                    }
+                } catch (EOObjectNotAvailableException eoonae) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Unable to restore serialized EO from saved query.  The EO for entity '" + entityName + "' with pk '" + pk + "' was not found.");
+                    }
+                }
+            } else {
+                if (log.isDebugEnabled()) {
+                    log.debug("Unable to restore serialized EO from saved query.  Could not find entity '" + entityName + ".");
+                }
+            }
+            return eo;
+        }
 
 		// Recursively walk down the given dictionary and convert any values
-		// that are SharedEntity EOs to a dictionary with keys (encodedSharedEO,
+		// that are SharedEntity EOs to a dictionary with keys (encodedSharedEO or encodedEO,
 		// entityName, pk)
-		protected NSMutableDictionary encodeDictionaryWithSharedEOs(NSDictionary dictionary) {
+		protected NSMutableDictionary encodeDictionaryWithEOs(NSDictionary dictionary) {
 			NSMutableDictionary theMutableDictionary = (NSMutableDictionary) ((dictionary instanceof NSMutableDictionary) ? dictionary : dictionary.mutableClone());
 
 			for (Enumeration anEnum = theMutableDictionary.keyEnumerator(); anEnum.hasMoreElements();) {
 				String key = (String) anEnum.nextElement();
 				Object value = theMutableDictionary.objectForKey(key);
 
-				if (value instanceof EOEnterpriseObject && ((EOEnterpriseObject) value).editingContext().equals(EOSharedEditingContext.defaultSharedEditingContext())) {
-					EOEnterpriseObject eo = (EOEnterpriseObject) value;
-
-					NSMutableDictionary encodedDict = encodeSharedEO(eo);
-
+				if (value instanceof EOEnterpriseObject && !ERXEOControlUtilities.isNewObject((EOEnterpriseObject)value)) {
+                    EOEnterpriseObject eo = (EOEnterpriseObject)value;
+                    NSMutableDictionary encodedDict = encodeEO(eo);
 					theMutableDictionary.setObjectForKey(encodedDict, key);
 				} else if (value instanceof NSDictionary) {
-					theMutableDictionary.setObjectForKey(encodeDictionaryWithSharedEOs((NSDictionary) value), key);
+					theMutableDictionary.setObjectForKey(encodeDictionaryWithEOs((NSDictionary) value), key);
 				}
 			}
 			return theMutableDictionary;
 		}
 
 		// Recursively walk down the given dictionary and convert any
-		// dictionaries with keys (encodedSharedEO, entityName, pk) back to the
+		// dictionaries with keys (encodedSharedEO or encodedEO, entityName, pk) back to the
 		// Shared EO
-		protected Object decodeDictionaryWithSharedEOs(NSDictionary dictionary) {
+		protected Object decodeDictionaryWithEOs(NSDictionary dictionary) {
 			NSMutableDictionary theMutableDictionary = (NSMutableDictionary) ((dictionary instanceof NSMutableDictionary) ? dictionary : dictionary.mutableClone());
 
-			if (isDictionaryAnEncodedSharedEO(dictionary)) {
-				Object eo = decodeSharedEO(dictionary);
+			if (isDictionaryAnEncodedSharedEO(dictionary) || isDictionaryAnEncodedEO(dictionary)) {
+				Object eo = decodeEO(dictionary);
 
-				// if we cant decode the sharedEO, then just return the
-				// dictionary
-				return (eo != null) ? eo : dictionary;
+                // If we can't decode the EO, then just return the dictionary.
+                return  (eo != null) ? eo : dictionary;
 			}
 
 			for (Enumeration anEnum = theMutableDictionary.keyEnumerator(); anEnum.hasMoreElements();) {
@@ -375,7 +404,7 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 				Object value = theMutableDictionary.objectForKey(key);
 
 				if (value instanceof NSDictionary) {
-					theMutableDictionary.setObjectForKey(decodeDictionaryWithSharedEOs((NSDictionary) value), key);
+					theMutableDictionary.setObjectForKey(decodeDictionaryWithEOs((NSDictionary) value), key);
 				}
 			}
 			return theMutableDictionary;
@@ -384,7 +413,7 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 
 	// Replacement helper class to archive the NSTimestamp. We need to remove
 	// the %z from the format string because
-	// NSTimestampFormatter cant deal with the %z
+	// NSTimestampFormatter can't deal with the %z
 	public static class _TimestampSupport extends EOKeyValueArchiving.Support {
 		private static NSTimestampFormatter _formatter = new NSTimestampFormatter("%Y-%m-%d %H:%M:%S");
 
@@ -423,12 +452,12 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 	public NSMutableArray loadSavedQueriesForPageConfigurationNamed(String pageConfigurationName) {
 		NSArray savedQueries = null;
 
-		EOKeyValueArchiving.Support.setSupportForClass(newEOKVArchiningTimestampSupport, NSTimestamp._CLASS);
+		EOKeyValueArchiving.Support.setSupportForClass(newEOKVArchivingTimestampSupport, NSTimestamp._CLASS);
 
 		try {
 			savedQueries = (NSArray) userPreferences().valueForKey(userPreferenceNameForPageConfiguration(pageConfigurationName));
 		} finally {
-			EOKeyValueArchiving.Support.setSupportForClass(originalEOKVArchiningTimestampSupport, NSTimestamp._CLASS);
+			EOKeyValueArchiving.Support.setSupportForClass(originalEOKVArchivingTimestampSupport, NSTimestamp._CLASS);
 		}
 
 		if (log.isDebugEnabled())
@@ -441,12 +470,12 @@ public abstract class ERDSavedQueriesComponent extends WOComponent {
 		if (log.isDebugEnabled())
 			log.debug("saveQueriesForPageConfigurationNamed(" + pageConfigurationName + "): queries = " + queries);
 
-		EOKeyValueArchiving.Support.setSupportForClass(newEOKVArchiningTimestampSupport, NSTimestamp._CLASS);
+		EOKeyValueArchiving.Support.setSupportForClass(newEOKVArchivingTimestampSupport, NSTimestamp._CLASS);
 
 		try {
 			userPreferences().takeValueForKey(queries, userPreferenceNameForPageConfiguration(pageConfigurationName));
 		} finally {
-			EOKeyValueArchiving.Support.setSupportForClass(originalEOKVArchiningTimestampSupport, NSTimestamp._CLASS);
+			EOKeyValueArchiving.Support.setSupportForClass(originalEOKVArchivingTimestampSupport, NSTimestamp._CLASS);
 		}
 	}
 
