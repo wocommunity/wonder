90ed048ea4cdf65381e0828044a80535baad2b0c
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2009-10-31 10:18:48 +0000

    java doc, example fixed, more push variants

--- Examples/Ajax/AjaxExample/Sources/PushExample.java
+++ Examples/Ajax/AjaxExample/Sources/PushExample.java
@@ -5,8 +5,10 @@ import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOResponse;
 
 import er.ajax.AjaxPushRequestHandler;
+import er.ajax.AjaxUtils;
 
 public class PushExample extends WOComponent {
+	
     public PushExample(WOContext context) {
         super(context);
     }
@@ -14,9 +16,18 @@ public class PushExample extends WOComponent {
     public String url() {
     	return context().urlWithRequestHandlerKey(AjaxPushRequestHandler.AjaxCometRequestHandlerKey, "test", "wosid=" +session().sessionID());
     }
+
+    @Override
+    public void appendToResponse(WOResponse response, WOContext context) {
+    	super.appendToResponse(response, context);
+    	AjaxUtils.addScriptResourceInHead(context, response, "prototype.js");
+    	AjaxUtils.addScriptResourceInHead(context, response, "wonder.js");
+    }
     
 	public WOActionResults push() {
-		AjaxPushRequestHandler.push(session().sessionID(), "test");
-		return new WOResponse();
+		AjaxPushRequestHandler.push(session().sessionID(), "test: " + System.currentTimeMillis());
+		WOResponse response = new WOResponse();
+		response.setContent("Sent some data");
+		return response;
 	}
 }
