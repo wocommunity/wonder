d5da639970fb870ed2807c562f9117f1fc911c93
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2009-11-03 02:35:28 +0000

    Adding component plus rules for D2W edit ERAttachment component

---
+++ Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/attachments/ERD2WEditAttachment.java
@@ -0,0 +1,38 @@
+package er.directtoweb.components.attachments;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2WComponent;
+
+/**
+ * D2W component for editing ERAttachments
+ * The configurationName is computed: 'Entity.propertyKey'
+ * 
+ * @author mendis
+ *
+ * @binding object
+ * @binding propertyKey
+ */
+public class ERD2WEditAttachment extends D2WComponent {
+
+	public ERD2WEditAttachment(WOContext aContext) {
+		super(aContext);
+	}
+	
+    
+    // accessors
+    public String configurationName() {
+    	return object().entityName() + "." + propertyKey();
+    }
+    
+    public void setObjectPropertyValue(Object value) {
+    	object().takeValueForKeyPath(value, propertyKey());
+    }
+
+    // actions    
+    public void removeAttachment() {
+    	//ERAttachment attachment = (ERAttachment) objectPropertyValue();
+    	//attachment.delete();
+    	setObjectPropertyValue(null);
+    }
+
+}
