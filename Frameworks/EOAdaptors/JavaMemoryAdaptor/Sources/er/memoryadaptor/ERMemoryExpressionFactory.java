b179eb5ab9f8045a4f6cc688734631bd556b23af
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-11-12 20:41:08 +0000

    bunch of hacks to make migrations "work" with memory adaptor

---
+++ Frameworks/EOAdaptors/JavaMemoryAdaptor/Sources/er/memoryadaptor/ERMemoryExpressionFactory.java
@@ -0,0 +1,15 @@
+package er.memoryadaptor;
+
+import com.webobjects.eoaccess.EOAdaptor;
+import com.webobjects.eoaccess.EOSQLExpressionFactory;
+
+/**
+ * This stub exists only to make memory adaptor migrations function.
+ * 
+ * @author mschrag
+ */
+public class ERMemoryExpressionFactory extends EOSQLExpressionFactory {
+  public ERMemoryExpressionFactory(EOAdaptor adaptor) {
+    super(adaptor);
+  }
+}
