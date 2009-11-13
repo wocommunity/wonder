ee5a5c549556525905b3807b1aab52750f16b5dd
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2009-11-13 21:11:30 +0000

    just getting this in .. doesn't do anything yet

---
+++ Tests/ERXTest/Sources/er/erxtest/d2w/ERD2WModelTest.java
@@ -0,0 +1,30 @@
+package er.erxtest.d2w;
+
+import junit.framework.TestCase;
+
+import com.webobjects.directtoweb.D2WContext;
+import com.webobjects.directtoweb.ERD2WContext;
+
+import er.directtoweb.ERD2WModel;
+import er.erxtest.Application;
+import er.extensions.ERXExtensions;
+import er.extensions.ERXTestUtilities;
+
+public class ERD2WModelTest extends TestCase {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    ERXExtensions.initApp(Application.class, new String[0]);
+  }
+
+  public void testModel() {
+    ERD2WModel model = new ERD2WModel(ERXTestUtilities.resourcePathURL("BugTracker.d2wmodel", ERD2WModelTest.class));
+    D2WContext d2wContext = ERD2WContext.newContext();
+    d2wContext._setModel(model);
+    //Employee e = 
+    //d2wContext.takeValueForKey(object(), "object");
+    //d2wContext.setEntity(EOUtilities.entityNamed(object().editingContext(), object().entityName()));
+    //d2wContext.setTask("inspect");
+
+  }
+}
