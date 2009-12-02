3a4a0878097aeaa66687c6ac5a1ceca6d0bd66f8
kiddyr <kiddyr@8dc76679-1c23-de11-85cd-0050568e728b>
2009-12-02 03:30:47 +0000

    A start to some tests of the ERXValueUtilities class and the ERXKeyGlobalIDTest from the mailing list, with the test stubbed out right now. If it should pass, then it should pass.

---
+++ Tests/ERXTest/Sources/er/extensions/eof/ERXKeyGlobalIDTest.java
@@ -0,0 +1,59 @@
+
+package er.extensions.eof;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.eocontrol.EOGlobalID;
+import com.webobjects.eocontrol.EOKeyGlobalID;
+
+import er.extensions.foundation.ERXValueUtilities;
+
+public class ERXKeyGlobalIDTest extends TestCase {
+
+	private EOEditingContext ec;
+	
+	public void setUp() throws Exception {
+		ec = new EOEditingContext();
+	}
+
+	// remove this when the next method does not fail
+	public void testOneForNow() { }
+	
+	// Remove the _ from the name of this method when this test passes.
+	public void _testEquals() {
+
+		EOEnterpriseObject emp = ERXEOControlUtilities.createAndInsertObject(ec, "Employee");
+		EOEnterpriseObject co = ERXEOControlUtilities.createAndInsertObject(ec, "Company");
+		
+		emp.takeValueForKey("Bob", "firstName");
+		emp.takeValueForKey("Roberts", "lastName");
+		emp.takeValueForKey(Boolean.FALSE, "manager");
+		
+		co.takeValueForKey("Foobar.com", "name");
+		emp.takeValueForKey(co, "company");
+
+		ec.saveChanges();
+
+		EOGlobalID gid = emp.editingContext().globalIDForObject(emp);
+
+		Assert.assertFalse(ERXValueUtilities.isNull(emp));
+		Assert.assertFalse(gid.isTemporary());
+
+		EOEnterpriseObject emp2 = ERXEOControlUtilities.localInstanceOfObject(ERXEC.newEditingContext(), emp);	
+
+		ERXKeyGlobalID xkgid = ERXKeyGlobalID.globalIDForGID((EOKeyGlobalID)gid);
+		Assert.assertEquals(xkgid, xkgid);
+		
+		EOKeyGlobalID gid2 = (EOKeyGlobalID)emp2.editingContext().globalIDForObject(emp2);
+		ERXKeyGlobalID xkgid2 = ERXKeyGlobalID.globalIDForGID(gid2);
+
+		Assert.assertEquals(xkgid, xkgid2);
+		
+		ec.deleteObject(emp);
+		ec.deleteObject(co);
+		ec.saveChanges();
+	}
+}
