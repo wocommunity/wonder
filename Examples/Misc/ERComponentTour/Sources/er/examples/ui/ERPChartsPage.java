b92c2fbfbc744a2f7c82c4383404054735c19a04
kiddyr <kiddyr@8dc76679-1c23-de11-85cd-0050568e728b>
2010-01-02 02:29:13 +0000

    Add some better, slightly more sensible data for the charts. Change WO in html from modern-style to 5.3-compatible. Start of fix to ant build support.

--- Examples/Misc/ERComponentTour/Sources/er/examples/ui/ERPChartsPage.java
+++ Examples/Misc/ERComponentTour/Sources/er/examples/ui/ERPChartsPage.java
@@ -16,25 +16,72 @@ public class ERPChartsPage extends ERXComponent {
 
 	private NSArray _itemSet1;
 	
+	// Reasonable dataset for LineChart, BarChart, BarChart3D, StackedBarChart, StackedBarChart3D, AreaChart, StackedAreaChart
+	//
 	public NSArray itemSet1() {
 		if (_itemSet1 == null) {
 			NSArray keys = new NSArray(Arrays.asList("name", "value", "category"));
 			
-			NSMutableDictionary oneA = new NSMutableDictionary(new NSArray(Arrays.asList("green", new Integer(1), "one")), keys);
-			NSMutableDictionary oneB = new NSMutableDictionary(new NSArray(Arrays.asList("green", new Integer(2), "two")), keys);
-			NSMutableDictionary oneC = new NSMutableDictionary(new NSArray(Arrays.asList("green", new Integer(3), "three")), keys);
+			NSMutableDictionary oneA = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(100), "01/2011")), keys);
+			NSMutableDictionary oneB = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(150), "02/2011")), keys);
+			NSMutableDictionary oneC = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(200), "03/2011")), keys);
 
-			NSMutableDictionary twoA = new NSMutableDictionary(new NSArray(Arrays.asList("blue", new Integer(2), "one")), keys);
-			NSMutableDictionary twoB = new NSMutableDictionary(new NSArray(Arrays.asList("blue", new Integer(3), "two")), keys);
-			NSMutableDictionary twoC = new NSMutableDictionary(new NSArray(Arrays.asList("blue", new Integer(4), "three")), keys);
+			NSMutableDictionary twoA = new NSMutableDictionary(new NSArray(Arrays.asList("RIM", new Integer(200), "01/2011")), keys);
+			NSMutableDictionary twoB = new NSMutableDictionary(new NSArray(Arrays.asList("RIM", new Integer(210), "02/2011")), keys);
+			NSMutableDictionary twoC = new NSMutableDictionary(new NSArray(Arrays.asList("RIM", new Integer(215), "03/2011")), keys);
 
-			NSMutableDictionary threeA = new NSMutableDictionary(new NSArray(Arrays.asList("red", new Integer(3), "one")), keys);
-			NSMutableDictionary threeB = new NSMutableDictionary(new NSArray(Arrays.asList("red", new Integer(4), "two")), keys);
-			NSMutableDictionary threeC = new NSMutableDictionary(new NSArray(Arrays.asList("red", new Integer(5), "three")), keys);
+			NSMutableDictionary threeA = new NSMutableDictionary(new NSArray(Arrays.asList("Android", new Integer(50), "01/2011")), keys);
+			NSMutableDictionary threeB = new NSMutableDictionary(new NSArray(Arrays.asList("Android", new Integer(55), "02/2011")), keys);
+			NSMutableDictionary threeC = new NSMutableDictionary(new NSArray(Arrays.asList("Android", new Integer(110), "03/2011")), keys);
 
-			_itemSet1 = new NSArray(Arrays.asList(oneA, oneB, oneC, twoA, twoB, twoC, threeA, threeB, threeC));
+			_itemSet1 = new NSArray(Arrays.asList(twoA, twoB, twoC, oneA, oneB, oneC, threeA, threeB, threeC));
 		}
-		System.out.println("itemSet1 = "+_itemSet1);
+		//System.out.println("itemSet1 = "+_itemSet1);
 		return _itemSet1;
+	}
+
+	private NSArray _itemSet1a;
+	
+	// "Wrong" dataset for AreaChart
+	//
+	public NSArray itemSet1a() {
+		if (_itemSet1a == null) {
+			NSArray keys = new NSArray(Arrays.asList("name", "value", "category"));
+			
+			NSMutableDictionary oneA = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(100), "01/2011")), keys);
+			NSMutableDictionary oneB = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(150), "02/2011")), keys);
+			NSMutableDictionary oneC = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(200), "03/2011")), keys);
+
+			NSMutableDictionary twoA = new NSMutableDictionary(new NSArray(Arrays.asList("RIM", new Integer(200), "01/2011")), keys);
+			NSMutableDictionary twoB = new NSMutableDictionary(new NSArray(Arrays.asList("RIM", new Integer(210), "02/2011")), keys);
+			NSMutableDictionary twoC = new NSMutableDictionary(new NSArray(Arrays.asList("RIM", new Integer(215), "03/2011")), keys);
+
+			NSMutableDictionary threeA = new NSMutableDictionary(new NSArray(Arrays.asList("Android", new Integer(50), "01/2011")), keys);
+			NSMutableDictionary threeB = new NSMutableDictionary(new NSArray(Arrays.asList("Android", new Integer(55), "02/2011")), keys);
+			NSMutableDictionary threeC = new NSMutableDictionary(new NSArray(Arrays.asList("Android", new Integer(110), "03/2011")), keys);
+
+			_itemSet1a = new NSArray(Arrays.asList(oneA, oneB, oneC, twoA, twoB, twoC, threeA, threeB, threeC));
+		}
+		//System.out.println("itemSet1a = "+_itemSet1a);
+		return _itemSet1a;
+	}
+
+	// Reasonable dataset for WaterfallChart
+	//
+	private NSArray _itemSet2;
+	
+	public NSArray itemSet2() {
+		if (_itemSet2 == null) {
+			NSArray keys = new NSArray(Arrays.asList("name", "value", "category"));
+			
+			NSMutableDictionary oneA = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(20), "01/2011")), keys);
+			NSMutableDictionary oneB = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(25), "02/2011")), keys);
+			NSMutableDictionary oneC = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(30), "03/2011")), keys);
+			NSMutableDictionary oneD = new NSMutableDictionary(new NSArray(Arrays.asList("iPhone", new Integer(75), "Total Q1")), keys);
+
+			_itemSet2 = new NSArray(Arrays.asList(oneA, oneB, oneC, oneD));
+		}
+		//System.out.println("itemSet2 = "+_itemSet2);
+		return _itemSet2;
 	}
 }
