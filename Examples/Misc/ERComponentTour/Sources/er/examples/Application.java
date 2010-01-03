3051506ac7962624813a08a8135c86f1e743ff1e
kiddyr <kiddyr@8dc76679-1c23-de11-85cd-0050568e728b>
2010-01-03 00:25:28 +0000

    Add functionality from ExcelGeneratorExample to a page in this example.

--- Examples/Misc/ERComponentTour/Sources/er/examples/Application.java
+++ Examples/Misc/ERComponentTour/Sources/er/examples/Application.java
@@ -1,14 +1,24 @@
-package er.examples	;
+package er.examples;
+
+import com.webobjects.foundation.NSBundle;
+import com.webobjects.foundation.NSDictionary;
 
 import er.extensions.appserver.ERXApplication;
+import er.extensions.foundation.ERXDictionaryUtilities;
 
 public class Application extends ERXApplication {
+
 	public static void main(String[] argv) {
 		ERXApplication.main(argv, Application.class);
 	}
 
 	public Application() {
 		ERXApplication.log.info("Welcome to " + name() + " !");
-		/* ** put your initialization code in here ** */
+        setPageRefreshOnBacktrackEnabled(true);
 	}
+
+    public NSDictionary<?,?> data() {
+    	NSDictionary<?,?> dict = ERXDictionaryUtilities.dictionaryFromPropertyList("ExcelStyles", NSBundle.mainBundle());
+    	return dict;
+    }
 }
