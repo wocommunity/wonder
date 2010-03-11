604d891134bfb8479e57e7a9580cc1ec1b8b7413
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-03-11 13:01:59 +0000

    Added support for ${uuid} in attachment paths

--- Frameworks/BusinessLogic/ERAttachment/Sources/er/attachment/processors/ERAttachmentProcessor.java
+++ Frameworks/BusinessLogic/ERAttachment/Sources/er/attachment/processors/ERAttachmentProcessor.java
@@ -3,6 +3,7 @@ package er.attachment.processors;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
@@ -56,6 +57,7 @@ public abstract class ERAttachmentProcessor<T extends ERAttachment> {
   private static final String HASH_VARIABLE = "\\$\\{hash\\}";
   private static final String PK_VARIABLE = "\\$\\{pk\\}";
   private static final String FILE_NAME_VARIABLE = "\\$\\{fileName\\}";
+  private static final String UUID_VARIABLE = "\\$\\{uuid\\}";
 
   private static NSMutableDictionary<String, ERAttachmentProcessor<?>> _processors;
   
@@ -127,7 +129,7 @@ public abstract class ERAttachmentProcessor<T extends ERAttachment> {
   }
 
   /**
-   * Parses a path template with ${ext}, ${fileName}, ${hash}, and ${pk} variables in it.  See the ERAttachment
+   * Parses a path template with ${ext}, ${fileName}, ${hash}, ${uuid}, and ${pk} variables in it.  See the ERAttachment
    * top level documentation for more information.
    * 
    * @param attachment the attachment being processed
@@ -157,6 +159,8 @@ public abstract class ERAttachmentProcessor<T extends ERAttachment> {
     //hashPathBuffer.append('/');
     //hashPathBuffer.append(filenameHash.substring(3));
     parsedPath = parsedPath.replaceAll(ERAttachmentProcessor.HASH_VARIABLE, hashPathBuffer.toString());
+
+    parsedPath = parsedPath.replaceAll(ERAttachmentProcessor.UUID_VARIABLE, UUID.randomUUID().toString());
 
     parsedPath = parsedPath.replaceAll(ERAttachmentProcessor.FILE_NAME_VARIABLE, recommendedFileName);
     parsedPath = parsedPath.replaceAll(ERAttachmentProcessor.PK_VARIABLE, attachment.primaryKeyInTransaction());
