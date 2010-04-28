0c56d9dea5d8bbe2f571b20c36d998c9ad0cd676
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2010-04-28 01:52:08 +0000

    Importing framework for jQuery support in WOnder

---
+++ Frameworks/Ajax/ERJQuery/Sources/er/jquery/widgets/AjaxUploadButton.java
@@ -0,0 +1,31 @@
+package er.jquery.widgets;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOMultipartIterator;
+import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WOMultipartIterator.WOFormData;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSData;
+import com.webobjects.foundation.NSDictionary;
+
+import er.extensions.foundation.ERXFileUtilities;
+
+/**
+ * Encapsulation of http://valums.com/ajax-upload/ implemented like WOFileUpload
+ * @see WOFileUpload		API/binding compatible with WOFileUpload. Additionally:
+ * 
+ * @author mendis
+ *
+ * @see er.prototaculous.AjaxUploadButton 	jQuery compatible version
+ */
+public class AjaxUploadButton extends AjaxUpload {
+
+	public AjaxUploadButton(WOContext aContext) {
+		super(aContext);
+	}
+}
