2a0f4e3462a504a180ac19753ff48ecdbe585a38
chillatgvc <chillatgvc@8dc76679-1c23-de11-85cd-0050568e728b>
2010-06-03 16:18:44 +0000

    WONDER-540 Modified AjaxModalDialogOpener to support rendering as other element types (e.g. div)

--- Frameworks/Ajax/Ajax/Sources/er/ajax/AjaxModalDialogOpener.java
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/AjaxModalDialogOpener.java
@@ -11,21 +11,19 @@ import com.webobjects.foundation.NSMutableDictionary;
 import er.extensions.appserver.ERXWOContext;
 
 /**
- * <p>Generates a link to open a specific AjaxModalDialog.  This is useful when you want to physically separate the modal dialog from what
+ * <p>Generates an element to open a specific AjaxModalDialog.  This is useful when you want to physically separate the modal dialog from what
  * opens it, for example if you want a modal dialog containing a form to have an opener inside of another form.  It is also useful if you
  * want to use a dialog in a repetition.  Using an AjaxModalDialogOpener in the repetition and moving the AjaxModalDialog outside of the
  * repetition will result in only a single rendering of the AjaxModalDialog in the page.  You can also move it before the repetition to 
  * speed up request handling when the dialog is open. Normally you will want to bind showOpener=false; on the AjaxModalDialog that this opens.
  * </p>
+ * 
  * <p> If you need to do some preparation before the dialog opens, use the action
  * method.This is called synchronously so make it quick!  The action method is useful for things like copying the item from a repetition
  * to use in a dialog that is not nested in the repetition. </p>
  * 
- * <p>The link shown can come from two sources:
- * <ul>
- * <li>the label binding</li>
- * <li>child elements</li>
- * </ul></p>
+ * <p>If you specify the <code>elementName</code> as <code>a</code> then the label binding can be used to specify the link, or you can use child elements.
+ * You can also specify the link's title using the title binding.</p>
  * 
  * <p>As the opener functions on the client only, it is possible for it to be rendered when <code>enabled</code> evaluates to 
  * <code>true</code> and clicked later when <code>enabled</code> would evaluate to <code>false</code>.  This condition is checked for
@@ -34,8 +32,9 @@ import er.extensions.appserver.ERXWOContext;
  * </p>
  * 
  * @binding dialogId required, ID of the AjaxModalDialog to open
- * @binding label the text for the link that opens the dialog box, if this used the child elements are ignored
- * @binding linkTitle used as title attribute of link opening dialog
+ * @binding elementName the element that you want the open rendered as
+ * @binding label only relevant if <code>elementName</code> is <code>a</code>: the text for the link that opens the dialog box, if this used the child elements are ignored.
+ * @binding linkTitle only relevant if <code>elementName</code> is <code>a</code>: used as title attribute of link opening dialog
  * @binding title the Title to be displayed in the ModalBox window header, can override what the dialog was created with
  * @binding action, optional action to call before opening the modal dialog.  
  * @binding enabled if false, nothing is rendered for this component.  This can be used instead of wrapping this in a WOConditional.
@@ -85,11 +84,18 @@ public class AjaxModalDialogOpener extends AjaxComponent {
 			return;
 		}
 		
-		response.appendContentString("<a href=\"javascript:void(0)\"");
+		String elementName = (String) valueForBinding("elementName", "a", context.component());
+
+		response.appendContentString("<" + elementName + " ");
+		
+		if (elementName.equals("a")) {
+		response.appendContentString("href=\"javascript:void(0)\"");
+		appendTagAttributeToResponse(response, "title", valueForBinding("linkTitle", null));
+		}
+		
 		appendTagAttributeToResponse(response, "id", id());
 		appendTagAttributeToResponse(response, "class", valueForBinding("class", null));
 		appendTagAttributeToResponse(response, "style", valueForBinding("style", null));
-		appendTagAttributeToResponse(response, "title", valueForBinding("linkTitle", null));
 		
 		// onclick calls the script that opens the AjaxModalDialog
 		response.appendContentString(" onclick=\"");
@@ -102,13 +108,14 @@ public class AjaxModalDialogOpener extends AjaxComponent {
 
 		response.appendContentString("return false;\" >");	
 
-		if (hasBinding("label")) {
+		if (elementName.equals("a") && hasBinding("label")) {
 			response.appendContentString((String) valueForBinding("label"));
 		} else {
 			// This will append the contents inside of the link
 			super.appendToResponse(response, context);
 		}
-		response.appendContentString("</a>");
+
+		response.appendContentString("</" + elementName + ">");
 	}
 
 	/**
