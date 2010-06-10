735c1e83c6c0d0cd1ec5fbd1e2e700416559dd3a
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2010-06-10 00:59:06 +0000

    Adding CSS style-able error panel

---
+++ Frameworks/Misc/ERDivaliteLook/Sources/er/divalite/components/ERLITErrorDictionaryPanel.java
@@ -0,0 +1,10 @@
+package er.divalite.components;
+
+import com.webobjects.appserver.WOContext;
+import er.extensions.components.ERXErrorDictionaryPanel;
+
+public class ERLITErrorDictionaryPanel extends ERXErrorDictionaryPanel {
+    public ERLITErrorDictionaryPanel(WOContext context) {
+        super(context);
+    }
+}
