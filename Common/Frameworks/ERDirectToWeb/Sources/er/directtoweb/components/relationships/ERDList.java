f6b887120389dd933583fdb938ff72a49b37a640
santoash <santoash@8dc76679-1c23-de11-85cd-0050568e728b>
2010-06-11 20:57:56 +0000

    createObjectAction() method now supports createObjectDelegate.

--- Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/components/relationships/ERDList.java
+++ Common/Frameworks/ERDirectToWeb/Sources/er/directtoweb/components/relationships/ERDList.java
@@ -58,31 +58,44 @@ public class ERDList extends ERDCustomEditComponent {
 
     public WOComponent createObjectAction() {
     	WOComponent nextPage = context().page();
-    	String editRelationshipConfigurationName = (String)valueForBinding("editRelationshipConfigurationName");
-    	if(editRelationshipConfigurationName != null && editRelationshipConfigurationName.length() > 0) {
-    		nextPage = D2W.factory().pageForConfigurationNamed(editRelationshipConfigurationName, session());
-    		if(nextPage instanceof EditRelationshipPageInterface) {
-    			EditRelationshipPageInterface epi = (EditRelationshipPageInterface)nextPage;
-    			epi.setMasterObjectAndRelationshipKey(object(), key());
-    			epi.setNextPage(context().page());
-    		} else if(nextPage instanceof EditPageInterface) {
-    	    	Object value = d2wContext().valueForKey("useNestedEditingContext");
-    	    	boolean createNestedContext = ERXValueUtilities.booleanValue(value);
-    	    	EOEnterpriseObject object = ERXEOControlUtilities.editableInstanceOfObject(object(), createNestedContext);
-	 			EOEditingContext ec = object.editingContext();
-    			ec.lock();
-    			try {
-    				EOEnterpriseObject eo = ERXEOControlUtilities.createAndAddObjectToRelationship(ec, object, key(), (String)valueForBinding("destinationEntityName"), null);
-    				EditPageInterface epi = (EditPageInterface)nextPage;
-    				epi.setObject(eo);
-    				epi.setNextPage(context().page());
-    			} finally {
-    				ec.unlock();
-    			}
-    		}
-    	} else {
-    		ERXEOControlUtilities.createAndAddObjectToRelationship(object().editingContext(), object(), key(), (String)valueForBinding("destinationEntityName"), null);
-    	}
+    	// if creationDelegate binding is provided then just call the delegate with the right params.
+    	// delegate is responsible for making sure the right page is returned after creating hte object.
+        if (useCreationDelegate ()) {
+            EOEnterpriseObject obj = masterObjectKeyPathForCreationDelegate ();
+            // defaults to object(), if masterObjectKeyPath is not provided
+            obj = (obj == null) ? object () : obj;
+            String relationshipName = relationshipName ();
+            // defaults to key(), if the relationshipName binding is not provided
+            relationshipName = relationshipName == null ? key () : relationshipName;
+            nextPage = createObjectDelegate ().create (obj, (String) valueForBinding ("destinationEntityName"), relationshipName);
+        }
+        else {
+        	String editRelationshipConfigurationName = (String)valueForBinding("editRelationshipConfigurationName");
+        	if(editRelationshipConfigurationName != null && editRelationshipConfigurationName.length() > 0) {
+        		nextPage = D2W.factory().pageForConfigurationNamed(editRelationshipConfigurationName, session());
+        		if(nextPage instanceof EditRelationshipPageInterface) {
+        			EditRelationshipPageInterface epi = (EditRelationshipPageInterface)nextPage;
+        			epi.setMasterObjectAndRelationshipKey(object(), key());
+        			epi.setNextPage(context().page());
+        		} else if(nextPage instanceof EditPageInterface) {
+        	    	Object value = d2wContext().valueForKey("useNestedEditingContext");
+        	    	boolean createNestedContext = ERXValueUtilities.booleanValue(value);
+        	    	EOEnterpriseObject object = ERXEOControlUtilities.editableInstanceOfObject(object(), createNestedContext);
+    	 			EOEditingContext ec = object.editingContext();
+        			ec.lock();
+        			try {
+        				EOEnterpriseObject eo = ERXEOControlUtilities.createAndAddObjectToRelationship(ec, object, key(), (String)valueForBinding("destinationEntityName"), null);
+        				EditPageInterface epi = (EditPageInterface)nextPage;
+        				epi.setObject(eo);
+        				epi.setNextPage(context().page());
+        			} finally {
+        				ec.unlock();
+        			}
+        		}
+        	} else {
+        		ERXEOControlUtilities.createAndAddObjectToRelationship(object().editingContext(), object(), key(), (String)valueForBinding("destinationEntityName"), null);
+        	}
+        }
     	return nextPage;
     }
     // we will get asked quite a lot of times, so caching is in order
@@ -133,5 +146,110 @@ public class ERDList extends ERDCustomEditComponent {
         log.info("asked for emptyListMessage");
         return "nix";
     }
+    
+    
+    /**
+     * Interface that all createObjectDelegate classes should implement
+     */
+    public static interface CreateObjectDelegate {
 
+        /**
+         * method used to create a newInstance of
+         * <code>destinationEntityNameM</code>.
+         * <p>
+         * Note: this delegate is responsible for making sure that the object is created properly and 
+         * the right page is returned.
+         * 
+         * @param parentObject
+         *            object to which the newly created object should be linked
+         * @param destinationEntityName
+         *            entity name of the destination in the list that we are
+         *            creating
+         * @param relationshipNameToAddTo
+         *            relationship name that should be used to link the newly
+         *            created instance to the <code>parentObject</code>
+         * @return {@link WOComponent} editPage for the destination entity (usually...)
+         */
+        public WOComponent create (Object parentObject, String destinationEntityName, String relationshipNameToAddTo);
+    }
+
+    /**
+     * @return {@link CreateObjectDelegate} instance from the rule
+     *         file/bindings. this is responsible for creating the object and
+     *         displaying the appropriate page.
+     *         <p>
+     *         Note: if this is binding is present, then the usual method of
+     *         figuring out what to create from
+     *         <code>destinationEntityName</code> and <code>object</code>
+     *         binding doesn't happen.
+     */
+    public CreateObjectDelegate createObjectDelegate () {
+        Object obj = valueForBinding ("createObjectDelegate");
+        CreateObjectDelegate delegate = new DefaultCreateObjectDelegate ();
+        if (obj != null && obj instanceof CreateObjectDelegate) {
+            delegate = (CreateObjectDelegate) obj;
+        }
+
+        return delegate;
+    }
+
+    /**
+     * @return true, if <code>createObjectDelegate</code> resolved to a valid
+     *         object. false, otherwise
+     */
+    public Boolean hasCreationDelegate () {
+        return createObjectDelegate () != null;
+    }
+
+    /**
+     * @return {@link Boolean} if true, createObjectAction method will use
+     *         delegate (either the one that is provided or the default one.
+     */
+    public Boolean useCreationDelegate () {
+        return ERXValueUtilities.booleanValue (valueForBinding ("useCreationDelegate"));
+    }
+
+    /**
+     * @return the keyPath to be used to compute the master object used by
+     *         {@link CreateObjectDelegate}. whatever this keypath resolves to
+     *         will be used to attach the newly created object.
+     */
+    public EOEnterpriseObject masterObjectKeyPathForCreationDelegate () {
+        return (EOEnterpriseObject) valueForBinding ("masterObjectKeyPathForCreationDelegate");
+    }
+
+    /**
+     * @return {@link String} relationshipName to connect <code>masterObj</code>
+     *         and the newly created object. this will be used to add the newly
+     *         createdObjects to both sides of relationship.
+     */
+    public String relationshipName () {
+        return (String) valueForBinding ("relationshipNameForCreationDelegate");
+    }
+    
+    /**
+     * Default creation delegate class. if useCreationDelegate binding is true
+     * and no valid createObjectDelegate is present, then this class will be
+     * used.
+     * 
+     * @author rajaram
+     */
+    public class DefaultCreateObjectDelegate implements CreateObjectDelegate {
+        /**
+         * {@inheritDoc}
+         */
+        public WOComponent create (Object parentObject, String destinationEntityName, String relationshipNameToAddTo) {
+            if (! (parentObject instanceof EOEnterpriseObject)) {
+                throw new IllegalArgumentException ("only instances of EOEnterpriseObject can be handled by DefaultCreateObjectDelegate.");
+            }
+            
+            EOEnterpriseObject masterObj = (EOEnterpriseObject) parentObject;
+            EOEnterpriseObject newEO = ERXEOControlUtilities.createAndAddObjectToRelationship (masterObj.editingContext (), masterObj, relationshipNameToAddTo, destinationEntityName, null);
+            
+            EditPageInterface epi = D2W.factory ().editPageForEntityNamed (destinationEntityName, session ());
+            epi.setObject (newEO);
+            
+            return (WOComponent) epi;
+        }
+    }
 }
