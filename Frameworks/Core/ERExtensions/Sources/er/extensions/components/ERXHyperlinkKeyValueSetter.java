2b44d8a697951eb584df4ab416f6b3a3f5331f46
anjo <anjo@8dc76679-1c23-de11-85cd-0050568e728b>
2010-07-28 13:42:23 +0000

    added action binding, component content, css class, onclick. string now optional

--- Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXHyperlinkKeyValueSetter.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXHyperlinkKeyValueSetter.java
@@ -6,15 +6,20 @@
  * included with this distribution in the LICENSE.NPL file.  */
 package er.extensions.components;
 
+import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+
+import er.extensions.ERXExtensions;
 
 /**
  * Sets a key value when the hyperlink is clicked.<br />
  * 
  * @binding value
  * @binding binding
- * @binding string
+ * @binding string optional link text
+ * @binding action optional action
+ * @binding class optional css class name
  */
 
 public class ERXHyperlinkKeyValueSetter extends WOComponent {
@@ -25,8 +30,13 @@ public class ERXHyperlinkKeyValueSetter extends WOComponent {
 
     public boolean isStateless() { return true; }
     
-    public WOComponent action() {
+    public WOActionResults action() {
         setValueForBinding(valueForBinding("value"), "binding");
-        return null;
+        return (WOActionResults) (canGetValueForBinding("action") ? valueForBinding("action") : null);
+    }
+    
+    public boolean disabled() {
+    	Object val = valueForBinding("binding");
+    	return ERXExtensions.safeEquals(val, valueForBinding("value"));
     }
 }
