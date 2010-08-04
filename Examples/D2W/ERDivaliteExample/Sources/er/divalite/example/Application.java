095350e2a005bc9198b82e496838720bb2cbf019
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2010-08-04 02:00:32 +0000

    Update for recent ERMoviesLogic changes

--- Examples/D2W/ERDivaliteExample/Sources/er/divalite/example/Application.java
+++ Examples/D2W/ERDivaliteExample/Sources/er/divalite/example/Application.java
@@ -1,5 +1,8 @@
 package er.divalite.example;
 
+import webobjectsexamples.businesslogic.ERMoviesLogic;
+
+import com.webobjects.foundation.NSBundle;
 import com.webobjects.foundation.NSLog;
 
 import er.extensions.appserver.ERXApplication;
@@ -13,5 +16,15 @@ public class Application extends ERXApplication {
         NSLog.out.appendln("Welcome to " + this.name() + " !");
         /* ** put your initialization code in here ** */
 		setDefaultRequestHandler(requestHandlerForKey(directActionRequestHandlerKey()));
+    }
+    
+    @Override
+    public void finishInitialization() {
+    	super.finishInitialization();
+    	
+        NSBundle bundle = NSBundle.bundleForClass(ERMoviesLogic.class);
+        String file = bundle.bundlePath() + "/Resources/Movies";
+        System.setProperty("Movies.URL", "jdbc:h2:file:" + file);
+        System.setProperty("Rentals.URL", "jdbc:h2:file:" + file);
     }
 }
