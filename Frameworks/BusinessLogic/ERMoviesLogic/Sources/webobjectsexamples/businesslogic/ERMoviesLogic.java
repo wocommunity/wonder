2c618490c339b4029331da4fe418bfce6907400f
davidleber <davidleber@8dc76679-1c23-de11-85cd-0050568e728b>
2010-08-24 18:47:15 +0000

    Re-enabled connection dictionary twiddling in finishInitializing of ERMoviesLogic to enable the use of the embedded H2 database. Enabled with the webobjectsexamples.ERMoviesLogic.useEmbeddedH2Database=true property.

--- Frameworks/BusinessLogic/ERMoviesLogic/Sources/webobjectsexamples/businesslogic/ERMoviesLogic.java
+++ Frameworks/BusinessLogic/ERMoviesLogic/Sources/webobjectsexamples/businesslogic/ERMoviesLogic.java
@@ -1,8 +1,10 @@
 package webobjectsexamples.businesslogic;
 
 import com.webobjects.foundation.NSBundle;
+import com.webobjects.foundation.NSLog;
 
 import er.extensions.ERXFrameworkPrincipal;
+import er.extensions.foundation.ERXProperties;
 
 public class ERMoviesLogic extends ERXFrameworkPrincipal {
     
@@ -12,11 +14,19 @@ public class ERMoviesLogic extends ERXFrameworkPrincipal {
 
     @Override
     public void finishInitialization() {
-        NSBundle bundle = NSBundle.bundleForClass(ERMoviesLogic.class);
-        // AK: deprecation, I know,,,
-//        String file = bundle.bundlePath() + "/Resources/Movies";
-//        System.setProperty("Movies.URL", "jdbc:h2:file:" + file);
-//        System.setProperty("Rentals.URL", "jdbc:h2:file:" + file);
+    	
+    	if (ERXProperties.booleanForKeyWithDefault("webobjectsexamples.ERMoviesLogic.useEmbeddedH2Database", false)) {
+    		NSBundle bundle = NSBundle.bundleForClass(ERMoviesLogic.class);
+
+    		String file = bundle.bundlePathURL().getPath() + "/Resources/Movies";
+    		String h2URL = "jdbc:h2:file:" + file;
+    		NSLog.out.appendln("ERMoviesLogic.finishInitialization, re-writing connection url to use embedded H2 db at: " + h2URL);
+    		System.setProperty("Movies.URL", h2URL);
+    		System.setProperty("Rentals.URL", h2URL);
+    		System.setProperty("ERAttachment.URL", h2URL);
+    		System.setProperty("ERTaggable.URL", h2URL);
+    	}
+		
     }
 
 }
