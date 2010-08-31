fc3abdd4e9adba62ec15f1448a7d89c7f4be2147
davidleber <davidleber@8dc76679-1c23-de11-85cd-0050568e728b>
2010-08-31 17:49:19 +0000

    Updated lucene version to 2.9.3, Added ERDocument (a thin wrapper around lucene's Document), added a couple of methods to ERIndex and deprecated some old ones.

---
+++ Frameworks/EOF/ERIndexing/Sources/er/indexing/ERDocument.java
@@ -0,0 +1,55 @@
+package er.indexing;
+
+import org.apache.lucene.document.Document;
+
+import com.webobjects.eocontrol.EOKeyGlobalID;
+import com.webobjects.foundation.NSKeyValueCoding;
+
+import er.extensions.eof.ERXKeyGlobalID;
+
+public class ERDocument implements NSKeyValueCoding {
+	
+	private static final String GID = "EOGlobalID";
+	 
+	private Document _doc;
+	private Float _score;
+	
+	public ERDocument(Document doc, float score) {
+		super();
+		_doc = doc;
+		_score = new Float(score);
+	}
+	
+	// ACCESSORS
+	
+	public Float score() {
+		return _score;
+	}
+	
+	public void setScore(Float score) {
+		_score = score;
+	}
+	
+	public EOKeyGlobalID eoKeyGlobalId() {
+		String gidString = _doc.get(GID);
+		EOKeyGlobalID gid = ERXKeyGlobalID.fromString(gidString).globalID();
+		return gid;
+	}
+	
+	// KVC
+	
+	@Override
+	public Object valueForKey(String key) {
+		Object result =  _doc.get(key);
+		if (result == null) {
+			result =  NSKeyValueCoding.DefaultImplementation.valueForKey(this, key);
+		}
+		return result;
+	}
+	
+	@Override
+	public void takeValueForKey(Object obj, String key) {
+		// do nuttin'
+	}
+	
+}
