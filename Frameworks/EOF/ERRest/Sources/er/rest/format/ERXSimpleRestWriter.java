fadc41e23f76c5e6e82415c692bbe5d07e6aae4e
phigminter <phigminter@8dc76679-1c23-de11-85cd-0050568e728b>
2010-08-31 15:21:25 +0000

    Removed interface override annotations which drive 1.5 javac nuts.

--- Frameworks/EOF/ERRest/Sources/er/rest/format/ERXSimpleRestWriter.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/format/ERXSimpleRestWriter.java
@@ -5,12 +5,10 @@ import er.rest.format.ERXRestFormat.Delegate;
 
 public class ERXSimpleRestWriter implements IERXRestWriter {
 
-	@Override
 	public void appendHeadersToResponse(ERXRestRequestNode node, IERXRestResponse response) {
 		
 	}
 
-	@Override
 	public void appendToResponse(ERXRestRequestNode node, IERXRestResponse response, Delegate delegate) {
 		response.appendContentString(node.toString());
 	}
