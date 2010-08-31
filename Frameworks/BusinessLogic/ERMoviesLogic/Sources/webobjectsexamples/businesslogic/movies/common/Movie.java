fee0957d0f53e85cd5affa2734ba5d5f7be7f857
davidleber <davidleber@8dc76679-1c23-de11-85cd-0050568e728b>
2010-08-31 17:13:41 +0000

    Added aggregate 'content' method needed for lucene demo

--- Frameworks/BusinessLogic/ERMoviesLogic/Sources/webobjectsexamples/businesslogic/movies/common/Movie.java
+++ Frameworks/BusinessLogic/ERMoviesLogic/Sources/webobjectsexamples/businesslogic/movies/common/Movie.java
@@ -2,6 +2,9 @@ package webobjectsexamples.businesslogic.movies.common;
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSLog;
+
 import er.taggable.ERTaggable;
 import er.taggable.ERTaggableEntity;
 
@@ -14,5 +17,40 @@ public class Movie extends _Movie {
     
     public static ERTaggableEntity<Movie> taggableEntity() {
         return ERTaggableEntity.taggableEntity(Movie.ENTITY_NAME);
+    }
+    
+    @SuppressWarnings("all")
+    public String content() {
+    	if (log.isDebugEnabled()) {
+    		log.debug( "Movie.searchableContent: " + this.title() );
+    	}
+    	StringBuffer buffer = new StringBuffer();
+    	
+    	buffer.append(this.title());
+    	buffer.append(" ");
+    	
+    	String studioName = (String)this.valueForKeyPath(Movie.STUDIO.dot(Studio.NameKey).toString());
+    	if (studioName != null) {
+    		buffer.append(studioName);
+    		buffer.append(" ");
+    	}
+    	
+		NSArray directorNames = (NSArray)this.valueForKeyPath(Movie.DIRECTORS.dot("fullName").toString());
+		if (directorNames != null && directorNames.count() > 0) {
+			buffer.append(directorNames.componentsJoinedByString(" "));
+			buffer.append(" ");
+		}
+		
+		NSArray talentNames = (NSArray)this.valueForKeyPath(Movie.ROLES.dot(MovieRole.TALENT).dot("fullName").toString());
+		if (talentNames != null && talentNames.count() > 0) {
+			String talentNamesString = talentNames.componentsJoinedByString(" ");
+			NSLog.out.appendln( "Movie.searchableContent: talent names: " + talentNamesString);
+			buffer.append(talentNamesString);
+			buffer.append(" ");
+		}
+		if (log.isDebugEnabled()) {
+			log.debug( "Movie.content: " + buffer );
+		}
+    	return buffer.toString();
     }
 }
