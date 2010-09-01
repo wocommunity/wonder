75f274b12a75cddc72370de6cba90c2d320956b6
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-01 06:05:34 +0000

    Merge Wonder 2.0 10820: Augmented AjaxExpansion to support setting closedLabel and openedLabel (like WOCollapsibleComponentContent) and pass-through of the on... functions to the underlying AjaxUpdateLink. (travis mostly did this, but didn't bring over the defaults ... not sure why)

--- Frameworks/Ajax/Ajax/Sources/er/ajax/AjaxExpansion.java
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/AjaxExpansion.java
@@ -92,9 +92,9 @@ public class AjaxExpansion extends AjaxComponent {
         String string = (String) valueForBinding("string");
         if (null == string) {
             if (isExpanded()) {
-                string = (String)valueForBinding("openedLabel");
+                string = (String)valueForBinding("openedLabel", "Close");
             } else {
-                string = (String)valueForBinding("closedLabel");
+                string = (String)valueForBinding("closedLabel", "Open");
             }
         }
         return string;
