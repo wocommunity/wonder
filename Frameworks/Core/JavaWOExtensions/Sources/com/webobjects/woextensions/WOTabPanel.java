2999569304dda08a449082a17c4937ad30c4221c
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-01 08:01:33 +0000

    Merge Wonder 2.0 11102: findbugs H V MS: com.webobjects.woextensions.WOTabPanel._undefinedMarker isn't final but should be At WOTabPanel.java:[line 35]

--- Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/WOTabPanel.java
+++ Frameworks/Core/JavaWOExtensions/Sources/com/webobjects/woextensions/WOTabPanel.java
@@ -33,7 +33,7 @@ import com.webobjects.foundation.NSKeyValueCodingAdditions;
 
 public class WOTabPanel extends WOComponent
 {
-    protected static String  _undefinedMarker="UNDEFINED";
+    protected static final String  _undefinedMarker="UNDEFINED";
 
     public Object  currentTab;
     protected Object  _selectedTab;
