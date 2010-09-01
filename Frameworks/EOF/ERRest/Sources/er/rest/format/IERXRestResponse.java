ecd6f01803100b70e283bcaf139f72f44ec1291d
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-01 05:26:20 +0000

    removing old merge of ERRest into Wonder 2.0/5.4

--- Frameworks/EOF/ERRest/Sources/er/rest/format/IERXRestResponse.java
+++
@@ -1,9 +0,0 @@
-package er.rest.format;
-
-public interface IERXRestResponse {
-	public void setHeader(String value, String key);
-
-	public void appendContentCharacter(char _ch);
-
-	public void appendContentString(String _str);
-}
