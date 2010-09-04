69f534e5852b4a5d716ea9b3887ddaf3b37224d5
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-04 02:12:07 +0000

    whoops .. accidental commit of some half-baked old changes

--- Frameworks/PlugIns/EROraclePlugIn/Sources/com/webobjects/jdbcadaptor/EROraclePlugIn.java
+++ Frameworks/PlugIns/EROraclePlugIn/Sources/com/webobjects/jdbcadaptor/EROraclePlugIn.java
@@ -1,6 +1,5 @@
 package com.webobjects.jdbcadaptor;
 
-import com.webobjects.foundation.NSDictionary;
 
 /** Overrides OraclePlugIn in order to provide the modified
  * EROracleExpression class to EOF.
@@ -20,14 +19,9 @@ public class EROraclePlugIn extends OraclePlugIn {
     return EROracleExpression.class;
   }
 
-  @Override
-  public NSDictionary jdbcInfo() {
-    return super.jdbcInfo();
-  }
-
-  /* (non-Javadoc)
-   * @see com.webobjects.jdbcadaptor.JDBCPlugIn#createSynchronizationFactory()
-   */
+//  /* (non-Javadoc)
+//   * @see com.webobjects.jdbcadaptor.JDBCPlugIn#createSynchronizationFactory()
+//   */
 //  public EOSynchronizationFactory createSynchronizationFactory() {
 //    return new EROracleSynchronizationFactory(adaptor());
 //  }
