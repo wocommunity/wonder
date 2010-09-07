6857c4429262073dbb022bae0366bb1f3856c914
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-07 04:55:07 +0000

    * Kind of crazy, experimental dojo-style rest transactions -- use at your own risk right now * ERXRest.transactionsEnabled property (defaults false, again ... experimental .. this may blow everything up) * ERXRest.defaultFormat property ("js", "xml", etc) * JSON schema (experimental, some rough edges still -- like recursive). In your add if (isSchemaRequest()) { return schemaResponse(queryFilter()) } in your index action * Always add the session id cookies to rest responses * Updated exmorph to 1.0.6 * Updated json-lib to 2.3

---
+++ Frameworks/EOF/ERRest/Sources/er/rest/util/ERXRestTransactionManager.java
@@ -0,0 +1,165 @@
+package er.rest.util;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSMutableDictionary;
+
+import er.extensions.eof.ERXEC;
+
+public class ERXRestTransactionManager {
+	private NSMutableDictionary<String, ERXRestTransaction> _transactions;
+	private IntRangeSet _sequenceIDs;
+
+	public ERXRestTransactionManager() {
+		_transactions = new NSMutableDictionary<String, ERXRestTransaction>();
+		_sequenceIDs = new IntRangeSet();
+	}
+
+	protected EOEditingContext newEditingContext() {
+		return ERXEC.newEditingContext();
+	}
+
+	public void addSequenceID(int sequenceID) {
+		_sequenceIDs.add(sequenceID);
+	}
+
+	public ERXRestTransaction transactionForID(String id) {
+		ERXRestTransaction transaction = _transactions.objectForKey(id);
+		if (transaction == null) {
+			transaction = new ERXRestTransaction(id, newEditingContext());
+			_transactions.setObjectForKey(transaction, id);
+		}
+		return transaction;
+	}
+	
+	public void removeTransaction(ERXRestTransaction transaction) {
+		_transactions.removeObjectForKey(transaction.identifier());
+	}
+
+	public boolean isTransactionReady(ERXRestTransaction transaction) {
+		boolean transactionReady = false;
+		// If we have a commit, the transaction might be ready
+		if (transaction.hasCommit()) {
+			int minimumSequenceID = transaction.minimumSequenceID();
+			int maximumSequenceID = transaction.maximumSequenceID();
+			// If we've seen every sequence ID in this transaction, the transaction might be ready
+			if (_sequenceIDs.contains(minimumSequenceID, maximumSequenceID)) {
+				// If the minimum sequence ID was the lowest we can possibly see, the transaction is ready
+				if (minimumSequenceID == minimumPossibleSequenceID()) {
+					transactionReady = true;
+				}
+				// If we've seen the sequence ID just below the minimum, the transaction is ready
+				else if (_sequenceIDs.contains(minimumSequenceID - 1)) {
+					transactionReady = true;
+				}
+				else {
+					// This is the vague part. Say the transaction is [5-10] and 4 got dropped by the
+					// client somehow. WTF now? Technically the transaction is ready, but we have no
+					// way to know because we don't know if 4 was the real start of the transaction.
+					// To protect against a hung transaction, we should probably let it go, but
+					// from a safety perspective, we just don't know.
+				}
+			}
+		}
+		return transactionReady;
+	}
+
+	protected int minimumPossibleSequenceID() {
+		return 1;
+	}
+
+	public static class IntRangeSet {
+		private List<IntRange> _ranges;
+
+		public IntRangeSet() {
+			_ranges = new LinkedList<IntRange>();
+		}
+
+		public boolean contains(int start, int end) {
+			boolean contains = false;
+			for (IntRange range : _ranges) {
+				if (start >= range.start && end <= range.end) {
+					contains = true;
+					break;
+				}
+				else if (range.start > start) {
+					break;
+				}
+			}
+			return contains;
+		}
+
+		public boolean contains(int value) {
+			boolean contains = false;
+			for (IntRange range : _ranges) {
+				if (value >= range.start && value <= range.end) {
+					contains = true;
+					break;
+				}
+				else if (range.start > value) {
+					break;
+				}
+			}
+			return contains;
+		}
+
+		public void add(int value) {
+			Iterator<IntRange> rangeIter = _ranges.iterator();
+			int index = 0;
+			boolean valueAdded = false;
+			while (rangeIter.hasNext()) {
+				IntRange range = rangeIter.next();
+				if (value <= range.end) {
+					if (value < range.start) {
+						if (value == range.start - 1) {
+							range.start = value;
+						}
+						else {
+							_ranges.add(index, new IntRange(value, value));
+						}
+					}
+					valueAdded = true;
+					break;
+				}
+				else if (value == range.end + 1) {
+					range.end = value;
+					if (rangeIter.hasNext()) {
+						IntRange nextRange = rangeIter.next();
+						if (nextRange.start == range.end + 1) {
+							range.end = nextRange.end;
+							rangeIter.remove();
+						}
+					}
+					valueAdded = true;
+					break;
+				}
+				index++;
+			}
+			if (!valueAdded) {
+				_ranges.add(new IntRange(value, value));
+			}
+		}
+
+		protected static class IntRange {
+			public int start;
+			public int end;
+
+			public IntRange(int start, int end) {
+				this.start = start;
+				this.end = end;
+			}
+
+			public boolean contains(int value) {
+				return value >= start && value <= end;
+			}
+
+			public String toString() {
+				return "[" + start + "-" + end + "]";
+			}
+		}
+
+	}
+}
---
+++ Frameworks/EOF/ERRest/Sources/er/rest/util/ERXRestTransactionRequestAdaptor.java
@@ -0,0 +1,215 @@
+package er.rest.util;
+
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WORequest;
+import com.webobjects.appserver.WOResponse;
+import com.webobjects.appserver.WOSession;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableDictionary;
+
+import er.extensions.appserver.ERXSession;
+import er.extensions.eof.ERXEC;
+import er.extensions.foundation.ERXProperties;
+import er.rest.routes.ERXRouteRequestHandler;
+
+/**
+ * EXPERIMENTAL.
+ * 
+ * @author mschrag
+ */
+public class ERXRestTransactionRequestAdaptor {
+	private static final String CLIENT_ID_HEADER_KEY = "Client-Id";
+	private static final String SEQUENCE_ID_HEADER_KEY = "Seq-Id";
+	private static final String TRANSACTION_HEADER_KEY = "Transaction";
+	private static final String OPEN_TRANSACTION_HEADER_VALUE = "open";
+	private static final String COMMIT_TRANSACTION_HEADER_VALUE = "commit";
+
+	private static final String EXECUTING_TRANSACTION_KEY = "er.rest.ERXRestTransaction.transaction";
+	private static final String TRANSACTION_MANAGER_KEY = "er.rest.ERXRestTransaction.transactionManager";
+	private static ERXRestTransactionRequestAdaptor _defaultAdaptor;
+
+	private boolean _transactionsEnabled;
+	private int _maxEventsPerTransaction;
+	
+	public static synchronized ERXRestTransactionRequestAdaptor defaultAdaptor() {
+		if (_defaultAdaptor == null) {
+			_defaultAdaptor = new ERXRestTransactionRequestAdaptor();
+		}
+		return _defaultAdaptor;
+	}
+
+	public ERXRestTransactionRequestAdaptor() {
+		_transactionsEnabled = ERXProperties.booleanForKeyWithDefault("ERXRest.transactionsEnabled", false);
+		_maxEventsPerTransaction = ERXProperties.intForKeyWithDefault("ERXRest.maxEventsPerTransaction", 50);
+	}
+
+	protected EOEditingContext newEditingContext() {
+		return ERXEC.newEditingContext();
+	}
+
+	public boolean transactionsEnabled() {
+		return _transactionsEnabled;
+	}
+	
+	public boolean hasSequence(WOContext context, WORequest request) {
+		return request.headerForKey(ERXRestTransactionRequestAdaptor.SEQUENCE_ID_HEADER_KEY) != null;
+	}
+
+	public boolean hasTransaction(WOContext context, WORequest request) {
+		return request.headerForKey(ERXRestTransactionRequestAdaptor.TRANSACTION_HEADER_KEY) != null;
+	}
+
+	public boolean isExecutingTransaction(WOContext context, WORequest request) {
+		return executingTransaction(context, request) != null;
+	}
+	
+	public ERXRestTransaction executingTransaction(WOContext context, WORequest request) {
+		ERXRestTransaction transaction = null;
+		@SuppressWarnings("unchecked")
+		NSDictionary<String, Object> userInfo = request.userInfo();
+		if (userInfo != null) {
+			transaction = (ERXRestTransaction)userInfo.objectForKey(ERXRestTransactionRequestAdaptor.EXECUTING_TRANSACTION_KEY);
+		}
+		return transaction;
+	}
+	
+	protected void setExecutingTransaction(ERXRestTransaction transaction, WOContext context, WORequest request) {
+		@SuppressWarnings("unchecked")
+		NSDictionary<String, Object> immutableUserInfo = request.userInfo();
+		NSMutableDictionary<String, Object> userInfo = (immutableUserInfo == null) ? new NSMutableDictionary<String, Object>() : immutableUserInfo.mutableClone();
+		userInfo.setObjectForKey(transaction, ERXRestTransactionRequestAdaptor.EXECUTING_TRANSACTION_KEY);
+		request.setUserInfo(userInfo);
+	}
+	
+	public ERXRestTransaction transaction(WOContext context, WORequest request) {
+		ERXRestTransaction transaction = null;
+		if (transaction == null) {
+			ERXRestTransactionManager transactionManager = transactionManager(context, request);
+			transaction = transaction(context, request, transactionManager);
+		}
+		return transaction;
+	}
+
+	public boolean willHandleRequest(WOContext context, WORequest request) {
+		boolean shouldDispatchRequest = true;
+		Integer sequenceIDInteger = sequenceID(request);
+		if (sequenceIDInteger != null) {
+			int sequenceID = sequenceIDInteger.intValue();
+			ERXRestTransactionManager transactionManager = transactionManager(context, request);
+			transactionManager.addSequenceID(sequenceID);
+
+			ERXRestTransaction.State state = state(request);
+			if (state != null) {
+				ERXRestTransaction transaction = transaction(context, request, transactionManager);
+				transaction.addEvent(sequenceID, state, request);
+				shouldDispatchRequest = false;
+
+				if (transaction.size() > _maxEventsPerTransaction) {
+					transactionManager.removeTransaction(transaction);
+					throw new IllegalArgumentException("You exceeded the maximum number of events for a single transaction.");
+				}
+			}
+		}
+		return shouldDispatchRequest;
+	}
+	
+	public boolean didHandleRequest(WOContext context, WORequest request) {
+		boolean shouldHandleRequest = true;
+		ERXRestTransactionManager transactionManager = transactionManager(context, request);
+		ERXRestTransaction transaction = transaction(context, request, transactionManager);
+		if (transactionManager.isTransactionReady(transaction)) {
+			shouldHandleRequest = false;
+
+			// MS: This is sketchy -- basically we're about to execute a pile of requests on the same thread and we can't
+			// check out the session again on the same thread, so we're going to forcefully check it back in
+			if (context._session() != null) {
+				// MS: Should we sleep it? Should we just not do this at all?
+				// wosession._sleepInContext(null);
+				WOApplication.application().sessionStore().checkInSessionForContext(context);
+				context._setSession(null);
+				ERXSession.setSession(null);
+			}
+			try {
+				EOEditingContext editingContext = transaction.editingContext();
+				try {
+					for (Object record : transaction.records()) {
+						WORequest recordRequest = (WORequest)record;
+						setExecutingTransaction(transaction, context, recordRequest);
+						recordRequest.removeHeadersForKey(ERXRestTransactionRequestAdaptor.CLIENT_ID_HEADER_KEY);
+						recordRequest.removeHeadersForKey(ERXRestTransactionRequestAdaptor.SEQUENCE_ID_HEADER_KEY);
+						recordRequest.removeHeadersForKey(ERXRestTransactionRequestAdaptor.TRANSACTION_HEADER_KEY);
+						
+						ERXRouteRequestHandler requestHandler = (ERXRouteRequestHandler)WOApplication.application().handlerForRequest(recordRequest);
+						WOResponse response = requestHandler.handleRequest(recordRequest);
+						if (response.status() < 200 || response.status() > 299) {
+							throw new RuntimeException("Transaction failed: " + response.contentString());
+						}
+					}
+					editingContext.saveChanges();
+				}
+				finally {
+					transactionManager.removeTransaction(transaction);
+					editingContext.dispose();
+				}
+			}
+			finally {
+				context.session();
+			}
+		}
+		return shouldHandleRequest;
+	}
+
+	protected String clientID(WORequest request) {
+		return request.headerForKey(ERXRestTransactionRequestAdaptor.CLIENT_ID_HEADER_KEY);
+	}
+
+	protected Integer sequenceID(WORequest request) {
+		Integer sequenceID = null;
+		String sequenceIDStr = request.headerForKey(ERXRestTransactionRequestAdaptor.SEQUENCE_ID_HEADER_KEY);
+		if (sequenceIDStr != null) {
+			sequenceID = Integer.parseInt(sequenceIDStr);
+		}
+		return sequenceID;
+	}
+
+	protected ERXRestTransaction.State state(WORequest request) {
+		String stateStr = request.headerForKey(ERXRestTransactionRequestAdaptor.TRANSACTION_HEADER_KEY);
+		ERXRestTransaction.State state;
+		if (stateStr == null) {
+			state = null;
+		}
+		else if (ERXRestTransactionRequestAdaptor.COMMIT_TRANSACTION_HEADER_VALUE.equals(stateStr)) {
+			state = ERXRestTransaction.State.Commit;
+		}
+		else if (ERXRestTransactionRequestAdaptor.OPEN_TRANSACTION_HEADER_VALUE.equals(stateStr)) {
+			state = ERXRestTransaction.State.Open;
+		}
+		else {
+			throw new IllegalArgumentException("Unknown transaction state: " + stateStr);
+		}
+		return state;
+	}
+
+	protected ERXRestTransactionManager transactionManager(WOContext context, WORequest request) {
+		WOSession session = context.session();
+		ERXRestTransactionManager transactionManager = (ERXRestTransactionManager) session.objectForKey(ERXRestTransactionRequestAdaptor.TRANSACTION_MANAGER_KEY);
+		if (transactionManager == null) {
+			transactionManager = new ERXRestTransactionManager();
+			session.setObjectForKey(transactionManager, ERXRestTransactionRequestAdaptor.TRANSACTION_MANAGER_KEY);
+		}
+		return transactionManager;
+	}
+
+	protected ERXRestTransaction transaction(WOContext context, WORequest request, ERXRestTransactionManager transactionManager) {
+		WOSession session = context.session();
+
+		String clientID = clientID(request);
+		if (clientID == null) {
+			clientID = session.sessionID();
+		}
+
+		return transactionManager.transactionForID(clientID);
+	}
+}
