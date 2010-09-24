052f839eb9dc435a6f102d809b087ebc81214e01
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-24 13:16:27 +0000

    merge from 5.4

--- Frameworks/EOF/ERRest/Sources/er/rest/format/ERXStringRestRequest.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/format/ERXStringRestRequest.java
@@ -2,13 +2,21 @@ package er.rest.format;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import com.webobjects.foundation.NSForwardException;
+import com.webobjects.foundation._NSStringUtilities;
 
 public class ERXStringRestRequest implements IERXRestRequest {
-
 	private String _contentString;
+	private String _encoding;
 	
 	public ERXStringRestRequest(String contentString) {
+		this(contentString, _NSStringUtilities.UTF8_ENCODING);
+	}
+	public ERXStringRestRequest(String contentString, String encoding) {
 		_contentString = contentString;
+		_encoding = encoding;
 	}
 	
 	public String stringContent() {
@@ -16,7 +24,12 @@ public class ERXStringRestRequest implements IERXRestRequest {
 	}
 
 	public InputStream streamContent() {
-		return new ByteArrayInputStream(_contentString.getBytes());
+		try {
+			return new ByteArrayInputStream(_contentString.getBytes(_encoding));
+		}
+		catch (UnsupportedEncodingException e) {
+			throw NSForwardException._runtimeExceptionForThrowable(e);
+		}
 	}
 
 }
