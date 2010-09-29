c1bb7dfd38795c9a5b8c0cffece7565bbf47378b
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-29 03:03:39 +0000

    Support for A. Vallum's new/true ajax file uploader - implemented separately from the iframe ajax upload widget

---
+++ Frameworks/Ajax/ERJQuery/Sources/er/jquery/widgets/FileUploaderContainer.java
@@ -0,0 +1,32 @@
+package er.jquery.widgets;
+
+import com.webobjects.appserver.WOContext;
+
+/**
+ * @see FileUploader
+ * 
+ * @author mendis
+ *
+ * A style-able file uploader 
+ */
+public class FileUploaderContainer extends FileUploader {
+    public FileUploaderContainer(WOContext context) {
+        super(context);
+    }
+
+	/*
+	 * Bindings/API
+	 */
+	public static interface Bindings extends AjaxUpload.Bindings {
+		public static final String elementName = "elementName";
+	}
+	
+	// accessors
+	private String _elementName() {
+		return (String) valueForBinding(Bindings.elementName);
+	}
+	
+	public String elementName() {
+		return _elementName() != null ? _elementName() : "div";
+	}
+}
