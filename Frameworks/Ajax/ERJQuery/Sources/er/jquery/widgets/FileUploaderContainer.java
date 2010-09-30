49eccc679af0a281655858d26eddf0588e9569b4
rmendis <rmendis@8dc76679-1c23-de11-85cd-0050568e728b>
2010-09-30 02:57:35 +0000

    Implemented proper handling of ajax responses via WO elementID

--- Frameworks/Ajax/ERJQuery/Sources/er/jquery/widgets/FileUploaderContainer.java
+++ Frameworks/Ajax/ERJQuery/Sources/er/jquery/widgets/FileUploaderContainer.java
@@ -1,6 +1,9 @@
 package er.jquery.widgets;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOResponse;
+
+import er.extensions.appserver.ERXResponseRewriter;
 
 /**
  * @see FileUploader
@@ -29,4 +32,19 @@ public class FileUploaderContainer extends FileUploader {
 	public String elementName() {
 		return _elementName() != null ? _elementName() : "div";
 	}
+	
+	// R&R
+    @Override
+	public void appendToResponse(WOResponse response, WOContext context) {    
+    	// open tag with id
+    	response.appendContentString("<" + elementName());
+    	response._appendTagAttributeAndValue("id", id(), false);
+    	response.appendContentCharacter('>');
+    	
+    	super.appendToResponse(response, context);
+    	
+    	// append javascript and close tag
+    	response.appendContentString("<noscript><p>Please enable JavaScript to use file uploader.</p></noscript></div>\n");
+    	response.appendContentString("<script type=\"text/javascript\">" + script() + "</script>");
+    }
 }
