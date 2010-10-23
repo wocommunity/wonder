d8dfc42ea396c89105e9aa19b76fa3b82a63244b
elvisrobert <elvisrobert@8dc76679-1c23-de11-85cd-0050568e728b>
2010-10-23 13:39:08 +0000

    http://issues.objectstyle.org/jira/browse/WONDER-477
    AjaxAutoComplete could need additional Bindings.
    "select" and "onShow" have been added in this patch - they are just forwarded to the scriptaculous component.
    The div that contains the suggestions is clipped due to the surrounding div. This patch moves the div up to the highest level and therefor suppresses clipping - the additional layer takes care for environments with higher z-indexes (scriptaculous uses a hard coded z-index of 1 for a special IE treatment)

--- Frameworks/Ajax/Ajax/WebServerResources/prototype.js
+++ Frameworks/Ajax/Ajax/WebServerResources/prototype.js
@@ -2367,7 +2367,7 @@ Element.Methods = {
   },
 
   getOffsetParent: function(element) {
-    if (element.offsetParent) return $(element.offsetParent);
+    if (element.offsetParent  && Element.visible(element)) return $(element.offsetParent);
     if (element == document.body) return $(element);
 
     while ((element = element.parentNode) && element != document.body)
