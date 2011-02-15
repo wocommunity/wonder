25e43d495d9567192bf68d6eb8faf92207a70559
mschrag <mschrag@8dc76679-1c23-de11-85cd-0050568e728b>
2011-02-15 00:49:43 +0000

    Jean-Francois granted us permission to relicense this under the top level Wonder BSD license.

--- Frameworks/Ajax/Ajax/Sources/er/ajax/json/serializer/NSDataSerializer.java
+++ Frameworks/Ajax/Ajax/Sources/er/ajax/json/serializer/NSDataSerializer.java
@@ -1,23 +1,3 @@
-/*
- * JSON-RPC-Java - a JSON-RPC to Java Bridge with dynamic invocation
- *
- * $Id$
- *
- * Copyright Metaparadigm Pte. Ltd. 2004.
- * Michael Clark <michael@metaparadigm.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public (LGPL)
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details: http://www.gnu.org/
- *
- */
-
 package er.ajax.json.serializer;
 
 import org.jabsorb.serializer.AbstractSerializer;
