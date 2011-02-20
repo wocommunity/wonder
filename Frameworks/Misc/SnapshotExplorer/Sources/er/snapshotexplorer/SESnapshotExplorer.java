995435c21029ae8b2e4be4e8fb6e2f2971b59c7e
elvisrobert <elvisrobert@8dc76679-1c23-de11-85cd-0050568e728b>
2011-02-20 18:58:00 +0000

    http://issues.objectstyle.org/jira/browse/WONDER-671
    Like in D2W, would be nice if SnapshotExplorer shows the Connection URL for Startup, because the can be different depends on ERXRouteRequestHandler is set to WO or RAILS
    
    Patch provided by Ken Ishimoto

--- Frameworks/Misc/SnapshotExplorer/Sources/er/snapshotexplorer/SESnapshotExplorer.java
+++ Frameworks/Misc/SnapshotExplorer/Sources/er/snapshotexplorer/SESnapshotExplorer.java
@@ -1,5 +1,9 @@
 package er.snapshotexplorer;
 
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.foundation.NSLog;
+
+import er.extensions.appserver.ERXApplication;
 import er.rest.ERXRestNameRegistry;
 import er.rest.routes.ERXRouteRequestHandler;
 import er.snapshotexplorer.controllers.SEEntityController;
@@ -20,5 +24,11 @@ public class SESnapshotExplorer {
     requestHandler.addDefaultRoutes("EOModelGroup", false, SEModelGroupController.class);
     requestHandler.addDefaultRoutes("EOModel", false, SEModelController.class);
     requestHandler.addDefaultRoutes("EOEntity", false, SEEntityController.class);
+    
+    // Display "How to Boot" Message in Log
+    WOApplication app = ERXApplication.application();  
+    String entity = requestHandler.controllerPathForEntityNamed("ModelGroup");
+    NSLog.out.appendln("SESnapshotExplorer ModelGroup launch line: " + app.directConnectURL() + "/" + ERXRouteRequestHandler.Key + "/" + entity + ".html");
+
   }
 }
