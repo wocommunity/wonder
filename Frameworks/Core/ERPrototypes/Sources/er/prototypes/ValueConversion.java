78395bb9bed510153ebc512e28180f32d0c0e720
nullterminated <nullterminated@8dc76679-1c23-de11-85cd-0050568e728b>
2011-03-20 00:53:22 +0000

    Attribute prototypes for serializable objects, immutable dictionaries and arrays.

--- Frameworks/Core/ERPrototypes/Sources/er/prototypes/ValueConversion.java
+++ Frameworks/Core/ERPrototypes/Sources/er/prototypes/ValueConversion.java
@@ -1,5 +1,9 @@
 package er.prototypes;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -11,11 +15,21 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.joda.time.LocalTime;
 
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSData;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSForwardException;
+import com.webobjects.foundation.NSPropertyListSerialization;
+
+import er.extensions.foundation.ERXMutableArray;
+import er.extensions.foundation.ERXMutableDictionary;
+
 /**
  * ValueConversion provides static methods to convert EOAttribute values 
  * into values stored by the database.
  */
 public class ValueConversion {
+	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(ValueConversion.class);
 	private static final TimeZone GMT = TimeZone.getTimeZone("GMT");
 	
@@ -51,5 +65,41 @@ public class ValueConversion {
 	public static Date jodaDateTime(DateTime value) {
 		Date d = new Date(value.getMillis());
 		return d;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static String stringArray(NSArray value) {
+		return NSPropertyListSerialization.stringFromPropertyList(value);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static NSData blobArray(NSArray value) {
+		return ERXMutableArray.toBlob(value);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static String stringDictionary(NSDictionary value) {
+		return NSPropertyListSerialization.stringFromPropertyList(value);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static NSData blobDictionary(NSDictionary value) {
+		return ERXMutableDictionary.toBlob(value);
+	}
+	
+	public static byte[] serializable(Serializable value) {
+		ObjectOutputStream oout = null;
+		
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			oout = new ObjectOutputStream(baos);
+			oout.writeObject(value);
+			byte[] bytes = baos.toByteArray();
+			return bytes;
+		} catch(IOException e) {
+			throw NSForwardException._runtimeExceptionForThrowable(e);
+		} finally {
+			try { if(oout != null){oout.close();} } catch(IOException e) {}
+		}
 	}
 }
--- Frameworks/Core/ERPrototypes/Sources/er/prototypes/ValueFactory.java
+++ Frameworks/Core/ERPrototypes/Sources/er/prototypes/ValueFactory.java
@@ -1,6 +1,12 @@
 package er.prototypes;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
@@ -15,13 +21,21 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.joda.time.LocalTime;
 
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSData;
+import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSForwardException;
+import com.webobjects.foundation.NSPropertyListSerialization;
+
+import er.extensions.foundation.ERXMutableArray;
+import er.extensions.foundation.ERXMutableDictionary;
 
 /**
  * ValueFactory provides static methods that produce EOAttribute values
  * from values stored in the database.
  */
 public class ValueFactory {
+	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(ValueFactory.class);
 	private static final TimeZone GMT = TimeZone.getTimeZone("GMT");
 	
@@ -73,5 +87,44 @@ public class ValueFactory {
 		DateTime dt = new DateTime(value.getTime());
 		return dt;
 	}
+	
+	@SuppressWarnings("rawtypes")
+	public static NSArray stringArray(String value) {
+		return (NSArray)NSPropertyListSerialization.propertyListFromString(value);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static NSArray blobArray(NSData value) {
+		return ERXMutableArray.fromBlob(value);
+	}
 
+	@SuppressWarnings("rawtypes")
+	public static NSDictionary stringDictionary(String value) {
+		return (NSDictionary) NSPropertyListSerialization.propertyListFromString(value);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static NSDictionary blobDictionary(NSData value) {
+		return ERXMutableDictionary.fromBlob(value);
+	}
+	
+	public static Serializable serializable(byte[] value) {		
+		ObjectInputStream ois = null;
+		
+		try {
+			ByteArrayInputStream bais = new ByteArrayInputStream(value);
+			ois = new ObjectInputStream(bais);
+			Serializable obj = (Serializable)ois.readObject();
+			if(obj instanceof Collection) {
+				obj = (Serializable)Collections.unmodifiableCollection((Collection)obj);
+			}
+			return obj;
+		} catch(IOException e) {
+			throw NSForwardException._runtimeExceptionForThrowable(e);
+		} catch(ClassNotFoundException e) {
+			throw NSForwardException._runtimeExceptionForThrowable(e);
+		} finally {
+			try{ if(ois != null){ois.close();} } catch(IOException e) {}
+		}
+	}
 }
