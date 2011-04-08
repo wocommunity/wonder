31a3732779d7fc087604deb8f5e4e1ab1051e45e
Mike Schrag <mschrag@pobox.com>
2011-04-08 12:30:37 -0400

    removed merge script (just testing the build server)

--- Frameworks/EOAdaptors/JavaMemoryAdaptor/Sources/er/memoryadaptor/ERMemoryAdaptor.java
+++ Frameworks/EOAdaptors/JavaMemoryAdaptor/Sources/er/memoryadaptor/ERMemoryAdaptor.java
@@ -28,7 +28,7 @@ public class ERMemoryAdaptor extends EOAdaptor {
 	@Override
 	public void setConnectionDictionary(NSDictionary dictionary) {
 		if (dictionary == null) {
-			super.setConnectionDictionary(NSDictionary.EmptyDictionary);
+			super.setConnectionDictionary(NSDictionary.<String, Object> emptyDictionary());
 		}
 		else {
 			super.setConnectionDictionary(dictionary);
--- MergeFromTrunk.sh
+++
@@ -1,6 +0,0 @@
-VERSION=`svn --version | grep -c 1.6`
-if [ $VERSION == "0" ]; then
-	echo "You must be running SVN 1.6 to do a trunk merge."
-	exit 0
-fi
-svn merge https://wonder.svn.sourceforge.net/svnroot/wonder/trunk/Wonder
