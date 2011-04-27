a406fb4a88c1552bb91c64e8f34cb2461f8de607
Jeffrey Simpson <simpson@yfu.org>
2011-04-27 12:31:47 -0400

    Initial Checkin

---
+++ Tests/PluginTest/Sources/er/plugintest/components/Main.java
@@ -0,0 +1,11 @@
+package er.plugintest.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.components.ERXComponent;
+
+public class Main extends ERXComponent {
+	public Main(WOContext context) {
+		super(context);
+	}
+}
