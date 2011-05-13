edc1e8b5ee958e7c6ea088ef87b6a2c10a93147c
Kieran Kelleher <kelleherk@gmail.com>
2011-05-13 10:53:18 -0400

    javadoc

--- Frameworks/Core/ERExtensions/Sources/er/extensions/qualifiers/ERXFalseQualifier.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/qualifiers/ERXFalseQualifier.java
@@ -5,6 +5,10 @@ import com.webobjects.eocontrol.EOQualifier;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSMutableSet;
 
+/**
+ * A qualifier that always evaluates to false, and thus <strong>never</strong> qualifies an objects.
+ * For example if you qualify an array or a fetch with this qualifier, the result will be an empty array.
+ */
 public class ERXFalseQualifier extends EOQualifier {
 	@Override
 	public void addQualifierKeysToSet(NSMutableSet keys) {
--- Frameworks/Core/ERExtensions/Sources/er/extensions/qualifiers/ERXFalseQualifierSupport.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/qualifiers/ERXFalseQualifierSupport.java
@@ -5,6 +5,10 @@ import com.webobjects.eoaccess.EOQualifierSQLGeneration;
 import com.webobjects.eoaccess.EOSQLExpression;
 import com.webobjects.eocontrol.EOQualifier;
 
+/**
+ * SQL Support for {@link ERXFalseQualifier}
+ *
+ */
 public class ERXFalseQualifierSupport extends EOQualifierSQLGeneration.Support {
 	public String sqlStringForSQLExpression(EOQualifier qualifier, EOSQLExpression expression) {
 		return "0=1";
