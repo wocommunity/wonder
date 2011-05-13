7e2110e321ff27b0a7fbfd70ca4ae8d11e3b30f8
Johann Werner <jw@oyosys.de>
2011-05-13 10:46:48 +0200

    remove unused image files and related methods/string constants

--- Applications/JavaMonitor/Sources/com/webobjects/monitor/application/NavigationPage.java
+++ Applications/JavaMonitor/Sources/com/webobjects/monitor/application/NavigationPage.java
@@ -23,48 +23,6 @@ import er.extensions.foundation.ERXProperties;
 public class NavigationPage extends MonitorComponent {
 	private static final long serialVersionUID = 338657623393333855L;
 
-	private static final String _appBarImageName = "applications_bar.gif";
-
-	private static final String _hostBarImageName = "hosts_bar.gif";
-
-	private static final String _siteBarImageName = "site_bar.gif";
-
-	private static final String _prefBarImageName = "preferences_bar.gif";
-
-	private static final String _helpBarImageName = "help_bar.gif";
-
-	private static final String _migrationBarImageName = "migration_bar.gif";
-
-	private static final String[] _barImageNames = new String[] { _appBarImageName, _hostBarImageName,
-		_siteBarImageName, _prefBarImageName, _helpBarImageName, _migrationBarImageName };
-
-	private static final String _appActiveImageName = "applications_tab_active.gif";
-
-	private static final String _hostActiveImageName = "hosts_tab_active.gif";
-
-	private static final String _siteActiveImageName = "site_tab_active.gif";
-
-	private static final String _prefActiveImageName = "preferences_tab_active.gif";
-
-	private static final String _helpActiveImageName = "help_tab_active.gif";
-
-	private static final String _migrationActiveImageName = "migration_tab_active.gif";
-
-	private static final String _appInactiveImageName = "applications_tab_inactive.gif";
-
-	private static final String _hostInactiveImageName = "hosts_tab_inactive.gif";
-
-	private static final String _siteInactiveImageName = "site_tab_inactive.gif";
-
-	private static final String _prefInactiveImageName = "preferences_tab_inactive.gif";
-
-	private static final String _helpInactiveImageName = "help_tab_inactive.gif";
-
-	private static final String _migrationInActiveImageName = "migration_tab_inactive.gif";
-
-
-	public String backgroundImageName = "background.gif";
-
 	public int currentPage = APP_PAGE;
 
 	public String title;
@@ -82,39 +40,6 @@ public class NavigationPage extends MonitorComponent {
 
 	public String pageTitle() {
 		return "WOMonitor: " + title;
-	}
-
-	public String barImageName() {
-		return _barImageNames[currentPage];
-	}
-
-	public String appImageName() {
-		return (currentPage == APP_PAGE) ? _appActiveImageName : _appInactiveImageName;
-	}
-
-	public String hostImageName() {
-		return (currentPage == HOST_PAGE) ? _hostActiveImageName : _hostInactiveImageName;
-	}
-
-	public String siteImageName() {
-		return (currentPage == SITE_PAGE) ? _siteActiveImageName : _siteInactiveImageName;
-	}
-
-	public String prefImageName() {
-		return (currentPage == PREF_PAGE) ? _prefActiveImageName : _prefInactiveImageName;
-	}
-
-	public String helpImageName() {
-		return (currentPage == HELP_PAGE) ? _helpActiveImageName : _helpInactiveImageName;
-	}
-
-	public String migrationImageName() {
-		return (currentPage == MIGRATION_PAGE) ? _migrationActiveImageName : _migrationInActiveImageName;
-	}
-
-	public String backgroundImageSrc() {
-		WOResourceManager aResourceManager = application().resourceManager();
-		return aResourceManager.urlForResourceNamed(backgroundImageName, null, null, context().request());
 	}
 
 	public WOComponent ApplicationsPageClicked() {
