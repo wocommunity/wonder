fae049bdf50283ec61315905f936eb05128839ba
Mike Schrag <mschrag@pobox.com>
2011-05-15 23:58:32 -0400

    ERRestTest + updated ERFoundation/ERWebObjects jars

---
+++ Tests/ERRestTest/Sources/er/rest/model/Company.java
@@ -0,0 +1,17 @@
+package er.rest.model;
+
+import org.apache.log4j.Logger;
+
+import com.webobjects.foundation.NSArray;
+
+public class Company extends _Company {
+    private static Logger log = Logger.getLogger(Company.class);
+
+    public String nonModelAttribute() {
+        return "NonModelAttribute";
+    }
+
+    public NSArray<Manufacturer> manufacturers() {
+        return Manufacturer.manufacturers();
+    }
+}
