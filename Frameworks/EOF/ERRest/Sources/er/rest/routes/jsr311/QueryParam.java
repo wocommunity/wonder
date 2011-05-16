863e5ec613cc512b65861075ce7deeb67c8ef635
Mike Schrag <mschrag@pobox.com>
2011-05-15 22:11:45 -0400

    * support @QueryParam, @CookieParam, @HeaderParam JSR-311 annotations in ERRest     * support primitives in annotated parameters

---
+++ Frameworks/EOF/ERRest/Sources/er/rest/routes/jsr311/QueryParam.java
@@ -0,0 +1,31 @@
+package er.rest.routes.jsr311;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Like JSR-311, @QueryParam allows you to annotate an action method parameter to specify
+ * that its value should be loaded from the query parameters.
+ * </p>
+ * 
+ * <pre>
+ * public WOActionResults testAction(@QueryParam("person") Person personParam) {
+ *     ...
+ * }
+ * </pre>
+ * 
+ * <p>
+ * This will automatically pass the value of the "person" query object (?person=10) into
+ * your action method as the "personParam" parameter.
+ * </p>
+ * 
+ * @author mschrag
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface QueryParam {
+	String value();
+}
