5b4e7eb2d315b3ec44793f14cd59b82bd1cf9e06
Mike Schrag <mschrag@pobox.com>
2011-05-15 23:58:32 -0400

    ERRestTest + updated ERFoundation/ERWebObjects jars

---
+++ Tests/ERRestTest/Sources/er/rest/model/Person.java
@@ -0,0 +1,11 @@
+package er.rest.model;
+
+import org.apache.log4j.Logger;
+
+public class Person extends _Person {
+    private static Logger log = Logger.getLogger(Person.class);
+
+    public Car car() {
+        return Car.cars().lastObject();
+    }
+}
