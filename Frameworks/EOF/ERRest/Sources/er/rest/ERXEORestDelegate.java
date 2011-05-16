7d0bbc17fd5d04b8465d04d966cd3ded8ea2e7ec
Mike Schrag <mschrag@pobox.com>
2011-05-15 22:11:06 -0400

    * new constructor for rest delegate that takes an EC     * json rest writer adds support for Date in addition to NSTimestamp     * fixed default date formats for SimpleDateFormat

--- Frameworks/EOF/ERRest/Sources/er/rest/ERXEORestDelegate.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/ERXEORestDelegate.java
@@ -22,6 +22,10 @@ public class ERXEORestDelegate extends ERXAbstractRestDelegate {
 	public ERXEORestDelegate() {
 	}
 	
+	public ERXEORestDelegate(EOEditingContext editingContext) {
+		setEditingContext(editingContext);
+	}
+	
 	@Override
 	public boolean __hasNumericPrimaryKeys(EOClassDescription classDescription) {
 		boolean numericPKs = false;
