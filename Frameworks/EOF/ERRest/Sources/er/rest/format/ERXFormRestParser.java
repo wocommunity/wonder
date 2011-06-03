02b2b33eb09820d8061d9199a0d35fbcba2d82e8
Mike Schrag <mschrag@pobox.com>
2011-06-03 12:09:05 -0400

    added a new rest parser that deals with form keys

---
+++ Frameworks/EOF/ERRest/Sources/er/rest/format/ERXFormRestParser.java
@@ -0,0 +1,21 @@
+package er.rest.format;
+
+import er.rest.ERXRestContext;
+import er.rest.ERXRestRequestNode;
+import er.rest.format.ERXRestFormat.Delegate;
+
+/**
+ * ERXFormRestParser is a rest parser that reads data from form values. 
+ * 
+ * @author mschrag
+ */
+public class ERXFormRestParser implements IERXRestParser {
+	public ERXRestRequestNode parseRestRequest(IERXRestRequest request, Delegate delegate, ERXRestContext context) {
+		ERXRestRequestNode rootNode = new ERXRestRequestNode();
+		for (String keyPath : request.keyNames()) {
+			rootNode.takeValueForKeyPath(request.objectForKey(keyPath), keyPath);
+		}
+		return rootNode;
+	}
+
+}
