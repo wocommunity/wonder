c601289c02b7d35013042349d8b86436511f3bda
Kieran Kelleher <kelleherk@gmail.com>
2011-06-23 13:40:04 -0400

    Simplified and re-organized CCAjaxLongResponsePage. Added a bunch of system properties for custom configuration and styling

--- Frameworks/Ajax/ERCoolComponents/Sources/er/coolcomponents/CCAjaxLongResponsePage.java
+++ Frameworks/Ajax/ERCoolComponents/Sources/er/coolcomponents/CCAjaxLongResponsePage.java
@@ -11,13 +11,13 @@ import com.webobjects.appserver.WOActionResults;
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 
-import er.ajax.AjaxProgress;
 import er.extensions.appserver.ERXNextPageForResultWOAction;
 import er.extensions.appserver.IERXPerformWOActionForResult;
 import er.extensions.concurrency.ERXExecutorService;
 import er.extensions.concurrency.ERXFutureTask;
 import er.extensions.concurrency.ERXTaskPercentComplete;
 import er.extensions.foundation.ERXAssert;
+import er.extensions.foundation.ERXProperties;
 import er.extensions.foundation.ERXRuntimeUtilities;
 import er.extensions.foundation.ERXStatusInterface;
 import er.extensions.foundation.ERXStopWatch;
@@ -26,29 +26,63 @@ import er.extensions.foundation.ERXStopWatch;
  * A generic long response page that controls the execution of and provides user feedback on a long
  * running task.
  * This is designed to be really easy, and flexible, for the developer to re-use.
- *
- * Usage:
- * 1) Create a {@link Callable} class that returns some result or a {@link Runnable} that just does some background operation.
- *    A) Optionally implement the {@link ERXStatusInterface} interface (one method to return status message)
- *    B) Optionally implement the {@link ERXTaskPercentComplete} interface (one method to return percentage complete)
- * 2) If you don't just want the originating page to be returned (default behaviour) then
- *    A) Create a simple class that implements @link {@link IERXPerformWOActionForResult} interface
- *    B) This controller class will get the result pushed into it when the task is complete. If the
+ * <p>
+ * The common case of running a task and automatically returning to the same originating page is 
+ * really simple and requires just a few lines of code, for example:
+ * <pre>
+ * <code>
+ * 	public WOActionResults runLongTask() {
+		Runnable task = new LongRunningTask();
+		
+		CCAjaxLongResponsePage nextPage = pageWithName(CCAjaxLongResponsePage.class);
+		nextPage.setLongRunningRunnable(task);
+		
+		return nextPage;
+	}
+
+ * </code>
+ * </pre>
+ * <p>
+ * <strong>Usage:</strong>
+ * <ol>
+ * <li>
+ * Create a {@link Runnable} task, or a {@link Callable} task that returns some result.
+ * 	<ol>
+ *    <li>Optionally implement the {@link ERXStatusInterface} interface (just one method to return status message) 
+ *    to have the task's status displayed in the long response page.
+ *    <li>Optionally implement the {@link ERXTaskPercentComplete} interface (just one method to return percentage complete)
+ *    to have a progress bar and a percentage complete automatically displayed in the long response page.
+ *    </ol>
+ * </li>
+ * <li>If you don't just want the originating page to be returned (default behavior) then
+ *    <ol>
+ *    <li>Create a simple class that implements @link {@link IERXPerformWOActionForResult} interface, or use {@link ERXNextPageForResultWOAction}, which
+ *    provides a fairly generic implementation of that interface
+ *    <li>This controller class will get the result pushed into it when the task is complete. If the
  *    task threw an uncaught error during execution, then the error is pushed in as the result.
- *    C) The nextPage method of this controller class can do whatever it needs to do with the result
- *    and return a new page according to your logic.
- * 3) In your component action, simply create an instance of this long response page just as you would
+ *    <li>The nextPage method of this controller class can do whatever it needs to do with the result
+ *    and return a new page according to your logic in {@link IERXPerformWOActionForResult#performAction()}.
+ * 	</ol>
+ * </li><li>
+ * In your component action, simply create an instance of this long response page just as you would
  *    create any other page.
- * 4) Push in an instance of your Callable task into the long response page using {@link CCAjaxLongResponsePage#setLongRunningCallable(Callable)}
- * 5) Optionally push in your custom next page controller for execution when the task is finished using {@link #setNextPageForResultController(IERXPerformWOActionForResult)}
- * 6) Just return the long response page in your action method
- *
+ * </li><li>
+ * Push in an instance of your Runnable (or Callable) task into the long response page using {@link CCAjaxLongResponsePage#setLongRunningCallable(Callable)}
+ * </li><li>
+ * Optionally push in your custom next page controller for execution when the task is finished using {@link #setNextPageForResultController(IERXPerformWOActionForResult)}
+ * </li><li>
+ * Just return the long response page in your action method
+ * </li></ol>
+ * 
+ * 
  * @author kieran
  *
  */
 public class CCAjaxLongResponsePage extends WOComponent {
-	private static final Logger log = Logger
-			.getLogger(CCAjaxLongResponsePage.class);
+	private static final Logger log = Logger.getLogger(CCAjaxLongResponsePage.class);
+	
+	private static final String STYLESHEET_FRAMEWORK = ERXProperties.stringForKeyWithDefault("er.coolcomponents.CCAjaxLongResponsePage.stylesheet.framework", "ERCoolComponents");
+	private static final String STYLESHEET_FILENAME = ERXProperties.stringForKeyWithDefault("er.coolcomponents.CCAjaxLongResponsePage.stylesheet.filename", "CCAjaxLongResponsePage.css");
 
 	private final WOComponent _referringPage;
 	
@@ -109,31 +143,44 @@ public class CCAjaxLongResponsePage extends WOComponent {
 		_longRunningRunnable = longRunningRunnable;
 	}
 	
-	private String _staticMessage;
+	private String _defaultStatus;
 	
-	/** 
-	 * @return a static message to display while the task is running 
-	 * 
-	 * */
-	public String staticMessage() {
-		return _staticMessage;
+	/** @return a status message that is displayed if the task does not provide a status message */
+	public String defaultStatus() {
+		if ( _defaultStatus == null ) {
+			_defaultStatus = ERXProperties.stringForKeyWithDefault("er.coolcomponents.CCAjaxLongResponsePage.defaultStatus", "Please wait...");
+		}
+		return _defaultStatus;
 	}
 	
-	/** 
-	 * @param staticMessage a static message to display while the task is running 
-	 * 
-	 * */
-	public void setStaticMessage(String staticMessage){
-		_staticMessage = staticMessage;
+	/** @param defaultStatus a status message that is displayed if the task does not provide a status message */
+	public void setDefaultStatus(String defaultStatus){
+		_defaultStatus = defaultStatus;
+	}
+	
+	private Integer _refreshInterval;
+	
+	/** @return the refresh interval in seconds. Defaults to value of er.coolcomponents.CCAjaxLongResponsePage.refreshInterval */
+	public Integer refreshInterval() {
+		if ( _refreshInterval == null ) {
+			_refreshInterval = ERXProperties.intForKeyWithDefault("er.coolcomponents.CCAjaxLongResponsePage.refreshInterval", 2);
+		}
+		return _refreshInterval;
+	}
+	
+	/** @param refreshInterval the refresh interval in seconds. Defaults to value of er.coolcomponents.CCAjaxLongResponsePage.refreshInterval */
+	public void setRefreshInterval(Integer refreshInterval){
+		_refreshInterval = refreshInterval;
 	}
 
 	private ERXFutureTask<?> _future;
 
 	/** 
-	 * @return the {@link Runnable} controller for the {@link Callable}
+	 * @return the {@link Future} that is bound to the long running task.
+	 * 
 	 * The first time this is accessed, it is lazily initialized and
-	 * it kicks off the long running task. So it is important that the initial page causes
-	 * this to be accessed - currently the isDone method ensures that happens. 
+	 * it kicks off the long running task. So it is important that the initial page response causes
+	 * this to be lazily initialized, otherwise the task will never get started. 
 	 * 
 	 * */
 	public ERXFutureTask<?> future() {
@@ -155,34 +202,6 @@ public class CCAjaxLongResponsePage extends WOComponent {
 		return _future;
 	}
 
-	/**
-	 * @return whether task is done. This needs to be executed on every refresh for the end of the task to fire events.
-	 */
-	public boolean isDone() {
-		boolean isDone = future().isDone();
-		taskProgress().setDone(isDone);
-		return isDone;
-	}
-
-	private AjaxProgress _taskProgress;
-
-	/** 
-	 * @return the AjaxProgress model. This is a layer between the ProgressBar component and the Future/Callable objects
-	 * 
-	 **/
-	public AjaxProgress taskProgress() {
-		if ( _taskProgress == null ) {
-			if (longRunningCallable() != null) {
-				_taskProgress = new LongResponseTaskProgress(future(), longRunningCallable());
-			} else {
-				_taskProgress = new LongResponseTaskProgress(future(), longRunningRunnable());
-			}
-			
-		}
-		return _taskProgress;
-	}
-
-
 	public WOActionResults nextPage() {
 		if (log.isDebugEnabled()) log.debug("nextPage action fired");
 		nextPageForResultController().setResult(result());
@@ -200,44 +219,29 @@ public class CCAjaxLongResponsePage extends WOComponent {
 	 * 
 	 **/
 	public Object result() {
-		ERXAssert.POST.isTrue(isDone());
+		ERXAssert.POST.isTrue(future().isDone());
 		if ( _result == null ) {
 			try {
 				_result = future().get();
 			} catch (CancellationException cancellationException) {
-				taskProgress().cancel();
 				_result = cancellationException;
 			} catch (InterruptedException interruptedException) {
-				taskProgress().cancel();
 				_result = interruptedException;
 			} catch (ExecutionException executionException) {
 				log.error("Long Response Error:\n" + ERXRuntimeUtilities.informationForException(executionException), executionException);
-				taskProgress().setFailure(executionException);
 				_result = executionException;
 			}
 		}
 		return _result;
 	}
 
-	/**
-	 * No point in showing it if we don't have percent complete feature.
-	 * When no percent complete we show spinner instead
-	 *
-	 * @return whether to show the progress bar or not
-	 */
-	public boolean showProgressBar() {
-		return future().hasPercentComplete();
-	}
-
 	private boolean isStopWatchRunning = false;
 
 	/**
-	 * Yes, kind of lame that we depend on the refresh calling elapsedTime to check if we are done and whether to stop the clock
-	 * 
 	 * @return the elapsedTime since the task started running
 	 */
 	public String elapsedTime() {
-		if (isDone() && isStopWatchRunning) {
+		if (future().isDone() && isStopWatchRunning) {
 			stopWatch().stop();
 			isStopWatchRunning = false;
 		} //~ if (isDone())
@@ -259,75 +263,59 @@ public class CCAjaxLongResponsePage extends WOComponent {
 		return _stopWatch;
 	}
 	
-	public static class LongResponseTaskProgress extends AjaxProgress  {
-		private final Future<?> future;
-		private final Object task;
 
-		public LongResponseTaskProgress(Future<?> future, Callable<?> task) {
-			super(100);
-			this.future = future;
-			this.task = task;
+	
+	public String controlScriptContent() {
+		String result = ";";
+		if (future().isDone()) {
+			//result = "performNextPageAction();";
+			// Delay 1 second so that user can see the 100% complete progress message
+			result = "window.setTimeout(performNextPageAction, 1000);";
 		}
-		
-		public LongResponseTaskProgress(Future<?> future, Runnable task) {
-			super(100);
-			this.future = future;
-			this.task = task;
-		}
-
-
-		@Override
-		public boolean isDone() {
-			boolean isFutureDone = future.isDone();
-			if (isFutureDone != super.isDone()) {
-				super.setDone(isFutureDone);
-			} //~ if (isFutureDone != super.isDone())
-			return super.isDone();
-		}
-
-		@Override
-		public void cancel() {
-			future.cancel(true);
-			super.cancel();
-		}
-
-		@Override
-		public String status() {
-			if (task instanceof ERXStatusInterface) {
-				return ((ERXStatusInterface)task).status();
-			}
-			
-			// OK, so the task does not implement the status interface, let's do defaults
-			String status = super.status();
-			if (status == null) {
-				if (future.isDone()) {
-					status = "Task Complete";
-					super.setDone(true);
-				} else {
-					status = "Processing...";
-				}
-			}
-			return status;
-		}
-
-		@Override
-		public double percentage() {
-			if (task instanceof ERXTaskPercentComplete) {
-				double percent = ((ERXTaskPercentComplete)task).percentComplete().doubleValue();
-				super.setValue((long)(100.0 * percent));
-				return percent;
-			}
-			return super.percentage();
-		}
-
-		public double percentNominalDisplay() {
-			return percentage() * 100.0d;
-		}
-
-
-
-
+		return result;
 	}
 
+	
+	
+	
+	/**
+	 * @return the table cell width value for the finished part of the progress bar, for example "56%". 
+	 * The same string can be used to display user-friendly percentage complete value.
+	 */
+	public String finishedPercentage() {
+		String result = "1%";
+		Double percentComplete = future().percentComplete();
+		if (percentComplete != null) {
+			long userPercentComplete = Math.round(percentComplete.doubleValue() * 100.0d);
+			if (userPercentComplete < 1) {
+				userPercentComplete = 1;
+			}
+			if (userPercentComplete > 100) {
+				userPercentComplete = 100;
+			}
+			result = userPercentComplete + "%";
+		}
+		return result;
+	}
+	
+	/**
+	 * @return true if logging is Debug level. Used to display page config info in the long response page itself during development.
+	 */
+	public boolean isDebugMode() {
+		return log.isDebugEnabled();
+	}
+	
+	public String styleSheetFramework() {
+		return STYLESHEET_FRAMEWORK;
+	}
+	
+	public String styleSheetFilename() {
+		return STYLESHEET_FILENAME;
+	}
+
+	
+	
+	
+	
 	
 }
