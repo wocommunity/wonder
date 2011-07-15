a17955f070b4c2a89b8d6f3dd34e502bcd4922c9
nullterminated <ramseygurley@gmail.com>
2011-07-14 20:06:48 -0700

    Fixed serializable issue in joda formatters

--- Frameworks/Core/ERExtensions/Sources/er/extensions/formatters/ERXJodaDateTimeFormatter.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/formatters/ERXJodaDateTimeFormatter.java
@@ -6,14 +6,18 @@ import java.text.ParsePosition;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import org.joda.time.Chronology;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.chrono.BaseChronology;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 public class ERXJodaDateTimeFormatter extends Format {
-	private final DateTimeFormatter formatter;
+	private transient DateTimeFormatter formatter;
+	private String _pattern;
+	private BaseChronology _chronology;
+	private Locale _locale;
+	private DateTimeZone _zone;
 	
 	public ERXJodaDateTimeFormatter(String pattern) {
 		this(pattern, null, null, null);
@@ -31,27 +35,36 @@ public class ERXJodaDateTimeFormatter extends Format {
 		this(pattern, null, locale, DateTimeZone.forTimeZone(zone));
 	}
 	
-	public ERXJodaDateTimeFormatter(String pattern, Chronology chronology, Locale locale, DateTimeZone zone) {
-		DateTimeFormatter f = DateTimeFormat.forPattern(pattern);
-		if(chronology != null) { f = f.withChronology(chronology); }
-		if(locale != null) { f = f.withLocale(locale); }
-		if(zone != null) { f = f.withZone(zone); }
-		formatter = f;
+	public ERXJodaDateTimeFormatter(String pattern, BaseChronology chronology, Locale locale, DateTimeZone zone) {
+		_pattern = pattern;
+		_chronology = chronology;
+		_locale = locale;
+		_zone = zone;
 	}
 	
 	public ERXJodaDateTimeFormatter(Locale locale, String style) {
 		this(DateTimeFormat.patternForStyle(style, locale));
 	}
+	
+	protected DateTimeFormatter formatter() {
+		if(formatter == null) {
+			formatter = DateTimeFormat.forPattern(_pattern);
+			if(_chronology != null) { formatter = formatter.withChronology(_chronology); }
+			if(_locale != null) { formatter = formatter.withLocale(_locale); }
+			if(_zone != null) { formatter = formatter.withZone(_zone); }
+		}
+		return formatter;
+	}
 
 	@Override
 	public StringBuffer format(Object obj, StringBuffer buffer, FieldPosition pos) {
-		formatter.printTo(buffer, (DateTime)obj);
+		formatter().printTo(buffer, (DateTime)obj);
 		return buffer;
 	}
 
 	@Override
 	public DateTime parseObject(String str, ParsePosition pos) {
-		DateTime dt = formatter.parseDateTime(str);
+		DateTime dt = formatter().parseDateTime(str);
 		pos.setIndex(str.length());
 		return dt;
 	}
