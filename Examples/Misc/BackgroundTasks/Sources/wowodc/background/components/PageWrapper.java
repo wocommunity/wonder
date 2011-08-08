ac8f42ef14e7071ef84b03e672bebf93eac21e88
Kieran Kelleher <kelleherk@gmail.com>
2011-08-08 18:55:15 -0400

    Added examples of error handling and custom error handler implementation. Moved app properties from Properties.dev to Properties, just in case someone deploys the example. Removed some dead code. (cherry picked from commit f699891d2c30e25a490b5563ef97748d93abadde)

--- Examples/Misc/BackgroundTasks/Sources/wowodc/background/components/PageWrapper.java
+++ Examples/Misc/BackgroundTasks/Sources/wowodc/background/components/PageWrapper.java
@@ -1,6 +1,9 @@
 package wowodc.background.components;
 
+import wowodc.background.utilities.Utilities;
+
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSMutableArray;
 
 import er.extensions.components.ERXComponent;
 
@@ -25,6 +28,28 @@ public class PageWrapper extends ERXComponent {
 	public void reset() {
 		// resets ivars at the end or RR phases
 		super.reset();
+	}
+	
+	public boolean hasErrors() {
+		return Utilities.hasErrors();
+	}
+	
+	/**
+	 * Why do it this way instead of a repetition? Just for fun....
+	 * 
+	 * @return the li html elements with the error messages
+	 */
+	public String errorString() {
+		NSMutableArray<String> errorMessages = Utilities.errorMessages();
+		StringBuilder b = new StringBuilder();
+		if (errorMessages.count() > 0) {
+			for (String message : errorMessages) {
+				b.append("<li class=\"error\" >");
+				b.append(message);
+				b.append("</li>");
+			}
+		}
+		return b.toString();
 	}
 
 
---
+++ Examples/Misc/BackgroundTasks/Sources/wowodc/background/tasks/T08CallableWithSimulatedError.java
@@ -0,0 +1,13 @@
+package wowodc.background.tasks;
+
+import java.util.concurrent.Callable;
+
+public class T08CallableWithSimulatedError implements Callable<String> {
+
+	public String call() throws Exception {
+		// Simulate an exception
+		throw new RuntimeException("This is a fake simulated exception in a Callable task");
+
+	}
+
+}
