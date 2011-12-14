baefb2061a5eeb4677d185327b744bb20649249c
Kieran Kelleher <kelleherk@gmail.com>
2011-12-14 11:32:38 -0500

    Added Department entity and a deliberately optional toOne relationship with Employee.

---
+++ Tests/ERXTest/Sources/er/erxtest/model/Department.java
@@ -0,0 +1,8 @@
+package er.erxtest.model;
+
+import org.apache.log4j.Logger;
+
+public class Department extends _Department {
+	@SuppressWarnings("unused")
+	private static Logger log = Logger.getLogger(Department.class);
+}
