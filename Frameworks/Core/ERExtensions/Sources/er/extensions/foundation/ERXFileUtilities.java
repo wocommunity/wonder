6cfe569d20738ad236cbca71eb5cf68bb7f51f61
Kieran Kelleher <kelleherk@gmail.com>
2012-01-12 22:15:32 -0500

    Javadoc and a few tests to confirm expected operation.

--- Frameworks/Core/ERExtensions/Sources/er/extensions/foundation/ERXFileUtilities.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/foundation/ERXFileUtilities.java
@@ -985,13 +985,14 @@ public class ERXFileUtilities {
         }
 
     /**
-     * Copys the source file to the destination
+     * Copies the source file to the destination.
+     * Automatically creates parent directory or directories of {@code dstFile) if they are missing.
      *
      * @param srcFile source file
-     * @param dstFile destination file
-     * @param deleteOriginals tells if original file will be deleted. Note that if the appuser has no write rights
-     * on the file it is NOT deleted unless force delete is true
-     * @param forceDelete if true then missing write rights are ignored and the file is deleted.
+     * @param dstFile destination file which may or may not exist already. If it exists, its contents will be overwritten.
+     * @param deleteOriginals if {@code true} then {@code srcFile} will be deleted. Note that if the appuser has no write rights
+     * on {@code srcFile} it is NOT deleted unless {@code forceDelete} is true
+     * @param forceDelete if {@code true} then missing write rights are ignored and the file is deleted.
      */
     public static void copyFileToFile(File srcFile, File dstFile, boolean deleteOriginals, boolean forceDelete) throws FileNotFoundException, IOException {
         if (srcFile.exists() && srcFile.isFile()) {
