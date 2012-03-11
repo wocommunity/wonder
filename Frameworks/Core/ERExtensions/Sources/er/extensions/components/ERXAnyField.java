2d767734bc346490283333e1a99763682a0cfb3d
Pascal Robert <probert@macti.ca>
2012-03-10 20:27:15 -0500

    Fixed JavaDoc (mostly bindings) for a couple of components.

--- Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXAnyField.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXAnyField.java
@@ -8,7 +8,21 @@ import er.extensions.foundation.ERXStringUtilities;
 import er.extensions.localization.ERXLocalizer;
 
 /**
- * Class for Wonder Component ERXAnyField.
+ * ERXAnyField, who extends WOAnyField, shows a popup with a list of keys for an entity to
+ * let you filter a display group.  
+ *
+ * @binding displayGroup The display group to apply the filter on
+ * @binding keyList Array of strings of available keys to filter the display group
+ * @binding key An item in the key list
+ * @binding selectedKey Selection made by the user in the key list
+ * @binding sourceEntity Name Name of the entity
+ * @binding value Value of the qualifier
+ * @binding displayKey
+ * @binding formatter
+ * @binding relationshipKey
+ * 
+ * @see WOAnyField
+ * 
  * @author ak on Thu Feb 27 2003
  */
 
