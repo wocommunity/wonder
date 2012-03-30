f89b421511cb65f8778cff6366d150ede0eb92c2
Johnny <jlmiller@kahalawai.com>
2012-03-30 10:15:33 -1000

    Added Lightbox/Slimbox component called MTMediaBox. Updated class path to include ERAttachment.

---
+++ Examples/Ajax/MooToolsExample/Sources/er/ajax/mootools/example/components/MTMediaBoxTestPage.java
@@ -0,0 +1,9 @@
+package er.ajax.mootools.example.components;
+
+import com.webobjects.appserver.WOContext;
+
+public class MTMediaBoxTestPage extends Main {
+    public MTMediaBoxTestPage(WOContext context) {
+        super(context);
+    }
+}
