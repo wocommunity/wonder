72a062fe3b25639de15ae3b82e19b6156e5499d5
Johnny <jlmiller@kahalawai.com>
2012-04-05 19:11:20 -1000

    MTAjaxInPlace java file. Forget to commit it with the others.

---
+++ Frameworks/Ajax/MooTools/Sources/er/ajax/mootools/MTAjaxInPlace.java
@@ -0,0 +1,10 @@
+package er.ajax.mootools;
+
+import com.webobjects.appserver.WOContext;
+import er.ajax.AjaxInPlace;
+
+public class MTAjaxInPlace extends AjaxInPlace {
+    public MTAjaxInPlace(WOContext context) {
+        super(context);
+    }
+}
