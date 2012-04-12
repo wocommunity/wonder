d8d028d86f22498d2b98c4f3e0a879b2538cd10a
Pascal Robert <probert@macti.ca>
2012-04-12 09:00:30 -0400

    Renaming the Java packages in JqueryMobileExample

---
+++ Examples/Ajax/JqueryMobileExample/Sources/er/jquerymobile/exampleapp/components/FormList.java
@@ -0,0 +1,16 @@
+package er.jquerymobile.exampleapp.components;
+
+import com.webobjects.appserver.WOContext;
+
+@SuppressWarnings("serial")
+public class FormList extends FormInsetList {
+
+  //********************************************************************
+  //  Constructor
+  //********************************************************************
+
+  public FormList(WOContext aContext) {
+    super(aContext);
+  }
+
+}
