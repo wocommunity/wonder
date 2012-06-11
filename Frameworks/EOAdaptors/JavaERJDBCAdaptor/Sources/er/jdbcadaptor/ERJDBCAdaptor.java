3d89b59c274a0a8f2cd4848f3e1740427ab56e42
Johann Werner <jw@oyosys.de>
2012-06-11 16:36:10 +0200

    remove useless TODOs, unused logger and empty Javadoc

--- Frameworks/EOAdaptors/JavaERJDBCAdaptor/Sources/er/jdbcadaptor/ERJDBCAdaptor.java
+++ Frameworks/EOAdaptors/JavaERJDBCAdaptor/Sources/er/jdbcadaptor/ERJDBCAdaptor.java
@@ -1,59 +1,37 @@
-/*
- * Created on Feb 6, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package er.jdbcadaptor;
+
 import java.sql.Connection;
 
 import com.webobjects.eoaccess.EOAdaptorContext;
 import com.webobjects.jdbcadaptor.JDBCAdaptor;
 
 import er.extensions.jdbc.ERXJDBCConnectionBroker;
-import er.extensions.logging.ERXLogger;
 
 
 /**
  * @author david
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
-public class ERJDBCAdaptor extends
-        JDBCAdaptor {
-
-    public static ERXLogger log = ERXLogger.getERXLogger(ERJDBCAdaptor.class);
+public class ERJDBCAdaptor extends JDBCAdaptor {
     
-    /**
-     * @param arg0
-     */
     public ERJDBCAdaptor(String arg0) {
         super(arg0);
     }
 
+    @Override
     public EOAdaptorContext createAdaptorContext() {
         return new ERJDBCContext(this);
     }
 
-    /**
-     */
     public Connection checkoutConnection() {
         Connection c = ERXJDBCConnectionBroker.connectionBrokerForAdaptor(this).getConnection();
 
         return c;
     }
 
-
-    /**
-     * @param connection
-     */
     public void freeConnection(Connection connection) {
         ERXJDBCConnectionBroker.connectionBrokerForAdaptor(this).freeConnection(connection);
     }
 
-    /**
-     */
     public boolean supportsTransactions() {
         return ERXJDBCConnectionBroker.connectionBrokerForAdaptor(this).supportsTransaction();
     }
