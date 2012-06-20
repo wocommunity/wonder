74f63729c20307d6fd6f42d479d1f97f0916342b
Kieran Kelleher <kelleherk@gmail.com>
2012-06-20 19:45:29 -0400

    Revert "Prevent java.lang.NoSuchMethodError: er.extensions.eof.ERXKeyFilter.include(Ljava/lang/String;)"
    This reverts commit 8e039542a5f2e1d21604a0b3578a7c0f51a45cb9.
    
    I guess I need to think twice about cherry-picking from my branch to integration.

--- Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXKeyFilter.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXKeyFilter.java
@@ -413,10 +413,6 @@ public class ERXKeyFilter {
 		return _lockedRelationships;
 	}
 	
-	public void include(String keyName) {
-		include(new ERXKey<Object>(keyName));
-	}
-	
 	/**
 	 * Includes the given set of keys in this filter, wrapping them in ERXKey objects for you.
 	 * 
