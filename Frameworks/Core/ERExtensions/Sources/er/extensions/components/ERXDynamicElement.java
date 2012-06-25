8bf4b3311580c1e828d0b8ba8908be8e8e55e919
Johann Werner <jw@oyosys.de>
2012-06-25 14:11:36 +0200

    add support class to store values in dynamic components

--- Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXDynamicElement.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/components/ERXDynamicElement.java
@@ -1,11 +1,14 @@
 package er.extensions.components;
 
+import java.util.Stack;
+
 import org.apache.log4j.Logger;
 
 import com.webobjects.appserver.WOAssociation;
 import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOElement;
+import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 import com.webobjects.appserver._private.WODynamicGroup;
 import com.webobjects.foundation.NSArray;
@@ -16,6 +19,13 @@ import er.extensions.appserver.ERXWOContext;
 
 /**
  * ERXDynamicElement provides a common base class for dynamic elements.
+ * <p>
+ * All subclasses of ERXDynamicElement have to be thread safe! The WOAssociation objects
+ * are thread safe as they don't hold a specific value but are used to retrieve the correct
+ * value from the current parent component. If you want or have to store values in instance
+ * variables use the inner class {@link ContextData} as value holder. Those objects will
+ * store values indirectly within the current context which means you can declare them as
+ * static.
  * 
  * @author jw
  */
@@ -249,6 +259,177 @@ public abstract class ERXDynamicElement extends WODynamicGroup {
 	protected void appendTagAttributeToResponse(WOResponse response, String name, Object value) {
 		if (value != null) {
 			response._appendTagAttributeAndValue(name, value.toString(), true);
+		}
+	}
+	
+	/**
+	 * Convenience method to override if you want to store values in ContextData object. Here you can call
+	 * the {@link ContextData#begin(WOContext, Object)} on your ContextData objects. This method should then
+	 * be called at the beginning of the {@link #appendToResponse(WOResponse, WOContext)},
+	 * {@link #takeValuesFromRequest(WORequest, WOContext)} and {@link #invokeAction(WORequest, WOContext)}.
+	 * 
+	 * @param context context of the transaction
+	 */
+	protected void beforeProcessing(WOContext context) {
+	}
+
+	/**
+	 * Convenience method to override if you want to store values in ContextData object. Here you can call
+	 * the {@link ContextData#end(WOContext)} on your ContextData objects. This method should then
+	 * be called at the end of the {@link #appendToResponse(WOResponse, WOContext)},
+	 * {@link #takeValuesFromRequest(WORequest, WOContext)} and {@link #invokeAction(WORequest, WOContext)}.
+	 * 
+	 * @param context context of the transaction
+	 */
+	protected void afterProcessing(WOContext context) {
+	}
+	
+	/**
+	 * This class is used to store values that cannot be stored in instance variables as
+	 * dynamic elements have to be thread-safe. Internally the userInfo of the current context is used
+	 * to store the values, so all information is only valid for one request-response cycle. A stack
+	 * is used per key so multiple nested components of the same type can use the same key safely.
+	 * <p>
+	 * You have to use <code>begin</code> before first usage of the field and <code>end</code> at
+	 * the end of the cycle to clean up the corresponding stack. Without balanced calls of
+	 * <code>begin</code> and <code>end</code> very bad things will happen. Thus it is advisable
+	 * to use a try-finally (see example below). As you have to make these calls during <code>appendToResponse</code>,
+	 * <code>taleValuesFromRequest</code> and <code>invokeAction</code> you should put those calls into
+	 * overridden methods of <code>beforeProcessing</code> and <code>afterProcessing</code> respectively
+	 * to avoid code duplication.
+	 * <p>
+	 * It is advisable too to choose a unique <code>key</code>, as different component types
+	 * will use the same userInfo. It is suggested to use a key composed by the component
+	 * and field name (e.g. <i>MyDynamicElement.myField</i>).
+	 * The ContextData field itself can and should be declared static as it doesn't store the
+	 * value itself, it is only used as an accessor to the value stored within the context.
+	 * <p>
+	 * Example:
+	 * <pre>
+	 * public static final ContextData<String> myField = new ContextData<String>("MyClass.myField");
+	 * 
+	 * public void appendToResponse(WOResponse response, WOContext context) {
+	 *   beforeProcessing(context);
+	 *   try {
+	 *     response.appendContentString("Current value: " + myField.value(context));
+	 *   } finally {
+	 *     afterProcessing(context);
+	 *   }
+	 * }
+	 * 
+	 * public void takeValuesFromRequest(WORequest request, WOContext context) {
+	 *   beforeProcessing(context);
+	 *   try {
+	 *     …
+	 *   } finally {
+	 *     afterProcessing(context);
+	 *   }
+	 * }
+	 * 
+	 * public WOActionResults invokeAction(WORequest request, WOContext context) {
+	 *   beforeProcessing(context);
+	 *   try {
+	 *     …
+	 *   } finally {
+	 *     afterProcessing(context);
+	 *   }
+	 * }
+	 * 
+	 * protected void beforeProcessing(WOContext context) {
+	 *   myField.begin(context, "myValue");
+	 * }
+	 * 
+	 * protected void afterProcessing(WOContext context) {
+	 *   myField.end(context);
+	 * }
+	 * </pre>
+	 * 
+	 * @param <T> type of data to store 
+	 * @author sgaertner
+	 */
+	protected static class ContextData<T> {
+		private final String _key;
+
+		public ContextData(String key) {
+			_key = key;
+		}
+
+		/**
+		 * The key this object uses to store values in the context's
+		 * userInfo.
+		 * 
+		 * @return the key
+		 */
+		public String key() {
+			return _key;
+		}
+
+		protected Stack<T> stack(WOContext context) {
+			Stack<T> stack = (Stack<T>) context.userInfoForKey(_key);
+			if (stack == null) {
+				stack = new Stack<T>();
+				context.setUserInfoForKey(stack, _key);
+			}
+			return stack;
+		}
+
+		/**
+		 * Push a constant value onto the stack.
+		 * 
+		 * @param context context of the transaction
+		 * @param value constant value
+		 */
+		public void begin(WOContext context, T value) {
+			stack(context).push(value);
+		}
+
+		/**
+		 * Push a value from a specific binding onto the stack.
+		 * 
+		 * @param context context of the transaction
+		 * @param component the current dynamic element
+		 * @param name binding name
+		 */
+		public void begin(WOContext context, ERXDynamicElement component, String name) {
+			stack(context).push((T) component.valueForBinding(name, context.component()));
+		}
+
+		/**
+		 * Replace the current value on the stack with a new value.
+		 * 
+		 * @param context context of the transaction
+		 * @param value constant value
+		 */
+		public void setValue(WOContext context, T value) {
+			stack(context).pop();
+			stack(context).push(value);
+		}
+
+		/**
+		 * Returns the current value.
+		 * 
+		 * @param context context of the transaction
+		 * @return current value
+		 */
+		public T value(WOContext context) {
+			return stack(context).peek();
+		}
+
+		/**
+		 * Removes the current value from the stack. If you pushed multiple values onto
+		 * the stack by calling begin() several times you have to call end() the same
+		 * number of times.
+		 *  
+		 * @param context context of the transaction
+		 */
+		public void end(WOContext context) {
+			stack(context).pop();
+		}
+
+		@Override
+		public String toString() {
+			return (new StringBuilder()).append("<").append(getClass().getName()).append(" key: ").append(_key).append(">")
+					.toString();
 		}
 	}
 }
