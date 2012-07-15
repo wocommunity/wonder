5848c13a3b9d1a4d4db19be454319330b3a2d471
Ken ISHIMOTO <ken@ksroom.com>
2012-07-15 20:53:57 +0200

    javadoc & remove unused Code

--- Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/bool/ERD2WCustomQueryBoolean.java
+++ Frameworks/Core/ERDirectToWeb/Sources/er/directtoweb/components/bool/ERD2WCustomQueryBoolean.java
@@ -9,12 +9,25 @@ import com.webobjects.foundation.NSArray;
 import er.extensions.localization.ERXLocalizer;
 
 /**
+ * <span class="en">
  * Better D2WQueryBoolean, which allows you to sprecify the choices names via a context key, 
  * containing the labels in a format like ("Don't care", "Yes", "No") or ("Yes", "No").
  * Also keeps the selected value. 
  * 
- * @author ak on Mon Dec 22 2003
  * @d2wKey choicesNames
+ * </span>
+ * 
+ * <span class="ja">
+ * D2WQueryBoolean 拡張版
+ * コンテキスト・キーでローカライズが可能です。
+ *  ("Don't care", "Yes", "No") 又は ("Yes", "No").
+ *  
+ * さらに、選択されている値を保存します 
+ * 
+ * @d2wKey choicesNames - ローカライズ名：("ERD2WBoolean.Yes", "ERD2WBoolean.No", "ERD2WBoolean.DontCare")
+ * </span>
+ * 
+ * @author ak on Mon Dec 22 2003
  */
 public class ERD2WCustomQueryBoolean extends D2WQueryBoolean {
 	/**
@@ -48,28 +61,19 @@ public class ERD2WCustomQueryBoolean extends D2WQueryBoolean {
     }
     
     public Object value() {
-        int index = 0;
-        Object value = displayGroup().queryMatch().valueForKey(propertyKey());
-        if(value != null) {
-            if(value.equals(Boolean.FALSE)) {
-                index = 2;
-            } else {
-                index = 1;
-            }
-        } else {
-            index = 0;
-        }
-        return value;
+        return displayGroup().queryMatch().valueForKey(propertyKey());
     }
 
     public void setValue(Object obj) {
         displayGroup().queryOperator().removeObjectForKey(propertyKey());
         displayGroup().queryMatch().removeObjectForKey(propertyKey());
         if(obj == null) {
+          if(log.isDebugEnabled())
             log.debug("Don't care");
         } else {
             displayGroup().queryMatch().takeValueForKey(obj, propertyKey());
-            log.debug(obj);
+            if(log.isDebugEnabled())
+              log.debug(obj);
         }
     }
 
