wocommunity/wonder

commit 9b68bfaa06d3aff4ff4b94c6d9ec444f31044e50

Adds in-memory qualification and sorting on a hidden PK attribute key.
Currently using a PK attribute key in a EOQualifier or EOSortOrdering only works for database fetches.
With this change, such a qualifier or sortOrdering will work in-memory now without UnknownKeyException.

Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXGenericRecord.java
@@ -1053,6 +1053,30 @@ public class ERXGenericRecord extends EOGenericRecord implements ERXGuardedObjec
 	}
 	
 	/**
+	 * Override so that we can handle the case of in-memory qualifier evaluation against a hidden primary key 
+	 * attribute (simple or component of compound). This will allow database qualifiers containing primary key
+	 * attribute names to be used for in-memory sorting and filtering.
+	 * 
+	 * @see com.webobjects.eocontrol.EOCustomObject#handleQueryWithUnboundKey(java.lang.String)
+	 */
+	@Override
+	public Object handleQueryWithUnboundKey(String key) {
+		NSDictionary pkDict = EOUtilities.primaryKeyForObject(editingContext(), this);
+		if (pkDict == null) {
+			// This will be the case for new unsaved objects, so just
+			// check if the user was using a key that is valid as a primary key attribute
+			if (entity().primaryKeyAttributeNames().contains(key)) {
+				// Valid hidden PK key, so return null since PK is still essentially null.
+				return null;
+			}
+		} else if (pkDict.allKeys().contains(key)) {
+			// Valid PK key, so return the atribute value.
+			return pkDict.valueForKey(key);
+		}
+		return super.handleQueryWithUnboundKey(key);
+	}
+	
+	/**
 	 * Overrides the default validation mechanisms to provide a few checks
 	 * before invoking super's implementation, which incidentally just invokes
 	 * validateValueForKey on the object's class description. The class

commit a282e280dd061dc72c3250127312da7b1aae1ee9

Fix bug introduced in https://github.com/projectwonder/wonder/commit/9b68bfaa06d3aff4ff4b94c6d9ec444f31044e50

Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXGenericRecord.java
@@ -1060,18 +1060,22 @@ public class ERXGenericRecord extends EOGenericRecord implements ERXGuardedObjec
 	 */
 	@Override
 	public Object handleQueryWithUnboundKey(String key) {
-		NSDictionary pkDict = EOUtilities.primaryKeyForObject(editingContext(), this);
-		if (pkDict == null) {
-			// This will be the case for new unsaved objects, so just
-			// check if the user was using a key that is valid as a primary key attribute
-			if (entity().primaryKeyAttributeNames().contains(key)) {
-				// Valid hidden PK key, so return null since PK is still essentially null.
-				return null;
-			}
-		} else if (pkDict.allKeys().contains(key)) {
-			// Valid PK key, so return the atribute value.
-			return pkDict.valueForKey(key);
-		}
+        if (!isDeletedEO()) {
+            NSDictionary pkDict = EOUtilities.primaryKeyForObject(editingContext(), this);
+            if (pkDict == null) {
+                // This will be the case for new unsaved objects, so just
+                // check if the user was using a key that is valid as a primary
+                // key attribute
+                if (entity().primaryKeyAttributeNames().contains(key)) {
+                    // Valid hidden PK key, so return null since PK is still
+                    // essentially null.
+                    return null;
+                }
+            } else if (pkDict.allKeys().contains(key)) {
+                // Valid PK key, so return the atribute value.
+                return pkDict.valueForKey(key);
+            }
+        }
 		return super.handleQueryWithUnboundKey(key);
 	}
 	