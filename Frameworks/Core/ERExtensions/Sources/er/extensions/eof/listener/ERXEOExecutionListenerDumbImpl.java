7edd825baaa6cfa62ae2d3832c0874ee7408f50b
James Brook <james_brook@yahoo.co.uk>
2012-11-21 17:36:24 +0100

    Feature branch for adding a listener to the method that times all SQL expressions. As well as logging we can now optionally do something else, e.g. notify an external monitoring system of slow SQL expressions.

---
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/eof/listener/ERXEOExecutionListenerDumbImpl.java
@@ -0,0 +1,7 @@
+package er.extensions.eof.listener;
+
+public class ERXEOExecutionListenerDumbImpl implements ERXEOExecutionListener {
+    public void log(long requestTime, String entityName) {
+        //Do nothing here
+    }
+}
