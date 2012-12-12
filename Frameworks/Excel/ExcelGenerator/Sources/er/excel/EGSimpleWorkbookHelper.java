e06663dcc45a03815297b33c3b3484f6402b12b3
Johann Werner <jw@oyosys.de>
2012-12-12 08:54:43 +0100

    close stream

--- Frameworks/Excel/ExcelGenerator/Sources/er/excel/EGSimpleWorkbookHelper.java
+++ Frameworks/Excel/ExcelGenerator/Sources/er/excel/EGSimpleWorkbookHelper.java
@@ -1,6 +1,7 @@
 package er.excel;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -255,6 +256,12 @@ public class EGSimpleWorkbookHelper {
 			_workbook.write(stream);
 		} catch (java.io.IOException e) {
 			return false;
+		} finally {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				// ignore
+			}
 		}
 		return true;
 	}
