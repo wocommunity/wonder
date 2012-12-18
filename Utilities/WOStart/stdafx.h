aee4f80ef1aa923c8f2784a42200208dfbe28949
Markus Stoll <markus.stoll@junidas.de>
2012-12-18 21:22:56 +0100

    new tool for WO5 app start under windows

---
+++ Utilities/WOStart/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : Includedatei für Standardsystem-Includedateien
+// oder häufig verwendete projektspezifische Includedateien,
+// die nur in unregelmäßigen Abständen geändert werden.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <stdlib.h>
+#include <process.h>
+
+
+// TODO: Hier auf zusätzliche Header, die das Programm erfordert, verweisen.
