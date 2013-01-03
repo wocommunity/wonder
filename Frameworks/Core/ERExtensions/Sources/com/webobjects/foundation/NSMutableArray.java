d5137e076c0b5bb9bcdb4c7ecf7d97d7a3ceb622
Johann Werner <jw@oyosys.de>
2013-01-03 10:49:37 +0100

    add missing serialVersionUID

--- Frameworks/Core/ERExtensions/Sources/com/webobjects/foundation/NSMutableArray.java
+++ Frameworks/Core/ERExtensions/Sources/com/webobjects/foundation/NSMutableArray.java
@@ -918,6 +918,13 @@ class SubList<E> extends NSMutableArray<E> {
 }
 
 class RandomAccessSubList<E> extends SubList<E> implements RandomAccess {
+    /**
+     * Do I need to update serialVersionUID?
+     * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+     * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+     */
+    private static final long serialVersionUID = 1L;
+
     RandomAccessSubList(NSMutableArray<E> list, int fromIndex, int toIndex) {
         super(list, fromIndex, toIndex);
     }
