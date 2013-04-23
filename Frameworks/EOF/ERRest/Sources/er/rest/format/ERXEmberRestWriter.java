21baa10fafc7fa6cef2f795ffcb50a7834bcb10e
Michael Kondratov <mkondratov@me.com>
2013-04-23 16:56:35 -0400

    arrays recognition

--- Frameworks/EOF/ERRest/Sources/er/rest/format/ERXEmberRestWriter.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/format/ERXEmberRestWriter.java
@@ -6,36 +6,29 @@ import er.rest.ERXRestRequestNode;
 public class ERXEmberRestWriter extends ERXJSONRestWriter {
 	@Override
 	protected ERXRestRequestNode processNode(ERXRestRequestNode node) {
+		
 		ERXRestRequestNode rootNode = new ERXRestRequestNode(null, true);
 
-		//ERXRestRequestNode recordsNode = new ERXRestRequestNode("content", false);
-		//recordsNode.setArray(false);
-		//rootNode.addChild(recordsNode);
+		if(node.isArray()) {
+			System.out.println("isArray");
+			//ERXRestRequestNode recordsNode = new ERXRestRequestNode(ERXRestNameRegistry.registry().externalNameForInternalName(node.type()), false);
+			
+			ERXRestRequestNode recordsNode = new ERXRestRequestNode("lots", false);
+			
+			recordsNode.setArray(true);
+			rootNode.addChild(recordsNode);
 
-		//ERXRestRequestNode recordsNode = rootNode;
-		
-		if (node.isArray()) {
+			
 			for (ERXRestRequestNode child : node.children()) {
-				rootNode.addChild(child);
+				recordsNode.addChild(child);
 			}
 		}
-		else {
+		else {		
+			System.out.println("not array");
 			rootNode.addChild(node);
 		}
 
-		/*
-		ERXRestRequestNode idsNode = new ERXRestRequestNode("ids", false);
-		idsNode.setArray(true);
-		rootNode.addChild(idsNode);
-
-		for (ERXRestRequestNode child : recordsNode.children()) {
-			Object id = child.id();
-			idsNode.addChild(new ERXRestRequestNode(null, id, false));
-		}
-
-		ERXRestRequestNode countNode = new ERXRestRequestNode("count", Integer.valueOf(recordsNode.children().size()), false);
-		rootNode.addChild(countNode);
-		*/
+		
 		return rootNode;
 	}
 }
