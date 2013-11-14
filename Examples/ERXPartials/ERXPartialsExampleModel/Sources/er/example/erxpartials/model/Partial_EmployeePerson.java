40458f76fa7cfd1dfa29a6cadcf3881906602f95
Johann Werner <jw@oyosys.de>
2013-11-14 10:31:11 +0100

    add @SuppressWarnings("unused") to subclasses of EOGenericRecord where necessary

--- Examples/ERXPartials/ERXPartialsExampleModel/Sources/er/example/erxpartials/model/Partial_EmployeePerson.java
+++ Examples/ERXPartials/ERXPartialsExampleModel/Sources/er/example/erxpartials/model/Partial_EmployeePerson.java
@@ -3,5 +3,6 @@ package er.example.erxpartials.model;
 import org.apache.log4j.Logger;
 
 public class Partial_EmployeePerson extends _Partial_EmployeePerson {
+  @SuppressWarnings("unused")
   private static Logger log = Logger.getLogger(Partial_EmployeePerson.class);
 }
