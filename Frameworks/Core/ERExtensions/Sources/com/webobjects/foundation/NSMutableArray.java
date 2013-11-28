102cdc208d5ea1efeba6538eadab5c7e4dae5050
Johann Werner <jw@oyosys.de>
2013-11-28 10:27:38 +0100

    fix toString for NSMutableArray.SubList

--- Frameworks/Core/ERExtensions/Sources/com/webobjects/foundation/NSMutableArray.java
+++ Frameworks/Core/ERExtensions/Sources/com/webobjects/foundation/NSMutableArray.java
@@ -966,6 +966,30 @@ class SubList<E> extends NSMutableArray<E> {
             throw new ConcurrentModificationException();
     }
 
+    @Override
+    public String toString() {
+        int count = count();
+        if (count == 0) {
+            return "()";
+        }
+        StringBuilder sb = new StringBuilder(128);
+        sb.append('(');
+        for (int i = 0; i < count; i++) {
+            Object object = l.get(i + offset);
+            if (i > 0) {
+                sb.append(", ");
+            }
+            if (object instanceof String) {
+                sb.append('"');
+                sb.append(object);
+                sb.append('"');
+            } else {
+                sb.append(object == this ? "THIS" : object);
+            }
+        }
+        sb.append(')');
+        return sb.toString();
+    }
 }
 
 class RandomAccessSubList<E> extends SubList<E> implements RandomAccess {
