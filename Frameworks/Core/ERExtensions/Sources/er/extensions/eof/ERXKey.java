18acc0429c055a5d28f31a6aea8f61eec4a14fa2
David Avendasora <avendasorada@me.com>
2014-02-07 13:37:41 -0500

    Fix typo before Marco (and everyone else) gives me loads of crap about it.

--- Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXKey.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/eof/ERXKey.java
@@ -1505,7 +1505,7 @@ public class ERXKey<T> {
 	 * @author mschrag
 	 */
 	public static enum Type {
-		Attribute, ToOneRelationship, ToManyRelationship, Operator, NonModelAttribute, NonModelToOneRelationshiop, NonModelToManyRelationship
+		Attribute, ToOneRelationship, ToManyRelationship, Operator, NonModelAttribute, NonModelToOneRelationship, NonModelToManyRelationship
 	}
 	
 	public interface ValueCoding {
@@ -2670,7 +2670,7 @@ public class ERXKey<T> {
 	}
 	
 	public boolean isToOneRelationship() {
-		return type() == ERXKey.Type.ToOneRelationship || type() == ERXKey.Type.NonModelToOneRelationshiop;
+		return type() == ERXKey.Type.ToOneRelationship || type() == ERXKey.Type.NonModelToOneRelationship;
 	}
 	
 	public boolean isToManyRelationship() {
