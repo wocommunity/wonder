7c48a8d56a178e6036e8da2b30e6232f27ffa4ce
Henrique Prange <hprange@gmail.com>
2014-06-24 18:28:48 -0300

    Unlock editing contexts disposed by the ERXRouteController
    The ERXRouteController disposes the editing context at the end of the request handling. If the editing context is autoLocked during the request, at least one lock will remain open at disposal time (because of the coalesceAutoLocks property). This change unlocks the editing context before disposing it, avoiding inaccurate warnings when the tracing open locks option is enabled.

--- Frameworks/EOF/ERRest/Sources/er/rest/routes/ERXRouteController.java
+++ Frameworks/EOF/ERRest/Sources/er/rest/routes/ERXRouteController.java
@@ -1822,6 +1822,7 @@ public class ERXRouteController extends WODirectAction {
 	 */
 	public void dispose() {
 		if (_shouldDisposeEditingContext && _editingContext != null) {
+			ERXEC.unlockAllContextsForCurrentThread();
 			_editingContext.dispose();
 			_editingContext = null;
 		}
