2586a5a05b829ad210171049ac9cab837ba578ea
swklein <stefan.klein@buero-sde.de>
2015-04-20 10:42:51 +0200

    New JqueryMobileExample
    Conver Example to new jqm 1.4.x framework

---
+++ Examples/Ajax/JqueryMobileExample/Sources/er/jquerymobile/exampleapp/components/InfoDialog.java
@@ -0,0 +1,14 @@
+package er.jquerymobile.exampleapp.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.components.ERXComponent;
+
+public class InfoDialog extends ERXComponent {
+	private static final long serialVersionUID = 1L;
+
+	public InfoDialog(WOContext context) {
+		super(context);
+	}
+
+}
