cb54b307bf03213702ea6180d45151e3019a6157
Fabian Peters <fabian@mercadu.de>
2015-10-12 19:47:45 +0200

    Enable use of ERCPreference from ERCoreBusinessLogic to allow testing of its integration with ERModern.

---
+++ Frameworks/BusinessLogic/ERMoviesLogic/Sources/webobjectsexamples/businesslogic/movies/common/AppUser.java
@@ -0,0 +1,36 @@
+package webobjectsexamples.businesslogic.movies.common;
+
+import org.apache.log4j.Logger;
+
+import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.foundation.NSArray;
+
+import er.corebusinesslogic.ERCoreUserInterface;
+
+public class AppUser extends _AppUser implements ERCoreUserInterface {
+
+    private static final long serialVersionUID = 1L;
+
+    @SuppressWarnings("unused")
+	private static Logger log = Logger.getLogger(AppUser.class);
+
+    /*
+     * ERCore user interface implementation
+     */
+
+    @Override
+    public void newPreference(EOEnterpriseObject pref) {
+        addObjectToBothSidesOfRelationshipWithKey(pref, "preferences");
+    }
+
+    @Override
+    public void setPreferences(NSArray array) {
+        takeStoredValueForKey(array.mutableClone(), "preferences");
+    }
+
+    @Override
+    public NSArray preferences() {
+        return (NSArray) storedValueForKey("preferences");
+    }
+
+}
--- Tests/ERModernMoviesTest/Sources/er/modern/movies/test/Session.java
+++ Tests/ERModernMoviesTest/Sources/er/modern/movies/test/Session.java
@@ -1,19 +1,35 @@
 package er.modern.movies.test;
 
+import com.webobjects.eocontrol.EOEnterpriseObject;
+
+import er.corebusinesslogic.ERCoreBusinessLogic;
 import er.extensions.appserver.ERXSession;
 
 public class Session extends ERXSession {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private MoviesNavigationController _navController;
-	
-	public Session() {
-	}
-	
-	public MoviesNavigationController navController() {
-		if (_navController == null) {
-			_navController = new MoviesNavigationController(this);
-		}
-		return _navController;
-	}
+    private MoviesNavigationController _navController;
+
+    public Session() {
+    }
+
+    @Override
+    public void awake() {
+        super.awake();
+        ERCoreBusinessLogic.setActor((EOEnterpriseObject) objectStore().valueForKey(
+                "user"));
+    }
+
+    @Override
+    public void sleep() {
+        ERCoreBusinessLogic.setActor(null);
+        super.sleep();
+    }
+
+    public MoviesNavigationController navController() {
+        if (_navController == null) {
+            _navController = new MoviesNavigationController(this);
+        }
+        return _navController;
+    }
 }
