db44d682ecdedb82d666226a398d0193bf59aac6
Luiz Alfredo Carrara Bertusso <luizalfredocb@gmail.com>
2021-03-02 11:12:05 -0300

    Comment lines that were previously commented

--- Frameworks/BusinessLogic/ERAttachment/Sources/er/attachment/processors/ERAttachmentProcessor.java
+++ Frameworks/BusinessLogic/ERAttachment/Sources/er/attachment/processors/ERAttachmentProcessor.java
@@ -161,8 +161,8 @@ public abstract class ERAttachmentProcessor<T extends ERAttachment> {
     hashPathBuffer.append(filenameHash.charAt(1));
     hashPathBuffer.append('/');
     hashPathBuffer.append(filenameHash.charAt(2));
-    hashPathBuffer.append('/');
-    hashPathBuffer.append(filenameHash.substring(3));
+//    hashPathBuffer.append('/');
+//    hashPathBuffer.append(filenameHash.substring(3));
     parsedPath = parsedPath.replaceAll(ERAttachmentProcessor.HASH_VARIABLE, hashPathBuffer.toString());
 
     parsedPath = parsedPath.replaceAll(ERAttachmentProcessor.UUID_VARIABLE, UUID.randomUUID().toString());
