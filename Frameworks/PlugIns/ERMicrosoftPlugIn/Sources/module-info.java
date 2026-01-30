c850f884e7497ac53f88304510a85a838994cc38
Ramsey Gurley <ramseygurley@gmail.com>
2026-01-30 14:27:45 +0900

    Update jdbc for microsoft plugin.

--- Frameworks/PlugIns/ERMicrosoftPlugIn/Sources/module-info.java
+++ Frameworks/PlugIns/ERMicrosoftPlugIn/Sources/module-info.java
@@ -1,6 +1,6 @@
 open module org.wocommunity.wonder.jdbcadaptor.microsoft {
 	requires org.apache.commons.lang3;
-	requires mssql.jdbc;
+	requires com.microsoft.sqlserver.jdbc;
 	requires org.wocommunity.webobjects.foundation;
 	requires org.wocommunity.webobjects.eocontrol;
 	requires org.wocommunity.webobjects.eoaccess;
